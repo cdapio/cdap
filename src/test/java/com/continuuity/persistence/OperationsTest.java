3de46ae68067df4a66e62e235d32a5182ecc5975
Jonathan Gray <jon@continuuity.com>
2012-05-14 11:10:11 -0700

    ENG-181 Add some unit tests to SimpleMemoryExecutor Operations

--- src/test/java/com/continuuity/persistence/OperationsTest.java
+++ src/test/java/com/continuuity/persistence/OperationsTest.java
@@ -4,8 +4,12 @@
 package com.continuuity.persistence;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Random;
+
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +17,7 @@ import org.junit.Test;
 import com.continuuity.fabric.engine.memory.MemoryEngine;
 import com.continuuity.fabric.engine.memory.MemorySimpleExecutor;
 import com.continuuity.fabric.operations.SimpleOperationExecutor;
+import com.continuuity.fabric.operations.impl.CompareAndSwap;
 import com.continuuity.fabric.operations.impl.Read;
 import com.continuuity.fabric.operations.impl.Write;
 import com.continuuity.fabric.operations.memory.MemorySimpleOperationExecutor;
@@ -69,7 +74,7 @@ public class OperationsTest {
 	}
 
 	@Test
-	public void testAllOperationsAgainstSimpleExecutor() {
+	public void testAllOperationsAgainstSimpleMemoryExecutor() throws Exception {
     MemoryEngine memoryEngine = new MemoryEngine();
     MemorySimpleExecutor memoryExecutor =
         new MemorySimpleExecutor(memoryEngine);
@@ -78,7 +83,7 @@ public class OperationsTest {
     testOperations(memoryOperationExecutor);
 	}
 
-	private void testOperations(SimpleOperationExecutor exec) {
+	private void testOperations(SimpleOperationExecutor exec) throws Exception {
 	  testSimpleReadWrite(exec);
 	  testOrderedReadWrite(exec);
 	  testReadModifyWrite(exec);
@@ -87,33 +92,176 @@ public class OperationsTest {
 	  testQueues(exec);
 	}
 
-  private void testSimpleReadWrite(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+  private void testSimpleReadWrite(SimpleOperationExecutor exec)
+  throws Exception {
+    byte [][] keys = new byte [][] { "key0".getBytes(), "key1".getBytes() };
+    byte [][] values = new byte [][] {"value0".getBytes(), "value1".getBytes()};
+
+    Write [] writes = new Write [] {
+        new Write(keys[0], values[0]), new Write(keys[1], values[1]) };
+
+    assertTrue(exec.execute(writes));
+
+    Read [] reads = new Read [] {
+        new Read(keys[0]), new Read(keys[1]) };
+
+    byte [] value = exec.execute(reads[0]);
+    assertEquals(new String(values[0]), new String(value));
     
+    value = exec.execute(reads[1]);
+    assertEquals(new String(values[1]), new String(value));
   }
 
-  private void testOrderedReadWrite(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+  private void testCompareAndSwap(SimpleOperationExecutor exec)
+  throws Exception {
     
-  }
-
-  private void testQueues(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+    byte [] key = Bytes.toBytes("somekey");
     
-  }
+    byte [] valueOne = Bytes.toBytes("value_one");
+    byte [] valueTwo = Bytes.toBytes("value_two");
+    byte [] valueThree = Bytes.toBytes("value_three");
+    
+    // normal write value one
+    exec.execute(new Write(key, valueOne));
+    
+    // CAS to Two
+//    assertTrue(exec.execute(new CompareAndSwap(key, valueOne, valueTwo)));
+//    
+//    // Read normally, get valueTwo
+//    assertTrue(Bytes.equals(valueTwo, exec.execute(new Read(key))));
+//    
+//    // Bad CAS from One to Two
+//    assertFalse(exec.execute(new CompareAndSwap(key, valueOne, valueTwo)));
+//    
+//    // CAS(key, valueTwo, valueTwo)
+//    assertTrue(exec.execute(new CompareAndSwap(key, valueTwo, valueTwo)));
+//    
+//    // Read normally, get valueTwo
+//    assertTrue(Bytes.equals(valueTwo, exec.execute(new Read(key))));
+//    
+//    // CAS(key, valueTwo, valueThree)
+//    assertTrue(exec.execute(new CompareAndSwap(key, valueTwo, valueThree)));
+//    
+    // Read normally, get valueThree
+    // assertEquals(valueThree, read(key))
+    
+    // Bad CAS from null to two
+    // CAS(key, null, valueTwo)
+    // assertFalse
+    
+    // Read normally, get valueThree
+    // assertEquals(valueThree, read(key))
+    
+    // CAS from three to null
+    // CAS(key, valueThree, null)
+    // assertTrue
+    
+    // Read, should not exist
+    // assertNull(read(key))
+    
+    // CAS from null to one
+    // CAS(key, null, valueOne)
 
-  private void testCompareAndSwap(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+    // Read normally, get valueOne
+    // assertEquals(valueOne, read(key))
+    
+    
+    byte [] valueChainKey = Bytes.toBytes("chainkey");
+    byte [][] valueChain = generateRandomByteArrays(20, 20); 
+    
+    // CompareAndSwap the first one, expecting null
+    
+    // CAS(valueChainKey, null, valueChain[0])
+    
+    // CAS down the chain
+    for (int i=1; i<valueChain.length; i++) {
+      // CAS(valueChainKey, valueChain[i-1], valueChain[i])
+      // assert true
+      // attempt CAS(valueChainKey, valueChain[i-1], valueChain[i])
+      // assert false
+    }
+    
+    // Verify the current value is the last in the chain
+    // assert equals (valueChain[valueChain.length-1], read(valueChainKey))
     
   }
 
   private void testIncrement(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+
+    
+    byte [][] keys = generateRandomByteArrays(10, 8);
+    
+    // increment first half of keys by 1
+    for (int i=0; i<keys.length/2; i++) {
+      // increment keys[i]
+    }
+    
+    // iterate all keys, only first half should have value of 1, others 0
+    for (int i=0; i<keys.length; i++) {
+      // read keys[i]
+      if (i < keys.length/2) {
+        // assertEquals(1, keys[i])
+      } else {
+        // assertEquals(0, keys[i])
+      }
+    }
+    
+    // decrement first half, everything should be 0
+    for (int i=0; i<keys.length/2; i++) {
+      // decrement keys[i]
+    }
+
+    for (int i=0; i<keys.length; i++) {
+      // assertEquals(0, keys[i])
+    }
+    
+    // increment each by their value of i
+    for (int i=0; i<keys.length; i++) {
+      // increment(keys[i], i)
+    }
+    
+    // read them back backwards, expecting their amount to = their position
+    for (int i=keys.length-1; i>=0; i--) {
+      // assertEquals(i, keys[i])
+    }
+    
+    // increment each by the total number minus their position
+    for (int i=0; i<keys.length; i++) {
+      int amount = keys.length - i;
+      // increment(keys[i], amount)
+    }
+    
+    // read them back, all should have the same value of keys.length
+    for (int i=0; i<keys.length; i++) {
+      // assertEquals(keys.length, keys[i])
+    }
     
   }
 
   private void testReadModifyWrite(SimpleOperationExecutor exec) {
-    // TODO Auto-generated method stub
+    // TODO Implement read-modify-write test
     
   }
+
+  private void testOrderedReadWrite(SimpleOperationExecutor exec) {
+    // TODO Implement ordered read-write test
+  }
+
+  private void testQueues(SimpleOperationExecutor exec) {
+    // TODO Implement queue test
+  }
+  
+  // Private helpers
+
+  private static final Random rand = new Random();
+
+  private byte[][] generateRandomByteArrays(int num, int length) {
+    byte [][] bytes = new byte[num][];
+    for (int i=0;i<num;i++) {
+      bytes[i] = new byte[length];
+      rand.nextBytes(bytes[i]);
+    }
+    return bytes;
+  }
+  
 }
