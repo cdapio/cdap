c5d1e659520b117e472967bf50509e06959ca588
Jonathan Gray <jon@continuuity.com>
2012-05-15 23:50:17 -0700

    ENG-236 Memory queue functional and basic unit tests, still more to go

--- src/main/java/com/continuuity/fabric/engine/memory/MemoryQueue.java
+++ src/main/java/com/continuuity/fabric/engine/memory/MemoryQueue.java
@@ -1,7 +1,8 @@
 package com.continuuity.fabric.engine.memory;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.continuuity.fabric.operations.queues.QueueConsumer;
@@ -10,15 +11,18 @@ import com.continuuity.fabric.operations.queues.QueuePartitioner;
 
 public class MemoryQueue {
 
-  private Entry head;
-  private Entry tail;
+  /** Maximum default timeout of 5 minutes (can be changed by tests) */
+  static long TIMEOUT = 5 * 60 * 1000;
+  
+  Entry head;
+  Entry tail;
 
   /** Used for locking, generating ids, and tracking the total queue size */
   private final AtomicLong entryIds = new AtomicLong(0);
 
   /** Map from GroupID to GroupHead */
-  private final Map<Integer,Entry> consumerGroupHeads =
-      new HashMap<Integer,Entry>();
+  private final ConcurrentHashMap<Integer,ConsumerGroup> consumerGroups =
+      new ConcurrentHashMap<Integer,ConsumerGroup>();
 
   public MemoryQueue() {
     this.head = null;
@@ -48,41 +52,94 @@ public class MemoryQueue {
       return pop(consumer, partitioner);
     }
     
-    // Determine the current head for this consumer group
-    Entry groupHead = null;
-    if (!this.consumerGroupHeads.containsKey(consumer.getGroupId())) {
+    // Determine the current state for this consumer group
+    ConsumerGroup group = null;
+    if (!this.consumerGroups.containsKey(consumer.getGroupId())) {
       // This group has not consumed before, set group head = head
-      this.consumerGroupHeads.put(consumer.getConsumerId(), head);
-      groupHead = head;
+      group = new ConsumerGroup(consumer.getGroupId());
+      group.setHead(head);
+      ConsumerGroup existingGroup = this.consumerGroups.putIfAbsent(
+          consumer.getGroupId(), group);
+      if (existingGroup != null) {
+        // Someone else added the group concurrently with us, use theirs
+        group = existingGroup;
+      }
     } else {
-      groupHead = this.consumerGroupHeads.get(consumer.getConsumerId());
+      group = this.consumerGroups.get(consumer.getConsumerId());
     }
     
-    // If groupHead is null, we are at the end, wait for a push
-    if (groupHead == null) {
+    // If group has no entries available, wait for a push
+    if (!group.hasEntriesAvailable()) {
       waitForPush();
       return pop(consumer, partitioner);
     }
     
     // Iterate entries to see if we should emit one
-    while (groupHead != null) {
-      QueueEntry entry = groupHead.makeQueueEntry();
-      if (partitioner.shouldEmit(consumer, entry)) {
-        entry.setConsumer(consumer);
-        return entry;
+    synchronized (group) {
+      Entry curEntry = group.getHead();
+      while (curEntry != null) {
+        // Check if this is already assigned to someone else in the same group
+        // or if it's already assigned to us
+        GroupConsumptionInfo info =
+            curEntry.getConsumerInfo(consumer.getGroupId());
+        if (info.isAvailable() ||
+            info.getConsumerId() == consumer.getConsumerId()) {
+          QueueEntry entry = curEntry.makeQueueEntry();
+          if (partitioner.shouldEmit(consumer, entry)) {
+            entry.setConsumer(consumer);
+            info.setPopConsumer(consumer);
+            return entry;
+          }
+        }
+        // Entry is taken or not applicable to this consumer, iterate to next
+        curEntry = curEntry.getNext();
       }
-      groupHead = groupHead.getNext();
     }
-    
+      
     // Didn't find anything.  Wait and try again.
     waitForPush();
     return pop(consumer, partitioner);
   }
 
   public boolean ack(QueueEntry entry) {
-
+    // Queue should not be empty
+    if (this.head == null) return false;
     
-    // TODO Auto-generated method stub
+    QueueConsumer consumer = entry.getConsumer();
+
+    // Group info should not be null
+    ConsumerGroup group = this.consumerGroups.get(consumer.getGroupId());
+    if (group == null) return false;
+    
+    synchronized (group) {
+      Entry curEntry = group.getHead();
+      while (curEntry != null) {
+        // Check if this is the entry we want to ack
+        GroupConsumptionInfo info =
+            curEntry.getConsumerInfo(consumer.getGroupId());
+        if (info.isPopped() &&
+            info.getConsumerId() == consumer.getConsumerId()) {
+          // Verified it was previously popped and owned by us, ack
+          info.ack();
+          // See if we need to update HEAD pointer
+          if (curEntry.equals(group.getHead())) {
+            // We just ACKd the head, try move the head down
+            curEntry = curEntry.getNext();
+            while (curEntry != null) {
+              group.setHead(curEntry);
+              if (curEntry.getConsumerInfo(consumer.getGroupId()).isAcked()) {
+                curEntry = curEntry.getNext();
+              } else {
+                break;
+              }
+            }
+          }
+          return true;
+        }
+        // Entry is taken or not applicable to this consumer, iterate to next
+        curEntry = curEntry.getNext();
+      }
+    }
     return false;
   }
 
@@ -95,29 +152,128 @@ public class MemoryQueue {
     }
   }
 
+  class ConsumerGroup {
+    private final int id;
+    private Entry head;
+    private boolean empty;
+    ConsumerGroup(int id) {
+      this.id = id;
+      this.head = null;
+    }
+    public void setEmpty(boolean b) {
+      this.empty = true;
+    }
+    public boolean hasEntriesAvailable() {
+      return !empty;
+    }
+    public void setHead(Entry head) {
+      this.head = head;
+    }
+    public Entry getHead() {
+      return this.head;
+    }
+    int getId() {
+      return this.id;
+    }
+  }
+
   class Entry {
     private final byte [] value;
     private final long id;
     private Entry next;
+    
+    private final Map<Integer,GroupConsumptionInfo> consumers =
+        new TreeMap<Integer,GroupConsumptionInfo>();
+
     Entry(byte [] value, long id) {
       this.value = value;
       this.id = id;
       this.next = null;
     }
-    public byte [] getValue() {
+    byte [] getValue() {
       return this.value;
     }
-    public long getId() {
+    long getId() {
       return this.id;
     }
-    public void setNext(Entry entry) {
+    void setNext(Entry entry) {
       this.next = entry;
     }
-    public Entry getNext() {
+    Entry getNext() {
       return this.next;
     }
-    public QueueEntry makeQueueEntry() {
+    synchronized GroupConsumptionInfo getConsumerInfo(int groupId) {
+      GroupConsumptionInfo info = this.consumers.get(groupId);
+      if (info == null) {
+        info = new GroupConsumptionInfo();
+        this.consumers.put(groupId, info);
+      }
+      return info;
+    }
+    QueueEntry makeQueueEntry() {
       return new QueueEntry(this.value, this.id);
     }
+    public boolean equals(Entry other) {
+      if (other.getId() == getId()) return true;
+      return false;
+    }
+  }
+  
+  class GroupConsumptionInfo {
+    private int consumerId;
+    private long stamp;
+    private ConsumptionState state;
+    GroupConsumptionInfo() {
+      this.consumerId = -1;
+      this.state = ConsumptionState.AVAILABLE;
+      this.updateStamp();
+    }
+    /**
+     * @return
+     */
+    public boolean isAcked() {
+      return this.state == ConsumptionState.ACKED;
+    }
+    public void ack() {
+      this.state = ConsumptionState.ACKED;
+      updateStamp();
+    }
+    public boolean isPopped() {
+      return this.state == ConsumptionState.POPPED;
+    }
+    /**
+     * @param consumer
+     */
+    public void setPopConsumer(QueueConsumer consumer) {
+      this.consumerId = consumer.getConsumerId();
+      this.state = ConsumptionState.POPPED;
+      updateStamp();
+    }
+    boolean isAvailable() {
+      if (this.state == ConsumptionState.AVAILABLE ||
+          this.state == ConsumptionState.TIMED_OUT) {
+        return true;
+      }
+      if (this.state == ConsumptionState.POPPED &&
+          this.stamp < (System.currentTimeMillis() - TIMEOUT)) {
+        // Timed out!
+        this.state = ConsumptionState.TIMED_OUT;
+        return true;
+      }
+      return false;
+    }
+    public long getStamp() {
+      return stamp;
+    }
+    public void updateStamp() {
+      this.stamp = System.currentTimeMillis();
+    }
+    public int getConsumerId() {
+      return consumerId;
+    }
+  }
+  
+  enum ConsumptionState {
+    AVAILABLE, POPPED, ACKED, TIMED_OUT
   }
 }
--- src/test/java/com/continuuity/fabric/engine/memory/TestMemoryQueue.java
+++ src/test/java/com/continuuity/fabric/engine/memory/TestMemoryQueue.java
@@ -1,11 +1,13 @@
 package com.continuuity.fabric.engine.memory;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
@@ -17,7 +19,7 @@ import com.continuuity.fabric.operations.queues.QueuePartitioner;
 public class TestMemoryQueue {
 
   @Test
-  public void testSingleConsumer() throws Exception {
+  public void testSingleConsumerSimple() throws Exception {
     MemoryQueue queue = new MemoryQueue();
 
     byte [] valueOne = Bytes.toBytes("value1");
@@ -40,26 +42,152 @@ public class TestMemoryQueue {
     assertTrue(Bytes.equals(entry.getValue(), valueOne));
 
     // ack
-//    assertTrue(queue.ack(entry));
-//
-//    // pop, should get second value
-//    entry = queue.pop(consumer, partitioner);
-//    assertTrue(Bytes.equals(entry.getValue(), valueTwo));
-//
-//    // ack
-//    assertTrue(queue.ack(entry));
+    assertTrue(queue.ack(entry));
 
-//    // spawn a thread to pop
-//    QueuePopper popper = new QueuePopper(queue, consumer, partitioner);
-//    popper.pop();
-//
-//    // nothing in queue so popper should be empty
-//    assertTrue(popper.isEmpty());
-//
-//
+    // pop, should get second value
+    entry = queue.pop(consumer, partitioner);
+    assertTrue(Bytes.equals(entry.getValue(), valueTwo));
+
+    // ack
+    assertTrue(queue.ack(entry));
   }
 
+  @Test
+  public void testSingleConsumerThreaded() throws Exception {
+    MemoryQueue queue = new MemoryQueue();
 
+    byte [] valueOne = Bytes.toBytes("value1");
+    byte [] valueTwo = Bytes.toBytes("value2");
+
+
+    // pop it with the single consumer and random partitioner
+    QueueConsumer consumer = new QueueConsumer(0, 0, 1);
+    QueuePartitioner partitioner = new QueuePartitioner.RandomPartitioner();
+    
+    // spawn a thread to pop
+    QueuePopper popper = new QueuePopper(queue, consumer, partitioner);
+    popper.start();
+    
+    // popper should be empty
+    assertNull(popper.nonBlockPop());
+    
+    // trigger a pop
+    assertTrue(popper.triggerPop());
+
+    // nothing in queue so popper should be empty
+    QueueEntry entry = popper.blockPop(100);
+    assertNull(entry);
+
+    // push
+    assertTrue(queue.push(valueOne));
+    assertNotNull(queue.pop(consumer, partitioner));
+    
+    Thread.sleep(1000);
+    // popper should hit now
+    entry = popper.blockPop(100);
+    assertNotNull(entry);
+    assertTrue(Bytes.equals(entry.getValue(), valueOne));
+    
+    // shut down
+    popper.shutdown();
+  }
+
+  static class QueuePopper extends Thread {
+    
+    private final MemoryQueue queue;
+    private final QueueConsumer consumer;
+    private final QueuePartitioner partitioner;
+    
+    private AtomicBoolean popTrigger = new AtomicBoolean(false);
+    
+    private QueueEntry entry;
+    
+    private boolean keepgoing = true;
+    
+    /**
+     * @param queue
+     * @param consumer
+     * @param partitioner
+     */
+    QueuePopper(MemoryQueue queue, QueueConsumer consumer,
+        QueuePartitioner partitioner) {
+      this.queue = queue;
+      this.consumer = consumer;
+      this.partitioner = partitioner;
+      this.entry = null;
+    }
+
+    QueueEntry nonBlockPop() {
+      if (this.entry == null) return null;
+      QueueEntry ret = this.entry;
+      this.entry = null;
+      return ret;
+    }
+    
+    QueueEntry blockPop(long timeout) {
+      QueueEntry entry = nonBlockPop();
+      if (entry != null) return entry;
+      long cur = System.currentTimeMillis();
+      long end = cur + timeout;
+      while (entry == null && cur < end) {
+        synchronized (popTrigger) {
+          try {
+            popTrigger.wait(timeout);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        entry = nonBlockPop();
+        cur = System.currentTimeMillis();
+      }
+      return entry;
+    }
+    /**
+     * trigger the thread to pop.
+     * @return true if a pop was triggered, false if not
+     */
+    boolean triggerPop() {
+      synchronized (popTrigger) {
+        if (popTrigger.get()) {
+          return false;
+        }
+        popTrigger.set(true);
+        popTrigger.notifyAll();
+        return true;
+      }
+    }
+    
+    public void shutdown() {
+      this.keepgoing = false;
+      this.interrupt();
+    }
+
+    @Override
+    public void run() {
+      while (keepgoing) {
+        synchronized (popTrigger) {
+          while (!popTrigger.get() && keepgoing) {
+            try {
+              popTrigger.wait();
+            } catch (InterruptedException e) {
+              if (keepgoing) e.printStackTrace();
+            }
+          }
+          popTrigger.set(false);
+        }
+        if (this.entry != null) continue;
+        QueueEntry entry = null;
+        while (entry == null && keepgoing) {
+          try {
+            entry = queue.pop(consumer, partitioner);
+          } catch (InterruptedException e) {
+            if (keepgoing) e.printStackTrace();
+          }
+        }
+        this.entry = entry;
+      }
+    }
+  }
   @Test
   public void testHashMapBehavior() {
     byte [] value = Bytes.toBytes("value");
