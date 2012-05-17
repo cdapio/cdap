9744ec4e50bf18f3a80b3d3ccd096498f02bf55d
Nitin Motgi <nitin@continuuity.com>
2012-05-16 20:46:41 -0700

    ENG-166 Adding in unique id generator from snowflake

---
+++ src/main/java/com/continuuity/common/conflake/UniqueIDGenerator.java
@@ -0,0 +1,79 @@
+package com.continuuity.common.conflake;
+
+/**
+ * Is a distributed unique ID generator. The implementation is taken from
+ * twitter snowflake project.
+ *
+ * <p>
+ * Following are the properties of the IDs generated
+ * <ul>
+ *   <li>ID generation could be distributed across multiple data centers and multiple workers within a data center</li>
+ *   <li>The numbers generated are roughly time ordered.</li>
+ *   <li>The numbers are 64 bits, so they are compact and directly sortable.</li>
+ * </ul>
+ * </p>
+ *
+ */
+public final class UniqueIDGenerator {
+  private final long twepoch = 1288834974657L;
+
+  private final long workerIdBits = 5L;
+  private final long datacenterIdBits = 5L;
+  private final long sequenceBits = 12L;
+
+  private final long workerIdShift = sequenceBits;
+  private final long datacenterIdShift = sequenceBits + workerIdBits;
+  private final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+  private final long sequenceMask = -1L ^ (-1L << sequenceBits);
+
+
+  private final long workerId;
+  private final long datacenterId;
+
+  private long sequence;
+  private long lastTimestamp = -1L;
+
+  /**
+   * Creates an instance of UniqueIDGenerator generator.
+   * @param datacenterId unique id that in the range of 0-4096 (2^0 - 2^5)
+   * @param workerId unique id associated with the worker serving the ids (0-4096)
+   */
+  public UniqueIDGenerator(long datacenterId, long workerId) {
+    this.workerId = workerId;
+    this.datacenterId = datacenterId;
+    this.sequence = 0;
+  }
+
+  private long tilNextMillis(long lastTimestamp) {
+    long timestamp = System.currentTimeMillis();
+    while(timestamp <= lastTimestamp) {
+      timestamp = System.currentTimeMillis();
+    }
+    return timestamp;
+  }
+
+  /**
+   * Returns the next unique monotonically increasing number.
+   * @return next unique id.
+   */
+  public long next() {
+    long timestamp = System.currentTimeMillis();
+
+    if(lastTimestamp == timestamp) {
+      sequence = (sequence + 1) & sequenceMask;
+      if(sequence == 0)  {
+        timestamp = tilNextMillis(lastTimestamp);
+      }
+    } else {
+      sequence = 0;
+    }
+
+    lastTimestamp = timestamp;
+
+    long id  = ((timestamp - twepoch) << timestampLeftShift)
+      | (datacenterId << datacenterIdShift) | (workerId << workerIdShift) | sequence;
+    sequence++;
+    return id;
+  }
+
+}
---
+++ src/test/java/com/continuuity/common/conflake/UniqueIDGeneratorTest.java
@@ -0,0 +1,54 @@
+package com.continuuity.common.conflake;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ *
+ */
+public class UniqueIDGeneratorTest {
+
+  public class IDGenerator implements Runnable {
+    private final UniqueIDGenerator idGenerator;
+    private final List<Long> clash = new ArrayList<Long>();
+
+    public IDGenerator(long datacenterId, long workerId) {
+      idGenerator = new UniqueIDGenerator(datacenterId, workerId);
+    }
+
+    @Override
+    public void run() {
+      for(int i = 0; i < 10000; ++i) {
+        long id = idGenerator.next();
+        clash.add(id);
+      }
+    }
+
+    Collection getClashMap() {
+      return clash;
+    }
+  }
+
+  @Test
+  public void testSimple() throws Exception {
+    IDGenerator id1 = new IDGenerator(1, 1);
+    IDGenerator id2 = new IDGenerator(1, 2);
+
+    Thread thread1 = new Thread(id1);
+    Thread thread2 = new Thread(id2);
+
+    thread1.start();
+    thread2.start();
+
+    thread1.join();
+    thread2.join();
+
+    Collection gen1 = id1.getClashMap();
+    Collection gen2 = id2.getClashMap();
+
+    gen1.retainAll(gen2);
+    Assert.assertTrue(gen1.size() == 0);
+  }
+}
