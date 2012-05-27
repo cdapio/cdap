65a44b46652d15382e1308b502e5cb0e2a1416b9
Eric Scheie <eric@continuuity.com>
2012-05-26 19:35:38 -0700

    First cut at a continuutiy gradle plugin that can be shared between all repos.

---
+++ buildSrc/src/main/groovy/com/continuuity/gradle/ContinuuityPlugin.groovy
@@ -0,0 +1,68 @@
+package com.continuuity.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Eric
+ * Date: 5/26/12
+ * Time: 5:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ContinuuityPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project t) {
+        println "Continuuity Build System"
+
+        ConfigurableStreamHandlerFactory streamHandlerFactory = new ConfigurableStreamHandlerFactory("classpath",
+                new ClasspathHandler());
+        URL.setURLStreamHandlerFactory(streamHandlerFactory);
+
+        Map map = new HashMap();
+        map.put("from", new URL("classpath:com/continuuity/gradle/common.gradle"));
+        t.apply(map);
+
+        map.put("from", new URL("classpath:com/continuuity/gradle/sonar.gradle"));
+        t.apply(map);
+
+        map.put("from", new URL("classpath:com/continuuity/gradle/clover.gradle"));
+        t.apply(map);
+    }
+
+    /** A {@link URLStreamHandler} that handles resources on the classpath. */
+    public class ClasspathHandler extends URLStreamHandler {
+        /** The classloader to find resources from. */
+        private final ClassLoader classLoader;
+
+        public ClasspathHandler() {
+            this.classLoader = getClass().getClassLoader();
+        }
+
+        @Override
+        protected URLConnection openConnection(URL u) throws IOException {
+            String scriptPath = u.getHost() + u.getPath();
+            //println "Loading gradle script: $scriptPath"
+            final URL resourceUrl = classLoader.getResource(scriptPath);
+            return resourceUrl.openConnection();
+        }
+    }
+
+    class ConfigurableStreamHandlerFactory implements URLStreamHandlerFactory {
+        private final Map<String, URLStreamHandler> protocolHandlers;
+
+        public ConfigurableStreamHandlerFactory(String protocol, URLStreamHandler urlHandler) {
+            protocolHandlers = new HashMap<String, URLStreamHandler>();
+            addHandler(protocol, urlHandler);
+        }
+
+        public void addHandler(String protocol, URLStreamHandler urlHandler) {
+            protocolHandlers.put(protocol, urlHandler);
+        }
+
+        public URLStreamHandler createURLStreamHandler(String protocol) {
+            return protocolHandlers.get(protocol);
+        }
+    }
+}
