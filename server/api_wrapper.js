3daa9605a76df324a70812949b1205c5c2792427
Don Mosites <dmosites@Dons-MacBook-Air.local>
2012-05-30 18:03:46 -0700

    Adding thrift impl for engine interface

--- server/api_wrapper.js
+++ server/api_wrapper.js
@@ -1,30 +1,90 @@
 
 var http = require('http');
+var thrift = require('thrift');
+
+var ManagerService = require('./gen-nodejs/ManagerService.js'),
+	MonitorService = require('./gen-nodejs/MonitorService.js'),
+	SubscriptionService = require('./gen-nodejs/SubscriptionService.js'),
+	engine_types = require('./gen-nodejs/engine_types.js');
 
 (function () {
 
 	this.configure = function (host, port) {
-		this.HOST = host;
-		this.PORT = port;
+
+		this.connection = thrift.createConnection(host, port);
+		this.connection.on('error', function (error) {
+			console.log('THRIFT ERROR', error);
+		});
+
+		this.Manager = thrift.createClient(ManagerService, this.connection);
+		this.Monitor = thrift.createClient(MonitorService, this.connection);
+
+		Subscriber.start();
+
 	};
 
 	this.request = function (method, params, done) {
-		var client = http.createClient(this.PORT, this.HOST);
-		var request = client.request('GET', '/' + method + '/' + (params || ''));
-		request.on('response', function (response) {
-			var data = [];
-			response.setEncoding('utf8');
-			response.on('data', function (chunk) {
-				data.push(chunk);
-			});
-			response.on('end', function () {
-				done(data.join(""));
-			});
-		});
-		request.end();
+		
+		if (method in this.Manager) {
+			done('Unknown method for service Manager: ' + method, null);
+		} else {
+			this.Manager[method].apply(this.Manager, params.concat(done));
+		}
+
 	};
 
-	this.subscribe = function (object, callback) {
+	this.status = function (params, done) {
+
+		this.Monitor.getFlowStatus(params.concat(done));
+
+	};
+
+	var Subscriber = {
+		server: thrift.createServer(SubscriptionService, {
+			fire: function (id, event) {
+				this.map[id](event);
+				return true;
+			},
+			ping: function () {
+				return 'Pong';
+			}
+		}),
+		start: function () {
+			this.server.listen(1000);
+		},
+		register: function (id, callback) {
+			this.map[id] = callback;
+		},
+		unregister: function (id) {
+			delete this.map[id];
+		}
+	};
+
+	this.subscribe = function (ns, name, endpoint, callback) {
+
+		this.Monitor.subscribe(ns, name, endpoint, function (error, response) {
+
+			if (error !== null) {
+				console.log('SUB ERROR', ns, name, endpoint, error);
+			} else {
+				Subscriber.register(response.id, callback);
+			}
+
+		});
+
+	};
+
+	this.unsubscribe = function (ns, name) {
+
+		this.Monitor.unsubscribe(ns, name, function (error, response) {
+
+			if (error !== null) {
+				console.log('UNSUB ERROR', ns, name, error);
+			} else {
+				Subscriber.unregister(response.id);
+			}
+
+		});
 
 	};
 
--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
-var express = require('express'),
+var Env, express = require('express'),
 	app = express.createServer(express.basicAuth(function (u, p) {
 		return Env.USERNAME === u && Env.PASSWORD === p;
 	})),
 	io = require('socket.io').listen(app);
 
-var Env = require('./env');
+Env = require('./env');
 Env.configure(app, express, io);
 
 app.listen(Env.PORT);
@@ -13,7 +13,7 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('rest', function (request) {
 		console.log('Received', request);
-		Env.api.request(request.method, request.params, function (response) {
+		Env.api.request(request.method, request.params, function (error, response) {
 			console.log('Responding', response);
 			socket.emit('exec', {
 				method: request.method,
