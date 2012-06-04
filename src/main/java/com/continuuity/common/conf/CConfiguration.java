fbd1c947cd9096bfcdad6f3ca09bda0e93b79bef
Todd Papaioannou <todd@continuuity.com>
2012-06-03 18:55:55 -0700

    CConfiguration now will no longer be initialized with all the Hadoop default properties.

--- src/main/java/com/continuuity/common/conf/CConfiguration.java
+++ src/main/java/com/continuuity/common/conf/CConfiguration.java
@@ -7,42 +7,65 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Extension of Hadoop configuration to include Continuuity related
- * configurations.
+ * CConfiguration is an extension of the Hadoop Configuration class. By default,
+ * this class attempts to look for configuration options in the following files:
+ * TODO: Do we really need two files, named like this?
+ * * <ul>
+ *   <li>continuuity-default.xml</li>
+ *   <li>continuuity-site.xml</li>
+ * </ul>
+ *
+ * <strong>Note:</strong> This class will lazily load any configuration
+ * properties, therefore you will not be able to access them until you
+ * have called one of the getXXX methods at least once.
  */
 @InterfaceAudience.Public
 @InterfaceStability.Stable
 public class CConfiguration extends Configuration {
   @SuppressWarnings("unused")
-  private static final Logger LOG = LoggerFactory.getLogger(CConfiguration.class);
+  private static final Logger LOG =
+      LoggerFactory.getLogger(CConfiguration.class);
 
   /**
-   * Adds continuuity related configuration files.
-   * @param conf
+   * Adds Continuuity related configuration files to the Configuration object.
+   *
+   * @param conf  The configuration object to add the resources to.
    * @return Configuration object.
    */
   public static Configuration addResources(Configuration conf) {
+
+    // Clear any default properties
     conf.clear();
+
+    // Load our Continuuity resources
     conf.addResource("continuuity-default.xml");
     conf.addResource("continuuity-site.xml");
+
     return conf;
   }
 
   /**
    * Creates an instance of configuration.
+   *
    * @return an instance of CConfiguration.
    */
   public static Configuration create() {
-    Configuration conf = new Configuration();
+
+    // Create a new configuration instance, but do NOT initialize with
+    // the Hadoop default properties.
+    Configuration conf = new Configuration(false);
+
     return addResources(conf);
   }
 
   /**
-   * Creates an instance of configuration.
-   * @param that Other configuration.
-   * @return merged CConfiguration instance.
+   * Clone a Configuration and create a new instance from existing values.
+   *
+   * @param that  Other configuration.
+   * @return A cloned Configuration instance.
    */
-  public static Configuration create(final Configuration that) {
+  public static Configuration clone(final Configuration that) {
     return new Configuration(that);
   }
-}
+
+} // end of CConfiguration class
