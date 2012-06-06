1f824f9e5f0b17f22b93d846a9b5692481823b6a
Jonathan Gray <jon@continuuity.com>
2012-06-06 14:53:59 -0700

    ENG-274 Added javadoc to VCTable interface

--- src/main/java/com/continuuity/data/table/VersionedColumnarTable.java
+++ src/main/java/com/continuuity/data/table/VersionedColumnarTable.java
@@ -4,31 +4,133 @@ import java.util.Map;
 
 import com.continuuity.common.utils.ImmutablePair;
 
+/**
+ * A core table interface that supports columnar and versioned tables.
+ *
+ * Columnar implies that a row can have any number of dynamically inserted
+ * columns (column-oriented).  Columns are sorted in ascending binary order.
+ *
+ * Versioned implies that every row+column can have multiple versions and these
+ * can be used with a {@link ReadPointer} to provide visibility constraint
+ * possibilities (ie. for use with a transactional system).
+ */
 public interface VersionedColumnarTable {
 
+  /**
+   * Writes the specified value at the specified version for the specified
+   * row and column.
+   * @param row
+   * @param column
+   * @param version
+   * @param value
+   */
   public void put(byte [] row, byte [] column, long version, byte [] value);
 
+  /**
+   * Writes the specified values for the specified columns at the specified
+   * version for the specified row.
+   * @param row
+   * @param columns
+   * @param version
+   * @param values
+   */
   public void put(byte [] row, byte [][] columns, long version,
       byte [][] values);
 
+  /**
+   * Deletes all versions of the specified row and column that have a version
+   * less than or equal to the specified version.
+   * @param row
+   * @param column
+   * @param version
+   */
   public void delete(byte [] row, byte [] column, long version);
 
+  /**
+   * Reads the latest version of all columns in the specified row, utilizing
+   * the specified read pointer to enforce visibility constraints.
+   * @param row
+   * @param readPointer
+   * @return map of columns to values
+   */
   public Map<byte [], byte []> get(byte [] row, ReadPointer readPointer);
 
+  /**
+   * Reads the latest version of the specified column in the specified row,
+   * utilizing the specified read pointer to enforce visibility constraints,
+   * and returns the value.
+   * @param row
+   * @param column
+   * @param readPointer
+   * @return value of the latest visible column in the specified row
+   */
   public byte [] get(byte [] row, byte [] column, ReadPointer readPointer);
 
+  /**
+   * Reads the latest version of the specified column in the specified row,
+   * utilizing the specified read pointer to enforce visibility constraints,
+   * and returns both the value as well as the version this value exists at.
+   * @param row
+   * @param column
+   * @param readPointer
+   * @return value and version of the latest visible column in the specified row
+   */
   public ImmutablePair<byte[],Long> getWithVersion(byte [] row, byte [] column,
       ReadPointer readPointer);
 
+  /**
+   * Reads the latest versions of all columns in the specified row that are
+   * between the specified start (inclusive) and stop (exclusive) columns,
+   * utilizing the specified read pointer to enforce visibility constraints.
+   * @param row
+   * @param startColumn beginning of range of columns, inclusive
+   * @param stopColumn end of range of columns, exclusive
+   * @param readPointer
+   * @return map of columns to values
+   */
   public Map<byte [], byte []> get(byte [] row, byte [] startColumn,
       byte [] stopColumn, ReadPointer readPointer);
 
+  /**
+   * Reads the latest versions of the specified columns in the specified row,
+   * utilizing the specified read pointer to enforce visibility constraints.
+   * @param row
+   * @param columns
+   * @param readPointer
+   * @return map of columns to values
+   */
   public Map<byte [], byte []> get(byte [] row, byte [][] columns,
       ReadPointer readPointer);
 
+  /**
+   * Increments (atomically) the specified row and column by the specified
+   * amount, utilizing the specified read pointer to enforce visibility
+   * constraints when performing the initial read.  The specified write version
+   * will be used when performing the post-incremented write.
+   * @param row
+   * @param column
+   * @param amount amount to increment column by
+   * @param readPointer
+   * @param writeVersion
+   * @return value of counter after this increment is performed
+   */
   public long increment(byte [] row, byte [] column, long amount,
       ReadPointer readPointer, long writeVersion);
 
+  /**
+   * Compares-and-swaps (atomically) the value of the specified row and column
+   * by looking for the specified expected value and if found, replacing with
+   * the specified new value.  Utilizes the specified read pointer to enforce
+   * visibility constraints on the read, utilizes the specified write version
+   * to perform 
+   * @param row
+   * @param column
+   * @param expectedValue
+   * @param newValue
+   * @param readPointer
+   * @param writeVersion
+   * @return
+   */
   public boolean compareAndSwap(byte [] row, byte [] column,
       byte [] expectedValue, byte [] newValue, ReadPointer readPointer,
       long writeVersion);
