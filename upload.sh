c790316d0c18324ce9d686a4a8bba9f22781f9cc
Don Mosites <don@continuuity.com>
2012-06-08 01:28:05 -0700

    Adding npm module link to gradle build

--- upload.sh
+++ upload.sh
@@ -4,8 +4,7 @@
 ssh-add ./deploy_key
 
 rsync -v -r --delete ./build deploy@staging.continuuity.com:~/
-rsync -v -r --delete --exclude="node_modules/*" ./server deploy@staging.continuuity.com:~/
 
-ssh deploy@staging.continuuity.com 'sudo npm link ./server'
+ssh deploy@staging.continuuity.com 'sudo npm link build/server'
 ssh deploy@staging.continuuity.com 'sudo killall node'
-ssh deploy@staging.continuuity.com 'sudo NODE_ENV=devstaging node ./server/main.js </dev/null >/dev/null 2>&1 &'
+ssh deploy@staging.continuuity.com 'sudo NODE_ENV=staging build/node build/server/main.js </dev/null >/dev/null 2>&1 &'
