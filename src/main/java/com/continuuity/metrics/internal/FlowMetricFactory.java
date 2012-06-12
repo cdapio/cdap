570db0c151cb6865b93716db7eb1b23b9124812f
Nitin Motgi <nitin@continuuity.com>
2012-06-11 23:04:43 -0700

    ENG-317 ENG-302 Adding metrics to flowlet processors

--- src/main/java/com/continuuity/metrics/internal/FlowMetricFactory.java
+++ src/main/java/com/continuuity/metrics/internal/FlowMetricFactory.java
@@ -7,7 +7,7 @@ import com.google.common.collect.ImmutableMap;
  *
  */
 public final class FlowMetricFactory {
-  public static CMetrics newFlowMetrics(ImmutableMap<String, String> fields, String group, String type) {
-    return new FlowMetrics(new FlowMetricsContext(fields), group, type);
+  public static CMetrics newFlowMetrics(ImmutableMap<String, String> fields, String type) {
+    return new FlowMetrics(new FlowMetricsContext(fields), "flow", type);
   }
 }
