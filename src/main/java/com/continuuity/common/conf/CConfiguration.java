9a1442d9fbb27db11baf9bb0472c815fcc2e5ff1
Andreas Neumann <andreas@continuuity.com>
2012-06-18 15:49:50 -0700

    change CConfiguration to use sub projects instead of continuuity-site/default

--- src/main/java/com/continuuity/common/conf/CConfiguration.java
+++ src/main/java/com/continuuity/common/conf/CConfiguration.java
@@ -8,12 +8,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * CConfiguration is an extension of the Hadoop Configuration class. By default,
- * this class attempts to look for configuration options in the following files:
- * TODO: Do we really need two files, named like this?
- * * <ul>
- *   <li>continuuity-default.xml</li>
- *   <li>continuuity-site.xml</li>
- * </ul>
+ * this class provides a empty configuration. To add a set of resources from an
+ * XML file, make sure the file is in the classpath and use
+ * <code>config.addResource("my file name");</code>
  *
  * <strong>Note:</strong> This class will lazily load any configuration
  * properties, therefore you will not be able to access them until you
@@ -36,11 +33,6 @@ public class CConfiguration extends Configuration {
 
     // Clear any default properties
     conf.clear();
-
-    // Load our Continuuity resources
-    conf.addResource("continuuity-default.xml");
-    conf.addResource("continuuity-site.xml");
-
     return conf;
   }
 
@@ -54,7 +46,6 @@ public class CConfiguration extends Configuration {
     // Create a new configuration instance, but do NOT initialize with
     // the Hadoop default properties.
     CConfiguration conf = new CConfiguration();
-
     return addResources(conf);
   }
 
--- src/test/java/com/continuuity/common/conf/CConfigurationTest.java
+++ src/test/java/com/continuuity/common/conf/CConfigurationTest.java
@@ -1,6 +1,5 @@
 package com.continuuity.common.conf;
 
-import org.apache.hadoop.conf.Configuration;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,9 +10,24 @@ public class CConfigurationTest {
 
   @Test
   public void testConfiguration() throws Exception {
+    // first test empty config object
     CConfiguration conf = CConfiguration.create();
     String a = conf.get("continuuity.test.A");
     String b = conf.get("continuuity.test.B");
+    Assert.assertNull(a);
+    Assert.assertNull(b);
+    // load some defaults and make sure they work
+    conf.addResource("test-default.xml");
+    a = conf.get("continuuity.test.A");
+    b = conf.get("continuuity.test.B");
+    Assert.assertNotNull(a);
+    Assert.assertNotNull(b);
+    Assert.assertEquals("A", a);
+    Assert.assertEquals("B", b);
+    // override one of the defaults and verify
+    conf.addResource("test-override.xml");
+    a = conf.get("continuuity.test.A");
+    b = conf.get("continuuity.test.B");
     Assert.assertNotNull(a);
     Assert.assertNotNull(b);
     Assert.assertEquals("A", a);
@@ -23,6 +37,7 @@ public class CConfigurationTest {
   @Test
   public void testAddedConfiguration() throws Exception {
     CConfiguration conf = CConfiguration.create();
+    conf.addResource("test-default.xml");
     conf.set("continuuity.test.addedA", "AddedA");
     conf.set("continuuity.test.addedB", "AddedB");
     conf.set("continuuity.test.A", "A+");
