7b5a81cdfa8fd858e2dc911b423a465376ec6cad
Andreas Neumann <andreas@continuuity.com>
2012-06-26 00:24:43 -0700

    add read capability to stream client and gateway

--- src/main/java/com/continuuity/gateway/Gateway.java
+++ src/main/java/com/continuuity/gateway/Gateway.java
@@ -141,7 +141,7 @@ public class Gateway implements Server {
       // TODO: This should probably be done in the addConnector method?
       if (connector instanceof Collector) {
         ((Collector) connector).setConsumer(this.consumer);
-      } else if (connector instanceof Accessor) {
+      } else if (connector instanceof DataAccessor) {
         ((Accessor) connector).setExecutor(this.executor);
       }
 
