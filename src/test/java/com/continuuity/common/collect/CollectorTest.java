aa847bf00dd4f7e8289dfcfa81b9983e0356f445
anew <andreas@continuuity.com>
2012-06-26 17:21:40 -0700

    ENG-353 stream-client tool. These are utility classes for collecting events from the stream. I am checking them into common because they may be useful for others.

---
+++ src/test/java/com/continuuity/common/collect/CollectorTest.java
@@ -0,0 +1,53 @@
+package com.continuuity.common.collect;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollectorTest {
+
+
+  private Integer[] collect(Collector<Integer> collector, int n) {
+    for (int i = 0; i < n; i++)
+      if (!collector.addElement(i)) break;
+    return collector.finish();
+  }
+
+  @Test
+  public void testAllCollector() {
+    Assert.assertArrayEquals(collect(new AllCollector<Integer>(Integer.class), 0), new Integer[] { });
+    Assert.assertArrayEquals(collect(new AllCollector<Integer>(Integer.class), 4), new Integer[] { 0, 1, 2, 3 });
+  }
+
+  @Test
+  public void testFirstNCollector() {
+    // add 0 elements
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(1, Integer.class), 0), new Integer[] { });
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(10, Integer.class), 0), new Integer[] { });
+    // add more than capacity
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(1, Integer.class), 10), new Integer[] { 0 });
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(4, Integer.class), 10), new Integer[] { 0, 1, 2, 3 });
+    // add same as capacity
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(1, Integer.class), 1), new Integer[] { 0 });
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(4, Integer.class), 4), new Integer[] { 0, 1, 2, 3 });
+    // add less than capacity
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(4, Integer.class), 1), new Integer[] { 0 });
+    Assert.assertArrayEquals(collect(new FirstNCollector<Integer>(10, Integer.class), 4), new Integer[] { 0, 1, 2, 3 });
+  }
+
+  @Test
+  public void testLastNCollector() {
+    // add 0 elements
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(1, Integer.class), 0), new Integer[] { });
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(10, Integer.class), 0), new Integer[] { });
+    // add more than capacity
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(1, Integer.class), 10), new Integer[] { 9 });
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(4, Integer.class), 10), new Integer[] { 6, 7, 8, 9 });
+    // add same as capacity
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(1, Integer.class), 1), new Integer[] { 0 });
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(4, Integer.class), 4), new Integer[] { 0, 1, 2, 3 });
+    // add less than capacity
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(4, Integer.class), 1), new Integer[] { 0 });
+    Assert.assertArrayEquals(collect(new LastNCollector<Integer>(10, Integer.class), 4), new Integer[] { 0, 1, 2, 3 });
+  }
+
+}
