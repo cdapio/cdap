abe7c571199e2404623e7f9d04b30be5e4f1d397
anew <andreas@continuuity.com>
2012-07-14 15:43:43 -0700

    ENG-408 More unit tests for remote opex

--- src/test/java/com/continuuity/data/operation/executor/remote/OperationExecutorServiceTest.java
+++ src/test/java/com/continuuity/data/operation/executor/remote/OperationExecutorServiceTest.java
@@ -393,7 +393,7 @@ public class OperationExecutorServiceTest {
     Assert.assertEquals(0, keys.size());
   }
 
-  // TODO test batch, one that succeeds and one that fails
+  /** test batch, one that succeeds and one that fails */
   @Test
   public void testBatchSuccessAndFailure() throws Exception {
     // clear the fabric
@@ -482,11 +482,75 @@ public class OperationExecutorServiceTest {
     Assert.assertArrayEquals("1".getBytes(), dequeueResult.getValue());
   }
 
-  // TODO test clearFabric
-  // TODO test readAllKeys
 
+  /** test clearFabric */
+  @Test
+  public void testClearFabric() {
+    final byte[] a = { 'a' };
+    final byte[] x = { 'x' };
+    final byte[] q = "queue://q".getBytes();
+    final byte[] s = "stream://s".getBytes();
 
-  // TODO test Enqueue
+    // write to a table, a queue, and a stream
+    Assert.assertTrue(remote.execute(new Write(a, x)));
+    Assert.assertTrue(remote.execute(new QueueEnqueue(q, x)));
+    Assert.assertTrue(remote.execute(new QueueEnqueue(s, x)));
+
+    // clear everything
+    opex.execute(new ClearFabric(true, true, true));
+
+    // verify that all is gone
+    Assert.assertNull(remote.execute(new ReadKey(a)));
+    QueueConsumer consumer = new QueueConsumer(0, 1, 1);
+    QueueConfig config = new
+        QueueConfig(new QueuePartitioner.RandomPartitioner(), true);
+    Assert.assertTrue(
+        remote.execute(new QueueDequeue(q, consumer, config)).isEmpty());
+    Assert.assertTrue(
+        remote.execute(new QueueDequeue(s, consumer, config)).isEmpty());
+
+    // write back all values
+    Assert.assertTrue(remote.execute(new Write(a, x)));
+    Assert.assertTrue(remote.execute(new QueueEnqueue(q, x)));
+    Assert.assertTrue(remote.execute(new QueueEnqueue(s, x)));
+
+    // clear only the tables
+    opex.execute(new ClearFabric(true, false, false));
+
+    // verify that the tables are gone, but queues and streams are there
+    Assert.assertNull(remote.execute(new ReadKey(a)));
+    Assert.assertArrayEquals(x,
+        remote.execute(new QueueDequeue(q, consumer, config)).getValue());
+    Assert.assertArrayEquals(x,
+        remote.execute(new QueueDequeue(s, consumer, config)).getValue());
+
+    // write back to the table
+    Assert.assertTrue(remote.execute(new Write(a, x)));
+
+    // clear only the queues
+    opex.execute(new ClearFabric(false, true, false));
+
+    // verify that the queues are gone, but tables and streams are there
+    Assert.assertArrayEquals(x, remote.execute(new ReadKey(a)));
+    Assert.assertTrue(
+        remote.execute(new QueueDequeue(q, consumer, config)).isEmpty());
+    Assert.assertArrayEquals(x,
+        remote.execute(new QueueDequeue(s, consumer, config)).getValue());
+
+    // write back to the queue
+    Assert.assertTrue(remote.execute(new QueueEnqueue(q, x)));
+
+    // clear only the streams
+    opex.execute(new ClearFabric(false, false, true));
+
+    // verify that the streams are gone, but tables and queues are there
+    Assert.assertArrayEquals(x, remote.execute(new ReadKey(a)));
+    Assert.assertArrayEquals(x,
+        remote.execute(new QueueDequeue(q, consumer, config)).getValue());
+    Assert.assertTrue(
+        remote.execute(new QueueDequeue(s, consumer, config)).isEmpty());
+  }
+
   // TODO test getGroupId
   // TODO test Dequeue, with Hash and with Random
   // TODO test Ack, with multi and single
