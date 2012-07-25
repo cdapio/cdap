a99d31eb9a0a8433193e7c302662319ca4c9408b
dereklwood <derek@continuuity.com>
2012-07-24 18:47:15 -0700

    overwriting gradle release target for web-cloud-app, adding startup script

---
+++ conf/server-env.sh
@@ -0,0 +1,8 @@
+# Set environment variables here.
+
+# Main cmd is the non-java command to run.  
+MAIN_CMD=/opt/node
+
+# Arguments for MAIN_CMD
+MAIN_CMD_ARGS="$CONTINUUITY_HOME/web_cloud_app/web-app/server/main.js"
+
