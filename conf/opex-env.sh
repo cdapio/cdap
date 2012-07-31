94dadf36142049da6d6ee423b885c9962386c579
dereklwood <derek@continuuity.com>
2012-07-31 12:14:51 -0700

    adding startup conf script used in distribution service

---
+++ conf/opex-env.sh
@@ -0,0 +1,13 @@
+# Set environment variables here.
+
+# Main class to be invoked.
+MAIN_CLASS=com.continuuity.data.runtime.OpexServiceMain
+
+# Arguments for main class.
+MAIN_CLASS_ARGS="start"
+
+# Add Hadoop HDFS classpath
+#EXTRA_CLASSPATH="$YARN_HOME/etc/hadoop/*"
+
+JAVA_HEAPMAX=-Xmx512m
+
