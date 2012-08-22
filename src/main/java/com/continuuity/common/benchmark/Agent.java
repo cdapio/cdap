9e6905c0ade8071cc8363fcc11eef04085a6b551
anew <andreas@continuuity.com>
2012-08-21 19:09:32 -0700

    ENG-476 Create performance subproject for benchmarks

--- src/main/java/com/continuuity/common/benchmark/Agent.java
+++
@@ -1,8 +0,0 @@
-package com.continuuity.common.benchmark;
-
-public abstract class Agent {
-
-  public abstract void runOnce(int iteration, int agentId, int numAgents)
-      throws BenchmarkException;
-
-}
