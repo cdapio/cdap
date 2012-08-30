bce5023a19e9433c5133a40599ebad5b627671bf
Nitin Motgi <nitin@continuuity.com>
2012-08-30 02:13:51 -0700

    ENG-440, ENG-489 Adding constants for specifying jdbc connection url and defaults

--- src/main/java/com/continuuity/common/conf/Constants.java
+++ src/main/java/com/continuuity/common/conf/Constants.java
@@ -9,15 +9,27 @@ public class Constants {
    * Common across components.
    */
   public static final String CFG_ZOOKEEPER_ENSEMBLE = "zookeeper.quorum";
+
   // ENG-443 setting for avoiding OOME when telnetd to server port.
   public static final String CFG_MAX_READ_BUFFER = "thrift.max.read.buffer";
+
+  // ENG-440 Connection URL for location used for storing the flows states
+  // and history.
+  public static final String CFG_STATE_STORAGE_CONNECTION_URL =
+    "state.storage.connection.url";
 
   /**
    * Default constants for common
    */
   public static final String DEFAULT_ZOOKEEPER_ENSEMBLE = "localhost:2181";
+
   // ENG-443 1 MB max read buffer size.
   public static final int DEFAULT_MAX_READ_BUFFER = 1048576;
+
+  // ENG-440 Connection URL for location used for storing the flows states
+  // and history.
+  public static final String DEFAULT_STATE_STORAGE_CONNECTION_URL =
+    "jdbc:hsqldb:mem:flowmanagement?user=sa";
 
   /**
    * Configuration key names used by resource manager.
@@ -58,9 +70,13 @@ public class Constants {
   /**
    * Configuration key names used by flow manager
    */
-  public static final String CFG_FLOW_MANAGER_SERVER_PORT = "flow.manager.server.port";
-  public static final String CFG_FLOW_MANAGER_SERVER_THREADS = "flow.manager.server.threads";
-  public static final String CFG_FLOW_MANAGER_SERVER_ADDRESS = "flow.manager.server.address";
+  public static final String CFG_FLOW_MANAGER_SERVER_PORT =
+        "flow.manager.server.port";
+  public static final String CFG_FLOW_MANAGER_SERVER_THREADS =
+        "flow.manager.server.threads";
+  public static final String CFG_FLOW_MANAGER_SERVER_ADDRESS =
+        "flow.manager.server.address";
+
 
   /**
    * Default constants defined for flow manager.
@@ -68,6 +84,7 @@ public class Constants {
   public static final String DEFAULT_FLOW_MANAGER_SERVER_PORT = "45001";
   public static final String DEFAULT_FLOW_MANAGER_SERVER_THREADS = "2";
   public static final String DEFAULT_FLOW_MANAGER_SERVER_ADDRESS = "0.0.0.0";
+
 
   /**
    * Configuration key names used flow monitor.
