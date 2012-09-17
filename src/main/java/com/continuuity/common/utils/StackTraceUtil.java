07249ae8c3dd97955afcab54572af6d2fc93bafc
Nitin Motgi <nitin@continuuity.com>
2012-09-17 11:47:00 -0700

    ENG-622 adding a stack trace utility that converts an exception stack trace into it's string representation

---
+++ src/main/java/com/continuuity/common/utils/StackTraceUtil.java
@@ -0,0 +1,23 @@
+package com.continuuity.common.utils;
+
+/**
+ * Utilities for stack trace.
+ */
+public class StackTraceUtil {
+
+  /**
+   * Converts an exception into a string representation to be used for
+   * printing in logs.
+   *
+   * @param e Exception
+   * @return String representation of stack trace.
+   */
+  public static String toStringStackTrace(Exception e) {
+    StringBuilder sb = new StringBuilder();
+    StackTraceElement[] elements = e.getStackTrace();
+    for(StackTraceElement element : elements) {
+      sb.append(element.toString() + "\n");
+    }
+    return sb.toString();
+  }
+}
