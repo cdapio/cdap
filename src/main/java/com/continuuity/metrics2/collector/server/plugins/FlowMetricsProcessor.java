064dde5a295a5bc07767f07d110959a77aafac3b
Nitin Motgi <nitin@continuuity.com>
2012-09-17 21:52:41 -0700

    ENG-634 Fixing metric name for inert into mysqldb

--- src/main/java/com/continuuity/metrics2/collector/server/plugins/FlowMetricsProcessor.java
+++ src/main/java/com/continuuity/metrics2/collector/server/plugins/FlowMetricsProcessor.java
@@ -214,9 +214,8 @@ public final class FlowMetricsProcessor implements MetricsProcessor {
       stmt.setString(4, elements.getRunId());
       stmt.setString(5, elements.getFlowletId());
       stmt.setInt(6, elements.getInstanceId());
-      stmt.setString(7, request.getMetricName());
+      stmt.setString(7, elements.getMetric());
       stmt.setFloat(8, request.getValue());
-      stmt.setFloat(9, request.getValue());
       stmt.executeUpdate();
       stmt.close();
     } catch (SQLException e) {
