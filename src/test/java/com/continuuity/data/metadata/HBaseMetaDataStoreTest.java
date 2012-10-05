5d7f5675245279bb9284731cdde1d94b2a7de6f5
Jonathan Gray <jon@continuuity.com>
2012-10-04 18:04:29 -0700

    ENG-720 Removing invalid hsql instantiation in hbase test

--- src/test/java/com/continuuity/data/metadata/HBaseMetaDataStoreTest.java
+++ src/test/java/com/continuuity/data/metadata/HBaseMetaDataStoreTest.java
@@ -1,24 +1,17 @@
 package com.continuuity.data.metadata;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
 import com.continuuity.data.hbase.HBaseTestBase;
 import com.continuuity.data.operation.executor.OperationExecutor;
 import com.continuuity.data.runtime.DataFabricDistributedModule;
-import com.continuuity.data.runtime.DataFabricLocalModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 
 public abstract class HBaseMetaDataStoreTest extends MetaDataStoreTest {
-
-  @BeforeClass
-  public static void setupOpex() throws Exception {
-    Injector injector = Guice.createInjector (
-        new DataFabricLocalModule("jdbc:hsqldb:mem:membenchdb", null));
-    opex = injector.getInstance(OperationExecutor.class);
-  }
 
   @BeforeClass
   public static void startService() throws Exception {
