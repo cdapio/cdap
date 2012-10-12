4da4b4046adc6e0331816e0caac4ae2d4945d3d0
Don Mosites <don@continuuity.com>
2012-10-12 16:47:38 -0700

    Rearrangements for syntax hook

---
+++ check_syntax.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+jshint ./client/core/ ./client/developer/ ./server --config ./jshint.json
--- tools/check_syntax.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-jshint ../client/core/ ../client/developer/ ../server --config jshint.json
