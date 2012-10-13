12cfdb3481bc7b3e174307800c55611ae8e30874
dereklwood <derek@continuuity.com>
2012-10-12 18:50:30 -0700

    adding metadataservice service configuration

---
+++ conf/metadataservice-env.sh
@@ -0,0 +1,11 @@
+# Main class to be invoked.
+MAIN_CLASS=com.continuuity.runtime.MetadataServerMain
+
+# Arguments for main class.
+#MAIN_CLASS_ARGS=""
+
+# Add Hadoop HDFS classpath
+EXTRA_CLASSPATH=""
+
+# Specify Heap Size.
+JAVA_HEAPMAX=-Xmx1024m
