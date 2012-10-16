73026584771d0817f430e647ac88cd681fcb1667
anew <andreas@continuuity.com>
2012-10-15 19:11:17 -0700

    ENG-767 Implement stream metrics

--- src/main/java/com/continuuity/data/operation/executor/omid/OmidTransactionalOperationExecutor.java
+++ src/main/java/com/continuuity/data/operation/executor/omid/OmidTransactionalOperationExecutor.java
@@ -24,12 +24,13 @@ import com.continuuity.data.table.OrderedVersionedColumnarTable;
 import com.continuuity.data.table.ReadPointer;
 import com.continuuity.data.util.TupleMetaDataAnnotator.DequeuePayload;
 import com.continuuity.data.util.TupleMetaDataAnnotator.EnqueuePayload;
-import com.esotericsoftware.minlog.Log;
 import com.google.common.base.Objects;
 import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.*;
@@ -46,6 +47,9 @@ import java.util.concurrent.ConcurrentSkipListMap;
 @Singleton
 public class OmidTransactionalOperationExecutor
 implements TransactionalOperationExecutor {
+
+  private static final Logger Log
+    = LoggerFactory.getLogger(OmidTransactionalOperationExecutor.class);
 
   public String getName() {
     return "omid(" + tableHandle.getName() + ")";
