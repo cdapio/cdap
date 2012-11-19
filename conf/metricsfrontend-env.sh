d19ed16e5d44158eabd2dd76da982f0013462824
Nitin Motgi <nitin@continuuity.com>
2012-11-19 14:44:18 -0800

    Adding in Hadoop path to read logs from DFS

--- conf/metricsfrontend-env.sh
+++ conf/metricsfrontend-env.sh
@@ -5,7 +5,7 @@ MAIN_CLASS=com.continuuity.runtime.MetricsFrontendServerMain
 #MAIN_CLASS_ARGS=""
 
 # Add Hadoop HDFS classpath
-EXTRA_CLASSPATH=""
+EXTRA_CLASSPATH="$CONTINUUITY_HOME/datafabric/conf:$YARN_HOME/conf/"
 
 # Specify Heap Size.
 JAVA_HEAPMAX=-Xmx1024m
