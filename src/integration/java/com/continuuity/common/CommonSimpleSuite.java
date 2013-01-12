f2fe9a4826348da06f57d998a97e0d56b5988e43
Nitin Motgi <nitin@continuuity.com>
2013-01-11 17:43:05 -0800

    Adding integration tests

---
+++ src/integration/java/com/continuuity/common/CommonSimpleSuite.java
@@ -0,0 +1,23 @@
+package com.continuuity.common;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({OtherTest.class })
+public class CommonSimpleSuite {
+    static {
+        System.out.println("suite class loaded");
+    }
+
+    @org.junit.BeforeClass public static void init() {
+        System.out.println("before suite class out");
+        System.out.println("non-asci char: ż");
+        System.err.println("before suite class err");
+    }
+    @org.junit.AfterClass public static void end() {
+        System.out.println("after suite class out");
+        System.err.println("after suite class err");
+    }
+}
