02f2e3a346b008005aba2278fdb4e6b8a0dd32c5
anew <andreas@continuuity.com>
2013-01-21 15:28:20 -0800

    ENG-1728 more refactoring, moved the sample code to test

--- src/main/java/com/continuuity/data/operation/SimpleBatchCollectionClient.java
+++ src/main/java/com/continuuity/data/operation/SimpleBatchCollectionClient.java
@@ -1,4 +1,4 @@
-package com.continuuity.data.dataset;
+package com.continuuity.data.operation;
 
 import com.continuuity.api.data.BatchCollectionClient;
 import com.continuuity.api.data.BatchCollector;
