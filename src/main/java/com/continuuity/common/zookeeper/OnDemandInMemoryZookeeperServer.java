460045f6ae4bd390ccc2c51e78e87c0fdac6a79a
poorna <poorna@continuuity.com>
2013-01-21 20:22:31 -0800

    Changing the singleton idiom so that exceptions thrown during start of Zookeeper server can be propagated back to clients.

--- src/main/java/com/continuuity/common/zookeeper/OnDemandInMemoryZookeeperServer.java
+++ src/main/java/com/continuuity/common/zookeeper/OnDemandInMemoryZookeeperServer.java
@@ -1,32 +1,30 @@
 package com.continuuity.common.zookeeper;
 
 import com.continuuity.common.utils.PortDetector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
 /**
  * Thread safe, lazy instantiator idiom
  */
-public class OnDemandInMemoryZookeeperServer {
-  private static final Logger LOG =
-    LoggerFactory.getLogger(OnDemandInMemoryZookeeperServer.class);
+public final class OnDemandInMemoryZookeeperServer {
+  private static volatile InMemoryZookeeper inMemoryZookeeper = null;
 
-  private static class InMemoryZookeeperRefHolder {
-    private static InMemoryZookeeper inMemoryZookeeper = OnDemandInMemoryZookeeperServer.create();
-  }
-
-  public static InMemoryZookeeper getSingletonInstance() {
-    return InMemoryZookeeperRefHolder.inMemoryZookeeper;
-  }
-
-  private static InMemoryZookeeper create() {
-    try {
-      int port = PortDetector.findFreePort();
-      return new InMemoryZookeeper(port);
-    } catch (Exception e) {
-      LOG.error("Failed to start zookeeper", e);
+  public static InMemoryZookeeper getSingletonInstance() throws IOException, InterruptedException {
+    InMemoryZookeeper result = inMemoryZookeeper;
+    if (result == null) {
+      synchronized(OnDemandInMemoryZookeeperServer.class) {
+        result = inMemoryZookeeper;
+        if (result == null) {
+          inMemoryZookeeper = result = createInstance();
+        }
+      }
     }
-    return null;
+    return result;
+  }
+
+  private static InMemoryZookeeper createInstance() throws IOException, InterruptedException {
+    int port = PortDetector.findFreePort();
+    return new InMemoryZookeeper(port);
   }
 }
 
