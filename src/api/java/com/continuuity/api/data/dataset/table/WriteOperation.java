ad6f8ea216c1ad08e60c7d050fcc222682dbb813
anew <andreas@continuuity.com>
2013-01-23 15:06:22 -0800

    ENG-1728 add unit test framework and key value table tests

--- src/api/java/com/continuuity/api/data/dataset/table/WriteOperation.java
+++ src/api/java/com/continuuity/api/data/dataset/table/WriteOperation.java
@@ -3,17 +3,5 @@ package com.continuuity.api.data.dataset.table;
 /**
  * common interface for all write operations
  */
-public abstract class WriteOperation {
-
-  protected byte[] row;
-
-  /** Constructor from the row key */
-  public WriteOperation(byte[] row) {
-    this.row = row;
-  }
-
-  /** get the row key of the row to write */
-  public byte[] getRow() {
-    return this.row;
-  }
+public interface WriteOperation {
 }
