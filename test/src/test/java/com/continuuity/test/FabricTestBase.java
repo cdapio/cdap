260e71b9f297261bb44aae84cf857c46bb25648c
poorna <poorna@continuuity.com>
2013-01-23 11:40:43 -0800

    Initial verstion of Flow introspection through proxies. Also moving FlowTestHelper from flow/ to test/

--- test/src/test/java/com/continuuity/test/FabricTestBase.java
+++ test/src/test/java/com/continuuity/test/FabricTestBase.java
@@ -39,10 +39,9 @@ import com.continuuity.data.operation.executor.OperationExecutor;
 import com.continuuity.data.operation.executor.omid.OmidTransactionalOperationExecutor;
 import com.continuuity.data.operation.ttqueue.QueueEnqueue;
 import com.continuuity.data.runtime.DataFabricModules;
-import com.continuuity.flow.FlowTestHelper;
-import com.continuuity.flow.FlowTestHelper.TestFlowHandle;
-import com.continuuity.flow.FlowTestHelper.TestQueryHandle;
-import com.continuuity.flow.FlowTestHelper.TestDataSetRegistry;
+import com.continuuity.test.FlowTestHelper.TestFlowHandle;
+import com.continuuity.test.FlowTestHelper.TestQueryHandle;
+import com.continuuity.test.FlowTestHelper.TestDataSetRegistry;
 import com.continuuity.flow.definition.api.FlowDefinition;
 import com.continuuity.flow.definition.impl.FlowStream;
 import com.continuuity.flow.flowlet.internal.FlowletContextImpl;
@@ -230,9 +229,9 @@ public abstract class FabricTestBase {
    * Starts the specified query.
    * <p>
    * Check the status of the query and whether it started properly using
-   * {@link com.continuuity.flow.FlowTestHelper.TestQueryHandle#isSuccess()}.
+   * {@link FlowTestHelper.TestQueryHandle#isSuccess()}.
    * <p>
-   * Always stop the query using {@link com.continuuity.flow.FlowTestHelper.TestQueryHandle#stop()}.
+   * Always stop the query using {@link FlowTestHelper.TestQueryHandle#stop()}.
    * @param queryProvider the query to start
    * @return handle to running query
    */
