fe813c91bd1195017176c2ee7e42237c6837b9f9
Alex Baranau <alexb@continuuity.com>
2013-01-24 16:50:58 -0800

    ENG-1656: timeseries dataset first cut

--- src/test/java/com/continuuity/api/data/set/SimpleTimeseriesTableTest.java
+++ src/test/java/com/continuuity/api/data/set/SimpleTimeseriesTableTest.java
@@ -3,6 +3,7 @@ package com.continuuity.api.data.set;
 import com.continuuity.api.data.DataFabric;
 import com.continuuity.api.data.DataSetSpecification;
 import com.continuuity.api.data.OperationContext;
+import com.continuuity.api.data.OperationException;
 import com.continuuity.api.data.util.Bytes;
 import com.continuuity.data.DataFabricImpl;
 import com.continuuity.data.dataset.DataSetInstantiator;
@@ -18,10 +19,11 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class TimeseriesTableTest {
-  private static TimeseriesTable table;
+public class SimpleTimeseriesTableTest {
+  private static SimpleTimeseriesTable table;
   static SimpleBatchCollectionClient collectionClient = new SimpleBatchCollectionClient();
   static OperationExecutor executor;
   static DataFabric fabric;
@@ -37,7 +39,7 @@ public class TimeseriesTableTest {
     fabric = new DataFabricImpl(executor, OperationContext.DEFAULT);
 
     // configure a couple of data sets
-    specs = Lists.newArrayList(new TimeseriesTable("metricsTable").configure());
+    specs = Lists.newArrayList(new SimpleTimeseriesTable("metricsTable").configure());
 
     // create an app context for running a procedure
     instantiator = new DataSetInstantiator();
@@ -77,49 +79,87 @@ public class TimeseriesTableTest {
     long ts = System.currentTimeMillis();
 
     // m1e1 = metric: 1, entity: 1
-    TimeseriesTable.Entry m1e1 = new TimeseriesTable.Entry(metric1, Bytes.toBytes(3L), ts, tag3, tag2, tag1);
+    SimpleTimeseriesTable.Entry m1e1 =
+      new SimpleTimeseriesTable.Entry(metric1, Bytes.toBytes(3L), ts, tag3, tag2, tag1);
     table.write(m1e1);
-    TimeseriesTable.Entry m1e2 = new TimeseriesTable.Entry(metric1, Bytes.toBytes(10L), ts + 2 * second, tag3);
+    SimpleTimeseriesTable.Entry m1e2 =
+      new SimpleTimeseriesTable.Entry(metric1, Bytes.toBytes(10L), ts + 2 * second, tag3);
     table.write(m1e2);
-    TimeseriesTable.Entry m1e3 = new TimeseriesTable.Entry(metric1, Bytes.toBytes(15L), ts + 2 * hour, tag1);
+    SimpleTimeseriesTable.Entry m1e3 =
+      new SimpleTimeseriesTable.Entry(metric1, Bytes.toBytes(15L), ts + 2 * hour, tag1);
     table.write(m1e3);
-    TimeseriesTable.Entry m1e4 = new TimeseriesTable.Entry(metric1, Bytes.toBytes(23L), ts + 3 * hour, tag2, tag3);
+    SimpleTimeseriesTable.Entry m1e4 =
+      new SimpleTimeseriesTable.Entry(metric1, Bytes.toBytes(23L), ts + 3 * hour, tag2, tag3);
     table.write(m1e4);
-    TimeseriesTable.Entry m1e5 = new TimeseriesTable.Entry(metric1, Bytes.toBytes(55L), ts + 3 * hour + 1 * second);
+    SimpleTimeseriesTable.Entry m1e5 =
+      new SimpleTimeseriesTable.Entry(metric1, Bytes.toBytes(55L), ts + 3 * hour + 2 * second);
     table.write(m1e5);
 
-    TimeseriesTable.Entry m2e1 = new TimeseriesTable.Entry(metric2, Bytes.toBytes(4L), ts);
+    SimpleTimeseriesTable.Entry m2e1 =
+      new SimpleTimeseriesTable.Entry(metric2, Bytes.toBytes(4L), ts);
     table.write(m2e1);
-    TimeseriesTable.Entry m2e2 = new TimeseriesTable.Entry(metric2, Bytes.toBytes(11L), ts + 2 * second, tag3);
+    SimpleTimeseriesTable.Entry m2e2 =
+      new SimpleTimeseriesTable.Entry(metric2, Bytes.toBytes(11L), ts + 2 * second, tag2);
     table.write(m2e2);
-    TimeseriesTable.Entry m2e3 = new TimeseriesTable.Entry(metric2, Bytes.toBytes(16L), ts + 2 * hour, tag1);
+    SimpleTimeseriesTable.Entry m2e3 =
+      new SimpleTimeseriesTable.Entry(metric2, Bytes.toBytes(16L), ts + 2 * hour, tag2);
     table.write(m2e3);
-    TimeseriesTable.Entry m2e4 = new TimeseriesTable.Entry(metric2, Bytes.toBytes(24L), ts + 3 * hour, tag2, tag3);
+    SimpleTimeseriesTable.Entry m2e4 =
+      new SimpleTimeseriesTable.Entry(metric2, Bytes.toBytes(24L), ts + 3 * hour, tag1, tag3);
     table.write(m2e4);
-    TimeseriesTable.Entry m2e5 = new TimeseriesTable.Entry(metric2, Bytes.toBytes(56L), ts + 3 * hour + 1 * second,
+    SimpleTimeseriesTable.Entry m2e5 =
+      new SimpleTimeseriesTable.Entry(metric2, Bytes.toBytes(56L), ts + 3 * hour + 2 * second,
                                                             tag3, tag1);
     table.write(m2e5);
 
+    // whole interval is searched
     assertReadResult(table.read(metric1, ts, ts + 5 * hour), m1e1, m1e2, m1e3, m1e4, m1e5);
+    assertReadResult(table.read(metric1, ts, ts + 5 * hour, tag2), m1e1, m1e4);
+    assertReadResult(table.read(metric1, ts, ts + 5 * hour, tag4), new SimpleTimeseriesTable.Entry[0]);
+    assertReadResult(table.read(metric1, ts, ts + 5 * hour, tag2, tag4), new SimpleTimeseriesTable.Entry[0]);
+    // This is extreme case, should not be really used by anyone. Still we want to test that it won't fail. It returns
+    // nothing because there's hard limit on the number of rows traversed during the read.
+    assertReadResult(table.read(metric1, 0, Long.MAX_VALUE));
 
+    // part of the interval
+    assertReadResult(table.read(metric1, ts + second, ts + 2 * second), m1e2);
+    assertReadResult(table.read(metric1, ts + hour, ts + 3 * hour), m1e3, m1e4);
+    assertReadResult(table.read(metric1, ts + second, ts + 3 * hour), m1e2, m1e3, m1e4);
+    assertReadResult(table.read(metric1, ts + second, ts + 3 * hour, tag3), m1e2, m1e4);
+    assertReadResult(table.read(metric1, ts + second, ts + 3 * hour, tag3, tag2), m1e4);
+
+    // different metric
+    assertReadResult(table.read(metric2, ts + hour, ts + 3 * hour, tag2), m2e3);
   }
 
-  private void assertReadResult(List<TimeseriesTable.Entry> result, TimeseriesTable.Entry... entries) {
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidTimeRangeCondition() throws OperationException {
+    SimpleBatchCollector collector = new SimpleBatchCollector();
+    collectionClient.setCollector(collector);
+
+    long ts = System.currentTimeMillis();
+    table.read(Bytes.toBytes("any"), ts, ts - 100);
+  }
+
+  // TODO: test for wrong params: end time less than start time
+  private void assertReadResult(List<SimpleTimeseriesTable.Entry> result, SimpleTimeseriesTable.Entry... entries) {
     Assert.assertEquals(entries.length, result.size());
     for (int i = 0; i < entries.length; i++) {
       assertEquals(entries[i], result.get(i));
     }
   }
 
-  private void assertEquals(final TimeseriesTable.Entry left, final TimeseriesTable.Entry right) {
+  private void assertEquals(final SimpleTimeseriesTable.Entry left, final SimpleTimeseriesTable.Entry right) {
     Assert.assertArrayEquals(left.getKey(), right.getKey());
     Assert.assertEquals(left.getTimestamp(), right.getTimestamp());
     Assert.assertArrayEquals(left.getValue(), right.getValue());
     Assert.assertArrayEquals(left.getValue(), right.getValue());
-    assertEquals(left.getTags(), right.getTags());
+    assertEqualsIgnoreOrder(left.getTags(), right.getTags());
   }
 
-  private void assertEquals(final byte[][] left, final byte[][] right) {
+  private void assertEqualsIgnoreOrder(final byte[][] left, final byte[][] right) {
+    Arrays.sort(left, Bytes.BYTES_COMPARATOR);
+    Arrays.sort(right, Bytes.BYTES_COMPARATOR);
     Assert.assertEquals(left.length, right.length);
     for (int i = 0; i < left.length; i++) {
       Assert.assertArrayEquals(left[i], right[i]);
