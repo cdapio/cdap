64e28fa33a2b29de2f424131db9e7238ad4202bb
Sreevatsan Raman <sree@Sreevatsans-MacBook-Pro.local>
2013-01-29 14:59:06 -0800

    add package info for streams

--- src/api/java/com/continuuity/api/stream/package-info.java
+++ src/api/java/com/continuuity/api/stream/package-info.java
@@ -1,6 +1,28 @@
 /**
- * Place-holder for stream package info
+ * This package contains Stream and StreamSpecification APIs.
  *
+ * Streams are primary means to push data from external systems into the Continuuity AppFabric.
+ * Streams are identified by a Unique ID string and must be explicitly created before being used.
+ *
+ * Streams are used along with datasets and flows to create applications. For example:
+ * <blockquote>
+ *   <pre>
+ *     public MyApplication implements Application {
+ *       @Override
+ *       public ApplicationSpecification configure() {
+ *         MyDataSet myDataset = new MyDataset("my");
+ *         TimeseriesDataSet timeseriesDataset = new TimeseriesDataSet("mytimeseries");
+ *         Stream clickStream = new Stream("mystream");
+ *         return new ApplicationSpecification.Builder()
+ *            .addDataSet(myDataset)
+ *            .addDataSet(timeseriesDataset);
+ *            .addStream(clickStream)
+ *            .addFlow(ClickProcessingFlow.class)
+ *            .create();
+ *       }
+ *     }
+ *   </pre>
+ * </blockquote>
  *
  */
-package com.continuuity.api.stream;
+  package com.continuuity.api.stream;
