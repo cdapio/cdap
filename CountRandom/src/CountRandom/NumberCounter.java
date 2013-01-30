dcdb44eb301c04fe57ee22732003ff0398877a58
anew <andreas@continuuity.com>
2013-01-30 13:30:15 -0800

    ENG-1865 Wire up flow/query system with data sets

--- CountRandom/src/CountRandom/NumberCounter.java
+++ CountRandom/src/CountRandom/NumberCounter.java
@@ -1,16 +1,28 @@
 package CountRandom;
 
-import com.continuuity.api.data.Increment;
+import com.continuuity.api.data.DataSetInstantiationException;
+import com.continuuity.api.data.dataset.table.Increment;
+import com.continuuity.api.data.dataset.table.Table;
 import com.continuuity.api.flow.flowlet.*;
 import com.continuuity.api.flow.flowlet.builders.TupleSchemaBuilder;
 
 public class NumberCounter extends ComputeFlowlet {
 
+  static final byte[] column = { 'c', 'o', 'u', 'n', 't' };
+
+  Table counters;
+
+  @Override
+  public void initialize() {
+    //super.initialize();
+    this.counters = this.getFlowletContext().getDataSet("counters");
+  }
+
   @Override
   public void process(Tuple tuple, TupleContext tupleContext, OutputCollector outputCollector) {
     Integer i = tuple.get("number");
     getFlowletContext().getLogger().info("Processing integer " + i.intValue());
-    outputCollector.add(new Increment(i.toString().getBytes(), 1L));
+    counters.stage(new Increment(i.toString().getBytes(), column, 1L));
   }
 
   @Override
