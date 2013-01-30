8f5aa0507a25bee63098f0299abe2258550c0d4f
Sreevatsan Raman <sree@Sreevatsans-MacBook-Pro.local>
2013-01-30 12:03:42 -0800

    Add package info for lib

--- src/api/java/com/continuuity/api/data/lib/package-info.java
+++ src/api/java/com/continuuity/api/data/lib/package-info.java
@@ -1,6 +1,5 @@
 /**
- * Place-holder for dataset package info
- *
+ * This package is deprecated. Please use {@link com.continuuity.api.data.DataSet}s for info.
  *
  */
 package com.continuuity.api.data.lib;
