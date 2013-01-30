150b2b9ff0b82cc908e27cb72d41c02a853fdccf
Sreevatsan Raman <sree@Sreevatsans-MacBook-Pro.local>
2013-01-30 11:30:22 -0800

    Add java doc for utils

--- src/api/java/com/continuuity/api/data/util/package-info.java
+++ src/api/java/com/continuuity/api/data/util/package-info.java
@@ -1,6 +1,8 @@
 /**
- * Place-holder for dataset package info
- *
+ * This package contains utility classes and helper functions
+ * {@link com.continuuity.api.data.util.Bytes} class handles byte arrays, conversions to/from other types,
+ * comparisons, hash code generation, manufacturing keys for HashMaps or HashSets
+ * {@link com.continuuity.api.data.util.Helpers} classes contain utility functions for timestamp
  *
  */
-package com.continuuity.api.data.lib;
+package com.continuuity.api.data.util;
