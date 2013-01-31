0bf295d8a00582d5e3984e563ab2b4bd360239f2
Mario Lichtsinn <mario@continuuity.com>
2013-01-31 13:39:09 -0800

    fixing npe

--- src/main/java/com/continuuity/gateway/tools/StreamClient.java
+++ src/main/java/com/continuuity/gateway/tools/StreamClient.java
@@ -79,7 +79,8 @@ public class StreamClient {
    */
   void usage(boolean error) {
     PrintStream out = (error ? System.err : System.out);
-    String name = System.getProperty("script").replaceAll("[./]", "");
+    String name = "stream-client";
+    if (System.getProperty("script") != null) name = System.getProperty("script").replaceAll("[./]", "");
     Copyright.print(out);
     out.println("Usage: ");
     out.println("  " + name +
