c739b518e795ccbf04fe14e9324a0b03948c0a7d
Nitin Motgi <nitin@continuuity.com>
2013-02-09 03:06:43 -0800

    Changing to ListenableFuture and finished adding reading in the output file

--- src/main/java/com/continuuity/app/deploy/internal/InMemoryConfigurator.java
+++ src/main/java/com/continuuity/app/deploy/internal/InMemoryConfigurator.java
@@ -4,29 +4,77 @@
 
 package com.continuuity.app.deploy.internal;
 
+import com.continuuity.api.Application;
+import com.continuuity.api.ApplicationSpecification;
+import com.continuuity.api.io.Schema;
+import com.continuuity.api.io.SchemaTypeAdapter;
+import com.continuuity.app.deploy.ConfigResponse;
 import com.continuuity.app.deploy.Configurator;
+import com.continuuity.classloader.JarClassLoader;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 /**
- *
+ * In Memory Configurator doesn't spawn a external process, but
+ * does this in memory.
  */
 public class InMemoryConfigurator implements Configurator  {
-  private final String jarFilename;
+  /**
+   * JAR file path.
+   */
+  private final File jarFilename;
 
-  public InMemoryConfigurator(String jarFilename) {
+  /**
+   * Constructor.
+   * @param jarFilename name of the jar file for which configure is invoked in-memory.
+   */
+  public InMemoryConfigurator(File jarFilename) {
+    Preconditions.checkNotNull(jarFilename);
     this.jarFilename = jarFilename;
   }
 
+  /**
+   * Executes the <code>Application.configure</code> within the same JVM.
+   * <p>
+   *   This method could be dangerous and should be used only in singlenode.
+   * </p>
+   * @return A instance of {@link ListenableFuture}.
+   */
   @Override
-  public Response call() throws Exception {
-    // Load the JAR.
-    // Invoke configure();
-    // Return JSON.
-    return null;
-  }
+  public ListenableFuture<ConfigResponse> config() {
+    SettableFuture result = SettableFuture.create();
+    try {
+      // Load the JAR using the JAR class load and load the manifest file.
+      Object mainClass;
+      JarClassLoader loader = new JarClassLoader(jarFilename.getAbsolutePath());
+      mainClass = loader.getMainClass(Application.class);
 
-  @Override
-  public void destory() {
-    return;
-  }
+      // Convert it to the type application.
+      Application application = (Application) mainClass;
 
+      // Now, we call configure, which returns application specification.
+      ApplicationSpecification specification = application.configure();
+
+      // Convert the specification to JSON.
+      Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(Schema.class, new SchemaTypeAdapter())
+                    .create();
+
+      // We write the Application specification to output file in JSON format.
+      StringWriter writer = new StringWriter();
+      gson.toJson(specification, writer);
+      result.set(new DefaultConfigResponse(0, new StringReader(writer.toString())));
+    } catch (Exception e) {
+      Futures.immediateFailedFuture(e);
+    }
+    return result;
+  }
 }
--- src/main/java/com/continuuity/app/deploy/internal/SandboxConfigurator.java
+++ src/main/java/com/continuuity/app/deploy/internal/SandboxConfigurator.java
@@ -4,63 +4,136 @@
 
 package com.continuuity.app.deploy.internal;
 
+import com.continuuity.app.deploy.ConfigResponse;
 import com.continuuity.app.deploy.Configurator;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.net.URI;
 
 /**
- * Quarantined configuration generates the configuration of an {@link com.continuuity.api.ApplicationSpecification}
- * by running it outside of the server JVM.
+ * SandboxConfigurator spawns a seperate JVM to run configuration of an Application.
+ * <p>
+ *   This class is responsible for starting the process of generating configuration
+ *   by passing in the JAR file of Application be configured by a seperate JVM.
+ * </p>
  */
 public class SandboxConfigurator implements Configurator {
-  private final String jarFilename;
-  private final long timeout;
+  /**
+   * Prefix of temporary file.
+   */
+  private final static String PREFIX = "app-specification";
+  /**
+   * Extension of temporary.
+   */
+  private final static String EXT = ".json";
+
+  /**
+   * Name of JAR file.
+   */
+  private final File jarFilename;
+
+  /**
+   * Sandbox process.
+   */
   private Process process;
-  private boolean destoryed = false;
 
-  public SandboxConfigurator(String jarFilename, long timeout) {
+  /**
+   * Constructor
+   * @param jarFilename Name of the JAR file.
+   */
+  public SandboxConfigurator(File jarFilename) {
+    Preconditions.checkNotNull(jarFilename);
     this.jarFilename = jarFilename;
-    this.timeout = timeout;
   }
 
+  /**
+   * Helper for simplifying creating {@link SandboxConfigurator}
+   * @param jarFilename Name of the file.
+   * @return An instance of {@link ListenableFuture}
+   */
+  public static ListenableFuture<ConfigResponse> config(File jarFilename) {
+    SandboxConfigurator sc = new SandboxConfigurator(jarFilename);
+    return sc.config();
+  }
+
+  /**
+   * Runs the <code>Application.configure()</code> in a sandbox JVM
+   * with high level of security.
+   *
+   * @return An instance of {@link ListenableFuture}
+   */
   @Override
-  public Response call() {
+  public ListenableFuture<ConfigResponse> config() {
+    final SettableFuture<ConfigResponse> result = SettableFuture.create();
+    final File outputFile;
+
     try {
-      process = Runtime.getRuntime().exec(getCommand());
-      process.waitFor(); // Waits till the application is finished running.
-      Integer exit = process.exitValue();
-      if(exit == 0)  {
-        // Success, Read the output file construct a response and return.
-        return new Response() {
-          @Override
-          public InputStream get() {
-            return new ByteArrayInputStream(jarFilename.getBytes());
+      outputFile = File.createTempFile(PREFIX, EXT);
+
+      // Run the command in seperate JVM.
+      process = Runtime.getRuntime().exec(getCommand(outputFile));
+
+      // Add future to handle the case when the future is cancelled.
+      // OnSuccess, we don't do anything, onFailure, we make sure that
+      // process is destroyed.
+      Futures.addCallback(result, new FutureCallback<ConfigResponse>() {
+        @Override
+        public void onSuccess(final ConfigResponse result) {
+          return; // does nothing.
+        }
+
+        @Override
+        public void onFailure(final Throwable t) {
+          // In case the future was cancelled, we have to
+          // destroy the process.
+          if(result.isCancelled()) {
+            process.destroy();
           }
-        };
-      } else {
-        // Failed.
+        }
+      });
+    } catch(Exception e) {
+      // Returns a {@code ListenableFuture} which has an exception set immediately
+      // upon construction.
+      return Futures.immediateFailedFuture(e);
+    }
+
+    // Start a thread that waits for process to complete
+    new Thread() {
+      @Override
+      public void run() {
+        try {
+          // Wait for process to exit and extract the return. If cancelled the process will
+          // be shutdown.
+          process.waitFor();
+          int exit = process.exitValue();
+          if(exit == 0)  {
+            result.set(new DefaultConfigResponse(0, new BufferedReader(new FileReader(outputFile))));
+          } else {
+            result.set(new DefaultConfigResponse(exit, new StringReader("")));
+          }
+        } catch (Exception e) {
+          result.setException(e);
+        }
       }
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-      return null;
-    } catch (IOException e) {
-      return null;
-    }
-    return null;
+    }.start();
+
+    return result;
   }
 
-  private String getCommand() {
-    return "java -cp something.jar com.continuuity.app.deploy.SandboxJVM myapp.jar json.txt";
-  }
-
-  @Override
-  public void destory() {
-    if(! destoryed) {
-      // Kill the process.
-      process.destroy();
-      // Delete the file
-    }
+  /**
+   * @return Returns the command used to execute configure using <code>outputFile</code> in which
+   * the output of run would be stored.
+   */
+  private String getCommand(File outputFile) {
+    return String.format("--jar %s --output %s", jarFilename.getAbsolutePath(),
+                         outputFile.getAbsolutePath());
   }
 }
