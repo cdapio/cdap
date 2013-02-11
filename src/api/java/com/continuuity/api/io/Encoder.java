8435f2d8a26ceec12efbdde4d357bd228aeeb6cd
Terence Yim <terence@continuuity.com>
2013-02-11 15:59:32 -0800

    First cut of the low level data serialization codec

---
+++ src/api/java/com/continuuity/api/io/Encoder.java
@@ -0,0 +1,30 @@
+package com.continuuity.api.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/**
+ *
+ */
+public interface Encoder {
+
+  Encoder writeNull() throws IOException;
+
+  Encoder writeBool(boolean b) throws IOException;
+
+  Encoder writeInt(int i) throws IOException;
+
+  Encoder writeLong(long l) throws IOException;
+
+  Encoder writeFloat(float f) throws IOException;
+
+  Encoder writeDouble(double d) throws IOException;
+
+  Encoder writeString(String s) throws IOException;
+
+  Encoder writeBytes(byte[] bytes) throws IOException;
+
+  Encoder writeBytes(byte[] bytes, int off, int len) throws IOException;
+
+  Encoder writeBytes(ByteBuffer bytes) throws IOException;
+}
