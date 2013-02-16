b0c6dbbeaafe2bba0c713c1af62df50d9373c4f4
anew <andreas@continuuity.com>
2013-02-15 19:06:04 -0800

    ENG-2042 Merge ReadOperationExecutor into OperationExecutor

--- src/main/java/com/continuuity/data/operation/executor/OperationExecutor.java
+++ src/main/java/com/continuuity/data/operation/executor/OperationExecutor.java
@@ -2,10 +2,15 @@ package com.continuuity.data.operation.executor;
 
 
 import com.continuuity.api.data.OperationException;
+import com.continuuity.api.data.OperationResult;
 import com.continuuity.data.operation.OperationContext;
+import com.continuuity.data.operation.Read;
+import com.continuuity.data.operation.ReadAllKeys;
+import com.continuuity.data.operation.ReadColumnRange;
 import com.continuuity.data.operation.WriteOperation;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * TODO: Update these docs
@@ -33,7 +38,12 @@ import java.util.List;
  * properly committed or aborted in that case.
  */
 public interface OperationExecutor
-  extends ReadOperationExecutor, InternalOperationExecutor {
+  extends InternalOperationExecutor {
+
+  /**
+   * @return the name of the executor, set by the implementation for verbose messages
+   */
+  public String getName();
 
   /**
    * Performs a {@link com.continuuity.data.operation.Write} operation.
@@ -96,7 +106,38 @@ public interface OperationExecutor
     throws OperationException;
 
   /**
-   * @return the name of the executor, set by the implementation for verbose messages
+   * Executes a {@link com.continuuity.data.operation.Read} operation.
+   * @param read the operation
+   * @return a result object containing a map of columns to values if the key
+   *    is found. If the key is not found, the result will be empty and the
+   *    status code is KEY_NOT_FOUND.
+   * @throws OperationException is something goes wrong
    */
-  public String getName();
+  public OperationResult<Map<byte[], byte[]>> execute(OperationContext context,
+                                                      Read read)
+    throws OperationException;
+
+  /**
+   * Executes a {@link com.continuuity.data.operation.ReadAllKeys} operation.
+   * @param readKeys the operation
+   * @return a result object containing a list of keys if none found. If no
+   * keys are found, then the result object will be empty and the status
+   * code will be KEY_NOT_FOUND.
+   * @throws OperationException is something goes wrong
+   */
+  public OperationResult<List<byte[]>> execute(OperationContext context,
+                                               ReadAllKeys readKeys)
+    throws OperationException;
+
+  /**
+   * Executes a {@link com.continuuity.data.operation.ReadColumnRange} operation.
+   * @param readColumnRange the operation
+   * @return a result object containing a map of columns to values. If the
+   * key is not found, the result will be empty and the status code is
+   * KEY_NOT_FOUND. If the key exists but there are no columns the given range,
+   * then the result is empty with status code COLUMN_NOT_FOUND.
+   * @throws OperationException is something goes wrong
+   */
+  public OperationResult<Map<byte[], byte[]>> execute(OperationContext context,
+                                                      ReadColumnRange readColumnRange) throws OperationException;
 }
