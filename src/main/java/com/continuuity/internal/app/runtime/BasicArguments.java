c3bb7f5dcf23c63aac38dba42ae93ab69d55bbd8
Terence Yim <terence@continuuity.com>
2013-02-19 11:43:55 -0800

    Added ProgramOptions and refactoring around that.

---
+++ src/main/java/com/continuuity/internal/app/runtime/BasicArguments.java
@@ -0,0 +1,40 @@
+package com.continuuity.internal.app.runtime;
+
+import com.continuuity.app.runtime.Arguments;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *
+ */
+public final class BasicArguments implements Arguments {
+
+  private final Map<String, String> options;
+
+  public BasicArguments(Map<String, String> options) {
+    this.options = ImmutableMap.copyOf(options);
+  }
+
+  @Override
+  public boolean hasOption(String optionName) {
+    return options.containsKey(optionName);
+  }
+
+  @Override
+  public String getOption(String name) {
+    return options.get(name);
+  }
+
+  @Override
+  public String getOption(String name, String defaultOption) {
+    String value = getOption(name);
+    return value == null ? defaultOption : value;
+  }
+
+  @Override
+  public Iterator<Map.Entry<String, String>> iterator() {
+    return options.entrySet().iterator();
+  }
+}
