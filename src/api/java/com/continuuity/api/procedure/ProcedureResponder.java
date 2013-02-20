a32a65edcf8c55a9db1d44567199681b057b28b6
Terence Yim <terence@continuuity.com>
2013-02-20 13:03:23 -0800

    Added javadocs.

--- src/api/java/com/continuuity/api/procedure/ProcedureResponder.java
+++ src/api/java/com/continuuity/api/procedure/ProcedureResponder.java
@@ -14,7 +14,7 @@ public interface ProcedureResponder {
    * Adds to the response to be returned to the caller.
    *
    * @param response An instance of {@link ProcedureResponse} containing the response to be returned.
-   * @return  An instance of {@link ProcedureResponse.Writer}
+   * @return  An instance of {@link ProcedureResponse.Writer} for writing data of the response.
    * @throws IOException When there is issue sending the response to the callee.
    */
   ProcedureResponse.Writer response(ProcedureResponse response) throws IOException;
