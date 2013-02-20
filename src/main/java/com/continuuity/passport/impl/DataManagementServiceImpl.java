0a2a27c0da50e9a98ae1a1dbde347c64f60ff359
Sreevatsan Raman <sree@Sreevatsans-MacBook-Pro.local>
2013-02-20 07:25:20 -0800

    Add passport client to api

--- src/main/java/com/continuuity/passport/impl/DataManagementServiceImpl.java
+++ src/main/java/com/continuuity/passport/impl/DataManagementServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Implementation for Datamanagement service
+ * Implementation for Data-management service
  */
 public class DataManagementServiceImpl implements DataManagementService {
 
