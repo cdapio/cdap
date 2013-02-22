310e07c6c5093a5a15e81b88433bc7d5407e080f
Terence Yim <terence@continuuity.com>
2013-02-22 13:55:58 -0800

    Update Procedure response API to allows writing json data

--- src/api/java/com/continuuity/api/procedure/ProcedureResponder.java
+++ src/api/java/com/continuuity/api/procedure/ProcedureResponder.java
@@ -7,7 +7,9 @@ package com.continuuity.api.procedure;
 import java.io.IOException;
 
 /**
- * This interface defines a response generator.
+ * This interface defines a response generator. Caller should either call {@link #stream(ProcedureResponse)}
+ * to stream data back to client or call {@link #sendJson(ProcedureResponse, Object)} to send json
+ * object back to client.
  */
 public interface ProcedureResponder {
   /**
@@ -15,9 +17,18 @@ public interface ProcedureResponder {
    * {@link ProcedureResponder} will return the same {@link ProcedureResponse.Writer} instance and the
    * latter submitted {@link ProcedureResponse} would be ignored.
    *
-   * @param response An instance of {@link ProcedureResponse} containing the response to be returned.
+   * @param response A {@link ProcedureResponse} containing the response to be returned.
    * @return  An instance of {@link ProcedureResponse.Writer} for writing data of the response.
    * @throws IOException When there is issue sending the response to the callee.
    */
-  ProcedureResponse.Writer response(ProcedureResponse response) throws IOException;
+  ProcedureResponse.Writer stream(ProcedureResponse response) throws IOException;
+
+  /**
+   * Send a response with a json body.
+   *
+   * @param response A {@link ProcedureResponse} containing the response to be returned.
+   * @param object An object to be serialized into json and send as response body.
+   * @throws IOException When there is issue sending the response to the callee.
+   */
+  void sendJson(ProcedureResponse response, Object object) throws IOException;
 }
