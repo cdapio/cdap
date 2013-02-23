2099abeb99778746584872cb41365a45c6c09d46
Nitin Motgi <nitin@continuuity.com>
2013-02-23 01:31:01 -0800

    Added AbstractGeneratorFlowlet and test case that has generator flowlet

---
+++ src/api/java/com/continuuity/api/flow/flowlet/AbstractGeneratorFlowlet.java
@@ -0,0 +1,17 @@
+package com.continuuity.api.flow.flowlet;
+
+/**
+ * Default implementation of {@link Flowlet} for easy extension.
+ * It uses result of {@link #getName()} as the Flowlet name
+ * and result of {@link #getDescription()}} as the Flowlet description.
+ * <p>
+ *   Children classes can overrides the {@link #getName()}} and/or {@link #getDescription()}
+ *   methods to have custom namings. Children can also overrides the {@link #configure()} method
+ *   to have more controls on customization the {@link FlowletSpecification}.
+ * </p>
+ */
+public abstract class AbstractGeneratorFlowlet extends AbstractFlowlet implements GeneratorFlowlet {
+  protected AbstractGeneratorFlowlet(String name) {
+    super(name);
+  }
+}
