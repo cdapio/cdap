4781734c13318cc19c1cd6fda3891ea13a1ef568
Terence Yim <chtyim@gmail.com>
2013-02-23 16:09:29 -0800

    Removed useless classes.

--- src/test/java/com/continuuity/example/countandfilterwords/Common.java
+++
@@ -1,9 +0,0 @@
-package com.continuuity.example.countandfilterwords;
-
-public class Common {
-
-  static boolean verbose = false;
-  static boolean debug = false;
-
-  final static String counterTableName = "fieldcount";
-}
