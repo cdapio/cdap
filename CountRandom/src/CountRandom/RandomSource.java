07d24de581b79840e75d20a070eac4183e07b099
Nitin Motgi <nitin@continuuity.com>
2013-02-23 17:08:11 -0800

    Fixed issue with initialization of random

--- CountRandom/src/CountRandom/RandomSource.java
+++ CountRandom/src/CountRandom/RandomSource.java
@@ -10,9 +10,9 @@ import java.util.Random;
 public class RandomSource extends AbstractGeneratorFlowlet {
   private OutputEmitter<Integer> randomOutput;
 
-  Random random;
-  long millis = 0;
-  int direction = 1;
+  private final Random random = new Random();
+  private long millis = 0;
+  private int direction = 1;
 
   public RandomSource() {
     super("gen");
@@ -21,7 +21,7 @@ public class RandomSource extends AbstractGeneratorFlowlet {
   public void generate() throws InterruptedException {
     Integer randomNumber = new Integer(this.random.nextInt(10000));
     Thread.sleep(millis);
-    millis += direction;
+    millis *= direction;
     if(millis > 100 || millis < 1) {
       direction = direction * -1;
     }
