7cff60596969830d99d0aadf7c4fc652278f95c4
anew <andreas@continuuity.com>
2013-02-25 22:50:09 -0800

    final version of the word count example, same as in dev guide

--- WordCount/src/main/java/com/continuuity/examples/wordcount/WordAssociator.java
+++ WordCount/src/main/java/com/continuuity/examples/wordcount/WordAssociator.java
@@ -4,17 +4,14 @@ import com.continuuity.api.annotation.UseDataSet;
 import com.continuuity.api.data.OperationException;
 import com.continuuity.api.flow.flowlet.AbstractFlowlet;
 
-import java.util.Arrays;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class WordAssociator extends AbstractFlowlet {
   @UseDataSet("wordAssocs")
   private AssociationTable associationTable;
 
-  public void process(String [] words) throws OperationException {
+  public void process(Set<String> words) throws OperationException {
     // Store word associations
-    Set<String> wordSet = new TreeSet<String>(Arrays.asList(words));
-    this.associationTable.writeWordAssocs(wordSet);
+    this.associationTable.writeWordAssocs(words);
   }
 }
