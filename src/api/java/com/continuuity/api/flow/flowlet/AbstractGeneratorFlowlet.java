0562c459841f61165031e063885285cb0b768b1f
Terence Yim <chtyim@gmail.com>
2013-02-26 01:44:28 -0800

    More javadoc.

--- src/api/java/com/continuuity/api/flow/flowlet/AbstractGeneratorFlowlet.java
+++ src/api/java/com/continuuity/api/flow/flowlet/AbstractGeneratorFlowlet.java
@@ -1,14 +1,9 @@
 package com.continuuity.api.flow.flowlet;
 
 /**
- * Default implementation of {@link Flowlet} for easy extension.
- * It uses result of {@link #getName()} as the Flowlet name
- * and result of {@link #getDescription()}} as the Flowlet description.
- * <p>
- *   Children classes can overrides the {@link #getName()}} and/or {@link #getDescription()}
- *   methods to have custom namings. Children can also overrides the {@link #configure()} method
- *   to have more controls on customization the {@link FlowletSpecification}.
- * </p>
+ * The abstract class provides default implementation of {@link GeneratorFlowlet} for easy extension.
+ *
+ * @see AbstractFlowlet
  */
 public abstract class AbstractGeneratorFlowlet extends AbstractFlowlet implements GeneratorFlowlet {
 
