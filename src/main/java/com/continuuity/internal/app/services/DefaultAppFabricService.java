05b848142fd131ae089e2ddf9c745077f692db65
Terence Yim <chtyim@gmail.com>
2013-02-28 23:53:56 -0800

    Added delete program implementation. Fix reset implementation

--- src/main/java/com/continuuity/internal/app/services/DefaultAppFabricService.java
+++ src/main/java/com/continuuity/internal/app/services/DefaultAppFabricService.java
@@ -61,8 +61,10 @@ import com.continuuity.internal.app.services.legacy.QueryDefinitionImpl;
 import com.continuuity.internal.app.services.legacy.StreamNamerImpl;
 import com.continuuity.internal.filesystem.LocationCodec;
 import com.continuuity.metadata.MetadataService;
+import com.continuuity.metadata.thrift.Account;
 import com.continuuity.metrics2.frontend.MetricsFrontendServiceImpl;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
@@ -801,32 +803,57 @@ public class DefaultAppFabricService implements AppFabricService.Iface {
   }
 
   /**
-   * Deletes a flow specified by {@code FlowIdentifier}.
+   * Deletes a program specified by {@code FlowIdentifier}.
    *
    * @param identifier of a flow.
    * @throws AppFabricServiceException when there is an issue deactivating the flow.
    */
   @Override
   public void remove(AuthToken token, FlowIdentifier identifier) throws AppFabricServiceException {
-    Preconditions.checkNotNull(token);
+    Preconditions.checkNotNull(identifier, "No application id provided.");
+
+    Id.Program programId = Id.Program.from(identifier.getAccountId(),
+                                           identifier.getApplicationId(),
+                                           identifier.getFlowId());
+
+    // Make sure it is not running
+    Preconditions.checkState(!anyRunning(new Predicate<Id.Program>() {
+      @Override
+      public boolean apply(Id.Program programId) {
+        return programId.equals(programId);
+      }
+    }, Type.values()), "Program still running for application " + programId.getApplication() + "," + programId.getId());
+
+
+    Type programType = entityTypeToType(identifier);
+    for (Map.Entry<RunId, ProgramRuntimeService.RuntimeInfo> entry : runtimeService.list(programType).entrySet()) {
+      Preconditions.checkState(!programId.equals(entry.getValue().getProgramId()),
+                               "Program still running: application=%s, type=%s, program=%s",
+                               programId.getApplication(), programType, programId.getId());
+    }
+
+    // Delete the program from store.
+    try {
+      store.remove(programId);
+    } catch (OperationException e) {
+      throw new AppFabricServiceException("Fail to delete program " + e.getMessage());
+    }
   }
 
   @Override
   public void removeApplication(AuthToken token, FlowIdentifier identifier) throws AppFabricServiceException {
-    Preconditions.checkNotNull(token, "No auth token provided.");
     Preconditions.checkNotNull(identifier, "No application id provided.");
 
     Id.Account accountId = Id.Account.from(identifier.getAccountId());
-    Id.Application appId = Id.Application.from(accountId, identifier.getApplicationId());
+    final Id.Application appId = Id.Application.from(accountId, identifier.getApplicationId());
 
     // Check if all are stopped.
-    for (Map.Entry<RunId, ProgramRuntimeService.RuntimeInfo> entry :  runtimeService.list(Type.FLOW).entrySet()) {
-      Id.Program programId = entry.getValue().getProgramId();
-      if (programId.getApplication().equals(appId)) {
-        throw new IllegalStateException(String.format("Program still running: application=%s, type=%s, program=%s",
-                                                      appId.getId(), entry.getValue().getType(), programId.getId()));
+    Preconditions.checkState(!anyRunning(new Predicate<Id.Program>() {
+      @Override
+      public boolean apply(Id.Program programId) {
+        return programId.getApplication().equals(appId);
       }
-    }
+    }, Type.values()), "There are program still running for application " + appId.getId());
 
     // Delete the App from store
     try {
@@ -853,19 +880,48 @@ public class DefaultAppFabricService implements AppFabricService.Iface {
     }
   }
 
+  /**
+   * Check if any program that satisfy the given {@link Predicate} is running
+   * @param predicate Get call on each running {@link Id.Program}.
+   * @param types Types of program to check
+   * @return true if any of the running program satisfy the predicate, false otherwise.
+   */
+  private boolean anyRunning(Predicate<Id.Program> predicate, Type...types) {
+    for (Type type : types) {
+      for (Map.Entry<RunId, ProgramRuntimeService.RuntimeInfo> entry :  runtimeService.list(type).entrySet()) {
+        if (predicate.apply(entry.getValue().getProgramId())) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   @Override
   public void removeAll(AuthToken token, String account) throws AppFabricServiceException {
-    Preconditions.checkNotNull(token);
+    Preconditions.checkNotNull(account);
+    // TODO: Is it the same as reset??
   }
 
   @Override
   public void reset(AuthToken token, String account) throws AppFabricServiceException {
     Preconditions.checkNotNull(account);
 
+    final Id.Account accountId = Id.Account.from(account);
+
+    // Check if any program is still running
+    Preconditions.checkState(!anyRunning(new Predicate<Id.Program>() {
+      @Override
+      public boolean apply(Id.Program programId) {
+        return programId.getAccountId().equals(accountId);
+      }
+    }, Type.values()), "There are program still running under account " + accountId.getId());
+
+
     deleteMetrics(account);
     // delete all meta data
     try {
-      mds.deleteAll(account);
+      store.removeAll(accountId);
     } catch (Exception e) {
       String message = String.format("Error deleting all meta data for " +
                                        "account '%s': %s. At %s", account, e.getMessage(),
