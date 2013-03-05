c6f3a28c46d357c1aa1e4ff34a6c177ce43721be
anew <andreas@continuuity.com>
2013-03-04 22:11:53 -0800

    add a unit test to word count example

---
+++ WordCount/src/test/java/com/continuuity/examples/wordcount/WordCountTest.java
@@ -0,0 +1,72 @@
+package com.continuuity.examples.wordcount;
+
+import com.continuuity.internal.test.RuntimeStats;
+import com.continuuity.test.AppFabricTestBase;
+import com.continuuity.test.ApplicationManager;
+import com.continuuity.test.FlowManager;
+import com.continuuity.test.ProcedureClient;
+import com.continuuity.test.ProcedureManager;
+import com.continuuity.test.RuntimeMetrics;
+import com.continuuity.test.StreamWriter;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ */
+public class WordCountTest extends AppFabricTestBase {
+
+  static Type stringMapType = new TypeToken<Map<String, String>>(){}.getType();
+  static Type objectMapType = new TypeToken<Map<String, Object>>(){}.getType();
+
+  @Test
+  public void testWordCount() throws IOException, TimeoutException, InterruptedException {
+
+    // deploy the application
+    ApplicationManager appManager = deployApplication(WordCount.class);
+
+    // start the flow and the procedure
+    FlowManager flowManager = appManager.startFlow("WordCounter");
+    ProcedureManager procManager = appManager.startProcedure("RetrieveCount");
+
+    // send a few events to the stream
+    StreamWriter writer = appManager.getStreamWriter("wordStream");
+    writer.send("hello world");
+    writer.send("a wonderful world");
+    writer.send("the world says hello");
+
+    // wait for the events to be processed, or at most 5 seconds
+    RuntimeMetrics metrics = RuntimeStats.getFlowletMetrics("WordCount", "WordCounter", "associator");
+    metrics.waitForProcessed(3, 5, TimeUnit.SECONDS);
+
+    // now call the procedure
+    ProcedureClient client = procManager.getClient();
+
+    // first verify global statistics
+    String response = client.query("getStats", Collections.EMPTY_MAP);
+    Map<String, String> map = new Gson().fromJson(response, stringMapType);
+    Assert.assertEquals("9", map.get("totalWords"));
+    Assert.assertEquals("6", map.get("uniqueWords"));
+    Assert.assertEquals(((double)42)/9, (double)Double.valueOf(map.get("averageLength")), 0.001);
+
+    // now verify some statistics for one of the words
+    response = client.query("getCount", ImmutableMap.of("word","world"));
+    Map<String, Object> omap = new Gson().fromJson(response, objectMapType);
+    Assert.assertEquals("world", omap.get("word"));
+    Assert.assertEquals(3.0, omap.get("count"));
+    // the associations are a map within the map
+    Map<String, Double> assocs = (Map<String, Double>) omap.get("assocs");
+    Assert.assertEquals(2.0, (double)assocs.get("hello"), 0.000001);
+    Assert.assertTrue(assocs.containsKey("hello"));
+  }
+}
