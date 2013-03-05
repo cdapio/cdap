26eaf6bec24f7493e38aa93e4330cd5cfb517e2c
anew <andreas@continuuity.com>
2013-03-05 12:48:50 -0800

    added a java doc

--- HelloWorld/src/com/continuuity/examples/helloworld/HelloWorld.java
+++ HelloWorld/src/com/continuuity/examples/helloworld/HelloWorld.java
@@ -20,7 +20,12 @@ import com.continuuity.api.procedure.ProcedureResponse;
 import static com.continuuity.api.procedure.ProcedureResponse.Code.SUCCESS;
 
 /**
- *
+ * This is a simple HelloWorld example that uses one stream, on dataset, one flow and one procedure.
+ * <uL>
+ *   <li>A stream to send names to.</li>
+ *   <li>A flow with a single flowlet that reads the stream and stores each name in a KeyValueTable</li>
+ *   <li>A procedure that reads the name from the KeyValueTable and prints Hello [Name]!</li>
+ * </uL>
  */
 public class HelloWorld implements Application {
 
