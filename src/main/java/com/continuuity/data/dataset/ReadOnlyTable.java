ec1c7e308ad9cd00d30cb847d63b879ed93b331d
anew <andreas@continuuity.com>
2013-03-13 12:28:30 -0700

    refactor: ReadWriteTable should not inherit from ReadOnlyTable

--- src/main/java/com/continuuity/data/dataset/ReadOnlyTable.java
+++ src/main/java/com/continuuity/data/dataset/ReadOnlyTable.java
@@ -1,14 +1,10 @@
 package com.continuuity.data.dataset;
 
 import com.continuuity.api.data.OperationException;
-import com.continuuity.api.data.OperationResult;
 import com.continuuity.api.data.dataset.table.Increment;
-import com.continuuity.api.data.dataset.table.Read;
 import com.continuuity.api.data.dataset.table.Table;
 import com.continuuity.api.data.dataset.table.WriteOperation;
 import com.continuuity.data.DataFabric;
-import com.continuuity.data.operation.ReadColumnRange;
-import com.continuuity.data.operation.executor.TransactionAgent;
 import com.continuuity.data.operation.executor.TransactionProxy;
 
 import java.util.Map;
@@ -44,50 +40,7 @@ public class ReadOnlyTable extends RuntimeTable {
    * @param fabric the data fabric
    */
   ReadOnlyTable(Table table, DataFabric fabric, TransactionProxy proxy) {
-    super(table, fabric);
-    this.proxy = proxy;
-  }
-
-  /** the transaction proxy for all operations */
-  protected TransactionProxy proxy;
-
-  protected TransactionAgent getTransactionAgent() {
-    return this.proxy.getTransactionAgent();
-  }
-
-  // synchronous read
-  @Override
-  public OperationResult<Map<byte[], byte[]>> read(Read read)
-    throws OperationException {
-
-    if (read.getColumns() != null) {
-      // this is a multi-column read
-      com.continuuity.data.operation.Read op =
-        new com.continuuity.data.operation.Read(this.tableName(), read.getRow(), read.getColumns());
-      op.setMetricName(getMetricName());
-      if (this.proxy != null) {
-        // new-style: use transaction agent
-        return this.getTransactionAgent().execute(op);
-      } else {
-        // old-style: use data fabric
-        // TODO this will go away with the new flow system
-        return this.getDataFabric().read(op);
-      }
-    } else {
-      // this is a column-range red
-      ReadColumnRange op = new ReadColumnRange(
-        this.tableName(), read.getRow(), read.getStartCol(), read.getStopCol(), read.getLimit());
-      op.setMetricName(getMetricName());
-      if (this.proxy != null) {
-        // new-style: use transaction agent
-        return this.getTransactionAgent().execute(op);
-      } else {
-        // old-style: use data fabric
-        // TODO this will go away with the new flow system
-        return this.getDataFabric().read(op);
-
-      }
-    }
+    super(table, fabric, proxy);
   }
 
   // no support for write operations
--- src/main/java/com/continuuity/data/dataset/ReadWriteTable.java
+++ src/main/java/com/continuuity/data/dataset/ReadWriteTable.java
@@ -1,23 +1,14 @@
 package com.continuuity.data.dataset;
 
 
-import com.continuuity.api.data.OperationException;
-import com.continuuity.api.data.dataset.table.Delete;
-import com.continuuity.api.data.dataset.table.Increment;
-import com.continuuity.api.data.dataset.table.Swap;
 import com.continuuity.api.data.dataset.table.Table;
-import com.continuuity.api.data.dataset.table.Write;
-import com.continuuity.api.data.dataset.table.WriteOperation;
 import com.continuuity.data.DataFabric;
-import com.continuuity.data.operation.CompareAndSwap;
 import com.continuuity.data.operation.executor.TransactionProxy;
-
-import java.util.Map;
 
 /**
  * The read/write runtime implementation of the Table data set.
  */
-public class ReadWriteTable extends ReadOnlyTable {
+public class ReadWriteTable extends RuntimeTable {
 
   /**
    * Given a Table, create a new ReadWriteTable and make it the delegate for that
@@ -46,60 +37,4 @@ public class ReadWriteTable extends ReadOnlyTable {
   private ReadWriteTable(Table table, DataFabric fabric, TransactionProxy proxy) {
     super(table, fabric, proxy);
   }
-
-  /**
-   * Turn a table operation into a data fabric operation.
-   *
-   * @param op a table write operation
-   * @return the corresponding data fabric operation
-   */
-  private com.continuuity.data.operation.WriteOperation toOperation(WriteOperation op) {
-    com.continuuity.data.operation.WriteOperation operation;
-    if (op instanceof Write) {
-      Write write = (Write)op;
-      operation = new com.continuuity.data.operation.Write(
-          this.tableName(), write.getRow(), write.getColumns(), write.getValues());
-    }
-    else if (op instanceof Delete) {
-      Delete delete = (Delete)op;
-      operation = new com.continuuity.data.operation.Delete(
-          this.tableName(), delete.getRow(), delete.getColumns());
-    }
-    else if (op instanceof Increment) {
-      operation = toOperation((Increment)op);
-    }
-    else if (op instanceof Swap) {
-      Swap swap = (Swap)op;
-      operation = new CompareAndSwap(
-          this.tableName(), swap.getRow(), swap.getColumn(), swap.getExpected(), swap.getValue());
-    }
-    else { // can't happen but...
-      throw new IllegalArgumentException("Received an operation of unknown type " + op.getClass().getName());
-    }
-    operation.setMetricName(getMetricName());
-    return operation;
-  }
-
-  /**
-   * Helper to convert an increment operation
-   * @param increment the table increment
-   * @return a corresponding data fabric increment operation
-   */
-  private com.continuuity.data.operation.Increment toOperation(Increment increment) {
-    com.continuuity.data.operation.Increment operation = new com.continuuity.data.operation.Increment(
-      this.tableName(), increment.getRow(), increment.getColumns(), increment.getValues());
-    operation.setMetricName(getMetricName());
-    return operation;
-  }
-
-  @Override
-  public void write(WriteOperation op) throws OperationException {
-    this.getTransactionAgent().submit(toOperation(op));
-  }
-
-  @Override
-  public Map<byte[], Long> incrementAndGet(Increment increment) throws OperationException {
-    return this.getTransactionAgent().execute(toOperation(increment));
-  }
-
 }
