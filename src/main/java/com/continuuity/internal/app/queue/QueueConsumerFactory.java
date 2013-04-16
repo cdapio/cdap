67b0183d76e4b32221c0f7a29f1935d39517a15b
poorna <poorna@continuuity.com>
2013-04-16 16:04:04 -0700

    Propagating OperationException from QueueConfigure operation as a runtime exception.

--- src/main/java/com/continuuity/internal/app/queue/QueueConsumerFactory.java
+++ src/main/java/com/continuuity/internal/app/queue/QueueConsumerFactory.java
@@ -1,12 +1,6 @@
 package com.continuuity.internal.app.queue;
 
-import com.continuuity.api.data.OperationException;
-import com.continuuity.app.queue.QueueName;
-import com.continuuity.data.operation.ttqueue.QueueConfig;
 import com.continuuity.data.operation.ttqueue.QueueConsumer;
-import com.continuuity.data.operation.ttqueue.QueueAdmin;
-
-import java.util.Map;
 
 /**
  *  A factory interface to create QueueConsumer
@@ -17,7 +11,6 @@ public interface QueueConsumerFactory {
    * Creates a QueueConsumer with the given groupSize, and runs a QueueConfigure with the new QueueConsumer.
    * @param groupSize Size of the group of which the created QueueConsumer will be part of
    * @return Created QueueConsumer
-   * @throws OperationException An OperationException can be thrown during the execution of QueueConfigure operation
    */
-  QueueConsumer create(int groupSize) throws OperationException;
+  QueueConsumer create(int groupSize);
 }
