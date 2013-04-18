ee81929e00ad37f7119f483fb53953c85b364c67
Sree <sree@continuuity.com>
2013-04-17 18:38:51 -0700

    Add back static import for fail, that got removed

--- src/test/java/com/continuuity/data/operation/ttqueue/TestTTQueueNew.java
+++ src/test/java/com/continuuity/data/operation/ttqueue/TestTTQueueNew.java
@@ -37,6 +37,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  *
