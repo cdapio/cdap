0071dc4f6cfa7535b9adb153871990a799cc4dc5
anew <andreas@continuuity.com>
2013-05-08 01:09:54 -0700

    +review CR-121 ENG-2223  fix checkstyle bugs for package engine.hypersql|leveldb|memory

---
+++ src/main/java/com/continuuity/data/engine/memory/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
+ */
+
+/**
+ * This package contains the in-memory data fabric implementations.
+ */
+package com.continuuity.data.engine.memory;
