add882c378e7786f22bf51b6d2905e00ccfd1e12
anew <andreas@continuuity.com>
2013-05-07 17:26:55 -0700

    +review CR-121 ENG-2223  fix checkstyle bugs for package dataset

---
+++ src/main/java/com/continuuity/data/dataset/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
+ */
+
+/**
+ * This package contains implementations of the data set framework and built-in data sets.
+ */
+package com.continuuity.data.dataset;
---
+++ src/main/java/com/continuuity/data/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
+ */
+
+/**
+ * This package contains implementations of the data fabric.
+ */
+package com.continuuity.data;
