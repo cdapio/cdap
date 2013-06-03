378481d2f8025c6c12b067e63c7d5778fe095905
Mario Lichtsinn <mario@continuuity.com>
2013-06-02 22:09:37 -0700

    +review CR-241 ENG-2391 #time git status  created testbase sourceset, reorganized developer and default test account id # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch feature/createtestbase # Changes to be committed: # (use "git reset HEAD <file>..." to unstage) # # modified: build.gradle # new file: src/main/java/com/continuuity/data/Constants.java # modified: src/test/java/com/continuuity/data/util/OperationUtil.java # new file: src/testbase/java/com/continuuity/test/data/Constants.java #

---
+++ src/main/java/com/continuuity/data/Constants.java
@@ -0,0 +1,12 @@
+package com.continuuity.data;
+
+/**
+ * Constants is a utility class that contains a set of constants that are used throughout the AppFabric project.
+ */
+public class Constants {
+  /**
+   * Corresponds to account id used when running in local mode.
+   * NOTE: value should be in sync with the one used by UI.
+   */
+  public final static String DEVELOPER_ACCOUNT_ID = "developer";
+} // end of Constants class
