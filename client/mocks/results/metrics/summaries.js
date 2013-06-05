c4d0cc28540346058805c1950d7cad98dea67a08
Don Mosites <don@continuuity.com>
2013-06-05 14:11:11 -0700

    +review CR-270 ENG-2586 #time 30m  Mock data initial

---
+++ client/mocks/results/metrics/summaries.js
@@ -0,0 +1,21 @@
+/*
+ * Summary Metrics Result Mock
+ */
+
+define([], function () {
+
+	return {
+		"/process/busyness/flows": {
+			"status": 200,
+			"result": {
+				"summary": {
+					"min": 1,
+					"max": 1,
+					"avg": 1,
+					"trend": 1
+				}
+			}
+		}
+	};
+
+});
