305324cb95c892fed35e2b27ab410c7f8336e493
poorna <poorna@continuuity.com>
2013-07-01 11:10:39 -0700

    Checkstyle changes.

--- watchdog/src/main/java/com/continuuity/logging/appender/LogAppender.java
+++ watchdog/src/main/java/com/continuuity/logging/appender/LogAppender.java
@@ -4,7 +4,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
 /**
- * Continuuity log appender interface
+ * Continuuity log appender interface.
  */
 public abstract class LogAppender extends AppenderBase<ILoggingEvent> {
 
