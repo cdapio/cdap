65bcff972f3c52000b978bfe8a9650d5f7a50f0c
Sree <sree@continuuity.com>
2013-07-02 08:40:05 -0700

    Fix checkstyle in tests

--- data-fabric/src/test/java/com/continuuity/data/util/OperationUtil.java
+++ data-fabric/src/test/java/com/continuuity/data/util/OperationUtil.java
@@ -3,8 +3,11 @@ package com.continuuity.data.util;
 import com.continuuity.data.Constants;
 import com.continuuity.data.operation.OperationContext;
 
+/**
+ * Utility class.
+ */
 public class OperationUtil {
 
-  /** defaults to be used everywhere where we don't have authenticated accounts */
+  /** defaults to be used everywhere where we don't have authenticated accounts. */
   public static final OperationContext DEFAULT = new OperationContext(Constants.DEVELOPER_ACCOUNT_ID);
 }
