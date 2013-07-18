ea838d43bdd91966abd699cd919ce1beb4fb3e90
poorna <poorna@continuuity.com>
2013-07-17 18:23:04 -0700

    Review changes

--- kafka/src/main/java/com/continuuity/kafka/run/KafkaServerMain.java
+++ kafka/src/main/java/com/continuuity/kafka/run/KafkaServerMain.java
@@ -4,13 +4,10 @@ import com.continuuity.common.conf.CConfiguration;
 import com.continuuity.common.conf.Constants;
 import com.continuuity.common.runtime.DaemonMain;
 import com.continuuity.weave.internal.kafka.EmbeddedKafkaServer;
-import com.google.common.base.Throwables;
+import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Properties;
 
 /**
@@ -19,31 +16,38 @@ import java.util.Properties;
 public class KafkaServerMain extends DaemonMain {
   private static final Logger LOG = LoggerFactory.getLogger(KafkaServerMain.class);
 
-  private static final String KAFKA_CONFIG = "kafka.server.config";
-  private static final String ZOOKEEPER_CONNECT = "zookeeper.connect";
+  private static final String KAFKA_PORT_CONFIG = "kafka.port";
+  private static final String KAFKA_NUM_PARTITIONS_CONFIG = "kafka.num.partitions";
+  private static final String KAFKA_LOG_DIR_CONFIG = "kafka.log.dir";
 
-  private CConfiguration cConf;
+  private static final String ZOOKEEPER_NAMESPACE = "continuuity/kafka";
+
+  private Properties kafkaProperties;
   private EmbeddedKafkaServer kafkaServer;
 
   @Override
   public void init(String[] args) {
-    cConf = CConfiguration.create();
+    if (args.length != 1) {
+      String name = KafkaServerMain.class.getSimpleName();
+      throw new IllegalArgumentException(String.format("Usage: %s <brokerId>", name));
+    }
+
+    int brokerId = Integer.parseInt(args[0]);
+
+    CConfiguration cConf = CConfiguration.create();
+    String zkConnectStr = String.format("%s/%s", cConf.get(Constants.CFG_ZOOKEEPER_ENSEMBLE), ZOOKEEPER_NAMESPACE);
+    int port = cConf.getInt(KAFKA_PORT_CONFIG, -1);
+    int numPartitions = cConf.getInt(KAFKA_NUM_PARTITIONS_CONFIG, 1);
+    String logDir = cConf.get(KAFKA_LOG_DIR_CONFIG);
+
+    kafkaProperties = generateKafkaConfig(brokerId, zkConnectStr, port, numPartitions, logDir);
   }
 
   @Override
   public void start() {
     LOG.info("Starting embedded kafka server...");
 
-    String configFile = cConf.get(KAFKA_CONFIG);
-    LOG.info(String.format("Using config file %s", configFile));
-    Properties kafkaConfig = readKafkaConfig(configFile);
-
-    // If zookeeper config is not present, then read it from CConfiguration
-    if (kafkaConfig.getProperty(ZOOKEEPER_CONNECT) == null) {
-      kafkaConfig.setProperty(ZOOKEEPER_CONNECT, cConf.get(Constants.CFG_ZOOKEEPER_ENSEMBLE));
-    }
-
-    kafkaServer = new EmbeddedKafkaServer(KafkaServerMain.class.getClassLoader(), kafkaConfig);
+    kafkaServer = new EmbeddedKafkaServer(KafkaServerMain.class.getClassLoader(), kafkaProperties);
     kafkaServer.startAndWait();
 
     LOG.info("Embedded kafka server started successfully.");
@@ -60,23 +64,24 @@ public class KafkaServerMain extends DaemonMain {
     // Nothing to do
   }
 
-  private Properties readKafkaConfig(String configFile) {
-    Reader reader = null;
-    try {
-      reader = new FileReader(configFile);
-      Properties properties = new Properties();
-      properties.load(reader);
-      return properties;
-    } catch (IOException e) {
-      throw Throwables.propagate(e);
-    } finally {
-      if (reader != null) {
-        try {
-          reader.close();
-        } catch (IOException e) {
-          LOG.error(String.format("Exception while closing reader of file %s", configFile), e);
-        }
-      }
-    }
+  private Properties generateKafkaConfig(int brokerId, String zkConnectStr, int port, int numPartitions,
+                                         String logDir) {
+    Preconditions.checkState(port > 0, "Failed to get random port.");
+    Preconditions.checkState(numPartitions > 0, "Num partitions should be greater than zero.");
+
+    Properties prop = new Properties();
+    prop.setProperty("broker.id", Integer.toString(brokerId));
+    prop.setProperty("port", Integer.toString(port));
+    prop.setProperty("socket.send.buffer.bytes", "1048576");
+    prop.setProperty("socket.receive.buffer.bytes", "1048576");
+    prop.setProperty("socket.request.max.bytes", "104857600");
+    prop.setProperty("log.dir", logDir);
+    prop.setProperty("num.partitions", Integer.toString(numPartitions));
+    prop.setProperty("log.flush.interval.messages", "10000");
+    prop.setProperty("log.flush.interval.ms", "1000");
+    prop.setProperty("log.segment.bytes", "536870912");
+    prop.setProperty("zookeeper.connect", zkConnectStr);
+    prop.setProperty("zookeeper.connection.timeout.ms", "1000000");
+    return prop;
   }
 }
