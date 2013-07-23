9bd5df2d672c133ae9c30669a75f2c2fdbc91549
Terence Yim <chtyim@gmail.com>
2013-07-22 23:55:43 -0700

    Remove irrelevant tests from the old metrics system

--- overlord/src/test/java/com/continuuity/metrics2/collector/server/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
- */
-
-/**
- * Metrics2 Server Tests.
- */
-package com.continuuity.metrics2.collector.server;
-
--- overlord/src/test/java/com/continuuity/runtime/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
- */
-
-/**
- * Metrics2 Runtime Tests.
- */
-package com.continuuity.runtime;
-
