08d9675d59bc2e48fde143f3797b48027c51c31e
Sree <sree@continuuity.com>
2013-08-01 17:31:27 -0700

    add MultiObjectStore implementation based on table as data store

--- data-fabric/src/main/java/com/continuuity/data/dataset/DataSetInstantiationBase.java
+++ data-fabric/src/main/java/com/continuuity/data/dataset/DataSetInstantiationBase.java
@@ -4,6 +4,7 @@ import com.continuuity.api.data.DataSet;
 import com.continuuity.api.data.DataSetSpecification;
 import com.continuuity.api.data.OperationException;
 import com.continuuity.api.data.dataset.FileDataSet;
+import com.continuuity.api.data.dataset.MultiObjectStore;
 import com.continuuity.api.data.dataset.ObjectStore;
 import com.continuuity.api.data.dataset.table.Table;
 import com.continuuity.data.DataFabric;
@@ -210,6 +211,14 @@ public class DataSetInstantiationBase {
     // lead to infinite recursion!).
     if (obj instanceof ObjectStore<?> && !(obj instanceof RuntimeObjectStore)) {
       RuntimeObjectStore.setImplementation((ObjectStore<?>) obj, this.classLoader);
+      // but do not return yet, continue to inject data fabric into the runtime
+    }
+
+    // for object stores (and subclasses), we set the delegate to a RuntimeObjectStore. But RuntimeObjectStore
+    // is a subclass of ObjectStore itself, and there is no point in setting its delegate (it would actually
+    // lead to infinite recursion!).
+    if (obj instanceof MultiObjectStore<?> && !(obj instanceof RuntimeMultiObjectStore)) {
+      RuntimeMultiObjectStore.setImplementation((MultiObjectStore<?>) obj, this.classLoader);
       // but do not return yet, continue to inject data fabric into the runtime
     }
 
