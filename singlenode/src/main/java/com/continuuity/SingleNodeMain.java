443f1cb833b8667071b91e86745ba01e0ffa5619
Alex Gauthier <alexg@continuuity.com>
2013-08-06 16:07:56 -0700

    reverting node path issue

--- singlenode/src/main/java/com/continuuity/SingleNodeMain.java
+++ singlenode/src/main/java/com/continuuity/SingleNodeMain.java
@@ -185,7 +185,7 @@ public class SingleNodeMain {
    */
   public static boolean nodeExists() {
     try {
-      Process proc = Runtime.getRuntime().exec("/usr/local/bin/node -v");
+      Process proc = Runtime.getRuntime().exec("node -v");
       TimeUnit.SECONDS.sleep(2);
       int exitValue = proc.exitValue();
       if (exitValue != 0) {
