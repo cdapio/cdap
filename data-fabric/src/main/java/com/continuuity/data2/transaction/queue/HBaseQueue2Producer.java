81f7c98762116982ce48c8f65e04801607085424
Terence Yim <terence@continuuity.com>
2013-08-21 11:34:32 -0700

    Code refactoring. Added test case for rollback.

--- data-fabric/src/main/java/com/continuuity/data2/transaction/queue/HBaseQueue2Producer.java
+++ data-fabric/src/main/java/com/continuuity/data2/transaction/queue/HBaseQueue2Producer.java
@@ -7,8 +7,6 @@ import com.continuuity.api.common.Bytes;
 import com.continuuity.common.queue.QueueName;
 import com.continuuity.data.operation.ttqueue.QueueEntry;
 import com.continuuity.data2.transaction.Transaction;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.HTable;
@@ -16,79 +14,37 @@ import org.apache.hadoop.hbase.client.Put;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  *
  */
 public final class HBaseQueue2Producer extends AbstractQueue2Producer implements Closeable {
 
-  private final BlockingQueue<QueueEntry> queue;
   private final byte[] queueRowPrefix;
   private final HTable hTable;
   private final List<byte[]> rollbackKeys;
-  private Transaction transaction;
-  private int lastEnqueueCount;
 
   public HBaseQueue2Producer(HTable hTable, QueueName queueName, QueueMetrics queueMetrics) {
     super(queueMetrics);
-    this.queue = new LinkedBlockingQueue<QueueEntry>();
     this.queueRowPrefix = HBaseQueueUtils.getQueueRowPrefix(queueName);
     this.rollbackKeys = Lists.newArrayList();
     this.hTable = hTable;
   }
 
   @Override
-  public void enqueue(QueueEntry entry) throws IOException {
-    if (transaction != null) {
-      // Simply buffer it in memory.
-      queue.add(entry);
-    } else {
-      persist(ImmutableList.of(entry));
-    }
-  }
-
-  @Override
-  public void enqueue(Iterable <QueueEntry> entries) throws IOException {
-    if (transaction != null) {
-      Iterables.addAll(queue, entries);
-    } else {
-      persist(entries);
-    }
-  }
-
-  @Override
   public void startTx(Transaction tx) {
-    queue.clear();
+    super.startTx(tx);
     rollbackKeys.clear();
-    transaction = tx;
-  }
-
-  @Override
-  public Collection<byte[]> getTxChanges() {
-    // Always empty changes, as enqueue is append only, nothing could be conflict.
-    return ImmutableList.of();
-  }
-
-  @Override
-  public boolean commitTx() throws Exception {
-    List<QueueEntry> entries = Lists.newArrayListWithCapacity(queue.size());
-    queue.drainTo(entries);
-    lastEnqueueCount = entries.size();
-    persist(entries);
-    return true;
-  }
-
-  @Override
-  protected int getLastEnqueueCount() {
-    return lastEnqueueCount;
   }
 
   @Override
   public boolean rollbackTx() throws Exception {
+    // If nothing to rollback, simply return
+    if (rollbackKeys.isEmpty()) {
+      return true;
+    }
+
     // Delete the persisted entries
     List<Delete> deletes = Lists.newArrayList();
     for (byte[] rowKey : rollbackKeys) {
@@ -97,8 +53,6 @@ public final class HBaseQueue2Producer extends AbstractQueue2Producer implements
     }
     hTable.delete(deletes);
     hTable.flushCommits();
-
-    lastEnqueueCount = 0;
 
     return true;
   }
@@ -111,8 +65,7 @@ public final class HBaseQueue2Producer extends AbstractQueue2Producer implements
   /**
    * Persist queue entries into HBase.
    */
-  private void persist(Iterable<QueueEntry> entries) throws IOException {
-    // TODO: What key should it be if transaction is null?
+  protected void persist(Iterable<QueueEntry> entries, Transaction transaction) throws IOException {
     long writePointer = transaction.getWritePointer();
     byte[] rowKeyPrefix = Bytes.add(queueRowPrefix, Bytes.toBytes(writePointer));
     int count = 0;
@@ -121,9 +74,7 @@ public final class HBaseQueue2Producer extends AbstractQueue2Producer implements
     for (QueueEntry entry : entries) {
       // Row key = queue_name + writePointer + counter
       byte[] rowKey = Bytes.add(rowKeyPrefix, Bytes.toBytes(count++));
-      if (transaction != null) {
-        rollbackKeys.add(rowKey);
-      }
+      rollbackKeys.add(rowKey);
       // No need to write ts=writePointer, as the row key already contains the writePointer
       Put put = new Put(rowKey);
       put.add(HBaseQueueConstants.COLUMN_FAMILY,
