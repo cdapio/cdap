48c101f12bcba1962a20c13faca78a42bf65cc3e
Nitin Motgi <nitin@continuuity.com>
2013-09-03 21:51:50 -0700

    ENG-3233 Added ability to getStreams using rest end point on gateway.

---
+++ gateway/src/main/java/com/continuuity/gateway/v2/handlers/AuthenticatedHttpHandler.java
@@ -0,0 +1,42 @@
+package com.continuuity.gateway.v2.handlers;
+
+import com.continuuity.common.http.core.AbstractHttpHandler;
+import com.continuuity.gateway.auth.GatewayAuthenticator;
+import com.google.inject.Inject;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.continuuity.common.metrics.MetricsHelper.Status.BadRequest;
+import static com.continuuity.common.metrics.MetricsHelper.Status.NotFound;
+
+/**
+ *
+ */
+public abstract class AuthenticatedHttpHandler extends AbstractHttpHandler {
+  private static final Logger LOG = LoggerFactory.getLogger(AuthenticatedHttpHandler.class);
+  private final GatewayAuthenticator authenticator;
+
+  @Inject
+  public AuthenticatedHttpHandler(GatewayAuthenticator authenticator) {
+    this.authenticator = authenticator;
+  }
+
+  protected String getAuthenticatedAccountId(HttpRequest request) throws SecurityException, IllegalArgumentException {
+    // if authentication is enabled, verify an authentication token has been
+    // passed and then verify the token is valid
+    if (!authenticator.authenticateRequest(request)) {
+      LOG.trace("Received an unauthorized request");
+      throw new SecurityException("UnAuthorized access.");
+    }
+
+    String accountId = authenticator.getAccountId(request);
+    if(accountId == null || accountId.isEmpty()) {
+      LOG.trace("No valid account information found");
+      throw new IllegalArgumentException("Not a valid account id found.");
+    }
+    return accountId;
+  }
+
+}
