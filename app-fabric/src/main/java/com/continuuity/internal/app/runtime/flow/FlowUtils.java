194bb90126e302d86d69ba7ed2166b7785fc4f78
Terence Yim <chtyim@gmail.com>
2013-09-06 02:32:41 -0700

    Remove unused method

--- app-fabric/src/main/java/com/continuuity/internal/app/runtime/flow/FlowUtils.java
+++ app-fabric/src/main/java/com/continuuity/internal/app/runtime/flow/FlowUtils.java
@@ -1,31 +1,12 @@
 package com.continuuity.internal.app.runtime.flow;
 
 import com.continuuity.app.program.Program;
-import com.continuuity.app.queue.QueueSpecification;
-import com.continuuity.common.queue.QueueName;
 import com.google.common.hash.Hashing;
 
 /**
  * Set of static helper methods used by flow system.
  */
 public final class FlowUtils {
-
-  /**
-   * Gets the number of consumer groups of a given queue based on the queue specifications.
-   * @param queueSpecs Queue specifications to search for usage of given queue.
-   * @param queueName Name of the queue.
-   * @return Number of consumer groups of the given queue.
-   */
-  public static int getQueueConsumerGroups(Iterable<QueueSpecification> queueSpecs, QueueName queueName) {
-    int numGroups = 0;
-    for (QueueSpecification queueSpec : queueSpecs) {
-      if (queueName.equals(queueSpec.getQueueName())) {
-        numGroups++;
-      }
-    }
-    return numGroups;
-  }
-
 
   /**
    * Generates a queue consumer groupId for the given flowlet in the given program.
