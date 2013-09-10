636371b98caadb15960e7876d40bc86d647a94a6
poorna <poorna@continuuity.com>
2013-09-10 14:23:06 -0700

    Fixing merge issues with DataSetClientTest

--- gateway/src/test/java/com/continuuity/gateway/v2/tools/DataSetClientTest.java
+++ gateway/src/test/java/com/continuuity/gateway/v2/tools/DataSetClientTest.java
@@ -40,4 +40,29 @@ public class DataSetClientTest {
     Assert.assertNull(new DataSetClient().execute(new String[]{
       "read", "--table", table, "--row", row, "--column", column, "--counter"}, configuration));
   }
+
+  @Test
+  public void testClearDataset() throws OperationException {
+    CConfiguration configuration = CConfiguration.create();
+    String port = Integer.toString(GatewayFastTestsSuite.getPort());
+
+    final String table = "tVACLr";
+    final String row = "myRow";
+    final String column = "myCounter";
+    Assert.assertEquals("OK.", new DataSetClient().execute(new String[] {
+      "create", "--table", table, "--host", hostname, "--port", port }, configuration));
+    Assert.assertEquals("OK.", new DataSetClient().execute(new String[] {
+      "write", "--table", table, "--row", row, "--column", column, "--value", "41", "--counter",
+      "--host", hostname, "--port", port }, configuration));
+    Assert.assertEquals("42", new DataSetClient().execute(new String[] {
+      "increment", "--table", table, "--row", row, "--column", column, "--value", "1",
+      "--host", hostname, "--port", port }, configuration));
+
+    configuration.set(Constants.Gateway.ADDRESS, hostname);
+    configuration.set(Constants.Gateway.PORT, port);
+    Assert.assertEquals("OK.", new DataSetClient().execute(new String[]{
+      "clear", "--datasets"}, configuration));
+    Assert.assertNull(new DataSetClient().execute(new String[]{
+      "read", "--table", table, "--row", row, "--column", column, "--counter"}, configuration));
+  }
 }
