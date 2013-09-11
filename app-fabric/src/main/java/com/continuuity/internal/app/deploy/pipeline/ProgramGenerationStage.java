ad7cb8ed0e2415e0570ff283b27e6007156087f7
Terence Yim <terence@continuuity.com>
2013-09-11 15:19:03 -0700

    Added ProgramSpecification and code refactor/cleanup

--- app-fabric/src/main/java/com/continuuity/internal/app/deploy/pipeline/ProgramGenerationStage.java
+++ app-fabric/src/main/java/com/continuuity/internal/app/deploy/pipeline/ProgramGenerationStage.java
@@ -1,9 +1,7 @@
 package com.continuuity.internal.app.deploy.pipeline;
 
 import com.continuuity.api.ApplicationSpecification;
-import com.continuuity.api.batch.MapReduceSpecification;
-import com.continuuity.api.flow.FlowSpecification;
-import com.continuuity.api.procedure.ProcedureSpecification;
+import com.continuuity.api.ProgramSpecification;
 import com.continuuity.app.program.Program;
 import com.continuuity.app.program.Type;
 import com.continuuity.archive.ArchiveBundler;
@@ -14,6 +12,7 @@ import com.continuuity.pipeline.AbstractStage;
 import com.continuuity.weave.filesystem.Location;
 import com.continuuity.weave.filesystem.LocationFactory;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.reflect.TypeToken;
 
 import java.io.IOException;
@@ -49,46 +48,25 @@ public class ProgramGenerationStage extends AbstractStage<ApplicationSpecLocatio
       throw new IOException("Failed to create directory");
     }
 
-    // Now, we iterate through FlowSpecification and generate programs
-    for (FlowSpecification flow : appSpec.getFlows().values()) {
-      String name = String.format(Locale.ENGLISH, "%s/%s", Type.FLOW.toString(), applicationName);
-      Location flowAppDir = newOutputDir.append(name);
-      if (!flowAppDir.exists()) {
-        flowAppDir.mkdirs();
+    // Now, we iterate through all ProgramSpecification and generate programs
+    Iterable<ProgramSpecification> specifications = Iterables.concat(
+      appSpec.getMapReduces().values(),
+      appSpec.getFlows().values(),
+      appSpec.getProcedures().values()
+    );
+
+    for (ProgramSpecification spec: specifications) {
+      Type type = Type.typeOfSpecification(spec);
+      String name = String.format(Locale.ENGLISH, "%s/%s", type, applicationName);
+      Location programDir = newOutputDir.append(name);
+      if (!programDir.exists()) {
+        programDir.mkdirs();
       }
-      Location output = flowAppDir.append(String.format("%s.jar", flow.getName()));
-      Location loc = ProgramBundle.create(o.getApplicationId(), bundler, output, flow.getName(), flow.getClassName(),
-                                         Type.FLOW, appSpec);
+      Location output = programDir.append(String.format("%s.jar", spec.getName()));
+      Location loc = ProgramBundle.create(o.getApplicationId(), bundler, output, spec.getName(), spec.getClassName(),
+                                          type, appSpec);
       programs.add(new Program(loc));
     }
-
-    // Iterate through ProcedureSpecification and generate program
-    for (ProcedureSpecification procedure : appSpec.getProcedures().values()) {
-      String name = String.format(Locale.ENGLISH, "%s/%s", Type.PROCEDURE.toString(), applicationName);
-      Location procedureAppDir = newOutputDir.append(name);
-      if (!procedureAppDir.exists()) {
-        procedureAppDir.mkdirs();
-      }
-      Location output = procedureAppDir.append(String.format("%s.jar", procedure.getName()));
-      Location loc = ProgramBundle.create(o.getApplicationId(), bundler, output, procedure.getName(),
-                                         procedure.getClassName(), Type.PROCEDURE, appSpec);
-      programs.add(new Program(loc));
-    }
-
-    // Iterate through MapReduceSpecification and generate program
-    for (MapReduceSpecification job : appSpec.getMapReduces().values()) {
-      String name = String.format(Locale.ENGLISH, "%s/%s", Type.MAPREDUCE.toString(), applicationName);
-      Location jobAppDir = newOutputDir.append(name);
-      if (!jobAppDir.exists()) {
-        jobAppDir.mkdirs();
-      }
-      Location output = jobAppDir.append(String.format("%s.jar", job.getName()));
-      Location loc = ProgramBundle.create(o.getApplicationId(), bundler, output, job.getName(),
-                                         job.getClassName(), Type.MAPREDUCE, appSpec);
-      programs.add(new Program(loc));
-    }
-
-    // ...
 
     // Emits the received specification with programs.
     emit(new ApplicationWithPrograms(o, programs.build()));
