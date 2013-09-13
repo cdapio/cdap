c431067091d30d772436f9a464c4e293412b1e93
Sree <sree@continuuity.com>
2013-09-13 11:23:05 -0700

    Update workflow spec to not have _index suffix

--- app-fabric/src/api/java/com/continuuity/api/workflow/WorkflowSpecification.java
+++ app-fabric/src/api/java/com/continuuity/api/workflow/WorkflowSpecification.java
@@ -109,10 +109,7 @@ public interface WorkflowSpecification extends SchedulableProgramSpecification {
       MapReduceSpecification mapReduceSpec = new DefaultMapReduceSpecification(mapReduce);
 
       // Rename the map reduce based on the step in the workflow.
-      final String mapReduceName = String.format("%s_%s_%d", name, mapReduceSpec.getName(), actions.size());
-      Preconditions.checkArgument(!mapReduces.containsKey(mapReduceName),
-                                  "MapReduce %s already added for stage %s", mapReduceSpec.getName(), actions.size());
-
+      final String mapReduceName = String.format("%s_%s", name, mapReduceSpec.getName());
       mapReduceSpec = new ForwardingMapReduceSpecification(mapReduceSpec) {
         @Override
         public String getName() {
