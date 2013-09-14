ace313c1903f6b4f2b46892f43c6bd2352d55f1a
Terence Yim <terence@continuuity.com>
2013-09-13 18:22:09 -0700

    Fix gateway tests to use new flow API. Also not to depends on statically compiled file.

---
+++ gateway/src/test/java/com/continuuity/gateway/apps/wordcount/Counter.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2013, Continuuity Inc
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are not permitted
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.continuuity.gateway.apps.wordcount;
+
+import com.continuuity.api.annotation.ProcessInput;
+import com.continuuity.api.annotation.UseDataSet;
+import com.continuuity.api.common.Bytes;
+import com.continuuity.api.data.OperationException;
+import com.continuuity.api.data.dataset.KeyValueTable;
+import com.continuuity.api.flow.flowlet.AbstractFlowlet;
+import com.continuuity.api.flow.flowlet.OutputEmitter;
+
+/**
+ * Counter Flowlet.
+ */
+public class Counter extends AbstractFlowlet {
+  @UseDataSet("wordCounts")
+  private KeyValueTable wordCountsTable;
+  private OutputEmitter<String> wordOutput;
+
+  @ProcessInput("wordOut")
+  public void process(String word) throws OperationException {
+    // Count number of times we have seen this word
+    this.wordCountsTable.increment(Bytes.toBytes(word), 1L);
+
+    // Forward the word to the unique counter flowlet to do the unique count
+    wordOutput.emit(word);
+  }
+}
