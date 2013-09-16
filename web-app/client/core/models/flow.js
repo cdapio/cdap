683823bc7a6448a1e03913db76767c0c0849a4e1
Rohit <nrohits@gmail.com>
2013-09-16 14:33:12 -0700

    workflow stuff

--- web-app/client/core/models/flow.js
+++ web-app/client/core/models/flow.js
@@ -282,8 +282,8 @@ define([], function () {
 				}
 			}
 
-			// Determine if there are any anomolies i.e. nodelevel3 --> nodelevel3 and increment to push
-			// node to column 4.
+			// Determine if there are any anomolies i.e. nodelevel3 --> nodelevel3 and increment to
+			// nodelevel3 --> nodelevel4.
 			for (var i = 0, len = connections.length; i < len; i++) {
 				var conn = connections[i];
 				if (assignments[conn['sourceName']] === assignments[conn['targetName']]) {
