4fbed5defbb1ec64834283e8a0cbaffb92d95d59
poorna <poorna@continuuity.com>
2013-09-19 18:55:42 -0700

    Removing old files.

--- common/src/main/java/com/continuuity/common/zookeeper/election/internal/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
- */
-
-/**
- * Internal classes to handle leader election.
- */
-package com.continuuity.common.zookeeper.election.internal;
