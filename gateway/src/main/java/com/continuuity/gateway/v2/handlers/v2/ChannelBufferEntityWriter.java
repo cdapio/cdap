d9b8c7929738935478189500b96e988b6afc85ae
Terence Yim <terence@continuuity.com>
2013-09-20 23:53:46 -0700

    Refactor gateway ProcedureHandler, minimize copying as much as possible.

---
+++ gateway/src/main/java/com/continuuity/gateway/v2/handlers/v2/ChannelBufferEntityWriter.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
+ */
+package com.continuuity.gateway.v2.handlers.v2;
+
+import com.ning.http.client.Request;
+import org.jboss.netty.buffer.ChannelBuffer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ *
+ */
+public final class ChannelBufferEntityWriter implements Request.EntityWriter {
+
+  private final ChannelBuffer buffer;
+
+  public ChannelBufferEntityWriter(ChannelBuffer buffer) {
+    this.buffer = buffer;
+  }
+
+  @Override
+  public void writeEntity(OutputStream out) throws IOException {
+    buffer.readBytes(out, buffer.readableBytes());
+  }
+}
