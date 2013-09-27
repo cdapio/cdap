ad37addc0a49905889827a886d1f108a80bd248c
poorna <poorna@continuuity.com>
2013-09-27 13:55:43 -0700

    Addressing review comments - 1. postCall will not happen if method call is not made. 2. postCalls will continue getting called even if one postCall throws exception. 3. Passing HandlerInfo to pre and post calls instead of Method. 4. Misc changes.

--- common/src/main/java/com/continuuity/common/http/core/AbstractHandlerHook.java
+++ common/src/main/java/com/continuuity/common/http/core/AbstractHandlerHook.java
@@ -3,22 +3,20 @@ package com.continuuity.common.http.core;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
-import java.lang.reflect.Method;
-
 /**
  * A base implementation of {@link HandlerHook} that provides no-op for both
- * {@link #preCall(org.jboss.netty.handler.codec.http.HttpRequest, HttpResponder, java.lang.reflect.Method)}
- * and {@link #postCall(org.jboss.netty.handler.codec.http.HttpRequest,
- * org.jboss.netty.handler.codec.http.HttpResponseStatus, java.lang.reflect.Method)} methods.
+ * {@link HandlerHook#preCall(org.jboss.netty.handler.codec.http.HttpRequest, HttpResponder, HandlerInfo)}
+ * and {@link HandlerHook#postCall(org.jboss.netty.handler.codec.http.HttpRequest,
+ * org.jboss.netty.handler.codec.http.HttpResponseStatus, HandlerInfo)} methods.
  */
-public class AbstractHandlerHook implements HandlerHook {
+public abstract class AbstractHandlerHook implements HandlerHook {
   @Override
-  public boolean preCall(HttpRequest request, HttpResponder responder, Method method) {
+  public boolean preCall(HttpRequest request, HttpResponder responder, HandlerInfo handlerInfo) {
     return true;
   }
 
   @Override
-  public void postCall(HttpRequest request, HttpResponseStatus status, Method method) {
+  public void postCall(HttpRequest request, HttpResponseStatus status, HandlerInfo handlerInfo) {
     // no-op
   }
 }
