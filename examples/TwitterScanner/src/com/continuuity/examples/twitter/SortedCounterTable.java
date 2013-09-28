2ac339f6d74d6fd971922b8a5832ff4ae7595356
Terence Yim <chtyim@gmail.com>
2013-09-27 22:46:47 -0700

    Fix API.

--- examples/TwitterScanner/src/com/continuuity/examples/twitter/SortedCounterTable.java
+++ examples/TwitterScanner/src/com/continuuity/examples/twitter/SortedCounterTable.java
@@ -47,7 +47,7 @@ import java.util.TreeSet;
  */
 public class SortedCounterTable extends DataSet {
 
-  private final SortedCounterConfig config;
+  private SortedCounterConfig config;
   private Table counters;
 
   public SortedCounterTable(String name, SortedCounterConfig config) {
@@ -56,18 +56,17 @@ public class SortedCounterTable extends DataSet {
     this.config = config;
   }
 
-  public SortedCounterTable(DataSetSpecification spec) {
-    super(spec);
-    this.config = new SortedCounterConfig(spec.getProperty("config"));
-    this.counters = new Table(spec.getSpecificationFor("sct_" + this.getName()));
-  }
-
   @Override
   public DataSetSpecification configure() {
     return new DataSetSpecification.Builder(this)
       .property("config", this.config.toString())
-      .dataset(this.counters.configure())
       .create();
+  }
+
+  @Override
+  public void initialize(DataSetSpecification spec) {
+    super.initialize(spec);
+    this.config = new SortedCounterConfig(spec.getProperty("config"));
   }
 
   /**
