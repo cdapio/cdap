002de22f184f52f8c07d80dd1be4cd38c329041c
Alex Baranau <alexb@continuuity.com>
2013-10-17 18:03:34 -0700

    finishing merge with cp-dequeue

--- data-fabric/src/test/java/com/continuuity/data2/transaction/queue/QueueTest.java
+++ data-fabric/src/test/java/com/continuuity/data2/transaction/queue/QueueTest.java
@@ -371,22 +371,12 @@ public abstract class QueueTest {
 
   @Test
   public void testOneFIFOEnqueueDequeue() throws Exception {
-    QueueName queueName = QueueName.fromFlowlet("flow", "flowlet", "queue1");
-    Queue2Producer producer = queueClientFactory.createProducer(queueName);
-    TransactionContext txContext = createTxContext(producer);
-    txContext.start();
-    producer.enqueue(new QueueEntry(Bytes.toBytes(55)));
-    txContext.finish();
+    testOneEnqueueDequeue(DequeueStrategy.FIFO);
+  }
 
-    Queue2Consumer consumer1 = queueClientFactory.createConsumer(
-      queueName, new ConsumerConfig(0, 0, 1, DequeueStrategy.FIFO, null), 2);
-
-    txContext = createTxContext(consumer1);
-    txContext.start();
-    Assert.assertEquals(55, Bytes.toInt(consumer1.dequeue().iterator().next()));
-    txContext.finish();
-
-    verifyQueueIsEmpty(queueName, 1);
+  @Test
+  public void testOneRoundRobinEnqueueDequeue() throws Exception {
+    testOneEnqueueDequeue(DequeueStrategy.ROUND_ROBIN);
   }
 
   @Test
