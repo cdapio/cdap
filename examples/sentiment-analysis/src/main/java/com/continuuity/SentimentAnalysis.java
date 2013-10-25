24b532db19911ed51c1865180f6c7e034820258c
Nitin Motgi <nitin@continuuity.com>
2013-10-24 17:38:31 -0700

    wip

--- examples/sentiment-analysis/src/main/java/com/continuuity/SentimentAnalysis.java
+++ examples/sentiment-analysis/src/main/java/com/continuuity/SentimentAnalysis.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2013, Continuuity Inc
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are not permitted
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.continuuity;
 
 import com.continuuity.api.Application;
@@ -121,7 +138,7 @@ public class SentimentAnalysis implements Application {
      */
     @Override
     protected ExternalProgram init(FlowletContext context) {
-      URL file = this.getClass().getClassLoader().getResource("sentiment/score-sentence");
+      URL file = this.getClass().getResource("/sentiment/score-sentence");
       if (file != null) {
         File bash = new File("/bin/bash");
         if (bash.exists()) {
