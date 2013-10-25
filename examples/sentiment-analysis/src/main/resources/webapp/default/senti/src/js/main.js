5707f7dcf4c532d7ab2dd86d713d572ad7e08cd6
Rohit <nrohits@gmail.com>
2013-10-25 16:06:51 -0700

    fe changes

--- examples/sentiment-analysis/src/main/resources/webapp/default/senti/src/js/main.js
+++ examples/sentiment-analysis/src/main/resources/webapp/default/senti/src/js/main.js
@@ -1,1 +1,35 @@
-alert(1);
+/**
+ * Js for main page.
+ */
+
+var HOST = 'http://127.0.0.1:20000';
+var Homepage = function () {
+  this.init(arguments);
+};
+
+Homepage.prototype.init  = function () {
+  this.enableIntervals();
+};
+
+Homepage.prototype.enableIntervals = function () {
+  var self = this;
+  this.interval = setInterval(function() {
+    $.ajax({
+      url: HOST + '/v2/apps/sentiment/procedures/sentiment-query/methods/aggregates',
+      type: 'POST',
+      data: JSON.stringify({data: {}}),
+      contentType:"application/json; charset=utf-8",
+      dataType: 'json',
+      success: function(data) {
+        console.log(data)
+      }
+    });
+
+  }, 1000);
+};
+
+
+$(document).ready(function() {
+  new Homepage();
+});
+
