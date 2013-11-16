a751b6f5a9c8ae4f1b8a6cfde2038a4344cc47fb
poorna <poorna@continuuity.com>
2013-11-15 17:38:33 -0800

    Moving Gateway handlers to appropriate source roots to remove circular dependency.

---
+++ app-fabric/src/main/java/com/continuuity/gateway/handlers/procedure/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
+ */
+
+/**
+ *  Gateway handlers for procedure.
+ */
+package com.continuuity.gateway.handlers.procedure;
+
--- gateway/src/main/java/com/continuuity/gateway/runtime/GatewayModules.java
+++ gateway/src/main/java/com/continuuity/gateway/runtime/GatewayModules.java
@@ -2,21 +2,14 @@ package com.continuuity.gateway.runtime;
 
 import com.continuuity.common.conf.CConfiguration;
 import com.continuuity.common.conf.Constants;
-import com.continuuity.common.http.core.HttpHandler;
 import com.continuuity.common.runtime.RuntimeModule;
 import com.continuuity.common.utils.Networks;
 import com.continuuity.gateway.auth.GatewayAuthenticator;
 import com.continuuity.gateway.auth.NoAuthenticator;
 import com.continuuity.gateway.auth.PassportVPCAuthenticator;
-import com.continuuity.gateway.handlers.v2.AppFabricServiceHandler;
-import com.continuuity.gateway.handlers.v2.PingHandler;
-import com.continuuity.gateway.handlers.v2.ProcedureHandler;
-import com.continuuity.gateway.handlers.v2.WorkflowHandler;
-import com.continuuity.gateway.handlers.v2.dataset.ClearFabricHandler;
-import com.continuuity.gateway.handlers.v2.dataset.DatasetHandler;
-import com.continuuity.gateway.handlers.v2.dataset.TableHandler;
-import com.continuuity.gateway.handlers.v2.log.LogHandler;
-import com.continuuity.gateway.handlers.v2.stream.StreamHandler;
+import com.continuuity.gateway.handlers.AppFabricGatewayModules;
+import com.continuuity.gateway.handlers.GatewayHandlerModules;
+import com.continuuity.logging.gateway.handlers.LogHandlerModules;
 import com.continuuity.metrics.guice.MetricsQueryModule;
 import com.continuuity.passport.PassportConstants;
 import com.continuuity.passport.http.client.PassportClient;
@@ -24,9 +17,7 @@ import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.Provides;
-import com.google.inject.Scopes;
 import com.google.inject.Singleton;
-import com.google.inject.multibindings.Multibinder;
 import com.google.inject.name.Named;
 
 import javax.annotation.Nullable;
@@ -57,16 +48,9 @@ public class GatewayModules extends RuntimeModule {
     return new AbstractModule() {
       @Override
       protected void configure() {
-        Multibinder<HttpHandler> handlerBinder = Multibinder.newSetBinder(binder(), HttpHandler.class);
-        handlerBinder.addBinding().to(StreamHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(PingHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(AppFabricServiceHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(LogHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(ProcedureHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(WorkflowHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(TableHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(DatasetHandler.class).in(Scopes.SINGLETON);
-        handlerBinder.addBinding().to(ClearFabricHandler.class).in(Scopes.SINGLETON);
+        install(new GatewayHandlerModules());
+        install(new AppFabricGatewayModules());
+        install(new LogHandlerModules());
         install(new MetricsQueryModule());
       }
 
