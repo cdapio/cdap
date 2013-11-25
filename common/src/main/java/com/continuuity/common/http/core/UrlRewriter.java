38e0c96b1e94c872f675f294cc0fd818d0efd128
poorna <poorna@continuuity.com>
2013-11-25 14:10:26 -0800

    [ENG-3704] Review comments.

--- common/src/main/java/com/continuuity/common/http/core/UrlRewriter.java
+++ common/src/main/java/com/continuuity/common/http/core/UrlRewriter.java
@@ -4,11 +4,14 @@ import org.jboss.netty.handler.codec.http.HttpRequest;
 
 /**
  * Re-writes URL of an incoming request before any handlers or their hooks are called.
+ * This can be used to map an incoming URL to an URL that a handler understands. The re-writer overwrites the incoming
+ * URL with the new value.
  */
 public interface UrlRewriter {
   /**
    * Implement this to rewrite URL of an incoming request. The re-written URL needs to be updated back in
    * {@code request} using {@link HttpRequest#setUri(String)}.
+   *
    * @param request Incoming HTTP request.
    */
   void rewrite(HttpRequest request);
