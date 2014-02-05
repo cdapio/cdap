ed95333010330ba10445b2c0898c2e7c59f0347e
Don Mosites <don@continuuity.com>
2014-02-05 13:24:56 -0800

    Custom stylesheet _static/style.css

--- docs/loom-docs/source/conf.py
+++ docs/loom-docs/source/conf.py
@@ -106,6 +106,8 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'nature'
 
+html_style = 'style.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
