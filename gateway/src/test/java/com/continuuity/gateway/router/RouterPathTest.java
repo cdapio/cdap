c5b538aac7c6c250afd411f9b8804728a4487d36
Gokul Gunasekaran <gokul@Gokuls-MacBook-Pro.local>
2014-04-22 15:22:40 -0700

    RouterPathTest change to account for getRoutingPath method param change

--- gateway/src/test/java/com/continuuity/gateway/router/RouterPathTest.java
+++ gateway/src/test/java/com/continuuity/gateway/router/RouterPathTest.java
@@ -2,6 +2,10 @@ package com.continuuity.gateway.router;
 
 import com.continuuity.common.conf.Constants;
 import com.continuuity.gateway.GatewayFastTestsSuite;
+import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,7 +17,6 @@ public class RouterPathTest {
 
   private static RouterPathLookup pathLookup;
   private final String VERSION = "HTTP/1.1";
-  private final String APIKEY = "undefined";
 
   @BeforeClass
   public static void init() throws Exception {
@@ -23,35 +26,40 @@ public class RouterPathTest {
   @Test
   public void testRouterFlowPathLookUp() throws Exception {
     String flowPath = "/v2//apps/ResponseCodeAnalytics/flows/LogAnalyticsFlow/status";
-    String result = pathLookup.getRoutingPath(flowPath, "GET", APIKEY, VERSION);
+    HttpRequest httpRequest = new DefaultHttpRequest(new HttpVersion(VERSION), new HttpMethod("GET"), flowPath);
+    String result = pathLookup.getRoutingPath(flowPath, httpRequest);
     Assert.assertEquals(Constants.Service.APP_FABRIC_HTTP, result);
   }
 
   @Test
   public void testRouterWorkFlowPathLookUp() throws Exception {
     String procPath = "/v2/apps/PurchaseHistory/workflows/PurchaseHistoryWorkflow/status";
-    String result = pathLookup.getRoutingPath(procPath, "GET", APIKEY, VERSION);
+    HttpRequest httpRequest = new DefaultHttpRequest(new HttpVersion(VERSION), new HttpMethod("GET"), procPath);
+    String result = pathLookup.getRoutingPath(procPath, httpRequest);
     Assert.assertEquals(Constants.Service.APP_FABRIC_HTTP,  result);
   }
 
   @Test
   public void testRouterProcedurePathLookUp() throws Exception {
     String procPath = "/v2//apps/ResponseCodeAnalytics/procedures/StatusCodeProcedure/status";
-    String result = pathLookup.getRoutingPath(procPath, "GET", APIKEY, VERSION);
+    HttpRequest httpRequest = new DefaultHttpRequest(new HttpVersion(VERSION), new HttpMethod("GET"), procPath);
+    String result = pathLookup.getRoutingPath(procPath, httpRequest);
     Assert.assertEquals(Constants.Service.APP_FABRIC_HTTP,  result);
   }
 
   @Test
   public void testRouterDeployPathLookUp() throws Exception {
     String procPath = "/v2//apps/";
-    String result = pathLookup.getRoutingPath(procPath, "PUT", APIKEY, VERSION);
+    HttpRequest httpRequest = new DefaultHttpRequest(new HttpVersion(VERSION), new HttpMethod("GET"), procPath);
+    String result = pathLookup.getRoutingPath(procPath, httpRequest);
     Assert.assertEquals(Constants.Service.APP_FABRIC_HTTP,  result);
   }
 
   @Test
   public void testRouterFlowletInstancesLookUp() throws Exception {
     String procPath = "/v2//apps/WordCount/flows/WordCountFlow/flowlets/StreamSource/instances";
-    String result = pathLookup.getRoutingPath(procPath, "PUT", APIKEY, VERSION);
+    HttpRequest httpRequest = new DefaultHttpRequest(new HttpVersion(VERSION), new HttpMethod("GET"), procPath);
+    String result = pathLookup.getRoutingPath(procPath, httpRequest);
     Assert.assertEquals(Constants.Service.APP_FABRIC_HTTP,  result);
   }
 
