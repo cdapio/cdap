48cffdd922961cd62a16abb6664fe59e41f8f7e7
Parth Gandhi <parthgandhi@outlook.com>
2014-05-23 17:50:49 -0700

    Explicitly return an injector

--- security/src/main/java/com/continuuity/security/server/AuthenticationGuiceServletContextListener.java
+++ security/src/main/java/com/continuuity/security/server/AuthenticationGuiceServletContextListener.java
@@ -1,5 +1,10 @@
 package com.continuuity.security.server;
 
+import com.continuuity.common.guice.ConfigModule;
+import com.continuuity.common.guice.DiscoveryRuntimeModule;
+import com.continuuity.common.guice.IOModule;
+import com.continuuity.security.guice.SecurityModules;
+import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
@@ -17,6 +22,8 @@ public class AuthenticationGuiceServletContextListener extends GuiceServletConte
 
   @Override
   protected Injector getInjector() {
-    return injector;
+    return Guice.createInjector(new IOModule(), new ConfigModule(),
+                         new DiscoveryRuntimeModule().getSingleNodeModules(),
+                         new SecurityModules().getSingleNodeModules());
   }
 }
