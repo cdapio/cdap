1608f08a950a41f2633a256a3cc847fb936eb8de
Alex Baranau <alex.baranov.v@gmail.com>
2014-05-27 21:01:57 -0700

    delete dataset type and instances metadata

--- data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/service/DatasetInstanceHandler.java
+++ data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/service/DatasetInstanceHandler.java
@@ -60,6 +60,13 @@ public class DatasetInstanceHandler extends AbstractHttpHandler {
     responder.sendJson(HttpResponseStatus.OK, instanceManager.getAll());
   }
 
+  @DELETE
+  @Path("/data/instances/")
+  public void deleteAll(HttpRequest request, final HttpResponder responder) {
+    instanceManager.deleteAll();
+    responder.sendStatus(HttpResponseStatus.OK);
+  }
+
   @GET
   @Path("/data/instances/{instance-name}")
   public void getInfo(HttpRequest request, final HttpResponder responder,
