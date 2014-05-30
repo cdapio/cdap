631ef44fbed5e374332b56d1ca7614f94cbb6933
Alex Baranau <alex.baranov.v@gmail.com>
2014-05-29 17:36:39 -0700

    new App API: Application vs AbstractApplication

--- app-fabric/src/api/java/com/continuuity/api/app/AbstractApplication.java
+++ app-fabric/src/api/java/com/continuuity/api/app/AbstractApplication.java
@@ -1,7 +1,6 @@
 package com.continuuity.api.app;
 
 import com.continuuity.api.data.DataSet;
-import com.continuuity.api.data.DatasetInstanceCreationSpec;
 import com.continuuity.api.data.stream.Stream;
 import com.continuuity.api.flow.Flow;
 import com.continuuity.api.mapreduce.MapReduce;
@@ -9,36 +8,14 @@ import com.continuuity.api.procedure.Procedure;
 import com.continuuity.api.workflow.Workflow;
 import com.continuuity.internal.data.dataset.DatasetInstanceProperties;
 import com.continuuity.internal.data.dataset.module.DatasetModule;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * A support class for {@link Application}s which reduces repetition and results in
  * a more readable configuration. Simply implement {@link #configure()} to define your application.
  */
 public abstract class AbstractApplication implements Application {
-  private String name;
-  private String description;
-  private final List<Stream> streams = Lists.newArrayList();
-  private final List<DataSet> dataSets = Lists.newArrayList();
-  private final Map<String, Class<? extends DatasetModule>> datasetModules = Maps.newHashMap();
-  private final List<DatasetInstanceCreationSpec> datasetInstances = Lists.newArrayList();
-  private final List<Flow> flows = Lists.newArrayList();
-  private final Map<Procedure, Integer> procedures = Maps.newHashMap();
-  private final List<MapReduce> mapReduces = Lists.newArrayList();
-  private final List<Workflow> workflows = Lists.newArrayList();
-
   private ApplicationContext context;
-
-  public AbstractApplication() {
-    // name defaults to app class name
-    this.name = this.getClass().getSimpleName();
-    this.description = "";
-  }
+  private ApplicationConfigurer configurer;
 
   /**
    * Override this method to configure the application.
@@ -48,43 +25,9 @@ public abstract class AbstractApplication implements Application {
   @Override
   public final void configure(ApplicationConfigurer configurer, ApplicationContext context) {
     this.context = context;
+    this.configurer = configurer;
 
     configure();
-
-    configurer.setName(name);
-    configurer.setDescription(description);
-
-    for (Stream stream : streams) {
-      configurer.addStream(stream);
-    }
-
-    for (DataSet dataSet : dataSets) {
-      configurer.addDataSet(dataSet);
-    }
-
-    for (Map.Entry<String, Class<? extends DatasetModule>> module : datasetModules.entrySet()) {
-      configurer.addDatasetModule(module.getKey(), module.getValue());
-    }
-
-    for (DatasetInstanceCreationSpec instance : datasetInstances) {
-      configurer.createDataSet(instance.getInstanceName(), instance.getTypeName(), instance.getProperties());
-    }
-
-    for (Flow flow : flows) {
-      configurer.addFlow(flow);
-    }
-
-    for (Map.Entry<Procedure, Integer> procedure : procedures.entrySet()) {
-      configurer.addProcedure(procedure.getKey(), procedure.getValue());
-    }
-
-    for (MapReduce mapReduce : mapReduces) {
-      configurer.addMapReduce(mapReduce);
-    }
-
-    for (Workflow workflow : workflows) {
-      configurer.addWorkflow(workflow);
-    }
   }
 
   protected ApplicationContext getApplicationContext() {
@@ -92,67 +35,51 @@ public abstract class AbstractApplication implements Application {
   }
 
   protected void setName(String name) {
-    this.name = name;
+    configurer.setName(name);
   }
 
   protected void setDescription(String description) {
-    this.description = description;
+    configurer.setDescription(description);
   }
 
   protected void addStream(Stream stream) {
-    Preconditions.checkArgument(stream != null, "Stream cannot be null.");
-    streams.add(stream);
+    configurer.addStream(stream);
   }
 
   @Deprecated
   protected void addDataSet(DataSet dataSet) {
-    Preconditions.checkArgument(dataSet != null, "DataSet cannot be null.");
-    dataSets.add(dataSet);
+    configurer.addDataSet(dataSet);
   }
 
   protected void addDatasetModule(String moduleName, Class<? extends DatasetModule> moduleClass) {
-    Preconditions.checkArgument(moduleName != null, "Dataset module name cannot be null.");
-    Preconditions.checkArgument(moduleClass != null, "Dataset module class cannot be null.");
-    datasetModules.put(moduleName, moduleClass);
+    configurer.addDatasetModule(moduleName, moduleClass);
   }
 
   protected void createDataSet(String datasetInstanceName, String typeName, DatasetInstanceProperties properties) {
-    Preconditions.checkArgument(datasetInstanceName != null, "Dataset instance name cannot be null.");
-    Preconditions.checkArgument(typeName != null, "Dataset type name cannot be null.");
-    Preconditions.checkArgument(properties != null, "Instance properties name cannot be null.");
-    datasetInstances.add(new DatasetInstanceCreationSpec(datasetInstanceName, typeName, properties));
+    configurer.createDataSet(datasetInstanceName, typeName, properties);
   }
 
   protected void createDataSet(String datasetInstanceName, String typeName) {
-    Preconditions.checkArgument(datasetInstanceName != null, "Dataset instance name cannot be null.");
-    Preconditions.checkArgument(typeName != null, "Dataset type name cannot be null.");
-    datasetInstances.add(new DatasetInstanceCreationSpec(datasetInstanceName, typeName,
-                                                         DatasetInstanceProperties.EMPTY));
+    configurer.createDataSet(datasetInstanceName, typeName, DatasetInstanceProperties.EMPTY);
   }
 
   protected void addFlow(Flow flow) {
-    Preconditions.checkArgument(flow != null, "Flow cannot be null.");
-    flows.add(flow);
+    configurer.addFlow(flow);
   }
 
   protected void addProcedure(Procedure procedure) {
-    Preconditions.checkArgument(procedure != null, "Procedure cannot be null.");
-    procedures.put(procedure, 1);
+    configurer.addProcedure(procedure);
   }
 
-  protected void addProcedure(Procedure procedure, int instance) {
-    Preconditions.checkArgument(procedure != null, "Procedure cannot be null.");
-    Preconditions.checkArgument(instance >= 1, "Number of instances can't be less than 1");
-    procedures.put(procedure, instance);
+  protected void addProcedure(Procedure procedure, int instances) {
+    configurer.addProcedure(procedure, instances);
   }
 
   protected void addMapReduce(MapReduce mapReduce) {
-    Preconditions.checkArgument(mapReduce != null, "MapReduce cannot be null.");
-    mapReduces.add(mapReduce);
+    configurer.addMapReduce(mapReduce);
   }
 
   protected void addWorkflow(Workflow workflow) {
-    Preconditions.checkArgument(workflow != null, "Workflow cannot be null.");
-    workflows.add(workflow);
+    configurer.addWorkflow(workflow);
   }
 }
