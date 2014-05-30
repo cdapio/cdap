cbbcfc7124d93cd52408f53df08e3d47434a7196
Gokul Gunasekaran <gokul@Gokuls-MacBook-Pro.local>
2014-05-29 19:01:47 -0700

    Adding unit test for ServiceLoggingContext

--- watchdog/src/test/java/com/continuuity/logging/save/LogSaverTest.java
+++ watchdog/src/test/java/com/continuuity/logging/save/LogSaverTest.java
@@ -3,9 +3,11 @@ package com.continuuity.logging.save;
 import com.continuuity.common.conf.CConfiguration;
 import com.continuuity.common.logging.AccountLoggingContext;
 import com.continuuity.common.logging.ApplicationLoggingContext;
+import com.continuuity.common.logging.ComponentLoggingContext;
 import com.continuuity.common.logging.LoggingContext;
 import com.continuuity.common.logging.LoggingContextAccessor;
 import com.continuuity.common.logging.ServiceLoggingContext;
+import com.continuuity.common.logging.SystemLoggingContext;
 import com.continuuity.data.InMemoryDataSetAccessor;
 import com.continuuity.data2.transaction.inmemory.InMemoryTransactionManager;
 import com.continuuity.data2.transaction.inmemory.InMemoryTxSystemClient;
@@ -121,7 +123,7 @@ public class LogSaverTest extends KafkaTestBase {
 
     waitTillLogSaverDone(logBaseDir, "ACCT_1/APP_1/flow-FLOW_1/%s", "Test log message 59 arg1 arg2");
     waitTillLogSaverDone(logBaseDir, "ACCT_2/APP_2/flow-FLOW_2/%s", "Test log message 59 arg1 arg2");
-    waitTillLogSaverDone(logBaseDir, "reactor/services/service-metrics", "Test log message 59 arg1 arg2");
+    waitTillLogSaverDone(logBaseDir, "reactor/services/service-metrics/%s", "Test log message 59 arg1 arg2");
 
     logSaver.stopAndWait();
     multiElection.stopAndWait();
@@ -134,7 +136,7 @@ public class LogSaverTest extends KafkaTestBase {
     CheckpointManager checkpointManager = new CheckpointManager(LogSaver.getMetaTable(dataSetAccessor),
                                                                 txClient, KafkaTopic.getTopic());
     Assert.assertEquals(60, checkpointManager.getCheckpoint(0));
-    Assert.assertEquals(60, checkpointManager.getCheckpoint(1));
+    Assert.assertEquals(120, checkpointManager.getCheckpoint(1));
   }
 
   @Test
@@ -149,7 +151,7 @@ public class LogSaverTest extends KafkaTestBase {
 
   @Test
   public void testLogRead3() throws Exception {
-    testLogRead(new ServiceLoggingContext("reactor", "service", "metrics"));
+    testLogRead(new ServiceLoggingContext("reactor", "services", "metrics"));
   }
 
   private void testLogRead(LoggingContext loggingContext) throws Exception {
@@ -169,16 +171,27 @@ public class LogSaverTest extends KafkaTestBase {
     for (int i = 0; i < 60; ++i) {
       Assert.assertEquals(String.format("Test log message %d arg1 arg2", i),
                           allEvents.get(i).getLoggingEvent().getFormattedMessage());
-
-      Assert.assertEquals(
-        loggingContext.getSystemTagsMap().get(AccountLoggingContext.TAG_ACCOUNT_ID).getValue(),
-        allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(AccountLoggingContext.TAG_ACCOUNT_ID));
-      Assert.assertEquals(
-        loggingContext.getSystemTagsMap().get(ApplicationLoggingContext.TAG_APPLICATION_ID).getValue(),
-        allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(ApplicationLoggingContext.TAG_APPLICATION_ID));
-      Assert.assertEquals(
-        loggingContext.getSystemTagsMap().get(FlowletLoggingContext.TAG_FLOW_ID).getValue(),
-        allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(FlowletLoggingContext.TAG_FLOW_ID));
+      if (loggingContext instanceof ServiceLoggingContext) {
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(SystemLoggingContext.TAG_SYSTEM_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(SystemLoggingContext.TAG_SYSTEM_ID));
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(ComponentLoggingContext.TAG_COMPONENT_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(ComponentLoggingContext.TAG_COMPONENT_ID));
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(ServiceLoggingContext.TAG_SERVICE_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(ServiceLoggingContext.TAG_SERVICE_ID));
+      } else {
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(AccountLoggingContext.TAG_ACCOUNT_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(AccountLoggingContext.TAG_ACCOUNT_ID));
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(ApplicationLoggingContext.TAG_APPLICATION_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(ApplicationLoggingContext.TAG_APPLICATION_ID));
+        Assert.assertEquals(
+          loggingContext.getSystemTagsMap().get(FlowletLoggingContext.TAG_FLOW_ID).getValue(),
+          allEvents.get(i).getLoggingEvent().getMDCPropertyMap().get(FlowletLoggingContext.TAG_FLOW_ID));
+      }
     }
 
     LogCallback logCallback2 = new LogCallback();
