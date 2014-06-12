8f62f6707f6de0419afdaa823580b1c658df0697
Alex Baranau <alex.baranov.v@gmail.com>
2014-06-11 19:17:45 -0700

    fix unit-test

--- data-fabric/src/test/java/com/continuuity/data2/dataset2/AbstractDatasetFrameworkTest.java
+++ data-fabric/src/test/java/com/continuuity/data2/dataset2/AbstractDatasetFrameworkTest.java
@@ -1,8 +1,6 @@
 package com.continuuity.data2.dataset2;
 
 import com.continuuity.api.common.Bytes;
-import com.continuuity.data2.dataset2.lib.table.CoreDatasetsModule;
-import com.continuuity.data2.dataset2.lib.table.KeyValueTable;
 import com.continuuity.data2.dataset2.module.lib.TableModule;
 import com.continuuity.data2.dataset2.module.lib.inmemory.InMemoryOrderedTableModule;
 import com.continuuity.data2.transaction.DefaultTransactionExecutor;
@@ -12,19 +10,13 @@ import com.continuuity.data2.transaction.inmemory.MinimalTxSystemClient;
 import com.continuuity.internal.data.dataset.DatasetAdmin;
 import com.continuuity.internal.data.dataset.DatasetProperties;
 import com.continuuity.internal.data.dataset.lib.table.OrderedTable;
-import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public abstract class AbstractDatasetFrameworkTest {
-
-  private static final Logger LOG = LoggerFactory.getLogger(AbstractDatasetFrameworkTest.class);
-  private static final Gson GSON = new Gson();
 
   protected abstract DatasetFramework getFramework();
 
@@ -68,25 +60,6 @@ public abstract class AbstractDatasetFrameworkTest {
 
     // cleanup
     framework.deleteInstance("my_table");
-    framework.deleteModule("inMemory");
-  }
-
-  @Test
-  public void testCompositeDatasetWithModule() throws Exception {
-    // Configuring Dataset types
-    DatasetFramework framework = getFramework();
-
-    framework.addModule("inMemory", new InMemoryOrderedTableModule());
-    framework.addModule("default", new CoreDatasetsModule());
-
-    // Creating instance
-    framework.addInstance("keyValueTable", "my_table", DatasetProperties.EMPTY);
-
-    testCompositeDataset(framework);
-
-    // cleanup
-    framework.deleteInstance("my_table");
-    framework.deleteModule("default");
     framework.deleteModule("inMemory");
   }
 
@@ -147,20 +120,20 @@ public abstract class AbstractDatasetFrameworkTest {
     txnl.execute(new TransactionExecutor.Subroutine() {
       @Override
       public void apply() throws Exception {
-        table.write("key1", "value1");
+        table.put("key1", "value1");
       }
     });
     txnl.execute(new TransactionExecutor.Subroutine() {
       @Override
       public void apply() throws Exception {
-        Assert.assertEquals("value1", Bytes.toString(table.read("key1")));
+        Assert.assertEquals("value1", table.get("key1"));
       }
     });
     admin.truncate();
     txnl.execute(new TransactionExecutor.Subroutine() {
       @Override
       public void apply() throws Exception {
-        Assert.assertEquals(null, Bytes.toString(table.read("key1")));
+        Assert.assertEquals(null, table.get("key1"));
       }
     });
   }
