91da740791517dc11c9c9151c096d7f8955d948f
Alex Baranau <alex.baranov.v@gmail.com>
2014-06-20 14:50:32 -0700

    default orderedTable fix

--- data-fabric/src/main/java/com/continuuity/data2/dataset2/module/lib/leveldb/LevelDBOrderedTableModule.java
+++ data-fabric/src/main/java/com/continuuity/data2/dataset2/module/lib/leveldb/LevelDBOrderedTableModule.java
@@ -2,6 +2,7 @@ package com.continuuity.data2.dataset2.module.lib.leveldb;
 
 import com.continuuity.api.dataset.module.DatasetDefinitionRegistry;
 import com.continuuity.api.dataset.module.DatasetModule;
+import com.continuuity.api.dataset.table.OrderedTable;
 import com.continuuity.data2.dataset2.lib.table.leveldb.LevelDBOrderedTable;
 import com.continuuity.data2.dataset2.lib.table.leveldb.LevelDBOrderedTableDefinition;
 
@@ -13,6 +14,6 @@ public class LevelDBOrderedTableModule implements DatasetModule {
   public void register(DatasetDefinitionRegistry registry) {
     registry.add(new LevelDBOrderedTableDefinition("orderedTable"));
     // so that it can be resolved via @Dataset
-    registry.add(new LevelDBOrderedTableDefinition(LevelDBOrderedTable.class.getName()));
+    registry.add(new LevelDBOrderedTableDefinition(OrderedTable.class.getName()));
   }
 }
