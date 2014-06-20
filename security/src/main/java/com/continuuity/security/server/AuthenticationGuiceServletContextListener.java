61dadb22e7f8e56ae57092053f297092e2c84e92
Parth Gandhi <parth@continuuity.com>
2014-06-19 22:21:19 -0700

    Remove unnecessary modules

--- security/src/main/java/com/continuuity/security/server/AuthenticationGuiceServletContextListener.java
+++ security/src/main/java/com/continuuity/security/server/AuthenticationGuiceServletContextListener.java
@@ -1,10 +1,6 @@
 package com.continuuity.security.server;
 
 import com.continuuity.common.conf.CConfiguration;
-import com.continuuity.common.guice.ConfigModule;
-import com.continuuity.common.guice.DiscoveryRuntimeModule;
-import com.continuuity.common.guice.IOModule;
-import com.continuuity.security.guice.SecurityModules;
 import com.google.common.collect.Lists;
 import com.google.inject.Module;
 import org.eclipse.jetty.server.Handler;
@@ -28,10 +24,7 @@ public class AuthenticationGuiceServletContextListener extends GuiceResteasyBoot
 
   @Override
   protected List<? extends Module> getModules(ServletContext context) {
-    return Lists.newArrayList(new IOModule(), new ConfigModule(),
-                              new DiscoveryRuntimeModule().getSingleNodeModules(),
-                              new SecurityModules().getSingleNodeModules(),
-                              new SecurityHandlerModule(handlerMap, configuration));
+    return Lists.newArrayList(new SecurityHandlerModule(handlerMap, configuration));
   }
 
 }
