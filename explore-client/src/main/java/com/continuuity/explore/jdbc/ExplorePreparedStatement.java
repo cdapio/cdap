36c59998e6c7160392b029735c990c60efbc4e8c
Julien Guery <julien@continuuity.com>
2014-06-21 02:17:39 -0700

    Mark methods as unsupported

--- explore-client/src/main/java/com/continuuity/explore/jdbc/ExplorePreparedStatement.java
+++ explore-client/src/main/java/com/continuuity/explore/jdbc/ExplorePreparedStatement.java
@@ -55,261 +55,261 @@ public class ExplorePreparedStatement extends ExploreStatement implements Prepar
 
   @Override
   public void setNull(int i, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBoolean(int i, boolean b) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setByte(int i, byte b) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setShort(int i, short i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setInt(int i, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setLong(int i, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setFloat(int i, float v) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setDouble(int i, double v) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBigDecimal(int i, BigDecimal bigDecimal) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setString(int i, String s) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBytes(int i, byte[] bytes) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setDate(int i, Date date) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setTime(int i, Time time) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setTimestamp(int i, Timestamp timestamp) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setAsciiStream(int i, InputStream inputStream, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setUnicodeStream(int i, InputStream inputStream, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBinaryStream(int i, InputStream inputStream, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void clearParameters() throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setObject(int i, Object o, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setObject(int i, Object o) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void addBatch() throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setCharacterStream(int i, Reader reader, int i2) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setRef(int i, Ref ref) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBlob(int i, Blob blob) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setClob(int i, Clob clob) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setArray(int i, Array array) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public ResultSetMetaData getMetaData() throws SQLException {
-    return null;
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setDate(int i, Date date, Calendar calendar) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setTime(int i, Time time, Calendar calendar) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setTimestamp(int i, Timestamp timestamp, Calendar calendar) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNull(int i, int i2, String s) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setURL(int i, URL url) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public ParameterMetaData getParameterMetaData() throws SQLException {
-    return null;
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setRowId(int i, RowId rowId) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNString(int i, String s) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNCharacterStream(int i, Reader reader, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNClob(int i, NClob nClob) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setClob(int i, Reader reader, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBlob(int i, InputStream inputStream, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNClob(int i, Reader reader, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setSQLXML(int i, SQLXML sqlxml) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setObject(int i, Object o, int i2, int i3) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setAsciiStream(int i, InputStream inputStream, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBinaryStream(int i, InputStream inputStream, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setCharacterStream(int i, Reader reader, long l) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setAsciiStream(int i, InputStream inputStream) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBinaryStream(int i, InputStream inputStream) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setCharacterStream(int i, Reader reader) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNCharacterStream(int i, Reader reader) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setClob(int i, Reader reader) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setBlob(int i, InputStream inputStream) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 
   @Override
   public void setNClob(int i, Reader reader) throws SQLException {
-
+    throw new SQLException("Method not supported");
   }
 }
