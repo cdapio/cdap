afa0a26eb5f11ad1e2d866fdd0a9e882469cf97c
poorna <poorna@continuuity.com>
2014-06-23 16:19:59 -0700

    Calling RecordScannable.close in finally to prevent resource leaks.

--- explore/src/main/java/com/continuuity/hive/datasets/DatasetAccessor.java
+++ explore/src/main/java/com/continuuity/hive/datasets/DatasetAccessor.java
@@ -52,9 +52,11 @@ public class DatasetAccessor {
    */
   public static Type getRecordScannableType(Configuration conf) throws IOException {
     RecordScannable<?> recordScannable = instantiate(conf);
-    Type type = recordScannable.getRecordType();
-    recordScannable.close();
-    return type;
+    try {
+      return recordScannable.getRecordType();
+    } finally {
+      recordScannable.close();
+    }
   }
 
   private static RecordScannable instantiate(Configuration conf) throws IOException {
--- explore/src/main/java/com/continuuity/hive/datasets/DatasetInputFormat.java
+++ explore/src/main/java/com/continuuity/hive/datasets/DatasetInputFormat.java
@@ -36,19 +36,22 @@ public class DatasetInputFormat implements InputFormat<Void, ObjectWritable> {
 
     RecordScannable recordScannable = DatasetAccessor.getRecordScannable(jobConf);
 
-    Job job = new Job(jobConf);
-    JobContext jobContext = ShimLoader.getHadoopShims().newJobContext(job);
-    // TODO: figure out the significance of table paths - REACTOR-277
-    Path[] tablePaths = FileInputFormat.getInputPaths(jobContext);
+    try {
+      Job job = new Job(jobConf);
+      JobContext jobContext = ShimLoader.getHadoopShims().newJobContext(job);
+      // TODO: figure out the significance of table paths - REACTOR-277
+      Path[] tablePaths = FileInputFormat.getInputPaths(jobContext);
 
-    List<Split> dsSplits = recordScannable.getSplits();
-    recordScannable.close();
+      List<Split> dsSplits = recordScannable.getSplits();
 
-    InputSplit[] inputSplits = new InputSplit[dsSplits.size()];
-    for (int i = 0; i < dsSplits.size(); i++) {
-      inputSplits[i] = new DatasetInputSplit(dsSplits.get(i), tablePaths[0]);
+      InputSplit[] inputSplits = new InputSplit[dsSplits.size()];
+      for (int i = 0; i < dsSplits.size(); i++) {
+        inputSplits[i] = new DatasetInputSplit(dsSplits.get(i), tablePaths[0]);
+      }
+      return inputSplits;
+    } finally {
+      recordScannable.close();
     }
-    return inputSplits;
   }
 
   @Override
