81d428f321484c1e5091ec5d67940a1c358cb3d4
Gowri Shankar Panneer Selvam <shankar@Gowris-MacBook-Pro.local>
2014-06-24 16:10:30 -0700

    checkstyle fix

--- watchdog/src/main/java/com/continuuity/logging/gateway/handlers/LogHandler.java
+++ watchdog/src/main/java/com/continuuity/logging/gateway/handlers/LogHandler.java
@@ -19,13 +19,13 @@ import org.jboss.netty.handler.codec.http.QueryStringDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 /**
  * Handler to serve log requests.
  */
