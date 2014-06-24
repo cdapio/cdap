2984cc7ba154bb74a9b76b161bac67137765263b
John Jackson <john@continuuity.com>
2014-06-24 14:19:46 -0700

    Fixes javadocs build and resolves all warnings. However, additional content editing required.

--- continuuity-api/src/main/java/com/continuuity/api/dataset/package-info.java
+++ continuuity-api/src/main/java/com/continuuity/api/dataset/package-info.java
@@ -5,7 +5,9 @@
 /**
  * Defines APIs for accessing and managing data within Continuuity Reactor.
  * <p>
- * Implement {@link DatasetDefinition} or {@link Dataset} to define custom data set types. 
+ * Implement {@link com.continuuity.api.dataset.DatasetDefinition} or {@link com.continuuity.api.dataset.Dataset}
+ * to define custom data set types.
+ * <p>
  * Otherwise, use pre-packaged types available in the {@link com.continuuity.api.dataset.lib}  
  * and {@link com.continuuity.api.dataset.table} packages.
  */
