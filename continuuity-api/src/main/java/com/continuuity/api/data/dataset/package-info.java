f5ec3a852779a425f7a80c600ba0f09705cb0248
Alex Baranau <alex.baranov.v@gmail.com>
2014-06-23 17:08:09 -0700

    fix checkstyle

--- continuuity-api/src/main/java/com/continuuity/api/data/dataset/package-info.java
+++ continuuity-api/src/main/java/com/continuuity/api/data/dataset/package-info.java
@@ -5,8 +5,10 @@
 /**
  * Implements DataSets for object stores and indexed, key/value, and time series tables.
  * <ul>
- * <li>{@link com.continuuity.api.data.dataset.IndexedTable} implements a table that can be accessed via a secondary key</li>
- * <li>{@link com.continuuity.api.data.dataset.KeyValueTable} implements a simple key/value dataset on top of a table</li>
+ * <li>{@link com.continuuity.api.data.dataset.IndexedTable} implements a table that can be accessed via a secondary
+ * key</li>
+ * <li>{@link com.continuuity.api.data.dataset.KeyValueTable} implements a simple key/value dataset on top of
+ * a table</li>
  * <li>{@link com.continuuity.api.data.dataset.SimpleTimeseriesTable} implements a time series dataset</li>
  * </ul>
  */
