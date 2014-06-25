e6ccc025ca22294373dabd567e950cdda217b045
poorna <poorna@continuuity.com>
2014-06-25 13:11:10 -0700

    Temporarily disabling hive version check for testing on multiple distros.

--- explore/src/main/java/com/continuuity/explore/service/ExploreServiceUtils.java
+++ explore/src/main/java/com/continuuity/explore/service/ExploreServiceUtils.java
@@ -1,13 +1,11 @@
 package com.continuuity.explore.service;
 
-import com.continuuity.common.conf.StringUtils;
 import com.google.common.base.Function;
 import com.google.common.base.Splitter;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Iterables;
 
 import java.io.File;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -51,22 +49,22 @@ public class ExploreServiceUtils {
    * Check that Hive is in the class path - with a right version.
    */
   public static void checkHiveVersion(ClassLoader hiveClassLoader) {
-    try {
-      Class hiveVersionClass = hiveClassLoader.loadClass("org.apache.hive.common.util.HiveVersionInfo");
-      Method m = hiveVersionClass.getDeclaredMethod("getVersion");
-      String version = (String) m.invoke(null);
-      for (String supportedVersion : SUPPORTED_VERSIONS) {
-        if (version.startsWith(supportedVersion)) {
-          return;
-        }
-      }
-      throw new RuntimeException("Hive version " + version + " is not supported. " +
-          "Versions supported begin with one of the following: " +
-          StringUtils.arrayToString(SUPPORTED_VERSIONS));
-    } catch (RuntimeException e) {
-      throw e;
-    } catch (Throwable e) {
-      throw new RuntimeException("Hive jars not present in classpath", e);
-    }
+//    try {
+//      Class hiveVersionClass = hiveClassLoader.loadClass("org.apache.hive.common.util.HiveVersionInfo");
+//      Method m = hiveVersionClass.getDeclaredMethod("getVersion");
+//      String version = (String) m.invoke(null);
+//      for (String supportedVersion : SUPPORTED_VERSIONS) {
+//        if (version.startsWith(supportedVersion)) {
+//          return;
+//        }
+//      }
+//      throw new RuntimeException("Hive version " + version + " is not supported. " +
+//          "Versions supported begin with one of the following: " +
+//          StringUtils.arrayToString(SUPPORTED_VERSIONS));
+//    } catch (RuntimeException e) {
+//      throw e;
+//    } catch (Throwable e) {
+//      throw new RuntimeException("Hive jars not present in classpath", e);
+//    }
   }
 }
