620b398bc83c1fc547dfe5544a6cecfc41fc988a
Sree <sree@continuuity.com>
2014-07-02 10:41:00 -0700

    Add description to services

---
+++ watchdog/src/main/java/com/continuuity/logging/run/InMemoryMetricsProcessorServiceManager.java
@@ -0,0 +1,15 @@
+package com.continuuity.logging.run;
+
+import com.continuuity.common.conf.Constants;
+import com.continuuity.common.twill.AbstractInMemoryReactorServiceManager;
+
+/**
+ * InMemory MetricsProcessor Service Management class.
+ */
+public class InMemoryMetricsProcessorServiceManager extends AbstractInMemoryReactorServiceManager {
+
+  @Override
+  public String getDescription() {
+    return Constants.MetricsProcessor.SERVICE_DESCRIPTION;
+  }
+}
