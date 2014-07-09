fc7424240123d8e9250f941311b3de918b81c826
Alvin Wang <alvin@continuuity.com>
2014-07-09 03:09:07 -0700

    Fix TestBundleJarApp

--- common/src/main/java/com/continuuity/common/lang/ClassLoaders.java
+++ common/src/main/java/com/continuuity/common/lang/ClassLoaders.java
@@ -31,8 +31,7 @@ public final class ClassLoaders {
 
   private static final List<String> HADOOP_PACKAGES = Lists.newArrayList("org.apache.hadoop");
   private static final List<String> CONTINUUITY_API_PACKAGES = Lists.newArrayList(
-    "com.continuuity.api", "com.continuuity.test"
-  );
+    "com.continuuity.api", "com.continuuity.test");
 
   private ClassLoaders() { }
 
