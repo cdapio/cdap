7471bb5de928e9a48330fd5c85ff60cfa6e6d9e1
Gokul Gunasekaran <gokul@Gokuls-MacBook-Pro.local>
2014-07-10 20:15:40 -0700

    Package the GSLibraries for Mac and Linux version; Complete the GS process generation test case

---
+++ jetstream/src/main/java/com/continuuity/jetstream/util/Platform.java
@@ -0,0 +1,48 @@
+package com.continuuity.jetstream.util;
+
+/**
+ *
+ */
+public enum Platform {
+  X64_OSX,
+  X64_LINUX,
+  X64_WINDOWS,
+  UNSUPPORTED;
+
+  private static Platform platform = null;
+
+  public static Platform findPlatform() {
+    String os = System.getProperty("os.name").toLowerCase();
+    String arch = System.getProperty("os.arch").toLowerCase();
+    if (platform != null) {
+      return platform;
+    }
+
+    if (arch.startsWith("x86_64")) {
+      if (os.startsWith("win")) {
+        platform = X64_WINDOWS;
+      } else if (os.endsWith("nux")) {
+        platform = X64_LINUX;
+      } else if (os.startsWith("mac")) {
+        platform = X64_OSX;
+      } else {
+        platform = UNSUPPORTED;
+      }
+    } else {
+      platform = UNSUPPORTED;
+    }
+
+    return platform;
+  }
+
+  public static String gsLibraryResource() {
+    Platform plat = findPlatform();
+    if (plat == X64_LINUX) {
+      return "GSStreamLib_x64_linux.zip";
+    } else if (plat == X64_OSX) {
+      return "GSStreamLib_x64_osx.zip";
+    } else {
+      return null;
+    }
+  }
+}
--- jetstream/src/test/java/com/continuuity/api/GSConfigFileGeneratorTest.java
+++ jetstream/src/test/java/com/continuuity/api/GSConfigFileGeneratorTest.java
@@ -7,12 +7,20 @@ import com.continuuity.jetstream.gsflowlet.GSFlowletSpecification;
 import com.continuuity.jetstream.internal.DefaultGSFlowletConfigurer;
 import com.continuuity.jetstream.internal.LocalGSConfigFileGenerator;
 import com.continuuity.jetstream.internal.LocalGSConfigFileLocalizer;
+import com.continuuity.jetstream.util.Platform;
+import com.google.common.io.ByteStreams;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 /**
  * Tests the successful creation of GS Config Files.
@@ -23,16 +31,70 @@ public class GSConfigFileGeneratorTest {
   public static final TemporaryFolder TEMP_FOLDER = new TemporaryFolder();
 
   @Test
-  public void testBasicGSFlowletConfig() throws IOException {
+  public void testBasicGSFlowletConfig() throws IOException, InterruptedException {
     AbstractGSFlowlet flowlet = new GSFlowletBasic();
     DefaultGSFlowletConfigurer configurer = new DefaultGSFlowletConfigurer(flowlet);
     flowlet.create(configurer);
     GSFlowletSpecification spec = configurer.createGSFlowletSpec();
     GSConfigFileGenerator configFileGenerator = new LocalGSConfigFileGenerator();
     File tempDir = TEMP_FOLDER.newFolder();
-    GSConfigFileLocalizer configFileLocalizer = new LocalGSConfigFileLocalizer(tempDir, configFileGenerator);
+    tempDir.mkdir();
+
+    //Get the library zip, copy it to temp dir, unzip it
+    String libFile = Platform.gsLibraryResource();
+    File libZip = new File(tempDir, libFile);
+    copyResourceFileToDir(libFile, libZip);
+    unzipFile(libZip);
+
+    //Create directory structure to place the GS Config Files
+    File workDir = new File(tempDir, "work");
+    workDir.mkdir();
+    File queryDir = new File(workDir, "query");
+    queryDir.mkdir();
+
+    //Create the GS config files
+    GSConfigFileLocalizer configFileLocalizer = new LocalGSConfigFileLocalizer(queryDir, configFileGenerator);
     configFileLocalizer.localizeGSConfigFiles(spec);
-    //TODO: Verify the contents of the files. Also invoke translate_fta to make sure binaries are created successfully.
+
+    //Create GS binaries - and get the status of generation (if the generation was ok or not)
+    StringBuilder errorMsg = new StringBuilder();
+    boolean testExitValue = generateGSProcesses(queryDir, errorMsg);
+    Assert.assertTrue(errorMsg.toString(), testExitValue);
   }
 
+  private void copyResourceFileToDir(String fileName, File libZip) throws IOException {
+    InputStream ifres = this.getClass().getClassLoader().getResourceAsStream(fileName);
+    OutputStream outputStream = new FileOutputStream(libZip);
+    ByteStreams.copy(ifres, outputStream);
+  }
+
+  private void unzipFile(File libZip) throws IOException, InterruptedException {
+    String path = libZip.getAbsolutePath();
+    String dir = libZip.getParent();
+    Runtime rt = Runtime.getRuntime();
+    String cmd = String.format("unzip %s -d %s", path, dir);
+    Process unzip = rt.exec(cmd);
+    Assert.assertEquals(unzip.waitFor(), 0);
+  }
+
+  private boolean generateGSProcesses(File configLocation, StringBuilder errorMsg) throws IOException {
+    String[] command = {"bash", "-c", "../../bin/buildit"};
+    ProcessBuilder genGS = new ProcessBuilder(command);
+    genGS.directory(configLocation);
+    //Capture both stdout and stderr
+    genGS.redirectErrorStream(true);
+    Process p = genGS.start();
+    InputStream iStream = p.getInputStream();
+    InputStreamReader iReader = new InputStreamReader(iStream);
+    BufferedReader b = new BufferedReader(iReader);
+    String line;
+    while ((line = b.readLine()) != null) {
+      //Capture the error msg if the compilation was not successful
+      if (line.startsWith("ERROR")) {
+        errorMsg.append(line);
+      }
+    }
+    //Returns query compilation's success condition
+    return p.exitValue() == 0;
+  }
 }
