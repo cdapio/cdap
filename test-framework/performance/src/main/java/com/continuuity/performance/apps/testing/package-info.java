0e42d7d178882e8dd5b458bcd22e2f75bf770046
Terence Yim <terence@continuuity.com>
2014-07-14 12:58:41 -0700

    Remove the test framework directory. It’s not maintained.

--- test-framework/performance/src/main/java/com/continuuity/performance/apps/testing/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
- */
-
-/**
- * This package contains Continuuity Apps that are used for testing the performance of new features in the
- * Reactor.
- */
-package com.continuuity.performance.apps.testing;
--- test-framework/performance/src/test/java/com/continuuity/performance/test/app/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2012-2013 Continuuity,Inc. All Rights Reserved.
- */
-
-/**
- * This package contains examples of performance tests.
- */
-package com.continuuity.performance.test.app;
