e079ad7e7de348b2b0076c85e6542eff3eafba43
shu <shu@continuuity.com>
2014-07-16 15:37:15 -0700

    start stop being handled in bash script. implementing the common-daemon contract

--- data-fabric/bin/tx-test-main.sh
+++ data-fabric/bin/tx-test-main.sh
@@ -1,6 +1,3 @@
-#
-# Copyright 2012-2014 Continuuity,Inc. All Rights Reserved.
-#
 
 #!/bin/sh
 
@@ -13,7 +10,7 @@ lib="$bin"/../lib
 conf="$bin"/../conf
 script=`basename $0`
 
-# Need this for relative symlinks.
+# Resolve relative symlinks
 while [ -h "$PRG" ] ; do
     ls=`ls -ld "$PRG"`
     link=`expr "$ls" : '.*-> \(.*\)$'`
@@ -26,6 +23,22 @@ done
 SAVED="`pwd`"
 cd "`dirname \"$PRG\"`/.."
 APP_HOME="`pwd -P`"
+
+# Where log files are stored.  $CONTINUUITY_HOME/logs by default.
+export LOG_DIR=/var/log
+
+# A string representing this instance of hbase. $USER by default.
+export IDENT_STRING=$USER
+
+# The directory where pid files are stored. /tmp by default.
+export PID_DIR=/var/run
+
+pid=$PID_DIR/tx-service-${IDENT_STRING}.pid
+
+if [ ! -e "$pid" ] ; then
+  touch "$pid"
+fi
+
 
 # In other environment, the jars are expected to be in <HOME>/lib directory.
 # Load all the jar files. Not ideal, but we need to load only the things that
@@ -44,4 +57,62 @@ fi
 # Load all necessary configs
 CLASSPATH="$CLASSPATH:/etc/continuuity/conf/:/etc/hadoop/conf/:/usr/lib/hadoop/*:/usr/lib/hadoop-hdfs/*:/usr/lib/hbase/lib/*:/usr/lib/hadoop-yarn/*:/opt/continuuity/hbase-compat-0.96/lib/*:/etc/zookeeper/conf/*"
 
-java -cp ${CLASSPATH} -Dscript=$script com.continuuity.data2.transaction.TransactionServiceMain "$@"
+# Set Log location
+export LOG_PREFIX="tx-service-$IDENT_STRING-$HOSTNAME"
+export LOGFILE=$LOG_PREFIX.log
+loglog="${LOG_DIR}/${LOGFILE}"
+
+if [ ! -e "$loglog" ] ; then
+  touch "$loglog"
+fi
+
+
+stop() {
+  if [ -f $pid ]; then
+    pidToKill=`cat $pid`
+    # kill -0 == see if the PID exists
+    if kill -0 $pidToKill > /dev/null 2>&1; then
+      echo -n stopping $command
+      echo "`date` Terminating $command" >> $loglog
+      kill $pidToKill > /dev/null 2>&1
+      while kill -0 $pidToKill > /dev/null 2>&1;
+      do
+        echo -n "."
+        sleep 1;
+      done
+      rm $pid
+      echo
+    else
+      retval=$?
+      echo nothing to stop because kill -0 of pid $pidToKill failed with status $retval
+    fi
+    rm -f $pid
+  else
+    echo nothing to stop because no pid file $pid
+  fi
+}
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 {start|stop}"
+  exit 1
+fi
+
+if [ "x$1" == "xstart" ]; then
+  java -cp ${CLASSPATH} -Dscript=$script com.continuuity.data2.transaction.TransactionServiceMain "$@" <$loglog >>$loglog 2>&1 &
+  echo $! >$pid
+  exit 0
+fi
+
+if [ "x$1" == "xstop" ]; then
+  stop
+  exit 0
+fi
+
+if [ "x$1" == "xhelp" ]; then
+  echo "Usage: $0 {start|stop}"
+  exit 0
+fi
+
+# incorrect params
+exit 1
+
--- tx-core/src/main/java/com/continuuity/data2/transaction/TransactionServiceMain.java
+++ tx-core/src/main/java/com/continuuity/data2/transaction/TransactionServiceMain.java
@@ -16,10 +16,9 @@ import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.util.ShutdownHookManager;
 import org.apache.twill.common.Services;
 import org.apache.twill.discovery.DiscoveryServiceClient;
+import org.apache.twill.zookeeper.RetryStrategies;
 import org.apache.twill.zookeeper.ZKClient;
 import org.apache.twill.zookeeper.ZKClientService;
 import org.apache.twill.zookeeper.ZKClientServices;
@@ -27,7 +26,7 @@ import org.apache.twill.zookeeper.ZKClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.PrintStream;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -36,42 +35,58 @@ import java.util.concurrent.TimeUnit;
  */
 public class TransactionServiceMain {
 
+  private CConfiguration cConf = null;
+
+  private TransactionService txService = null;
+
   private static final Logger LOG = LoggerFactory.getLogger(TransactionServiceMain.class);
 
-  private static final int START = 1;
-  private static final int STOP = 2;
-
-  static void usage(boolean error) {
-    PrintStream out = (error ? System.err : System.out);
-    String name = TransactionServiceMain.class.getSimpleName();
-    out.println("Usage: ");
-    out.println("  " + name + " ( start | stop ) ");
+  public static void main(String args[]) throws Exception {
+    TransactionServiceMain instance = new TransactionServiceMain();
+    instance.doMain(args);
   }
 
-  public static void main(String args[]) throws Exception {
+  /**
+   * The main method. It simply call methods in the same sequence
+   * as if the program is started by jsvc.
+   */
+  public void doMain(final String[] args) throws Exception {
+    final CountDownLatch shutdownLatch = new CountDownLatch(1);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+      try {
+        try {
+          TransactionServiceMain.this.stop();
+        } finally {
+          try {
+            TransactionServiceMain.this.destroy();
+          } finally {
+            shutdownLatch.countDown();
+          }
+        }
+      } catch (Throwable t) {
+        LOG.error("Exception when shutting down: " + t.getMessage(), t);
+      }
+      }
+    });
+    init(args);
+    start();
 
-    if (args.length != 1) {
-      usage(true);
-      return;
-    }
-    if ("--help".equals(args[0])) {
-      usage(false);
-      return;
-    }
+    shutdownLatch.await();
+  }
 
-    int command;
+  /**
+   * Invoked by jsvc to initialize the program.
+   */
+  public void init(String[] args) {
+    cConf = CConfiguration.create();
+  }
 
-    if ("start".equals(args[0])) {
-      command = START;
-    } else if ("stop".equals(args[0])) {
-      command = STOP;
-    } else {
-      usage(true);
-      return;
-    }
-
-    CConfiguration cConf = CConfiguration.create();
-
+  /**
+   * Invoked by jsvc to start the program.
+   */
+  public void start() throws Exception {
     final ZKClientService zkClientService =
       ZKClientServices.delegate(
         ZKClients.reWatchOnExpire(
@@ -81,7 +96,7 @@ public class TransactionServiceMain {
                 Constants.Zookeeper.CFG_SESSION_TIMEOUT_MILLIS,
                 Constants.Zookeeper.DEFAULT_SESSION_TIMEOUT_MILLIS))
               .build(),
-            org.apache.twill.zookeeper.RetryStrategies.fixDelay(2, TimeUnit.MILLISECONDS)
+            RetryStrategies.fixDelay(2, TimeUnit.MILLISECONDS)
           )
         )
       );
@@ -99,40 +114,43 @@ public class TransactionServiceMain {
           bind(ZKClient.class).toInstance(zkClientService);
           bind(ThriftClientProvider.class).toProvider(ThriftClientProviderSupplier.class);
         }
-      });
+      }
+    );
 
     // start a tx server
-    final TransactionService txService = injector.getInstance(TransactionService.class);
+    txService = injector.getInstance(TransactionService.class);
+    Future<?> future = Services.getCompletionFuture(txService);
+    try {
+      txService.start();
+    } catch (Exception e) {
+      System.err.println("Failed to start service: " + e.getMessage());
+    }
+    future.get();
+  }
 
-    if (START == command) {
-      ShutdownHookManager.get().addShutdownHook(new Thread() {
-        @Override
-        public void run() {
-        try {
-          if (txService.isRunning()) {
-            txService.stopAndWait();
-          }
-        } catch (Throwable e) {
-          LOG.error("Failed to shutdown transaction service.", e);
-          // because shutdown hooks execute concurrently, the logger may be closed already: thus also print it.
-          System.err.println("Failed to shutdown transaction service: " + e.getMessage());
-          e.printStackTrace(System.err);
-        }
-        }
-      }, FileSystem.SHUTDOWN_HOOK_PRIORITY + 1);
-
-      Future<?> future = Services.getCompletionFuture(txService);
-      try {
-        txService.start();
-      } catch (Exception e) {
-        System.err.println("Failed to start service: " + e.getMessage());
+  /**
+   * Invoked by jsvc to stop the program.
+   */
+  public void stop() {
+    if (txService == null) {
+      return;
+    }
+    try {
+      if (txService.isRunning()) {
+        txService.stopAndWait();
       }
-      future.get();
-    } else {
-      System.out.println("Stopping Transaction Service...");
-      txService.stop();
+    } catch (Throwable e) {
+      LOG.error("Failed to shutdown transaction service.", e);
+      // because shutdown hooks execute concurrently, the logger may be closed already: thus also print it.
+      System.err.println("Failed to shutdown transaction service: " + e.getMessage());
+      e.printStackTrace(System.err);
     }
   }
+
+  /**
+   * Invoked by jsvc for resource cleanup.
+   */
+  public void destroy() { }
 
   /**
    * Provides implementation of {@link com.continuuity.data2.transaction.distributed.ThriftClientProvider}
