08e059cb34f47fb0636c89e60a245892f9e8e52b
Gowri Shankar Panneer Selvam <shankar@Gowris-MacBook-Pro.local>
2014-07-21 15:38:39 -0700

    doc fix

--- data-fabric/src/main/java/com/continuuity/data2/dataset2/DatasetFramework.java
+++ data-fabric/src/main/java/com/continuuity/data2/dataset2/DatasetFramework.java
@@ -99,7 +99,7 @@ public interface DatasetFramework {
    * This uses
    * {@link com.continuuity.api.dataset.DatasetDefinition#configure(String, DatasetProperties)}
    * method to build {@link com.continuuity.api.dataset.DatasetSpecification} which describes dataset instance
-   * and later used to initialize {@link DatasetAdmin} and {@link Dataset} for the dataset instance.
+   * and later used to upgrade {@link DatasetAdmin} and {@link Dataset} for the dataset instance.
    *
    * @param datasetTypeName dataset instance type name
    * @param datasetInstanceName dataset instance name
