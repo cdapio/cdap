95b3104b27d487870f5f0f3d0362671d5be1ab9b
Alex Baranau <alex.baranov.v@gmail.com>
2014-07-29 16:29:45 -0700

    fix metrics ttl condition cleanup

--- watchdog/src/main/java/com/continuuity/metrics/collect/LocalMetricsCollectionService.java
+++ watchdog/src/main/java/com/continuuity/metrics/collect/LocalMetricsCollectionService.java
@@ -76,6 +76,7 @@ public final class LocalMetricsCollectionService extends AggregatedMetricsCollec
     long retention = cConf.getLong(MetricsConstants.ConfigKeys.RETENTION_SECONDS + ".1.seconds",
                                    MetricsConstants.DEFAULT_RETENTION_HOURS);
 
+    // Try right away if there's anything to cleanup, then we'll schedule to do that periodically
     scheduler.schedule(createCleanupTask(retention), 1, TimeUnit.SECONDS);
   }
 
@@ -101,7 +102,9 @@ public final class LocalMetricsCollectionService extends AggregatedMetricsCollec
             return;
           }
         } catch (Exception e) {
+          // If we cannot determine that ttl is supported then try again in 1 second
           scheduler.schedule(this, 1, TimeUnit.SECONDS);
+          return;
         }
 
         long currentTime = TimeUnit.SECONDS.convert(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
