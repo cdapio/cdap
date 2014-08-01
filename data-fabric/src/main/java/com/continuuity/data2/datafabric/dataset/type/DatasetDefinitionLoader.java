5eeadee2b4a3e972847071d18dcf86be27e5772c
Gowri Shankar Panneer Selvam <shankar@Gowris-MacBook-Pro.local>
2014-08-01 12:32:14 -0700

    format fix

--- data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/type/DatasetDefinitionLoader.java
+++ data-fabric/src/main/java/com/continuuity/data2/datafabric/dataset/type/DatasetDefinitionLoader.java
@@ -42,6 +42,7 @@ import java.util.List;
  */
 public class DatasetDefinitionLoader {
   private static final Logger LOG = LoggerFactory.getLogger(DatasetDefinitionLoader.class);
+
   private final LocationFactory locationFactory;
 
   /**
