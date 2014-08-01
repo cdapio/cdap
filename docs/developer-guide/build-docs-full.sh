b9550436a0961b496cec82765eefa19419dbe594
John Jackson <john@continuuity.com>
2014-08-01 15:41:09 -0700

    Update the license PDFs and reST files from 2.3.0 to 2.4.0. Added some example commands to the docs.py to show how to build these PDFs using rst2pdf. Added a build-docs-full.sh (which we will delete before we open-source) that automatically builds the PDFs. Update the REST.rst to 2.4.0, which doesn't appear in the Pdf itself, so the PDF doesn't need to be updated.

---
+++ docs/developer-guide/build-docs-full.sh
@@ -0,0 +1,229 @@
+#!/usr/bin/env bash
+
+# Build script for Reactor docs
+# Builds the docs
+# Copies the javadocs into place
+# Zips everything up so it can be staged
+
+DATE_STAMP=`date`
+SCRIPT=`basename $0`
+
+SOURCE="source"
+BUILD="build"
+BUILD_PDF="build-pdf"
+HTML="html"
+APIDOCS="apidocs"
+JAVADOCS="javadocs"
+LICENSES="licenses"
+LICENSES_PDF="licenses-pdf"
+REACTOR="reactor"
+
+SCRIPT_PATH=`pwd`
+SOURCE_PATH="$SCRIPT_PATH/$SOURCE"
+BUILD_PATH="$SCRIPT_PATH/$BUILD"
+HTML_PATH="$BUILD_PATH/$HTML"
+
+DOCS_PY="$SCRIPT_PATH/../tools/scripts/docs.py"
+
+DOCS_INDEX_PAGE="index.html"
+DOCS_INDEX="$SCRIPT_PATH/../tools/staging/$DOCS_INDEX_PAGE"
+
+REST_SOURCE="$SOURCE_PATH/rest.rst"
+REST_PDF="$SCRIPT_PATH/$BUILD_PDF/rest.pdf"
+
+INSTALL_GUIDE="$SCRIPT_PATH/../install-guide"
+INSTALL_SOURCE="$INSTALL_GUIDE/source/install.rst"
+
+WWW_PATH="/var/www/website-docs"
+
+if [ "x$2" == "x" ]; then
+  REACTOR_PATH="$SCRIPT_PATH/../../"
+else
+  REACTOR_PATH="$2"
+fi
+REACTOR_JAVADOCS="$REACTOR_PATH/continuuity-api/target/site/apidocs"
+
+ZIP_FILE_NAME=$HTML
+ZIP="$ZIP_FILE_NAME.zip"
+STAGING_SERVER="stg-web101.sw.joyent.continuuity.net"
+
+function usage() {
+  cd $REACTOR_PATH
+  REACTOR_PATH=`pwd`
+  echo "Build script for Reactor docs"
+  echo "Usage: $SCRIPT < option > [reactor]"
+  echo ""
+  echo "  Options (select one)"
+  echo "    build        Clean build of javadocs, docs (HTML and PDF), copy javadocs and pdfs, zip results"
+  echo "    stage        Stages docs and logins to server"
+  echo "  or "
+  echo "    build-docs   Clean build of docs"
+  echo "    javadocs     Clean build of javadocs"
+  echo "    pdf-rest     Clean build of REST PDF"
+  echo "    pdf-install  Clean build of Install Guide PDF"
+  echo "    login        Logs you into $STAGING_SERVER"
+  echo "    reactor      Path to Reactor source for javadocs, if not $REACTOR_PATH"
+  echo "    zip          Zips docs into $ZIP"
+  echo "  or "
+  echo "    depends      Build Site listing dependencies"  
+  echo "    sdk          Build SDK"  
+  echo " "
+  exit 1
+}
+
+function clean() {
+  rm -rf $SCRIPT_PATH/$BUILD
+}
+
+function build_javadocs() {
+  cd $REACTOR_PATH
+  mvn clean package site -pl continuuity-api -am -Pjavadocs -DskipTests
+}
+
+function build_docs() {
+  clean
+  sphinx-build -b html -d build/doctrees source build/html
+}
+
+function build_pdf_rest() {
+#   version # version is not needed because the renaming is done by the pom.xml file
+  rm -rf $SCRIPT_PATH/$BUILD_PDF
+  mkdir $SCRIPT_PATH/$BUILD_PDF
+  python $DOCS_PY -g pdf -o $REST_PDF $REST_SOURCE
+}
+
+function build_pdf_install() {
+  version
+  INSTALL_PDF="$INSTALL_GUIDE/$BUILD_PDF/Reactor-Installation-Guide-v$REACTOR_VERSION.pdf"
+  rm -rf $INSTALL_GUIDE/$BUILD_PDF
+  mkdir $INSTALL_GUIDE/$BUILD_PDF
+  python $DOCS_PY -g pdf -o $INSTALL_PDF $INSTALL_SOURCE
+}
+
+function build_license_pdfs() {
+  rm -rf $SCRIPT_PATH/$LICENSES_PDF
+  mkdir $SCRIPT_PATH/$LICENSES_PDF
+  
+  LIC1_SOURCE="$SOURCE_PATH/licenses/reactor-enterprise-dependencies.rst"
+  LIC1_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-enterprise-dependencies.pdf"
+  python $DOCS_PY -g pdf -o $LIC1_PDF $LIC1_SOURCE
+
+  LIC2_SOURCE="$SOURCE_PATH/licenses/reactor-level-1-dependencies.rst"
+  LIC2_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-level-1-dependencies.pdf"
+  python $DOCS_PY -g pdf -o $LIC2_PDF $LIC2_SOURCE
+  
+  LIC3_SOURCE="$SOURCE_PATH/licenses/reactor-singlenode-dependencies.rst"
+  LIC3_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-singlenode-dependencies.pdf"
+  python $DOCS_PY -g pdf -o $LIC3_PDF $LIC3_SOURCE
+  
+# python docs.py -g pdf -o ../../../developer-guide/licenses-pdf/reactor-enterprise-dependencies.pdf ../../developer-guide/source/licenses/reactor-enterprise-dependencies.rst
+# python docs.py -g pdf -o ../../../developer-guide/licenses-pdf/reactor-level-1-dependencies.pdf    ../../developer-guide/source/licenses/reactor-level-1-dependencies.rst
+# python docs.py -g pdf -o ../../../developer-guide/licenses-pdf/reactor-singlenode-dependencies.pdf ../../developer-guide/source/licenses/reactor-singlenode-dependencies.rst
+
+}
+
+function copy_javadocs() {
+  cd $BUILD_PATH/$HTML
+  rm -rf $JAVADOCS
+  cp -r $REACTOR_JAVADOCS .
+  mv -f $APIDOCS $JAVADOCS
+}
+
+function copy_license_pdfs() {
+  cd $BUILD_PATH/$HTML/$LICENSES
+  cp $SCRIPT_PATH/$LICENSES_PDF/* .
+}
+
+function make_zip() {
+  cd $SCRIPT_PATH/$BUILD
+  zip -r $ZIP_FILE_NAME $HTML/*
+}
+
+function stage_docs() {
+  echo "Deploying docs..."
+  echo "rsync -vz $SCRIPT_PATH/$BUILD/$ZIP \"$USER@$STAGING_SERVER:$ZIP\""
+  rsync -vz $SCRIPT_PATH/$BUILD/$ZIP "$USER@$STAGING_SERVER:$ZIP"
+  version
+  cd_cmd="cd $WWW_PATH/$REACTOR; ls"
+  remove_cmd="sudo rm -rf $REACTOR_VERSION"
+  unzip_cmd="sudo unzip ~/$ZIP; sudo mv $HTML $REACTOR_VERSION"
+  echo ""
+  echo "To install on server:"
+  echo ""
+  echo "  $cd_cmd"
+  echo "  $remove_cmd; ls"
+  echo "  $unzip_cmd; ls"
+  echo ""
+  echo "or, on one line:"
+  echo ""
+  echo "  $cd_cmd; $remove_cmd; ls; $unzip_cmd; ls"
+  echo ""
+  echo "or, using current branch:"
+  echo ""
+  echo "  $cd_cmd"
+  echo "  $remove_cmd-$GIT_BRANCH; ls"
+  echo "  $unzip_cmd-$GIT_BRANCH; ls"
+  echo ""
+  login_staging_server
+}
+
+function login_staging_server() {
+  echo "Logging into:"
+  echo "ssh \"$USER@$STAGING_SERVER\""
+  ssh "$USER@$STAGING_SERVER"
+}
+
+function build() {
+   build_docs
+   build_javadocs
+   copy_javadocs
+   copy_license_pdfs
+   make_zip
+}
+
+function build_single_node() {
+  cd $REACTOR_PATH
+  mvn clean package -DskipTests -P examples && mvn package -pl singlenode -am -DskipTests -P dist,release
+}
+
+function build_sdk() {
+  build_pdf_rest
+  build_single_node
+}
+
+function build_dependencies() {
+  cd $REACTOR_PATH
+  mvn clean package site -am -Pjavadocs -DskipTests
+}
+
+function version() {
+  cd $REACTOR_PATH
+  REACTOR_VERSION=`mvn help:evaluate -o -Dexpression=project.version | grep -v '^\['`
+  IFS=/ read -a branch <<< "`git rev-parse --abbrev-ref HEAD`"
+  GIT_BRANCH="${branch[1]}"
+}
+
+if [ $# -lt 1 ]; then
+  usage
+  exit 1
+fi
+
+case "$1" in
+  build )              build; exit 1;;
+  build-javadocs )     build_javadocs; exit 1;;
+  build-docs )         build_docs; exit 1;;
+  build_single_node )  build_single_node; exit 1;;
+  build_license_pdfs ) build_license_pdfs; exit 1;;
+  copy_javadocs )      copy_javadocs; exit 1;;
+  copy_license_pdfs )  copy_license_pdfs; exit 1;;
+  javadocs )           build_javadocs; exit 1;;
+  depends )            build_dependencies; exit 1;;
+  login )              login_staging_server; exit 1;;
+  pdf-install )        build_pdf_install; exit 1;;
+  pdf-rest )           build_pdf_rest; exit 1;;
+  sdk )                build_sdk; exit 1;;
+  stage )              stage_docs; exit 1;;
+  version )            version; exit 1;;
+  zip )                make_zip; exit 1;;
+  * )                  usage; exit 1;;
+esac
