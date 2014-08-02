7da681f6ac4a5a392cfb76aad958df16b8be5604
Sree <sree@continuuity.com>
2014-08-02 00:49:10 -0700

    Fix unit-tests un-comment Ignored

--- app-fabric/src/test/java/com/continuuity/internal/app/services/http/handlers/AppFabricHttpHandlerTest.java
+++ app-fabric/src/test/java/com/continuuity/internal/app/services/http/handlers/AppFabricHttpHandlerTest.java
@@ -284,7 +284,6 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
    * Tests history of a procedure.
    */
   @Test
-  @Ignore
   public void testProcedureHistory() throws Exception {
     testHistory(WordCountApp.class, "WordCountApp", "procedures", "WordFrequency", false, 0);
   }
@@ -401,7 +400,6 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
    * Metadata tests through appfabric apis.
    */
   @Test
-  @Ignore
   public void testGetMetadata() throws Exception {
     try {
       HttpResponse response = doPost("/v2/unrecoverable/reset");
@@ -662,7 +660,6 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
    * Tests procedure instances.
    */
   @Test
-  @Ignore
   public void testProcedureInstances () throws Exception {
     Assert.assertEquals(200, doDelete("/v2/apps").getStatusLine().getStatusCode());
     Assert.assertEquals(200, doPost("/v2/unrecoverable/reset").getStatusLine().getStatusCode());
@@ -1617,7 +1614,6 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
    * Test for resetting app.
    */
   @Test
-  @Ignore
   public void testUnRecoverableResetAppRunning() throws Exception {
 
     HttpResponse response = deploy(WordCountApp.class);
@@ -1690,6 +1686,7 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
     Assert.assertEquals("RUNNING", returnedBody.get(0).get("status").getAsString());
     // start the service
     doPost("/v2/apps/AppWithServices/services/NoOpService/start");
+
     response = doPost(url, "[{'appId':'WordCountApp', 'programType':'Flow', 'programId':'WordCountFlow'}," +
       "{'appId': 'AppWithServices', 'programType': 'Service', 'programId': 'NoOpService'}," +
       "{'appId': 'WordCountApp', 'programType': 'Mapreduce', 'programId': 'VoidMapReduceJob'}]");
@@ -1699,9 +1696,14 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
     Assert.assertEquals("RUNNING", returnedBody.get(0).get("status").getAsString());
     Assert.assertEquals("RUNNING", returnedBody.get(1).get("status").getAsString());
     Assert.assertEquals("STOPPED", returnedBody.get(2).get("status").getAsString());
+
+    doPost("/v2/apps/WordCountApp/flows/WordCountFlow/stop");
+    doPost("/v2/apps/AppWithServices/services/NoOpService/stop");
+    waitState("flows", "WordCountApp", "WordCountFlow", "STOPPED");
+    waitState("services", "AppWithServices", "NoOpService", "STOPPED");
+
   }
 
-  @Ignore
   @Test
   public void testBatchInstances() throws Exception {
     String url = "/v2/instances";
@@ -1765,6 +1767,13 @@ public class AppFabricHttpHandlerTest extends AppFabricTestBase {
     returnedBody = readResponse(doPost(url, "[{'appId':'WordCountApp', 'programType':'Flow'," +
       "'programId':'WordCountFlow', 'runnableId': 'StreamSource'}]"), typeToken);
     Assert.assertEquals(2, returnedBody.get(0).get("requested").getAsInt());
+
+    doPost("/v2/apps/WordCountApp/flows/WordCountFlow/stop");
+    doPost("/v2/apps/AppWithServices/services/NoOpService/stop");
+
+    waitState("flows", "WordCountApp", "WordCountFlow", "STOPPED");
+    waitState("services", "AppWithServices", "NoOpService", "STOPPED");
+
   }
 
   private String readResponse(HttpResponse response) throws IOException {
