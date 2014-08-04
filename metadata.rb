9e8d12c92d69298e040dd7d2c0ea3163f1cfd37d
Chris Gianelloni <chris@continuuity.com>
2014-08-04 15:54:38 -0700

    Initial metadata.rb to make this a cookbook

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'cdap'
+maintainer       'Cask'
+maintainer_email 'ops@cask.co'
+license          'All rights reserved'
+description      'Installs/Configures cdap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w(apt hadoop java nodejs yum).each do |cb|
+  depends cb
+end
