360e69ba6628cd0413070a18045b1d4190298ef0
Chris Gianelloni <chris@continuuity.com>
2014-08-05 14:16:22 -0700

    Add yum-epel to dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures cdap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(apt hadoop java nodejs yum).each do |cb|
+%w(apt hadoop java nodejs ntp yum yum-epel).each do |cb|
   depends cb
 end
