aa911c3b9e4154ac6a4343442459d74fa3080bdf
Parth Gandhi <parth@continuuity.com>
2014-08-05 14:47:59 -0700

    Reorder imports

--- app-fabric/src/main/java/co/cask/cdap/gateway/handlers/AppFabricHttpHandler.java
+++ app-fabric/src/main/java/co/cask/cdap/gateway/handlers/AppFabricHttpHandler.java
@@ -16,7 +16,6 @@
 
 package co.cask.cdap.gateway.handlers;
 
-import com.continuuity.tephra.TransactionSystemClient;
 import co.cask.cdap.api.ProgramSpecification;
 import co.cask.cdap.api.data.DataSetInstantiationException;
 import co.cask.cdap.api.data.stream.StreamSpecification;
@@ -82,6 +81,7 @@ import co.cask.cdap.proto.ProgramTypes;
 import co.cask.cdap.proto.StreamRecord;
 import co.cask.http.BodyConsumer;
 import co.cask.http.HttpResponder;
+import com.continuuity.tephra.TransactionSystemClient;
 import com.google.common.base.Charsets;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
