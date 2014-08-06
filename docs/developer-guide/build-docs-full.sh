ab734b2cb2f88c3ba3f6d2479d3e0ec85565c492
John Jackson <john@continuuity.com>
2014-08-05 20:13:39 -0700

    More name-change updates.

--- docs/developer-guide/build-docs-full.sh
+++ docs/developer-guide/build-docs-full.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Build script for Reactor docs
+# Build script for docs
 # Builds the docs
 # Copies the javadocs into place
 # Zips everything up so it can be staged
@@ -16,7 +16,8 @@ APIDOCS="apidocs"
 JAVADOCS="javadocs"
 LICENSES="licenses"
 LICENSES_PDF="licenses-pdf"
-REACTOR="reactor"
+PRODUCT="cdap"
+PRODUCT_CAPS="CDAP"
 
 SCRIPT_PATH=`pwd`
 SOURCE_PATH="$SCRIPT_PATH/$SOURCE"
@@ -31,28 +32,24 @@ DOCS_INDEX="$SCRIPT_PATH/../tools/staging/$DOCS_INDEX_PAGE"
 REST_SOURCE="$SOURCE_PATH/rest.rst"
 REST_PDF="$SCRIPT_PATH/$BUILD_PDF/rest.pdf"
 
-INSTALL_GUIDE="$SCRIPT_PATH/../install-guide"
-INSTALL_SOURCE="$INSTALL_GUIDE/source/install.rst"
-
 WWW_PATH="/var/www/website-docs"
 
 if [ "x$2" == "x" ]; then
-  REACTOR_PATH="$SCRIPT_PATH/../../"
+  PRODUCT_PATH="$SCRIPT_PATH/../../"
 else
-  REACTOR_PATH="$2"
+  PRODUCT_PATH="$2"
 fi
-# REACTOR_JAVADOCS="$REACTOR_PATH/continuuity-api/target/site/apidocs"
-REACTOR_JAVADOCS="$REACTOR_PATH/target/site/apidocs"
+PRODUCT_JAVADOCS="$PRODUCT_PATH/target/site/apidocs"
 
 ZIP_FILE_NAME=$HTML
 ZIP="$ZIP_FILE_NAME.zip"
 STAGING_SERVER="stg-web101.sw.joyent.continuuity.net"
 
 function usage() {
-  cd $REACTOR_PATH
-  REACTOR_PATH=`pwd`
-  echo "Build script for Reactor docs"
-  echo "Usage: $SCRIPT < option > [reactor]"
+  cd $PRODUCT_PATH
+  PRODUCT_PATH=`pwd`
+  echo "Build script for '$PRODUCT_CAPS' docs"
+  echo "Usage: $SCRIPT < option > [source]"
   echo ""
   echo "  Options (select one)"
   echo "    build        Clean build of javadocs, docs (HTML and PDF), copy javadocs and pdfs, zip results"
@@ -63,11 +60,12 @@ function usage() {
   echo "    pdf-rest     Clean build of REST PDF"
   echo "    pdf-install  Clean build of Install Guide PDF"
   echo "    login        Logs you into $STAGING_SERVER"
-  echo "    reactor      Path to Reactor source for javadocs, if not $REACTOR_PATH"
   echo "    zip          Zips docs into $ZIP"
-  echo "  or "
-  echo "    depends      Build Site listing dependencies"  
-  echo "    sdk          Build SDK"  
+  echo "  or"
+  echo "    depends      Build Site listing dependencies"
+  echo "    sdk          Build SDK"
+  echo "  with"
+  echo "    source       Path to $PRODUCT source for javadocs, if not $PRODUCT_PATH"
   echo " "
   exit 1
 }
@@ -77,8 +75,7 @@ function clean() {
 }
 
 function build_javadocs() {
-  cd $REACTOR_PATH
-#   mvn clean package site -pl continuuity-api -am -Pjavadocs -DskipTests
+  cd $PRODUCT_PATH
   mvn clean site -DskipTests
 }
 
@@ -95,8 +92,10 @@ function build_pdf_rest() {
 }
 
 function build_pdf_install() {
+  INSTALL_GUIDE="$SCRIPT_PATH/../install-guide"
+  INSTALL_SOURCE="$INSTALL_GUIDE/source/install.rst"
   version
-  INSTALL_PDF="$INSTALL_GUIDE/$BUILD_PDF/Reactor-Installation-Guide-v$REACTOR_VERSION.pdf"
+  INSTALL_PDF="$INSTALL_GUIDE/$BUILD_PDF/$PRODUCT_CAPS-Installation-Guide-v$PRODUCT_VERSION.pdf"
   rm -rf $INSTALL_GUIDE/$BUILD_PDF
   mkdir $INSTALL_GUIDE/$BUILD_PDF
   python $DOCS_PY -g pdf -o $INSTALL_PDF $INSTALL_SOURCE
@@ -106,23 +105,23 @@ function build_license_pdfs() {
   rm -rf $SCRIPT_PATH/$LICENSES_PDF
   mkdir $SCRIPT_PATH/$LICENSES_PDF
   
-  LIC1_SOURCE="$SOURCE_PATH/licenses/reactor-enterprise-dependencies.rst"
-  LIC1_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-enterprise-dependencies.pdf"
+  LIC1_SOURCE="$SOURCE_PATH/licenses/$PRODUCT-enterprise-dependencies.rst"
+  LIC1_PDF="$SCRIPT_PATH/$LICENSES_PDF/$PRODUCT-enterprise-dependencies.pdf"
   python $DOCS_PY -g pdf -o $LIC1_PDF $LIC1_SOURCE
 
-  LIC2_SOURCE="$SOURCE_PATH/licenses/reactor-level-1-dependencies.rst"
-  LIC2_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-level-1-dependencies.pdf"
+  LIC2_SOURCE="$SOURCE_PATH/licenses/$PRODUCT-level-1-dependencies.rst"
+  LIC2_PDF="$SCRIPT_PATH/$LICENSES_PDF/$PRODUCT-level-1-dependencies.pdf"
   python $DOCS_PY -g pdf -o $LIC2_PDF $LIC2_SOURCE
   
-  LIC3_SOURCE="$SOURCE_PATH/licenses/reactor-singlenode-dependencies.rst"
-  LIC3_PDF="$SCRIPT_PATH/$LICENSES_PDF/reactor-singlenode-dependencies.pdf"
+  LIC3_SOURCE="$SOURCE_PATH/licenses/$PRODUCT-singlenode-dependencies.rst"
+  LIC3_PDF="$SCRIPT_PATH/$LICENSES_PDF/$PRODUCT-singlenode-dependencies.pdf"
   python $DOCS_PY -g pdf -o $LIC3_PDF $LIC3_SOURCE
 }
 
 function copy_javadocs() {
   cd $BUILD_PATH/$HTML
   rm -rf $JAVADOCS
-  cp -r $REACTOR_JAVADOCS .
+  cp -r $PRODUCT_JAVADOCS .
   mv -f $APIDOCS $JAVADOCS
 }
 
@@ -141,9 +140,9 @@ function stage_docs() {
   echo "rsync -vz $SCRIPT_PATH/$BUILD/$ZIP \"$USER@$STAGING_SERVER:$ZIP\""
   rsync -vz $SCRIPT_PATH/$BUILD/$ZIP "$USER@$STAGING_SERVER:$ZIP"
   version
-  cd_cmd="cd $WWW_PATH/$REACTOR; ls"
-  remove_cmd="sudo rm -rf $REACTOR_VERSION"
-  unzip_cmd="sudo unzip ~/$ZIP; sudo mv $HTML $REACTOR_VERSION"
+  cd_cmd="cd $WWW_PATH/$PRODUCT; ls"
+  remove_cmd="sudo rm -rf $PRODUCT_VERSION"
+  unzip_cmd="sudo unzip ~/$ZIP; sudo mv $HTML $PRODUCT_VERSION"
   echo ""
   echo "To install on server:"
   echo ""
@@ -179,7 +178,7 @@ function build() {
 }
 
 function build_single_node() {
-  cd $REACTOR_PATH
+  cd $PRODUCT_PATH
   mvn clean package -DskipTests -P examples && mvn package -pl singlenode -am -DskipTests -P dist,release
 }
 
@@ -188,15 +187,20 @@ function build_sdk() {
 }
 
 function build_dependencies() {
-  cd $REACTOR_PATH
+  cd $PRODUCT_PATH
   mvn clean package site -am -Pjavadocs -DskipTests
 }
 
 function version() {
-  cd $REACTOR_PATH
-  REACTOR_VERSION=`mvn help:evaluate -o -Dexpression=project.version | grep -v '^\['`
+  cd $PRODUCT_PATH
+  PRODUCT_VERSION=`mvn help:evaluate -o -Dexpression=project.version | grep -v '^\['`
   IFS=/ read -a branch <<< "`git rev-parse --abbrev-ref HEAD`"
   GIT_BRANCH="${branch[1]}"
+}
+
+function print_version() {
+  version
+  echo "PRODUCT_VERSION: $PRODUCT_VERSION"
 }
 
 if [ $# -lt 1 ]; then
@@ -219,7 +223,7 @@ case "$1" in
   pdf-rest )           build_pdf_rest; exit 1;;
   sdk )                build_sdk; exit 1;;
   stage )              stage_docs; exit 1;;
-  version )            version; exit 1;;
+  version )            print_version; exit 1;;
   zip )                make_zip; exit 1;;
   * )                  usage; exit 1;;
 esac
