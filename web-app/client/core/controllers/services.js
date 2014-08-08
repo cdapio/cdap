6cae558626c91515fb01d83c1b24509ec535e468
Ali Anwar <anwar1@berkeley.edu>
2014-08-07 18:21:33 -0700

    Fetch and create the services list once, and only update the attributes of the existing services objects, rather than creating them each time. Assumptions: system services list is static.

--- web-app/client/core/controllers/services.js
+++ web-app/client/core/controllers/services.js
@@ -12,14 +12,14 @@ define([], function () {
       var self = this;
       self.set('systemServices', []);
 
-      self.resetServices();
+      self.fetchServices();
 
       this.interval = setInterval(function () {
-        self.resetServices();
+        self.updateServices();
       }, C.POLLING_INTERVAL)
     },
 
-    resetServices: function () {
+    fetchServices: function () {
       var self = this;
       var systemServices = [];
 
@@ -58,6 +58,50 @@ define([], function () {
       });
     },
 
+    updateServices: function () {
+      var self = this;
+      self.HTTP.rest('system/services', function (services) {
+        services.map(function(service) {
+          var existingService = self.find(service.name);
+          
+          existingService.set('modelId', service.name);
+          existingService.set('description', service.description);
+          existingService.set('id', service.name);
+          existingService.set('name', service.name);
+          existingService.set('description', service.description);
+          existingService.set('status', service.status);
+          existingService.set('min', service.min);
+          existingService.set('max', service.max);
+          existingService.set('isIncreaseEnabled', service.requested < service.max);
+          existingService.set('isDecreaseEnabled', service.requested > service.min);
+          existingService.set('logs', service.logs);
+          existingService.set('requested', service.requested);
+          existingService.set('provisioned', service.provisioned);
+          existingService.set('logsStatusOk', !!(service.logs === 'OK'));
+          existingService.set('logsStatusNotOk', !!(service.logs === 'NOTOK'));
+          existingService.set('metricEndpoint', C.Util.getMetricEndpoint(service.name));
+          existingService.set('metricName', C.Util.getMetricName(service.name));
+          existingService.set('imgClass', service.status === 'OK' ? 'complete' : 'loading');
+          existingService.set('logClass', service.status === 'OK' ? 'complete' : 'loading');
+        });
+
+        // Bind all the tooltips after UI has rendered after call has returned.
+        setTimeout(function () {
+          $("[data-toggle='tooltip']").tooltip();
+        }, 1000);
+      });
+    },
+
+    find: function (serviceName) {
+      var systemServices = this.get('systemServices');
+      for (var i=0; i<systemServices.length; i++) {
+        var service = systemServices[i];
+        if(service.name === serviceName){
+          return service;
+        }
+      }
+      alert('problem! find in systemservices.js');
+    },
 
     changeInstances: function (service) {
       var inputStr = service.get('instancesInput');
