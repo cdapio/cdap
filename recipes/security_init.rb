32e1cebf3ae4f3d7aedf790a72c3a67f6a275aaa
Chris Gianelloni <chris@continuuity.com>
2014-08-13 12:49:05 -0700

    Recipe to initialize keystore for security server

---
+++ recipes/security_init.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: cdap
+# Recipe:: security_init
+#
+# Copyright (C) 2013-2014 Continuuity, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'cdap::security'
+
+# Create a new keystore if SSL is enabled
+execute 'create-security-server-ssl-keystore' do
+  ssl_enabled = node['cdap']['cdap_site']['security.server.ssl.enabled'] || false
+  password = node['cdap']['cdap_site']['security.server.ssl.keystore.password']
+  path = node['cdap']['cdap_site']['security.server.ssl.keystore.path']
+
+  command "keytool -genkey -noprompt -alias ext-auth -keysize 2048 -keyalg RSA -keystore #{path} -storepass #{password} -keypass #{password} -dname 'CN=security, OU=cdap, O=cdap, L=Palo Alto, ST=CA, C=US'"
+  not_if { File.exist?(path) }
+  only_if { ssl_enabled }
+end
