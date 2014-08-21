48a769a0137e4c775b35b48caba0b3fdd883cd7a
Ali Anwar <anwar1@berkeley.edu>
2014-08-21 12:42:40 -0700

    Additional change to remove runnable from service endpoint path.

--- gateway/src/main/java/co/cask/cdap/gateway/router/RouterPathLookup.java
+++ gateway/src/main/java/co/cask/cdap/gateway/router/RouterPathLookup.java
@@ -97,7 +97,7 @@ public final class RouterPathLookup extends AuthenticatedHttpHandler {
         //Discoverable Service Name -> procedure.%s.%s.%s", accountId, appId, procedureName ;
         String serviceName = String.format("procedure.%s.%s.%s", accId, uriParts[2], uriParts[4]);
         return serviceName;
-      } else if ((uriParts.length >= 9) && uriParts[3].equals("services") && uriParts[7].equals("methods")) {
+      } else if ((uriParts.length >= 7) && uriParts[3].equals("services") && uriParts[5].equals("methods")) {
         //TODO: review API: https://jira.continuuity.com/browse/REACTOR-812
         //User defined services handle methods on them:
         //Service Path:  "/v2/apps/{app-id}/services/{service-id}/methods/<user-defined-method-path>"
