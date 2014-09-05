1d70b7082c76e17309c3b59aac7de6e753a7283f
Ajai <ajainarayanan@gmail.com>
2014-09-05 15:22:43 -0700

    Fixes common config parsing utility

--- web-app/server/common/configParser.js
+++ web-app/server/common/configParser.js
@@ -1,6 +1,5 @@
 module.exports = {
-  extractConfigFromXml: extractConfigFromXml,
-  extractBaseConfig: extractBaseConfig
+  extractConfig: extractConfig
 };
 
 var promise = require('q'),
@@ -10,77 +9,34 @@ var promise = require('q'),
     util = require('./util'),
     configObj = {};
 
-function extractConfigFromXml(filename) {
-  var deferredObj = promise.defer(),
-      promises = [];
-
-  //There should definitely be better way to do this.
-  util.readFile(filename)
-    .then(parseConfigXmlFile.bind(this))
-    .then(parseCredentials.bind(this))
-    .then( function onConfigCredentialFetch(error, apiKey) {
-      configObj.apiKey = apiKey;
-      configObj.configSet = true;
-      deferredObj.resolve(configObj);
-    }.bind(this));
-  return deferredObj.promise;
-}
-
-function extractBaseConfig() {
+function extractConfig(mode) {
   var deferred = promise.defer(),
       decoder = new StringDecoder('utf8'),
-      xmlReader = spawn("jar", ['-xvf', __dirname + '/lib/common-2.3.1.jar', 'continuuity-default.xml']);
-
-  xmlReader.stderr.on('data', xmlReadFail);
-  xmlReader.stdout.on('end', function onXmlReadEnd(data) {
-    util.readFile('continuuity-default.xml')
-      .then( function onXmlFetch(result, error) {
-        return parseXml(result);
-      })
-      .then( function onConfigFetch(config) {
-        this.baseConfig = config;
-        this.isDefaultConfig = true;
-        deferred.resolve();
-      }.bind(this))
-  }.bind(this));
+      configReader;
+  if (mode === "enterprise") {
+    configReader = spawn("../../bin/config-tool", ['--output ../../cdap-config.json']);
+    configReader.stderr.on('data', configReadFail.bind(this));
+    configReader.stdout.on('end', function onXmlReadEnd(data) {
+      this.config = require("../../cdap-config.json");
+      this.configSet = true;
+      deferred.resolve();
+    });
+  } else {
+    this.config = require("../../cdap-config.json");
+    this.configSet = true;
+    setTimeout(function() {
+      deferred.resolve();
+    }, 10)
+  }
   return deferred.promise;
-}
-
-function parseXml(xml) {
-  var deferred = promise.defer(),
-      config = {},
-      item,
-      parser = new xml2js.Parser();
-
-  parser.parseString(xml, function onXmlBufferParse(err, result) {
-    result = result.property;
-    for (item in result) {
-      item = result[item];
-      config[item.name] = item.value;
-    }
-    deferred.resolve(config);
-  });
-  return deferred.promise;
-}
-
-function parseConfigXmlFile (result, error) {
-  parseXml(result)
-    .then(function (configuration) {
-      configObj = configuration;
-    })
-  return  util.readFile(__dirname + '/../../../VERSION', 'utf-8');
-}
-
-function parseCredentials(v) {
-  configObj.version = v;
-  return util.readFile(__dirname + '/.credential', 'utf-8');
 }
 
 function xmlReadFail() {
+  var decoder = new (require('string_decoder').StringDecoder)('utf-8');
   var textChunk = decoder.write(arguments[0]);
   if (textChunk) {
     this.logger.info(textChunk);
   } else {
-    this.logger.error('Extracting the xml file failed!');
+    this.logger.error('Extracting the config file failed!');
   }
 }
