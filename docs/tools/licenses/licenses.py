e072dc7ee00668d07b93846d2775ea3bf58a964c
John Jackson <john@continuuity.com>
2014-09-11 11:28:58 -0700

    First pass at fixing dependency issues.

--- docs/tools/licenses/licenses.py
+++ docs/tools/licenses/licenses.py
@@ -14,22 +14,33 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-
 #
 # Checks that the license dependencies files used match the dependencies in the product.
 # Run this script after building the SDK.
 # Usage: python licenses.py 
 #
+# Note that the level1 dependencies CSV file is assumed to have a header line at start.
+# It reads:
+# GroupId,ArtifactId
+
+
+# Changes
+# 
+# v0.0.5
+# Changed to accomodate multiple 
 
 
 from optparse import OptionParser
 import csv
 import os
+import pickle
 import subprocess
 import sys
 import tempfile
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
+
+MASTER = "cdap-dependencies-master.obj"
 
 MASTER_CSV = "cdap-dependencies-master.csv"
 
@@ -48,6 +59,7 @@ DEBUG = False
 
 def get_sdk_version():
     # Sets the CDAP Build Version via maven
+    return "2.5.0"
     mvn_version_cmd = "mvn help:evaluate -o -Dexpression=project.version -f ../../../pom.xml | grep -v '^\['"
     version = None
     try:
@@ -136,6 +148,13 @@ def parse_options():
         help="Prints out the master dependency file",
         default=False)
 
+    parser.add_option(
+        "-t", "--master_test",
+        action="store_true",
+        dest="master_test",
+        help="Loads and stores the master dependency file",
+        default=False)
+
     (options, args) = parser.parse_args()
     
     global DEBUG
@@ -158,13 +177,20 @@ def log(message, type):
     (sys.stdout if type == 'notice' else sys.stderr).write(message + "\n")
 
 
-def process_master():
+def load_master_csv():
     # Read in the master csv files and create a dictionary of it
-    # Keys are the jars, Values are the Library instances
+    # Dictionary format:
+    #   Keys are the jars
+    #   Values are instances of Library class
     # Get the current dependencies master csv file
     # "jar","Version","Classifier","License","License URL"
-    # Example:
+    # Example csv line:
     # "bonecp-0.8.0.RELEASE.jar","0.8.0","RELEASE","Apache License, Version 2.0","http://www.apache.org/licenses/LICENSE-2.0.html"
+    # As of v0.0.5, need to accomodate multiple versions of the same artifacts
+    # Changed the master format to be a python object, and save the dict directly
+    # Library object changed to accomodate multiple versions
+    # 
+    # 
     master_libs_dict = {}
     print "Reading master file"
     csv_path = os.path.join(SCRIPT_DIR_PATH, MASTER_CSV)
@@ -175,6 +201,7 @@ def process_master():
             row_count += 1
             jar = row[0]
             if len(row)==5:
+                # jar, license, license_url
                 lib = Library(row[0], row[3], row[4])
                 # Place lib reference in dictionary
                 if not master_libs_dict.has_key(lib.id):
@@ -192,9 +219,71 @@ def process_master():
     print "Master CSV: Rows read: %s; Unique Keys created: %s" % (row_count, len(keys))
     return master_libs_dict
 
-    
+def load_master():
+    # Look for pickled master
+    print "Looking for pickled master file"
+    if os.path.exists(os.path.join(SCRIPT_DIR_PATH, MASTER)):
+        return read_master_pickle()
+    else:
+        print "Pickled master file not found"
+        print "Looking for CSV master file"
+        # If not, use the csv
+        return load_master_csv()
+
+def save_master(master_dict):
+    # Write out a new master csv file
+    import csv
+    csv_path = os.path.join(SCRIPT_DIR_PATH, MASTER_CSV + ".new.csv")
+    if os.path.isfile(csv_path):
+        print "New master CSV: Master file already exists: %s" % csv_path
+    else:
+        with open(csv_path, 'w') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            keys = master_dict.keys()
+            keys.sort()
+            for k in keys:
+                r = master_dict[k].get_row()
+                print_debug(r)
+                csv_writer.writerow(r)
+        print "New master CSV: wrote %s records to:\n%s" % (len(keys), csv_path)
+
+def save_master_pickle(master_dict):
+    # Write out a new master pickled file
+    import pickle
+    obj_path = os.path.join(SCRIPT_DIR_PATH, MASTER + ".new.obj")
+    if os.path.isfile(obj_path):
+        print "New master object: Master file already exists: \n%s" % obj_path
+    else:
+        with open(obj_path, 'w') as obj_file:
+            pickle.dump(master_dict, obj_file)
+            keys = master_dict.keys()
+        print "New master object: wrote %s records to:\n%s" % (len(keys), obj_path)
+
+def read_master_pickle():
+    # Read an existing master pickled file
+    import pickle
+    obj_path = os.path.join(SCRIPT_DIR_PATH, MASTER)
+    if not os.path.isfile(obj_path):
+        print "Reading master object: Master file doesn't exist: \n%s" % obj_path
+        master_dict = {}
+    else:
+        with open(obj_path, 'r') as obj_file:
+            master_dict = pickle.load(obj_file)
+            keys = master_dict.keys()
+        print "Read master object: read %s records from:\n%s" % (len(keys), obj_path)
+    return master_dict
+
+def master_test():
+    print "master_test"
+    print "load_master"
+    master_dict = load_master()
+    print "save_master"
+    save_master(master_dict)
+    save_master_pickle(master_dict)
+
+
 def master_print():
-    master_libs_dict = process_master()
+    master_libs_dict = load_master()
     # Print out the results
     keys = master_libs_dict.keys()
     keys.sort()
@@ -211,7 +300,7 @@ def process_standalone(input_file, options):
 
     
 def process_level_1(input_file, options):
-    master_libs_dict = process_master()
+    master_libs_dict = load_master()
     # Build a lookup table for the artifacts
     # A dictionary relating an artifact to a Library instance
     master_artifact_ids= {}
@@ -275,16 +364,58 @@ def process_level_1(input_file, options):
         rst_data.append(row)
     return rst_data
 
+def convert_filename(d):
+    # New form of dependency: 
+    # antlr.antlr-2.7.7.jar
+    # org.apache.spark.spark-core_2.10-1.0.1.jar
+    # oss.spec.javax.annotation.jboss-annotations-api_1.1_spec-1.0.1.Final.jar
+    # The old jar would be jboss-annotations-api_1.1_spec-1.0.1.Final.jar
+    # 
+    # find first period before first hyphen before first underscore
+    # right-most hyphen (there is always a hyphen before the version)
+    # if there is an underscore, it currently is always in the component and could use it
+    l = len(d)
+    # find first underscore
+    u = d.find("_")
+    # find right-most hyphen and use it as the marker for the old jar
+    h = d.rfind("-")
+    if u != -1 and u < h: # an underscore before the right-most hypen
+        h = u # Use it as marker instead
+    # find first period before first marker
+    p = d.rfind(".", 0, h) +1
+    return d[p:]
+    
 
 def process_dependencies(dependency):
-    # Read in the current master csv file and create a structure with it
-    # Read in the new dependencies csv file
+    # Read in the current master file and create a structure with it
+    # Structure (master_dict) required: 
+    #   A dictionary where the 
+    #       keys: string consisting of "group-id.artifact-id"
+    #       values: Library instances
+    # Read in a new dependencies csv file
+    #   File format:
+    #       group-id.artifact-id.jar-string
+    #   jar-string (or "Package") consists of artifact-id.version."jar"
+    #   Example line:
+    #       com.github.stephenc.high-scale-lib.high-scale-lib-1.1.1.jar
+    # Convert line into components, and look in master_dict for entry
+    # If found:
+    #   add version (if not there already)
+    # If not found:
+    #   add a new instance to master_dict
+    #   add instance to list to be researched
     # Create and print to standard out the list of the references
-    # Make a list of the references for which links are missing and need to be added to the master
-    # Make a new master list
+    # Print to standard out list of the instances for which links are missing and need to be researched
+    # Make a new master list (it may had new versions added)
     # Return "Package","Version","Classifier","License","License URL"
+    # "Package" is the same as the jar
     
-    master_libs_dict = process_master()
+    master_libs_dict = load_master()
+    master_keys = master_libs_dict.keys()
+#     master_keys.sort()
+#     for k in master_keys:
+#         master_libs_dict[k].pretty_print()
+#     print_debug("Records: %s" % len(master_keys))  
     
     # Read dependencies
     new_libs_dict = {}
@@ -298,19 +429,45 @@ def process_dependencies(dependency):
         for row in csv_reader:
             row_count += 1
             jar = row[0]
-            lib = Library(row[0], "", "")
+            old_jar = convert_filename(jar)
+            print '%s\n  old jar: %s' % (jar, old_jar)
+            lib = Library(jar, "", "")
             print 'lib.id %s' % lib.id
-            # Look up lib reference in master dictionary; if not there, add it
-            if not master_libs_dict.has_key(lib.id):
+            # Look up lib reference in master dictionary; 
+            # if not there, try the old jar format
+            # if there, replace it;
+            # if still not there, it might be a different version; need to search for it.
+            # if still not there,
+            # create a new one
+            if master_libs_dict.has_key(lib.id):
+                pass
+            elif master_libs_dict.has_key(old_jar):
+                print "Converting key: %s to %s" % (old_jar, jar)
+                lib = master_libs_dict[lib.id]
+                del master_libs_dict[lib.id]
+                lib.id = jar
                 master_libs_dict[lib.id] = lib
-                missing_libs_dict[lib.id] = lib
-            # Place lib reference in dictionary
+            else:
+                # Look if any of the master_keys are in the jar
+                for k in master_keys:
+                    if jar.endswith(k):
+                        # Use this key
+                        print "Converting key: %s to %s" % (k, jar)
+                        lib = master_libs_dict[k]
+                        del master_libs_dict[k]
+                        lib.id = jar
+                        master_libs_dict[lib.id] = lib
+                        break
+            
+            master_libs_dict[lib.id] = lib
+            missing_libs_dict[lib.id] = lib
+
+            # Place lib reference in new libs dictionary
             if not new_libs_dict.has_key(lib.id):
                 new_libs_dict[lib.id] = master_libs_dict[lib.id]
             else:
                 lib.print_duplicate(new_libs_dict)
-
-    missing_entries = len(missing_libs_dict.keys())
+    
     for lib_dict in [master_libs_dict]:
         keys = lib_dict.keys()
         keys.sort()
@@ -324,28 +481,15 @@ def process_dependencies(dependency):
 
     print "New CSV: Rows: %s" % len(new_libs_dict.keys())
     print "New Master CSV: Rows: %s" % len(master_libs_dict.keys())
+    missing_entries = len(missing_libs_dict.keys())
     print "New Master CSV: Missing Entry Rows: %s" % missing_entries
     print "New Master CSV: Missing License Rows: %s" % missing_licenses
 
-    # Write out a new master cvs file, only if not already exists 
-    if missing_entries or missing_licenses:
-        import csv
-    
-        csv_path = os.path.join(SCRIPT_DIR_PATH, MASTER_CSV + ".new.csv")
-        if os.path.isfile(csv_path):
-            print "New master CSV: Master file already exists: %s" % csv_path
-        else:
-            with open(csv_path, 'w') as csv_file:
-                csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-                keys = master_libs_dict.keys()
-                keys.sort()
-                for k in keys:
-                    r = lib_dict[k].get_row()
-                    print_debug(r)
-                    csv_writer.writerow(r)
-            print "New master CSV: wrote %s records to:\n%s" % (len(keys), csv_path)
+    # Write out a new master csv file 
+    save_master(master_libs_dict)
 
     # Return the "Package","Version","Classifier","License","License URL"
+    # Need to insert BACK_DASH if an empty string, otherwise rst won't work
     rst_data = []
     keys = new_libs_dict.keys()
     keys.sort()
@@ -361,7 +505,7 @@ def process_dependencies(dependency):
 
 def print_rst_level_1(input_file, options):
     title = "Level 1"
-    file_base = LEVEL_1
+    file_base = LEVEL_1     
     header = '"Package","Artifact","License","License URL"'
     widths = "20, 20, 20, 40"
     data_list = process_level_1(input_file, options)
@@ -430,16 +574,17 @@ def print_debug(message):
 
 
 class Library:
+    # version 2
     MAX_SIZES={}
     PRINT_ORDER = ['id','jar','base','version','classifier','license','license_url']
     SPACE = " "*3
     
     def __init__(self, jar, license, license_url):
-        self.jar = jar # aka "package"
         self.id = ""
         self.base = ""
-        self.version =  ""
         self.classifier = ""
+        self.jar = jar # aka package
+        self.version = ""
         self.license = license
         self.license_url = license_url
         self._convert_jar()
@@ -453,24 +598,26 @@ class Library:
         # If that fails, tries without the .jar
         # If still no match, uses jar as base instead.
         # Converts the jar into its component parts: base, version, classifier
+        # Sets the id as either the base or combination of base and classifier
         import re
+        jar = self.jar
         s_jar = r'(?P<base>.*?)-(?P<version>\d*[0-9.]*\d+)([.-]*(?P<classifier>.*?))\.jar$'
         s_no_jar = r'(?P<base>.*?)-(?P<version>\d*[0-9.]*\d+)([.-]*(?P<classifier>.*?))'
         try:
-            m = re.match( s_jar, self.jar)
+            m = re.match( s_jar, jar)
             if not m:
-                m = re.match( s_no_jar, self.jar)
+                m = re.match( s_no_jar, jar)
             if m:
                 if m.group('classifier'):
                     c = m.group('classifier')
                 else:
                     c = "<none>"
-                    print_debug("%s: %s %s %s" % (self.jar, m.group('base'), m.group('version'), c ))
+                    print_debug("%s: %s %s %s" % (jar, m.group('base'), m.group('version'), c ))
                 self.base = m.group('base')
                 self.version =  m.group('version')
                 self.classifier = m.group('classifier')
             else:
-                self.base = self.jar
+                self.base = jar
             if self.classifier:
                 self.id = "%s-%s" % (self.base, self.classifier)
             else:
@@ -535,6 +682,9 @@ def main():
             
         elif options.master_print:
             master_print()
+            
+        elif options.master_test:
+            master_test()
             
         else:
             print "Unknown test type: %s" % options.test
