23547759ead0d12e3a24f8b251aaf08676295b27
Ajai <ajainarayanan@gmail.com>
2014-09-15 14:59:35 -0700

    Fixes router.server.port config property to reflect the new name

--- web-app/server/common/server.js
+++ web-app/server/common/server.js
@@ -114,7 +114,6 @@ WebAppServer.prototype.setAttributes = function setCommonAttributes() {
     }
     this.apiKey = this.config.apiKey;
     this.version = this.config.version;
-    this.configSet = this.configSet;
     this.configureExpress();
     this.setCookieSession(this.cookieName, this.secret);
 }
@@ -165,9 +164,9 @@ WebAppServer.prototype.setSecurityStatus = function (callback) {
     this.transferProtocol = "https://";
     url = 'https://' + this.config['router.server.address'] + ':' + this.config['router.ssl.server.port'] + path;
   } else {
-    this.routerBindPort = this.config['router.server.port'];
+    this.routerBindPort = this.config['router.bind.port'];
     this.transferProtocol = "http://";
-    url = 'http://' + this.config['router.server.address'] + ':' + this.config['router.server.port'] + path;
+    url = 'http://' + this.config['router.server.address'] + ':' + this.config['router.bind.port'] + path;
   }
   var interval = setInterval(function () {
     self.logger.info('Calling security endpoint: ', url);
