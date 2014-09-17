cace59779d216ea4962df2e8680054d9d1648948
Chris Gianelloni <chris@continuuity.com>
2014-09-17 12:27:33 -0700

    Only symlink if the file doesn't already exist

--- spec/fullstack_spec.rb
+++ spec/fullstack_spec.rb
@@ -12,6 +12,7 @@ describe 'cdap::fullstack' do
         stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
         stub_command('update-alternatives --display hbase-conf | grep best | awk \'{print $5}\' | grep /etc/hbase/conf.chef').and_return(false)
         stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
+        stub_command("test -e /usr/bin/node").and_return(true)
       end.converge(described_recipe)
     end
 
@@ -46,6 +47,27 @@ describe 'cdap::fullstack' do
 
     it 'runs execute[update cdap-conf alternatives]' do
       expect(chef_run).to run_execute('update cdap-conf alternatives')
+    end
+
+    it 'does not create /usr/bin/node link' do
+      expect(chef_run).not_to create_link('/usr/bin/node').with(
+        to: '/usr/local/bin/node'
+      )
+    end
+  end
+  context 'using older nodejs cookbook' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.4) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.default['cdap']['repo']['url'] = 'https://USER:PASS@cdap.repo/path/to/repo'
+        node.default['hadoop']['hdfs_site']['dfs.datanode.max.transfer.threads'] = '4096'
+        node.default['hadoop']['mapred_site']['mapreduce.framework.name'] = 'yarn'
+        stub_command('update-alternatives --display cdap-conf | grep best | awk \'{print $5}\' | grep /etc/cdap/conf.chef').and_return(false)
+        stub_command('update-alternatives --display hadoop-conf | grep best | awk \'{print $5}\' | grep /etc/hadoop/conf.chef').and_return(false)
+        stub_command('update-alternatives --display hbase-conf | grep best | awk \'{print $5}\' | grep /etc/hbase/conf.chef').and_return(false)
+        stub_command('update-alternatives --display hive-conf | grep best | awk \'{print $5}\' | grep /etc/hive/conf.chef').and_return(false)
+        stub_command("test -e /usr/bin/node").and_return(false)
+      end.converge(described_recipe)
     end
 
     it 'creates /usr/bin/node link' do
