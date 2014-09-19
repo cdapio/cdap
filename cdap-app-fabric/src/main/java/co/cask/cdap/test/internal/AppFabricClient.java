2fcafcb8300cd98e04785ba151e119fa4249d57c
Alvin Wang <alvin@continuuity.com>
2014-09-19 11:18:06 -0700

    Added clarifying comment to AppFabricClient unique manifest hack

--- cdap-app-fabric/src/main/java/co/cask/cdap/test/internal/AppFabricClient.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/test/internal/AppFabricClient.java
@@ -278,6 +278,8 @@ public class AppFabricClient {
               jarEntry = new JarEntry(entryName);
             }
 
+            // TODO: this is due to manifest possibly already existing in the jar, but we also
+            // create a manifest programatically so it's possible to have a duplicate entry here
             if ("META-INF/MANIFEST.MF".equalsIgnoreCase(jarEntry.getName())) {
               jarEntry = jarInput.getNextJarEntry();
               continue;
