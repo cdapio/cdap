9bfab05d0ba07a9ceb0ea0342afbafd0d84a3fbb
Chris Gianelloni <chris@continuuity.com>
2014-09-18 18:03:02 -0700

    Version bump to 0.1.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Installs/Configures cdap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 %w(apt hadoop java nodejs ntp yum yum-epel).each do |cb|
   depends cb
