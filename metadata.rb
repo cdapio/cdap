b7846e7fff5234b58d8f4ffe68f86f2868c6b150
Chris Gianelloni <chris@continuuity.com>
2014-09-25 02:05:13 -0700

    Version bump to 0.1.6

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'cdap'
 maintainer       'Cask'
 maintainer_email 'ops@cask.co'
 license          'All rights reserved'
-description      'Installs/Configures cdap'
+description      'Installs/Configures Cask DAP (CDAP)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1.6'
 
 %w(apt hadoop java nodejs ntp yum yum-epel).each do |cb|
   depends cb
