e4e7ff7f305c7bacc3e1d5271a0812115106986a
Alvin Wang <alvin@cask.co>
2014-10-15 12:04:45 -0700

    Fix cdap-cli packaging

--- cdap-cli/bin/cdap-cli.sh
+++ cdap-cli/bin/cdap-cli.sh
@@ -55,10 +55,12 @@ while [ -h "$PRG" ] ; do
     fi
 done
 
+CLASSPATH_COMMON=${lib}/aopalliance.aopalliance-1.0.jar:${lib}/co.cask.cdap.cdap-api-2.5.1.jar:${lib}/co.cask.cdap.cdap-authentication-client-1.0.1.jar:${lib}/co.cask.cdap.cdap-cli-2.5.1.jar:${lib}/co.cask.cdap.cdap-client-2.5.1.jar:${lib}/co.cask.cdap.cdap-common-2.5.1.jar:${lib}/co.cask.cdap.cdap-proto-2.5.1.jar:${lib}/co.cask.tephra.tephra-api-0.3.0.jar:${lib}/com.google.code.findbugs.jsr305-2.0.1.jar:${lib}/com.google.code.gson.gson-2.2.4.jar:${lib}/com.google.guava.guava-13.0.1.jar:${lib}/com.google.inject.extensions.guice-assistedinject-3.0.jar:${lib}/com.google.inject.extensions.guice-multibindings-3.0.jar:${lib}/com.google.inject.guice-3.0.jar:${lib}/com.googlecode.concurrent-trees.concurrent-trees-2.4.0.jar:${lib}/commons-codec.commons-codec-1.6.jar:${lib}/commons-logging.commons-logging-1.1.1.jar:${lib}/javax.inject.javax.inject-1.jar:${lib}/javax.ws.rs.javax.ws.rs-api-2.0.jar:${lib}/jline.jline-2.12.jar:${lib}/org.apache.httpcomponents.httpclient-4.2.5.jar:${lib}/org.apache.httpcomponents.httpcore-4.2.5.jar:${lib}/org.slf4j.slf4j-api-1.7.5.jar:${lib}/org.slf4j.slf4j-nop-1.7.5.jar
+
 if [ "$CLASSPATH" = "" ]; then
-  CLASSPATH=${lib}/co.cask.cdap.cdap-cli-2.5.1.jar
+  CLASSPATH=$CLASSPATH_COMMON
 else
-  CLASSPATH=$CLASSPATH:${lib}/co.cask.cdap.cdap-cli-2.5.1.jar
+  CLASSPATH=$CLASSPATH:$CLASSPATH_COMMON
 fi
 
 # Load the configuration too.
