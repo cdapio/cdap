c7af25d026cb44c9c0cac7e73dba573513c097e1
Chris Gianelloni <chris@cask.co>
2014-10-22 18:38:06 -0700

    Test for yum_repository/apt_repository. 100% test coverage!

---
+++ spec/repo_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'cdap::repo' do
+  context 'on Centos 6.5 x86_64' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+      end.converge(described_recipe)
+    end
+
+    it 'adds cask yum repository' do
+      expect(chef_run).to add_yum_repository('cask')
+    end
+  end
+
+  context 'on Ubuntu 12.04' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'ubuntu', version: 12.04) do |node|
+        node.automatic['domain'] = 'example.com'
+      end.converge(described_recipe)
+    end
+
+    it 'adds cask apt repository' do
+      expect(chef_run).to add_apt_repository('cask')
+    end
+  end
+end
