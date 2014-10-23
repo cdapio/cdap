ee2eea1c2fa3577632d7324b10383f9498dec82b
Chris Gianelloni <chris@cask.co>
2014-10-22 18:39:00 -0700

    Test for both ssl.enabled true and false

--- spec/web_app_init_spec.rb
+++ spec/web_app_init_spec.rb
@@ -4,6 +4,20 @@ describe 'cdap::web_app_init' do
   context 'on Centos 6.5 x86_64' do
     let(:chef_run) do
       ChefSpec::Runner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+        stub_command('test -e /usr/bin/node').and_return(true)
+      end.converge(described_recipe)
+    end
+
+    it 'does not run execute[generate-webapp-ssl-cert]' do
+      expect(chef_run).not_to run_execute('generate-webapp-ssl-cert')
+    end
+
+  end
+
+  context 'with SSL' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(platform: 'centos', version: 6.5) do |node|
         node.override['cdap']['cdap_site']['ssl.enabled'] = true
         stub_command('test -e /usr/bin/node').and_return(true)
       end.converge(described_recipe)
