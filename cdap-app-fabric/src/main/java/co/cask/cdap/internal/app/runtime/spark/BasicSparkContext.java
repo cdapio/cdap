2e9540b2a7b9796ab713b57482e4cc2c7f33a8ae
Rohit Sinha <rsinha@cask.co>
2014-10-24 11:11:35 -0700

    Spark Integration: Fix Jira issue number

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/spark/BasicSparkContext.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/spark/BasicSparkContext.java
@@ -56,7 +56,7 @@ public class BasicSparkContext extends AbstractContext implements SparkContext {
   private static final Pattern SPACES = Pattern.compile("\\s+");
   private static final String[] NO_ARGS = {};
 
-  // TODO: InstanceId is not supported in Spark jobs, see CDAP-2.
+  // TODO: InstanceId is not supported in Spark jobs, see CDAP-39.
   public static final String INSTANCE_ID = "0";
   private final Arguments runtimeArguments;
   private final SparkSpecification sparkSpec;
