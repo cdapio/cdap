3c22bcbb68eaf52d2fd06da895f4140d102541d7
Adrien Cahen <adrien@cask.co>
2014-11-06 10:29:56 -0800

    update title filter

--- app/filters/title.js
+++ app/filters/title.js
@@ -3,12 +3,12 @@
  * intended for use in the <title> tag.
  */
 
-angular.module(PKG.name+'.filters').filter('myTitleFilter', 
+angular.module(PKG.name+'.filters').filter('myTitleFilter',
 function myTitleFilter () {
 
   return function(state) {
     var title = state.data && state.data.title;
-    return (title ? title + ' | ' : '') + 'Coopr';
+    return (title ? title + ' | ' : '') + 'CDAP';
   };
 
 });
