d7d21365de82ad6bd425c9f46a4c59c79c061a58
Taras Yatskiv <tyatskiv@cask.co>
2014-11-19 23:19:25 +0200

    CDAP-538: Ported SparkPageRank app to use services instead of procedures.

--- cdap-examples/SparkPageRank/src/main/java/co/cask/cdap/examples/sparkpagerank/SparkPageRankApp.java
+++ cdap-examples/SparkPageRank/src/main/java/co/cask/cdap/examples/sparkpagerank/SparkPageRankApp.java
@@ -16,16 +16,12 @@
 
 package co.cask.cdap.examples.sparkpagerank;
 
-import co.cask.cdap.api.annotation.Handle;
 import co.cask.cdap.api.annotation.UseDataSet;
 import co.cask.cdap.api.app.AbstractApplication;
 import co.cask.cdap.api.data.stream.Stream;
 import co.cask.cdap.api.dataset.lib.ObjectStore;
 import co.cask.cdap.api.dataset.lib.ObjectStores;
-import co.cask.cdap.api.procedure.AbstractProcedure;
-import co.cask.cdap.api.procedure.ProcedureRequest;
-import co.cask.cdap.api.procedure.ProcedureResponder;
-import co.cask.cdap.api.procedure.ProcedureResponse;
+import co.cask.cdap.api.service.AbstractService;
 import co.cask.cdap.api.service.Service;
 import co.cask.cdap.api.service.http.AbstractHttpServiceHandler;
 import co.cask.cdap.api.service.http.HttpServiceRequest;
@@ -33,28 +29,24 @@ import co.cask.cdap.api.service.http.HttpServiceResponder;
 import co.cask.cdap.api.spark.AbstractSpark;
 import co.cask.cdap.api.spark.SparkSpecification;
 import co.cask.cdap.internal.io.UnsupportedTypeException;
-import com.google.common.base.Charsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.util.Arrays;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 /**
- * Application that calculates page rank of URLs from an input stream.
+ * An Application that calculates page rank of URLs from an input stream.
  */
 public class SparkPageRankApp extends AbstractApplication {
-
   static final String SERVICE_NAME = "GoogleTypePR";
 
   @Override
   public void configure() {
     setName("SparkPageRank");
-    setDescription("Spark page rank app");
+    setDescription("Spark page rank application.");
 
     // Ingest data into the Application via a Stream
     addStream(new Stream("backlinkURLStream"));
@@ -62,8 +54,8 @@ public class SparkPageRankApp extends AbstractApplication {
     // Run a Spark program on the acquired data
     addSpark(new SparkPageRankSpecification());
 
-    // Query the processed data using a Procedure
-    addProcedure(new RanksProcedure());
+    // Retrieve the processed data using a Service
+    addService(new RanksService());
 
     // Service which converts calculated pageranks to Google type page ranks
     addService(SERVICE_NAME, new GoogleTypePRHandler());
@@ -83,7 +75,7 @@ public class SparkPageRankApp extends AbstractApplication {
   /**
    * A Spark program that calculates page rank.
    */
-  public static class SparkPageRankSpecification extends AbstractSpark {
+  public static final class SparkPageRankSpecification extends AbstractSpark {
     @Override
     public SparkSpecification configure() {
       return SparkSpecification.Builder.with()
@@ -95,43 +87,45 @@ public class SparkPageRankApp extends AbstractApplication {
   }
 
   /**
-   * A Procedure that returns rank of the URL.
+   * A {@link Service} that responds with rank of the URL.
    */
-  public static class RanksProcedure extends AbstractProcedure {
+  public static final class RanksService extends AbstractService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(RanksProcedure.class);
+    public static final String SERVICE_NAME = "RanksService";
 
-    // Annotation indicates that ranks dataset is used in the Procedure.
+    @Override
+    protected void configure() {
+      setName(SERVICE_NAME);
+      setDescription("Rank service.");
+      addHandler(new RanksServiceHandler());
+    }
+  }
+
+  /**
+   * Ranks Service handler.
+   */
+  public static final class RanksServiceHandler extends AbstractHttpServiceHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RanksService.class);
+
     @UseDataSet("ranks")
     private ObjectStore<Integer> ranks;
 
-    @Handle("rank")
-    public void getRank(ProcedureRequest request, ProcedureResponder responder)
-      throws IOException, InterruptedException {
+    @Path("rank/{url}")
+    @GET
+    public void getRank(HttpServiceRequest request, HttpServiceResponder responder, @PathParam("url") String url) {
+      LOG.trace("Get rank of URL: {}", url);
 
-      // Get the url from the query parameters.
-      String urlParam = "";
-      for (String key : request.getArguments().keySet()) {
-        if (key.equalsIgnoreCase("url")) {
-          urlParam = request.getArgument(key);
-        }
-      }
-      if (urlParam.isEmpty()) {
-        responder.error(ProcedureResponse.Code.CLIENT_ERROR, "URL must be given as argument");
-      }
-      byte[] url = urlParam.getBytes(Charsets.UTF_8);
-
-      // Get the rank from the ranks dataset.
+      // Get the rank from the ranks dataset
       Integer rank = ranks.read(url);
       if (rank == null) {
-        responder.error(ProcedureResponse.Code.NOT_FOUND, "No rank found for " + urlParam);
+        LOG.trace("No rank found");
+        responder.sendError(HttpURLConnection.HTTP_NOT_FOUND, String.format("No rank found of %s", url));
         return;
       }
 
-      LOG.trace("Key: {}, Data: {}", Arrays.toString(url), rank);
-
-      // Send response in JSON format.
-      responder.sendJson(String.valueOf(rank));
+      LOG.trace("Returned rank: {}", rank);
+      responder.sendJson(rank);
     }
   }
 
