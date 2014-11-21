590344abd0f16f6174e0546cb34f30024c6c5fc0
Bhooshan Mogal <bhooshanmogal@Bhooshans-MacBook-Pro.local>
2014-11-21 15:14:54 -0800

    [CDAP-505] Added ability to infer IdentityStreamEventDecoder in StreamInputFormat. Updated SparkPageRankProgram to use IdentityStreamEventDecoder

--- cdap-data-fabric/src/main/java/co/cask/cdap/data/stream/StreamInputFormat.java
+++ cdap-data-fabric/src/main/java/co/cask/cdap/data/stream/StreamInputFormat.java
@@ -15,6 +15,7 @@
  */
 package co.cask.cdap.data.stream;
 
+import co.cask.cdap.api.flow.flowlet.StreamEvent;
 import co.cask.cdap.api.stream.StreamEventDecoder;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
@@ -232,6 +233,8 @@ public class StreamInputFormat<K, V> extends InputFormat<K, V> {
       setDecoderClassName(conf, TextStreamEventDecoder.class.getName());
     } else if (BytesWritable.class.equals(vClass)) {
       setDecoderClassName(conf, BytesStreamEventDecoder.class.getName());
+    } else if (StreamEvent.class.equals(vClass)) {
+      setDecoderClassName(conf, IdentityStreamEventDecoder.class.getName());
     } else {
       throw new IllegalArgumentException("The value class must be of type BytesWritable or Text if no decoder type " +
                                            "is provided");
