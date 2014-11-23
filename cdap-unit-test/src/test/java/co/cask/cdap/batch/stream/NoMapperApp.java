c85c1d6b055c2a206df812784abdb30dc0ef7799
Bhooshan Mogal <bhooshanmogal@Bhooshans-MacBook-Pro.local>
2014-11-22 20:38:13 -0800

    [CDAP-505] Reverted accidental stdout checkin

--- cdap-unit-test/src/test/java/co/cask/cdap/batch/stream/NoMapperApp.java
+++ cdap-unit-test/src/test/java/co/cask/cdap/batch/stream/NoMapperApp.java
@@ -35,7 +35,6 @@ public class NoMapperApp extends AbstractApplication {
 
   @Override
   public void configure() {
-    System.out.println("####### NoMapper");
     addStream(new Stream("nomapper"));
     createDataset("results", KeyValueTable.class);
     addMapReduce(new NoMapperMapReduce());
