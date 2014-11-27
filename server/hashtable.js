099734dd06e01c4ede33fd4966d08256eb1aec71
Adrien Cahen <adrien@cask.co>
2014-11-26 20:31:16 -0800

    add comment explaining hashtable patch

--- server/hashtable.js
+++ server/hashtable.js
@@ -1,9 +1,14 @@
 /*global require, module */
+/**
+ * HashTable from object-hash module,
+ *    patched to allow removing objects from the table.
+ *
+ * see https://github.com/puleos/object-hash/pull/12
+ */
 
 var hash = require('object-hash'),
     HashTable = hash.HashTable;
 
-// https://github.com/puleos/object-hash/pull/12
 HashTable.prototype.remove = function (obj) {
   var key = hash(obj),
       count = this.getCount(key);
