691a6464bb848d641c4cbf10783a2c5856925d1b
Terence Yim <terence@cask.co>
2014-12-05 13:57:42 -0800

    (CDAP-834) Reuse exploded jar directory when possible.

--- cdap-data-fabric/src/main/java/co/cask/cdap/data2/datafabric/dataset/type/DistributedDatasetTypeClassLoaderFactory.java
+++ cdap-data-fabric/src/main/java/co/cask/cdap/data2/datafabric/dataset/type/DistributedDatasetTypeClassLoaderFactory.java
@@ -16,22 +16,72 @@
 
 package co.cask.cdap.data2.datafabric.dataset.type;
 
+import co.cask.cdap.common.io.Locations;
 import co.cask.cdap.common.lang.DirectoryClassLoader;
 import co.cask.cdap.common.lang.jar.BundleJarUtil;
 import co.cask.cdap.proto.DatasetModuleMeta;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.hash.HashCodes;
+import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.apache.twill.filesystem.Location;
 import org.apache.twill.filesystem.LocationFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.Set;
 
 /**
  * Creates a {@link ClassLoader} for a {@link DatasetModuleMeta} by unpacking the moduleMeta jar,
  * since the moduleMeta jar may not be present in the local filesystem.
  */
+@Singleton
 public class DistributedDatasetTypeClassLoaderFactory implements DatasetTypeClassLoaderFactory {
 
+  private static final Logger LOG = LoggerFactory.getLogger(DistributedDatasetTypeClassLoaderFactory.class);
+
+  private static final ThreadLocal<MessageDigest> MESSAGE_DIGEST = new ThreadLocal<MessageDigest>() {
+    @Override
+    protected MessageDigest initialValue() {
+      try {
+        // Prefer MD5
+        return MessageDigest.getInstance("MD5");
+      } catch (NoSuchAlgorithmException e) {
+        // Doesn't expect it to fail. If really do, pick the first provider available.
+        Set<String> algorithms = ImmutableSortedSet.copyOf(Security.getAlgorithms("MessageDigest"));
+        if (algorithms.isEmpty()) {
+          throw new IllegalStateException("No MessageDigest algorithm available.");
+        }
+
+        try {
+          String algorithm = algorithms.iterator().next();
+          LOG.warn("Failed to get MD5 MessageDigest, use {} instead", algorithm);
+          return MessageDigest.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException ex) {
+          // This shouldn't happen
+          throw Throwables.propagate(ex);
+        }
+      }
+    }
+
+    @Override
+    public MessageDigest get() {
+      // Reset the MD before returning
+      MessageDigest md = super.get();
+      md.reset();
+      return md;
+    }
+  };
   private final LocationFactory locationFactory;
 
   @Inject
@@ -46,9 +96,30 @@ public class DistributedDatasetTypeClassLoaderFactory implements DatasetTypeClas
     }
 
     // In distributed mode, Dataset ClassLoader is a URLClassLoader with all the jars inside the dataset.jar.
-    // Creating tempDir is fine since it will be created inside a YARN container and cleaned up.
-    File tempDir = Files.createTempDir();
-    BundleJarUtil.unpackProgramJar(locationFactory.create(moduleMeta.getJarLocation()), tempDir);
-    return new DirectoryClassLoader(tempDir, parentClassLoader, "lib");
+
+    // Copy the jar from remote location to a local temp file, and compute checksum while copying
+    Location jarLocation = locationFactory.create(moduleMeta.getJarLocation());
+    File tmpJar = File.createTempFile(jarLocation.getName(), null);
+    try {
+      MessageDigest messageDigest = MESSAGE_DIGEST.get();
+      DigestOutputStream digestOutput = new DigestOutputStream(new FileOutputStream(tmpJar), messageDigest);
+      try {
+        ByteStreams.copy(Locations.newInputSupplier(jarLocation), digestOutput);
+      } finally {
+        digestOutput.close();
+      }
+
+      // The folder name to expand to is formed by the module name and the checksum.
+      String dirName = String.format("%s.%s", moduleMeta.getName(), HashCodes.fromBytes(messageDigest.digest()));
+      File expandDir = new File(tmpJar.getParent(), dirName);
+      if (!expandDir.exists()) {
+        // Expand the jar
+        BundleJarUtil.unpackProgramJar(Files.newInputStreamSupplier(tmpJar), expandDir);
+      }
+
+      return new DirectoryClassLoader(expandDir, parentClassLoader, "lib");
+    } finally {
+      tmpJar.delete();
+    }
   }
 }
