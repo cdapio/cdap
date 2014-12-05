0b62643ccd5f5e25eb8aa532d98be3d9c5749b83
Taras Yatskiv <tyatskiv@cask.co>
2014-12-05 17:23:53 +0200

    CDAP-735: Updated visualization of Spark job metrics.

--- cdap-web-app/client/core/controllers/spark-status.js
+++ cdap-web-app/client/core/controllers/spark-status.js
@@ -9,7 +9,7 @@ define([], function () {
         __STATUS_UPDATE_TIMEOUT: 1000,
         showLogsMessage: false,
         __updateStatusTimeout: null,
-        __updateStatus: function(appName, jobName) {
+        __updateStatus: function (appName, jobName) {
             var model = this.get('model');
 
             this.HTTP.rest('apps', appName, 'spark', jobName, 'status', function (response) {
@@ -28,19 +28,17 @@ define([], function () {
             var model = this.get('model');
             var self = this;
 
-            var runStatusUpdate = function() {
-                self.__updateStatusTimeout = setTimeout(function () {
-                    self.__updateStatus(model.app, model.name);
-                    self.__updateMetrics();
-                    runStatusUpdate();
-                }, self.__STATUS_UPDATE_TIMEOUT);
+            var runPageUpdate = function () {
+                self.__updateStatus(model.app, model.name);
+                self.__updateMetrics();
             };
 
-            runStatusUpdate();
+            this.__pageUpdateInterval = setInterval(runPageUpdate, this.__STATUS_UPDATE_TIMEOUT);
+            runPageUpdate();
         },
 
         unload: function () {
-            clearTimeout(this.__updateStatusTimeout);
+            clearInterval(this.__pageUpdateInterval);
         },
 
         /**
@@ -66,15 +64,11 @@ define([], function () {
 
         },
 
-        onCurrentStatusChanged: function() {
-          if (this.get("model").get("currentState") === "RUNNING") {
-            this.set("showLogsMessage", true);
-          }
-        }.observes("model.currentState"),
-
-        logsLinkHandler: function() {
-          this.transitionToRoute("Spark.Log");
-        }
+        onCurrentStatusChanged: function () {
+            if (this.get("model").get("currentState") === "RUNNING") {
+                this.set("showLogsMessage", true);
+            }
+        }.observes("model.currentState")
     });
 
     Controller.reopenClass({
