23df5cb61d278b20688abfe52270fa7501d29fb4
Albert Shau <albert@continuuity.com>
2014-12-10 12:23:16 -0800

    remove comment as well.

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceRuntimeService.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceRuntimeService.java
@@ -574,7 +574,6 @@ final class MapReduceRuntimeService extends AbstractExecutionThreadService {
     // Excludes libraries that are for sure not needed.
     // Hadoop - Available from the cluster
     // Spark - MR never uses Spark
-    // Fastutil - 16MB library that only used in tehpra server
     ApplicationBundler appBundler = new ApplicationBundler(ImmutableList.of("org.apache.hadoop",
                                                                             "org.apache.spark"),
                                                            ImmutableList.of("org.apache.hadoop.hbase",
