f8daf494aac6772d98417e4ca75bd22e475807e5
Adrien Cahen <adrien@cask.co>
2014-12-11 21:51:20 -0800

    get rid of searchbar

--- app/directives/navbar/navbar.js
+++ app/directives/navbar/navbar.js
@@ -3,14 +3,25 @@
  */
 
 angular.module(PKG.name+'.commons').directive('myNavbar',
-function myNavbarDirective ($dropdown, myAuth, caskTheme, MY_CONFIG) {
+function myNavbarDirective ($dropdown, $alert, myAuth, caskTheme, MY_CONFIG) {
   return {
     restrict: 'A',
     templateUrl: 'navbar/navbar.html',
 
     link: function (scope, element, attrs) {
 
-      $dropdown(angular.element(element[0].querySelector('a.dropdown-toggle')), {
+      scope.currentNamespace = "Namespace 1";
+
+      var toggles = element[0].querySelectorAll('a.dropdown-toggle');
+
+      // namespace dropdown
+      $dropdown(angular.element(toggles[0]), {
+        template: 'navbar/namespace.html',
+        animation: 'am-flip-x'
+      });
+
+      // right dropdown
+      $dropdown(angular.element(toggles[1]), {
         template: 'navbar/dropdown.html',
         animation: 'am-flip-x',
         placement: 'bottom-right',
@@ -18,9 +29,7 @@ function myNavbarDirective ($dropdown, myAuth, caskTheme, MY_CONFIG) {
       });
 
       scope.logout = myAuth.logout;
-
       scope.theme = caskTheme;
-
       scope.securityEnabled = MY_CONFIG.securityEnabled;
 
       scope.navbarLinks = [
@@ -29,6 +38,14 @@ function myNavbarDirective ($dropdown, myAuth, caskTheme, MY_CONFIG) {
         { sref: 'admin',      label: 'Admin'        }
       ];
 
+
+      scope.doSearch = function () {
+        $alert({
+          title: 'Sorry!',
+          content: 'Search is not yet implemented.',
+          type: 'danger'
+        });
+      };
     }
   };
 });
