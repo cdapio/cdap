1fa457f19fc8ee0b84caa314ec73022d4c946b06
Adrien Cahen <adrien@cask.co>
2014-12-14 20:21:02 -0800

    fix quotes on home-ctrl

--- app/features/home/home-ctrl.js
+++ app/features/home/home-ctrl.js
@@ -5,13 +5,16 @@
 angular.module(PKG.name+'.feature.home').controller('HomeCtrl',
 function ($scope, $alert, MyDataSource) {
 
-  var dataSrc = new MyDataSource($scope);
   $scope.apps = [];
+
+  var dataSrc = new MyDataSource($scope);
+
   dataSrc.fetch({
-    _cdap: "GET /apps/"
+    _cdap: 'GET /apps/'
   }, function(res) {
+
     $scope.apps = res;
-    console.log("Apps: ", $scope.apps);
+
   });
 
 });
