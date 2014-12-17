1b03644bc12942f91e0b43e1333d0a1f87a501e4
Gary Helmling <gary@cask.co>
2014-12-17 13:23:04 -0800

    Refactor and simplify internal processing of increments

--- cdap-hbase-compat-0.98/src/main/java/co/cask/cdap/data2/increment/hbase98/IncrementSummingScanner.java
+++ cdap-hbase-compat-0.98/src/main/java/co/cask/cdap/data2/increment/hbase98/IncrementSummingScanner.java
@@ -16,6 +16,7 @@
 
 package co.cask.cdap.data2.increment.hbase98;
 
+import com.google.common.base.Preconditions;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.Cell;
@@ -28,6 +29,8 @@ import org.apache.hadoop.hbase.regionserver.RegionScanner;
 import org.apache.hadoop.hbase.regionserver.ScanType;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import javax.annotation.Nullable;
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -40,26 +43,25 @@ class IncrementSummingScanner implements RegionScanner {
   private static final Log LOG = LogFactory.getLog(IncrementSummingScanner.class);
 
   private final HRegion region;
-  private final InternalScanner baseScanner;
+  private final WrappedScanner baseScanner;
   private RegionScanner baseRegionScanner;
   private final int batchSize;
   private final ScanType scanType;
   // Highest timestamp, beyond which we cannot aggregate increments during flush and compaction.
   // Increments newer than this may still be visible to running transactions
   private final long compactionUpperBound;
-  private List<Cell> cellsToConsume = new ArrayList<Cell>();
 
-  IncrementSummingScanner(HRegion region, int batchSize, InternalScanner baseScanner, ScanType scanType) {
-    this(region, batchSize, baseScanner, scanType, Long.MAX_VALUE);
+  IncrementSummingScanner(HRegion region, int batchSize, InternalScanner internalScanner, ScanType scanType) {
+    this(region, batchSize, internalScanner, scanType, Long.MAX_VALUE);
   }
 
-  IncrementSummingScanner(HRegion region, int batchSize, InternalScanner baseScanner, ScanType scanType,
+  IncrementSummingScanner(HRegion region, int batchSize, InternalScanner internalScanner, ScanType scanType,
                           long compationUpperBound) {
     this.region = region;
     this.batchSize = batchSize;
-    this.baseScanner = baseScanner;
-    if (baseScanner instanceof RegionScanner) {
-      this.baseRegionScanner = (RegionScanner) baseScanner;
+    this.baseScanner = new WrappedScanner(internalScanner);
+    if (internalScanner instanceof RegionScanner) {
+      this.baseRegionScanner = (RegionScanner) internalScanner;
     }
     this.scanType = scanType;
     this.compactionUpperBound = compationUpperBound;
@@ -133,86 +135,90 @@ class IncrementSummingScanner implements RegionScanner {
     }
     Cell previousIncrement = null;
     long runningSum = 0;
-    boolean hasMore = true;
     int addedCnt = 0;
-    do {
-      // 1. fill up results to consume, if needed
-      if (cellsToConsume.isEmpty()) {
-        hasMore = baseScanner.next(cellsToConsume, limit);
+    baseScanner.startNext();
+    Cell cell = null;
+    while ((cell = baseScanner.peekNextCell(limit)) != null && (limit <= 0 || addedCnt < limit)) {
+      if (LOG.isTraceEnabled()) {
+        LOG.trace("Checking cell " + cell);
       }
-      // compact any delta writes
-      for (int i = 0; i < cellsToConsume.size(); i++) {
-        Cell cell = cellsToConsume.get(i);
-        // 2. check if we've hit the limit for results
-        if (limit > 0 && addedCnt >= limit) {
-          // haven't reached the end of current cells, so hasMore is true
-          cellsToConsume = cellsToConsume.subList(i, cellsToConsume.size() - 1);
-          return true;
+      // any cells visible to in-progress transactions must be kept unchanged
+      if (cell.getTimestamp() > compactionUpperBound) {
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("Including cell visible to in-progress, cell=" + cell);
         }
+        cells.add(cell);
+        addedCnt++;
+        baseScanner.nextCell(limit);
+        continue;
+      }
 
-        // 3. if this is an increment
-        if (IncrementHandler.isIncrement(cell) && cell.getTimestamp() < compactionUpperBound) {
-          if (LOG.isTraceEnabled()) {
-            LOG.trace("Found increment for row=" + Bytes.toStringBinary(CellUtil.cloneRow(cell)) + ", " +
-                "column=" + Bytes.toStringBinary(CellUtil.cloneQualifier(cell)));
-          }
-          if (!sameCell(previousIncrement, cell)) {
-            if (previousIncrement != null) {
-              // 3a. if different qualifier, and prev qualifier non-null
-              // 3ai. emit the previous sum
-              if (LOG.isTraceEnabled()) {
-                LOG.trace("Including increment: sum=" + runningSum + ", cell=" + previousIncrement);
-              }
-              cells.add(newCell(previousIncrement, runningSum));
-              addedCnt++;
-            }
-            previousIncrement = cell;
-            runningSum = 0;
-          }
-          // add this increment to the tally
-          runningSum += Bytes.toLong(cell.getValueArray(),
-              cell.getValueOffset() + IncrementHandler.DELTA_MAGIC_PREFIX.length);
-        } else {
-          // 4. otherwise (not an increment or not aggregatable)
+      // compact any delta writes
+      if (IncrementHandler.isIncrement(cell)) {
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("Found increment for row=" + Bytes.toStringBinary(CellUtil.cloneRow(cell)) + ", " +
+              "column=" + Bytes.toStringBinary(CellUtil.cloneQualifier(cell)));
+        }
+        if (!sameCell(previousIncrement, cell)) {
           if (previousIncrement != null) {
-            boolean skipCurrent = false;
-            if (sameCell(previousIncrement, cell)) {
-              // 4a. if qualifier matches previous and this is a long, add to running sum, emit
-              runningSum += Bytes.toLong(cell.getValueArray(), cell.getValueOffset());
-              skipCurrent = true;
-            }
+            // if different qualifier, and prev qualifier non-null
+            // emit the previous sum
             if (LOG.isTraceEnabled()) {
               LOG.trace("Including increment: sum=" + runningSum + ", cell=" + previousIncrement);
             }
             cells.add(newCell(previousIncrement, runningSum));
-            addedCnt++;
             previousIncrement = null;
-            runningSum = 0;
-
-            if (skipCurrent) {
-              continue;
-            }
+            addedCnt++;
+            // continue without advancing, current cell will be consumed on the next iteration
+            continue;
           }
-          // 4b. otherwise emit the current cell
-          if (LOG.isTraceEnabled()) {
-            LOG.trace("Including raw cell: " + cell);
-          }
-          cells.add(cell);
-          addedCnt++;
+          previousIncrement = cell;
+          runningSum = 0;
         }
+        // add this increment to the tally
+        runningSum += Bytes.toLong(cell.getValueArray(),
+            cell.getValueOffset() + IncrementHandler.DELTA_MAGIC_PREFIX.length);
+      } else {
+        // otherwise (not an increment)
+        if (previousIncrement != null) {
+          if (sameCell(previousIncrement, cell)) {
+            // if qualifier matches previous and this is a long, add to running sum, emit
+            runningSum += Bytes.toLong(cell.getValueArray(), cell.getValueOffset());
+            // this cell already processed as part of the previous increment
+            baseScanner.nextCell(limit);
+          }
+          if (LOG.isTraceEnabled()) {
+            LOG.trace("Including increment: sum=" + runningSum + ", cell=" + previousIncrement);
+          }
+          cells.add(newCell(previousIncrement, runningSum));
+          addedCnt++;
+          previousIncrement = null;
+          runningSum = 0;
+
+          continue;
+        }
+        // otherwise emit the current cell
+        if (LOG.isTraceEnabled()) {
+          LOG.trace("Including raw cell: " + cell);
+        }
+
+        cells.add(cell);
+        addedCnt++;
       }
-      // if we hit the limit before reaching the end of current results, we would have returned above (step 2)
-      cellsToConsume.clear();
-    } while (hasMore && limit > 0 && addedCnt < limit);
+      // if we made it this far, consume the current cell
+      baseScanner.nextCell(limit);
+    }
     // emit any left over increment, if we hit the end
     if (previousIncrement != null) {
+      // in any situation where we exited due to limit, previousIncrement should already be null
+      Preconditions.checkState(limit <= 0 || addedCnt < limit);
       if (LOG.isTraceEnabled()) {
         LOG.trace("Including leftover increment: sum=" + runningSum + ", cell=" + previousIncrement);
       }
       cells.add(newCell(previousIncrement, runningSum));
     }
 
-    return hasMore;
+    return baseScanner.hasMore();
   }
 
   private boolean sameCell(Cell first, Cell second) {
@@ -240,5 +246,82 @@ class IncrementSummingScanner implements RegionScanner {
   @Override
   public void close() throws IOException {
     baseScanner.close();
+  }
+
+  private static class WrappedScanner implements Closeable {
+    private boolean hasMore;
+    private byte[] currentRow;
+    private List<Cell> cellsToConsume = new ArrayList<Cell>();
+    private int currentIdx;
+    private final InternalScanner scanner;
+
+    public WrappedScanner(InternalScanner scanner) {
+      this.scanner = scanner;
+    }
+
+    /**
+     * Called to signal the start of the next() call by the scanner.
+     */
+    public void startNext() {
+      currentRow = null;
+      // signals the start of the next call
+    }
+
+    /**
+     * Returns the next available cell for the current row, without advancing the pointer.  Calling this method
+     * multiple times in a row will continue to return the same cell.
+     *
+     * @param limit the limit of number of cells to return if the next batch must be fetched by the wrapped scanner
+     * @return the next available cell or null if no more cells are available for the current row
+     * @throws IOException
+     */
+    public @Nullable Cell peekNextCell(int limit) throws IOException {
+      if (currentIdx >= cellsToConsume.size()) {
+        // finished current batch
+        cellsToConsume.clear();
+        currentIdx = 0;
+        hasMore = scanner.next(cellsToConsume, limit);
+      }
+      Cell cell = null;
+      if (currentIdx < cellsToConsume.size()) {
+        cell = cellsToConsume.get(currentIdx);
+        if (currentRow == null) {
+          currentRow = CellUtil.cloneRow(cell);
+        } else if (!CellUtil.matchingRow(cell, currentRow)) {
+          // moved on to the next row
+          // don't consume current cell and signal no more cells for this row
+          return null;
+        }
+      }
+      return cell;
+    }
+
+    /**
+     * Returns the next available cell for the current row and advances the pointer to the next cell.  This method
+     * can be called multiple times in a row to advance through all the available cells.
+     *
+     * @param limit the limit of number of cells to return if the next batch must be fetched by the wrapped scanner
+     * @return the next available cell or null if no more cells are available for the current row
+     * @throws IOException
+     */
+    public @Nullable Cell nextCell(int limit) throws IOException {
+      Cell cell = peekNextCell(limit);
+      if (cell != null) {
+        currentIdx++;
+      }
+      return cell;
+    }
+
+    /**
+     * Returns whether or not the underlying scanner has more rows.
+     */
+    public boolean hasMore() {
+      return currentIdx < cellsToConsume.size() ? true : hasMore;
+    }
+
+    @Override
+    public void close() throws IOException {
+      scanner.close();
+    }
   }
 }
