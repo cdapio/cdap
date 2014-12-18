cc5939a3c3711ec2038e80cb63e7789c91b83648
Bhooshan Mogal <bhooshan@cask.co>
2014-12-18 11:14:29 -0800

    [CDAP-1057] Improved type check so type can be reused

--- cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/ProgramLifecycleHttpHandler.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/ProgramLifecycleHttpHandler.java
@@ -523,12 +523,13 @@ public class ProgramLifecycleHttpHandler extends AbstractAppFabricHttpHandler {
                                @PathParam("namespace-id") String namespaceId,
                                @PathParam("app-id") String appId,
                                @PathParam("program-category") String programCategory) {
-    if (!isValidProgramType(programCategory)) {
+    ProgramType type = getProgramType(programCategory);
+    if (type == null) {
       responder.sendString(HttpResponseStatus.METHOD_NOT_ALLOWED, String.format("Program type '%s' not supported",
                                                                                 programCategory));
       return;
     }
-    programList(responder, namespaceId, ProgramType.valueOfCategoryName(programCategory), appId, store);
+    programList(responder, namespaceId, type, appId, store);
   }
 
   /**
@@ -1241,12 +1242,12 @@ public class ProgramLifecycleHttpHandler extends AbstractAppFabricHttpHandler {
     return EnumSet.of(ProgramType.FLOW, ProgramType.SERVICE, ProgramType.PROCEDURE).contains(programType);
   }
 
-  private boolean isValidProgramType(String programType) {
+  @Nullable
+  private ProgramType getProgramType(String programType) {
     try {
-      ProgramType.valueOfCategoryName(programType);
-      return true;
+      return ProgramType.valueOfCategoryName(programType);
     } catch (Exception e) {
-      return false;
+      return null;
     }
   }
 }
