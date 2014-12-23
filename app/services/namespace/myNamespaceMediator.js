65edcce690c28736a543bb0cab7f7e81c0052c78
Ajai <ajainarayanan@gmail.com>
2014-12-22 17:59:16 -0800

    Modifies myNamespaceMediator to use single promise

--- app/services/namespace/myNamespaceMediator.js
+++ app/services/namespace/myNamespaceMediator.js
@@ -2,45 +2,32 @@ angular.module(PKG.name + '.services')
   .service('myNamespaceMediator', function myNameSpaceMediator($q) {
     this.currentNamespace = null;
     this.namespaceList = [];
-    this.defferedList = [];
-    this.deferredNsList = [];
-    this.subscribers = [];
+    var currentNsdeferred = $q.defer(),
+        nsListdeferred = $q.defer();;
 
     this.setCurrentNamespace = function(ns) {
       this.currentNamespace = ns;
-      angular.forEach(this.defferedList, function(deferred) {
-        deferred.resolve(this.currentNamespace);
-      }.bind(this));
-      this.defferedList = [];
+      currentNsdeferred.resolve(this.currentNamespace);
     };
 
     this.setNamespaceList = function (nsList) {
       this.namespaceList = nsList;
-      angular.forEach(this.deferredNsList, function(deferred) {
-        deferred.resolve(this.namespaceList);
-      });
-      this.deferredNsList = [];
       this.setCurrentNamespace(nsList[0]);
+      nsListdeferred.resolve(this.namespaceList);
     };
 
     this.getCurrentNamespace = function() {
-      var deferred = $q.defer();
       if (this.currentNamespace) {
-        deferred.resolve(this.currentNamespace);
-      } else {
-        this.defferedList.push(deferred);
+        currentNsdeferred.resolve(this.currentNamespace);
       }
-      return deferred.promise;
+      return currentNsdeferred.promise;
     };
 
     this.getNamespaceList = function() {
-      var deferred = $q.defer();
       if (this.namespaceList.length > 0) {
-        deferred.resolve(this.namespaceList);
-      } else {
-        this.deferredNsList.push(deferred);
+        nsListdeferred.resolve(this.namespaceList);
       }
-      return deferred.promise;
+      return nsListdeferred.promise;
     };
 
   });
