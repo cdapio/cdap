04ef0a54a55d8e74b08b259901d6d6202682e750
Ajai <ajainarayanan@gmail.com>
2014-12-26 12:44:43 -0800

    Updates finally clause of myAuth.login in login feature

--- app/features/login/login-ctrl.js
+++ app/features/login/login-ctrl.js
@@ -3,7 +3,7 @@
  */
 
 angular.module(PKG.name+'.feature.login').controller('LoginCtrl',
-function ($scope, myAuth, $alert, $state, cfpLoadingBar, $timeout, MYAUTH_EVENT, MY_CONFIG, caskFocusManager, myNamespace) {
+function ($scope, myAuth, $alert, $state, cfpLoadingBar, $timeout, MYAUTH_EVENT, MY_CONFIG, caskFocusManager, myNamespace, stateParamsCache) {
 
   $scope.credentials = myAuth.remembered();
 
@@ -12,38 +12,8 @@ function ($scope, myAuth, $alert, $state, cfpLoadingBar, $timeout, MYAUTH_EVENT,
   $scope.doLogin = function (c) {
     $scope.submitting = true;
     cfpLoadingBar.start();
-
     myAuth.login(c)
       .finally(function(){
-        // Get state params used in the previous state (if any)
-        var params = stateParamsCache.getCache();
-        var isParams = Object.keys(params).length;
-
-        // If you are already on a namespace(apps/data view) then use it to navigate to the overview page.
-        if ($state.params.namespaceId) {
-          $state.go('ns.overview', {namespaceId: $state.params.namespaceId});
-        } else if (isParams){
-
-          // Transition from /login?next state to a state that you were previously in.
-          if ($state.params.next && isParams) {
-            $state.go(next, params);
-          }
-
-        } else {
-
-          // Logging in for the firt time (no history of previous state or any selected namespaces)
-          // So get the list and select the default option.
-          myNamespace.getList()
-            .then(function(list) {
-              $state.go('ns.overview', {namespaceId: list[0].name});
-            });
-
-        }
-        $alert({
-          title:'Welcome!',
-          content:'You\'re logged in!',
-          type:'success'
-        });
         $scope.submitting = false;
         cfpLoadingBar.complete();
       });
