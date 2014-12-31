ff753f8dd3afcd65664fb1cd974f7e1367961080
Ajai <ajainarayanan@gmail.com>
2014-12-31 15:32:03 -0800

    Fixes admin feature based on routing changes

--- app/features/admin/controllers/AdminController.js
+++ app/features/admin/controllers/AdminController.js
@@ -1,40 +1,38 @@
 angular.module(PKG.name + '.feature.admin')
   .controller('AdminController', function ($scope, $state, myNamespace) {
-    $scope.toggleMenu = function(menu) {
-      menu.showSubMenu = !menu.showSubMenu;
-    };
+
     myNamespace.getList()
       .then(function(list) {
-        debugger;
         $scope.nsList = list.map(generateNsObject);
       });
+
     function generateNsObject(item) {
       return {
-        state: 'admin.namespace({nsId: "' + item.displayName +'" })',
+        state: '',
         label: item.displayName,
         children: [
           {
-            state: 'admin.namespace.settings({nsId: "' + item.displayName +'" })',
+            state: 'admin.namespace.detail.settings({nsadmin: "' + item.displayName +'" })',
             label: 'Settings',
             children: []
           },
           {
-            state: 'admin.namespace.users({nsId: "' + item.displayName +'" })',
+            state: 'admin.namespace.detail.users({nsadmin: "' + item.displayName +'" })',
             label: 'Users',
             children: []
           },
           {
-            state: 'admin.namespace.datatypes({nsId: "' + item.displayName +'" })',
+            state: 'admin.namespace.detail.datatypes({nsadmin: "' + item.displayName +'" })',
             label: 'Data Types',
             children: []
           },
           {
-            state: 'admin.namespace.datasets({nsId: "' + item.displayName +'" })',
+            state: 'admin.namespace.detail.datasets({nsadmin: "' + item.displayName +'" })',
             label: 'Datasets',
             children: []
           },
           {
-            state: 'admin.namespace.apps({nsId: "' + item.displayName +'" })',
+            state: 'admin.namespace.detail.apps({nsadmin: "' + item.displayName +'" })',
             label: 'Apps',
             children: []
           }
--- app/features/admin/routes.js
+++ app/features/admin/routes.js
@@ -5,55 +5,67 @@ angular.module(PKG.name + '.feature.admin')
       .state('admin', {
         abstract: true,
         data: {
-          authorizedRoles: MYAUTH_ROLE.all
-        },
-        url: '/admin',
-        data: {
           authorizedRoles: MYAUTH_ROLE.all,
           highlightTab: 'management'
         },
+        url: '/admin',
         templateUrl: '/assets/features/admin/templates/admin.html',
         controller: 'AdminController'
       })
+
         .state('admin.overview', {
           url: '',
           templateUrl: '/assets/features/admin/templates/overview.html'
         })
+
         .state('admin.system', {
           url: '/system',
           templateUrl: '/assets/features/admin/templates/system.html'
         })
+
         .state('admin.security', {
           url: '/security',
           templateUrl: '/assets/features/admin/templates/security.html'
         })
-        .state('admin.nscreate', {
-          url: '/namespace/create',
-          templateUrl: '/assets/features/admin/templates/namespace/create.html',
-          controller: 'NamespaceCreateController'
-        })
+
         .state('admin.namespace', {
-          url: '/namespace/edit/:namespace',
-          templateUrl: '/assets/features/admin/templates/namespace.html'
+          abstract: true,
+          url: '/namespace',
+          template: '<ui-view/>'
         })
-          .state('admin.namespace.settings', {
-            url: '/settings',
-            templateUrl: '/assets/features/admin/templates/namespace/settings.html'
+          .state('admin.namespace.create', {
+            url: '/create',
+            templateUrl: '/assets/features/admin/templates/namespace/create.html',
+            controller: 'NamespaceCreateController'
           })
-          .state('admin.namespace.users', {
-            url: '/users',
-            templateUrl: '/assets/features/admin/templates/namespace/users.html'
+
+          .state('admin.namespace.detail', {
+            url: '/detail/:nsadmin',
+            templateUrl: '/assets/features/admin/templates/namespace.html'
           })
-          .state('admin.namespace.datatypes', {
-            url: '/datatypes',
-            templateUrl: '/assets/features/admin/templates/namespace/datatypes.html'
-          })
-          .state('admin.namespace.datasets', {
-            url: '/datasets',
-            templateUrl: '/assets/features/admin/templates/namespace/datasets.html'
-          })
-          .state('admin.namespace.apps', {
-            url: '/apps',
-            templateUrl: '/assets/features/admin/templates/namespace/apps.html'
-          });
+
+            .state('admin.namespace.detail.settings', {
+              url: '/settings',
+              templateUrl: '/assets/features/admin/templates/namespace/settings.html'
+            })
+
+            .state('admin.namespace.detail.users', {
+              url: '/users',
+              templateUrl: '/assets/features/admin/templates/namespace/users.html'
+            })
+
+            .state('admin.namespace.detail.datatypes', {
+              url: '/datatypes',
+              templateUrl: '/assets/features/admin/templates/namespace/datatypes.html'
+            })
+
+            .state('admin.namespace.detail.datasets', {
+              url: '/datasets',
+              templateUrl: '/assets/features/admin/templates/namespace/datasets.html'
+            })
+
+            .state('admin.namespace.detail.apps', {
+              url: '/apps',
+              templateUrl: '/assets/features/admin/templates/namespace/apps.html'
+            });
   });
