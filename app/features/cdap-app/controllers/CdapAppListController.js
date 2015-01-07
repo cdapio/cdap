629c729b632830cf6796a10dbf8bf3fe1f252b25
Ajai <ajainarayanan@gmail.com>
2015-01-06 17:55:34 -0800

    Implements file upload in apps list view
    Conflicts:
    	app/features/cdap-app/controllers/CdapAppListController.js
    	app/features/cdap-app/templates/list.html

--- app/features/cdap-app/controllers/CdapAppListController.js
+++ app/features/cdap-app/controllers/CdapAppListController.js
@@ -1,6 +1,5 @@
 angular.module(PKG.name + '.feature.cdap-app')
-  .controller('CdapAppListController', function CdapAppList($scope, MyDataSource) {
-
+  .controller('CdapAppListController', function CdapAppList($scope, MyDataSource, fileUploader, $alert, $state, $stateParams) {
     var data = new MyDataSource($scope);
 
     data.request({
@@ -10,4 +9,34 @@ angular.module(PKG.name + '.feature.cdap-app')
       $scope.apps = res;
     });
 
+    $scope.onFileSelected = function(files, event) {
+      for (var i = 0; i < files.length; i++) {
+        fileUploader.upload({
+          path: '/namespaces/' + $state.params.namespace + '/apps',
+          file: files[i]
+        })
+          .then(
+            function success() {
+              $alert({
+                type: 'success',
+                title: 'Upload sucess!',
+                message: 'The file is uploaded successfully!'
+              });
+
+              $state.transitionTo($state.current, $stateParams, {
+                  reload: true,
+                  inherit: false,
+                  notify: true
+              });
+            },
+            function error() {
+              $alert({
+                type: 'error',
+                title: 'Boink!',
+                message: 'Something is goofed up!'
+              });
+            }
+          );
+      }
+    }
   });
