aec24b040efc7040386c89b61441f6cf4e5d605d
Gokul Gunasekaran <gokul@cask.co>
2015-01-08 21:39:16 -0800

    Reverting changes to Scope

--- cdap-api/src/main/java/co/cask/cdap/api/common/Scope.java
+++ cdap-api/src/main/java/co/cask/cdap/api/common/Scope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014-2015 Cask Data, Inc.
+ * Copyright © 2014 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -21,7 +21,7 @@ import com.google.common.collect.Maps;
 import java.util.Map;
 
 /**
- * Enum for the different types of scopes.
+ * Enum for the different types of scopes. For now, only "dataset", but more will come.
  */
 public enum Scope {
   DATASET("dataset");
