6e7b5c15a7d5fdd147c0d2323928db1e42333905
Adrien Cahen <adrien@cask.co>
2015-01-09 17:51:09 -0800

    setup directive dev

--- app/directives/metric-picker/metric-picker.js
+++ app/directives/metric-picker/metric-picker.js
@@ -1,10 +1,18 @@
 angular.module(PKG.name + '.commons')
-  .directive('myMetricPicker', function () {
+  .directive('myMetricPicker', function ($log) {
     return {
       restrict: 'E',
       scope: {
         model: '='
       },
-      templateUrl: 'metric-picker/metric-picker.html'
+      templateUrl: 'metric-picker/metric-picker.html',
+
+      controller: function ($scope) {
+        $log.log('controller', $scope);
+      },
+
+      link: function (scope, elem, attr) {
+        $log.log('link', arguments);
+      }
     };
   });
