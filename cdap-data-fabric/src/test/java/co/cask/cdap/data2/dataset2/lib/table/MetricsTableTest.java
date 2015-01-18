9dcc318fd95bab70d174976c864ef1d87e97978c
Sree <sree@continuuity.com>
2015-01-17 21:47:43 -0800

    Disable failing tests. CDAP-1186 has details

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/dataset2/lib/table/MetricsTableTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/dataset2/lib/table/MetricsTableTest.java
@@ -25,6 +25,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSortedMap;
 import com.google.common.collect.Maps;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Collection;
@@ -148,6 +149,7 @@ public abstract class MetricsTableTest {
   }
 
   @Test
+  @Ignore
   public void testConcurrentIncrement() throws Exception {
     final MetricsTable table = getTable("testConcurrentIncrement");
     final int rounds = 500;
