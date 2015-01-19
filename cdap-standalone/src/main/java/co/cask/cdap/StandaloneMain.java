ec3ec1536dff428b7beb4d75fa21f9374e8918e1
Sree <sree@continuuity.com>
2015-01-19 10:07:05 -0800

    Specifying Constants for adapter directory

--- cdap-standalone/src/main/java/co/cask/cdap/StandaloneMain.java
+++ cdap-standalone/src/main/java/co/cask/cdap/StandaloneMain.java
@@ -331,6 +331,7 @@ public class StandaloneMain {
     //Run dataset service on random port
     List<Module> modules = createPersistentModules(cConf, hConf, webAppPath);
 
+    cConf.set(Constants.AppFabric.ADAPTER_DIR, "/Users/sree/plugins");
     return new StandaloneMain(modules, cConf, webAppPath);
   }
 
