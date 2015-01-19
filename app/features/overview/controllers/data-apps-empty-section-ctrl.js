b45560264ee2e76ecea329bc528ddc19cc06c8c8
Ajai <ajainarayanan@gmail.com>
2015-01-19 12:15:29 -0800

    Adds uploading app to overview feature and moves controller files under a folder

---
+++ app/features/overview/controllers/data-apps-empty-section-ctrl.js
@@ -0,0 +1,30 @@
+angular.module(PKG.name + '.feature.overview')
+  .controller('EmptySectionCtrl', function($scope, myFileUploader, $state, $alert) {
+    $scope.onFileSelected = function(files) {
+      for (var i = 0; i < files.length; i++) {
+        myFileUploader.upload({
+          path: '/namespaces/' + $state.params.namespace + '/apps',
+          file: files[i]
+        })
+          .then(success,error);
+      }
+
+      function success() {
+        $alert({
+          type: 'success',
+          title: 'Upload success!',
+          content: 'The application has been uploaded successfully!'
+        });
+        $state.reload();
+      }
+
+      // Independent xhr request. Failure case will not be handled by $rootScope.
+      function error(err) {
+        $alert({
+          type: 'danger',
+          title: 'Upload failed!',
+          content: err || ''
+        });
+      }
+    };
+  })
