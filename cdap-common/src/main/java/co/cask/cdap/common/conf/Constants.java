c378e3a6cd82117354d1641dbc2d4bf53a6b3e41
Julien Guery <julien@cask.co>
2015-01-19 21:24:11 -0800

    Addressed small PR comments

--- cdap-common/src/main/java/co/cask/cdap/common/conf/Constants.java
+++ cdap-common/src/main/java/co/cask/cdap/common/conf/Constants.java
@@ -590,6 +590,7 @@ public final class Constants {
       public static final String STREAM_HEARTBEAT_FEED_CATEGORY = "streamHeartbeats";
       public static final String STREAM_FEED_CATEGORY = "streams";
 
+      /** Default number of bytes received by a stream after which a notification is sent */
       public static final long DEFAULT_DATA_THRESHOLD = 1000000000;
     }
   }
