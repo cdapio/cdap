2671754cb2c23403678d38f31944f862f1ac2169
Bhooshan Mogal <bhooshan@cask.co>
2015-01-19 20:17:11 -0800

    [CDAP-776] Updated MetricsSearchTestRun to use non-default namespaces

--- cdap-gateway/src/test/java/co/cask/cdap/gateway/handlers/metrics/MetricsSearchTestRun.java
+++ cdap-gateway/src/test/java/co/cask/cdap/gateway/handlers/metrics/MetricsSearchTestRun.java
@@ -17,7 +17,6 @@
 package co.cask.cdap.gateway.handlers.metrics;
 
 import co.cask.cdap.app.metrics.MapReduceMetrics;
-import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.metrics.MetricsCollector;
 import co.cask.cdap.common.metrics.MetricsScope;
 import com.google.common.collect.ImmutableList;
@@ -30,13 +29,22 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
 
 /**
  * Search available contexts and metrics tests
  */
 public class MetricsSearchTestRun extends MetricsSuiteTestBase {
+
+  private static final String MY_SPACE = "myspace";
+  private static final String YOUR_SPACE = "yourspace";
+
+  private static final Gson GSON = new Gson();
+
+  private static final Type LIST_TYPE = new TypeToken<List<String>>() { }.getType();
 
   @BeforeClass
   public static void setup() throws Exception {
@@ -46,45 +54,40 @@ public class MetricsSearchTestRun extends MetricsSuiteTestBase {
   private static void setupMetrics() throws Exception {
     HttpResponse response = doDelete("/v2/metrics");
     Assert.assertEquals(HttpResponseStatus.OK.getCode(), response.getStatusLine().getStatusCode());
+
+    // Adding metrics for app "WordCount" in namespace "myspace", "WCount" in "yourspace"
     MetricsCollector collector =
-      collectionService.getCollector(MetricsScope.USER, getFlowletContext(Constants.DEFAULT_NAMESPACE, "WordCount",
-                                                                          "WordCounter", "splitter"));
+      collectionService.getCollector(MetricsScope.USER, getFlowletContext(MY_SPACE, "WordCount", "WordCounter",
+                                                                          "splitter"));
     collector.increment("reads", 1);
     collector.increment("writes", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getFlowletContext(Constants.DEFAULT_NAMESPACE, "WCount", "WordCounter",
-                                                                 "splitter"));
+                                               getFlowletContext(YOUR_SPACE, "WCount", "WordCounter", "splitter"));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getFlowletContext(Constants.DEFAULT_NAMESPACE, "WCount", "WCounter",
-                                                                 "splitter"));
+                                               getFlowletContext(YOUR_SPACE, "WCount", "WCounter", "splitter"));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getFlowletContext(Constants.DEFAULT_NAMESPACE, "WCount", "WCounter",
-                                                                 "counter"));
+                                               getFlowletContext(YOUR_SPACE, "WCount", "WCounter", "counter"));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getProcedureContext(Constants.DEFAULT_NAMESPACE, "WCount", "RCounts"));
+                                               getProcedureContext(YOUR_SPACE, "WCount", "RCounts"));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getMapReduceTaskContext(Constants.DEFAULT_NAMESPACE, "WCount",
-                                                                       "ClassicWordCount",
+                                               getMapReduceTaskContext(YOUR_SPACE, "WCount", "ClassicWordCount",
                                                                        MapReduceMetrics.TaskType.Mapper));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getMapReduceTaskContext(Constants.DEFAULT_NAMESPACE, "WCount",
-                                                                       "ClassicWordCount",
+                                               getMapReduceTaskContext(YOUR_SPACE, "WCount", "ClassicWordCount",
                                                                        MapReduceMetrics.TaskType.Reducer));
     collector.increment("reads", 1);
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getFlowletContext(Constants.DEFAULT_NAMESPACE, "WordCount",
-                                                                 "WordCounter", "splitter"));
+                                               getFlowletContext(MY_SPACE, "WordCount", "WordCounter", "splitter"));
     collector.increment("reads", 1);
     collector.increment("writes", 1);
 
     collector = collectionService.getCollector(MetricsScope.USER,
-                                               getFlowletContext(Constants.DEFAULT_NAMESPACE, "WordCount",
-                                                                 "WordCounter", "collector"));
+                                               getFlowletContext(MY_SPACE, "WordCount", "WordCounter", "collector"));
     collector.increment("aa", 1);
     collector.increment("zz", 1);
     collector.increment("ab", 1);
@@ -95,16 +98,21 @@ public class MetricsSearchTestRun extends MetricsSuiteTestBase {
 
   @Test
   public void testMetricsContexts() throws Exception {
-    metricsResponseCheck("/v3/namespaces/default/metrics/user?search=childContext", 2,
-                         ImmutableList.<String>of("WCount", "WordCount"));
-    metricsResponseCheck("/v3/namespaces/default/metrics/user/WordCount.f?search=childContext", 1,
-                         ImmutableList.<String>of("WordCounter"));
-    metricsResponseCheck("/v3/namespaces/default/metrics/user/WCount?search=childContext", 3,
-                         ImmutableList.<String>of("b", "f", "p"));
-    metricsResponseCheck("/v3/namespaces/default/metrics/user/WCount.b.ClassicWordCount?search=childContext", 2,
-                         ImmutableList.<String>of("m", "r"));
-    metricsResponseCheck("/v3/namespaces/default/metrics/user/WCount.b.ClassicWordCount.m?search=childContext", 0,
-                         ImmutableList.<String>of());
+    // WordCount is in myspace, WCount in yourspace
+    metricsResponseCheck(getMetricsSearchUri(MY_SPACE, null), 1, ImmutableList.of("WordCount"));
+    metricsResponseCheck(getMetricsSearchUri(YOUR_SPACE, null), 1, ImmutableList.of("WCount"));
+
+    // WordCount should be found in myspace, not in yourspace
+    metricsResponseCheck(getMetricsSearchUri(MY_SPACE, "WordCount.f"), 1, ImmutableList.of("WordCounter"));
+    metricsResponseCheck(getMetricsSearchUri(YOUR_SPACE, "WordCount.f"), 0, ImmutableList.<String>of());
+
+    // WCount should be found in yourspace, not in myspace
+    metricsResponseCheck(getMetricsSearchUri(YOUR_SPACE, "WCount"), 3, ImmutableList.of("b", "f", "p"));
+    metricsResponseCheck(getMetricsSearchUri(MY_SPACE, "WCount"), 0, ImmutableList.<String>of());
+
+    // verify other metrics for WCount app
+    metricsResponseCheck(getMetricsSearchUri(YOUR_SPACE, "WCount.b.ClassicWordCount"), 2, ImmutableList.of("m", "r"));
+    metricsResponseCheck(getMetricsSearchUri(YOUR_SPACE, "WCount.b.ClassicWordCount.m"), 0, ImmutableList.<String>of());
   }
 
   private void metricsResponseCheck(String url, int expected, List<String> expectedValues) throws Exception {
@@ -120,24 +128,62 @@ public class MetricsSearchTestRun extends MetricsSuiteTestBase {
 
   @Test
   public void testMetrics() throws Exception {
-    String base = "/v3/namespaces/default/metrics/user/WordCount.f.WordCounter.splitter/metrics";
+    String base = getMetricsUri(MY_SPACE, "WordCount.f.WordCounter.splitter");
     HttpResponse response = doGet(base);
     Assert.assertEquals(200, response.getStatusLine().getStatusCode());
 
     String result = EntityUtils.toString(response.getEntity());
-    List<String> resultList = new Gson().fromJson(result, new TypeToken<List<String>>() { }.getType());
+    List<String> resultList = new Gson().fromJson(result, LIST_TYPE);
     Assert.assertEquals(2, resultList.size());
 
-    base = "/v3/namespaces/default/metrics/user/WordCount.f.WordCounter.collector/metrics";
+    base = getMetricsUri(MY_SPACE, "WordCount.f.WordCounter.collector");
     response = doGet(base);
     Assert.assertEquals(200, response.getStatusLine().getStatusCode());
 
     result = EntityUtils.toString(response.getEntity());
-    resultList = new Gson().fromJson(result, new TypeToken<List<String>>() { }.getType());
+    resultList = GSON.fromJson(result, LIST_TYPE);
     Assert.assertEquals(3, resultList.size());
     Assert.assertEquals("aa", resultList.get(0));
     Assert.assertEquals("ab", resultList.get(1));
     Assert.assertEquals("zz", resultList.get(2));
 
+    // make sure that WordCount is not found in yourspace
+    base = getMetricsUri(YOUR_SPACE, "WordCount.f.WordCounter.collector");
+    response = doGet(base);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    result = EntityUtils.toString(response.getEntity());
+    resultList = GSON.fromJson(result, LIST_TYPE);
+    Assert.assertEquals(0, resultList.size());
+
+    // test WCount
+    base = getMetricsUri(YOUR_SPACE, "WCount.b.ClassicWordCount.m");
+    response = doGet(base);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    result = EntityUtils.toString(response.getEntity());
+    resultList = GSON.fromJson(result, LIST_TYPE);
+    Assert.assertEquals(1, resultList.size());
+    Assert.assertEquals("reads", resultList.get(0));
+
+    // WCount should not be found in myspace
+    base = getMetricsUri(MY_SPACE, "WCount.b.ClassicWordCount.m");
+    response = doGet(base);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    result = EntityUtils.toString(response.getEntity());
+    resultList = GSON.fromJson(result, LIST_TYPE);
+    Assert.assertEquals(0, resultList.size());
+  }
+
+  private static String getMetricsSearchUri(String namespaceId, @Nullable String prefix) {
+    String metricsSearchUri;
+    if (prefix == null) {
+      metricsSearchUri = String.format("/v3/namespaces/%s/metrics/user?search=childContext", namespaceId);
+    } else {
+      metricsSearchUri = String.format("/v3/namespaces/%s/metrics/user/%s?search=childContext", namespaceId, prefix);
+    }
+    return metricsSearchUri;
+  }
+
+  private static String getMetricsUri(String namespaceId, String metric) {
+    return String.format("/v3/namespaces/%s/metrics/user/%s/metrics", namespaceId, metric);
   }
 }
