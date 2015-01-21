6d8a745508b88bd182fa7910bcf5b6d8b641d07f
Ali Anwar <ali@cask.co>
2015-01-20 17:31:30 -0800

    remove creation of plugin dir, add TODO. also revert the usage of CLI snapshot version

--- cdap-standalone/src/main/java/co/cask/cdap/StandaloneMain.java
+++ cdap-standalone/src/main/java/co/cask/cdap/StandaloneMain.java
@@ -330,8 +330,7 @@ public class StandaloneMain {
 
     //Run dataset service on random port
     List<Module> modules = createPersistentModules(cConf, hConf, webAppPath);
-
-    cConf.set(Constants.AppFabric.ADAPTER_DIR, System.getProperty("user.dir") + "/plugins");
+    // TODO: Setup plugins directory in configuration.
     return new StandaloneMain(modules, cConf, webAppPath);
   }
 
