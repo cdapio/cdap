3850328bfe5bf9c8a3ba9bf42c0f32e3fe3d3421
Ajai <ajainarayanan@gmail.com>
2015-01-22 16:42:51 -0800

    Fixes camelcase filter for empty inputs

--- app/filters/camelcase.js
+++ app/filters/camelcase.js
@@ -1,6 +1,6 @@
 angular.module(PKG.name + '.filters')
   .filter('camelCaseFilter', function() {
     return function(input) {
-      return input.charAt(0).toUpperCase() + input.substr(1);
+      return input && input.charAt(0).toUpperCase() + input.substr(1);
     }
   })
