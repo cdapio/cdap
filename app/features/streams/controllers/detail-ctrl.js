97e135a904f8a3df9243fbe1d9a251b2a52ffb4f
Ajai <ajainarayanan@gmail.com>
2015-01-23 13:56:31 -0800

    Adds Tabbed view for Streams feature

--- app/features/streams/controllers/detail-ctrl.js
+++ app/features/streams/controllers/detail-ctrl.js
@@ -1,5 +1,5 @@
 angular.module(PKG.name + '.feature.streams')
-  .controller('CdapStreamDetailController', function($scope, MyDataSource, $stateParams) {
+  .controller('CdapStreamDetailController', function($scope, MyDataSource, $state) {
     var dataSrc = new MyDataSource($scope);
     $scope.dropdown = [
       {
@@ -13,9 +13,46 @@ angular.module(PKG.name + '.feature.streams')
     ];
 
     dataSrc.request({
-      _cdapPathV2: '/streams/' + $stateParams.streamId
+      _cdapPathV2: '/streams/' + $state.params.streamId
     })
       .then(function(streams) {
         $scope.stream = streams;
       });
+
+    $scope.tabs = [
+      'Status',
+      'Explore',
+      'Programs',
+      'Inner',
+      'Metadata',
+      'Lineage',
+      'Resources'
+    ].map(function (t){
+      return {
+        title: t,
+        state: t.toLowerCase(),
+        partial: '/assets/features/streams/templates/tabs/' + t.toLowerCase() + '.html'
+      };
+    });
+
+    $scope.$watch('tabs.activeTab', function (newVal) {
+      $state.go($state.includes('**.tab') ? $state.current : '.tab', {
+        tab: $scope.tabs[newVal].state
+      });
+    });
+
+    $scope.$on('$stateChangeSuccess', function (event, state) {
+      var tab = $scope.tabs
+        .map(function(t) {
+          return t.state;
+        })
+        .indexOf($state.params.tab);
+
+      if((tab < 0 || tab>=$scope.tabs.length)) {
+        tab = 0;
+      }
+      $scope.tabs.activeTab = tab;
+    });
+
+
   });
