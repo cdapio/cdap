5f03486a6935297adbc58a0e5b84910436664938
Albert Shau <albert@continuuity.com>
2015-01-23 18:32:53 -0800

    CDAP-1199 remove comment

--- cdap-data-fabric/src/main/java/co/cask/cdap/data/format/AvroRecordFormat.java
+++ cdap-data-fabric/src/main/java/co/cask/cdap/data/format/AvroRecordFormat.java
@@ -44,7 +44,6 @@ public class AvroRecordFormat extends ByteBufferRecordFormat<GenericRecord> {
   @Override
   public GenericRecord read(ByteBuffer input) {
     try {
-      // TODO: CDAP-1199 look at reusing decoder and generic record
       return datumReader.read(null, decoderFactory.binaryDecoder(Bytes.toBytes(input), binaryDecoder));
     } catch (IOException e) {
       throw new UnexpectedFormatException("Unable to decode the stream body as avro.", e);
