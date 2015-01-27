52937d1e74d9e5b86a248e88734fa88bcfdc0b1a
Adrien Cahen <adrien@cask.co>
2015-01-26 17:00:27 -0800

    build.sh generates target

--- bin/build.sh
+++ bin/build.sh
@@ -4,22 +4,51 @@
 #  directory that will contain everything
 #  needed to run the UI in production
 
-export PWD=`pwd`
-export UI_PATH=${0%/*/*}
-export DEFAULT_TARGET_PATH="${UI_PATH}/target"
-export TARGET_PATH=${1-$DEFAULT_TARGET_PATH}
+PWD=`pwd`
+UI_PATH=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+DEFAULT_TARGET_PATH="${UI_PATH}/target"
+TARGET_PATH=${1-$DEFAULT_TARGET_PATH}
 
 read -p "Blow away ${TARGET_PATH} and build in it (y/n)? " choice
 case "$choice" in
-  y|Y ) echo "building...";;
+  y|Y ) echo "OK, building CDAP 3.0 user interface!";;
   * ) exit 0;
 esac
 
-# delete and recreate target path
+echo "┌─────────────────────────────────────────────────────────────────"
+echo "├┄ deleting and recreating target path..."
 [ -d ${TARGET_PATH} ] && rm -rf ${TARGET_PATH}
 mkdir ${TARGET_PATH}
 
-touch "${TARGET_PATH}/hello.txt"
+echo "├┄ ensuring global dependencies are present..."
+npm install -g bower gulp &>/dev/null
 
-echo "Done!"
+echo "├┄ cleaning house in ${UI_PATH}..."
+cd ${UI_PATH}
+[ -d ${UI_PATH}/node_modules ] && rm -rf ${UI_PATH}/node_modules
+[ -d ${UI_PATH}/bower_components ] && rm -rf ${UI_PATH}/bower_components
+
+echo "├┄ installing development (npm) dependencies..."
+npm install &>/dev/null
+
+echo "├┄ installing client-side (bower) dependencies..."
+bower install &>/dev/null
+
+echo "├┄ making a fresh dist..."
+gulp clean &>/dev/null
+gulp distribute &>/dev/null
+
+echo "├┄ copying files to the target..."
+cp package.json ${TARGET_PATH}
+cp bower.json ${TARGET_PATH}
+cp server.js ${TARGET_PATH}
+cp -r server/ ${TARGET_PATH}/server
+cp -r dist/ ${TARGET_PATH}/dist
+
+echo "├┄ installing production (npm) dependencies..."
+cd ${TARGET_PATH}
+npm install --production &>/dev/null
+
+echo "└┄ All done!"
+cd ${PWD}
 echo
