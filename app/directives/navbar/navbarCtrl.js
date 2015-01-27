9043ac7b41d83d04d5cbc0ba66ce90fd6f352f8b
Ajai <ajainarayanan@gmail.com>
2015-01-27 14:50:09 -0800

    Updates navbar directive based on displayName field changes

--- app/directives/navbar/navbarCtrl.js
+++ app/directives/navbar/navbarCtrl.js
@@ -3,12 +3,14 @@ angular.module(PKG.name + '.commons')
 
     $scope.namespaces = [];
 
-    $scope.$on (MYAUTH_EVENT.loginSuccess, function (event) {
+    function updateNamespaceList() {
       myNamespace.getList()
         .then(function(list) {
           $scope.namespaces = list;
         });
-    });
+    }
+
+    $scope.$on (MYAUTH_EVENT.loginSuccess, updateNamespaceList);
     $scope.getDisplayName = myNamespace.getDisplayName.bind(myNamespace);
 
     $scope.$on (MYAUTH_EVENT.logoutSuccess, function (event) {
@@ -17,6 +19,9 @@ angular.module(PKG.name + '.commons')
 
     $scope.$on('$stateChangeSuccess', function(event, toState) {
       $scope.highlightTab = toState.data && toState.data.highlightTab;
+      // This is required when user creates a new namespace in admin section.
+      // As of now $dropdown doesn't have broadcast event for click.
+      updateNamespaceList();
     });
 
     $scope.doSearch = function () {
