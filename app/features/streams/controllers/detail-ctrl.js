a285df1edf8425f0f2feb745906b34c9736fc02b
Ajai <ajainarayanan@gmail.com>
2015-01-28 16:28:07 -0800

    Modifies streams to use my-tab directive

--- app/features/streams/controllers/detail-ctrl.js
+++ app/features/streams/controllers/detail-ctrl.js
@@ -27,32 +27,7 @@ angular.module(PKG.name + '.feature.streams')
       'Metadata',
       'Lineage',
       'Resources'
-    ].map(function (t){
-      return {
-        title: t,
-        state: t.toLowerCase(),
-        partial: '/assets/features/streams/templates/tabs/' + t.toLowerCase() + '.html'
-      };
-    });
-
-    $scope.$watch('tabs.activeTab', function (newVal) {
-      $state.go($state.includes('**.tab') ? $state.current : '.tab', {
-        tab: $scope.tabs[newVal].state
-      });
-    });
-
-    $scope.$on('$stateChangeSuccess', function (event, state) {
-      var tab = $scope.tabs
-        .map(function(t) {
-          return t.state;
-        })
-        .indexOf($state.params.tab);
-
-      if((tab < 0 || tab>=$scope.tabs.length)) {
-        tab = 0;
-      }
-      $scope.tabs.activeTab = tab;
-    });
-
-
+    ];
+    $scope.tabsPartialPath = '/assets/features/streams/templates/tabs/';
+    
   });
