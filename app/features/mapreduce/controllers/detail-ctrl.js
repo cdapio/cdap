4080c18614f1703d087ea0f40c61164c1eca87d6
Ajai <ajainarayanan@gmail.com>
2015-01-28 16:27:34 -0800

    Modifies mapreduce feature to use my-tab

--- app/features/mapreduce/controllers/detail-ctrl.js
+++ app/features/mapreduce/controllers/detail-ctrl.js
@@ -6,30 +6,7 @@ angular.module(PKG.name + '.feature.mapreduce')
       'Metadata',
       'History',
       'Resources'
-    ].map(function (t){
-      return {
-        title: t,
-        state: t.toLowerCase(),
-        partial: '/assets/features/mapreduce/templates/tabs/' + t.toLowerCase() + '.html'
-      };
-    });
-
-    $scope.$watch('tabs.activeTab', function (newVal) {
-      $state.go($state.includes('**.tab') ? $state.current : '.tab', {
-        tab: $scope.tabs[newVal].state
-      });
-    });
-
-    $scope.$on('$stateChangeSuccess', function (event, state) {
-      var tab = $scope.tabs
-        .map(function(t) {
-          return t.state;
-        })
-        .indexOf($state.params.tab);
-
-      if((tab < 0 || tab>=$scope.tabs.length)) {
-        tab = 0;
-      }
-      $scope.tabs.activeTab = tab;
-    });
+    ];
+    $scope.tabsPartialPath = '/assets/features/mapreduce/templates/tabs/';
+    
   });
