adc73d8848059dc2a1cc37cb949013eedb61a792
Chris Gianelloni <chris@cask.co>
2015-01-29 13:02:46 -0800

    Setup CDAP_LOG_DIR... COOPR-519 and CDAP-1268

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -7,7 +7,8 @@ describe 'cdap::config' do
         node.automatic['domain'] = 'example.com'
         node.default['hadoop']['hdfs_site']['dfs.datanode.max.transfer.threads'] = '4096'
         node.default['hadoop']['mapred_site']['mapreduce.framework.name'] = 'yarn'
-        stub_command('update-alternatives --display cdap-conf | grep best | awk \'{print $5}\' | grep /etc/cdap/conf.chef').and_return(false)
+        node.default['cdap']['cdap_site']['hdfs.user'] = 'cdap'
+        stub_command(/update-alternatives --display /).and_return(false)
       end.converge(described_recipe)
     end
 
@@ -21,6 +22,24 @@ describe 'cdap::config' do
 
     it 'runs execute[update cdap-conf alternatives]' do
       expect(chef_run).to run_execute('update cdap-conf alternatives')
+    end
+  end
+
+  context 'using cdap 2.7.0' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+        node.default['hadoop']['hdfs_site']['dfs.datanode.max.transfer.threads'] = '4096'
+        node.default['hadoop']['mapred_site']['mapreduce.framework.name'] = 'yarn'
+        node.override['cdap']['version'] = '2.7.0-1'
+        node.default['cdap']['cdap_env']['log_dir'] = '/test/logs/cdap'
+        stub_command(/update-alternatives --display /).and_return(false)
+        stub_command(/test -L /).and_return(false)
+      end.converge(described_recipe)
+    end
+
+    it 'creates /etc/cdap/conf.chef/cdap-env.sh' do
+      expect(chef_run).to create_template('/etc/cdap/conf.chef/cdap-env.sh')
     end
   end
 end
