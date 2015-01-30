323a9ecd809d331f135111cd14d283470cab5693
Adrien Cahen <adrien@cask.co>
2015-01-30 14:45:32 -0800

    fix prefstore test

--- test/unit/prefstore-test.js
+++ test/unit/prefstore-test.js
@@ -1,31 +1,31 @@
 'use strict';
 
-describe('myUiSettings', function(){
+describe('mySettings', function(){
 
   // include the module
   beforeEach(module('cdap-ui.services'));
 
   // get ref to the service to test
-  var myUiSettings;
+  var mySettings;
   beforeEach(inject(function($injector) {
-    myUiSettings = $injector.get('myUiSettings');
+    mySettings = $injector.get('mySettings');
   }));
 
   // actual testing follows
 
   it('has the expected endpoint', function() {
-    expect(myUiSettings.endpoint).toEqual('/preferences/uisettings');
+    expect(mySettings.endpoint).toEqual('/configuration/consolesettings');
   });
 
 
   describe('get', function() {
 
     it('is a method', function() {
-      expect(myUiSettings.get).toEqual(jasmine.any(Function));
+      expect(mySettings.get).toEqual(jasmine.any(Function));
     });
 
     it('returns a promise', function() {
-      var result = myUiSettings.get('test');
+      var result = mySettings.get('test');
       expect(result.then).toEqual(jasmine.any(Function));
     });
 
