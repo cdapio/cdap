3f01e2c650efd6e3f63d13cfb83cb51b66fd4f7f
Ajai <ajainarayanan@gmail.com>
2015-02-02 00:10:29 -0800

    Adds Status and instance info to service status

--- app/features/services/controllers/tabs/status-ctrl.js
+++ app/features/services/controllers/tabs/status-ctrl.js
@@ -6,6 +6,8 @@ angular.module(PKG.name + '.feature.services')
           $state.params.programId;
 
     $scope.endPoints = [];
+    $scope.instances = {};
+    $scope.status = '';
 
     dataSrc.request({
       _cdapNsPath: path
@@ -14,5 +16,37 @@ angular.module(PKG.name + '.feature.services')
         angular.forEach(res.handlers, function(value, key) {
           $scope.endPoints = $scope.endPoints.concat(value.endpoints);
         });
+        $scope.instances = res.instances;
       });
+
+    dataSrc.request({
+      _cdapNsPath: path + '/runnables/' + $state.params.programId + '/instances'
+    })
+      .then(function(res) {
+        $scope.instances = res;
+      });
+
+    dataSrc.poll({
+      _cdapNsPath: path + '/status'
+    }, function(res) {
+      $scope.status = res.status || 'Unknown';
+    });
+
+
+    $scope.start = function() {
+      $scope.status = 'STARTING';
+      dataSrc.request({
+        _cdapNsPath: path + '/start',
+        method: 'POST'
+      });
+    };
+
+    $scope.stop = function() {
+      $scope.status = 'STOPPING';
+      dataSrc.request({
+        _cdapNsPath: path + '/stop',
+        method: 'POST'
+      });
+    }
+
   });
