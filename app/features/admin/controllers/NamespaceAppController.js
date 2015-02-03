6698ce099c42ac17b0af45d7a72846a28fda83af
Andrew Westlund <astewes@gmail.com>
2015-02-03 13:08:36 -0800

    adds app upload functionality to admin namespace apps

---
+++ app/features/admin/controllers/NamespaceAppController.js
@@ -0,0 +1,20 @@
+angular.module(PKG.name + '.feature.admin').controller('AdminNamespaceAppController',
+function ($scope, $state, myAppUploader, MyDataSource) {
+
+  $scope.apps = [];
+
+  var myDataSrc = new MyDataSource($scope),
+       partialPath = '/assets/features/admin/templates/';
+
+  myDataSrc.request({
+    _cdapPath: '/namespaces/' + $state.params.nsadmin + '/apps'
+  })
+    .then(function(response) {
+      $scope.apps = response;
+
+    });
+
+  $scope.onFileSelected = function(files) {
+    myAppUploader.upload(files, $state.params.nsadmin);
+  }
+});
