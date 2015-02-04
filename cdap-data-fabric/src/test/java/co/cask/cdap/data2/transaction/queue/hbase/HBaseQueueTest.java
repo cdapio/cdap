14e906ef56454cbaea581ed3348be497101cae60
Terence Yim <terence@cask.co>
2015-02-03 18:18:38 -0800

    Fix queue unit test that fails.

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/transaction/queue/hbase/HBaseQueueTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/transaction/queue/hbase/HBaseQueueTest.java
@@ -151,6 +151,8 @@ public abstract class HBaseQueueTest extends QueueTest {
       .with(new AbstractModule() {
         @Override
         protected void configure() {
+          // The tests are actually testing stream on queue implementation, hence bind it to the queue implementation
+          bind(StreamAdmin.class).to(HBaseStreamAdmin.class);
           bind(StreamMetaStore.class).to(InMemoryStreamMetaStore.class);
         }
       })
