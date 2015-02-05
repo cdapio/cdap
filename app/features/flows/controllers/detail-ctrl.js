aab86612f2db40839c23066cf2781c26a762f849
Ajai <ajainarayanan@gmail.com>
2015-02-05 09:13:37 -0800

    Fixes flow detail controller for different navigation scenarios

--- app/features/flows/controllers/detail-ctrl.js
+++ app/features/flows/controllers/detail-ctrl.js
@@ -1,6 +1,18 @@
 angular.module(PKG.name + '.feature.flows')
-  .controller('FlowsDetailController', function($scope, $state) {
-    if (!$state.params.runId) {
-      $state.go('flows.detail.runs');
+  .controller('FlowsDetailController', function($scope, $state, $timeout) {
+    if ($state.includes('**.runs.**')) {
+      // If going to flow detail run tab don't do anything.
+      // ui-router already has everything to load the state.
+      return;
+    } else if (!$state.params.runId && $state.includes('flows.detail.*')) {
+      // If going to any state other than run tab just proceed.
+      $timeout(function() {
+        $state.go($state.current);
+      });
+    } else {
+      // If coming from paren default to the run tab.
+      $timeout(function() {
+        $state.go('flows.detail.runs');
+      })
     }
-  });
+});
