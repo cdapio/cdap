3947bafaebf70fe4a8245cd940a7024b8905febd
Ajai <ajainarayanan@gmail.com>
2015-02-05 11:20:04 -0800

    Adds the ability to start and stop a flow
    Conflicts:
    	app/features/flows/controllers/detail-ctrl.js
    
    Conflicts:
    	app/features/flows/templates/detail.html

--- app/features/flows/controllers/detail-ctrl.js
+++ app/features/flows/controllers/detail-ctrl.js
@@ -1,4 +1,34 @@
 angular.module(PKG.name + '.feature.flows')
-  .controller('FlowsDetailController', function() {
+  .controller('FlowsDetailController', function($scope, $state, $timeout, MyDataSource) {
+    var dataSrc = new MyDataSource($scope),
+        basePath = '/apps/' +
+            $state.params.appId +
+            '/flows/' +
+            $state.params.programId;
 
-  });
+    dataSrc.poll({
+      _cdapNsPath: basePath + '/runs'
+    }, function(res) {
+        $scope.activeRuns = res.map(function(r) {
+          return (r.status === 'RUNNING'? 1: 0);
+        })
+          .reduce(function(prev, curr) {
+            return prev + curr;
+          });
+      });
+
+    dataSrc.poll({
+      _cdapNsPath: basePath + '/status'
+    }, function(res) {
+      $scope.status = res.status
+    });
+
+    $scope.toggleFlow = function(action) {
+      $scope.status = action;
+      dataSrc.request({
+        method: 'POST',
+        _cdapNsPath: basePath + '/' + action
+      });
+    };
+
+});
