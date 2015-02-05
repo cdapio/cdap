d92fe4c04958a9f92d8488184849ab5cfc90a4a3
Ajai <ajainarayanan@gmail.com>
2015-02-04 17:19:31 -0800

    Fixes flow routes for nested tabs pattern

--- app/features/flows/routes.js
+++ app/features/flows/routes.js
@@ -16,22 +16,81 @@ angular.module(PKG.name + '.feature.flows')
           templateUrl: '/assets/features/flows/templates/list.html',
           controller: 'CdapflowsListController',
           ncyBreadcrumb: {
-            parent: 'apps.detail.overview',
-            label: 'Flows'
+            skip: true
           }
         })
         .state('flows.detail', {
           url: '/:programId',
           templateUrl: '/assets/features/flows/templates/detail.html',
+          controller: 'FlowsDetailController',
           ncyBreadcrumb: {
-            parent: 'flows.list',
+            parent: 'apps.detail.overview',
             label: '{{$state.params.programId | caskCapitalizeFilter}}'
           }
         })
-          .state('flows.detail.tab', {
-            url: '/:tab',
+          .state('flows.detail.runs', {
+            url: '/runs',
+            templateUrl: '/assets/features/flows/templates/tabs/runs.html',
+            controller: 'FlowsDetailRunController',
             ncyBreadcrumb: {
               skip: true
             }
+          })
+
+            .state('flows.detail.runs.detail', {
+              url: '/:runId',
+              template: '<ui-view/>',
+              abstract: true
+            })
+              .state('flows.detail.runs.detail.status', {
+                url: '/status',
+                template: '<div> Status: {{$state.params.runId}} </div>',
+                ncyBreadcrumb: {
+                  skip: true
+                }
+              })
+              .state('flows.detail.runs.detail.flowlets', {
+                url: '/flowlets',
+                template: '<div> Flowlets: {{$state.params.runId}} </div>',
+                ncyBreadcrumb: {
+                  skip: true
+                }
+              })
+              .state('flows.detail.runs.detail.data', {
+                url: '/data',
+                template: '<div> Data: {{$state.params.runId}} </div>',
+                ncyBreadcrumb: {
+                  skip: true
+                }
+              })
+              .state('flows.detail.runs.detail.configuration', {
+                url: '/configuration',
+                template: '<div> Configuration: {{$state.params.runId}} </div>',
+                ncyBreadcrumb: {
+                  skip: true
+                }
+              })
+              .state('flows.detail.runs.detail.log', {
+                url: '/log',
+                template: '<div> Log: {{$state.params.runId}} </div>',
+                ncyBreadcrumb: {
+                  skip: true
+                }
+              })
+          .state('flows.detail.schedules', {
+            url: '/schedules',
+            templateUrl: '/assets/features/flows/templates/tabs/schedules.html'
+          })
+          .state('flows.detail.metadata', {
+            url: '/metadata',
+            templateUrl: '/assets/features/flows/templates/tabs/metadata.html'
+          })
+          .state('flows.detail.history', {
+            url: '/history',
+            templateUrl: '/assets/features/flows/templates/tabs/history.html'
+          })
+          .state('flows.detail.resources', {
+            url: '/resources',
+            templateUrl: '/assets/features/flows/templates/tabs/resources.html'
           });
   });
