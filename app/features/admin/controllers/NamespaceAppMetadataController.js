f338e1b5d59cdb9de6d75ce72d6577083973751c
Andrew Westlund <astewes@gmail.com>
2015-02-06 10:37:19 -0800

    moves path to a variable and fixes indentation

--- app/features/admin/controllers/NamespaceAppMetadataController.js
+++ app/features/admin/controllers/NamespaceAppMetadataController.js
@@ -1,31 +1,31 @@
-angular.module(PKG.name + '.feature.admin')
-  .controller('AdminNamespaceAppMetadataController', function ($scope, $state, $alert, $timeout, MyDataSource) {
+angular.module(PKG.name + '.feature.admin').controller('AdminNamespaceAppMetadataController',
+function ($scope, $state, $alert, $timeout, MyDataSource) {
 
-    var data = new MyDataSource($scope);
+  var data = new MyDataSource($scope);
+  var path = '/namespaces/' + $state.params.nsadmin + '/apps/' + $state.params.appId;
 
+  data.request({
+    _cdapPath: path
+  })
+    .then(function(apps) {
+      $scope.apps = apps;
+    });
+
+  $scope.deleteApp = function(app) {
     data.request({
-      _cdapPath: '/namespaces/' + $state.params.nsadmin + '/apps/' + $state.params.appId
-    })
-      .then(function(apps) {
-        $scope.apps = apps;
-
+      _cdapPath: '/namespaces/' + $state.params.nsadmin + '/apps/' + $state.params.appId,
+      method: 'DELETE'
+    }, function(res) {
+      $alert({
+        type: 'success',
+        title: app,
+        content: 'App deleted successfully'
       });
-
-    $scope.deleteApp = function(app) {
-      data.request({
-        _cdapPath: '/namespaces/' + $state.params.nsadmin + '/apps/' + $state.params.appId,
-        method: 'DELETE'
-      }, function(res) {
-        $alert({
-          type: 'success',
-          title: app,
-          content: 'App deleted successfully'
-        });
-        // FIXME: Have to avoid $timeout here. Un-necessary.
-        $timeout(function() {
-          $state.go('^.apps');
-        });
+      // FIXME: Have to avoid $timeout here. Un-necessary.
+      $timeout(function() {
+        $state.go('^.apps');
       });
-    };
+    });
+  };
 
 });
