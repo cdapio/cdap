d4d8685dfcdaa17037e9ef6248c19ee763d61ffa
Edwin Elia <edwin@cask.co>
2015-02-06 15:15:08 -0800

    add edwin routes

--- app/features/foo/routes.js
+++ app/features/foo/routes.js
@@ -11,6 +11,38 @@ angular.module(PKG.name+'.feature.foo')
         url: '/foo',
         templateUrl: '/assets/features/foo/foo.html'
       })
+      .state('test-edwin', {
+        url: '/test/edwin',
+        templateUrl: '/assets/features/foo/edwin.html',
+        controller: function ($scope) {
+          $scope.data = {
+            nodes: [
+              { id: 'one', value: { label: 'one' } },
+              { id: 'two', value: { label: 'two' } },
+              { id: 'three', value: { label: 'three' } },
+              { id: 'four', value: { label: 'four' } },
+              { id: 'five', value: { label: 'five' } },
+              { id: 'whew', value: { label: 'whew' } },
+              { id: 'err', value: { label: 'err' } },
+              { id: 'six', value: { label: 'six' } },
+              { id: 'seven', value: { label: 'seven' } }
+            ],
+            links: [
+              { u: 'one', v: 'three' },
+              { u: 'two', v: 'three' },
+              { u: 'three', v: 'four' },
+              { u: 'three', v: 'five'},
+              { u: 'whew', v: 'seven'},
+              { u: 'four', v: 'seven' },
+              { u: 'six', v: 'seven'},
+              { u: 'err', v: 'seven'},
+              { u: 'five', v: 'six' },
+              { u: 'two', v: 'seven'},
+              { u: 'four', v: 'six'}
+            ]
+          };
+        }
+      })
 
       .state('test-settings', {
         url: '/test/settings',
