cad1898258006fb4254d8d1f26d2b6987c6427de
Andrew Westlund <astewes@gmail.com>
2015-02-06 15:53:28 -0800

    adds tabs and templates for service metadata and logs

--- app/features/admin/controllers/ServiceDetailController.js
+++ app/features/admin/controllers/ServiceDetailController.js
@@ -10,4 +10,18 @@ function ($scope, $state, MyDataSource) {
         $scope.instances = response;
       });
 
+    $scope.tabs = [];
+    if ($state.is('admin.system.services.detail')) {
+      $scope.tabs.activeTab = 0;
+    } else {
+      $scope.tabs.activeTab = ($state.is('admin.system.services.detail.metadata')? 0: 1);
+    }
+    $scope.$watch('tabs.activeTab', function(newValue, oldValue) {
+      var toState = ($state.includes('admin.system.services.detail.*') ? '^': '');
+      if(newValue === 0) {
+        $state.go(toState + '.metadata');
+      } else if(newValue === 1) {
+        $state.go(toState + '.logs');
+      }
+    })
 });
