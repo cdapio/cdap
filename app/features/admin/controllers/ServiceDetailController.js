c393c8a7828db0edc9e06bc1da8755ec621eff9c
Andrew Westlund <astewes@gmail.com>
2015-02-05 16:46:18 -0800

    adds available data for admin/manage service metadata screen

--- app/features/admin/controllers/ServiceDetailController.js
+++ app/features/admin/controllers/ServiceDetailController.js
@@ -1,18 +1,13 @@
-angular.module(PKG.name + '.feature.admin')
-  .controller('AdminServiceDetailController', function ($scope, $state) {
-    $scope.service = {
-        name: $state.params.serviceName,
-        description: 'sehiueoowriugn weogiunwg woginwgowg woginwg owign wgoiwg owign wg oiwng woieg',
-        zookeeper: 'string',
-        hostPort: [
-        {
-            host: 'abc',
-            port: 'xyz'
-        },
-        {
-            host: 'def',
-            port: 'uvw'
-        }
-        ]
-    };
-  });
+angular.module(PKG.name + '.feature.admin').controller('AdminServiceDetailController',
+function ($scope, $state, MyDataSource) {
+
+    var myDataSrc = new MyDataSource($scope);
+
+    myDataSrc.request({
+      _cdapPathV2: '/system/services/' + $state.params.serviceName + '/instances'
+    })
+      .then(function(response) {
+        $scope.instances = response;
+      });
+
+});
