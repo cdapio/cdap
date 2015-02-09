2326373bb47f980e9dbe1e15551d1eeb6624c0b4
Bhooshan Mogal <bhooshan@cask.co>
2015-02-09 13:16:21 -0800

    [CDAP-1169] Adding NamespaceAdmin for namespace admin operations

---
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/namespace/NamespaceAdmin.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.internal.app.namespace;
+
+import co.cask.cdap.common.exception.AlreadyExistsException;
+import co.cask.cdap.common.exception.NotFoundException;
+import co.cask.cdap.proto.Id;
+import co.cask.cdap.proto.NamespaceMeta;
+
+import java.util.List;
+
+/**
+ * Admin class for managing a namespace's lifecycle
+ */
+public interface NamespaceAdmin {
+
+  /**
+   * Lists all namespaces
+   *
+   * @return a list of {@link NamespaceMeta} for all namespaces
+   */
+  public List<NamespaceMeta> listNamespaces();
+
+  /**
+   * Gets details of a namespace
+   *
+   * @param namespaceId the {@link Id.Namespace} of the requested namespace
+   * @return the {@link NamespaceMeta} of the requested namespace
+   * @throws NotFoundException if the requested namespace is not found
+   */
+  public NamespaceMeta getNamespace(Id.Namespace namespaceId) throws NotFoundException;
+
+  /**
+   * Checks if the specified namespace exists
+   *
+   * @param namespaceId the {@link Id.Namespace} to check for existence
+   * @return true, if the specifed namespace exists, false otherwise
+   */
+  public boolean hasNamespace(Id.Namespace namespaceId);
+
+  /**
+   * Creates a new namespace
+   *
+   * @param metadata the {@link NamespaceMeta} for the new namespace to be created
+   * @throws AlreadyExistsException if the specified namespace already exists
+   */
+  public void createNamespace(NamespaceMeta metadata) throws AlreadyExistsException;
+
+  /**
+   * Deletes the specified namespace
+   *
+   * @param namespaceId the {@link Id.Namespace} of the specified namespace
+   * @throws NotFoundException if the specified namespace does not exist
+   */
+  public void deleteNamespace(Id.Namespace namespaceId) throws NotFoundException;
+}
