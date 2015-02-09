7f1440609fca2d7eb0e22b169834cfc9403178bf
Ajai <ajainarayanan@gmail.com>
2015-02-08 21:59:29 -0800

    Adds the ability to start and stop a mapreduce job

--- app/features/mapreduce/controllers/detail-ctrl.js
+++ app/features/mapreduce/controllers/detail-ctrl.js
@@ -1,4 +1,33 @@
 angular.module(PKG.name + '.feature.mapreduce')
-  .controller('MapreduceDetailController', function() {
+  .controller('MapreduceDetailController', function(MyDataSource, $state, $scope) {
+    var dataSrc = new MyDataSource($scope),
+        basePath = '/apps/' +
+            $state.params.appId +
+            '/mapreduce/' +
+            $state.params.programId;
 
+    dataSrc.poll({
+      _cdapNsPath: basePath + '/runs'
+    }, function(res) {
+        $scope.activeRuns = (res.map(function(r) {
+          return (r.status === 'RUNNING'? 1: 0);
+        }) || [])
+          .reduce(function(prev, curr) {
+            return prev + curr;
+          });
+      });
+
+    dataSrc.poll({
+      _cdapNsPath: basePath + '/status'
+    }, function(res) {
+      $scope.status = res.status;
+    });
+
+    $scope.toggleFlow = function(action) {
+      $scope.status = action;
+      dataSrc.request({
+        method: 'POST',
+        _cdapNsPath: basePath + '/' + action
+      });
+    };
   })
