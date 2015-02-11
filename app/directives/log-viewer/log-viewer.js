5d8dfcbe6fbbcb6cb80728f2dc28ba24a29f7ef1
Adrien Cahen <adrien@cask.co>
2015-02-10 17:11:56 -0800

    log-viewer first version

--- app/directives/log-viewer/log-viewer.js
+++ app/directives/log-viewer/log-viewer.js
@@ -1,7 +1,8 @@
 angular.module(PKG.name + '.commons')
-  .directive('myLogViewer', function ($filter) {
+  .directive('myLogViewer', function ($filter, $timeout) {
 
-    var capitalize = $filter('caskCapitalizeFilter');
+    var capitalize = $filter('caskCapitalizeFilter'),
+        filterFilter = $filter('filter');
 
     return {
       restrict: 'E',
@@ -13,27 +14,51 @@ angular.module(PKG.name + '.commons')
       templateUrl: 'log-viewer/log-viewer.html',
 
       controller: function ($scope) {
+
         $scope.filters = 'all,info,warn,error,debug,other'.split(',')
-          .map(function (type) {
+          .map(function (key) {
+            var p;
+            switch(key) {
+              case 'all':
+                p = function() { return true; };
+                break;
+              case 'other':
+                p = function(line) { return !(/- (INFO|WARN|ERROR|DEBUG)/).test(line.log); };
+                break;
+              default:
+                p = function(line) { return (new RegExp('- '+key.toUpperCase())).test(line.log); };
+            }
             return {
-              key: type,
-              display: capitalize(type),
-              entries: []
+              key: key,
+              label: capitalize(key),
+              entries: [],
+              predicate: p
             };
           });
 
-        $scope.activeFilter = 'all';
+        $scope.$watch('model', function (newVal) {
+          angular.forEach($scope.filters, function (one) {
+            one.entries = filterFilter($scope.model, one.predicate);
+          });
+        });
 
-        $scope.setFilter = function (k) {
-          $scope.activeFilter = k;
+      },
+
+      link: function (scope, element, attrs) {
+        var termEl = angular.element(element[0].querySelector('.terminal'));
+
+        scope.setFilter = function (k) {
+
+          scope.activeFilter = filterFilter(scope.filters, {key:k})[0];
+
+          $timeout(function(){
+            termEl.prop('scrollTop', termEl.prop('scrollHeight'));
+          });
+
         };
 
-        $scope.filterFn = function () {
-          switch($scope.activeFilter) {
-            case 'all':
-            break;
-          }
-        };
+        scope.setFilter('all');
       }
     };
   });
+
