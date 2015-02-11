ab995429d99129e20c1d09210015aac500957ade
Ali Anwar <ali@cask.co>
2015-02-10 19:40:23 -0800

    Add streamAdmin test case

---
+++ cdap-data-fabric-tests/src/test/java/co/cask/cdap/data2/transaction/stream/StreamAdminTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.transaction.stream;
+
+import co.cask.cdap.api.flow.flowlet.StreamEvent;
+import co.cask.cdap.common.conf.Constants;
+import co.cask.cdap.data.file.FileWriter;
+import co.cask.cdap.data.stream.StreamFileWriterFactory;
+import co.cask.cdap.proto.Id;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ */
+public abstract class StreamAdminTest {
+
+  protected abstract StreamAdmin getStreamAdmin();
+
+  protected abstract StreamFileWriterFactory getFileWriterFactory();
+
+  @Test
+  public void testCreateExist() throws Exception {
+    StreamAdmin streamAdmin = getStreamAdmin();
+
+    String streamName = "streamName";
+    Id.Stream streamId = Id.Stream.from(Constants.DEFAULT_NAMESPACE, streamName);
+    Id.Stream otherStreamId = Id.Stream.from("otherNamespace", streamName);
+
+    Assert.assertFalse(streamAdmin.exists(streamId));
+    Assert.assertFalse(streamAdmin.exists(otherStreamId));
+
+    streamAdmin.create(streamId);
+    // Even though both streams have the same name, {@code otherStreamId} does not exist because it is in a different
+    // namespace than the one created above.
+    Assert.assertTrue(streamAdmin.exists(streamId));
+    Assert.assertFalse(streamAdmin.exists(otherStreamId));
+
+    streamAdmin.create(otherStreamId);
+    Assert.assertTrue(streamAdmin.exists(streamId));
+    Assert.assertTrue(streamAdmin.exists(otherStreamId));
+  }
+
+  @Test
+  public void testDropAllInNamespace() throws Exception {
+    StreamAdmin streamAdmin = getStreamAdmin();
+
+    Id.Stream otherStream = Id.Stream.from("otherNamespace", "otherStream");
+
+    List<Id.Stream> defaultStreams = Lists.newArrayList();
+    for (int i = 0; i < 4; i++) {
+      defaultStreams.add(Id.Stream.from(Constants.DEFAULT_NAMESPACE, "stream" + i));
+    }
+
+    List<Id.Stream> allStreams = Lists.newArrayList();
+    allStreams.addAll(defaultStreams);
+    allStreams.add(otherStream);
+
+    for (Id.Stream stream : allStreams) {
+      streamAdmin.create(stream);
+      writeEvent(stream);
+      // all of the streams should have data in it after writing to them
+      Assert.assertNotEquals(0, getStreamSize(stream));
+    }
+
+    streamAdmin.dropAllInNamespace(Id.Namespace.from(Constants.DEFAULT_NAMESPACE));
+
+    // All of the streams within the default namespace should have no data in them
+    for (Id.Stream defaultStream : defaultStreams) {
+      Assert.assertEquals(0, getStreamSize(defaultStream));
+    }
+    // otherStream isn't in the default namespace so its data is not deleted in the above call to dropAllInNamespace.
+    Assert.assertNotEquals(0, getStreamSize(otherStream));
+
+    // truncate should also delete all the data of a stream
+    streamAdmin.truncate(otherStream);
+    Assert.assertEquals(0, getStreamSize(otherStream));
+  }
+
+  private long getStreamSize(Id.Stream streamId) throws IOException {
+    StreamAdmin streamAdmin = getStreamAdmin();
+    StreamConfig config = streamAdmin.getConfig(streamId);
+    return streamAdmin.fetchStreamSize(config);
+  }
+
+  // simply writes a static string to a stream
+  private void writeEvent(Id.Stream streamId) throws IOException {
+    StreamConfig streamConfig = getStreamAdmin().getConfig(streamId);
+    FileWriter<StreamEvent> streamEventFileWriter = getFileWriterFactory().create(streamConfig, 0);
+    streamEventFileWriter.append(new StreamEvent(Charsets.UTF_8.encode("EVENT")));
+  }
+
+}
---
+++ cdap-data-fabric-tests/src/test/java/co/cask/cdap/data2/transaction/stream/hbase/HBaseFileStreamAdminTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.transaction.stream.hbase;
+
+import co.cask.cdap.common.conf.CConfiguration;
+import co.cask.cdap.common.conf.Constants;
+import co.cask.cdap.common.guice.ConfigModule;
+import co.cask.cdap.common.guice.DiscoveryRuntimeModule;
+import co.cask.cdap.common.guice.LocationRuntimeModule;
+import co.cask.cdap.common.guice.ZKClientModule;
+import co.cask.cdap.data.hbase.HBaseTestBase;
+import co.cask.cdap.data.hbase.HBaseTestFactory;
+import co.cask.cdap.data.runtime.DataFabricDistributedModule;
+import co.cask.cdap.data.runtime.TransactionMetricsModule;
+import co.cask.cdap.data.stream.StreamAdminModules;
+import co.cask.cdap.data.stream.StreamFileWriterFactory;
+import co.cask.cdap.data.stream.service.InMemoryStreamMetaStore;
+import co.cask.cdap.data.stream.service.StreamMetaStore;
+import co.cask.cdap.data2.transaction.stream.StreamAdmin;
+import co.cask.cdap.data2.transaction.stream.StreamAdminTest;
+import co.cask.cdap.notifications.feeds.NotificationFeedManager;
+import co.cask.cdap.notifications.feeds.service.NoOpNotificationFeedManager;
+import co.cask.cdap.test.SlowTests;
+import co.cask.tephra.TransactionManager;
+import co.cask.tephra.TransactionSystemClient;
+import co.cask.tephra.inmemory.InMemoryTxSystemClient;
+import co.cask.tephra.persist.NoOpTransactionStateStorage;
+import co.cask.tephra.persist.TransactionStateStorage;
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+import com.google.inject.util.Modules;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.twill.internal.zookeeper.InMemoryZKServer;
+import org.apache.twill.zookeeper.ZKClientService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ *
+ */
+@Category(SlowTests.class)
+
+public class HBaseFileStreamAdminTest extends StreamAdminTest {
+
+  @ClassRule
+  public static TemporaryFolder tmpFolder = new TemporaryFolder();
+
+  private static HBaseTestBase testHBase;
+  private static StreamAdmin streamAdmin;
+  private static TransactionManager txManager;
+  private static StreamFileWriterFactory fileWriterFactory;
+
+  @BeforeClass
+  public static void init() throws Exception {
+    InMemoryZKServer zkServer = InMemoryZKServer.builder().setDataDir(tmpFolder.newFolder()).build();
+    zkServer.startAndWait();
+
+    testHBase = new HBaseTestFactory().get();
+    testHBase.startHBase();
+
+    Configuration hConf = testHBase.getConfiguration();
+
+    CConfiguration cConf = CConfiguration.create();
+    cConf.setInt(Constants.Stream.CONTAINER_INSTANCES, 1);
+    cConf.set(Constants.CFG_LOCAL_DATA_DIR, tmpFolder.newFolder().getAbsolutePath());
+    cConf.set(Constants.Zookeeper.QUORUM, zkServer.getConnectionStr());
+
+    Injector injector = Guice.createInjector(
+      new ConfigModule(cConf, hConf),
+      new ZKClientModule(),
+      new LocationRuntimeModule().getInMemoryModules(),
+      new DiscoveryRuntimeModule().getInMemoryModules(),
+      new TransactionMetricsModule(),
+      Modules.override(new DataFabricDistributedModule(), new StreamAdminModules().getDistributedModules())
+        .with(new AbstractModule() {
+          @Override
+          protected void configure() {
+            bind(TransactionStateStorage.class).to(NoOpTransactionStateStorage.class);
+            bind(TransactionSystemClient.class).to(InMemoryTxSystemClient.class).in(Singleton.class);
+            bind(StreamMetaStore.class).to(InMemoryStreamMetaStore.class);
+            bind(NotificationFeedManager.class).to(NoOpNotificationFeedManager.class);
+          }
+        })
+    );
+    ZKClientService zkClientService = injector.getInstance(ZKClientService.class);
+    zkClientService.startAndWait();
+
+    streamAdmin = injector.getInstance(StreamAdmin.class);
+    txManager = injector.getInstance(TransactionManager.class);
+    fileWriterFactory = injector.getInstance(StreamFileWriterFactory.class);
+
+    txManager.startAndWait();
+  }
+
+  @AfterClass
+  public static void finish() throws Exception {
+    txManager.stopAndWait();
+    testHBase.stopHBase();
+  }
+
+  @Override
+  protected StreamAdmin getStreamAdmin() {
+    return streamAdmin;
+  }
+
+  @Override
+  protected StreamFileWriterFactory getFileWriterFactory() {
+    return fileWriterFactory;
+  }
+}
