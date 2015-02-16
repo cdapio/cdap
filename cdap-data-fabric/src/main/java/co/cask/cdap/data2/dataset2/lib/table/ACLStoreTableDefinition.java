eb422dcd7b096c712b8142a60a1c48cacbe15d6c
Alvin Wang <alvin@cask.co>
2015-02-16 14:24:08 -0800

    ACLStoreTableDataset: Dataset implementation of ACLStore

---
+++ cdap-data-fabric/src/main/java/co/cask/cdap/data2/dataset2/lib/table/ACLStoreTableDefinition.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2014 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.dataset2.lib.table;
+
+import co.cask.cdap.api.data.schema.UnsupportedTypeException;
+import co.cask.cdap.api.dataset.DatasetAdmin;
+import co.cask.cdap.api.dataset.DatasetDefinition;
+import co.cask.cdap.api.dataset.DatasetProperties;
+import co.cask.cdap.api.dataset.DatasetSpecification;
+import co.cask.cdap.api.dataset.lib.AbstractDatasetDefinition;
+import co.cask.cdap.api.dataset.lib.IndexedObjectStore;
+import co.cask.cdap.api.dataset.lib.ObjectStores;
+import co.cask.common.authorization.ACLEntry;
+import com.google.common.base.Throwables;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * {@link DatasetDefinition} for {@link ACLStoreTable}.
+ */
+public class ACLStoreTableDefinition extends AbstractDatasetDefinition<ACLStoreTable, DatasetAdmin> {
+
+  private final DatasetDefinition<? extends IndexedObjectStore, ?> tableDefinition;
+
+  public ACLStoreTableDefinition(String name, DatasetDefinition<? extends IndexedObjectStore, ?> tableDefinition) {
+    super(name);
+    this.tableDefinition = tableDefinition;
+  }
+
+  @Override
+  public DatasetSpecification configure(String instanceName, DatasetProperties properties) {
+    return DatasetSpecification.builder(instanceName, getName())
+      .properties(properties.getProperties())
+      .datasets(tableDefinition.configure("data", getObjectStoreProperties()))
+      .build();
+  }
+
+  @Override
+  public DatasetAdmin getAdmin(DatasetSpecification spec, ClassLoader classLoader) throws IOException {
+    return tableDefinition.getAdmin(spec.getSpecification("data"), classLoader);
+  }
+
+  @Override
+  public ACLStoreTable getDataset(DatasetSpecification spec, Map<String, String> arguments,
+                             ClassLoader classLoader) throws IOException {
+    DatasetSpecification tableSpec = spec.getSpecification("data");
+    IndexedObjectStore<ACLEntry> data = tableDefinition.getDataset(tableSpec, arguments, classLoader);
+    return new ACLStoreTableDataset(spec, data);
+  }
+
+  private DatasetProperties getObjectStoreProperties() {
+    try {
+      return ObjectStores.objectStoreProperties(ACLEntry.class, DatasetProperties.EMPTY);
+    } catch (UnsupportedTypeException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+}
