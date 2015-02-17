ba3ff3ff096033eb80138175be63f7538b6f3e84
Andrew Westlund <astewes@gmail.com>
2015-02-17 14:24:16 -0800

    fixes beta label in Safari

--- app/features/admin/controllers/AdminOverviewController.js
+++ app/features/admin/controllers/AdminOverviewController.js
@@ -1,9 +1,10 @@
 angular.module(PKG.name + '.feature.admin')
   .controller('AdminOverviewController', function ($scope, $state, myNamespace, MyDataSource, myLocalStorage, MY_CONFIG) {
-    var dataSrc = new MyDataSource($scope);
+    var dataSrc = new MyDataSource($scope),
+        PREFKEY = 'feature.admin.overview.welcomeIsHidden';
 
-    myLocalStorage.get('AdminOverview.welcomeIsHidden')
-      .then(function(v) {
+    myLocalStorage.get(PREFKEY)
+      .then(function (v) {
         $scope.welcomeIsHidden = v;
       });
 
@@ -15,17 +16,17 @@ angular.module(PKG.name + '.feature.admin')
 
     // TODO: add dataset and stream counts per namespace
     myNamespace.getList()
-      .then(function(list) {
+      .then(function (list) {
         $scope.nsList = list;
-        $scope.nsList.forEach(function(namespace) {
+        $scope.nsList.forEach(function (namespace) {
           getApps(namespace)
-            .then(function(apps) {
+            .then(function (apps) {
               namespace.appsCount = apps.length;
             });
         });
       });
 
-    function getApps(namespace) {
+    function getApps (namespace) {
       return dataSrc.request({
         _cdapPath: '/namespaces/' + namespace.id + '/apps'
       });
