120e5e83d828623e36531dad711cd8626eb30eb8
Ajai <ajainarayanan@gmail.com>
2015-02-16 16:18:44 -0800

    Adds angular-moment in workflows feature for duration of run

--- app/features/workflows/controllers/tabs/runs/flow-ctrl.js
+++ app/features/workflows/controllers/tabs/runs/flow-ctrl.js
@@ -1,8 +1,9 @@
 angular.module(PKG.name + '.feature.workflows')
-  .controller('WorkflowsDetailRunStatusController', function($state, $scope, MyDataSource) {
+  .controller('WorkflowsDetailRunStatusController', function($state, $scope, MyDataSource, amMoment) {
     var dataSrc = new MyDataSource($scope),
         basePath = '/apps/' + $state.params.appId + '/workflows/' + $state.params.programId;
-
+    $scope.moment = amMoment;
+    $scope.moment.changeLocale('en');
     $scope.status = null;
     $scope.duration = null;
     $scope.startTime = null;
@@ -26,22 +27,11 @@ angular.module(PKG.name + '.feature.workflows')
     dataSrc.poll({
       _cdapNsPath: basePath + '/runs'
     }, function(res) {
-        var startTime, endTime, duration;
         angular.forEach(res, function(run) {
-          startTime = run.start * 1000;
-          endTime = run.end * 1000;
-          if (run.runid === $state.params.runId) {
-            $scope.status = run.status;
-            duration = ((startTime? endTime - startTime: Date.now() - startTime) / 1000) || 'N/A';
-            if (duration < 60000) {
-              $scope.duration = duration + ' seconds';
-            } else if (duration > 60000 && duration < 3600000) {
-              $scope.duration = duration + ' minutes';
-            } else {
-              $scope.duration = 'N/A';
-            }
-            $scope.startTime = new Date(startTime);
-          }
+          $scope.startTime = new Date(run.start * 1000);
+          $scope.status = run.status;
+          $scope.duration = (run.end ? (run.end * 1000) - $scope.startTime : 0);
+          console.log("Duration:", $scope.duration);
         });
       });
 
