825ef2a173bee016c360d37056eee7660dca348f
Alvin Wang <alvin@cask.co>
2015-02-17 15:34:43 -0800

    Update PrintCLIDocsTableCommand to use new categories

--- cdap-cli/src/main/java/co/cask/cdap/cli/docs/GenerateCLIDocsTable.java
+++ cdap-cli/src/main/java/co/cask/cdap/cli/docs/GenerateCLIDocsTable.java
@@ -17,16 +17,8 @@
 package co.cask.cdap.cli.docs;
 
 import co.cask.cdap.cli.CLIConfig;
-import co.cask.cdap.cli.command.HelpCommand;
-import co.cask.cdap.cli.commandset.DefaultCommands;
-import co.cask.cdap.client.config.ClientConfig;
-import co.cask.cdap.common.conf.CConfiguration;
+import co.cask.cdap.cli.CLIMain;
 import co.cask.common.cli.Command;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -40,19 +32,7 @@ public class GenerateCLIDocsTable {
   private final Command printDocsCommand;
 
   public GenerateCLIDocsTable(final CLIConfig cliConfig) throws URISyntaxException, IOException {
-    Injector injector = Guice.createInjector(
-      new AbstractModule() {
-        @Override
-        protected void configure() {
-          bind(CLIConfig.class).toInstance(cliConfig);
-          bind(ClientConfig.class).toInstance(cliConfig.getClientConfig());
-          bind(CConfiguration.class).toInstance(CConfiguration.create());
-        }
-      }
-    );
-    Iterable<Command> commands = Iterables.concat(new DefaultCommands(injector),
-                                                  ImmutableList.<Command>of(new HelpCommand(null, null)));
-    this.printDocsCommand = new PrintCLIDocsTableCommand(commands);
+    this.printDocsCommand = new PrintCLIDocsTableCommand(new CLIMain(cliConfig).getCommandsSupplier());
   }
 
   public static void main(String[] args) throws Exception {
--- cdap-cli/src/main/java/co/cask/cdap/cli/docs/PrintCLIDocsTableCommand.java
+++ cdap-cli/src/main/java/co/cask/cdap/cli/docs/PrintCLIDocsTableCommand.java
@@ -16,84 +16,34 @@
 
 package co.cask.cdap.cli.docs;
 
+import co.cask.cdap.cli.CommandCategory;
+import co.cask.cdap.cli.command.HelpCommand;
 import co.cask.common.cli.Arguments;
 import co.cask.common.cli.Command;
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.LinkedListMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import co.cask.common.cli.CommandSet;
+import com.google.common.base.Predicates;
+import com.google.common.base.Supplier;
 import com.google.common.collect.Multimap;
 
 import java.io.PrintStream;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Generates data for the table in cdap-docs/reference-manual/source/cli-api.rst.
  */
-public class PrintCLIDocsTableCommand implements Command {
+public class PrintCLIDocsTableCommand extends HelpCommand {
 
-  private final Iterable<Command> commands;
-
-  public PrintCLIDocsTableCommand(Iterable<Command> commands) {
-    this.commands = commands;
+  public PrintCLIDocsTableCommand(Supplier<Iterable<CommandSet<Command>>> commands) {
+    super(commands);
   }
 
-  private enum Category {
-    GENERAL("General", ImmutableSet.of("")),
-    CALL_AND_CONNECT("Calling and Connecting", ImmutableSet.of("call", "connect")),
-    CREATE("Creating", ImmutableSet.of("create")),
-    DELETE("Deleting", ImmutableSet.of("delete")),
-    DEPLOY("Deploying", ImmutableSet.of("deploy")),
-    DESCRIBE("Describing", ImmutableSet.of("describe")),
-    EXECUTE("Executing Queries", ImmutableSet.of("execute")),
-    GET("Retrieving Information", ImmutableSet.of("get")),
-    LIST("Listing Elements", ImmutableSet.of("list")),
-    SEND("Sending Events", ImmutableSet.of("send")),
-    SET("Setting", ImmutableSet.of("set")),
-    START("Starting", ImmutableSet.of("start")),
-    STOP("Stopping", ImmutableSet.of("stop")),
-    TRUNCATE("Truncating", ImmutableSet.of("truncate"));
-
-    private final String name;
-    private final ImmutableSet<String> prefixes;
-
-    Category(String name, ImmutableSet<String> prefixes) {
-      this.name = name;
-      this.prefixes = prefixes;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    public ImmutableSet<String> getPrefixes() {
-      return prefixes;
-    }
-  }
 
   @Override
   public void execute(Arguments arguments, PrintStream output) throws Exception {
-    List<Command> commandList = com.googlecode.concurrenttrees.common.Iterables.toList(commands);
-    Multimap<Category, Command> categorizedCommands = categorizeCommands(commandList);
-
-    for (Category category : Category.values()) {
-      List<Command> commandsInCategory = Lists.newArrayList(
-        Optional.fromNullable(categorizedCommands.get(category)).or(ImmutableList.<Command>of()));
-
-      output.printf("   **%s**\n", category.getName());
-      Collections.sort(commandsInCategory, new Comparator<Command>() {
-        @Override
-        public int compare(Command o, Command o2) {
-          return o.getPattern().compareTo(o2.getPattern());
-        }
-      });
-      for (Command command : commandsInCategory) {
+    Multimap<String, Command> categorizedCommands = categorizeCommands(
+      commands.get(), CommandCategory.GENERAL, Predicates.<Command>alwaysTrue());
+    for (String category : categorizedCommands.keySet()) {
+      output.printf("   **%s**\n", category);
+      for (Command command : categorizedCommands.get(category)) {
         output.printf("   ``%s``,\"%s\"\n", command.getPattern(), command.getDescription().replace("\"", "\"\""));
       }
     }
@@ -108,28 +58,4 @@ public class PrintCLIDocsTableCommand implements Command {
   public String getDescription() {
     return "null";
   }
-
-  private Multimap<Category, Command> categorizeCommands(List<Command> commandList) {
-    Multimap<Category, Command> result = LinkedListMultimap.create();
-
-    Map<String, Category> prefixToCategoryMap = Maps.newHashMap();
-    for (Category category : Category.values()) {
-      for (String prefix : category.getPrefixes()) {
-        prefixToCategoryMap.put(prefix, category);
-      }
-    }
-
-    for (Command command : commandList) {
-      String patternFirstWord = command.getPattern().split(" ")[0];
-      if (prefixToCategoryMap.containsKey(patternFirstWord)) {
-        Category category = prefixToCategoryMap.get(patternFirstWord);
-        result.put(category, command);
-      } else {
-        result.put(Category.GENERAL, command);
-      }
-    }
-
-    return result;
-  }
-
 }
