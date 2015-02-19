1dfa84edb88cab1dfd8eac8dc26828540d20c8ea
nistala <nrohits@gmail.com>
2015-02-19 12:15:31 -0800

    minor comments addressed

--- app/features/flows/controllers/tabs/runs/flowlets/detail-ctrl.js
+++ app/features/flows/controllers/tabs/runs/flowlets/detail-ctrl.js
@@ -1,7 +1,5 @@
 angular.module(PKG.name + '.feature.flows')
-  .controller('FlowsFlowletDetaiController', function($state, $scope, MyDataSource) {
-    var dataSrc = new MyDataSource($scope),
-        basePath = '/apps/' + $state.params.appId + '/flows/' + $state.params.programId;
+  .controller('FlowsFlowletDetaiController', function($state, $scope) {
 
     $scope.flowletId = $state.params.flowletId;
 
