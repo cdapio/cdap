d6e13ee19aaf5a87c51ad1e9d9a96875ec439f02
Gokul Gunasekaran <gokul@cask.co>
2015-02-19 12:07:55 -0800

    Merge develop changes and fixing compilation

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/worker/BasicWorkerContext.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/worker/BasicWorkerContext.java
@@ -38,6 +38,7 @@ import co.cask.cdap.data2.dataset2.NamespacedDatasetFramework;
 import co.cask.cdap.internal.app.runtime.AbstractContext;
 import co.cask.cdap.internal.app.runtime.service.BasicServiceWorkerContext;
 import co.cask.cdap.logging.context.WorkerLoggingContext;
+import co.cask.cdap.proto.Id;
 import co.cask.tephra.TransactionContext;
 import co.cask.tephra.TransactionFailureException;
 import co.cask.tephra.TransactionSystemClient;
@@ -162,8 +163,8 @@ public class BasicWorkerContext extends AbstractContext implements WorkerContext
     final TransactionContext context = new TransactionContext(transactionSystemClient);
     try {
       context.start();
-      runnable.run(new DynamicDatasetContext(context, datasetFramework, getProgram().getClassLoader(),
-                                             datasets, runtimeArgs) {
+      runnable.run(new DynamicDatasetContext(Id.Namespace.from(program.getNamespaceId()), context, datasetFramework,
+                                             getProgram().getClassLoader(), datasets, runtimeArgs) {
         @Override
         protected LoadingCache<Long, Map<DatasetCacheKey, Dataset>> getDatasetsCache() {
           return datasetsCache;
