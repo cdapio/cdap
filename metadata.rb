30cf236bf879bc09a8297e1bfcdceb2a70f04487
Chris Gianelloni <chris@cask.co>
2015-02-20 12:48:49 -0800

    Version bump to 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'All rights reserved'
 description      'Installs/Configures Cask Data Application Platform (CDAP)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 %w(apt hadoop java nodejs ntp yum yum-epel).each do |cb|
   depends cb
