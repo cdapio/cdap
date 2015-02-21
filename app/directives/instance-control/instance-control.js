6c1490b7af419ec2da03a1c381a5563682f5f7ae
nistala <nrohits@gmail.com>
2015-02-20 17:14:47 -0800

    better handle errors

--- app/directives/instance-control/instance-control.js
+++ app/directives/instance-control/instance-control.js
@@ -1,12 +1,12 @@
 angular.module(PKG.name + '.commons')
-  .directive('myInstanceControl', function ($timeout, MyDataSource) {
+  .directive('myInstanceControl', function ($alert, MyDataSource) {
 
     return {
       restrict: 'E',
 
       scope: {
         model: '=',
-        basepath: '='
+        basePath: '='
       },
 
       templateUrl: 'instance-control/instance-control.html',
@@ -20,9 +20,19 @@ angular.module(PKG.name + '.commons')
         scope.handleSet = function () {
           scope.processing = true;
 
+          if (scope.model.requested === undefined || scope.model.requested < 0) {
+            $alert({
+              title: 'Invalid instances: ',
+              content: 'you must request a valid number of instances.',
+              type: 'danger'
+            });
+            scope.processing = false;
+            return;
+          }
+
           dataSrc.request({
             method: 'PUT',
-            _cdapPathV2: scope.basepath + '/instances',
+            _cdapPathV2: scope.basePath + '/instances',
             data: {'instances': scope.model.requested}
           }).then(function success (response) {
             scope.model.provisioned = scope.model.requested;
