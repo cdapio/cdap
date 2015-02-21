56ca912cae3836da885a8edaae6e66f4cb1469c0
Adrien Cahen <adrien@cask.co>
2015-02-20 16:44:13 -0800

    adds bytes filter files

---
+++ app/filters/bytes.js
@@ -0,0 +1,15 @@
+// adapted from https://gist.github.com/thomseddon/3511330
+
+angular.module(PKG.name+'.filters').filter('bytes', function() {
+  return function(bytes, precision) {
+    if (bytes<1 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
+      return 'n/a';
+    }
+    if (typeof precision === 'undefined') {
+      precision = bytes>1023 ? 1 : 0;
+    }
+    var number = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +
+      ' ' + ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'][number];
+  }
+});
