f1bd36ba649e86c9680868f02eee135ddc31b8d8
Sree <sree@continuuity.com>
2015-02-23 12:37:56 -0800

    Add V3 Monitor Handler and tests

--- cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/MonitorHandlerV2.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/MonitorHandlerV2.java
@@ -1,55 +1,27 @@
-/*
- * Copyright © 2014-2015 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package co.cask.cdap.gateway.handlers;
 
 import co.cask.cdap.app.store.ServiceStore;
 import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.twill.MasterServiceManager;
 import co.cask.cdap.gateway.auth.Authenticator;
-import co.cask.cdap.gateway.handlers.util.AbstractAppFabricHttpHandler;
-import co.cask.cdap.proto.SystemServiceMeta;
 import co.cask.http.HttpResponder;
-import com.google.common.collect.Lists;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.google.inject.Inject;
 import org.jboss.netty.handler.codec.http.HttpRequest;
-import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
 /**
- * Monitor Handler returns the status of different discoverable services
+ *
  */
-@Path(Constants.Gateway.API_VERSION_3)
-public class MonitorHandler extends AbstractMonitorHandler {
-
+@Path(Constants.Gateway.API_VERSION_2)
+public class MonitorHandlerV2 extends AbstractMonitorHandler {
   @Inject
-  public MonitorHandler(Authenticator authenticator, Map<String, MasterServiceManager> serviceMap,
-                        ServiceStore serviceStore) throws Exception {
+  public MonitorHandlerV2(Authenticator authenticator, Map<String, MasterServiceManager> serviceMap,
+                          ServiceStore serviceStore) throws Exception {
     super(authenticator, serviceMap, serviceStore);
   }
 
