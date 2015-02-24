904545d48a764845dcc374169b6e50e47728f85b
nistala <nrohits@gmail.com>
2015-02-24 01:11:32 -0800

    instance control controller

--- app/directives/instance-control/instance-control.js
+++ app/directives/instance-control/instance-control.js
@@ -1,18 +1,22 @@
 angular.module(PKG.name + '.commons')
-  .directive('myInstanceControl', function ($alert, MyDataSource) {
+  .directive('myInstanceControl', function ($alert) {
 
     return {
       restrict: 'E',
+      controller: 'instanceControlController',
+      scope: {
+        model: '=',
+        basePath: '='
+      },
       templateUrl: 'instance-control/instance-control.html',
-      
-      link: function (scope, element, attrs, controller) {
+      link: function (scope, element, attrs) {
 
         scope.processing = false;
 
         scope.handleSet = function () {
           scope.processing = true;
 
-          if (scope.instances.requested === undefined || scope.instances.requested < 0) {
+          if (scope.model.requested === undefined || scope.model.requested < 0) {
             $alert({
               title: 'Invalid instances: ',
               content: 'you must request a valid number of instances.',
@@ -25,9 +29,9 @@ angular.module(PKG.name + '.commons')
           scope.myDataSrc.request({
             method: 'PUT',
             _cdapPathV2: scope.basePath + '/instances',
-            data: {'instances': scope.instances.requested}
+            data: {'instances': scope.model.requested}
           }).then(function success (response) {
-            scope.instances.provisioned = scope.instances.requested;
+            scope.model.provisioned = scope.model.requested;
           }, function error (err) {
             //pass
           }).finally(function () {
@@ -37,5 +41,8 @@ angular.module(PKG.name + '.commons')
         }
       }
     };
+  })
+  .controller('instanceControlController', function ($scope, MyDataSource) {
+    $scope.myDataSrc = new MyDataSource($scope);
   });
 
--- app/features/admin/controllers/ServiceDetailController.js
+++ app/features/admin/controllers/ServiceDetailController.js
@@ -1,9 +1,9 @@
 angular.module(PKG.name + '.feature.admin').controller('AdminServiceDetailController',
 function ($scope, $state, MyDataSource) {
     $scope.basePath = '/system/services/' + $state.params.serviceName;
-    $scope.myDataSrc = new MyDataSource($scope);
+    var myDataSrc = new MyDataSource($scope);
 
-    $scope.myDataSrc.request({
+    myDataSrc.request({
       _cdapPathV2: $scope.basePath  + '/instances'
     })
       .then(function(response) {
