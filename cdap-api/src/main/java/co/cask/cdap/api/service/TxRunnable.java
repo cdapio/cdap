255fe492b5ef94ca05a0b6ae8d1dbac0550c4e29
Ali Anwar <ali@cask.co>
2015-02-24 18:33:36 -0800

    old TxRunnable to delegate to new TxRunnable (in an effort to reduce the things that users have to change code that worked on 2.6 - only redeploy)

---
+++ cdap-api/src/main/java/co/cask/cdap/api/service/TxRunnable.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.api.service;
+
+import co.cask.cdap.api.data.DatasetContext;
+import co.cask.cdap.api.dataset.Dataset;
+
+/**
+ * A runnable that provides a {@link DatasetContext} to programs which may be used to get
+ * access to and use datasets.
+ * @deprecated As of version 2.8.0, replaced by {@link co.cask.cdap.api.TxRunnable}
+ */
+public interface TxRunnable {
+
+  /**
+   * Provides a {@link DatasetContext} to get instances of {@link Dataset}s.
+   *
+   * <p>
+   *   Operations executed on a dataset within the execution of this method are committed as a single transaction.
+   *   The transaction is started before this method is invoked and is committed upon successful execution.
+   *   Exceptions thrown while committing the transaction or thrown by user-code result in a rollback of the
+   *   transaction.
+   * </p>
+   * @param context to get datasets from.
+   * @throws Exception
+   */
+  void run(DatasetContext context) throws Exception;
+
+}
