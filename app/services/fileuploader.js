dc3a9bcc0bebb63f42d88269dbd9dc6862c7219a
nistala <nrohits@gmail.com>
2015-02-25 15:04:17 -0800

    removing apikey

--- app/services/fileuploader.js
+++ app/services/fileuploader.js
@@ -20,7 +20,6 @@ angular.module(PKG.name + '.services')
         xhr.open('POST', path, true);
         xhr.setRequestHeader('Content-type', 'application/octet-stream');
         xhr.setRequestHeader('X-Archive-Name', fileObj.file.name);
-        xhr.setRequestHeader('X-ApiKey', '');
         xhr.setRequestHeader('Authorization', 'Bearer ' + myAuth.currentUser.token);
         xhr.send(fileObj.file);
         cfpLoadingBar.start();
