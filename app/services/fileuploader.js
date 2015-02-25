5a2590e63c23a96bce85c43398d8533aba1ff1de
nistala <nrohits@gmail.com>
2015-02-25 13:52:38 -0800

    handle local case

--- app/services/fileuploader.js
+++ app/services/fileuploader.js
@@ -1,36 +1,41 @@
 angular.module(PKG.name + '.services')
-  .factory('myFileUploader', function($rootScope, $q, $window, cfpLoadingBar) {
+  .factory('myFileUploader', function($rootScope, $q, $window, $alert, cfpLoadingBar) {
     function upload(fileObj){
       var deferred = $q.defer();
-
-      if (!$rootScope.currentUser || !$rootScope.currentUser.token) {
+      console.log($rootScope.currentUser);
+      if (!$rootScope.currentUser) {
         deferred.reject(400);
-      }
-
-      var xhr = new $window.XMLHttpRequest();
-      xhr.upload.addEventListener('progress', function (e) {
-        if (e.type === 'progress') {
-          console.log('App Upload in progress!');
-        }
-      });
-      var path = fileObj.path;
-      xhr.open('POST', path, true);
-      xhr.setRequestHeader('Content-type', 'application/octet-stream');
-      xhr.setRequestHeader('X-Archive-Name', fileObj.file.name);
-      xhr.setRequestHeader('X-ApiKey', '');
-      xhr.setRequestHeader('Authorization', 'Bearer ' + $rootScope.currentUser.token);
-      xhr.send(fileObj.file);
-      cfpLoadingBar.start();
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status > 399){
-            deferred.reject(xhr.response);
-          } else {
-            deferred.resolve();
+        $alert({
+          title: 'Must specify user: ',
+          content: 'Could not find user.',
+          type: 'danger'
+        });
+      } else {
+        var xhr = new $window.XMLHttpRequest();
+        xhr.upload.addEventListener('progress', function (e) {
+          if (e.type === 'progress') {
+            console.log('App Upload in progress!');
           }
-          cfpLoadingBar.complete();
-        }
-      };
+        });
+        var path = fileObj.path;
+        xhr.open('POST', path, true);
+        xhr.setRequestHeader('Content-type', 'application/octet-stream');
+        xhr.setRequestHeader('X-Archive-Name', fileObj.file.name);
+        xhr.setRequestHeader('X-ApiKey', '');
+        xhr.setRequestHeader('Authorization', 'Bearer ' + $rootScope.currentUser.token);
+        xhr.send(fileObj.file);
+        cfpLoadingBar.start();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            if (xhr.status > 399){
+              deferred.reject(xhr.response);
+            } else {
+              deferred.resolve();
+            }
+            cfpLoadingBar.complete();
+          }
+        };
+      }
       return deferred.promise;
     }
     return {
