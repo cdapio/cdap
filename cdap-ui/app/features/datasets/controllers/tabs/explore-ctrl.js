d439ac11594633271f17b87defc3aa1994b5a700
Adrien Cahen <adrien@cask.co>
2015-02-27 14:00:16 -0800

    add explore ctrl

---
+++ cdap-ui/app/features/datasets/controllers/tabs/explore-ctrl.js
@@ -0,0 +1,8 @@
+angular.module(PKG.name + '.feature.datasets')
+  .controller('CdapDatasetExploreController',
+    function($scope, MyDataSource, $state, myHelpers, $log) {
+
+      $scope.activePanel = 0;
+
+    }
+  );
