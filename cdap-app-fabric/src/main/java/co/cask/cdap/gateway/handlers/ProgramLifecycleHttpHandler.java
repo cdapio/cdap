41f9bdcaec2924c8fec02619728ac9ef2fa0e0c6
Alvin Wang <alvin@cask.co>
2015-03-03 15:21:46 -0800

    Addressed comments

--- cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/ProgramLifecycleHttpHandler.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/gateway/handlers/ProgramLifecycleHttpHandler.java
@@ -469,7 +469,7 @@ public class ProgramLifecycleHttpHandler extends AbstractAppFabricHttpHandler {
 
     ProgramType type = getProgramType(programType);
     if (type == null) {
-      responder.sendString(HttpResponseStatus.BAD_REQUEST,
+      responder.sendString(HttpResponseStatus.METHOD_NOT_ALLOWED,
                            String.format("Program type '%s' not supported", programType));
       return;
     }
@@ -957,7 +957,7 @@ public class ProgramLifecycleHttpHandler extends AbstractAppFabricHttpHandler {
                        @PathParam("program-id") String programId) {
     ProgramType type = getProgramType(programCategory);
     if (type == null) {
-      responder.sendString(HttpResponseStatus.BAD_REQUEST,
+      responder.sendString(HttpResponseStatus.METHOD_NOT_ALLOWED,
                            String.format("Live-info not supported for program type '%s'", programCategory));
       return;
     }
