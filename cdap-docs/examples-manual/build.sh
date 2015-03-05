f3f4cdf6fd4b95861d7e093593cea773b0db6bf3
John Jackson <john@cask.co>
2015-03-04 20:48:48 -0800

    Updates.

--- cdap-docs/examples-manual/build.sh
+++ cdap-docs/examples-manual/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright © 2014 Cask Data, Inc.
+# Copyright © 2014-2015 Cask Data, Inc.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -24,19 +24,6 @@ source ../_common/common-build.sh
 
 CHECK_INCLUDES=$TRUE
 
-function guide_rewrite_pandoc() {
-  echo "Re-writing using pandoc and sed $1 $2"
-  # Re-writes the links in the RST file to point to a local copy of any image links.
-  INCLUDES_DIR=$1
-  GUIDE=$2
-  REDIRECT_S="../../../../.." # Source, 5 redirects
-  REDIRECT_T="\.\./\.\./\.\./\.\./\.\." # Target, 5 redirects, escaped
-  
-  mkdir $INCLUDES_DIR/$GUIDE
-  pandoc -t rst -r markdown $INCLUDES_DIR/$REDIRECT_S/$GUIDE/README.md  -o $INCLUDES_DIR/$GUIDE/README_SOURCE.rst
-  sed -e "s|figure:: docs/images|figure:: $REDIRECT_T/$GUIDE/docs/images|g" -e "s|.. code:: |.. code-block:: |g" $INCLUDES_DIR/$GUIDE/README_SOURCE.rst > $INCLUDES_DIR/$GUIDE/README.rst
-}
-
 function guide_rewrite_sed() {
   echo "Re-writing using sed $1 $2"
   # Re-writes the links in the RST file to point to a local copy of any image links.
@@ -55,12 +42,10 @@ function pandoc_includes() {
   guide_rewrite_sed $1 cdap-flow-guide
   guide_rewrite_sed $1 cdap-flume-guide
   guide_rewrite_sed $1 cdap-kafka-ingest-guide
-  # Uses pandoc to translate the README markdown files to rst in the target directory
-  # and then re-writes all the image links...
-  guide_rewrite_pandoc $1 cdap-mapreduce-guide
-  guide_rewrite_pandoc $1 cdap-spark-guide
-  guide_rewrite_pandoc $1 cdap-timeseries-guide
-  guide_rewrite_pandoc $1 cdap-twitter-ingest-guide
+  guide_rewrite_sed $1 cdap-mapreduce-guide
+  guide_rewrite_sed $1 cdap-spark-guide
+  guide_rewrite_sed $1 cdap-timeseries-guide
+  guide_rewrite_sed $1 cdap-twitter-ingest-guide
 }
 
 run_command $1
