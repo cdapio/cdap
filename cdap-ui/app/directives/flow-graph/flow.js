27ecda6ee5244f665051390aae444da8ee8c6c2c
Edwin Elia <edwin@cask.co>
2015-03-09 13:27:15 -0700

    change flow graph to use number filter instead of bytes

--- cdap-ui/app/directives/flow-graph/flow.js
+++ cdap-ui/app/directives/flow-graph/flow.js
@@ -37,7 +37,7 @@ module.directive('myFlowGraph', function (d3, dagreD3, $state, $filter) {
         var instanceMap = {};
         var labelMap = {};
 
-        var bytesFilter = $filter('bytes');
+        var numberFilter = $filter('myNumber');
 
         var renderer = new dagreD3.render();
         var g = new dagreD3.graphlib.Graph();
@@ -108,7 +108,7 @@ module.directive('myFlowGraph', function (d3, dagreD3, $state, $filter) {
           parent.insert('text')
             .attr('x', - flowletCircleRadius)
             .attr('y', metricCountPadding)
-            .text(bytesFilter(scope.model.metrics[labelMap[node.label].name]))
+            .text(numberFilter(scope.model.metrics[labelMap[node.label].name]))
             .attr('class', 'flow5shapes flowlet-event-count');
 
           node.intersect = function(point) {
@@ -137,7 +137,7 @@ module.directive('myFlowGraph', function (d3, dagreD3, $state, $filter) {
           // Elements are positioned with respect to shapeSvg.
           var width = shapeSvg.node().getBBox().width;
           var circleXPos = -1 * width/2;
-         
+
           parent.append('circle')
             .attr('cx', circleXPos)
             .attr('r', metricCircleRadius)
@@ -146,7 +146,7 @@ module.directive('myFlowGraph', function (d3, dagreD3, $state, $filter) {
           parent.append('text')
             .attr('x', circleXPos)
             .attr('y', metricCountPadding)
-            .text(bytesFilter(scope.model.metrics[labelMap[node.label].name]))
+            .text(numberFilter(scope.model.metrics[labelMap[node.label].name]))
             .attr('class', 'flow-shapes stream-event-count');
 
           node.intersect = function(point) {
@@ -216,7 +216,7 @@ module.directive('myFlowGraph', function (d3, dagreD3, $state, $filter) {
           } else {
             $state.go('flows.detail.runs.tabs.status.flowletsDetail', {flowletId: nodeId});
           }
-          
+
         }
 
         /**
