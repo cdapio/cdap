54d965013a32154e79267e881d400c09df15b666
Alvin Wang <alvin@cask.co>
2015-03-09 15:27:21 -0700

    CDAP-1300: ScheduleClient for listing, suspending, and resuming schedules

--- cdap-client-tests/src/test/java/co/cask/cdap/client/app/FakeApp.java
+++ cdap-client-tests/src/test/java/co/cask/cdap/client/app/FakeApp.java
@@ -18,8 +18,7 @@ package co.cask.cdap.client.app;
 
 import co.cask.cdap.api.app.AbstractApplication;
 import co.cask.cdap.api.data.stream.Stream;
-import co.cask.cdap.common.conf.Constants;
-import co.cask.cdap.proto.Id;
+import co.cask.cdap.internal.schedule.TimeSchedule;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
@@ -33,6 +32,7 @@ public class FakeApp extends AbstractApplication {
   public static final String NAME = "FakeApp";
   public static final String STREAM_NAME = "fakeStream";
   public static final String DS_NAME = "fakeds";
+  public static final String SCHEDULE_NAME = "someSchedule";
 
   public static final List<String> FLOWS = Lists.newArrayList(FakeFlow.NAME);
   public static final List<String> PROCEDURES = Lists.newArrayList(FakeProcedure.NAME);
@@ -55,6 +55,8 @@ public class FakeApp extends AbstractApplication {
     createDataset(DS_NAME, FakeDataset.class.getName());
     addProcedure(new FakeProcedure());
     addFlow(new FakeFlow());
+    addWorkflow(new FakeWorkflow());
+    scheduleWorkflow(new TimeSchedule(SCHEDULE_NAME, "", "0 0 1 1 *"), FakeWorkflow.NAME);
     addService(PingService.NAME, new PingService());
   }
 }
