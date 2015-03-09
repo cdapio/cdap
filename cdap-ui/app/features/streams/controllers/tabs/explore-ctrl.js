99c62f58fe7b6abf4f3718431e283a0f99181909
Adrien Cahen <adrien@cask.co>
2015-03-09 13:50:50 -0700

    explore search form

--- cdap-ui/app/features/streams/controllers/tabs/explore-ctrl.js
+++ cdap-ui/app/features/streams/controllers/tabs/explore-ctrl.js
@@ -6,12 +6,30 @@ angular.module(PKG.name + '.feature.streams')
 
       $scope.activePanel = 0;
 
-      dataSrc
-        .poll({
-          _cdapNsPath: '/streams/' + $state.params.streamId + '/events'
-        }, function (result) {
-          $scope.events = result;
-        });
+
+      var now = Date.now();
+
+      $scope.eventSearch = {
+        startMs: now-(60*60*1000*2), // two hours ago
+        endMs: now,
+        limit: 10,
+        results: []
+      };
+
+      $scope.doEventSearch = function () {
+        dataSrc
+          .request({
+            _cdapNsPath: '/streams/' + $state.params.streamId +
+              '/events?start=' + $scope.eventSearch.startMs +
+              '&end=' + $scope.eventSearch.endMs +
+              '&limit=' + $scope.eventSearch.limit
+          }, function (result) {
+            $scope.eventSearch.results = result;
+          });
+      };
+
+
+      $scope.doEventSearch();
 
     }
   );
