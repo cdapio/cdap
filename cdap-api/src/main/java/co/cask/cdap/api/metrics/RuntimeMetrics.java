cec4e342d7cc9643f3454dd7da5255520d472eb7
Alvin Wang <alvin@cask.co>
2015-03-12 10:46:38 -0700

    Addressed comments

--- cdap-api/src/main/java/co/cask/cdap/api/metrics/RuntimeMetrics.java
+++ cdap-api/src/main/java/co/cask/cdap/api/metrics/RuntimeMetrics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 Cask Data, Inc.
+ * Copyright © 2014-2015 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,6 +15,7 @@
  */
 
 package co.cask.cdap.api.metrics;
+
 import co.cask.cdap.api.annotation.Beta;
 
 import java.util.concurrent.TimeUnit;
