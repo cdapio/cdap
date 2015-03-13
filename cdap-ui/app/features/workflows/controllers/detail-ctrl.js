3d36bad99248d58e2081ae4a87901733393f7d1d
Ajai <ajainarayanan@gmail.com>
2015-03-13 00:40:25 -0700

    Fixes workflow detail controller to use myWorkFlowApi correctly

--- cdap-ui/app/features/workflows/controllers/detail-ctrl.js
+++ cdap-ui/app/features/workflows/controllers/detail-ctrl.js
@@ -5,18 +5,19 @@ angular.module(PKG.name + '.feature.workflows')
           workflowId: $state.params.programId
         };
 
-    myWorkFlowApi.runs(angular.extend({status: 'running'}, params), function(res) {
+    var prom = myWorkFlowApi.runs(angular.extend({status: 'running'}, params))
+    prom.$promise.then(function(res) {
+        console.info("Getting Updates: ", res);
+        $scope.runs = res;
+        var count = 0;
+        angular.forEach(res, function(runs) {
+          if (runs.status === 'RUNNING') {
+            count += 1;
+          }
+        });
 
-      $scope.runs = res;
-      var count = 0;
-      angular.forEach(res, function(runs) {
-        if (runs.status === 'RUNNING') {
-          count += 1;
-        }
+        $scope.activeRuns = count;
       });
-
-      $scope.activeRuns = count;
-    });
 
 
     $scope.activeRuns = 0;
@@ -30,5 +31,10 @@ angular.module(PKG.name + '.feature.workflows')
       $scope.status = action;
       myWorkFlowApi[action](params);
     };
+
+    $scope.$on('$destroy', function() {
+      myWorkFlowApi.runsStop(params);
+      myWorkFlowApi.statusStop(params);
+    });
 
 });
