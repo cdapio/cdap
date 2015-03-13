bebcf6f883b93624bda96e46d8f07e4dd9cda2a5
Andrew Westlund <astewes@gmail.com>
2015-03-13 16:03:52 -0700

    adds specific functionality to myFlowGraph directive

--- cdap-ui/app/directives/flow-graph/flow.js
+++ cdap-ui/app/directives/flow-graph/flow.js
@@ -2,7 +2,6 @@ var module = angular.module(PKG.name+'.commons');
 
 var baseDirective = {
   restrict: 'E',
-  // replace: true,
   templateUrl: 'flow-graph/flow.html',
   scope: {
     model: '='
@@ -25,6 +24,26 @@ module.controller('myFlowController', function($scope, d3, dagreD3) {
     }
   }
 
+  $scope.instanceMap = {};
+  $scope.labelMap = {};
+
+  /**
+   * Radius for instances circle in flowlets. This is a determined as a factor of the size of the
+   * instances text.
+   */
+  $scope.getInstancesScaledRadius = function(instances, radius) {
+    var base = radius;
+    var extra = (instances.toString().length - 1) * base / 2;
+    return base + extra;
+  }
+
+  /**
+   * Gets number of instances from node map.
+   */
+  $scope.getInstances = function(nodeId) {
+    return $scope.instanceMap[nodeId].instances ? $scope.instanceMap[nodeId].instances : 0;
+  }
+
   $scope.$watch('model', update);
   $scope.$watchCollection('model.metrics', update);
 });
@@ -32,63 +51,37 @@ module.controller('myFlowController', function($scope, d3, dagreD3) {
 module.directive('myFlowGraph', function ($filter, $state) {
   return angular.extend({
     link: function (scope, elem, attr) {
-      scope.render = function () {
-        var nodes = scope.model.nodes;
-        var edges = scope.model.edges;
-        var instanceMap = {};
-        var labelMap = {};
-
-        var numberFilter = $filter('myNumber');
-
-        var renderer = new dagreD3.render();
-        var g = new dagreD3.graphlib.Graph();
-        var metricCircleRadius = 25;
-        var instanceCircleRadius = 10;
-        var flowletCircleRadius = 60;
-        // Since names are padded inside of shapes, this needs the same padding to be vertically center aligned.
-        var metricCountPadding = 5;
-
-        var streamDiagramWidth = 40;
-        var streamDiagramHeight = 30;
-        var instanceBufferHeight = 30;
-
-        g.setGraph({
-          nodesep: 60,
-          ranksep: 70,
-          rankdir: 'LR',
-          marginx: 30,
-          marginy: 30
-        })
-          .setDefaultEdgeLabel(function () { return {}; });
-
-        // First set nodes and edges.
-        angular.forEach(nodes, function (node) {
-          var nodeLabel = node.name.length > 8 ? node.name.substr(0, 5) + '...' : node.name;
-          instanceMap[node.name] = node;
-          labelMap[nodeLabel] = node;
-          if (node.type === 'STREAM') {
-            g.setNode(node.name, { shape: 'stream', label: nodeLabel});
-
-          } else {
-            g.setNode(node.name, { shape: 'flowlet', label: nodeLabel});
-          }
-        });
-
-        angular.forEach(edges, function (edge) {
-          g.setEdge(edge.sourceName, edge.targetName);
-        });
-
-        // Draw the flowlet shape.
-        renderer.shapes().flowlet = function(parent, bbox, node) {
-          var instances = getInstances(node.elem.__data__); // No other way to get name from node.
-          var instanceCircleScaled = getInstancesScaledRadius(instances, instanceCircleRadius);
+      scope.render = genericRender.bind(null, scope, $filter);
+      var metricCircleRadius = 25;
+      var instanceCircleRadius = 10;
+      var flowletCircleRadius = 60;
+      // Since names are padded inside of shapes, this needs the same padding to be vertically center aligned.
+      var metricCountPadding = 5;
+      var streamDiagramWidth = 40;
+      var streamDiagramHeight = 30;
+      var instanceBufferHeight = 30;
+      var numberFilter = $filter('myNumber');
+      scope.getShapes = function() {
+        var shapes = {};
+        shapes.flowlet = function(parent, bbox, node) {
+          var w = bbox.width;
+          var h = bbox.height;
+          var points = [
+            //clockwise points from top
+            { x: -streamDiagramWidth, y: streamDiagramHeight}, //e
+            { x: -streamDiagramWidth, y: -h - streamDiagramHeight}, //a
+            { x: w/2, y: -h - streamDiagramHeight}, //b
+            { x: w, y: -h/2}, //c
+            { x: w/2, y: streamDiagramHeight} //d
+          ];
+          var instances = scope.getInstances(node.elem.__data__); // No other way to get name from node.
+          var instanceCircleScaled = scope.getInstancesScaledRadius(instances, instanceCircleRadius);
           var shapeSvg = parent.insert('circle', ':first-child')
             .attr('x', -bbox.width / 2)
             .attr('y', -bbox.height / 2)
             .attr('r', flowletCircleRadius)
             .attr('class', 'flow-shapes foundation-shape flowlet-svg');
 
-          // Elements are positioned with respect to shapeSvg.
           parent.insert('circle')
             .attr('cx', flowletCircleRadius - instanceCircleScaled)
             .attr('cy', -instanceBufferHeight)
@@ -109,7 +102,7 @@ module.directive('myFlowGraph', function ($filter, $state) {
           parent.insert('text')
             .attr('x', - flowletCircleRadius)
             .attr('y', metricCountPadding)
-            .text(numberFilter(scope.model.metrics[labelMap[node.label].name]))
+            .text(numberFilter(scope.model.metrics[scope.labelMap[node.label].name]))
             .attr('class', 'flow5shapes flowlet-event-count');
 
           node.intersect = function(point) {
@@ -119,8 +112,7 @@ module.directive('myFlowGraph', function ($filter, $state) {
           return shapeSvg;
         };
 
-        // Draw the stream shape.
-        renderer.shapes().stream = function(parent, bbox, node) {
+        shapes.stream = function(parent, bbox, node) {
           var w = bbox.width,
           h = bbox.height,
           points = [
@@ -157,85 +149,29 @@ module.directive('myFlowGraph', function ($filter, $state) {
           return shapeSvg;
         };
 
-
-        // Set up an SVG group so that we can translate the final graph and tooltip.
-        var svg = d3.select('svg').attr('fill', 'white');
-        var svgGroup = d3.select('svg g');
-        var tip = d3.tip()
-          .attr('class', 'd3-tip')
-          .offset([-10, 0]);
-        svg.call(tip);
-
-        // Set up zoom support
-        var zoom = d3.behavior.zoom().on('zoom', function() {
-          svgGroup.attr('transform', 'translate(' + d3.event.translate + ')' +
-                                      'scale(' + d3.event.scale + ')');
-        });
-        // svg.call(zoom);
-
-        // Run the renderer. This is what draws the final graph.
-        renderer(d3.select('svg g'), g);
-
-        // Set up onclick after rendering.
-        svg
-          .selectAll('g.node')
-          .on('click', handleNodeClick);
-
-        svg
-          .selectAll('g.node text')
-          .on('mouseover', handleShowTip)
-          .on('mouseout', handleHideTip);
-
-
-        // Center svg.
-        var initialScale = 1.1;
-        var svgWidth = svg.node().getBoundingClientRect().width;
-        zoom
-          .translate([(svgWidth - g.graph().width * initialScale) / 2, 20])
-          .scale(initialScale)
-          .event(svg);
-        svg.attr('height', g.graph().height * initialScale + 40);
-
-        /**
-         * Radius for instances circle in flowlets. This is a determined as a factor of the size of the
-         * instances text.
-         */
-        function getInstancesScaledRadius(instances, radius) {
-          var base = radius;
-          var extra = (instances.toString().length - 1) * base / 2;
-          return base + extra;
-        }
-
-        /**
-         * Handles node click and sends to flowlet page.
-         */
-
-
-        /**
-         * Gets number of instances from node map.
-         */
-        function getInstances(nodeId) {
-          return instanceMap[nodeId].instances ? instanceMap[nodeId].instances : 0;
-        }
-
-        /**
-         * Handles showing tooltip on mouseover of node name.
-         */
-        function handleShowTip(nodeId) {
-          tip
-            .html(function(d) {
-              return '<strong>' + instanceMap[nodeId].type +':</strong> <span class="tip-node-name">'+ nodeId +'</span>';
-            })
-            .show();
-        }
-
-        /**
-         * Handles hiding tooltip on mouseout of node name.
-         */
-        function handleHideTip(nodeId) {
-          tip.hide();
-        }
+        return shapes;
       };
+
+      scope.getShape = function(name) {
+        var shapeName;
+
+        switch(name) {
+          case 'STREAM':
+            shapeName = 'stream';
+            break;
+          default:
+            shapeName = 'flowlet';
+            break;
+        }
+        return shapeName;
+      };
+
+      scope.handleNodeClick = function(nodeId) {
+        handleHideTip(nodeId);
+        var instance = instanceMap[nodeId];
+        $state.go('flows.detail.runs.tabs.status.flowletsDetail', {flowletId: nodeId});
+      };
+
     }
   }, baseDirective);
 });
@@ -323,18 +259,12 @@ module.directive('myWorkflowGraph', function ($filter, $state) {
   }, baseDirective);
 });
 
-
 function genericRender(scope, $filter) {
   var nodes = scope.model.nodes;
   var edges = scope.model.edges;
-  var instanceMap = {};
-  var labelMap = {};
-
-  var numberFilter = $filter('myNumber');
 
   var renderer = new dagreD3.render();
   var g = new dagreD3.graphlib.Graph();
-
 
   g.setGraph({
     nodesep: 60,
@@ -348,8 +278,8 @@ function genericRender(scope, $filter) {
   // First set nodes and edges.
   angular.forEach(nodes, function (node) {
     var nodeLabel = node.name.length > 8 ? node.name.substr(0, 5) + '...' : node.name;
-    instanceMap[node.name] = node;
-    labelMap[nodeLabel] = node;
+    scope.instanceMap[node.name] = node;
+    scope.labelMap[nodeLabel] = node;
     if (node.type === 'ACTION') {
       g.setNode(node.name, { shape: scope.getShape('ACTION'), label: nodeLabel});
     } else {
@@ -391,7 +321,6 @@ function genericRender(scope, $filter) {
     .on('mouseover', handleShowTip)
     .on('mouseout', handleHideTip);
 
-
   // Center svg.
   var initialScale = 1.1;
   var svgWidth = svg.node().getBoundingClientRect().width;
@@ -402,29 +331,12 @@ function genericRender(scope, $filter) {
   svg.attr('height', g.graph().height * initialScale + 40);
 
   /**
-   * Radius for instances circle in flowlets. This is a determined as a factor of the size of the
-   * instances text.
-   */
-  function getInstancesScaledRadius(instances, radius) {
-    var base = radius;
-    var extra = (instances.toString().length - 1) * base / 2;
-    return base + extra;
-  }
-
-  /**
-   * Gets number of instances from node map.
-   */
-  function getInstances(nodeId) {
-    return instanceMap[nodeId].instances ? instanceMap[nodeId].instances : 0;
-  }
-
-  /**
    * Handles showing tooltip on mouseover of node name.
    */
   function handleShowTip(nodeId) {
     tip
       .html(function(d) {
-        return '<strong>' + instanceMap[nodeId].type +':</strong> <span class="tip-node-name">'+ nodeId +'</span>';
+        return '<strong>' + scope.instanceMap[nodeId].type +':</strong> <span class="tip-node-name">'+ nodeId +'</span>';
       })
       .show();
   }
