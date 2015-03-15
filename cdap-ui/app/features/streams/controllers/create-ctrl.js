fd4d9426a3a7f0e15e61dabacdd5c72301171338
Ajai <ajainarayanan@gmail.com>
2015-03-15 11:36:09 -0700

    Adds the ability to add/remove properties in streams create modal

--- cdap-ui/app/features/streams/controllers/create-ctrl.js
+++ cdap-ui/app/features/streams/controllers/create-ctrl.js
@@ -1,5 +1,7 @@
 angular.module(PKG.name + '.feature.streams')
-  .controller('StreamsCreateController', function($scope) {
+  .controller('StreamsCreateController', function($scope, $filter) {
+    var i = 0,
+        filterFilter = $filter('filter');
     $scope.type = 'Time Series';
     $scope.id = 'Some Long string that is supposed to be ID';
     $scope.displayName = 'Stream1DisplayName';
@@ -14,4 +16,19 @@ angular.module(PKG.name + '.feature.streams')
         value: 'value2'
       }
     ];
+
+    $scope.addProperties = function() {
+      $scope.properties.push({
+        key: 'Property ' + i,
+        value: ''
+      });
+      i+=1;
+    };
+
+    $scope.removeProperty = function(property) {
+      var match = filterFilter($scope.properties, property);
+      if (match.length) {
+        $scope.properties.splice($scope.properties.indexOf(match[0]), 1);
+      }
+    };
   });
