bdc2a0a5ce959b28f8e6256302969c2334fbcc79
Edwin Elia <edwinselia@gmail.com>
2015-03-24 15:08:58 -0700

    create stream

--- cdap-ui/app/features/streams/controllers/create-ctrl.js
+++ cdap-ui/app/features/streams/controllers/create-ctrl.js
@@ -1,34 +1,62 @@
 angular.module(PKG.name + '.feature.streams')
-  .controller('StreamsCreateController', function($scope, $filter) {
+  .controller('StreamsCreateController', function($scope, $filter, MyDataSource, $modalInstance) {
+
+    var dataSrc = new MyDataSource($scope);
+
     var i = 0,
         filterFilter = $filter('filter');
-    $scope.type = 'Time Series';
-    $scope.id = 'Some Long string that is supposed to be ID';
-    $scope.displayName = 'Stream1DisplayName';
-    $scope.description = '';
-    $scope.properties = [
-      {
-        key: 'key1',
-        value: 'value1'
-      },
-      {
-        key: 'key2',
-        value: 'value2'
-      }
-    ];
+    // $scope.type = 'Time Series';
+    // $scope.id = 'Some Long string that is supposed to be ID';
+    $scope.displayName = '';
+    // $scope.properties = [
+    //   {
+    //     key: 'key1',
+    //     value: 'value1'
+    //   },
+    //   {
+    //     key: 'key2',
+    //     value: 'value2'
+    //   }
+    // ];
 
-    $scope.addProperties = function() {
-      $scope.properties.push({
-        key: 'Property ' + i,
-        value: ''
-      });
-      i+=1;
+    // $scope.addProperties = function() {
+    //   $scope.properties.push({
+    //     key: 'Property ' + i,
+    //     value: ''
+    //   });
+    //   i+=1;
+    // };
+
+    // $scope.removeProperty = function(property) {
+    //   var match = filterFilter($scope.properties, property);
+    //   if (match.length) {
+    //     $scope.properties.splice($scope.properties.indexOf(match[0]), 1);
+    //   }
+    // };
+
+
+
+    $scope.createStream = function() {
+      dataSrc
+        .request({
+          _cdapNsPath: '/streams/' + $scope.displayName,
+          method: 'PUT'
+        })
+        .then(function(res) {
+          console.log('res',res);
+          $modalInstance.close(res);
+        }, function(err) {
+          console.log('err', err);
+          $scope.error = err;
+        });
     };
 
-    $scope.removeProperty = function(property) {
-      var match = filterFilter($scope.properties, property);
-      if (match.length) {
-        $scope.properties.splice($scope.properties.indexOf(match[0]), 1);
-      }
+    $scope.closeModal = function() {
+      $modalInstance.close();
     };
+
+    $scope.emptyInput = function() {
+      return $scope.displayName.length === 0;
+    };
+
   });
