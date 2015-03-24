64652a3b1520dee4f5cec27a10329ca293133803
Edwin Elia <edwinselia@gmail.com>
2015-03-24 15:19:13 -0700

    code cleanup

--- cdap-ui/app/features/streams/controllers/create-ctrl.js
+++ cdap-ui/app/features/streams/controllers/create-ctrl.js
@@ -1,62 +1,26 @@
 angular.module(PKG.name + '.feature.streams')
-  .controller('StreamsCreateController', function($scope, $filter, MyDataSource, $modalInstance) {
+  .controller('StreamsCreateController', function($scope, MyDataSource, $modalInstance) {
 
     var dataSrc = new MyDataSource($scope);
 
-    var i = 0,
-        filterFilter = $filter('filter');
-    // $scope.type = 'Time Series';
-    // $scope.id = 'Some Long string that is supposed to be ID';
-    $scope.displayName = '';
-    // $scope.properties = [
-    //   {
-    //     key: 'key1',
-    //     value: 'value1'
-    //   },
-    //   {
-    //     key: 'key2',
-    //     value: 'value2'
-    //   }
-    // ];
-
-    // $scope.addProperties = function() {
-    //   $scope.properties.push({
-    //     key: 'Property ' + i,
-    //     value: ''
-    //   });
-    //   i+=1;
-    // };
-
-    // $scope.removeProperty = function(property) {
-    //   var match = filterFilter($scope.properties, property);
-    //   if (match.length) {
-    //     $scope.properties.splice($scope.properties.indexOf(match[0]), 1);
-    //   }
-    // };
-
-
+    $scope.streamId = '';
 
     $scope.createStream = function() {
       dataSrc
         .request({
-          _cdapNsPath: '/streams/' + $scope.displayName,
+          _cdapNsPath: '/streams/' + $scope.streamId,
           method: 'PUT'
         })
         .then(function(res) {
-          console.log('res',res);
           $modalInstance.close(res);
         }, function(err) {
-          console.log('err', err);
           $scope.error = err;
         });
     };
 
     $scope.closeModal = function() {
       $modalInstance.close();
-    };
 
-    $scope.emptyInput = function() {
-      return $scope.displayName.length === 0;
     };
 
   });
