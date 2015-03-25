1d74e082e3f9f1833b7f2e24ac81a7e07a06237d
Ajai <ajainarayanan@gmail.com>
2015-03-25 12:00:11 -0700

    Adds a note to loading service about event broadcast on rootscope

--- cdap-ui/app/services/app-level-loading-service.js
+++ cdap-ui/app/services/app-level-loading-service.js
@@ -6,6 +6,10 @@ angular.module(PKG.name + '.services')
         return deferred.promise;
       } else {
         deferred = $q.defer();
+        // If we are going to use a lot of event system
+        // then we need a service thats handles events
+        // instead of broadcasting it on $rootScope and
+        // making all scopes to inherit it
         $rootScope.$broadcast('showLoadingIcon');
         deferred.resolve(true);
         return deferred.promise;
@@ -16,6 +20,7 @@ angular.module(PKG.name + '.services')
       if (!deferred) {
         return $q.when(true);
       } else {
+        // Same applies here as mentioned in the above comment.
         $rootScope.$broadcast('hideLoadingIcon');
         deferred.resolve(true);
         deferred = null;
