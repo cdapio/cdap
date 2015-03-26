3d69854af5fcca18957f7994d7c82f541c2317c1
Gokul Gunasekaran <gokul@cask.co>
2015-03-25 20:47:00 -0700

    Removing setResources from SourceContext since it is not applicable

--- cdap-app-templates/cdap-etl/cdap-etl-api/src/main/java/co/cask/cdap/templates/etl/api/realtime/SourceContext.java
+++ cdap-app-templates/cdap-etl/cdap-etl-api/src/main/java/co/cask/cdap/templates/etl/api/realtime/SourceContext.java
@@ -16,7 +16,6 @@
 
 package co.cask.cdap.templates.etl.api.realtime;
 
-import co.cask.cdap.api.Resources;
 import co.cask.cdap.api.RuntimeContext;
 
 /**
@@ -44,11 +43,4 @@ public interface SourceContext extends RuntimeContext {
    * @return instance count
    */
   int getInstanceCount();
-
-  /**
-   * Overrides the resources, such as memory and virtual cores, to use for the execution.
-   *
-   * @param resources Resources to be used for execution
-   */
-  void setResources(Resources resources);
 }
