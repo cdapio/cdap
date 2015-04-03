bd85bcfbcf1df2bedfe7f76f5fd45c615a825eba
Alvin Wang <alvin@cask.co>
2015-04-02 20:02:20 -0700

    Added GaugeStats

---
+++ cdap-watchdog/src/main/java/co/cask/cdap/metrics/stats/GaugeStats.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.metrics.stats;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+/**
+ * Computes statistics for a gauge.
+ */
+public class GaugeStats {
+
+  private Multiset<Long> values;
+
+  private long min;
+  private long max;
+  private long count;
+
+  public GaugeStats() {
+    values = HashMultiset.create();
+    count = 0;
+    min = Long.MAX_VALUE;
+    max = Long.MIN_VALUE;
+  }
+
+  public void gauge(long value) {
+    values.add(value);
+    min = Math.min(min, value);
+    max = Math.max(max, value);
+    count++;
+  }
+
+  public long getCount() {
+    return count;
+  }
+
+  public float getAverage() {
+    long sum = 0;
+    for (Long value : values) {
+      int valueCount = values.count(value);
+      sum += value * valueCount;
+    }
+
+    return sum * 1.0f / count;
+  }
+
+  public float getMin() {
+    return min;
+  }
+
+  public float getMax() {
+    return max;
+  }
+}
