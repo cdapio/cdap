118b626868d8d4dba52b6998e8c0dd93f34d58ae
Gokul Gunasekaran <gokul@cask.co>
2015-04-06 15:57:44 -0700

    Remove unused GSON

--- cdap-app-templates/cdap-etl/cdap-etl-batch/src/test/java/co/cask/cdap/templates/etl/batch/ETLMapReduceTest.java
+++ cdap-app-templates/cdap-etl/cdap-etl-batch/src/test/java/co/cask/cdap/templates/etl/batch/ETLMapReduceTest.java
@@ -27,7 +27,6 @@ import co.cask.cdap.test.TestBase;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -39,8 +38,6 @@ import java.util.concurrent.TimeUnit;
  * Tests for {@link ETLBatchTemplate}.
  */
 public class ETLMapReduceTest extends TestBase {
-
-  private static final Gson GSON = new Gson();
 
   @Test
   public void testConfig() throws Exception {
