afa585f6badd7653ae78135456b91d4873130127
Edwin Elia <edwinselia@gmail.com>
2015-04-07 13:31:32 -0700

    change popover width

--- cdap-ui/app/services/alert.js
+++ cdap-ui/app/services/alert.js
@@ -1,6 +1,6 @@
 angular.module(PKG.name+'.services')
   .service('myAlert', function($rootScope){
-    var __list = [{title: 500, content: 'this is a long error suchAsThisNeedsToBeBrokenIntoSeparateLines'}];
+    var __list = [];
     function alert(item) {
       if (angular.isObject(item) && Object.keys(item).length) {
         __list.push(item);
