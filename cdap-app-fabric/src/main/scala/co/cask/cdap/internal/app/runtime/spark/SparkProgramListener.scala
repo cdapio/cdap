930a79dd1d10656761d863628dbb0e8a7b70f487
Terence Yim <terence@cask.co>
2015-04-09 10:48:33 -0700

    Implements SparkListener using Scala

---
+++ cdap-app-fabric/src/main/scala/co/cask/cdap/internal/app/runtime/spark/SparkProgramListener.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.internal.app.runtime.spark
+
+import org.apache.spark.scheduler.{JobSucceeded, SparkListener, SparkListenerJobEnd}
+
+/**
+ * Listens to events on running Spark Programs. Currently this listener only perform operations
+ * on SparkListener.onJobEnd.
+ */
+class SparkProgramListener extends SparkListener {
+
+  override def onJobEnd(jobEnd: SparkListenerJobEnd) {
+
+    jobEnd.jobResult match {
+      case JobSucceeded => SparkProgramWrapper.setSparkProgramSuccessful(true)
+      case _ => SparkProgramWrapper.setSparkProgramSuccessful(false)
+    }
+  }
+}
