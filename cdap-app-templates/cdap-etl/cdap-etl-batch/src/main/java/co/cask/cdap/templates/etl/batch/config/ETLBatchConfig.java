e88170ded3462710697c710253e28a8aa8db7e43
Albert Shau <albert@cask.co>
2015-04-10 11:16:51 -0700

    CDAP-1756 add missing file

---
+++ cdap-app-templates/cdap-etl/cdap-etl-batch/src/main/java/co/cask/cdap/templates/etl/batch/config/ETLBatchConfig.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.templates.etl.batch.config;
+
+import co.cask.cdap.templates.etl.api.config.ETLStage;
+import com.google.common.base.Objects;
+
+import java.util.List;
+
+/**
+ * ETL Adapter Configuration.
+ */
+public final class ETLBatchConfig {
+  private final String schedule;
+  private final ETLStage source;
+  private final ETLStage sink;
+  private final List<ETLStage> transforms;
+
+  public ETLBatchConfig(String schedule, ETLStage source, ETLStage sink, List<ETLStage> transforms) {
+    this.schedule = schedule;
+    this.source = source;
+    this.sink = sink;
+    this.transforms = transforms;
+  }
+
+  public String getSchedule() {
+    return schedule;
+  }
+
+  public ETLStage getSource() {
+    return source;
+  }
+
+  public ETLStage getSink() {
+    return sink;
+  }
+
+  public List<ETLStage> getTransforms() {
+    return transforms;
+  }
+}
