379826e70bdca92574778e872e19860ea3708c58
Ajai <ajainarayanan@gmail.com>
2015-04-17 18:58:03 -0700

    Fixes flows feature to include logs in the run level view

--- cdap-ui/app/features/flows/controllers/tabs/runs/tabs/log-ctrl.js
+++ cdap-ui/app/features/flows/controllers/tabs/runs/tabs/log-ctrl.js
@@ -2,12 +2,14 @@ angular.module(PKG.name + '.feature.flows')
   .controller('FlowsRunDetailLogController', function($scope, MyDataSource, $state) {
 
     var dataSrc = new MyDataSource($scope),
-        basePath = '/apps/' + $state.params.appId + '/flows/' + $state.params.programId;
+        basePath = '/apps/' + $state.params.appId +
+                   '/flows/' + $state.params.programId +
+                   '/runs/' + $scope.runs.selected.runid;
 
     $scope.logs = [];
 
     dataSrc.poll({
-      _cdapNsPath: basePath + '/logs/next?maxSize=50'
+      _cdapNsPath: basePath + '/logs/next?max=50'
     }, function(res) {
       $scope.logs = res;
     });
