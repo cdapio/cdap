9e385c95d23eee451ee3fd0db996dc8bc9e6d782
Edwin Elia <edwinselia@gmail.com>
2015-04-20 11:04:48 -0700

    initial implementation

--- cdap-ui/app/features/explore/explore.js
+++ cdap-ui/app/features/explore/explore.js
@@ -11,15 +11,30 @@ angular.module(PKG.name + '.feature.explore')
       _cdapNsPath: '/data/datasets'
     }).then(function(res) {
       $scope.dataList = $scope.dataList.concat(res);
+      console.log('res', res);
+      // initializing
+      $scope.type = 'dataset';
+      $scope.name = res[0].name;
     });
 
     dataSrc.request({
       _cdapNsPath: '/streams'
     }).then(function(res) {
+      angular.forEach(res, function(s) {
+        dataSrc.request({
+          _cdapNsPath: '/streams/' + s.name
+        }).then(function(properties) {
+          s.properties = properties;
+        });
+      });
+
       $scope.dataList = $scope.dataList.concat(res);
     });
 
     $scope.click = function (data) {
+      $scope.selected = data;
+      console.log('data', JSON.parse(data.properties));
+
 
       if (data.type === 'Stream') {
         $scope.type = 'stream';
