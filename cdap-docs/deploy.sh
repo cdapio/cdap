6383e182fac01472a5dba4dc05aaa4c278457e43
David Bajot <dbajot@cask.co>
2015-04-19 17:16:54 -0700

    add logic for web directories based on branches and optional test directory

--- cdap-docs/deploy.sh
+++ cdap-docs/deploy.sh
@@ -18,7 +18,14 @@
 # Deployment script for docs
 # Deploys zip files created by build scripts
 #
-# 'PROJECT' (e.g. cdap) environment variable must be set or the script will exit
+# DOC_DIR is an optional directory to use instead of derived branch name. E.g.:
+#   if branch name=feature/docs-build-testing, and we want to use testdir
+#   we would set DOC_DIR=testdir and add it to the bamboo tasks' variables
+#   and the remote directory would be 2.8.1/testdir (testdir treated as branch)
+# If DOC_DIR is not set, bamboo.planRepository.<position>.branch will be used,
+#   e.g. /var/www/html/cdap/2.8.1/feature-myfix, unless the branch name
+#   is release/* or develop/* in which case, it will just be put into
+#   the version directory, e.g. /var/www/html/cdap/2.8.1
 #
 # function variables reference: (these were kept consistent throughout)
 # $1 user name (typically bamboo)
@@ -28,41 +35,6 @@
 # $5 local path to zip archive file
 ################################################################################
 
-function get_version () {
-  TMP_VERSION=`grep "<version>" ../pom.xml`
-  TMP_VERSION=${TMP_VERSION#*<version>}
-  VERSION=${TMP_VERSION%%</version>*}
-}
-
-get_version
-
-# parameters that can be passed to this script (as environment variables)
-DEBUG=${DEBUG:-no}
-DEPLOY_TO_STG=${DEPLOY_TO_STG:-no}
-DEPLOY_TO_DOCS=${DEPLOY_TO_DOCS:-no}
-PROJECT=${PROJECT}   # mandatory
-DOC_DIR=${DOC_DIR:-${VERSION}}
-BUILD_WORKING_DIR=${BUILD_WORKING_DIR:-/var/bamboo/xml-data/build-dir/CDAP-DRBD-JOB1}
-DOCS_SERVER1=${DOCS_SERVER1:-docs1.cask.co}
-DOCS_SERVER2=${DOCS_SERVER2:-docs2.cask.co}
-STG_SERVER=${STG_SERVER:-docs-staging.cask.co}
-REMOTE_STG_BASE=${REMOTE_STG_BASE:-/var/www/html/cdap}
-REMOTE_DOCS_BASE=${REMOTE_DOCS_BASE:-/var/www/docs/cdap}
-
-#
-USER=bamboo
-PROJECT_DOCS=${PROJECT}-docs
-WEB_FILE=${PROJECT}-docs-${VERSION}-web.zip
-GITHUB_FILE=${PROJECT}-docs-${VERSION}-github.zip
-FILE_PATH=${BUILD_WORKING_DIR}/${PROJECT}/${PROJECT_DOCS}/build
-DOCS_SERVERS="${DOCS_SERVER1} ${DOCS_SERVER2}"
-REMOTE_STG_DIR="${REMOTE_STG_BASE}/${DOC_DIR}"
-REMOTE_DOCS_DIR="${REMOTE_DOCS_BASE}/${DOC_DIR}"
-
-RSYNC_OPTS='-aPh'
-SSH_OPTS='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-RSYNC_PATH='sudo rsync'
-
 # output trimmer
 decho () {
   if [[ "${DEBUG}" == 'yes' ]]; then
@@ -71,6 +43,67 @@ decho () {
     RSYNC_QUIET='--quiet'
   fi
 }
+
+# set the mandatory variables first (from info in project's main pom.xml)
+function get_version () {
+  TMP_VERSION=`grep "<version>" ../pom.xml`
+  TMP_VERSION=${TMP_VERSION#*<version>}
+  VERSION=${TMP_VERSION%%</version>*}
+}
+
+function get_project () {
+  TMP_PROJECT=`grep "<artifactId>" ../pom.xml`
+  TMP_PROJECT=${TMP_PROJECT#*<artifactId>}
+  PROJECT=${TMP_PROJECT%%</artifactId>*}
+}
+
+function convert_branch_name () {
+  decho "convert '/' to '-' in branch name"
+  DOC_DIR=`echo $DOC_DIR | tr '/' '-'`
+}
+
+set_remote_dir () {
+  DOC_DIR=${DOC_DIR:-${BRANCH_NAME}}
+  convert_branch_name
+  echo "DOC_DIR=${DOC_DIR}"
+  if [[ "${DOC_DIR}" == release* || "${DOC_DIR}" == develop* ]]; then
+    DIR=''
+  else
+    DIR=branches/${DOC_DIR}
+  fi
+  echo "SUBDIR=${DIR}"
+}
+
+# parameters that can be passed to this script (as environment variables)
+DEBUG=${DEBUG:-no}
+DEPLOY_TO_STG=${DEPLOY_TO_STG:-no}
+DEPLOY_TO_DOCS=${DEPLOY_TO_DOCS:-no}
+BUILD_WORKING_DIR=${BUILD_WORKING_DIR:-/var/bamboo/xml-data/build-dir/CDAP-DRBD-JOB1}
+DOCS_SERVER1=${DOCS_SERVER1:-docs1.cask.co}
+DOCS_SERVER2=${DOCS_SERVER2:-docs2.cask.co}
+STG_SERVER=${STG_SERVER:-docs-staging.cask.co}
+REMOTE_STG_BASE=${REMOTE_STG_BASE:-/var/www/html/cdap}
+REMOTE_DOCS_BASE=${REMOTE_DOCS_BASE:-/var/www/docs/cdap}
+
+decho "DOC_DIR=${DOC_DIR}"
+decho "BRANCH_NAME=${BRANCH_NAME}"
+
+get_version
+get_project
+set_remote_dir
+#
+USER=bamboo
+PROJECT_DOCS=${PROJECT}-docs
+WEB_FILE=${PROJECT}-docs-${VERSION}-web.zip
+GITHUB_FILE=${PROJECT}-docs-${VERSION}-github.zip
+FILE_PATH=${BUILD_WORKING_DIR}/${PROJECT}/${PROJECT_DOCS}/build
+DOCS_SERVERS="${DOCS_SERVER1} ${DOCS_SERVER2}"
+REMOTE_STG_DIR="${REMOTE_STG_BASE}/${DIR}"
+REMOTE_DOCS_DIR="${REMOTE_DOCS_BASE}/${DIR}"
+
+RSYNC_OPTS='-aPh'
+SSH_OPTS='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+RSYNC_PATH='sudo rsync'
 
 die ( ) { echo ; echo "ERROR: ${*}" ; echo ; exit 1; }
 
