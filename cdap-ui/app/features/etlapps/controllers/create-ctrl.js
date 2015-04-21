afdfcfaeb941eebc52c55faab3a9cfe67cc1b44b
Ajai <ajainarayanan@gmail.com>
2015-04-21 14:39:39 -0700

    Adds the ability to delete a transform from etl adapter create view

--- cdap-ui/app/features/etlapps/controllers/create-ctrl.js
+++ cdap-ui/app/features/etlapps/controllers/create-ctrl.js
@@ -196,6 +196,19 @@ angular.module(PKG.name + '.feature.etlapps')
       });
     };
 
+    $scope.deleteTrasformProperty = function(transform) {
+      var filterFilter = $filter('filter');
+      var index = $scope.transforms.indexOf(transform);
+      $scope.transforms.splice(index, 1);
+      if (!$scope.transforms.length) {
+        $scope.transforms.push({
+          name: 'Add a Transforms',
+          placeHolderTransform: true,
+          properties: {}
+        });
+      }
+    };
+
     $scope.doSave = function() {
       var transforms = [],
           i;
@@ -249,6 +262,10 @@ angular.module(PKG.name + '.feature.etlapps')
 
     $scope.saveAsDraft = function() {
       if (!$scope.metadata.name.length) {
+        $alert({
+          type: info,
+          content: 'Please provide a name for the Adapter to be saved as draft'
+        });
         return;
       }
       $scope.etlDrafts[$scope.metadata.name] = {
