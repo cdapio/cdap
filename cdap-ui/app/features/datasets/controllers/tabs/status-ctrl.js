c331e3df56a9f30b04015c86474dace5564a3cbe
Edwin Elia <edwinselia@gmail.com>
2015-04-22 12:58:13 -0700

    add schema on dataset

--- cdap-ui/app/features/datasets/controllers/tabs/status-ctrl.js
+++ cdap-ui/app/features/datasets/controllers/tabs/status-ctrl.js
@@ -1,9 +1,10 @@
 angular.module(PKG.name + '.feature.datasets')
   .controller('CdapDatasetDetailStatusController',
-    function($scope, MyDataSource, $state, myHelpers, $log) {
+    function($scope, MyDataSource, $state, myHelpers) {
       $scope.writes = 0;
       $scope.reads = 0;
       $scope.transactions = 0;
+      var query = myHelpers.objectQuery;
       var dataSrc = new MyDataSource($scope),
           currentDataset = $state.params.datasetId;
 
@@ -30,59 +31,25 @@ angular.module(PKG.name + '.feature.datasets')
                     '&context=namespace.' +
                     $state.params.namespace +
                     '.dataset.' +
-                    $state.params.datasetId +
+                    currentDataset +
                     '&start=now-1s&end=now-1s&resolution=1s';
 
         dataSrc.poll({
           _cdapPath : path ,
           method: 'POST'
         }, function(metricData) {
-          var data = myHelpers.objectQuery(metricData, 'series', 0, 'data', 0, 'value');
+          var data = query(metricData, 'series', 0, 'data', 0, 'value');
           $scope[metric.scopeProperty] = data;
         });
       }
 
-      var query = myHelpers.objectQuery;
-      // Temporary API until we get a status API for each dataset.
       dataSrc.request({
-        _cdapNsPath: '/data/datasets'
+        _cdapNsPath: '/data/explore/tables/dataset_' + currentDataset + '/info'
       })
         .then(function(res) {
-          var match = res.filter(function(ds) {
-            return ds.name.indexOf(currentDataset) > -1;
-          });
-          try {
-            $scope.schema = (
-              query(JSON.parse( query(match, 0, 'properties', 'schema') || null ),'fields') || []
-            )
-              .map(function(field) {
-                if (angular.isArray(field.type)) {
-                  field.type = flattenFieldType(field.type);
-                }
-                return field;
-              });
-          } catch(e) {
-            $log.error('Parsing schema for datasets failed! - ' + e);
-          }
+
+          $scope.schema = query(res, 'schema');
+
         });
 
-      function flattenFieldType(type) {
-        return type.reduce(function(prev, curr) {
-          if (angular.isArray(prev)) {
-            // Field of type null | (some.table.field.name | null)
-            return curr + ' | ' + flattenFieldType(prev);
-          } else if (angular.isObject(prev)) {
-            if (prev.items) {
-              // Field of type null | ([some.table.field.name | some.table.otherfield.name ] | null)
-              return curr + ' | ('+ flattenFieldType(prev.items) + ')';
-            } else {
-              // Field of type null | some.table.field.name [some.table.field.type]. The some.table.field.name could be a foreign key?
-              return curr + ' | ' + prev.name + ' [' + prev.type + '] ';
-            }
-          } else {
-            // Field of type null | string
-            return curr + ' | ' + prev;
-          }
-        });
-      }
   });
