64d3501a4c6fd219bd20a7e0a6ee2afab3a9f94c
Ajai <ajainarayanan@gmail.com>
2015-04-23 13:02:47 -0700

    Fixes workflows detail view to show workflow diagram with no runs + moves controller to run-detail template

--- cdap-ui/app/features/workflows/controllers/tabs/runs/runs-detail-ctrl.js
+++ cdap-ui/app/features/workflows/controllers/tabs/runs/runs-detail-ctrl.js
@@ -1,5 +1,22 @@
-angular.module(PKG.name + '.feature.flows')
-  .controller('WorkflowsRunsDetailController', function($scope) {
+angular.module(PKG.name + '.feature.workflows')
+  .controller('WorkflowsRunsDetailController', function($scope, MyDataSource, $state) {
+    var dataSrc = new MyDataSource($scope),
+        basePath = '/apps/' + $state.params.appId + '/workflows/' + $state.params.programId;;
+    $scope.status = null;
+    $scope.duration = null;
+    $scope.startTime = null;
+
+    if ($scope.runs.length) {
+      dataSrc.poll({
+        _cdapNsPath: basePath + '/runs/' + $scope.runs.selected.runid
+      }, function(res) {
+          startMs = res.start * 1000;
+          $scope.startTime = new Date(startMs);
+          $scope.status = res.status;
+          $scope.duration = (res.end ? (res.end * 1000) - startMs : 0);
+          console.info($scope.startTime, $scope.status, $scope.duration);
+      });
+    }
 
     $scope.tabs = [{
       title: 'Status',
@@ -13,7 +30,7 @@ angular.module(PKG.name + '.feature.flows')
     $scope.activeTab = $scope.tabs[0];
 
     $scope.$on('$destroy', function(event) {
-      event.targetScope.runs.selected = null;
+      event.currentScope.runs.selected = null;
     });
 
     $scope.selectTab = function(tab) {
