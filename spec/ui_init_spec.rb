457bc7e44706eff9535ad0f151e221fce9da335d
dereklwood <derek@continuuity.com>
2015-04-22 18:50:48 -0700

    add ui tests, cloned from web-app

---
+++ spec/ui_init_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'cdap::ui_init' do
+  context 'on Centos 6.5 x86_64' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
+        node.automatic['domain'] = 'example.com'
+        stub_command('test -e /usr/bin/node').and_return(true)
+      end.converge(described_recipe)
+    end
+
+    it 'does not run execute[generate-ui-ssl-cert]' do
+      expect(chef_run).not_to run_execute('generate-ui-ssl-cert')
+    end
+  end
+
+  context 'with SSL' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'centos', version: 6.5) do |node|
+        node.override['cdap']['cdap_site']['ssl.enabled'] = true
+        stub_command('test -e /usr/bin/node').and_return(true)
+      end.converge(described_recipe)
+    end
+
+    it 'executes generate-ui-ssl-cert' do
+      expect(chef_run).to run_execute('generate-ui-ssl-cert')
+    end
+  end
+end
