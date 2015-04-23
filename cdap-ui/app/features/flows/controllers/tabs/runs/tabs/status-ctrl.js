218590046d635497097d2d1c2adb961deb5d3390
Ajai <ajainarayanan@gmail.com>
2015-04-23 14:02:01 -0700

    Fixes flows detail view for no runs

--- cdap-ui/app/features/flows/controllers/tabs/runs/tabs/status-ctrl.js
+++ cdap-ui/app/features/flows/controllers/tabs/runs/tabs/status-ctrl.js
@@ -4,20 +4,11 @@ angular.module(PKG.name + '.feature.flows')
     var dataSrc = new MyDataSource($scope),
         basePath = '/apps/' + $state.params.appId + '/flows/' + $state.params.programId;
 
-    if ($state.params.runid) {
-      var match = filterFilter($scope.runs, {runid: $state.params.runid});
-      if (match.length) {
-        $scope.runs.selected = match[0];
-      }
-    }
     var metricStreamPath = '/metrics/query?metric=system.collect.events' +
                            '&context=namespace.' +
                            $state.params.namespace +
                            '.stream.';
     $scope.data = {};
-    $scope.status = null;
-    $scope.duration = null;
-    $scope.startTime = null;
 
     FlowDiagramData.fetchData($state.params.appId, $state.params.programId)
       .then(function(data) {
@@ -26,21 +17,13 @@ angular.module(PKG.name + '.feature.flows')
       });
 
     if ($scope.runs.length) {
-      var metricFlowletPath = '/metrics/query?metric=system.process.events.processed' +
+      metricFlowletPath = '/metrics/query?metric=system.process.events.processed' +
                             '&context=ns.' +
                             $state.params.namespace +
                             '.app.' + $state.params.appId +
                             '.flow.' + $state.params.programId +
                             '.run.' + $scope.runs.selected.runid +
                             '.flowlet.';
-      dataSrc.poll({
-        _cdapNsPath: basePath + '/runs/' + $scope.runs.selected.runid
-      }, function(res) {
-        var startMs = res.start * 1000;
-          $scope.startTime = new Date(startMs);
-          $scope.status = res.status;
-          $scope.duration = (res.end ? (res.end * 1000) - startMs : 0);
-        });
     }
 
     function pollMetrics() {
