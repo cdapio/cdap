03285adb5561abad9b2c2fb04d11be1ce10616fa
Ajai <ajainarayanan@gmail.com>
2015-04-25 16:50:05 -0700

    Minor code cleanup to widget-container directive

--- cdap-ui/app/directives/widget-container/widget-container.js
+++ cdap-ui/app/directives/widget-container/widget-container.js
@@ -10,28 +10,34 @@ angular.module(PKG.name + '.commons')
       },
       replace: false,
       link: function (scope, element, attrs) {
-        var angularElement;
-        switch(scope.myconfig[scope.name].type) {
+        var angularElement,
+            infoElement;
+        switch(scope.myconfig.widget) {
           case 'textbox':
             angularElement = angular.element('<input class="form-control" ng-model="model"/>');
-            angularElement.attr('placeholder', scope.myconfig[scope.name].info);
-            element.append(angularElement);
+            angularElement.attr('placeholder', scope.myconfig.description);
             break;
           case 'password':
             angularElement = angular.element('<input class="form-control" ng-model="model"/>');
             angularElement.attr('type', 'password');
-            angularElement.attr('placeholder', scope.myconfig[scope.name].info);
-            element.append(angularElement);
+            angularElement.attr('placeholder', scope.myconfig.info);
             break;
           case 'json-editor':
-            angularElement = angular.element('<textarea class="form-control" cask-json-edit="model"></textarea>');
-            angularElement.attr('placeholder', scope.myconfig[scope.name].info);
-            element.append(angularElement);
+            angularElement = angular.element('<textarea style="resize: vertical;" class="form-control" cask-json-edit="model"></textarea>');
+            angularElement.attr('placeholder', scope.myconfig.info);
             break;
           case 'javascript-editor':
             angularElement = angular.element('<div style="width: 400px;height: 300px;" ui-ace></div>');
-            element.append(angularElement);
             break;
+          case 'default':
+            angularElement = angular.element('<input ng-model="model"/ >');
+            break;
+        }
+
+        element.append(angularElement);
+        if (scope.myconfig.info) {
+          infoElement = angular.element('<span class="fa fa-exclamation-circle text-info" tooltip="{{myconfig.info}}"></span>');
+          element.append(infoElement);
         }
         element.removeAttr('widget-container');
         $compile(element)(scope);
