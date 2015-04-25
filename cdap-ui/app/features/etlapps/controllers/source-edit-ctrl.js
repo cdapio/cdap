139924ac9d5ba2aa2c863657f2165f06c395aa76
Ajai <ajainarayanan@gmail.com>
2015-04-25 16:51:29 -0700

    Removes code from plugin-config-factory based on changes to config json format

--- cdap-ui/app/features/etlapps/controllers/source-edit-ctrl.js
+++ cdap-ui/app/features/etlapps/controllers/source-edit-ctrl.js
@@ -1,21 +1,9 @@
 angular.module(PKG.name + '.feature.etlapps')
   .controller('SourceEditController', function($scope, MyDataSource, PluginConfigFactory) {
-    $scope.somethingfetched = false;
+    $scope.configfetched = false;
     PluginConfigFactory.fetch($scope, 'etlRealtime', 'TwitterSource')
       .then(function(res) {
-        var something = {};
-         var combined = angular.extend({}, res.groups.group1.fields, res.groups.group2.fields);
-         angular.forEach(combined, function(value, key) {
-           something[key] = {
-             description: value['widget-description'],
-             info: value['widget-info'],
-             label: value['widget-label'],
-             properties: value['widget-properties'],
-             type: value['widget-type']
-           };
-         });
-         $scope.somethingfetched = true;
-         $scope.something = something;
-         console.info("Something:", something);
+        $scope.configfetched = true;
+        $scope.config = res;
       });
   });
