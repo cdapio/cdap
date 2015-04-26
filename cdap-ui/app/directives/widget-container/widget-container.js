1cf91968fda6170c2c3189bd72eb5a63b1960155
Ajai <ajainarayanan@gmail.com>
2015-04-25 17:35:02 -0700

    Removes un-used code from widgetcontainer directive + modifies TwitterSource.json

--- cdap-ui/app/directives/widget-container/widget-container.js
+++ cdap-ui/app/directives/widget-container/widget-container.js
@@ -1,8 +1,5 @@
 angular.module(PKG.name + '.commons')
   .directive('widgetContainer', function($compile) {
-    var registry = {
-      'textbox': 'textbox'
-    };
 
     return {
       restrict: 'A',
@@ -23,11 +20,6 @@ angular.module(PKG.name + '.commons')
           case 'password':
             angularElement = angular.element('<input class="form-control" ng-model="model"/>');
             angularElement.attr('type', 'password');
-            angularElement.attr('placeholder', scope.myconfig[scope.name].info);
-            element.append(angularElement);
-            break;
-          case 'textarea':
-            angularElement = angular.element('<textarea class="form-control" ng-model="model"></textarea>');
             angularElement.attr('placeholder', scope.myconfig[scope.name].info);
             element.append(angularElement);
             break;
