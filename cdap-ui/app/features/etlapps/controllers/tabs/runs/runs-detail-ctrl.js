6e384cd915c06c3747c233edc972debec2ec2d3e
Ajai <ajainarayanan@gmail.com>
2015-04-27 22:18:19 -0700

    Adds run detail status + log view for adapter feature

---
+++ cdap-ui/app/features/etlapps/controllers/tabs/runs/runs-detail-ctrl.js
@@ -0,0 +1,45 @@
+angular.module(PKG.name + '.feature.etlapps')
+  .controller('AdapterRunDetailController', function(MyDataSource, $filter, $state, $scope) {
+    var dataSrc = new MyDataSource($scope),
+        filterFilter = $filter('filter'),
+        basePath = '/adapters/' + $state.params.adapterId;
+
+    $scope.status = null;
+    $scope.duration = null;
+    $scope.startTime = null;
+
+    if ($state.params.runid) {
+      var match = filterFilter($scope.runs, {runid: $state.params.runid});
+      if (match.length) {
+        $scope.runs.selected = match[0];
+      }
+    }
+
+    if ($scope.runs.length) {
+      dataSrc.poll({
+        _cdapNsPath: basePath + '/runs/' + $scope.runs.selected.runid
+      }, function(res) {
+        var startMs = res.start * 1000;
+        $scope.startTime = new Date(startMs);
+        $scope.status = res.status;
+        $scope.duration = (res.end ? (res.end * 1000) - startMs : 0);
+      });
+    }
+    $scope.tabs = [{
+      title: 'Status',
+      template: '/assets/features/etlapps/templates/tabs/runs/tabs/status.html'
+    }, {
+      title: 'Logs',
+      template: '/assets/features/etlapps/templates/tabs/runs/tabs/log.html'
+    }];
+
+    $scope.activeTab = $scope.tabs[0];
+
+    $scope.$on('$destroy', function(event) {
+      event.currentScope.runs.selected = null;
+    });
+
+    $scope.selectTab = function(tab, node) {
+      $scope.activeTab = tab;
+    };
+  })
