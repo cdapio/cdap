a0e9fb9a9cd106c0612e639e11f66d4e0b76e7e0
Andrew Westlund <astewes@gmail.com>
2015-04-28 11:02:45 -0700

    replaces Delete option with a link to the admin namespace app metadata

--- cdap-ui/app/features/apps/controllers/list-ctrl.js
+++ cdap-ui/app/features/apps/controllers/list-ctrl.js
@@ -10,22 +10,6 @@ angular.module(PKG.name + '.feature.apps')
       });
     $scope.onFileSelected = myAppUploader.upload;
 
-
-    $scope.deleteApp = function(app) {
-      data.request({
-        _cdapNsPath: '/apps/' + app,
-        method: 'DELETE'
-      }, function(res) {
-        $alert({
-          type: 'success',
-          title: app,
-          content: 'App deleted successfully'
-        });
-        // FIXME: Have to avoid $timeout here. Un-necessary.
-        $timeout($state.reload);
-      });
-    };
-
     $scope.closeModal = function() {
       $modalInstance.close();
 
