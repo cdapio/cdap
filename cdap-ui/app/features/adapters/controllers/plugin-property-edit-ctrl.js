50b6d20a1a884659cdbe5da6f8a64f080e41416c
Ajai <ajainarayanan@gmail.com>
2015-04-28 14:54:42 -0700

    Fixes adapters create controller to handle no-config use case

--- cdap-ui/app/features/adapters/controllers/plugin-property-edit-ctrl.js
+++ cdap-ui/app/features/adapters/controllers/plugin-property-edit-ctrl.js
@@ -1,9 +1,21 @@
 angular.module(PKG.name + '.feature.adapters')
   .controller('PluginEditController', function($scope, MyDataSource, PluginConfigFactory) {
     $scope.configfetched = false;
-    PluginConfigFactory.fetch($scope, $scope.$parent.metadata.type, $scope.plugin)
-      .then(function(res) {
-        $scope.configfetched = true;
-        $scope.config = res;
-      });
+    $scope.noconfig = false;
+    $scope.noproperty = Object.keys(
+      $scope.plugin.properties || {}
+    ).length;
+
+    if ($scope.noproperty) {
+      PluginConfigFactory.fetch($scope, $scope.$parent.metadata.type, $scope.plugin.name)
+        .then(
+          function success(res) {
+            $scope.configfetched = true;
+            $scope.config = res;
+          },
+          function error(err) {
+            $scope.noconfig = true;
+          }
+        );
+    }
   });
