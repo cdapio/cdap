62c0a93de6bc3ac86f1cb53b29f025407462eb7d
Ajai <ajainarayanan@gmail.com>
2015-04-27 22:15:51 -0700

    Adds detail controller + view for adapters

---
+++ cdap-ui/app/features/etlapps/controllers/detail-ctrl.js
@@ -0,0 +1,41 @@
+angular.module(PKG.name + '.feature.etlapps')
+  .controller('AdpaterDetailController', function($scope, rRuns, $state, MyDataSource) {
+    var dataSrc = new MyDataSource($scope);
+    var path = '/adapters/' + $state.params.adapterId;
+
+    $scope.start = function() {
+      $scope.status = 'STARTING';
+
+      var requestObj = {
+        _cdapNsPath: path + '/start',
+        method: 'POST'
+      };
+
+      if ($scope.runtimeArgs && Object.keys($scope.runtimeArgs).length > 0) {
+        requestObj.body = $scope.runtimeArgs;
+      }
+
+      dataSrc.request(requestObj)
+        .then(function() {
+          $state.go($state.current, $state.params, {reload: true});
+        });
+    };
+
+    $scope.stop = function() {
+      $scope.status = 'STOPPING';
+      dataSrc.request({
+        _cdapNsPath: path + '/stop',
+        method: 'POST'
+      })
+        .then(function() {
+          $state.go($state.current, $state.params, {reload: true});
+        });
+    };
+
+    dataSrc.poll({
+      _cdapNsPath: path + '/status'
+    }, function(res) {
+      $scope.status = res.status;
+    });
+
+  });
