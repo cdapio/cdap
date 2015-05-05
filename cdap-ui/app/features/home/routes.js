bfa21c3a7140468c06a94225266b24fdc23f4797
Edwin Elia <edwinselia@gmail.com>
2015-05-04 17:21:55 -0700

    search for default namespace to exist before assigning

--- cdap-ui/app/features/home/routes.js
+++ cdap-ui/app/features/home/routes.js
@@ -39,7 +39,7 @@ angular.module(PKG.name+'.feature.home')
             return myNamespace.getList();
           }
         },
-        controller: function ($state, rNsList, mySessionStorage, myLoadingService) {
+        controller: function ($state, rNsList, mySessionStorage, myLoadingService, myAlert, $filter) {
           // check that $state.params.namespace is valid
           var n = rNsList.filter(function (one) {
             return one.name === $state.params.namespace;
@@ -51,7 +51,20 @@ angular.module(PKG.name+'.feature.home')
           if(!n.length) {
             mySessionStorage.get(PREFKEY)
               .then(function (latest) {
-                var d = latest || 'default';
+
+                var def = $filter('filter')(rNsList, {name: 'default'}, true);
+
+                if (def.length < 1) {
+                  def = rNsList[0];
+                  myAlert({
+                    title: 'Cannot find default namespace',
+                    content: 'Reverting to ' + def.name + ' namespace.'
+                  });
+                } else {
+                  def = def[0];
+                }
+
+                var d = latest || def.name;
                 console.warn('invalid namespace, defaulting to', d);
                 $state.go(
                   $state.current,
