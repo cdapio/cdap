7c6e2aaa86b0f8d0c89e55d322374b18a519dc10
Ajai <ajainarayanan@gmail.com>
2015-05-13 17:11:12 -0700

    Modifies workflows controllers to use myWorkFlowApi for data + moves helper functions for workflow diagrams to a factory

--- cdap-ui/app/features/workflows/controllers/tabs/runs/runs-detail-ctrl.js
+++ cdap-ui/app/features/workflows/controllers/tabs/runs/runs-detail-ctrl.js
@@ -2,7 +2,6 @@ angular.module(PKG.name + '.feature.workflows')
   .controller('WorkflowsRunsDetailController', function($scope, MyDataSource, $state, $filter) {
     var dataSrc = new MyDataSource($scope),
         filterFilter = $filter('filter');
-
     if ($state.params.runid) {
       var match = filterFilter($scope.runs, {runid: $state.params.runid});
       if (match.length) {
