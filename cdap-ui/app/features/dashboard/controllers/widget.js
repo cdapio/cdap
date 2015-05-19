00113d8f8b1c521e2b4762480b0033d0566cd79e
Ajai <ajainarayanan@gmail.com>
2015-05-18 17:10:51 -0700

    Fixes table widget to render the table properly + minor code cleanup in chart-helpers.js

--- cdap-ui/app/features/dashboard/controllers/widget.js
+++ cdap-ui/app/features/dashboard/controllers/widget.js
@@ -106,28 +106,47 @@ angular.module(PKG.name+'.feature.dashboard')
     });
   })
 
-  .controller('WidgetTableCtrl', function ($scope) {
-    $scope.$watch('chartData', function (chartData) {
-      if (!chartData) {
-        return;
+  .controller('WidgetTableCtrl', function ($scope, MyDataSource, MyChartHelpers, MyMetricsQueryHelper) {
+    
+    $scope.metrics = $scope.wdgt.metric;
+    var dataSrc = new MyDataSource($scope);
+
+    dataSrc.request(
+      {
+        _cdapPath: '/metrics/query',
+        method: 'POST',
+        body: MyMetricsQueryHelper.constructQuery(
+          'qid',
+          MyMetricsQueryHelper.contextToTags($scope.metrics.context),
+          $scope.metrics
+        )
       }
-      var tableData = [];
-      chartData.xCoords.forEach(function(timestamp, index) {
-        if (index === 0) {
-          // the first index of each column is just 'x' or the metric name
-          return;
-        }
-        var rowData = [timestamp];
-        chartData.columns.forEach(function(column) {
-          // If it begins with 'x', it is timestamps
-          if (column.length && column[0] !== 'x') {
-            rowData.push(column[index]);
+    )
+      .then(function(res) {
+        var processedData = MyChartHelpers.processData(
+          res,
+          'qid',
+          $scope.metrics.names,
+          $scope.metrics.resolution
+        );
+        processedData = MyChartHelpers.c3ifyData(processedData, $scope.metrics, {});
+        var tableData = [];
+        processedData.xCoords.forEach(function(timestamp, index) {
+          if (index === 0) {
+            // the first index of each column is just 'x' or the metric name
+            return;
           }
+          var rowData = [timestamp];
+          processedData.columns.forEach(function(column) {
+            // If it begins with 'x', it is timestamps
+            if (column.length && column[0] !== 'x') {
+              rowData.push(column[index]);
+            }
+          });
+          tableData.push(rowData);
         });
-        tableData.push(rowData);
+        $scope.tableData = tableData;
       });
-      $scope.tableData = tableData;
-    });
   })
 
   .controller('WidgetPieCtrl', function ($scope, $alert) {
