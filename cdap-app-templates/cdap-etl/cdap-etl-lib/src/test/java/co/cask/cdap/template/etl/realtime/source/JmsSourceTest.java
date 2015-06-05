95b7c828ca892b852f64c01ca342add09338bf78
henry-cask <henry@cask.co>
2015-06-05 15:31:54 -0700

    Add custom properties in the JMSSource config for custom JMS properties.

--- cdap-app-templates/cdap-etl/cdap-etl-lib/src/test/java/co/cask/cdap/template/etl/realtime/source/JmsSourceTest.java
+++ cdap-app-templates/cdap-etl/cdap-etl-lib/src/test/java/co/cask/cdap/template/etl/realtime/source/JmsSourceTest.java
@@ -202,7 +202,7 @@ public class JmsSourceTest {
   private void initializeJmsSource(String destination, int messageReceive, String initialContextFactory,
                                    String providerUrl) {
     jmsSource = new JmsSource(new JmsPluginConfig(destination, initialContextFactory, providerUrl, messageReceive,
-                                                  JmsSource.DEFAULT_CONNECTION_FACTORY, null, null));
+                                                  JmsSource.DEFAULT_CONNECTION_FACTORY, null, null, null));
   }
 
   /**
