79127c31f79b7d593ffe1e5afa696ed93661a539
Ajai <ajainarayanan@gmail.com>
2015-06-09 19:02:56 -0700

    Modifies stream tabs controllers to use controller-as syntax

--- cdap-ui/app/features/streams/controllers/tabs/explore-ctrl.js
+++ cdap-ui/app/features/streams/controllers/tabs/explore-ctrl.js
@@ -1,40 +1,38 @@
 angular.module(PKG.name + '.feature.streams')
-  .controller('CdapStreamExploreController',
-    function($scope, $state, EventPipe, myStreamApi) {
+  .controller('StreamExploreController', function($scope, $state, EventPipe, myStreamApi) {
 
-      $scope.activePanel = [0];
-      $scope.name = $state.params.streamId;
+    this.activePanel = [0];
+    this.name = $state.params.streamId;
 
-      EventPipe.on('explore.newQuery', function() {
-        $scope.activePanel = [0,1,2];
-      });
+    EventPipe.on('explore.newQuery', function() {
+      this.activePanel = [0,1,2];
+    }.bind(this));
 
-      var now = Date.now();
+    var now = Date.now();
 
-      $scope.eventSearch = {
-        startMs: now-(60*60*1000*2), // two hours ago
-        endMs: now,
-        limit: 10,
-        results: []
+    this.eventSearch = {
+      startMs: now-(60*60*1000*2), // two hours ago
+      endMs: now,
+      limit: 10,
+      results: []
+    };
+
+    this.doEventSearch = function () {
+      var params = {
+        namespace: $state.params.namespace,
+        streamId: $state.params.streamId,
+        scope: $scope,
+        start: this.eventSearch.startMs,
+        end: this.eventSearch.endMs,
+        limit: this.eventSearch.limit
       };
+      myStreamApi.eventSearch(params)
+        .$promise
+        .then(function (res) {
+          this.eventSearch.results = res;
+        }.bind(this));
+    };
 
-      $scope.doEventSearch = function () {
-        var params = {
-          namespace: $state.params.namespace,
-          streamId: $state.params.streamId,
-          scope: $scope,
-          start: $scope.eventSearch.startMs,
-          end: $scope.eventSearch.endMs,
-          limit: $scope.eventSearch.limit
-        };
-        myStreamApi.eventSearch(params)
-          .$promise
-          .then(function (res) {
-            $scope.eventSearch.results = res;
-          });
-      };
+    this.doEventSearch();
 
-      $scope.doEventSearch();
-
-    }
-  );
+  });
