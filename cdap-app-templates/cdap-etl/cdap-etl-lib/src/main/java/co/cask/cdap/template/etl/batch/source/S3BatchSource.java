19c5375224711f038c1e1b359817d07db26a0436
Jay Jin <jayjin@Jays-Macintosh.local>
2015-06-15 15:36:31 -0700

    ui not working still

--- cdap-app-templates/cdap-etl/cdap-etl-lib/src/main/java/co/cask/cdap/template/etl/batch/source/S3BatchSource.java
+++ cdap-app-templates/cdap-etl/cdap-etl-lib/src/main/java/co/cask/cdap/template/etl/batch/source/S3BatchSource.java
@@ -19,6 +19,7 @@ package co.cask.cdap.template.etl.batch.source;
 import co.cask.cdap.api.annotation.Description;
 import co.cask.cdap.api.annotation.Name;
 import co.cask.cdap.api.annotation.Plugin;
+import co.cask.cdap.api.common.Bytes;
 import co.cask.cdap.api.data.format.StructuredRecord;
 import co.cask.cdap.api.data.schema.Schema;
 import co.cask.cdap.api.dataset.lib.KeyValue;
@@ -27,13 +28,20 @@ import co.cask.cdap.template.etl.api.Emitter;
 import co.cask.cdap.template.etl.api.batch.BatchContext;
 import co.cask.cdap.template.etl.api.batch.BatchSource;
 import co.cask.cdap.template.etl.api.batch.BatchSourceContext;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link BatchSource} to use S3 as a Source.
@@ -47,10 +55,9 @@ public class S3BatchSource extends BatchSource<LongWritable, Text, StructuredRec
   private S3BatchConfig config;
   private static final Schema DEFAULT_SCHEMA = Schema.recordOf(
     "event",
-   Schema.Field.of("ts", Schema.of(Schema.Type.LONG)),
-   Schema.Field.of("body", Schema.of(Schema.Type.BYTES))
+    Schema.Field.of("key", Schema.of(Schema.Type.STRING)),
+   Schema.Field.of("body", Schema.of(Schema.Type.STRING))
   );
-
 
   public S3BatchSource (S3BatchConfig config) {
     this.config = config;
@@ -59,41 +66,73 @@ public class S3BatchSource extends BatchSource<LongWritable, Text, StructuredRec
   @Override
   public void prepareRun(BatchSourceContext context) throws Exception {
     Job job = context.getHadoopJob();
-
-    job.getConfiguration().set("fs.s3n.awsAccessKeyId", config.accessID);
-    job.getConfiguration().set("fs.s3n.awsSecretAccessKey", config.accessKey);
-
-    FileInputFormat.addInputPath(job, new Path(config.path));
+    job.getConfiguration().set("fs.s3n.awsAccessKeyId", "AKIAIGO2GTK4V7XRUHWQ");
+    job.getConfiguration().set("fs.s3n.awsSecretAccessKey", "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");
+    FileInputFormat.setInputPathFilter(job, S3Filter.class);
+    FileInputFormat.addInputPath(job, new Path("s3n://caskalytics/some_logs/"));
   }
 
 
+  @Override
   public void transform(KeyValue<LongWritable, Text> input, Emitter<StructuredRecord> emitter) throws Exception {
     StructuredRecord output = StructuredRecord.builder(DEFAULT_SCHEMA)
-      .set("ts", input.getKey().get())
-      .set("body", input.getValue())
+      .set("key", String.valueOf(System.currentTimeMillis()))
+      .set("body", input.getValue().toString())
       .build();
     emitter.emit(output);
-
   }
+
+  /**
+   * fucking javadoc checkstyle
+   */
+  public class S3Filter extends Configured implements PathFilter {
+
+    @Override
+    public boolean accept(Path path) {
+      Date prevHour = new Date();
+      prevHour.setTime(System.currentTimeMillis() - TimeUnit.HOURS.toMillis(1));
+      String currentTime = new SimpleDateFormat("yyyy-MM-dd-HH").format(prevHour);
+      currentTime = "2015-01-25-07";
+
+      String filename = path.toString();
+      if (filename.equals("s3n://caskalytics/some_logs") || filename.contains(currentTime)) {
+        LOG.info("true");
+        return true;
+      }
+      LOG.info("false");
+      return false;
+
+      //return true;
+    }
+  }
+
 
   /**
    * insert javadoc here
    */
   public static class S3BatchConfig extends PluginConfig {
 
+    @Name("name")
+    @Description("name lel")
     private String name;
 
+    /*@Name("accessID")
+    @Description("Access Id for s3")
     private String accessID;
 
+    @Name("accessKey")
+    @Description("Access Key for s3")
     private String accessKey;
 
-    private String path;
+    @Name("path")
+    @Description("Path to files to be read")
+    private String path;*/
 
-    public S3BatchConfig(String name, String accessID, String accessKey, String path) {
+    public S3BatchConfig(String name/*, String accessID, String accessKey, String path*/) {
       this.name = name;
-      this.accessID = accessID;
+      /*this.accessID = accessID;
       this.accessKey = accessKey;
-      this.path = path;
+      this.path = path;*/
     }
 
   }
