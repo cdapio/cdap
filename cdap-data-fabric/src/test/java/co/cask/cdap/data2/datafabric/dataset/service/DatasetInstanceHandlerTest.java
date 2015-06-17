9601ece8c6eb82e9993aa0208edfcd589b5728bb
Alvin Wang <alvin@cask.co>
2015-06-17 11:10:12 -0700

    try-finally in DatasetInstanceHandlerTest methods to cleanup properly per method

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/datafabric/dataset/service/DatasetInstanceHandlerTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/datafabric/dataset/service/DatasetInstanceHandlerTest.java
@@ -63,60 +63,62 @@ public class DatasetInstanceHandlerTest extends DatasetServiceTestBase {
     // nothing in the beginning
     Assert.assertEquals(0, instances.size());
 
-    // create dataset instance with type that is not yet known to the system should fail
-    DatasetProperties props = DatasetProperties.builder().add("prop1", "val1").build();
-    Assert.assertEquals(HttpStatus.SC_NOT_FOUND, createInstance("dataset1", "datasetType2", props));
+    try {
+      // create dataset instance with type that is not yet known to the system should fail
+      DatasetProperties props = DatasetProperties.builder().add("prop1", "val1").build();
+      Assert.assertEquals(HttpStatus.SC_NOT_FOUND, createInstance("dataset1", "datasetType2", props));
 
-    // deploy modules
-    deployModule("module1", TestModule1.class);
-    deployModule("module2", TestModule2.class);
+      // deploy modules
+      deployModule("module1", TestModule1.class);
+      deployModule("module2", TestModule2.class);
 
-    // create dataset instance
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("dataset1", "datasetType2", props));
+      // create dataset instance
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("dataset1", "datasetType2", props));
 
-    // verify module cannot be deleted which type is used for the dataset
-    int modulesBeforeDelete = getModules().getResponseObject().size();
-    Assert.assertEquals(HttpStatus.SC_CONFLICT, deleteModule("module2"));
-    Assert.assertEquals(HttpStatus.SC_CONFLICT, deleteModules());
-    Assert.assertEquals(modulesBeforeDelete, getModules().getResponseObject().size());
+      // verify module cannot be deleted which type is used for the dataset
+      int modulesBeforeDelete = getModules().getResponseObject().size();
+      Assert.assertEquals(HttpStatus.SC_CONFLICT, deleteModule("module2"));
+      Assert.assertEquals(HttpStatus.SC_CONFLICT, deleteModules());
+      Assert.assertEquals(modulesBeforeDelete, getModules().getResponseObject().size());
 
-    // verify instance was created
-    instances = getInstances().getResponseObject();
-    Assert.assertEquals(1, instances.size());
-    // verifying spec is same as expected
-    DatasetSpecification dataset1Spec = createSpec("dataset1", "datasetType2", props);
-    Assert.assertEquals(dataset1Spec, instances.get(0));
+      // verify instance was created
+      instances = getInstances().getResponseObject();
+      Assert.assertEquals(1, instances.size());
+      // verifying spec is same as expected
+      DatasetSpecification dataset1Spec = createSpec("dataset1", "datasetType2", props);
+      Assert.assertEquals(dataset1Spec, instances.get(0));
 
-    // verify created instance info can be retrieved
-    DatasetInstanceMeta datasetInfo = getInstance("dataset1").getResponseObject();
-    Assert.assertEquals(dataset1Spec, datasetInfo.getSpec());
-    Assert.assertEquals(dataset1Spec.getType(), datasetInfo.getType().getName());
-    // type meta should have 2 modules that has to be loaded to create type's class and in the order they must be loaded
-    List<DatasetModuleMeta> modules = datasetInfo.getType().getModules();
-    Assert.assertEquals(2, modules.size());
-    DatasetTypeHandlerTest.verify(modules.get(0), "module1", TestModule1.class, ImmutableList.of("datasetType1"),
-                                  Collections.<String>emptyList(), ImmutableList.of("module2"));
-    DatasetTypeHandlerTest.verify(modules.get(1), "module2", TestModule2.class, ImmutableList.of("datasetType2"),
-                                  ImmutableList.of("module1"), Collections.<String>emptyList());
+      // verify created instance info can be retrieved
+      DatasetInstanceMeta datasetInfo = getInstance("dataset1").getResponseObject();
+      Assert.assertEquals(dataset1Spec, datasetInfo.getSpec());
+      Assert.assertEquals(dataset1Spec.getType(), datasetInfo.getType().getName());
+      // type meta should have 2 modules that has to be loaded to create type's class and in the order they must be loaded
+      List<DatasetModuleMeta> modules = datasetInfo.getType().getModules();
+      Assert.assertEquals(2, modules.size());
+      DatasetTypeHandlerTest.verify(modules.get(0), "module1", TestModule1.class, ImmutableList.of("datasetType1"),
+                                    Collections.<String>emptyList(), ImmutableList.of("module2"));
+      DatasetTypeHandlerTest.verify(modules.get(1), "module2", TestModule2.class, ImmutableList.of("datasetType2"),
+                                    ImmutableList.of("module1"), Collections.<String>emptyList());
 
-    // try to retrieve non-existed instance
-    Assert.assertEquals(HttpStatus.SC_NOT_FOUND, getInstance("non-existing-dataset").getResponseCode());
+      // try to retrieve non-existed instance
+      Assert.assertEquals(HttpStatus.SC_NOT_FOUND, getInstance("non-existing-dataset").getResponseCode());
 
-    // cannot create instance with same name again
-    Assert.assertEquals(HttpStatus.SC_CONFLICT, createInstance("dataset1", "datasetType2", props));
-    Assert.assertEquals(1, getInstances().getResponseObject().size());
+      // cannot create instance with same name again
+      Assert.assertEquals(HttpStatus.SC_CONFLICT, createInstance("dataset1", "datasetType2", props));
+      Assert.assertEquals(1, getInstances().getResponseObject().size());
 
-    // cannot delete non-existing dataset instance
-    Assert.assertEquals(HttpStatus.SC_NOT_FOUND, deleteInstance("non-existing-dataset"));
-    Assert.assertEquals(1, getInstances().getResponseObject().size());
+      // cannot delete non-existing dataset instance
+      Assert.assertEquals(HttpStatus.SC_NOT_FOUND, deleteInstance("non-existing-dataset"));
+      Assert.assertEquals(1, getInstances().getResponseObject().size());
+    } finally {
+      // delete dataset instance
+      Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("dataset1"));
+      Assert.assertEquals(0, getInstances().getResponseObject().size());
 
-    // delete dataset instance
-    Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("dataset1"));
-    Assert.assertEquals(0, getInstances().getResponseObject().size());
-
-    // delete dataset modules
-    Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module2"));
-    Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module1"));
+      // delete dataset modules
+      Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module2"));
+      Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module1"));
+    }
   }
 
   @Test
@@ -128,110 +130,114 @@ public class DatasetInstanceHandlerTest extends DatasetServiceTestBase {
     // nothing in the beginning
     Assert.assertEquals(0, instances.size());
 
-    DatasetProperties props = DatasetProperties.builder().add("prop1", "val1").build();
+    try {
+      DatasetProperties props = DatasetProperties.builder().add("prop1", "val1").build();
 
-    // deploy modules
-    deployModule("module1", TestModule1.class);
-    deployModule("module2", TestModule2.class);
+      // deploy modules
+      deployModule("module1", TestModule1.class);
+      deployModule("module2", TestModule2.class);
 
-    // create dataset instance
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("dataset1", "datasetType2", props));
+      // create dataset instance
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("dataset1", "datasetType2", props));
 
-    // verify instance was created
-    instances = getInstances().getResponseObject();
-    Assert.assertEquals(1, instances.size());
+      // verify instance was created
+      instances = getInstances().getResponseObject();
+      Assert.assertEquals(1, instances.size());
 
-    Map<String, String> newProps = ImmutableMap.of("prop2", "val2");
+      Map<String, String> newProps = ImmutableMap.of("prop2", "val2");
 
-    // update dataset instance
-    Assert.assertEquals(HttpStatus.SC_OK, updateInstance("dataset1", newProps));
-    Assert.assertEquals("val2", getInstance("dataset1").getResponseObject().getSpec().getProperty("prop2"));
-    Assert.assertNull(getInstance("dataset1").getResponseObject().getSpec().getProperty("prop1"));
+      // update dataset instance
+      Assert.assertEquals(HttpStatus.SC_OK, updateInstance("dataset1", newProps));
+      Assert.assertEquals("val2", getInstance("dataset1").getResponseObject().getSpec().getProperty("prop2"));
+      Assert.assertNull(getInstance("dataset1").getResponseObject().getSpec().getProperty("prop1"));
+    } finally {
+      // delete dataset instance
+      Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("dataset1"));
+      Assert.assertEquals(0, getInstances().getResponseObject().size());
 
-    // delete dataset instance
-    Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("dataset1"));
-    Assert.assertEquals(0, getInstances().getResponseObject().size());
-
-    // delete dataset modules
-    Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module2"));
-    Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module1"));
+      // delete dataset modules
+      Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module2"));
+      Assert.assertEquals(HttpStatus.SC_OK, deleteModule("module1"));
+    }
   }
 
   @Test
   public void testCreateDelete() throws Exception {
-    deployModule("default-table", InMemoryTableModule.class);
-    deployModule("default-core", CoreDatasetsModule.class);
+    try {
+      deployModule("default-table", InMemoryTableModule.class);
+      deployModule("default-core", CoreDatasetsModule.class);
 
-    // cannot create instance with same name again
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable2", "table", DatasetProperties.EMPTY));
-    Assert.assertEquals(2, getInstances().getResponseObject().size());
+      // cannot create instance with same name again
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable2", "table", DatasetProperties.EMPTY));
+      Assert.assertEquals(2, getInstances().getResponseObject().size());
 
-    // we want to verify that data is also gone, so we write smth to tables first
-    // using default namespace here since this is testing v2 APIs right now. TODO: add tests for v3 APIs
-    final Table table1 = dsFramework.getDataset(Id.DatasetInstance.from(Constants.DEFAULT_NAMESPACE, "myTable1"),
-                                                DatasetDefinition.NO_ARGUMENTS, null);
-    final Table table2 = dsFramework.getDataset(Id.DatasetInstance.from(Constants.DEFAULT_NAMESPACE, "myTable2"),
-                                                DatasetDefinition.NO_ARGUMENTS, null);
-    TransactionExecutor txExecutor =
-      new DefaultTransactionExecutor(new InMemoryTxSystemClient(txManager),
-                                     ImmutableList.of((TransactionAware) table1, (TransactionAware) table2));
+      // we want to verify that data is also gone, so we write smth to tables first
+      // using default namespace here since this is testing v2 APIs right now. TODO: add tests for v3 APIs
+      final Table table1 = dsFramework.getDataset(Id.DatasetInstance.from(Constants.DEFAULT_NAMESPACE, "myTable1"),
+                                                  DatasetDefinition.NO_ARGUMENTS, null);
+      final Table table2 = dsFramework.getDataset(Id.DatasetInstance.from(Constants.DEFAULT_NAMESPACE, "myTable2"),
+                                                  DatasetDefinition.NO_ARGUMENTS, null);
+      TransactionExecutor txExecutor =
+        new DefaultTransactionExecutor(new InMemoryTxSystemClient(txManager),
+                                       ImmutableList.of((TransactionAware) table1, (TransactionAware) table2));
 
-    txExecutor.execute(new TransactionExecutor.Subroutine() {
-      @Override
-      public void apply() throws Exception {
-        table1.put(new Put("key1", "col1", "val1"));
-        table2.put(new Put("key2", "col2", "val2"));
-      }
-    });
+      txExecutor.execute(new TransactionExecutor.Subroutine() {
+        @Override
+        public void apply() throws Exception {
+          table1.put(new Put("key1", "col1", "val1"));
+          table2.put(new Put("key2", "col2", "val2"));
+        }
+      });
 
-    // verify that we can read the data
-    txExecutor.execute(new TransactionExecutor.Subroutine() {
-      @Override
-      public void apply() throws Exception {
-        Assert.assertEquals("val1", table1.get(new Get("key1", "col1")).getString("col1"));
-        Assert.assertEquals("val2", table2.get(new Get("key2", "col2")).getString("col2"));
-      }
-    });
+      // verify that we can read the data
+      txExecutor.execute(new TransactionExecutor.Subroutine() {
+        @Override
+        public void apply() throws Exception {
+          Assert.assertEquals("val1", table1.get(new Get("key1", "col1")).getString("col1"));
+          Assert.assertEquals("val2", table2.get(new Get("key2", "col2")).getString("col2"));
+        }
+      });
 
-    // delete table, check that it is deleted, create again and verify that it is empty
-    Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("myTable1"));
-    ObjectResponse<List<DatasetSpecification>> instances = getInstances();
-    Assert.assertEquals(1, instances.getResponseObject().size());
-    Assert.assertEquals("myTable2", instances.getResponseObject().get(0).getName());
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
-    Assert.assertEquals(2, getInstances().getResponseObject().size());
+      // delete table, check that it is deleted, create again and verify that it is empty
+      Assert.assertEquals(HttpStatus.SC_OK, deleteInstance("myTable1"));
+      ObjectResponse<List<DatasetSpecification>> instances = getInstances();
+      Assert.assertEquals(1, instances.getResponseObject().size());
+      Assert.assertEquals("myTable2", instances.getResponseObject().get(0).getName());
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
+      Assert.assertEquals(2, getInstances().getResponseObject().size());
 
-    // verify that table1 is empty. Note: it is ok for test purpose to re-use the table clients
-    txExecutor.execute(new TransactionExecutor.Subroutine() {
-      @Override
-      public void apply() throws Exception {
-        Assert.assertTrue(table1.get(new Get("key1", "col1")).isEmpty());
-        Assert.assertEquals("val2", table2.get(new Get("key2", "col2")).getString("col2"));
-        // writing smth to table1 for subsequent test
-        table1.put(new Put("key3", "col3", "val3"));
-      }
-    });
+      // verify that table1 is empty. Note: it is ok for test purpose to re-use the table clients
+      txExecutor.execute(new TransactionExecutor.Subroutine() {
+        @Override
+        public void apply() throws Exception {
+          Assert.assertTrue(table1.get(new Get("key1", "col1")).isEmpty());
+          Assert.assertEquals("val2", table2.get(new Get("key2", "col2")).getString("col2"));
+          // writing smth to table1 for subsequent test
+          table1.put(new Put("key3", "col3", "val3"));
+        }
+      });
 
-    // delete all tables, check that they deleted, create again and verify that they are empty
-    deleteInstances();
-    Assert.assertEquals(0, getInstances().getResponseObject().size());
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
-    Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable2", "table", DatasetProperties.EMPTY));
-    Assert.assertEquals(2, getInstances().getResponseObject().size());
+      // delete all tables, check that they deleted, create again and verify that they are empty
+      deleteInstances();
+      Assert.assertEquals(0, getInstances().getResponseObject().size());
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable1", "table", DatasetProperties.EMPTY));
+      Assert.assertEquals(HttpStatus.SC_OK, createInstance("myTable2", "table", DatasetProperties.EMPTY));
+      Assert.assertEquals(2, getInstances().getResponseObject().size());
 
-    // verify that tables are empty. Note: it is ok for test purpose to re-use the table clients
-    txExecutor.execute(new TransactionExecutor.Subroutine() {
-      @Override
-      public void apply() throws Exception {
-        Assert.assertTrue(table1.get(new Get("key3", "col3")).isEmpty());
-        Assert.assertTrue(table2.get(new Get("key2", "col2")).isEmpty());
-      }
-    });
-
-    // cleanup
-    deleteInstances();
-    Assert.assertEquals(HttpStatus.SC_OK, deleteModules());
+      // verify that tables are empty. Note: it is ok for test purpose to re-use the table clients
+      txExecutor.execute(new TransactionExecutor.Subroutine() {
+        @Override
+        public void apply() throws Exception {
+          Assert.assertTrue(table1.get(new Get("key3", "col3")).isEmpty());
+          Assert.assertTrue(table2.get(new Get("key2", "col2")).isEmpty());
+        }
+      });
+    } finally {
+      // cleanup
+      deleteInstances();
+      Assert.assertEquals(HttpStatus.SC_OK, deleteModules());
+    }
   }
 
 
