419397b822b63cbc526706a7f53ebdabb4e08db1
Ajai <ajainarayanan@gmail.com>
2015-06-17 17:25:23 -0700

    Adds the ability to stop polling in program-status directive when program is killed/stopped/completed

--- cdap-ui/app/directives/program-status/program-status.js
+++ cdap-ui/app/directives/program-status/program-status.js
@@ -10,6 +10,7 @@ angular.module(PKG.name + '.commons')
       },
       templateUrl: 'program-status/program-status.html',
       controller: function($scope, MyDataSource, $state) {
+        var pollPromise;
         // $scope.runs comes from parent controller
         if ($scope.runs.length !== 0) {
           var dataSrc = new MyDataSource($scope),
@@ -21,13 +22,16 @@ angular.module(PKG.name + '.commons')
             path = '/apps/' + $state.params.appId + '/' + $scope.type + '/' + $state.params.programId + '/runs/' + $scope.runid;
           }
 
-          dataSrc.poll({
+          pollPromise = dataSrc.poll({
             _cdapNsPath: path
           }, function (res) {
             var startMs = res.start * 1000;
             $scope.start = new Date(startMs);
             $scope.status = res.status;
             $scope.duration = (res.end ? (res.end * 1000) - startMs : 0);
+            if (['COMPLETED', 'KILLED', 'STOPPED', 'FAILED'].indexOf($scope.status) !== -1) {
+              dataSrc.stopPoll(pollPromise.__pollId__);
+            }
           });
         }
 
