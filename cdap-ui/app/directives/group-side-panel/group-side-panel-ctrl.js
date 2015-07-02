4a3dbc1e90165334623556b0ce3beb8ead65872a
Ajai <ajainarayanan@gmail.com>
2015-07-01 23:19:11 -0700

    Adds the test/prototype plumb view to foo routes

--- cdap-ui/app/directives/group-side-panel/group-side-panel-ctrl.js
+++ cdap-ui/app/directives/group-side-panel/group-side-panel-ctrl.js
@@ -1,7 +1,16 @@
 angular.module(PKG.name + '.commons')
   .controller('MySidePanel', function ($scope, myAdapterApi, MyPlumbService) {
     this.items = $scope.panelGroups;
+    this.expanded = true;
     this.openPlugins = function (type) {
+      if (this.openedPluginType === type && this.showPlugin) {
+        this.showPlugin = false;
+        this.openedPluginType = null;
+        return;
+      }
+
+      this.openedPluginType = type;
+
       switch (type) {
         case 'source':
           myAdapterApi.fetchSources({
@@ -29,13 +38,39 @@ angular.module(PKG.name + '.commons')
       }
     };
 
-    this.addPlugin = function(config, type) {
+    this.addPlugin = function(event, config, type) {
+      event.stopPropagation();
       MyPlumbService.updateConfig(config, type);
     };
 
+    function getIcon(plugin) {
+      var iconMap = {
+        'script': 'fa-code',
+        'twitter': 'fa-twitter',
+        'cube': 'fa-cubes',
+        'data': 'fa-database',
+        'database': 'fa-database',
+        'table': 'fa-table',
+        'kafka': 'icon-kafka',
+        'stream': 'icon-plugin-stream',
+        'avro': 'icon-avro',
+        'jms': 'icon-jms'
+      };
+
+      var pluginName = plugin.toLowerCase();
+      var icon = iconMap[pluginName] ? iconMap[pluginName]: 'fa-plug';
+      return icon;
+    }
+
     this.initializePlugins = function(type, plugins) {
       this.plugins = plugins.map(function(plugin) {
-        return angular.extend({type: type}, plugin);
+        return angular.extend(
+          {
+            type: type,
+            icon: getIcon(plugin.name)
+          },
+          plugin
+        );
       });
     };
 
@@ -47,4 +82,5 @@ angular.module(PKG.name + '.commons')
       this.showPlugin = true;
     };
 
+    this.openPlugins('source');
   });
