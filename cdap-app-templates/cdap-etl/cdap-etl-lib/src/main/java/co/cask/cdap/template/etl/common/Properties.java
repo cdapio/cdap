93b5e5008b3f4cc8ce9a36fe4dfa9fae0dfb0091
ataylor4 <ashley@cask.co>
2015-07-09 13:29:20 -0700

    WIP

--- cdap-app-templates/cdap-etl/cdap-etl-lib/src/main/java/co/cask/cdap/template/etl/common/Properties.java
+++ cdap-app-templates/cdap-etl/cdap-etl-lib/src/main/java/co/cask/cdap/template/etl/common/Properties.java
@@ -111,6 +111,18 @@ public final class Properties {
   }
 
   /**
+   * Properties for elasticsearch
+   */
+  public static class Elasticsearch {
+    public static final String INDEX_NAME = "index name";
+    public static final String TYPE_NAME = "type name";
+    public static final String SCHEMA = "mapping";
+    public static final String HOST = "host name for master node";
+    public static final String PORT = "port";
+
+  }
+
+  /**
    * Properties for ProjectionTransform
    */
   public static class ProjectionTransform {
