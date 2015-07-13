6efdba97558f501c2c23c20911d006792fdd188e
Sree <sree@continuuity.com>
2015-07-13 11:05:39 -0700

    Code review comments

--- cdap-master/src/main/java/co/cask/cdap/data/tools/DatasetUpgrader.java
+++ cdap-master/src/main/java/co/cask/cdap/data/tools/DatasetUpgrader.java
@@ -95,10 +95,7 @@ public class DatasetUpgrader extends AbstractUpgrader {
   private void upgradeUserTables() throws Exception {
     HBaseAdmin hAdmin = new HBaseAdmin(hConf);
     for (HTableDescriptor desc : hAdmin.listTables()) {
-      String tableName = desc.getNameAsString();
-      if (!isTableInSystemNamespace(tableName) &&
-           isUserTable(tableName) &&
-           isTableCreatedByCDAP(desc)) {
+      if (isCDAPUserTable(desc)) {
         upgradeUserTable(desc);
       }
     }
@@ -137,26 +134,29 @@ public class DatasetUpgrader extends AbstractUpgrader {
     LOG.info("Upgraded hbase table: {}", tableId);
   }
 
-  // Note: This check can be safely used for user table since we create meta.
-  // CDAP-2963 should be fixed so that we can make use of this check generically for all cdap tables
-  private boolean isTableCreatedByCDAP(HTableDescriptor desc) {
-    return (desc.getValue("cdap.version") != null);
-  }
 
-  private boolean isTableInSystemNamespace(String tableName) {
-    // dataset in system namespace starts with <prefix>_system ex: cdap_system
-    return tableName.startsWith(String.format("%s_%s", this.datasetTablePrefix, "system"));
-  }
-
-  private boolean isUserTable(String tableName) {
+  private boolean isCDAPUserTable(HTableDescriptor desc) {
+    String tableName = desc.getNameAsString();
+    // If table is in system namespace: (starts with <tablePrefix>_system
+    // or if it is not created by CDAP it is not user table
+    if (tableName.startsWith(String.format("%s_%s", this.datasetTablePrefix, Constants.SYSTEM_NAMESPACE)) ||
+       (!isTableCreatedByCDAP(desc))) {
+      return false;
+    }
     // User tables are named differently in default vs non-default namespace
     // User table in default namespace starts with cdap.user
     // User table in Non-default namespace is a table that doesn't have
     //    system.queue or system.stream or system.sharded.queue
-   return defaultNSUserTablePrefix.matcher(tableName).matches() ||
-          // Note: if the user has created a dataset called system.* then we will not upgrade the table.
-          // CDAP-2977 should be fixed to have a cleaner fix for this.
-          !(tableName.contains("system.queue") || tableName.contains("system.stream") ||
-            tableName.contains("system.sharded.queue"));
+    return defaultNSUserTablePrefix.matcher(tableName).matches() ||
+      // Note: if the user has created a dataset called system.* then we will not upgrade the table.
+      // CDAP-2977 should be fixed to have a cleaner fix for this.
+      !(tableName.contains("system.queue") || tableName.contains("system.stream") ||
+        tableName.contains("system.sharded.queue"));
+  }
+
+  // Note: This check can be safely used for user table since we create meta.
+  // CDAP-2963 should be fixed so that we can make use of this check generically for all cdap tables
+  private boolean isTableCreatedByCDAP(HTableDescriptor desc) {
+    return (desc.getValue("cdap.version") != null);
   }
 }
