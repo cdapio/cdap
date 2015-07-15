d291c4b43468f64cbe2607a81a26ed638a106069
Terence Yim <terence@cask.co>
2015-07-15 13:47:07 -0700

    Remove unused import

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/transaction/queue/hbase/HBaseQueueTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/transaction/queue/hbase/HBaseQueueTest.java
@@ -103,7 +103,6 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * HBase queue tests.
