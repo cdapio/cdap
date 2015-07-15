47a8124ffe0b716f9b14855b569187269f784371
Ajai <ajainarayanan@gmail.com>
2015-07-15 14:10:46 -0700

    Cleansup my-plumb directive controller + new factory for generic functions

--- cdap-ui/app/directives/plumb/my-plumb-ctrl.js
+++ cdap-ui/app/directives/plumb/my-plumb-ctrl.js
@@ -1,78 +1,17 @@
 angular.module(PKG.name + '.commons')
-  .controller('MyPlumbController', function MyPlumbController(jsPlumb, $scope, $timeout, MyPlumbService, myHelpers, AdapterErrorFactory) {
+  .controller('MyPlumbController', function MyPlumbController(jsPlumb, $scope, $timeout, MyPlumbService, myHelpers, MyPlumbFactory, AdapterErrorFactory) {
     this.plugins = $scope.config || [];
     this.instance = null;
     // this.nodesErrors = AdapterErrorFactory.nodesError;
     // this.canvasError = AdapterErrorFactory.canvasError;
 
-    var defaultSettings = {
-      Connector : [ 'Flowchart' ],
-      ConnectionsDetachable: true
-    };
-    var commonSettings = {
-      endpoint:'Dot',
-      maxConnections: -1,
-      paintStyle: {
-          strokeStyle: 'white',
-          fillStyle: '#666',
-          radius: 7,
-          lineWidth: 3
-      },
-      connectorOverlays: [ [ 'Arrow', { location:1, width: 10, height: 10 } ] ],
-      anchors: [ 'Perimeter', {shape: 'Circle'}]
-    };
-    var sourceSettings = angular.extend({
-      isSource: true,
-      anchor: 'Right'
-    }, commonSettings);
-    var sinkSettings = angular.extend({
-      isTarget: true,
-      anchor: 'Left'
-    }, commonSettings);
-
-    function getIcon(plugin) {
-      var iconMap = {
-        'script': 'fa-code',
-        'twitter': 'fa-twitter',
-        'cube': 'fa-cubes',
-        'data': 'fa-database',
-        'database': 'fa-database',
-        'table': 'fa-table',
-        'kafka': 'icon-kafka',
-        'stream': 'icon-plugin-stream',
-        'avro': 'icon-avro',
-        'jms': 'icon-jms'
-      };
-
-      var pluginName = plugin.toLowerCase();
-      var icon = iconMap[pluginName] ? iconMap[pluginName]: 'fa-plug';
-      return icon;
-    }
-
     this.addPlugin = function addPlugin(config, type) {
       this.plugins.push(angular.extend({
-        icon: getIcon(config.name)
+        icon: MyPlumbFactory.getIcon(config.name)
       }, config));
       $timeout(drawNode.bind(this, config.id, type));
       $timeout(this.instance.repaintEverything);
     };
-
-    function drawNode(id, type) {
-      switch(type) {
-        case 'source':
-          this.instance.addEndpoint(id, sourceSettings, {uuid: id});
-          break;
-        case 'sink':
-          this.instance.addEndpoint(id, sinkSettings, {uuid: id});
-          break;
-        case 'transform':
-          // Need to id each end point so that it can be used later to make connections.
-          this.instance.addEndpoint(id, sourceSettings, {uuid: 'Left' + id});
-          this.instance.addEndpoint(id, sinkSettings, {uuid: 'Right' + id});
-          break;
-      }
-      this.instance.draggable(id);
-    }
 
     this.removePlugin = function(index, nodeId) {
       this.instance.detachAllConnections(nodeId);
@@ -108,37 +47,47 @@ angular.module(PKG.name + '.commons')
     MyPlumbService.registerCallBack(this.addPlugin.bind(this));
     MyPlumbService.errorCallback(errorNotification.bind(this));
 
-    // Using Dagre here to generate x and y co-ordinates for each node.
-    // When we fork and branch and have complex connections this will be useful for us.
-    // Right now this returns a pretty simple straight linear graph.
-    this.getGraph = function() {
-      var graph = new dagre.graphlib.Graph();
-      graph.setGraph({
-        nodesep: 60,
-        ranksep: 100,
-        rankdir: 'LR',
-        marginx: 30,
-        marginy: 30
-      });
+    this.drawGraph = function() {
+      var graph = MyPlumbFactory.getGraph(this.plugins);
+      var nodes = graph.nodes()
+        .map(function(node) {
+          return graph.node(node);
+        });
+
+      var marginLeft = $scope.getGraphMargins(this.plugins);
       this.plugins.forEach(function(plugin) {
-        graph.setNode(plugin.id, {label: plugin.id, width: 100, height: 100});
-      });
-      dagre.layout(graph);
-      return graph;
+        plugin.icon = MyPlumbFactory.getIcon(plugin.name);
+        plugin.style = MyPlumbFactory.generateStyles(plugin.id, nodes, 200, marginLeft);
+        drawNode.call(this, plugin.id, plugin.type);
+      }.bind(this));
+
+      drawConnections.call(this);
+
+      MyPlumbService.setConnections(this.instance.getConnections());
+
+      mapSchemas.call(this);
+
+      $timeout(this.instance.repaintEverything);
     };
 
-    function generateStyles(name, nodes, xmargin, ymargin) {
-      var styles = {};
-      var nodeStylesFromDagre = nodes.filter(function(node) {
-        return node.label === name;
-      });
-      if (nodeStylesFromDagre.length) {
-        styles = {
-          'top': (nodeStylesFromDagre[0].x + xmargin) + 'px',
-          'left': (nodeStylesFromDagre[0].y + ymargin) + 'px'
-        };
+    function drawNode(id, type) {
+      var sourceSettings = MyPlumbFactory.getSettings().source,
+          sinkSettings = MyPlumbFactory.getSettings().sink;
+
+      switch(type) {
+        case 'source':
+          this.instance.addEndpoint(id, sourceSettings, {uuid: id});
+          break;
+        case 'sink':
+          this.instance.addEndpoint(id, sinkSettings, {uuid: id});
+          break;
+        case 'transform':
+          // Need to id each end point so that it can be used later to make connections.
+          this.instance.addEndpoint(id, sourceSettings, {uuid: 'Left' + id});
+          this.instance.addEndpoint(id, sinkSettings, {uuid: 'Right' + id});
+          break;
       }
-      return styles;
+      this.instance.draggable(id);
     }
 
     function drawConnections() {
@@ -184,28 +133,7 @@ angular.module(PKG.name + '.commons')
       });
     }
 
-    this.drawGraph = function() {
-      var graph = this.getGraph();
-      var nodes = graph.nodes()
-        .map(function(node) {
-          return graph.node(node);
-        });
-
-      var marginLeft = $scope.getGraphMargins(this.plugins);
-      this.plugins.forEach(function(plugin) {
-        plugin.icon = getIcon(plugin.name);
-        plugin.style = generateStyles(plugin.id, nodes, 200, marginLeft);
-        drawNode.call(this, plugin.id, plugin.type);
-      }.bind(this));
-
-      drawConnections.call(this);
-
-      MyPlumbService.setConnections(this.instance.getConnections());
-
-      mapSchemas.call(this);
-
-      $timeout(this.instance.repaintEverything);
-    };
+    MyPlumbService.registerCallBack(this.addPlugin.bind(this));
 
     $scope.$on('$destroy', function() {
       this.instance.reset();
@@ -216,7 +144,7 @@ angular.module(PKG.name + '.commons')
       jsPlumb.setContainer('plumb-container');
       this.instance = jsPlumb.getInstance();
 
-      this.instance.importDefaults(defaultSettings);
+      this.instance.importDefaults(MyPlumbFactory.getSettings().default);
 
       // Need to move this to the controller that is using this directive.
       this.instance.bind('connection', function () {
