d4d52e063ce1a5bd2c8f164032fc7cd1904df6cf
dereklwood <derek@continuuity.com>
2015-07-16 12:24:22 -0700

    ability to not install prerequisites

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,29 +17,9 @@
 # limitations under the License.
 #
 
-# We require Java, so include it and add it to Hadoop env
-include_recipe 'java'
-log 'java-home' do
-  message "JAVA_HOME = #{node['java']['java_home']}"
-end
-ENV['JAVA_HOME'] = node['java']['java_home']
-%w(hadoop hbase hive).each do |envfile|
-  node.default[envfile]["#{envfile}_env"]['java_home'] = node['java']['java_home']
-end
-
-# We need Hadoop/HBase installed
-%w(default hbase).each do |recipe|
-  include_recipe "hadoop::#{recipe}"
-end
-
-# Hive is optional
-if node['cdap'].key?('cdap_site') && node['cdap']['cdap_site'].key?('explore.enabled') &&
-   node['cdap']['cdap_site']['explore.enabled'].to_s == 'true'
-
-  log 'hive-explore-enabled' do
-    message 'Explore module enabled, installing Hive libraries'
-  end
-  include_recipe 'hadoop::hive'
+# Install Java and Hadoop Clients
+unless node['cdap'].key?('skip_prerequisites') && node['cdap']['skip_prerequisites'].to_s == 'true'
+  include_recipe 'cdap::prerequisites'
 end
 
 include_recipe 'ntp'
