7c1df766188fc1a807ecb45e7bde50572532b690
dereklwood <derek@continuuity.com>
2015-07-16 12:34:58 -0700

    fix date

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cdap
 # Attribute:: default
 #
-# Copyright © 2013-2014 Cask Data, Inc.
+# Copyright © 2013-2015 Cask Data, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
