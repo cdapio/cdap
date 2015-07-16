5e520b07fce15190a794a013102c0ab92c53b928
dereklwood <derek@continuuity.com>
2015-07-16 14:57:22 -0700

    replace hdfs dfs with the more generic hadoop fs

--- recipes/init.rb
+++ recipes/init.rb
@@ -19,16 +19,16 @@
 
 # We also need the configuration, so we can run HDFS commands
 execute 'initaction-create-hdfs-cdap-dir' do
-  not_if  "hdfs dfs -test -d #{node['cdap']['cdap_site']['hdfs.namespace']}", :user => node['cdap']['cdap_site']['hdfs.user']
-  command "hdfs dfs -mkdir -p #{node['cdap']['cdap_site']['hdfs.namespace']} && hdfs dfs -chown #{node['cdap']['cdap_site']['hdfs.user']} #{node['cdap']['cdap_site']['hdfs.namespace']}"
+  not_if  "hadoop fs -test -d #{node['cdap']['cdap_site']['hdfs.namespace']}", :user => node['cdap']['cdap_site']['hdfs.user']
+  command "hadoop fs -mkdir -p #{node['cdap']['cdap_site']['hdfs.namespace']} && hadoop fs -chown #{node['cdap']['cdap_site']['hdfs.user']} #{node['cdap']['cdap_site']['hdfs.namespace']}"
   timeout 300
   user 'hdfs'
   group 'hdfs'
 end
 
 execute 'initaction-create-hdfs-cdap-user-dir' do
-  not_if  "hdfs dfs -test -d /user/#{node['cdap']['cdap_site']['hdfs.user']}", :user => node['cdap']['cdap_site']['hdfs.user']
-  command "hdfs dfs -mkdir -p /user/#{node['cdap']['cdap_site']['hdfs.user']} && hdfs dfs -chown #{node['cdap']['cdap_site']['hdfs.user']} /user/#{node['cdap']['cdap_site']['hdfs.user']}"
+  not_if  "hadoop fs -test -d /user/#{node['cdap']['cdap_site']['hdfs.user']}", :user => node['cdap']['cdap_site']['hdfs.user']
+  command "hadoop fs -mkdir -p /user/#{node['cdap']['cdap_site']['hdfs.user']} && hadoop fs -chown #{node['cdap']['cdap_site']['hdfs.user']} /user/#{node['cdap']['cdap_site']['hdfs.user']}"
   timeout 300
   user 'hdfs'
   group 'hdfs'
@@ -36,15 +36,15 @@ end
 
 %w(cdap yarn).each do |u|
   execute "initaction-create-hdfs-mr-jhs-staging-intermediate-done-dir-#{u}" do
-    not_if "hdfs dfs -test -d /tmp/hadoop-yarn/staging/history/done_intermediate/#{u}", :user => u
-    command "hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate/#{u} && hdfs dfs -chown #{u} /tmp/hadoop-yarn/staging/history/done_intermediate/#{u} && hdfs dfs -chmod ugo+rx /tmp/hadoop-yarn/staging/history/done_intermediate/#{u}"
+    not_if "hadoop fs -test -d /tmp/hadoop-yarn/staging/history/done_intermediate/#{u}", :user => u
+    command "hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate/#{u} && hadoop fs -chown #{u} /tmp/hadoop-yarn/staging/history/done_intermediate/#{u} && hadoop fs -chmod ugo+rx /tmp/hadoop-yarn/staging/history/done_intermediate/#{u}"
     timeout 300
     user 'hdfs'
     group 'hdfs'
   end
   execute "initaction-create-hdfs-mr-jhs-staging-done-dir-#{u}" do
-    not_if "hdfs dfs -test -d /tmp/hadoop-yarn/staging/history/done/#{u}", :user => u
-    command "hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging/history/done/#{u} && hdfs dfs -chown #{u} /tmp/hadoop-yarn/staging/history/done/#{u} && hdfs dfs -chmod ugo+rx /tmp/hadoop-yarn/staging/history/done/#{u}"
+    not_if "hadoop fs -test -d /tmp/hadoop-yarn/staging/history/done/#{u}", :user => u
+    command "hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done/#{u} && hadoop fs -chown #{u} /tmp/hadoop-yarn/staging/history/done/#{u} && hadoop fs -chmod ugo+rx /tmp/hadoop-yarn/staging/history/done/#{u}"
     timeout 300
     user 'hdfs'
     group 'hdfs'
