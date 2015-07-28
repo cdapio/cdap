c9883d0e215092f90e28c6664b05758c9be32c73
Edwin Elia <edwinselia@gmail.com>
2015-07-28 14:12:44 -0700

    add programmatically determine ellipsis tooltip

--- cdap-ui/app/directives/tooltip-toggle/tooltip-toggle.js
+++ cdap-ui/app/directives/tooltip-toggle/tooltip-toggle.js
@@ -17,4 +17,29 @@ angular.module(PKG.name + '.commons')
         });
       }
     };
+  })
+
+  .directive('tooltipEllipsis', function ($timeout) {
+    return {
+      restrict: 'A',
+      scope: {
+        ellipsis: '='
+      },
+      link: function (scope, element, attrs) {
+        function isEllipsisActive(e) {
+          return (e[0].offsetWidth > e[0].parentElement.offsetWidth - 20);
+        }
+
+        scope.$watch(attrs.tooltipEllipsis, function () {
+          if (attrs.tooltipEllipsis) {
+            if (isEllipsisActive(element)) {
+              scope.ellipsis = true;
+            } else {
+              scope.ellipsis = false;
+            }
+          }
+        });
+
+      }
+    };
   });
