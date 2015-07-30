833e373742911fed8be6c0b00dabb5534f994c02
Gokul Gunasekaran <gokul@cask.co>
2015-07-30 11:47:11 -0700

    Adding note to refactor context provider, builder MRcontext class. Addressing PR comment

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceContextProvider.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceContextProvider.java
@@ -90,6 +90,8 @@ public final class MapReduceContextProvider {
     return context;
   }
 
+  // TODO: CDAP-3160 : Refactor to remove the need for the stop method below. Provider/Builder classes should not have
+  // methods like stop(), finish() or close().
   public synchronized void stop() {
     if (contextBuilder != null) {
       contextBuilder.finish();
