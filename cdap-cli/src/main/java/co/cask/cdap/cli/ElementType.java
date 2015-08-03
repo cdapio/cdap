63f0b8aa9238ef6cf3eb9a6a66c17b7aa2eb8968
Alvin Wang <alvin@cask.co>
2015-08-03 12:58:20 -0700

    CDAP-3147: CLI stop workflow

--- cdap-cli/src/main/java/co/cask/cdap/cli/ElementType.java
+++ cdap-cli/src/main/java/co/cask/cdap/cli/ElementType.java
@@ -64,7 +64,7 @@ public enum ElementType {
 
   WORKFLOW("workflow", new Noun("workflow"), new Noun("Workflow"), ProgramType.WORKFLOW, null,
            ArgumentName.WORKFLOW,
-           Capability.RUNS, Capability.STATUS, Capability.START,
+           Capability.RUNS, Capability.STATUS, Capability.START, Capability.STOP,
            Capability.LIST, Capability.RUNTIME_ARGS, Capability.PREFERENCES),
 
   FLOWLET("flowlet", new Noun("flowlet"), new Noun("Flowlet"), null, ProgramType.FLOW,
