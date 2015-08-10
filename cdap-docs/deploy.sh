6d3f1491643cafa41fffc4020b1e8c340270edb5
Chris Gianelloni <chris@cask.co>
2015-08-10 14:48:09 -0700

    Copy deploy.sh from develop

--- cdap-docs/deploy.sh
+++ cdap-docs/deploy.sh
@@ -120,8 +120,9 @@ DOCS_SERVERS="${DOCS_SERVER1} ${DOCS_SERVER2}"
 REMOTE_STG_DIR="${REMOTE_STG_BASE}/${PROJECT}/${REMOTE_DIR}"		# e.g. /var/www/html/staging/cdap/develop
 REMOTE_DOCS_DIR="${REMOTE_DOCS_BASE}/${PROJECT}/${REMOTE_DIR}"		# e.g. /var/www/docs/cdap/release
 
+SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 RSYNC_OPTS='-aPh'
-SSH_OPTS='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+RSYNC_SSH_OPTS="ssh ${SSH_OPTS}"
 RSYNC_PATH='sudo rsync'
 
 die ( ) { echo ; echo "ERROR: ${*}" ; echo ; exit 1; }
@@ -149,13 +150,13 @@ function copy_zip_file () {
   cp ${_local_dir}/${_zip_file} ${_local_dir}/${_version}/
 }
 
-# create remote directory prior to rsync
+# delete and create remote directory prior to rsync
 function make_remote_dir () {
   local _user=${1}
   local _host=${2}
   local _rdir=${3} # remote directory
-  decho "ssh ${_user}@${_host} \"sudo mkdir -p ${_rdir}\""
-  ssh ${_user}@${_host} "sudo mkdir -p ${_rdir}" || die "could not create ${_rdir} directory on ${_host}"
+  decho "ssh ${SSH_OPTS} ${_user}@${_host} \"sudo rm -rf ${_rdir} && sudo mkdir -p ${_rdir}\""
+  ssh ${SSH_OPTS} ${_user}@${_host} "sudo rm -rf ${_rdir} && sudo mkdir -p ${_rdir}" || die "could not create ${_rdir} directory on ${_host}"
   decho ""
 }
 
@@ -167,13 +168,9 @@ function sync_local_dir_to_remote_dir () {
   local _zip_file=${4}
   local _local_dir=${5}
   local _version=${6}
-  # if there is no branch name to append, append version to directory name, otherwise all files end up one level too high
-  if [ "${BRANCH}" == '' ]; then
-    _rdir=${_rdir}/${_version}
-  fi
   decho ""
-  decho "rsync ${RSYNC_OPTS} -e \"${SSH_OPTS}\" --rsync-path=\"${RSYNC_PATH}\" ${_local_dir}/${_version} \"${_user}@${_host}:${_rdir}\"" 
-  rsync ${RSYNC_OPTS} -e "${SSH_OPTS}" --rsync-path="${RSYNC_PATH}" ${_local_dir}/${_version}/.h* ${_local_dir}/${_version}/* "${_user}@${_host}:${_rdir}" || die "could not rsync ${_local_dir} to ${_rdir} on ${_host}" 
+  decho "rsync ${RSYNC_OPTS} -e \"${RSYNC_SSH_OPTS}\" --rsync-path=\"${RSYNC_PATH}\" ${_local_dir}/${_version} \"${_user}@${_host}:${_rdir}\"" 
+  rsync ${RSYNC_OPTS} -e "${RSYNC_SSH_OPTS}" --rsync-path="${RSYNC_PATH}" ${_local_dir}/${_version}/.h* ${_local_dir}/${_version}/* "${_user}@${_host}:${_rdir}" || die "could not rsync ${_local_dir} to ${_rdir} on ${_host}" 
 }
 
 # main deploy function
@@ -186,6 +183,12 @@ function deploy () {
   local _version=${6}
   local _branch=${7}
   decho "deploying to ${_host}"
+  # if there is no branch name to append, append version to directory name, otherwise:
+  #   cdap dir gets blown away in remote dir delete/create
+  #   otherwise all files end up one level too high after rsync
+  if [ "${BRANCH}" == '' ]; then
+    _remote_dir=${_remote_dir}/${_version}
+  fi
   copy_zip_file ${_zip_file} ${_local_dir} ${_version}
   make_remote_dir ${_user} ${_host} ${_remote_dir}
   sync_local_dir_to_remote_dir ${_user} ${_host} ${_remote_dir} ${_zip_file} ${_local_dir} ${_version}
@@ -200,16 +203,20 @@ decho "DEPLOY_TO_DOCS=${DEPLOY_TO_DOCS}"
 
 ### DEVELOP => Staging
 if [[ "${DEPLOY_TO_STG}" == 'yes' ]]; then
-  decho "Deploying to Staging server"
+  decho "Deploying artifacts to Staging server"
   deploy ${USER} ${STG_SERVER} ${REMOTE_STG_DIR} ${ZIP_FILE} ${FILE_PATH} ${VERSION} ${BRANCH}
 fi
 
 ### RELEASE => Docs Servers
 if [[ "${DEPLOY_TO_DOCS}" == 'yes' ]]; then
-  decho "Deploying to Docs servers"
-  for i in ${DOCS_SERVERS}; do
-    deploy ${USER} ${i} ${REMOTE_DOCS_DIR} ${ZIP_FILE} ${FILE_PATH} ${VERSION} ${BRANCH}
-  done
+  if [ "${BRANCH}" == '' ]; then
+    decho "Deploying artifacts to Docs servers"
+    for i in ${DOCS_SERVERS}; do
+      deploy ${USER} ${i} ${REMOTE_DOCS_DIR} ${ZIP_FILE} ${FILE_PATH} ${VERSION}
+    done
+  else
+    decho "Do not deploy artifacts from feature branches to Docs servers"
+  fi
 fi
-decho "####################### DEPLOYING DONE #######################"
+decho "####################### DONE DEPLOYING #######################"
 
