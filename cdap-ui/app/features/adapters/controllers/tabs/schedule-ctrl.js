a20e868caaa74d1fb1b30a85f7cc9a7b0d8fd913
Ajai <ajainarayanan@gmail.com>
2015-08-19 15:22:49 -0700

    Fixes adapters schedules view to use scheudles-view component

--- cdap-ui/app/features/adapters/controllers/tabs/schedule-ctrl.js
+++ cdap-ui/app/features/adapters/controllers/tabs/schedule-ctrl.js
@@ -2,22 +2,12 @@ angular.module(PKG.name + '.feature.adapters')
   .controller('ScheduleController', function($scope, rAdapterDetail) {
     this.schedules = [];
     var schedule = {};
-    var cronExpression;
     if (angular.isObject(rAdapterDetail.schedule)) {
       schedule = rAdapterDetail.schedule.schedule;
       schedule.status = 'SCHEDULED';
       schedule.scheduleType = 'TIME';
       schedule.isOpen = true;
       schedule.lastrun = 'UNAVAILABLE';
-
-      cronExpression = schedule.cronExpression.split(' ');
-      schedule.time = {};
-      schedule.time.min = cronExpression[0];
-      schedule.time.hour = cronExpression[1];
-      schedule.time.day = cronExpression[2];
-      schedule.time.month = cronExpression[3];
-      schedule.time.week = cronExpression[4];
-
       this.schedules.push(schedule);
     }
   });
