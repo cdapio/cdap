4abf425dfd5f7d9b19d11c01794f32e209e8f895
David Bajot <dbajot@cask.co>
2015-08-21 15:47:44 -0700

    input JSON example

--- cdap-validation/bin/install_validator.py
+++ cdap-validation/bin/install_validator.py
@@ -11,6 +11,20 @@ ambari = { 'api_test': 'v1/clusters', 'version': 'v1', 'subdir': 'ambari_configs
 ### input processing functions
 # coming soon
 
+# To test the framework, create a JSON formatted input file called 'input.json' in the same directory as the scripts.
+# file should have the following structure:
+
+#{
+#  "params":
+#  {
+#    "verbose": "default",
+#    "uri": "http://ambari_server_fqdn:8080/",
+#    "userpass": "username:password",
+#    "cluster_name": "clustername",
+#    "modules": "config"
+#  }
+#}
+
 ### test framework functions
 testing.testing(base_variables)
 
