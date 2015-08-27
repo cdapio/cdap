551b4d3037a617c46244f0ced9dc0fb0f866909b
David Bajot <dbajot@cask.co>
2015-08-27 16:34:32 -0700

    more clean up

--- cdap-distributions/bin/prep_and_stage_bundles.sh
+++ cdap-distributions/bin/prep_and_stage_bundles.sh
@@ -17,23 +17,21 @@
 ######################################################################################
 # Create RPM and Debian bundles and upload them directly to docs1
 # This script expects to receive 3 parameters in this order:
-#   $1 USER                     REMOTE_USER
+#   $1 remote user              REMOTE_USER
 #   $2 remote host              REMOTE_HOST
 #   $3 target directory         REMOTE_INCOMING_DIR
 ######################################################################################
+_script=`basename $0`
 DEBUG=${DEBUG:-no}              ### set to yes for debugging
 RUN_DATE=`date '+%Y%m%d_%R'`
 SCRIPT=`basename ${BASH_SOURCE[0]}`                     ### Set Script Name variable
 BUILD_PACKAGE='cdap'                                   ## e.g. cdap ## a.k.a. BUILD_PACKAGE
-VERSION=$(cdap-distributions/target/stage-packaging/opt/cdap/distributions/VERSION)
+VERSION=$(<cdap-distributions/target/stage-packaging/opt/cdap/distributions/VERSION)
 REMOTE_USER=${1}                                        ### remote user
 REMOTE_HOST=${2:-127.0.0.1}                             ### remote host
 REMOTE_INCOMING_DIR=${3}                                ### target directory on remote host
-BUILD_PACKAGE=${BUILD_PACKAGE:-cdap}
-REMOTE_BASE_DIR="incoming"
-USER=${USER:-bamboo}
 TMP_DIR='/tmp/bundles'
-S3STG_DIR=${S3STG_DIR:-/data/s3stg} # change it #####
+S3STG_DIR=${S3STG_DIR:-/data/s3stg}
 DEB_BUNDLE="${BUILD_PACKAGE}-deb-bundle"
 RPM_BUNDLE="${BUILD_PACKAGE}-rpm-bundle"
 DEB_BUNDLE_DIR="${TMP_DIR}/${DEB_BUNDLE}"
@@ -44,7 +42,7 @@ DEB_BUNDLE_TGZ="${DISTRIBUTED_DEB_BUNDLE}-${VERSION}.tgz"
 RPM_BUNDLE_TGZ="${DISTRIBUTED_RPM_BUNDLE}-${VERSION}.tgz"
 DEB_BUNDLE_TGZ_PATH="${DEB_BUNDLE_DIR}/${DEB_BUNDLE_TGZ}"
 RPM_BUNDLE_TGZ_PATH="${RPM_BUNDLE_DIR}/${RPM_BUNDLE_TGZ}"
-S3STG_INCOMING="${S3STG_DIR}/incoming"
+#S3STG_INCOMING="${S3STG_INCOMING:-/data/s3stg/incoming}"
 S3STG_INCOMING_PROJ="${S3STG_INCOMING}/${BUILD_PACKAGE}"
 STG_DIR="${S3STG_INCOMING}/downloads/co/cask/${BUILD_PACKAGE}"
 STG_DEB_DIR="${STG_DIR}/${DISTRIBUTED_DEB_BUNDLE}/${VERSION}"
@@ -74,7 +72,7 @@ die ( ) { echo ; echo "ERROR: ${*}" ; echo ; exit 1; }
 
 clean() { test -d ${TMP_DIR} && rm -rf ${TMP_DIR}; };
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
   die "You need to pass project and version to ${_script}"$'\n\n'"Example: ./${_script} cdap 3.0.0"
 fi
 
@@ -87,28 +85,32 @@ create_bundles() {
    mkdir -p ${DEB_BUNDLE_DIR} ${RPM_BUNDLE_DIR} || die "Unable to create bundle processing directories"
    mkdir -p ${STG_DEB_DIR} ${STG_RPM_DIR} || die "Unable to create bundle staging directories"
    OUTGOING_DIR=${BUILD_PACKAGE}/${VERSION}
-   echo ${OUTGOING_DIR}
+   decho ${OUTGOING_DIR}
    
    ### FIND ARTIFACTS
    cd ${S3STG_INCOMING_PROJ} || die "Unable to cd to ${S3STG_INCOMING_PROJ} directory"
-   find . -name "${BUILD_PACKAGE}-*${VERSION}-*.deb" -exec cp '{}' ${DEB_BUNDLE_DIR}/. \; || die "Unable to copy Debian ${VERSION} packages to TMP directory"
+   find . -name "${BUILD_PACKAGE}_*${VERSION}-*.deb" -exec cp '{}' ${DEB_BUNDLE_DIR}/. \; || die "Unable to copy Debian ${VERSION} packages to TMP directory"
    find . -name "${BUILD_PACKAGE}-*${VERSION}-*.rpm" -exec cp '{}' ${RPM_BUNDLE_DIR}/. \; || die "Unable to copy ${VERSION} RPMs to TMP directory"
    
    # create and verify debian tarball
    cd ${DEB_BUNDLE_DIR} || die "Unable to cd to ${DEB_BUNDLE_DIR} directory"
    tar czvf ${DEB_BUNDLE_TGZ} * || die "Unable to create Debian tar bundle"
-   tar tzvf ${DEB_BUNDLE_TGZ} || die "Unable to show contents of Debian tar bundle"
+   if [[ "${DEBUG}" == 'yes' ]]; then
+     tar tzf ${DEB_BUNDLE_TGZ} || die "Unable to show contents of Debian tar bundle"
+   fi
    
    # create and verify RPM tarball
    cd ${RPM_BUNDLE_DIR} || die "Unable to cd to ${RPM_BUNDLE_DIR} directory"
    tar czvf ${RPM_BUNDLE_TGZ} * || die "Unable to create RPM tar bundle"
-   tar tzvf ${RPM_BUNDLE_TGZ} || die "Unable to show contents of RPM tar bundle"
+   if [[ "${DEBUG}" == 'yes' ]]; then
+     tar tzf ${RPM_BUNDLE_TGZ} || die "Unable to show contents of RPM tar bundle"
+   fi
 }
 
 sync_bundles() {
    # SYNC bundle(s) to remote server
-   decho "rsyncing with rsync -av -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null\" ${RSYNC_QUIET} ${DEB_BUNDLE_TGZ_PATH} ${STG_DEB_DIR}/.  2>&1"
-   decho "rsyncing with rsync -av -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null\" ${RSYNC_QUIET} ${RPM_BUNDLE_TGZ_PATH} ${STG_RPM_DIR}/.  2>&1"
+   decho 'rsyncing with rsync -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${RSYNC_QUIET} ${DEB_BUNDLE_TGZ_PATH} ${STG_DEB_DIR}/.  2>&1'
+   decho 'rsyncing with rsync -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${RSYNC_QUIET} ${RPM_BUNDLE_TGZ_PATH} ${STG_RPM_DIR}/.  2>&1'
    rsync -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${RSYNC_QUIET} ${DEB_BUNDLE_TGZ_PATH} ${STG_DEB_DIR}/. 2>&1 || die "could not rsync ${DEB_BUNDLE_TGZ_PATH} as ${REMOTE_USER} to ${REMOTE_HOST}: ${!}"
    rsync -av -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${RSYNC_QUIET} ${RPM_BUNDLE_TGZ_PATH} ${STG_RPM_DIR}/. 2>&1 || die "could not rsync ${RPM_BUNDLE_TGZ_PATH} as ${REMOTE_USER} to ${REMOTE_HOST}: ${!}"
 
@@ -135,4 +137,3 @@ sync_bundles
 
 
 exit 0
-
