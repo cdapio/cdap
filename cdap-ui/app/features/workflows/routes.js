9cd7a3aa30d563b401b3a11add672356ba8a5308
Andrew Westlund <astewes@gmail.com>
2015-08-28 12:25:02 -0700

    Fixes workflow routes + detail and run detail templates

--- cdap-ui/app/features/workflows/routes.js
+++ cdap-ui/app/features/workflows/routes.js
@@ -2,72 +2,57 @@ angular.module(`${PKG.name}.feature.workflows`)
   .config(function($stateProvider, $urlRouterProvider, MYAUTH_ROLE) {
     $stateProvider
       .state('workflows', {
-        url: '/workflows',
+        url: '/workflows/:programId',
         abstract: true,
         parent: 'programs',
         data: {
           authorizedRoles: MYAUTH_ROLE.all,
           highlightTab: 'development'
         },
+        resolve : {
+          rRuns: function(MyDataSource, $stateParams, $q) {
+            var defer = $q.defer();
+            var dataSrc = new MyDataSource();
+            // Using _cdapPath here as $state.params is not updated with
+            // runid param when the request goes out
+            // (timing issue with re-direct from login state).
+            dataSrc.request({
+              _cdapPath: `/namespaces/${$stateParams.namespace}/apps/${$stateParams.appId}/workflows/${$stateParams.programId}/runs`
+            })
+              .then(function(res) {
+                defer.resolve(res);
+              });
+            return defer.promise;
+          }
+        },
         template: '<ui-view/>'
       })
 
         .state('workflows.detail', {
-          url: '/:programId',
+          url: '/runs',
+          templateUrl: '/assets/features/workflows/templates/detail.html',
+          controller: 'WorkflowsRunsController',
+          controllerAs: 'RunsController',
           data: {
             authorizedRoles: MYAUTH_ROLE.all,
             highlightTab: 'development'
           },
           ncyBreadcrumb: {
             parent: 'apps.detail.overview.status',
-            label: 'Workflows',
-            skip: true
-          },
-          resolve : {
-            rRuns: function(MyDataSource, $stateParams, $q) {
-              var defer = $q.defer();
-              var dataSrc = new MyDataSource();
-              // Using _cdapPath here as $state.params is not updated with
-              // runid param when the request goes out
-              // (timing issue with re-direct from login state).
-              dataSrc.request({
-                _cdapPath: `/namespaces/${$stateParams.namespace}/apps/${$stateParams.appId}/workflows/${$stateParams.programId}/runs`
-              })
-                .then(function(res) {
-                  defer.resolve(res);
-                });
-              return defer.promise;
-            }
-          },
-          templateUrl: '/assets/features/workflows/templates/detail.html'
+            label: '{{$state.params.programId}}'
+          }
         })
 
-          .state('workflows.detail.runs', {
-            url: '/runs',
-            data: {
-              authorizedRoles: MYAUTH_ROLE.all,
-              highlightTab: 'development'
-            },
+          .state('workflows.detail.run', {
+            url: '/:runid',
+            templateUrl: '/assets/features/workflows/templates/tabs/runs/run-detail.html',
+            controller: 'WorkflowsRunsDetailController',
+            controllerAs: 'RunsDetailController',
             ncyBreadcrumb: {
-              label: '{{$state.params.programId}}'
-            },
-            templateUrl: '/assets/features/workflows/templates/tabs/runs.html',
-            controller: 'WorkflowsRunsController',
-            controllerAs: 'RunsController'
+              label: '{{$state.params.runid}}',
+              parent: 'workflows.detail'
+            }
           })
-            .state('workflows.detail.runs.run', {
-              url: '/:runid',
-              data: {
-                authorizedRoles: MYAUTH_ROLE.all,
-                highlightTab: 'development'
-              },
-              ncyBreadcrumb: {
-                label: '{{$state.params.runid}}'
-              },
-              templateUrl: '/assets/features/workflows/templates/tabs/runs/run-detail.html',
-              controller: 'WorkflowsRunsDetailController'
-            })
-
           .state('workflows.detail.history', {
             url: '/history',
             data: {
@@ -82,7 +67,6 @@ angular.module(`${PKG.name}.feature.workflows`)
             controller: 'WorkflowsRunsController',
             controllerAs: 'RunsController'
           })
-
           .state('workflows.detail.schedules', {
             url: '/schedules',
             data: {
