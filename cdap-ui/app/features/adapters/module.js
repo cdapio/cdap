7fc41e9ab5e9c89ba8bad594e735c324a136dd12
Bhooshan Mogal <bhooshan@cask.co>
2015-09-02 19:05:39 -0700

    Added copyright to all .js files

--- cdap-ui/app/features/adapters/module.js
+++ cdap-ui/app/features/adapters/module.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 angular.module(PKG.name + '.feature.adapters', [])
   .constant('IMPLICIT_SCHEMA', {
     clf: '{"type":"record","name":"etlSchemaBody","fields":[{"name":"remote_host","type":["string","null"]},{"name":"remote_login","type":["string","null"]},{"name":"auth_user","type":["string","null"]},{"name":"date","type":["string","null"]},{"name":"request","type":["string","null"]},{"name":"status","type":["int","null"]},{"name":"content_length","type":["int","null"]},{"name":"referrer","type":["string","null"]},{"name":"user_agent","type":["string","null"]}]}',
