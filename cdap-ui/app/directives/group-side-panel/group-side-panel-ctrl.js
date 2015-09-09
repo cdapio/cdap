a3863a0d9e09b33094c5c51b7e9fe13a6747cca5
Andrew Westlund <astewes@gmail.com>
2015-09-09 16:39:00 -0700

    moves top panel + left panel (styling only) to adapters

--- cdap-ui/app/directives/group-side-panel/group-side-panel-ctrl.js
+++ cdap-ui/app/directives/group-side-panel/group-side-panel-ctrl.js
@@ -15,81 +15,11 @@
  */
 
 angular.module(PKG.name + '.commons')
-  .controller('MySidePanel', function ($scope, $stateParams, MyAppDAGService, ModalConfirm, $alert, $bootstrapModal, EventPipe) {
+  .controller('MySidePanel', function ($scope) {
     this.groups = $scope.panelGroups;
     this.placement = $scope.placement;
     this.panel = $scope.panel;
-
     this.isSubMenu = $scope.isSubMenu === 'true';
-
-    this.metadata = MyAppDAGService['metadata'];
-    function resetMetadata() {
-      this.metadata = MyAppDAGService['metadata'];
-    }
-
-    MyAppDAGService.registerResetCallBack(resetMetadata.bind(this));
-
-    if ($stateParams.name) {
-      this.metadata.name = $stateParams.name;
-    }
-    if ($stateParams.type) {
-      if (['ETLBatch', 'ETLRealtime'].indexOf($stateParams.type) !== -1) {
-        this.metadata.template.type = $stateParams.type;
-      } else {
-        $alert({
-          type: 'danger',
-          content: 'Invalid template type. Has to be either ETLBatch or ETLRealtime'
-        });
-      }
-    }
-
-    this.showMetadataModal = function() {
-      EventPipe.emit('popovers.close');
-
-      if (this.metadata.error) {
-        delete this.metadata.error;
-      }
-      MyAppDAGService.isConfigTouched = true;
-      $bootstrapModal
-        .open({
-          templateUrl: '/assets/features/adapters/templates/create/popovers/metadata-detail.html',
-          size: 'lg',
-          windowClass: 'adapter-modal',
-          keyboard: true,
-          controller: ['$scope', 'metadata', function($scope, metadata) {
-            $scope.modelCopy = angular.copy(this.metadata);
-            $scope.metadata = metadata;
-            $scope.reset = function () {
-              metadata['name'] = $scope.modelCopy.name;
-              metadata['description'] = $scope.modelCopy.description;
-            }.bind(this);
-
-            function closeFn() {
-              $scope.reset();
-              $scope.$close('cancel');
-            }
-
-            ModalConfirm.confirmModalAdapter(
-              $scope,
-              $scope.metadata,
-              $scope.modelCopy,
-              closeFn
-            );
-
-
-          }.bind(this)],
-          resolve: {
-            metadata: function() {
-              return this['metadata'];
-            }.bind(this)
-          }
-        })
-        .result
-        .finally(function() {
-          MyAppDAGService.metadata.name = this.metadata.name;
-          MyAppDAGService.metadata.description = this.metadata.description;
-        }.bind(this));
-    };
 
     this.openGroup = function (group) {
       if (this.openedGroup === group.name && this.showGroupItems) {
