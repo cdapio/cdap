67263a855ebcc189d97983147498587f8d1cf167
Rohit Sinha <rsinha@cask.co>
2015-09-11 16:48:27 -0700

    Add ScannerContextReader for HBase1.1 support

--- cdap-hbase-compat-1.1/src/main/java/co/cask/cdap/data2/increment/hbase11/IncrementSummingScanner.java
+++ cdap-hbase-compat-1.1/src/main/java/co/cask/cdap/data2/increment/hbase11/IncrementSummingScanner.java
@@ -30,6 +30,7 @@ import org.apache.hadoop.hbase.regionserver.Region;
 import org.apache.hadoop.hbase.regionserver.RegionScanner;
 import org.apache.hadoop.hbase.regionserver.ScanType;
 import org.apache.hadoop.hbase.regionserver.ScannerContext;
+import org.apache.hadoop.hbase.regionserver.ScannerContextReader;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.Closeable;
@@ -151,7 +152,15 @@ class IncrementSummingScanner implements RegionScanner {
     int addedCnt = 0;
     baseScanner.startNext();
     Cell cell;
-    while ((cell = baseScanner.peekNextCell(scannerContext)) != null && (getBatch() <= 0 || addedCnt < getBatch())) {
+    // We need to to access the batch limit (limit on number of columns) in the ScannerContext.
+    // The ScannerContext does not exposes its method to get the limits so we have a Reader under the same package
+    // which can supply us these limits given a ScannerContext. Also, we cannot depend on the limit tracking of the
+    // internal scanner as it does not differentiate between an increment cell and a non-increment cell.
+    // So, we read all the cells from the internal scanner in batches of limit and do the tracking for columns here
+    // and stop when we have reached the limit.
+    ScannerContextReader scannerContextReader = new ScannerContextReader(scannerContext);
+    int limit = scannerContextReader.getBatchLimit();
+    while ((cell = baseScanner.peekNextCell(scannerContext)) != null && (limit <= 0 || addedCnt < limit)) {
       // we use the "peek" semantics so that only once cell is ever emitted per iteration
       // this makes is clearer and easier to enforce that the returned results are <= limit
       if (LOG.isTraceEnabled()) {
