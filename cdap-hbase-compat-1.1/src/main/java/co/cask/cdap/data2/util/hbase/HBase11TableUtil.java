9b55bccfdd8095442289d70b90c29600cae6c8df
Rohit Sinha <rsinha@cask.co>
2015-09-14 19:22:31 -0700

    Get batch limit from ScannerContext through reflection

--- cdap-hbase-compat-1.1/src/main/java/co/cask/cdap/data2/util/hbase/HBase11TableUtil.java
+++ cdap-hbase-compat-1.1/src/main/java/co/cask/cdap/data2/util/hbase/HBase11TableUtil.java
@@ -47,9 +47,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.security.CodeSource;
 import java.util.List;
 import java.util.Map;
 
