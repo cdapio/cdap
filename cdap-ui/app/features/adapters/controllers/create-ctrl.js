513a6d33c9ea2b84afe832f4f2d50a60d8637bd1
Ajai <ajainarayanan@gmail.com>
2015-09-15 00:11:14 -0700

    Adds an intermediate state to Hydrator studio from anywhere

--- cdap-ui/app/features/adapters/controllers/create-ctrl.js
+++ cdap-ui/app/features/adapters/controllers/create-ctrl.js
@@ -1,56 +1,33 @@
-/*
- * Copyright © 2015 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 angular.module(PKG.name + '.feature.adapters')
-  .controller('AdapterCreateController', function(MyAppDAGService, $scope, rConfig, $modalStack, EventPipe, $window, $timeout) {
+  .controller('AdapterCreateController', function($timeout, $state, $alert) {
+    this.importFile = function(files) {
+      var reader = new FileReader();
+      reader.readAsText(files[0], 'UTF-8');
 
-    var confirmOnPageExit = function (e) {
-
-      if (!MyAppDAGService.isConfigTouched) { return; }
-      // If we haven't been passed the event get the window.event
-      e = e || $window.event;
-      var message = 'You have unsaved changes.';
-      // For IE6-8 and Firefox prior to version 4
-      if (e) {
-        e.returnValue = message;
-      }
-      // For Chrome, Safari, IE8+ and Opera 12+
-      return message;
+      reader.onload = function (evt) {
+         var data = evt.target.result;
+         var jsonData;
+         try {
+           jsonData = JSON.parse(data);
+         } catch(e) {
+           $alert({
+             type: 'danger',
+             content: 'Error in the JSON imported.'
+           });
+           console.log('ERROR in imported json: ', e);
+           return;
+         }
+         $state.go('adapters.create.studio', {
+           data: jsonData,
+           type: jsonData.artifact.name
+         });
+      };
     };
-    $window.onbeforeunload = confirmOnPageExit;
-
-    $scope.$on('$stateChangeStart', function (event) {
-      if (MyAppDAGService.isConfigTouched) {
-        var response = confirm('You have unsaved changes. Are you sure you want to exit this page?');
-        if (!response) {
-          event.preventDefault();
-        }
-      }
-    });
-
-    if (rConfig) {
+    
+    this.openFileBrowser = function() {
       $timeout(function() {
-        MyAppDAGService.setNodesAndConnectionsFromDraft(rConfig);
+        document.getElementById('adapter-import-config-link').click();
       });
-    }
+    };
 
-    $scope.$on('$destroy', function() {
-      $modalStack.dismissAll();
-      $window.onbeforeunload = null;
-      EventPipe.cancelEvent('plugin.reset');
-      EventPipe.cancelEvent('schema.clear');
-    });
   });
