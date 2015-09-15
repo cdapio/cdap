24200edb1667298ef4aeb32e5dd947b00f7636bf
Ajai <ajainarayanan@gmail.com>
2015-09-15 11:23:56 -0700

    Adds license headers + Pipelines button on the overview

--- cdap-ui/app/features/adapters/controllers/create-ctrl.js
+++ cdap-ui/app/features/adapters/controllers/create-ctrl.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
 angular.module(PKG.name + '.feature.adapters')
   .controller('AdapterCreateController', function($timeout, $state, $alert) {
     this.importFile = function(files) {
@@ -23,7 +40,7 @@ angular.module(PKG.name + '.feature.adapters')
          });
       };
     };
-    
+
     this.openFileBrowser = function() {
       $timeout(function() {
         document.getElementById('adapter-import-config-link').click();
