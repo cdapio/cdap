2bbb7468a09a5008f7c6699364130cf6ab575979
Gokul Gunasekaran <gokul@cask.co>
2015-09-16 16:14:06 -0700

    Package moved

--- cdap-unit-test/src/test/java/co/cask/cdap/test/app/AppWithDuplicateStreams.java
+++ cdap-unit-test/src/test/java/co/cask/cdap/test/app/AppWithDuplicateStreams.java
@@ -19,7 +19,7 @@ package co.cask.cdap.test.app;
 import co.cask.cdap.api.Config;
 import co.cask.cdap.api.app.AbstractApplication;
 import co.cask.cdap.api.dataset.lib.KeyValueTable;
-import co.cask.cdap.api.templates.plugins.PluginProperties;
+import co.cask.cdap.api.plugin.PluginProperties;
 import co.cask.cdap.api.worker.AbstractWorker;
 
 /**
