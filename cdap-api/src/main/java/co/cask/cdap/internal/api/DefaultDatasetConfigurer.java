4e21a606680aeec52538fc9fd7cd823f1d073601
Gokul Gunasekaran <gokul@cask.co>
2015-09-16 16:31:13 -0700

    Initial check-in of error checks to catch addition of multiple datasets, streams, datasetmodules

--- cdap-api/src/main/java/co/cask/cdap/internal/api/DefaultDatasetConfigurer.java
+++ cdap-api/src/main/java/co/cask/cdap/internal/api/DefaultDatasetConfigurer.java
@@ -23,9 +23,12 @@ import co.cask.cdap.api.dataset.Dataset;
 import co.cask.cdap.api.dataset.DatasetProperties;
 import co.cask.cdap.api.dataset.module.DatasetModule;
 import co.cask.cdap.internal.dataset.DatasetCreationSpec;
+import com.google.common.base.Preconditions;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Implementation of {@link DatasetConfigurer} for adding datasets and streams in Configurers.
@@ -50,21 +53,43 @@ public class DefaultDatasetConfigurer implements DatasetConfigurer {
   }
 
   public void addStreams(Map<String, StreamSpecification> streams) {
+    Set<String> duplicateStreams = findDuplicates(streams.keySet(), this.streams.keySet());
+    checkArgument(duplicateStreams.isEmpty(),
+                  "Streams %s have been added already. Remove the duplicates.", duplicateStreams);
     this.streams.putAll(streams);
   }
 
   public void addDatasetSpecs(Map<String, DatasetCreationSpec> datasetSpecs) {
+    Set<String> duplicateDatasetSpecs = findDuplicates(datasetSpecs.keySet(), this.datasetSpecs.keySet());
+    checkArgument(duplicateDatasetSpecs.isEmpty(),
+                  "Dataset Instances %s have already been added. Remove the duplicates.", duplicateDatasetSpecs);
     this.datasetSpecs.putAll(datasetSpecs);
   }
 
   public void addDatasetModules(Map<String, String> datasetModules) {
+    for (Map.Entry<String, String> newEntry : datasetModules.entrySet()) {
+      if (datasetModules.containsKey(newEntry.getKey())) {
+        Preconditions.checkArgument(datasetModules.get(newEntry.getKey()).equals(newEntry.getValue()),
+                                    String.format(
+                                      "DatasetModule %s has been already added with different class %s",
+                                      newEntry.getKey(), datasetModules.get(newEntry.getKey())));
+      }
+    }
     this.datasetModules.putAll(datasetModules);
+  }
+
+  private Set<String> findDuplicates(Set<String> setOne, Set<String> setTwo) {
+    Set<String> duplicates = new HashSet<>(setOne);
+    duplicates.retainAll(setTwo);
+    return duplicates;
   }
 
   @Override
   public void addStream(Stream stream) {
     checkArgument(stream != null, "Stream cannot be null.");
     StreamSpecification spec = stream.configure();
+    checkArgument(!streams.containsKey(spec.getName()),
+                  "Stream %s has already been added. Remove the duplicate addition.", spec.getName());
     streams.put(spec.getName(), spec);
   }
 
@@ -78,12 +103,16 @@ public class DefaultDatasetConfigurer implements DatasetConfigurer {
   public void addDatasetModule(String moduleName, Class<? extends DatasetModule> moduleClass) {
     checkArgument(moduleName != null, "Dataset module name cannot be null.");
     checkArgument(moduleClass != null, "Dataset module class cannot be null.");
+    checkArgument(!datasetModules.containsKey(moduleName),
+                  "DatasetModule %s has already been added. Remove the duplicate addition.", moduleName);
     datasetModules.put(moduleName, moduleClass.getName());
   }
 
   @Override
   public void addDatasetType(Class<? extends Dataset> datasetClass) {
     checkArgument(datasetClass != null, "Dataset class cannot be null.");
+    checkArgument(!datasetModules.containsKey(datasetClass.getName()),
+                  "DatasetType %s has already been added. Remove the duplicate addition.", datasetClass.getName());
     datasetModules.put(datasetClass.getName(), datasetClass.getName());
   }
 
@@ -92,6 +121,8 @@ public class DefaultDatasetConfigurer implements DatasetConfigurer {
     checkArgument(datasetInstanceName != null, "Dataset instance name cannot be null.");
     checkArgument(typeName != null, "Dataset type name cannot be null.");
     checkArgument(properties != null, "Instance properties name cannot be null.");
+    checkArgument(!datasetSpecs.containsKey(datasetInstanceName),
+                  "DatasetInstance %s has already been added. Remove the duplicate addition.", datasetInstanceName);
     datasetSpecs.put(datasetInstanceName,
                          new DatasetCreationSpec(datasetInstanceName, typeName, properties));
   }
@@ -107,8 +138,10 @@ public class DefaultDatasetConfigurer implements DatasetConfigurer {
     checkArgument(datasetInstanceName != null, "Dataset instance name cannot be null.");
     checkArgument(datasetClass != null, "Dataset class name cannot be null.");
     checkArgument(properties != null, "Instance properties name cannot be null.");
+    checkArgument(!datasetSpecs.containsKey(datasetInstanceName),
+                  "DatasetInstance %s has already been added. Remove the duplicate addition.", datasetInstanceName);
     datasetSpecs.put(datasetInstanceName,
-                         new DatasetCreationSpec(datasetInstanceName, datasetClass.getName(), properties));
+                     new DatasetCreationSpec(datasetInstanceName, datasetClass.getName(), properties));
     datasetModules.put(datasetClass.getName(), datasetClass.getName());
   }
 
