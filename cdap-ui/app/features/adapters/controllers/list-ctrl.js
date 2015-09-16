005a55007327a2e9095e3004b867d4bf05a2a840
Andrew Westlund <astewes@gmail.com>
2015-09-16 11:18:00 -0700

    adds styling + Drafts to My Pipelines list

--- cdap-ui/app/features/adapters/controllers/list-ctrl.js
+++ cdap-ui/app/features/adapters/controllers/list-ctrl.js
@@ -14,9 +14,11 @@
  * the License.
  */
 
+var alertpromise;
 angular.module(PKG.name + '.feature.adapters')
-  .controller('AdaptersListController', function(myAdapterApi, $stateParams, GLOBALS) {
+  .controller('AdaptersListController', function(myAdapterApi, $stateParams, GLOBALS, mySettings, $state, $alert, $timeout, myAlert, myHelpers) {
     this.adaptersList = [];
+    this.drafts  = [];
     this.currentPage = 1;
     this.searchText = '';
 
@@ -28,5 +30,60 @@ angular.module(PKG.name + '.feature.adapters')
       .$promise
       .then(function success(res) {
         this.adaptersList = res;
+        console.log(this.adaptersList);
       }.bind(this));
+
+      mySettings.get('adapterDrafts')
+        .then(function(res) {
+          if (res && Object.keys(res).length) {
+            angular.forEach(res, function(value, key) {
+              this.drafts.push({
+                isdraft: true,
+                name: key,
+                template: myHelpers.objectQuery(value, 'artifact', 'name'),
+                status: '-',
+                description: myHelpers.objectQuery(value, 'description')
+              });
+            });
+          }
+        });
+
+      this.deleteDraft = function(draftName) {
+        mySettings.get('adapterDrafts')
+          .then(function(res) {
+            if (res[draftName]) {
+              delete res[draftName];
+            }
+            return mySettings.set('adapterDrafts', res);
+          })
+          .then(
+            function success() {
+              var alertObj = {
+                type: 'success',
+                content: 'Adapter draft ' + draftName + ' delete successfully'
+              }, e;
+              if (!alertpromise) {
+                alertpromise = $alert(alertObj);
+                e = this.$on('alert.hide', function() {
+                  alertpromise = null;
+                  e(); // un-register from listening to the hide event of a closed alert.
+                });
+              }
+              $state.reload();
+            },
+            function error() {
+              var alertObj = {
+                type: 'danger',
+                content: 'Adapter draft ' + draftName + ' delete failed'
+              }, e;
+              if (!alertpromise) {
+                alertpromise = $alert(alertObj);
+                e = this.$on('alert.hide', function() {
+                  alertpromise = null;
+                  e();
+                });
+              }
+              $state.reload();
+            });
+      };
   });
