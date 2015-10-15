1df72855caacd3c3136de6916d101ebea9e1c376
sagarkapare <sagar@cask.co>
2015-10-14 17:05:20 -0700

    Added implementation of SearchService.

---
+++ cdap-watchdog/src/main/java/co/cask/cdap/search/SearchService.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.search;
+
+import com.google.common.util.concurrent.AbstractExecutionThreadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ */
+public class SearchService extends AbstractExecutionThreadService {
+
+  private Process process;
+  private BufferedReader bufferedReader;
+  private static final Logger LOG = LoggerFactory.getLogger(SearchService.class);
+
+  @Override
+  protected void startUp() throws Exception {
+    System.setProperty("solr.solr.home", "/Users/sagarkapare/work/solr/solr-5.3.1");
+    ProcessBuilder builder = new ProcessBuilder("/Users/sagarkapare/work/solr/solr-5.3.1/bin/solr start");
+    process = builder.start();
+    final InputStream is = process.getInputStream();
+    final InputStreamReader isr = new InputStreamReader(is);
+    bufferedReader = new BufferedReader(isr);
+  }
+
+  @Override
+  protected void run() throws Exception {
+    LOG.info("Solr running ...");
+    try {
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+        LOG.trace(line);
+      }
+    } catch (Exception e) {
+      LOG.error(e.getMessage());
+    }
+  }
+
+  /**
+   * Invoked to request the service to stop.
+   * <p/>
+   * <p>By default this method does nothing.
+   */
+  @Override
+  protected void triggerShutdown() {
+    process.destroy();
+  }
+
+  /**
+   * Stop the service.
+   */
+  @Override
+  protected void shutDown() throws Exception {
+    LOG.info("Shutting down Solr ...");
+    process.waitFor();
+  }
+}
