e953db14a6c92c9e1f93d9350547fcc15e80cc4b
Ajai <ajainarayanan@gmail.com>
2015-10-19 17:20:19 -0700

    Adds tags to all apps, datasets & streams in UI

--- cdap-ui/app/services/metadata/my-metadata-api.js
+++ cdap-ui/app/services/metadata/my-metadata-api.js
@@ -18,7 +18,10 @@ angular.module(PKG.name + '.services')
   .factory('myMetadataApi', function(myCdapUrl, $resource, myAuth, myHelpers) {
 
     var url = myCdapUrl.constructUrl,
-        programPath = '/namespaces/:namespace/apps/:appId/:programType/:programId/metadata';
+        programPath = '/namespaces/:namespace/apps/:appId/:programType/:programId/metadata',
+        datasetsPath = '/namespaces/:namespace/datasets/:datasetId/metadata',
+        streamsPath = '/namespaces/:namespace/streams/:streamId/metadata',
+        appsPath = '/namespaces/:namespace/apps/:appId/metadata';
 
     return $resource(
       url({ _cdapPath: programPath }),
@@ -31,6 +34,19 @@ angular.module(PKG.name + '.services')
     {
       setProgramMetadata: myHelpers.getConfig('POST', 'REQUEST', programPath + '/tags'),
       getProgramMetadata: myHelpers.getConfig('GET', 'REQUEST', programPath + '/tags', true),
-      deleteProgramMetadata: myHelpers.getConfig('DELETE', 'REQUEST', programPath + '/tags/:tag')
+      deleteProgramMetadata: myHelpers.getConfig('DELETE', 'REQUEST', programPath + '/tags/:tag'),
+
+      setAppsMetadata: myHelpers.getConfig('POST', 'REQUEST', appsPath + '/tags'),
+      getAppsMetadata: myHelpers.getConfig('GET', 'REQUEST', appsPath + '/tags', true),
+      deleteAppsMetadata: myHelpers.getConfig('DELETE', 'REQUEST', appsPath + '/tags/:tag'),
+
+      setDatasetsMetadata: myHelpers.getConfig('POST', 'REQUEST', datasetsPath + '/tags'),
+      getDatasetsMetadata: myHelpers.getConfig('GET', 'REQUEST', datasetsPath + '/tags', true),
+      deleteDatasetsMetadata: myHelpers.getConfig('DELETE', 'REQUEST', datasetsPath + '/tags/:tag'),
+
+      setStreamsMetadata: myHelpers.getConfig('POST', 'REQUEST', streamsPath + '/tags'),
+      getStreamsMetadata: myHelpers.getConfig('GET', 'REQUEST', streamsPath + '/tags', true),
+      deleteStreamsMetadata: myHelpers.getConfig('DELETE', 'REQUEST', streamsPath + '/tags/:tag'),
+
     });
   });
