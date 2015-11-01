9f6347b1f1a229e057ee4c15711c44cdc0e4c642
Ajai <ajainarayanan@gmail.com>
2015-10-31 21:55:59 -0700

    License headers

--- cdap-ui/app/features/hydrator/controllers/detail/tabs/datasets-ctrl.js
+++ cdap-ui/app/features/hydrator/controllers/detail/tabs/datasets-ctrl.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2015 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 angular.module(PKG.name + '.feature.hydrator')
   .controller('HydratorDetailDatasetsController', function(DetailRunsStore) {
     this.setDefaults = function() {
@@ -10,6 +26,6 @@ angular.module(PKG.name + '.feature.hydrator')
     this.setState = function() {
       this.state.datasets =  DetailRunsStore.getDatasets().concat(DetailRunsStore.getStreams());
     };
-    
+
     DetailRunsStore.registerOnChangeListener(this.setState.bind(this));
   });
