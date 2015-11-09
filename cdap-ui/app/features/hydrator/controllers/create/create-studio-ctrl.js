093c1cce00b7632c88c672e7038a5643a73d8bac
Edwin Elia <edwinselia@gmail.com>
2015-11-09 13:50:30 -0800

    modify template for the code refactor

--- cdap-ui/app/features/hydrator/controllers/create/create-studio-ctrl.js
+++ cdap-ui/app/features/hydrator/controllers/create/create-studio-ctrl.js
@@ -15,8 +15,11 @@
  */
 
 class HydratorCreateStudioController {
-  constructor(LeftPanelStore, LeftPanelActionsFactory, ConfigActionsFactory, $stateParams, rConfig, ConfigStore, $rootScope) {
+  constructor(LeftPanelStore, LeftPanelActionsFactory, LeftPanelDispatcher,ConfigActionsFactory, $stateParams, rConfig, ConfigStore, $rootScope) {
     // This is required because before we fireup the actions related to the store, the store has to be initialized to register for any events.
+
+    this.LeftPanelActionsFactory = LeftPanelActionsFactory;
+
     ConfigStore.setDefaults();
     if ($stateParams.type) {
       ConfigActionsFactory.setArtifact({
@@ -30,10 +33,20 @@ class HydratorCreateStudioController {
       ConfigActionsFactory.setName(rConfig.name);
       ConfigActionsFactory.setDescription(rConfig.description);
     }
+
+    this.isExpanded = LeftPanelStore.getState();
+    LeftPanelStore.registerOnChangeListener( () => {
+      this.isExpanded = LeftPanelStore.getState();
+    });
+
+  }
+
+  toggleSidebar() {
+    this.LeftPanelActionsFactory.togglePanel();
   }
 }
 
-HydratorCreateStudioController.$inject = ['LeftPanelStore', 'LeftPanelActionsFactory', 'ConfigActionsFactory', '$stateParams', 'rConfig', 'ConfigStore', '$rootScope'];
+HydratorCreateStudioController.$inject = ['LeftPanelStore', 'LeftPanelActionsFactory', 'LeftPanelDispatcher', 'ConfigActionsFactory', '$stateParams', 'rConfig', 'ConfigStore', '$rootScope'];
 angular.module(PKG.name + '.feature.hydrator')
   .controller('HydratorCreateStudioController', HydratorCreateStudioController);
 
