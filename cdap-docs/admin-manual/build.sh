fb997e042340c16231ba67b35e8ff659d1f68681
John Jackson <john@cask.co>
2015-11-19 13:56:50 -0800

    Move the router.service.address into the Router section; fix the MD5 hash; add references to the page generation to aid linking.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -25,7 +25,7 @@ CHECK_INCLUDES=${TRUE}
 
 function download_includes() {
   echo_red_bold "Check guarded files for changes"
-  test_an_include 8053e348dc442c6093c3b9d62ea4dcae "${DEFAULT_XML}"
+  test_an_include aab114b3f60da9900fdabdea0f11217b "${DEFAULT_XML}"
 
   echo "Building rst file from cdap-default.xml..."
   local includes_dir=${1}
