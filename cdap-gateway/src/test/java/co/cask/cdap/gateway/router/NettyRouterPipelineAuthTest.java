7f20d776eab1ba0e1f394d156d8876108827a7b5
anew <andreas@continuuity.com>
2015-12-18 21:35:28 -0800

    address comments

--- cdap-gateway/src/test/java/co/cask/cdap/gateway/router/NettyRouterPipelineAuthTest.java
+++ cdap-gateway/src/test/java/co/cask/cdap/gateway/router/NettyRouterPipelineAuthTest.java
@@ -61,21 +61,21 @@ public class NettyRouterPipelineAuthTest {
   @Test
   public void testRouterAuthBypass() throws Exception {
     // mock token validator passes for no token and any token other than "Bearer failme"
-    testGet(200, "hello"   , "/v1/echo/hello");
-    testGet(200, "hello"   , "/v1/echo/hello", ImmutableMap.of("Authorization", "Bearer x"));
+    testGet(200, "hello", "/v1/echo/hello");
+    testGet(200, "hello", "/v1/echo/hello", ImmutableMap.of("Authorization", "Bearer x"));
     // so this should fail
-    testGet(401, null      , "/v1/echo/hello", ImmutableMap.of("Authorization", "Bearer failme"));
+    testGet(401, null, "/v1/echo/hello", ImmutableMap.of("Authorization", "Bearer failme"));
     // but /v1/echo/dontfail is configured to bypass auth
     testGet(200, "dontfail", "/v1/echo/dontfail", ImmutableMap.of("Authorization", "Bearer failme"));
     // it only bypasses on exact match, not prefix match
-    testGet(401, null      , "/v1/echo/dontfailme", ImmutableMap.of("Authorization", "Bearer failme"));
+    testGet(401, null, "/v1/echo/dontfailme", ImmutableMap.of("Authorization", "Bearer failme"));
 
     // /v1/repeat is configured to bypass auth validation, on prefix match
     testGet(200, "hello", "/v1/repeat/hello");
     testGet(200, "hello", "/v1/repeat/hello", ImmutableMap.of("Authorization", "Bearer x"));
     testGet(200, "hello", "/v1/repeat/hello", ImmutableMap.of("Authorization", "Bearer failme"));
     // even with a token that fails validation, we get the correct status code 404
-    testGet(404, null   , "/v1/repeat/dontfail/me", ImmutableMap.of("Authorization", "Bearer failme"));
+    testGet(404, null, "/v1/repeat/dontfail/me", ImmutableMap.of("Authorization", "Bearer failme"));
   }
 
   private void testGet(int expectedStatus, String expectedResponse, String path) throws Exception {
