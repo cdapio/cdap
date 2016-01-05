be570a3c3a0cc46d39fdab313eba303f90aaa1df
Ajai <ajainarayanan@gmail.com>
2016-01-04 19:41:27 -0800

    Moves all error functions to non-store-pipeline-error-factory + removes pipeline-error-factory

--- cdap-ui/app/features/hydrator/services/non-store-pipeline-error-factory.js
+++ cdap-ui/app/features/hydrator/services/non-store-pipeline-error-factory.js
@@ -25,11 +25,20 @@
   in a way you give it an input it gives back an output. No store dependency.
 */
 
-let countRequiredFields = (node) => {
-  var requiredFieldCount = 0;
-  if (angular.isObject(node._backendProperties) && Object.keys(node._backendProperties).length) {
+/*
+  DAG level validation functions get the input, iterate through the data, validate each data and then call the callback for each data.
+  The validation function doesn't mutate the data. The callback that gets called can decide whether to change it or just use the error message.
 
-    angular.forEach(node._backendProperties, function (value, key) {
+  In this case DAG level validations generally are based on list of nodes hence this pattern. This is not perfect yet but
+  for a list of "data" and possibly more than one type of error calling the callback for each "data" works now. We need to evolve the error handling
+  in hydrator.
+
+*/
+
+let countUnFilledRequiredFields = (node) => {
+  var requiredFieldCount = 0;
+  if (Object.keys(node._backendProperties).length) {
+    angular.forEach(node._backendProperties, (value, key) => {
       if (value.required) {
         if (!node.plugin.properties || !node.plugin.properties[key]) {
           requiredFieldCount++;
@@ -40,31 +49,27 @@ let countRequiredFields = (node) => {
   return requiredFieldCount;
 };
 
-let updatePluginsWithRequiredFieldsCount = (GLOBALS, nodes = [])=> {
-  if (!nodes.length) {
+let isRequiredFieldsFilled = (myHelpers, nodes, cb) => {
+  if (!myHelpers.objectQuery(nodes, 'length')) {
     return;
   }
-  let isRuleValid = true;
   nodes.forEach( node => {
-    node.requiredFieldCount = countRequiredFields(node);
-    if (node.requiredFieldCount > 0) {
-      node.error = {
-        message: GLOBALS.en.hydrator.studio.genericMissingRequiredFieldsError
-      };
-      isRuleValid = false;
+    let unFilledRequiredFieldsCount = countUnFilledRequiredFields(node);
+    let error = 'MISSING-REQUIRED-FIELDS';
+    if (unFilledRequiredFieldsCount > 0) {
+      cb(error, node, unFilledRequiredFieldsCount);
     } else {
-      node.error = false;
+      cb(false, node, unFilledRequiredFieldsCount);
     }
   });
 };
 
-let checkAndUpdateUniqueNodeNames = (nodes = []) => {
-  if (!nodes.length){
-    return;
-  }
-
-  let nodesIdMap = {};
+let isUniqueNodeNames = (myHelpers, nodes, cb) => {
   let isRuleValid = true;
+  if (!myHelpers.objectQuery(nodes, 'length')) {
+    return isRuleValid;
+  }
+  let nodesIdMap = {};
   angular.forEach(nodes, function (node) {
     if (!nodesIdMap[node.plugin.label]) {
       nodesIdMap[node.plugin.label] = [];
@@ -73,26 +78,108 @@ let checkAndUpdateUniqueNodeNames = (nodes = []) => {
   });
   angular.forEach(nodesIdMap, function(nodeArray) {
     if(nodeArray.length > 1) {
-      isRuleValid = false;
       nodeArray.forEach( function(n) {
-        n.error = n.error || {};
-        n.error.message = `Node with name '${n.plugin.label}' already exists`;
-        n.requiredFieldCount = '!';
+        let error = 'DUPLICATE-NAME';
+        cb(error, n);
       });
+    } else {
+      cb(false, nodeArray[0]);
     }
   });
   return isRuleValid;
 };
 
-let NonStorePipelineErrorFactory = (GLOBALS) => {
+let isNodeNameUnique = (myHelpers, nodeName, nodes, cb) => {
+  if (!myHelpers.objectQuery(nodes, 'length') || !nodeName.length) {
+    cb(false);
+    return;
+  }
+  let error;
+  let filteredNames = nodes.filter( node => {
+    return node.plugin.label === nodeName;
+  });
+  if (filteredNames.length > 1) {
+    error = 'DUPLICATE-NAME';
+    cb(error);
+    return;
+  }
+  cb(false);
+};
+
+
+let hasValidName = (name, cb) => {
+  let error;
+  let pattern = /^[\w]+$/;
+  if (!name) {
+    error = 'MISSING-NAME';
+    cb(error);
+    return;
+  }
+  if (!pattern.test(name)) {
+    error = 'INVALID-NAME';
+    cb(error);
+    return;
+  }
+  cb(false);
+};
+
+let hasOnlyOneSource = (myHelpers, GLOBALS, nodes, cb) => {
+  let error;
+  let countSource;
+  if (!myHelpers.objectQuery(nodes, 'length')) {
+    cb(false);
+  }
+  nodes.forEach( node => {
+    if (GLOBALS.pluginConvert[node.type] === 'source') {
+      countSource++;
+    }
+  });
+  if (countSource === 0) {
+    error = 'NO-SOURCE-FOUND';
+    cb(error);
+    return;
+  }
+  if (countSource > 1) {
+    error = 'MORE-THAN-ONE-SOURCE-FOUND';
+    cb(error);
+    return;
+  }
+  cb(false);
+};
+
+let hasAtLeastOneSink = (myHelpers, GLOBALS, nodes, cb) => {
+  let error;
+  let countSink;
+  if (!myHelpers.objectQuery(nodes, 'length')) {
+    cb(false);
+  }
+  nodes.forEach( node => {
+    if (GLOBALS.pluginConvert[node.type] === 'sink') {
+      countSink++;
+    }
+  });
+  if (countSink === 0) {
+    error = 'NO-SINK-FOUND';
+    cb(error);
+    return;
+  }
+  cb(false);
+};
+
+let NonStorePipelineErrorFactory = (GLOBALS, myHelpers) => {
+  // If we had used SystemJs or requirejs this could have been avoided.
   return {
-    checkAndUpdateUniqueNodeNames: checkAndUpdateUniqueNodeNames,
-    updatePluginsWithRequiredFieldsCount: updatePluginsWithRequiredFieldsCount.bind(null, GLOBALS),
-    countRequiredFields: countRequiredFields
+    isUniqueNodeNames: isUniqueNodeNames.bind(null, myHelpers),
+    isRequiredFieldsFilled: isRequiredFieldsFilled.bind(null, myHelpers),
+    countUnFilledRequiredFields,
+    hasValidName,
+    hasOnlyOneSource: hasOnlyOneSource.bind(null, myHelpers, GLOBALS),
+    hasAtLeastOneSink: hasAtLeastOneSink.bind(null, myHelpers, GLOBALS),
+    isNodeNameUnique: isNodeNameUnique.bind(null, myHelpers)
   };
 };
 
-NonStorePipelineErrorFactory.$inject = ['GLOBALS'];
+NonStorePipelineErrorFactory.$inject = ['GLOBALS', 'myHelpers'];
 
 angular.module(`${PKG.name}.feature.hydrator`)
   .factory('NonStorePipelineErrorFactory', NonStorePipelineErrorFactory);
