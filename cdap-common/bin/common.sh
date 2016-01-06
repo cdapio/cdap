36869d617866b22010641ec102855cf1ae1e83f8
Derek Wood <derek@cask.co>
2016-01-06 11:42:21 -0800

    making failure to find spark-env.sh non-fatal as this location is not guaranteed

--- cdap-common/bin/common.sh
+++ cdap-common/bin/common.sh
@@ -270,7 +270,6 @@ cdap_set_spark_env() {
       source "${SPARK_HOME}"/conf/spark-env.sh
     else
       echo "ERROR - Failed to locate ${SPARK_HOME}/conf/spark-env.sh"
-      return 1
     fi
   fi
 }
