11ca6a921f4a4e15a43b1d8befe0712ea7dc3230
Gokul Gunasekaran <gokul@cask.co>
2016-01-07 11:25:03 -0800

    CDAP-4357 Fix the expected count of pending queue events

--- cdap-app-fabric/src/test/java/co/cask/cdap/runtime/FlowTest.java
+++ cdap-app-fabric/src/test/java/co/cask/cdap/runtime/FlowTest.java
@@ -294,14 +294,18 @@ public class FlowTest {
       // source emits 4, then forward-one reads 1, hence 3 should be pending
       waitForPending(tagsForSourceToOne, 3, 5000); // wait a little longer as flow needs to start
       waitForPending(tagsForAllToOne, 3, 100); // wait a little longer as flow needs to start
-      // forward-two receives each of the 4 as a sting and an int, but has read only 1
-      // so there should be 3 + 4 = 7 pending, but we don't know which queue has 3 and which has 4
+      // forward-two receives each of the 4 as a sting and an int, but could have read 1 per each queue
+      // so there should be either 3 + 4 = 7 pending or 3 + 3 = 6 pending,
+      // but we don't know whether the queue pending count will be 4, 3 or 3, 4 or 3, 3
       long intPending = waitForPending(tagsForSourceToTwoInts, 3, 4L, 1000);
-      waitForPending(tagsForSourceToTwoStrings, 7 - intPending, 1000);
-      waitForPending(tagsForSourceToTwo, 7, 100);
-      waitForPending(tagsForAllToTwo, 7, 100);
-      // neither one nor two have emitted, so the total pending should be 9
-      waitForPending(tagsForAll, 10, 100);
+      long stringPending = waitForPending(tagsForSourceToTwoStrings, 3, 4L, 1000);
+      long totalPending = intPending + stringPending;
+      Assert.assertTrue(totalPending == 6 || totalPending == 7);
+      waitForPending(tagsForSourceToTwo, 7, 6L, 100);
+      waitForPending(tagsForAllToTwo, 7, 6L, 100);
+      // neither one nor two have emitted, so the total pending should be = 12 - 1 (forward-one) - 1 or 2 (forward-two)
+      // => 10 or 9 events
+      waitForPending(tagsForAll, 10, 9L, 100);
 
       // kick on forward-one, it should now consume all its events
       Assert.assertTrue(new File(tempFolder, "one").createNewFile());
@@ -324,7 +328,7 @@ public class FlowTest {
       waitForPending(tagsForTwoToSink, 8, 1000);
       waitForPending(tagsForAllToSink, 11, 100);
 
-      // kick off sink, its penidng events should now go to zero
+      // kick off sink, its pending events should now go to zero
       Assert.assertTrue(new File(tempFolder, "three").createNewFile());
       waitForPending(tagsForOneToSink, 0, 2000);
       waitForPending(tagsForTwoToSink, 0, 2000);
