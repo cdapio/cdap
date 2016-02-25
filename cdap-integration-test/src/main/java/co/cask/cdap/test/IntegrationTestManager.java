c033a9a0d830af004f5793a1f74a51cd558297f7
CuriousVini <captainjacksparrow1010@gmail.com>
2016-02-25 14:59:06 -0800

    Adding getApplicationManager() method to IntegrationTestBase

--- cdap-integration-test/src/main/java/co/cask/cdap/test/IntegrationTestManager.java
+++ cdap-integration-test/src/main/java/co/cask/cdap/test/IntegrationTestManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Cask Data, Inc.
+ * Copyright © 2015-2016 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -167,6 +167,11 @@ public class IntegrationTestManager implements TestManager {
   public ApplicationManager deployApplication(Id.Application appId,
                                               AppRequest appRequest) throws Exception {
     applicationClient.deploy(appId, appRequest);
+    return new RemoteApplicationManager(appId, clientConfig, restClient);
+  }
+
+  @Override
+  public ApplicationManager getApplicationManager(Id.Application appId) {
     return new RemoteApplicationManager(appId, clientConfig, restClient);
   }
 
