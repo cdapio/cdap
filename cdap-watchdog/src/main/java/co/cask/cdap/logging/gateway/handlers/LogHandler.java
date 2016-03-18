3113e8345f8014d201126972e5938e02b0fbd4a6
CuriousVini <captainjacksparrow1010@gmail.com>
2016-03-18 03:25:02 -0700

    Addressing comments

--- cdap-watchdog/src/main/java/co/cask/cdap/logging/gateway/handlers/LogHandler.java
+++ cdap-watchdog/src/main/java/co/cask/cdap/logging/gateway/handlers/LogHandler.java
@@ -344,7 +344,7 @@ public class LogHandler extends AbstractHttpHandler {
   }
 
   private static TimeRange parseTime(long fromTimeSecsParam, long toTimeSecsParam, HttpResponder responder) {
-    long currentTimeMillis =  System.currentTimeMillis();
+    long currentTimeMillis = System.currentTimeMillis();
     long fromMillis = fromTimeSecsParam < 0 ?
       currentTimeMillis - TimeUnit.HOURS.toMillis(1) : TimeUnit.SECONDS.toMillis(fromTimeSecsParam);
     long toMillis = toTimeSecsParam < 0 ? currentTimeMillis : TimeUnit.SECONDS.toMillis(toTimeSecsParam);
