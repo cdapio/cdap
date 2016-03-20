62b896a4db4d891b7ebd939842f308f91d6b6b6a
Ajai <ajainarayanan@gmail.com>
2016-03-19 17:59:10 -0700

    Adds a success message popup once a plugin template has been added

--- cdap-ui/app/features/hydrator-beta/controllers/create/popovers/plugin-templates-create-edit-ctrl.js
+++ cdap-ui/app/features/hydrator-beta/controllers/create/popovers/plugin-templates-create-edit-ctrl.js
@@ -1,5 +1,5 @@
 angular.module(`${PKG.name}.feature.hydrator-beta`)
-  .controller('PluginTemplatesCreateEditCtrl', function ($scope, PluginTemplateStoreBeta, PluginTemplateActionBeta, PluginActionsFactoryBeta, $stateParams) {
+  .controller('PluginTemplatesCreateEditCtrl', function ($scope, PluginTemplateStoreBeta, PluginTemplateActionBeta, PluginActionsFactoryBeta, $stateParams, myAlertOnValium) {
       $scope.closeTemplateCreationModal = ()=> {
         PluginTemplateActionBeta.reset();
         $scope.$close();
@@ -13,6 +13,10 @@ angular.module(`${PKG.name}.feature.hydrator-beta`)
         if (getIsSaveSuccessfull) {
           PluginTemplateActionBeta.reset();
           PluginActionsFactoryBeta.fetchTemplates({namespace: $stateParams.namespace});
+          myAlertOnValium.show({
+            type: 'success',
+            content: 'Plugin template creation successfull'
+          });
           $scope.$close();
         }
       });
