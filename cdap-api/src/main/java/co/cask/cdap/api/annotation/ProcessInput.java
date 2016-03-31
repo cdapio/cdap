76fd6a3648400781741ceb47fecb2bce3665f3e5
John Jackson <john@cask.co>
2016-03-30 18:14:17 -0700

    Corrected URLs.

--- cdap-api/src/main/java/co/cask/cdap/api/annotation/ProcessInput.java
+++ cdap-api/src/main/java/co/cask/cdap/api/annotation/ProcessInput.java
@@ -40,8 +40,8 @@ import java.lang.annotation.Target;
  * </pre>
  *
  * <p>
- * See the <i><a href="http://cask.co/docs/cdap/current/en/developers-manual/index.html">CDAP Developers' Manual</a></i>
- * and the <i><a href="http://cask.co/docs/cdap/current/en/examples-manual/index.html">CDAP examples</a></i>
+ * See the <i><a href="http://docs.cask.co/cdap/current/en/developers-manual/index.html">CDAP Developers' Manual</a></i>
+ * and the <i><a href="http://docs.cask.co/cdap/current/en/examples-manual/index.html">CDAP examples</a></i>
  * for more information.
  * </p>
  *
