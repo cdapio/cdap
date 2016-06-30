cf7633ad2fedfcc717946f00bfc566e9fb7ff19e
John Jackson <john@cask.co>
2016-06-29 18:00:03 -0700

    Remove authentication.

--- cdap-docs/_common/_themes/cdap/static/cdap-annotator.js
+++ cdap-docs/_common/_themes/cdap/static/cdap-annotator.js
@@ -8,13 +8,9 @@
 
 $(document).ready(function() {
   var content = $('#documentwrapper').annotator();
-  content.annotator('setupPlugins', {}, {
-    Store: false
-  });
   content.annotator('addPlugin', 'Store', {
     // The endpoint of the store on your server.
     prefix: 'http://annotationstorage10595-1000.dev.continuuity.net:5000',
   });
-
 });
 
