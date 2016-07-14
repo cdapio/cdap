2d414d3086f2ef5e2850a9b32fd7c46a4aaef4dc
Ajai <ajainarayanan@gmail.com>
2016-07-14 10:39:01 -0700

    Fixes pipeline non detail runs store & console stores to have listen & unsubscribe functions

--- cdap-ui/app/features/hydratorplusplus/services/detail/stores/pipeline-detail-nonruns-store.js
+++ cdap-ui/app/features/hydratorplusplus/services/detail/stores/pipeline-detail-nonruns-store.js
@@ -41,7 +41,11 @@ angular.module(PKG.name + '.feature.hydratorplusplus')
     };
 
     this.registerOnChangeListener = function(callback) {
-      this.changeListeners.push(callback);
+      let index = this.changeListeners.push(callback);
+      // un-subscribe for listners.
+      return () => {
+        this.changeListeners.splice(index-1, 1);
+      };
     };
     this.emitChange = function() {
       this.changeListeners.forEach(function(callback) {
