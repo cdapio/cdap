3a220bfa8443f35761c02547bbbe9e0c098e4c41
John Jackson <john@cask.co>
2016-07-22 10:19:54 -0700

    Add resource guarantees to the cdap-default.xml file. (#6125)
    Fix some capitalization inconsistencies.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -19,7 +19,7 @@
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="c3223f49bd5cbc2201509da9565dd50d"
+DEFAULT_XML_MD5_HASH="f7a825d42551f16dba208602c52017a4"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
