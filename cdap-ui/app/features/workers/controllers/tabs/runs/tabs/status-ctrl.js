a689ef23f1006b5a07c8e011917e0c144d2a920d
Edwin Elia <edwinselia@gmail.com>
2016-07-25 15:04:25 -0700

    fix status controller in workers doing unnecessary check

--- cdap-ui/app/features/workers/controllers/tabs/runs/tabs/status-ctrl.js
+++ cdap-ui/app/features/workers/controllers/tabs/runs/tabs/status-ctrl.js
@@ -15,16 +15,7 @@
  */
 
 angular.module(PKG.name + '.feature.worker')
-  .controller('WorkersRunsDetailStatusController', function($state, $scope, MyCDAPDataSource, $filter) {
-    var filterFilter = $filter('filter');
-
-    if ($state.params.runid) {
-      var match = filterFilter($scope.runs, {runid: $state.params.runid});
-      if (match.length) {
-        $scope.runs.selected = match[0];
-      }
-    }
-
+  .controller('WorkersRunsDetailStatusController', function($state) {
     this.basePath = '/namespaces/' + $state.params.namespace +
       '/apps/' + $state.params.appId +
       '/workers/' + $state.params.programId;
