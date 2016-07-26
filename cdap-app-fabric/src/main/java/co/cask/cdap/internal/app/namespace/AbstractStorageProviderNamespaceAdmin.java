88b16e37e0dd723d81e7a8502a6dbe14485ff343
Rohit Sinha <rsinha@cask.co>
2016-07-26 14:17:35 -0700

    Address comments

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/namespace/AbstractStorageProviderNamespaceAdmin.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/namespace/AbstractStorageProviderNamespaceAdmin.java
@@ -134,19 +134,19 @@ abstract class AbstractStorageProviderNamespaceAdmin implements StorageProviderN
       // namespaceLocationFactory since the location needs to be aware of local/distributed fs.
       Location customNamespacedLocation = namespacedLocationFactory.get(namespaceMeta.getNamespaceId().toId());
       if (!customNamespacedLocation.exists()) {
-        // TODO: Add username in the below exception message.
         throw new IOException(String.format(
           "The provided home directory '%s' for namespace '%s' does not exists. Please create it on filesystem " +
-            "with sufficient privileges for the user and then try creating a namespace.",
-          customNamespacedLocation.toString(), namespaceMeta.getNamespaceId()));
+            "with sufficient privileges for the user %s and then try creating a namespace.",
+          customNamespacedLocation.toString(), namespaceMeta.getNamespaceId(),
+          namespaceMeta.getConfig().getPrincipal()));
       }
       // we also expect it to empty since non-empty directories can lead to various inconsistencies CDAP-6743
       if (customLocation.list().length != 0) {
-        // TODO: Add username in the below exception message.
         throw new IOException(String.format(
-          "The provided home directory '%s' for namespace '%s' is not empty. Namespace can only be mapped to an " +
-            "empty directory. Please try creating the namespace again with an empty directory mapping.",
-          customNamespacedLocation.toString(), namespaceMeta.getNamespaceId()));
+          "The provided home directory '%s' for namespace '%s' is not empty. Please try creating the namespace " +
+            "again with an empty directory mapping and sufficient privileges for the user %s.",
+          customNamespacedLocation.toString(), namespaceMeta.getNamespaceId(),
+          namespaceMeta.getConfig().getPrincipal()));
       }
     } else {
       // no namespace custom location was provided one must be created by cdap
