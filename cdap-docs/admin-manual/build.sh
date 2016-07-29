8acb5cb00b61b8e350f756906ac78f19fbfbef9e
Bhooshan Mogal <bhooshan@cask.co>
2016-07-29 00:18:49 -0700

    Fix the default for security.store.provider to be kms, since file causes guice failures in master startup. Add file as the default for standalone. Update md5 hash

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -19,7 +19,7 @@
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="3d22866379b41f7a4b54de78b5d1edf5"
+DEFAULT_XML_MD5_HASH="da31a1100b62cba191e61132bb667a0d"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
