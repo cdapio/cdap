e292183e49b6b13a5abe00b0022cc76f9027e84e
Ajai <ajainarayanan@gmail.com>
2016-08-01 20:17:55 -0700

    Fixes metadata top always open up with focusing on name textbox in hydrator studio view

--- cdap-ui/app/features/hydratorplusplus/controllers/create/toppanel-ctrl.js
+++ cdap-ui/app/features/hydratorplusplus/controllers/create/toppanel-ctrl.js
@@ -87,10 +87,6 @@ class HydratorPlusPlusTopPanelCtrl{
     this.invalidName = false;
 
     this.$timeout.cancel(this.focusTimeout);
-    if (!this.$stateParams.isClone) {
-      return;
-    }
-    this.$stateParams.isClone = null;
     this.focusTimeout = this.$timeout(() => {
       document.getElementById('pipeline-name-input').focus();
     });
