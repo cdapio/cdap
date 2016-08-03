6721034f39897315e54a0da7cdca3c706400457e
Terence Yim <terence@cask.co>
2016-08-02 21:46:23 -0700

    (CDAP-6889) Fix race condition in AppFabricTestHelper for namespace creation

--- cdap-app-fabric/src/test/java/co/cask/cdap/internal/AppFabricTestHelper.java
+++ cdap-app-fabric/src/test/java/co/cask/cdap/internal/AppFabricTestHelper.java
@@ -145,10 +145,17 @@ public class AppFabricTestHelper {
     ensureNamespaceExists(namespace, CConfiguration.create());
   }
 
-  public static void ensureNamespaceExists(Id.Namespace namespace, CConfiguration cConf) throws Exception {
+  private static void ensureNamespaceExists(Id.Namespace namespace, CConfiguration cConf) throws Exception {
     NamespaceAdmin namespaceAdmin = getInjector(cConf).getInstance(NamespaceAdmin.class);
-    if (!namespaceAdmin.exists(namespace)) {
-      namespaceAdmin.create(new NamespaceMeta.Builder().setName(namespace).build());
+    try {
+      if (!namespaceAdmin.exists(namespace)) {
+        namespaceAdmin.create(new NamespaceMeta.Builder().setName(namespace).build());
+      }
+    } catch (NamespaceAlreadyExistsException e) {
+      // There can be race between exists() and create() call.
+      if (!namespaceAdmin.exists(namespace)) {
+        throw new IllegalStateException("Failed to create namespace " + namespace, e);
+      }
     }
   }
 
