625c3ae1a300cbe96c3d823b83bc1f455ca7a6b7
Ajai <ajainarayanan@gmail.com>
2016-08-04 15:16:52 -0700

    Adds pipeline-resource factory to render pipeline resources (both driver and executor)

---
+++ cdap-ui/app/directives/my-pipeline-resource/my-pipeline-driver-resource-ctrl.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+function MyPipelineDriverResourceCtrl($scope) {
+  'ngInject';
+  $scope.virtualCores = $scope.store.getDriverVirtualCores();
+  $scope.memoryMb = $scope.store.getDriverMemoryMb();
+  $scope.cores = Array.apply(null, {length: 21}).map(Number.call, Number).filter(a => a > 0);
+
+  $scope.onMemoryMbChange = function() {
+    $scope.actionCreator.setDriverMemoryMb($scope.memoryMb);
+  };
+  $scope.onVirtualCoresChange = function() {
+    $scope.actionCreator.setDriverVirtualCores($scope.virtualCores);
+  };
+}
+angular.module(PKG.name + '.commons')
+  .controller('MyPipelineDriverResourceCtrl', MyPipelineDriverResourceCtrl);
