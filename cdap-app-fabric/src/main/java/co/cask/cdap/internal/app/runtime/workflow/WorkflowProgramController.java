f702dfca6d23179c40b815e5aa3a94bb4f67f95b
sagarkapare <sagar@cask.co>
2016-08-12 10:10:20 -0700

    CDAP-6008,CDAP-4647 Mark Workflow as failed if action executed by the Workflow fails.

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/workflow/WorkflowProgramController.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/workflow/WorkflowProgramController.java
@@ -81,10 +81,13 @@ final class WorkflowProgramController extends AbstractProgramController {
 
       @Override
       public void terminated(Service.State from) {
-        LOG.info("Workflow service terminated from {}. Un-registering service {}.", from, serviceName);
-        cancelAnnounce.cancel();
+        LOG.info("Workflow service terminated from {}. Un-registering service {}. getState: {}", from, serviceName,
+                 getState());
+        if (cancelAnnounce != null) {
+          cancelAnnounce.cancel();
+        }
         LOG.info("Service {} unregistered.", serviceName);
-        if (getState() != State.STOPPING) {
+        if (from != Service.State.STOPPING) {
           // service completed itself.
           complete();
         } else {
