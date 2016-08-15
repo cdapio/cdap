a777a271a1141d7549b48da7be96243cdd0190a5
Andrew Westlund <astewes@gmail.com>
2016-08-15 16:33:50 -0700

    uses globals for context; replaces hyphen with colon

--- cdap-ui/app/features/hydratorplusplus/controllers/create/popovers/pre-configured-ctrl.js
+++ cdap-ui/app/features/hydratorplusplus/controllers/create/popovers/pre-configured-ctrl.js
@@ -28,21 +28,7 @@ class HydratorPlusPlusPreConfiguredCtrl {
     this.myAlertOnValium = myAlertOnValium;
 
     this.typeFilter = rTemplateType;
-
-    switch(this.typeFilter) {
-      case 'cdap-data-pipeline':
-        this.templateContext = 'Data Pipeline';
-        break;
-      case 'cdap-data-streams':
-        this.templateContext = 'Data Pipeline';
-        break;
-      case 'cdap-etl-batch':
-        this.templateContext = 'ETL Batch';
-        break;
-      case 'cdap-etl-realtime':
-        this.templateContext = 'ETL Realtime';
-        break;
-    }
+    this.templateContext = this.GLOBALS.artifactConvert[rTemplateType];
 
     this.fetchTemplates().then((plugins) => {
       this.templates = plugins;
