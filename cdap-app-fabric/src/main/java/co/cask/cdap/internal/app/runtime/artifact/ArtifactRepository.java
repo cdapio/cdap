312eb4e25bf8202eb1c026cbdd27b97db411bbca
Ali Anwar <anwar1@berkeley.edu>
2016-08-17 13:26:41 -0700

    Revert "CDAP-6912 Instantiate application and configure it under impersonation"
    This reverts commit 1e2ee4b07f51320b81778218cefa3d1f80853c2f.

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/artifact/ArtifactRepository.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/artifact/ArtifactRepository.java
@@ -104,7 +104,7 @@ public class ArtifactRepository {
                             AuthenticationContext authenticationContext) {
     this.artifactStore = artifactStore;
     this.artifactClassLoaderFactory = new ArtifactClassLoaderFactory(cConf, programRunnerFactory);
-    this.artifactInspector = new ArtifactInspector(cConf, artifactClassLoaderFactory, impersonator);
+    this.artifactInspector = new ArtifactInspector(cConf, artifactClassLoaderFactory);
     this.systemArtifactDirs = new ArrayList<>();
     for (String dir : cConf.get(Constants.AppFabric.SYSTEM_ARTIFACTS_DIR).split(";")) {
       File file = new File(dir);
