c21abbb65e4d89718aa7842a7b9d037a41c22668
John Jackson <john@cask.co>
2016-08-21 20:32:04 -0700

    Correct download filename.

--- cdap-docs/hydrator-manual/build.sh
+++ cdap-docs/hydrator-manual/build.sh
@@ -233,7 +233,7 @@ function download_includes() {
   download_md_file core-plugins File-batchsource.md
   download_md_file core-plugins FTP-batchsource.md
   download_md_file core-plugins GroupByAggregate-batchaggregator.md
-  download_md_file core-plugins HDFSFile-action.md
+  download_md_file core-plugins HDFSMove-action.md
   download_md_file core-plugins JavaScript-transform.md
   download_md_file core-plugins JMS-realtimesource.md
   download_md_file core-plugins Joiner-batchjoiner.md
