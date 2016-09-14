078895f49c54ce349085074f3e8baf23c0c5c813
Chris Gianelloni <chris@cask.co>
2016-09-14 15:20:14 -0400

    Use latest released cdap-ambari-service version (#157)

--- attributes/ambari.rb
+++ attributes/ambari.rb
@@ -16,5 +16,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['cdap']['ambari']['version'] = '3.4.1-1'
+default['cdap']['ambari']['version'] = '3.5.2-1'
 default['cdap']['ambari']['install'] = false
