d97e762886a7fb2d8720b4d002ff98d9abc8d517
Ajai <ajainarayanan@gmail.com>
2016-09-13 22:19:47 -0700

    Adds initial implementation of Wizards in CDAP
    Adds following components,
    
    - Wizard - A wrapper that connects everything that we call as wizard
    - WizardModal - A wrapper for dropping in the modal anywhere we want. A generic re-usualble way to show modal(wizards) anywhere in a more consistent way.
    - ResourceCenter - Nothing fancy as of now. Just shows a dummy list of entities that we can create in CDAP
    - ResourceCenter Entity - A short info on the entity that is being created + a create link (to be used in the resource center)
    - TimeToLive - A naive implentation of setting ttl for streams in UI (Need some inputs)
    - SelectWithOptions - One element to render a dropdown instead of nesting <Input> with <options> like the one from reactstrap. This is specifically useful when we use connect from react-redux
    - SimpleSchema - A naive implementation of a simple schema editor. Nothing fancy. A table of textbox, dropdown, checkbox & a delete button (just like the way we have in hydrator).
    
    Adds following services,
    
    - WizardConfigs - One place to find all WizardConfigurations that are being used in CDAP
    - WizardStores - Once place to find all stores used by each Wizard instances.
    - Have added aliases to webpack to refer to any component/service from anywhere in the application (could avoid using relative paths with a lot of ../.)
    
    - Have added an optional field in ui-settings.json to enable debugging in UI (console log in the browser).
    - Have created aliases for components & services in webpack.config.js to refer to any component or service from anywhere
      without relative URLs

---
+++ cdap-ui/app/cdap/components/SimpleSchema/SchemaStore.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+import {combineReducers, createStore} from 'redux';
+import SchemaStoreActions from './SchemaStoreActions';
+const defaultRow = {
+ name: '',
+ type: '',
+ isNullable: false
+};
+const defaultSchema = {
+ "type":"record",
+ "name":"etlSchemaBody",
+ "fields":[defaultRow]
+};
+const defaultAction = {
+ type: '',
+ payload: {}
+};
+const schema = (state = defaultSchema, action = defaultAction) => {
+ let stateCopy;
+ switch(action.type) {
+   case SchemaStoreActions.setFieldName:
+     stateCopy = Object.assign({}, state);
+     if (action.payload.keyCode === 13) {
+       stateCopy.fields.push({
+         name: '',
+         type: '',
+         isNullable: false
+       });
+       return stateCopy;
+     }
+     stateCopy = Object.assign({}, state);
+     if (action.payload.name === null || typeof action.payload.name === 'undefined') {
+       return stateCopy;
+     }
+     stateCopy.fields[action.payload.index].name = action.payload.name;
+     return stateCopy;
+   case SchemaStoreActions.setFieldType:
+     stateCopy = Object.assign({}, state);
+     stateCopy.fields[action.payload.index].type = action.payload.type;
+     return stateCopy;
+   case SchemaStoreActions.setFieldIsNullable:
+     stateCopy = Object.assign({}, state);
+     stateCopy.fields[action.payload.index].isNullable = action.payload.isNullable;
+     return stateCopy;
+   case SchemaStoreActions.removeField:
+      if (action.payload.keyCode === 13) {
+         return state;
+      }
+     stateCopy = Object.assign({}, state);
+     stateCopy.fields.splice(action.payload.index, 1);
+     if (!stateCopy.fields.length) {
+        stateCopy.fields.push({
+         name: '',
+         type: '',
+         isNullable: false
+       });
+     }
+     return stateCopy;
+   default:
+     return state;
+ }
+};
+
+const createSchemaStore = (initialState = defaultSchema) => {
+  return createStore(
+    combineReducers({schema}),
+    initialState
+  );
+};
+
+export { createSchemaStore };
