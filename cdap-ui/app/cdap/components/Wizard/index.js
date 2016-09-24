a9c130c62a106a2ec559ec1d6b56df761ff95a27
Edwin Elia <edwinselia@gmail.com>
2016-09-23 18:26:35 -0700

    update styling confirmation overlay for wizard

--- cdap-ui/app/cdap/components/Wizard/index.js
+++ cdap-ui/app/cdap/components/Wizard/index.js
@@ -154,14 +154,7 @@ export default class Wizard extends Component{
           </span>
         );
       }
-      // if (canFinish(matchedStep.id, this.props.wizardConfig)) {
-      //   return (
-      //     <div>
-      //       {navButtons}
-      //       {finishButton}
-      //     </div>
-      //   );
-      // }
+
       return navButtons;
     }.bind(this);
     const isStepComplete = function isStepComplete(stepId) {
@@ -186,20 +179,20 @@ export default class Wizard extends Component{
                 .map((value) => {
                   return (
                     <li key={shortid.generate()}>
-                      Step - {value.index} : {value.label} - {value.complete ? doneLabel : skipLabel}
+                      Step {value.index + 1} : {value.label} - {value.complete ? doneLabel : skipLabel}
                     </li>
                   );
                 })
             }
-            <div className="clearfix">
-              <div
-                className="btn btn-primary pull-right"
-                onClick={this.props.onClose.bind(null, true)}
-              >
-                Close
-              </div>
-            </div>
           </ul>
+          <div className="clearfix text-center done-button">
+            <div
+              className="btn btn-primary"
+              onClick={this.props.onClose.bind(null, true)}
+            >
+              Done
+            </div>
+          </div>
         </div>
       );
     }.bind(this);
