94b1f177030afd7ab22b33e009ec8a64f05b3371
John Jackson <john@cask.co>
2016-10-11 10:17:51 -0700

    Fix typo and MD5 hash

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="788b8faed1ed42f8d0a4e5768c806864"
+DEFAULT_XML_MD5_HASH="b38a2ab7f345e193599a6689bbd943e5"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
