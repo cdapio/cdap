e7e3407f47392500d5fbe8fd529db71a2a423514
Patrick Guimarais <pdc269@nyu.edu>
2016-10-28 18:07:02 -0700

    uses keycode

--- cdap-ui/app/cdap/components/KeyValuePairs/KeyValuePair.js
+++ cdap-ui/app/cdap/components/KeyValuePairs/KeyValuePair.js
@@ -21,18 +21,18 @@ class KeyValuePair extends Component {
 
   constructor(props){
     super(props);
-    this.keyPress = this.keyPress.bind(this);
+    this.keyDown = this.keyDown.bind(this);
   }
-  keyPress(e) {
-    if(e.key === "Enter"){
+  keyDown(e) {
+    if(e.keyCode === 13){
       this.props.addRow();
     }
   }
   render() {
     return (
       <div className="key-value-pair-preference">
-        <input type="text" value={this.props.name} autoFocus={true} onKeyPress={this.keyPress} onChange={this.props.onChange.bind(null, 'key')} placeholder={T.translate('commons.keyValPairs.keyPlaceholder')} className="key-input" />
-        <input type="text" value={this.props.value} onKeyPress={this.keyPress} onChange={this.props.onChange.bind(null, 'value')} placeholder={T.translate('commons.keyValPairs.valuePlaceholder')} className="value-input" />
+        <input type="text" value={this.props.name} autoFocus={true} onKeyDown={this.keyDown} onChange={this.props.onChange.bind(null, 'key')} placeholder={T.translate('commons.keyValPairs.keyPlaceholder')} className="key-input" />
+        <input type="text" value={this.props.value} onKeyDown={this.keyDown} onChange={this.props.onChange.bind(null, 'value')} placeholder={T.translate('commons.keyValPairs.valuePlaceholder')} className="value-input" />
         <button type="submit" className="fa fa-plus add-row-btn" onClick={this.props.addRow} />
         <button type="submit" className="fa fa-trash remove-row-btn" onClick={this.props.removeRow} />
       </div>
