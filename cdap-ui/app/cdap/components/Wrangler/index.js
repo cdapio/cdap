478c839d0677a59d70793c73149367e088fd7b94
Edwin Elia <edwinselia@gmail.com>
2016-11-03 17:10:21 -0700

    show wrangled data on table

--- cdap-ui/app/cdap/components/Wrangler/index.js
+++ cdap-ui/app/cdap/components/Wrangler/index.js
@@ -16,12 +16,95 @@
 
 import React, { Component } from 'react';
 import Papa from 'papaparse';
+import shortid from 'shortid';
 
 require('./Wrangler.less');
 
 export default class Wrangler extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      header: false,
+      dynamicTyping: false,
+      skipEmptyLines: false,
+      data: []
+    };
+  }
+
+  wrangle() {
+    let input = this.wranglerInput.value;
+
+    let papaConfig = {
+      header: this.state.header,
+      dynamicTyping: this.state.dynamicTyping,
+      skipEmptyLines: this.state.skipEmptyLines,
+      complete: this.handleData.bind(this)
+    };
+
+    Papa.parse(input, papaConfig);
+  }
+
+  handleData(papa) {
+    let formattedData;
+    if (Array.isArray(papa.data[0])) {
+      formattedData = papa.data.map((row) => {
+        let obj = {};
+
+        row.forEach((col, index) => {
+          let key = `column${index+1}`;
+          obj[key] = col;
+        });
+
+        return obj;
+      });
+    } else {
+      formattedData = papa.data;
+    }
+
+    this.setState({
+      data: formattedData
+    });
+  }
+
+  handleSetHeaders() {
+    this.setState({header: !this.state.header});
+  }
+  handleSetDynamicTyping() {
+    this.setState({dynamicTyping: !this.state.dynamicTyping});
+  }
+  handleSetSkipEmptyLines() {
+    this.setState({skipEmptyLines: !this.state.skipEmptyLines});
+  }
+
+  renderResultTable() {
+    if (this.state.data.length === 0) { return null; }
+
+    const data = this.state.data;
+
+    let headers = Object.keys(data[0]);
+
+    return (
+      <div className="wrangler-data">
+        <table className="table">
+          <thead>
+            <tr>
+              { headers.map((head) => <th key={head}>{head}</th>) }
+            </tr>
+          </thead>
+
+          <tbody>
+            { data.map((row) => {
+              return (
+                <tr key={shortid.generate()}>
+                  { headers.map((head) => <td key={shortid.generate()}>{row[head]}</td>) }
+                </tr>
+              );
+            }) }
+          </tbody>
+        </table>
+      </div>
+    );
   }
 
   render() {
@@ -37,7 +120,10 @@ export default class Wrangler extends Component {
           </div>
           <div className="col-xs-6">
             <h3>Copy Input Text</h3>
-            <textarea className="form-control"></textarea>
+            <textarea
+              className="form-control"
+              ref={(ref) => this.wranglerInput = ref}
+            />
           </div>
         </div>
 
@@ -54,21 +140,28 @@ export default class Wrangler extends Component {
             <div className="checkbox">
               {/* header */}
               <label>
-                <input type="checkbox"/> First line as column name?
+                <input type="checkbox"
+                  onChange={this.handleSetHeaders.bind(this)}
+                  checked={this.state.headers}
+                /> First line as column name?
               </label>
             </div>
 
             <div className="checkbox">
               {/* skipEmptyLines */}
               <label>
-                <input type="checkbox"/> Skip empty lines?
+                <input type="checkbox"
+                  onChange={this.handleSetSkipEmptyLines.bind(this)}
+                /> Skip empty lines?
               </label>
             </div>
 
             <div className="checkbox">
               {/* dynamicTyping */}
               <label>
-                <input type="checkbox"/> Attempt to convert number and booleans?
+                <input type="checkbox"
+                  onChange={this.handleSetDynamicTyping.bind(this)}
+                /> Attempt to convert number and booleans?
               </label>
             </div>
           </form>
@@ -76,8 +169,18 @@ export default class Wrangler extends Component {
 
         <br/>
         <div className="text-center">
-          <button className="btn btn-primary">Wrangle</button>
+          <button
+            className="btn btn-primary"
+            onClick={this.wrangle.bind(this)}
+          >
+            Wrangle
+          </button>
         </div>
+
+        <br/>
+
+        {this.renderResultTable()}
+
       </div>
     );
   }
