efa4fe2ce0aa82ad5db0141f14e3e7af8e5ff0c7
Ajai <ajainarayanan@gmail.com>
2016-11-06 01:13:34 -0800

    Adds Helpers module to have ObjectQuery function

---
+++ cdap-ui/app/cdap/services/helpers.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import isObject from 'lodash/isObject';
+
+/*
+  Purpose: Query a json object or an array of json objects
+  Return: Returns undefined if property is not defined(never set) and
+          and a valid value (including null) if defined.
+  Usage:
+    var obj1 = [
+      {
+        p1: 'something',
+        p2: {
+          p21: 'angular',
+          p22: 21,
+          p23: {
+            p231: 'ember',
+            p232: null
+          }
+        },
+        p3: 1296,
+        p4: [1, 2, 3],
+        p5: null
+      },
+      {
+        p101: 'somethingelse'
+      }
+    ]
+    1. query(obj1, 0, 'p1') => 'something'
+    2. query(obj1, 0, 'p2', 'p22') => 21
+    3. query(obj1, 0, 'p2', 'p32') => { p231: 'ember'}
+    4. query(obj1, 0, 'notaproperty') => undefined
+    5. query(obj1, 0, 'p2', 'p32', 'somethingelse') => undefined
+    6. query(obj1, 1, 'p2', 'p32') => undefined
+    7. query(obj1, 0, 'p2', 'p23', 'p232') => null
+    8. query(obj1, 0, 'p5') => null
+ */
+
+function objectQuery(obj) {
+  if (!isObject(obj)) {
+    return null;
+  }
+  for (var i = 1; i < arguments.length; i++) {
+    if (!isObject(obj)) {
+      return undefined;
+    }
+    obj = obj[arguments[i]];
+  }
+  return obj;
+}
+
+export {
+  objectQuery
+};
