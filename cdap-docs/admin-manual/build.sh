cdd1450676fb751632d5b0b0e90f991ef114f4b4
John Jackson <john@cask.co>
2016-11-15 10:36:35 -0800

    Fix hashes. Adjust text of property description to match style.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="d3ef6e62a6f3bc46fb2052b49d606874"
+DEFAULT_XML_MD5_HASH="638883c2cdb8576c0a30a1b6a5c6bedd"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
