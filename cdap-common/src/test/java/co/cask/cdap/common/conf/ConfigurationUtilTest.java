aee6702374133801c97eef72593cbfaa8bef9810
Ali Anwar <anwar1@berkeley.edu>
2016-11-16 23:41:28 -0800

    test case wip

--- cdap-common/src/test/java/co/cask/cdap/common/conf/ConfigurationUtilTest.java
+++ cdap-common/src/test/java/co/cask/cdap/common/conf/ConfigurationUtilTest.java
@@ -16,10 +16,15 @@
 
 package co.cask.cdap.common.conf;
 
+import co.cask.cdap.common.io.Codec;
 import com.google.common.collect.ImmutableMap;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.conf.Configuration;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Map;
 
@@ -27,6 +32,54 @@ import java.util.Map;
  * Tests for {@link ConfigurationUtil}.
  */
 public class ConfigurationUtilTest {
+
+  @Test
+  public void testMultipleMacros() throws IOException {
+    Configuration conf = new Configuration();
+    Configuration confToEncode = new Configuration();
+
+    confToEncode.set("my.key", "my.value");
+    for (int i = 0; i < 30; i++) {
+      confToEncode.set("key.num." + i, "${my.key}");
+    }
+    ConfigurationUtil.set(conf, "test.key", HConfCodec.INSTANCE, confToEncode);
+//    conf.set("my.key", "foo");
+    Configuration decodedConf = ConfigurationUtil.get(conf, "test.key", HConfCodec.INSTANCE);
+    int i = 2;
+  }
+
+  /**
+   * Codec to encode/decode HBase Configuration object.
+   */
+  public static class HConfCodec implements Codec<org.apache.hadoop.conf.Configuration> {
+    public static final HConfCodec INSTANCE = new HConfCodec();
+
+    private HConfCodec() {
+      // Use the static INSTANCE to get an instance.
+    }
+
+    @Override
+    public byte[] encode(org.apache.hadoop.conf.Configuration object) throws IOException {
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      object.writeXml(bos);
+      bos.close();
+      return bos.toByteArray();
+    }
+
+    @Override
+    public org.apache.hadoop.conf.Configuration decode(byte[] data) throws IOException {
+      if (data == null) {
+        return new Configuration();
+//        return HBaseConfiguration.create();
+      }
+
+      ByteArrayInputStream bin = new ByteArrayInputStream(data);
+      org.apache.hadoop.conf.Configuration hConfiguration = new org.apache.hadoop.conf.Configuration();
+      hConfiguration.addResource(bin);
+      return hConfiguration;
+    }
+  }
+
 
   @Test
   public void testNamedConfigurations() throws IOException {
