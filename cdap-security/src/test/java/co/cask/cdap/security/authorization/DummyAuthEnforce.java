ccfaa36231169e67ed3687d18f81b9b889afd9d1
Rohit Sinha <rsinha@cask.co>
2016-12-07 13:46:31 -0800

    CDAP-7634 Support multiple parts and Query and Path Param in AuthEnforce

--- cdap-security/src/test/java/co/cask/cdap/security/authorization/DummyAuthEnforce.java
+++ cdap-security/src/test/java/co/cask/cdap/security/authorization/DummyAuthEnforce.java
@@ -21,7 +21,13 @@ import co.cask.cdap.common.security.AuthEnforce;
 import co.cask.cdap.common.security.AuthEnforceRewriter;
 import co.cask.cdap.proto.id.InstanceId;
 import co.cask.cdap.proto.id.NamespaceId;
+import co.cask.cdap.proto.id.ScheduleId;
+import co.cask.cdap.proto.id.StreamId;
 import co.cask.cdap.proto.security.Action;
+import com.google.inject.name.Named;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 /**
  * A Dummy class which is just an wrapper and have different inner classes with {@link AuthEnforce} annotations.
@@ -61,9 +67,45 @@ public class DummyAuthEnforce {
       // exception during enforcement
     }
 
-    // to test that presence of a method with AuthEnforce in classs does not affect other methods which does have
+    // to test that presence of a method with AuthEnforce in class does not affect other methods which does have
     // AuthEnforce annotation
     public void testNoAuthEnforceAnnotation(@Name("namespaceId") NamespaceId namespaceId) throws Exception {
+      throw new EnforceNotCalledException();
+    }
+
+    // test AuthEnforce annotation which has multiple string parts in entities
+    @AuthEnforce(entities = {"namespace", "stream"}, enforceOn = StreamId.class, actions = Action.ADMIN)
+    public void testMultipleParts(@Name("namespace") String namespace, @Name("stream") String stream) throws Exception {
+      // the above annotation will call enforce after class rewrite which should throw an exception.
+      // If the line below is reached it means that enforce was not called as it supposed to be
+      throw new EnforceNotCalledException();
+    }
+
+    // test AuthEnforce where method parameters are marked with QueryParam and PathParam
+    @AuthEnforce(entities = {"namespace", "stream"}, enforceOn = StreamId.class, actions = Action.ADMIN)
+    public void testQueryPathParamAnnotations(@QueryParam("namespace") String namespace,
+                                              @PathParam("stream") String stream) throws Exception {
+      // the above annotation will call enforce after class rewrite which should throw an exception.
+      // If the line below is reached it means that enforce was not called as it supposed to be
+      throw new EnforceNotCalledException();
+    }
+
+    // test the preference of Name annotation when a method parameter is marked with Name and PathParam both
+    @AuthEnforce(entities = "namespace", enforceOn = NamespaceId.class, actions = Action.ADMIN)
+    public void testMultipleAnnotationsPref(@Name("namespace") @PathParam("namespaceId") NamespaceId namespaceId)
+            throws Exception {
+      // the above annotation will call enforce after class rewrite which should throw an exception.
+      // If the line below is reached it means that enforce was not called as it supposed to be
+      throw new EnforceNotCalledException();
+    }
+
+    // test the preference of Name annotation when two different parameters are marked with same name but one with Name
+    @AuthEnforce(entities = "namespaceId", enforceOn = NamespaceId.class, actions = Action.ADMIN)
+    public void testNameAnnotationPref(@Name("namespaceId") NamespaceId namespaceId,
+                                       @PathParam("namespaceId") String stream)
+            throws Exception {
+      // the above annotation will call enforce after class rewrite which should throw an exception.
+      // If the line below is reached it means that enforce was not called as it supposed to be
       throw new EnforceNotCalledException();
     }
   }
@@ -106,7 +148,7 @@ public class DummyAuthEnforce {
     }
 
     // tests that when a method parameter has Named annotation same as class field name and when specified in
-    // AuthEnforce entities method parameter gets prefernce
+    // AuthEnforce entities method parameter gets preference
     @AuthEnforce(entities = "someEntity", enforceOn = InstanceId.class, actions = Action.ADMIN)
     public void testParaPreference(@Name("someEntity") InstanceId instanceId) throws Exception {
       // the above annotation will call enforce after class rewrite which should throw an exception.
@@ -136,6 +178,99 @@ public class DummyAuthEnforce {
   }
 
   /**
+   * Class which has {@link AuthEnforce} annotation and parameter annotated with invalid annotation {@link Named}
+   */
+  public class InvalidParameterAnnotationType {
+
+    @AuthEnforce(entities = "wrongType", enforceOn = NamespaceId.class, actions = {Action.ADMIN, Action.READ})
+    public void testEntityNameAbsence(@Named("wrongType") NamespaceId namespaceId) throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation and two parameters with same name
+   */
+  public class DuplicateAnnotationName {
+
+    @AuthEnforce(entities = "wrongType", enforceOn = StreamId.class, actions = {Action.ADMIN, Action.READ})
+    public void testDuplicationAnnotationWithSameName(@Name("name") String s1, @Name("name") String s2)
+            throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation have multiple parts which is not only string type
+   */
+  public class EntitytWithString {
+
+    @AuthEnforce(entities = {"entity", "string"}, enforceOn = StreamId.class, actions = {Action.ADMIN, Action.READ})
+    public void testEntityAndString(@Name("entity") NamespaceId p1, @Name("string") String p2)
+            throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation with same name in {@link QueryParam} and {@link PathParam}
+   */
+  public class SameQueryAndPathParam {
+
+    @AuthEnforce(entities = "wrongType", enforceOn = StreamId.class, actions = {Action.ADMIN, Action.READ})
+    public void testDuplicationAnnotationWithSameName(@QueryParam("name") String s1, @PathParam("name") String s2)
+            throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation with invalid multiple parts
+   */
+  public class LessMultipleParts {
+
+    @AuthEnforce(entities = {"namespace"}, enforceOn = StreamId.class, actions = Action.ADMIN)
+    public void testLessMultipleParts(@Name("namespace") String namespace) throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation with invalid multiple parts
+   */
+  public class MoreMultipleParts {
+
+    @AuthEnforce(entities = {"namespace", "artifact", "version"}, enforceOn = StreamId.class, actions = Action.READ)
+    public void testMoreMultipleParts(@Name("namespace") String namespace, @Name("artifact") String artifact,
+                                      @Name("version") String version) throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation with invalid multiple parts
+   */
+  public class MultipleEntityIds {
+
+    @AuthEnforce(entities = {"namespace", "stream"}, enforceOn = StreamId.class, actions = Action.READ)
+    public void testMultipleEntityIds(@Name("namespace") NamespaceId namespace, @Name("artifact") StreamId stream)
+            throws Exception {
+      // no-op
+    }
+  }
+
+  /**
+   * Class which has {@link AuthEnforce} annotation with invalid enforce on
+   */
+  public class InvalidAuthEnforceEntityType {
+
+    @AuthEnforce(entities = {"schedule"}, enforceOn = ScheduleId.class, actions = Action.READ)
+    public void testInvalidEnforceOn(@Name("schedule") ScheduleId scheduleId) throws Exception {
+      // no-op
+    }
+  }
+
+  /**
    * Class which has {@link AuthEnforce} annotation without an invalid {@link Name} annotated parameter than the one
    * specified in {@link AuthEnforce#entities()}
    */
@@ -153,7 +288,7 @@ public class DummyAuthEnforce {
   public class DuplicateEntityName {
 
     @AuthEnforce(entities = "duplicateName", enforceOn = NamespaceId.class, actions = {Action.ADMIN, Action.READ})
-    public void testDuplicatEntityName(@Name("duplicateName") NamespaceId namespaceId,
+    public void testDuplicateEntityName(@Name("duplicateName") NamespaceId namespaceId,
                                        @Name("duplicateName") NamespaceId anotherNamespaceId) throws Exception {
       // no-op
     }
