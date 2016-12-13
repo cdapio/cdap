94e2d46bd3c0fcd61b83c4c0b741243f4a11a904
John Jackson <john@cask.co>
2016-12-13 10:38:03 -0800

    Update MD5 Hash; remove extra blank line.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,8 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="5f37143e83fd49d7f9786856538d3cea"
-
+DEFAULT_XML_MD5_HASH="f0e78493bdbf7989182f696e385ab1ed"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
