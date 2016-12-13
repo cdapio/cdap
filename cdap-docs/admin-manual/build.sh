dacb85ebd2a2ffb9b30b9b3ee726dc55ea707ea8
Chengfeng <mao@cask.co>
2016-12-13 13:52:42 -0800

    [CDAP-7775] update description of "app.program.extra.classpath" and "program.container.dist.jars"

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="fba3f38265dd6323aaf90634b6b9c2ea"
+DEFAULT_XML_MD5_HASH="e6db16490af07c50760366d6456987b6"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
