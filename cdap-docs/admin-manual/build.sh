be4a0e973a06e810a7cb8b19000028c3ef220ef8
John Jackson <john@cask.co>
2016-12-16 23:03:18 -0800

    Update build.sh
    update Hash

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="c70a9fda12d295935d00fee809e00f3f"
+DEFAULT_XML_MD5_HASH="605d24b1e58a17c21b8329bfff5fc986"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
