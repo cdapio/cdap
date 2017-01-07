0d5c813b8c42a2ef61e6f836547658a7f47a4dbe
CuriousVini <vinisha@cask.co>
2017-01-06 17:40:55 -0800

    changing version in misc files

--- cdap-ui/app/cdap/components/MarketPlaceEntity/__tests__/MarketPlaceEntity.test.js
+++ cdap-ui/app/cdap/components/MarketPlaceEntity/__tests__/MarketPlaceEntity.test.js
@@ -38,7 +38,7 @@ const entity = {
   description: 'This is a test entity',
   org: 'Cask',
   created: Date.now(),
-  cdapVersion: '4.0.0'
+  cdapVersion: '4.0.1-SNAPSHOT'
 };
 const entityDetail = {
   "specVersion": "1.0",
