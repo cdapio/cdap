25553f02d26d1e100684b71336cff88e18c08346
Gokul Gunasekaran <gokul@cask.co>
2017-01-07 22:13:06 -0800

    Skip if region is not available

--- cdap-hbase-compat-1.0-cdh/src/main/java/co/cask/cdap/data2/transaction/messaging/coprocessor/hbase10cdh/MessageTableRegionObserver.java
+++ cdap-hbase-compat-1.0-cdh/src/main/java/co/cask/cdap/data2/transaction/messaging/coprocessor/hbase10cdh/MessageTableRegionObserver.java
@@ -157,6 +157,10 @@ public class MessageTableRegionObserver extends BaseRegionObserver {
   @Override
   public InternalScanner preFlushScannerOpen(ObserverContext<RegionCoprocessorEnvironment> c, Store store,
                                              KeyValueScanner memstoreScanner, InternalScanner s) throws IOException {
+    if (!c.getEnvironment().getRegion().isAvailable()) {
+      return s;
+    }
+
     LoadingCache<ByteBuffer, Map<String, String>> cache;
     try {
       cache = getTopicCache(c.getEnvironment());
@@ -183,6 +187,10 @@ public class MessageTableRegionObserver extends BaseRegionObserver {
                                                List<? extends KeyValueScanner> scanners, ScanType scanType,
                                                long earliestPutTs, InternalScanner s,
                                                CompactionRequest request) throws IOException {
+    if (!c.getEnvironment().getRegion().isAvailable()) {
+      return s;
+    }
+
     LoadingCache<ByteBuffer, Map<String, String>> cache;
     try {
       cache = getTopicCache(c.getEnvironment());
