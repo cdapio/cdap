70f147d45f43acf57b29f03b0ace64b8c1fb0f64
John Jackson <john@cask.co>
2017-01-13 16:25:14 -0800

    Simplify code and set the correct release repo URL.

--- cdap-docs/developers-manual/source/conf.py
+++ cdap-docs/developers-manual/source/conf.py
@@ -21,18 +21,18 @@ def setup(app):
     app.add_config_value('snapshot_version', '', 'env')
 
 # Set the condition
+snapshot_version = release.endswith('SNAPSHOT')
 archetype_version = "-DarchetypeVersion=%s" % release
-if release.endswith('SNAPSHOT'):
-    snapshot_version = True
+
+if snapshot_version:
     archetype_repository = '-DarchetypeRepository=https://oss.sonatype.org/content/repositories/snapshots/'
     archetype_repository_version = "%s %s" % (archetype_repository, archetype_version)
 else:
-    snapshot_version = False
-    archetype_repository = '-DarchetypeRepository=https://oss.sonatype.org/service/local/staging/deploy/maven2'
+    # For releases, repo is not required, but included here to simplify the code as replacements can't be empty
+    archetype_repository = '-DarchetypeRepository=https://repo1.maven.org/maven2/'
     archetype_repository_version = archetype_version
 
 rst_epilog += """
-
 .. |archetype-repository| replace:: %(archetype_repository)s
 .. |archetype-version| replace:: %(archetype_version)s
 .. |archetype-repository-version| replace:: %(archetype_repository_version)s
