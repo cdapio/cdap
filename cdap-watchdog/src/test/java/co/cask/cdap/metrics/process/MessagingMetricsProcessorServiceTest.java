2048041821abc74e18f0d1b67a750b1791497e88
Chengfeng <mao@cask.co>
2017-01-16 19:40:34 -0800

    add kafka processor test

--- cdap-watchdog/src/test/java/co/cask/cdap/metrics/process/MessagingMetricsProcessorServiceTest.java
+++ cdap-watchdog/src/test/java/co/cask/cdap/metrics/process/MessagingMetricsProcessorServiceTest.java
@@ -20,13 +20,12 @@ import co.cask.cdap.api.dataset.lib.cube.AggregationFunction;
 import co.cask.cdap.api.dataset.lib.cube.TimeValue;
 import co.cask.cdap.api.messaging.TopicNotFoundException;
 import co.cask.cdap.api.metrics.MetricDataQuery;
-import co.cask.cdap.api.metrics.MetricSearchQuery;
 import co.cask.cdap.api.metrics.MetricStore;
 import co.cask.cdap.api.metrics.MetricTimeSeries;
 import co.cask.cdap.api.metrics.MetricType;
 import co.cask.cdap.api.metrics.MetricValues;
+import co.cask.cdap.api.metrics.MetricsContext;
 import co.cask.cdap.api.metrics.NoopMetricsContext;
-import co.cask.cdap.api.metrics.TagValue;
 import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.guice.IOModule;
 import co.cask.cdap.common.guice.NonCustomLocationUnitTestModule;
@@ -49,26 +48,38 @@ import co.cask.cdap.metrics.store.MetricDatasetFactory;
 import co.cask.cdap.security.auth.context.AuthenticationContextModules;
 import co.cask.cdap.security.authorization.AuthorizationEnforcementModule;
 import co.cask.cdap.security.authorization.AuthorizationTestModule;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
 import com.google.inject.util.Modules;
 import org.apache.tephra.TransactionManager;
+import org.apache.twill.internal.kafka.EmbeddedKafkaServer;
+import org.apache.twill.internal.kafka.client.ZKKafkaClientService;
+import org.apache.twill.internal.utils.Networks;
+import org.apache.twill.internal.zookeeper.InMemoryZKServer;
+import org.apache.twill.kafka.client.Compression;
+import org.apache.twill.kafka.client.KafkaClientService;
+import org.apache.twill.kafka.client.KafkaPublisher;
+import org.apache.twill.zookeeper.ZKClientService;
 import org.junit.Assert;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 /**
@@ -77,9 +88,14 @@ import java.util.Set;
 public class MessagingMetricsProcessorServiceTest extends MessagingMetricsTestBase {
   private static final Logger LOG = LoggerFactory.getLogger(MessagingMetricsProcessorServiceTest.class);
 
-  // Intentionally set fetcher persist threshold to small values, so that MessagingMetricsProcessorService
-  // internally can exhaust message iterator and complete the loop.
-  private static final int MESSAGING_FETCHER_PERSIST_THRESHOLD = 1;
+  @ClassRule
+  public static TemporaryFolder tmpFolder1 = new TemporaryFolder();
+  @ClassRule
+  public static TemporaryFolder tmpFolder2 = new TemporaryFolder();
+
+  private InMemoryZKServer zkServer;
+  private EmbeddedKafkaServer kafkaServer;
+
   private static final int PARTITION_SIZE = 2;
   private static final long START_TIME = System.currentTimeMillis() / 1000;
 
@@ -91,46 +107,86 @@ public class MessagingMetricsProcessorServiceTest extends MessagingMetricsTestBa
     injector.getInstance(TransactionManager.class).startAndWait();
     injector.getInstance(DatasetOpExecutor.class).startAndWait();
     injector.getInstance(DatasetService.class).startAndWait();
+    zkServer = InMemoryZKServer.builder().build();
+    zkServer.startAndWait();
+
+    Properties kafkaConfig = generateKafkaConfig(tmpFolder1);
+    kafkaServer = new EmbeddedKafkaServer(kafkaConfig);
+    kafkaServer.startAndWait();
+    ZKClientService zkClient = ZKClientService.Builder.of(zkServer.getConnectionStr()).build();
+    zkClient.startAndWait();
+    KafkaClientService kafkaClient = new ZKKafkaClientService(zkClient);
+    kafkaClient.startAndWait();
+
+    MetricStore metricStore = injector.getInstance(MetricStore.class);
 
     Set<Integer> partitions = new HashSet<>();
     for (int i = 0; i < PARTITION_SIZE; i++) {
       partitions.add(i);
     }
 
-    MetricStore metricStore = injector.getInstance(MetricStore.class);
-    MessagingMetricsProcessorService metricsProcessorService =
-      new MessagingMetricsProcessorService(injector.getInstance(MetricDatasetFactory.class), topicPrefix,
-                                           partitions, messagingService, injector.getInstance(SchemaGenerator.class),
-                                           injector.getInstance(DatumReaderFactory.class),
-                                           metricStore,
-                                           MESSAGING_FETCHER_PERSIST_THRESHOLD);
-
     final Map<String, String> metricsContext = new HashMap<>();
     metricsContext.put(Constants.Metrics.Tag.NAMESPACE, "NS_1");
     metricsContext.put(Constants.Metrics.Tag.APP, "APP_1");
     metricsContext.put(Constants.Metrics.Tag.FLOW, "FLOW_1");
+    MetricsContext context = new NoopMetricsContext(metricsContext);
 
-    metricsProcessorService.setMetricsContext(new NoopMetricsContext(metricsContext));
-    metricsProcessorService.startAndWait();
+    KafkaMetricsProcessorService kafkaMetricsProcessorService =
+      new KafkaMetricsProcessorService(kafkaClient,
+                                       injector.getInstance(MetricDatasetFactory.class),
+                                       new MetricsMessageCallbackFactory(injector.getInstance(SchemaGenerator.class),
+                                                                         injector.getInstance(DatumReaderFactory.class),
+                                                                         metricStore, 4), topicPrefix, partitions);
+
+    kafkaMetricsProcessorService.setMetricsContext(context);
+    kafkaMetricsProcessorService.startAndWait();
+
+    KafkaPublisher publisher = kafkaClient.getPublisher(KafkaPublisher.Ack.FIRE_AND_FORGET, Compression.SNAPPY);
+    KafkaPublisher.Preparer preparer = publisher.prepare(topicPrefix);
+
     Map<String, Long> expected = new HashMap<>();
-    // Publish metrics and record expected metrics
-    for (int i = 1; i <= 5; i++) {
-      publishMetrics(i, metricsContext, expected);
+    // Publish metrics to Kafka and record expected metrics before kafkaMetricsProcessorService starts
+    for (int i = 1; i < 10; i++) {
+      addKafkaMetrics(i, metricsContext, expected, preparer);
     }
+    preparer.send();
 
-    Thread.sleep(5000);
-    // Stop and restart metricsProcessorService
-    metricsProcessorService.stopAndWait();
-    metricsProcessorService =
+
+
+    // Intentionally set fetcher persist threshold to a small value, so that MessagingMetricsProcessorService
+    // internally can persist metrics when more messages are to be fetched
+    MessagingMetricsProcessorService messagingMetricsProcessorService =
       new MessagingMetricsProcessorService(injector.getInstance(MetricDatasetFactory.class), topicPrefix,
                                            partitions, messagingService, injector.getInstance(SchemaGenerator.class),
                                            injector.getInstance(DatumReaderFactory.class),
                                            metricStore,
-                                           MESSAGING_FETCHER_PERSIST_THRESHOLD);
-    metricsProcessorService.startAndWait();
+                                           1);
 
-    for (int i = 6; i <= 10; i++) {
-      publishMetrics(i, metricsContext, expected);
+
+
+    messagingMetricsProcessorService.setMetricsContext(context);
+    messagingMetricsProcessorService.startAndWait();
+
+    // Publish metrics and record expected metrics
+    for (int i = 10; i < 20; i++) {
+      publishMessagingMetrics(i, metricsContext, expected);
+    }
+
+    Thread.sleep(5000);
+    // Stop and restart messagingMetricsProcessorService
+    messagingMetricsProcessorService.stopAndWait();
+    // Intentionally set fetcher persist threshold to a large value, so that MessagingMetricsProcessorService
+    // internally only persists metrics during terminating.
+    messagingMetricsProcessorService =
+      new MessagingMetricsProcessorService(injector.getInstance(MetricDatasetFactory.class), topicPrefix,
+                                           partitions, messagingService, injector.getInstance(SchemaGenerator.class),
+                                           injector.getInstance(DatumReaderFactory.class),
+                                           metricStore,
+                                           100);
+    messagingMetricsProcessorService.startAndWait();
+
+    for (int i = 20; i < 30; i++) {
+      publishMessagingMetrics(i, metricsContext, expected);
     }
 
     Thread.sleep(5000);
@@ -138,20 +194,41 @@ public class MessagingMetricsProcessorServiceTest extends MessagingMetricsTestBa
     // Query metrics from the metricStore and compare them with the expected ones
     assertMetricsResult(metricStore, metricsContext, expected);
 
-    metricsProcessorService.stopAndWait();
+    messagingMetricsProcessorService.stopAndWait();
+    kafkaServer.stopAndWait();
+    zkServer.stopAndWait();
   }
 
-  private void publishMetrics(int i, Map<String, String> metricsContext, Map<String, Long> expected) throws IOException, TopicNotFoundException {
-    String metricName = "processed" + "_" + i;
-    MetricValues metric =
-      new MetricValues(metricsContext, metricName, START_TIME + i * 100, i, MetricType.GAUGE);
+  private void addKafkaMetrics(int i, Map<String, String> metricsContext, Map<String, Long> expected,
+                                   KafkaPublisher.Preparer preparer)
+    throws IOException, TopicNotFoundException {
+    MetricValues metric = getExpectedMetricValues(i, metricsContext, expected);
+    // Encode each MetricRecord into bytes and make it an individual kafka message in a message set.
     recordWriter.encode(metric, encoder);
+    // partitioning by the context
+    preparer.add(ByteBuffer.wrap(encoderOutputStream.toByteArray()), metric.getTags().hashCode());
+    encoderOutputStream.reset();
+    LOG.info("Published metric: {}", metric);
+    encoderOutputStream.reset();
+  }
+
+  private void publishMessagingMetrics(int i, Map<String, String> metricsContext, Map<String, Long> expected)
+    throws IOException, TopicNotFoundException {
+    MetricValues metric = getExpectedMetricValues(i, metricsContext, expected);
     messagingService.publish(StoreRequestBuilder.of(metricsTopics[i % PARTITION_SIZE])
                                .addPayloads(encoderOutputStream.toByteArray()).build());
     LOG.info("Published metric: {}", metric);
     encoderOutputStream.reset();
-    // TODO correct context format
+  }
+
+  private MetricValues getExpectedMetricValues(int i, Map<String, String> metricsContext, Map<String, Long> expected)
+    throws TopicNotFoundException, IOException {
+    String metricName = "processed" + "_" + i;
+    MetricValues metric =
+      new MetricValues(metricsContext, metricName, START_TIME + i * 100, i, MetricType.GAUGE);
+    recordWriter.encode(metric, encoder);
     expected.put("system." + metricName, (long) i);
+    return metric;
   }
 
   private void assertMetricsResult(MetricStore metricStore, Map<String, String> metricsContext,
@@ -191,5 +268,31 @@ public class MessagingMetricsProcessorServiceTest extends MessagingMetricsTestBa
       }
     }));
     return list;
+  }
+
+
+  private Properties generateKafkaConfig(TemporaryFolder tmpFolder) throws IOException {
+    int port = Networks.getRandomPort();
+    Preconditions.checkState(port > 0, "Failed to get random port.");
+
+    Properties prop = new Properties();
+    prop.setProperty("broker.id", "1");
+    prop.setProperty("port", Integer.toString(port));
+    prop.setProperty("num.network.threads", "2");
+    prop.setProperty("num.io.threads", "2");
+    prop.setProperty("socket.send.buffer.bytes", "1048576");
+    prop.setProperty("socket.receive.buffer.bytes", "1048576");
+    prop.setProperty("socket.request.max.bytes", "104857600");
+    prop.setProperty("log.dir", tmpFolder.newFolder().getAbsolutePath());
+    prop.setProperty("num.partitions", "1");
+    prop.setProperty("log.flush.interval.messages", "10000");
+    prop.setProperty("log.flush.interval.ms", "1000");
+    prop.setProperty("log.retention.hours", "1");
+    prop.setProperty("log.segment.bytes", "536870912");
+    prop.setProperty("log.cleanup.interval.mins", "1");
+    prop.setProperty("zookeeper.connect", zkServer.getConnectionStr());
+    prop.setProperty("zookeeper.connection.timeout.ms", "1000000");
+
+    return prop;
   }
 }
