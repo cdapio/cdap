079c0788c7221b091b9571c8e0150abdf68dd604
Edwin Elia <edwinselia@gmail.com>
2017-01-16 16:48:56 -0800

    add Version and VersionRange classes for FE

---
+++ cdap-ui/app/cdap/services/VersionRange/Version.js
@@ -0,0 +1,93 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+// FE implementation of ArtifactVersion java class
+// co.cask.cdap.api.artifact.ArtifactVersion
+//
+// [major].[minor].[fix](-)[suffix]
+
+export default class Version {
+  constructor(versionString) {
+    let major,
+        minor,
+        fix,
+        suffix;
+
+    // Get Suffix
+    let versionSuffixSplit = versionString.split('-');
+    suffix = versionSuffixSplit[1] || null;
+
+    // Split versions
+    let versionSplit = versionSuffixSplit[0].split('.');
+    major = versionSplit[0];
+    minor = versionSplit[1];
+    fix = versionSplit[2];
+
+    this.version = versionString;
+    this.major = parseInt(major, 10) || null;
+    this.minor = parseInt(minor, 10) || null;
+    this.fix = parseInt(fix, 10) || null;
+    this.suffix = suffix;
+  }
+
+  isSnapshot() {
+    return this.suffix !== null && this.suffix.length !== 0 && this.suffix.toLowerCase() === 'snapshot';
+  }
+
+  // -1, 0, 1   smaller, equal, greater
+  // special case for no suffix is greater than with suffix. (release > snapshot)
+  compareTo(other) {
+    let cmp = this.compare(this.major, other.major);
+    if (cmp !== 0) {
+      return cmp;
+    }
+
+    cmp = this.compare(this.minor, other.minor);
+    if (cmp !== 0) {
+      return cmp;
+    }
+
+    cmp = this.compare(this.fix, other.fix);
+    if (cmp !== 0) {
+      return cmp;
+    }
+
+    // All numerical part of the version are the same, compare the suffix.
+    // A special case is no suffix is "greater" than with suffix. This is usually true (e.g. release > snapshot)
+    if (this.suffix === null) {
+      return other.suffix === null ? 0 : 1;
+    }
+
+    return other.suffix === null ? -1 : 0;
+  }
+
+  compare(first, second) {
+    if ((first === null && second === null) || first === second) {
+      return 0;
+    }
+
+    if (first === null || first < second) {
+      return -1;
+    }
+
+    if (second === null || first > second) {
+      return 1;
+    }
+
+    // for invalid comparison
+    return null;
+  }
+}
---
+++ cdap-ui/app/cdap/services/VersionRange/__tests__/versionRange.test.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import VersionRange from 'services/VersionRange';
+import Version from 'services/VersionRange/Version';
+
+describe('Version Range Class', () => {
+  const EXCLUSIVE_RANGE = new VersionRange('[1.0.0, 1.1.0)');
+  const INCLUSIVE_RANGE = new VersionRange('[1.0.0, 1.1.0]');
+  const EXCLUSIVE_SNAPSHOT_RANGE = new VersionRange('[1.0.0-SNAPSHOT, 1.1.0)');
+
+  const IN_RANGE = new Version('1.0.1');
+  const OUT_RANGE = new Version('1.2.0');
+  const EDGE_RANGE = new Version('1.1.0');
+  const RELEASE_VERSION = new Version('1.0.0');
+  const SNAPSHOT_VERSION = new Version('1.0.0-SNAPSHOT');
+
+  it('should detect a version is in range', () => {
+    expect(EXCLUSIVE_RANGE.versionIsInRange(IN_RANGE)).toBe(true);
+  });
+
+  it('should detect a version out of range', () => {
+    expect(EXCLUSIVE_RANGE.versionIsInRange(OUT_RANGE)).toBe(false);
+  });
+
+  it('should detect edge range version', () => {
+    expect(EXCLUSIVE_RANGE.versionIsInRange(EDGE_RANGE)).toBe(false);
+    expect(INCLUSIVE_RANGE.versionIsInRange(EDGE_RANGE)).toBe(true);
+  });
+
+  it('should correctly determine snapshot range', () => {
+    expect(EXCLUSIVE_SNAPSHOT_RANGE.versionIsInRange(RELEASE_VERSION)).toBe(true);
+    expect(EXCLUSIVE_RANGE.versionIsInRange(SNAPSHOT_VERSION)).toBe(false);
+  });
+});
