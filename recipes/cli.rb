600589f08c54dd1ed8a9907c2926f3e2320ace0f
Derek Wood <derek@cask.co>
2017-01-25 17:19:33 -0800

    constrain version of base package installed with cli

--- recipes/cli.rb
+++ recipes/cli.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: cdap
 # Recipe:: cli
 #
-# Copyright © 2013-2015 Cask Data, Inc.
+# Copyright © 2013-2017 Cask Data, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +24,10 @@ end
 
 include_recipe 'cdap::repo'
 
-package 'cdap-cli' do
-  action :install
-  version node['cdap']['version']
+# Install base and cli packages. Order matters.
+%w(cdap cdap-cli).each do |pkg|
+  package pkg do
+    action :install
+    version node['cdap']['version']
+  end
 end
