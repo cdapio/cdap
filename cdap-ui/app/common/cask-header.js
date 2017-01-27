8dc49405a8236899aa68764e49c692dfb359f3ad
Edwin Elia <edwinselia@gmail.com>
2017-01-26 16:56:24 -0800

    add VersionStore to all UI apps

--- cdap-ui/app/common/cask-header.js
+++ cdap-ui/app/common/cask-header.js
@@ -20,4 +20,14 @@
  var HeaderBrand = require('../cdap/components/HeaderBrand').default;
  var Store = require('../cdap/services/NamespaceStore').default;
  var Wrangler = require('../wrangler/components/Wrangler').default;
- export {Store, HeaderBrand, HeaderActions, Wrangler};
+ var VersionStore = require('../cdap/services/VersionStore').default;
+ var VersionActions = require('../cdap/services/VersionStore/VersionActions').default;
+ export {
+  Store,
+  HeaderBrand,
+  HeaderActions,
+  Wrangler,
+  VersionStore,
+  VersionActions
+};
+
