ebd832fb8a51def3ccaa5b196c80b9825eb8ddca
John Jackson <john@cask.co>
2017-01-29 21:22:23 -0800

    "Get me rewrite!"

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="47352bba4edbdd0c4788b1ddacb747d4"
+DEFAULT_XML_MD5_HASH="303c787d8c3d9b8767cfc75cfbba0cf5"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
