e365ddf08daddeda6cf3f8bc35a74745294bf203
deepakwadhwani <Deepak@Ds-Macbook.local>
2017-01-30 13:14:22 -0800

    Ignore any extra regions on Slave cluster

--- cdap-master/src/main/java/co/cask/cdap/data/tools/ReplicationStatusTool.java
+++ cdap-master/src/main/java/co/cask/cdap/data/tools/ReplicationStatusTool.java
@@ -242,7 +242,8 @@ public class ReplicationStatusTool {
       System.out.println("Number of regions on the Master and Slave Clusters do not match.");
     }
 
-    complete = !checkDifferences(masterTimeMap.keySet(), slaveTimeMap.keySet(), "Region", true);
+    // Verify that all regions on the Master cluster are present on the Slave cluster
+    complete = !checkDifferences(masterTimeMap.keySet(), slaveTimeMap.keySet(), "Region");
 
     //Check the maps for all regions
     for (Map.Entry<String, Long> writeTimeEntry : masterTimeMap.entrySet()) {
@@ -264,21 +265,13 @@ public class ReplicationStatusTool {
     }
   }
 
-  static boolean checkDifferences(Set<String> masterMap, Set<String> slaveMap, String keyName, boolean checkSlave) {
+  // Verify that all entries on the Master Cluster are present on the Slave Cluster
+  static boolean checkDifferences(Set<String> masterMap, Set<String> slaveMap, String keyName) {
     boolean different = false;
     Set<String> extraInMaster = Sets.difference(masterMap, slaveMap);
-    Set<String> extraInSlave = Sets.difference(slaveMap, masterMap);
-
     for (String key : extraInMaster) {
       System.out.println(keyName + " " + key + " found on Master but not on Slave Cluster.");
       different = true;
-    }
-
-    if (checkSlave) {
-      for (String key : extraInSlave) {
-        System.out.println(keyName + " " + key + " found on Slave but not on Master Cluster.");
-        different = true;
-      }
     }
     return different;
   }
@@ -421,7 +414,7 @@ public class ReplicationStatusTool {
 
     // Verify that all files on Master are present on Slave. Ignore any extra files on Slave. This could
     // happen when old snapshot files are pruned by CDAP on the Master cluster.
-    complete = !checkDifferences(masterChecksumMap.keySet(), slaveChecksumMap.keySet(), "File", false);
+    complete = !checkDifferences(masterChecksumMap.keySet(), slaveChecksumMap.keySet(), "File");
 
     for (Map.Entry<String, String> checksumEntry : masterChecksumMap.entrySet()) {
       String file = checksumEntry.getKey();
