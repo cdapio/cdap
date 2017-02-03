8b6ffa33876f54352cd9b18fdd69918ca979cb52
Terence Yim <terence@cask.co>
2017-02-03 12:19:51 -0800

    added unit-test

--- cdap-watchdog/src/test/java/co/cask/cdap/logging/pipeline/KafkaLogProcessorPipelineTest.java
+++ cdap-watchdog/src/test/java/co/cask/cdap/logging/pipeline/KafkaLogProcessorPipelineTest.java
@@ -40,16 +40,22 @@ import org.apache.twill.kafka.client.KafkaPublisher;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
+import java.io.File;
 import java.io.Flushable;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.net.URL;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -71,6 +77,9 @@ import javax.xml.transform.stream.StreamResult;
 public class KafkaLogProcessorPipelineTest {
 
   @ClassRule
+  public static final TemporaryFolder TEMP_FOLDER = new TemporaryFolder();
+
+  @ClassRule
   public static final KafkaTester KAFKA_TESTER = new KafkaTester();
 
   @Test
@@ -89,9 +98,9 @@ public class KafkaLogProcessorPipelineTest {
       new PipelineContext("test", loggerContext), checkpointManager,
       KAFKA_TESTER.getBrokerService(), config);
 
-    // Publish some log messages to Kafka
     pipeline.startAndWait();
 
+    // Publish some log messages to Kafka
     long now = System.currentTimeMillis();
     publishLog(topic, ImmutableList.of(
       createLoggingEvent("test.logger", Level.INFO, "0", now - 1000),
@@ -144,6 +153,101 @@ public class KafkaLogProcessorPipelineTest {
     loggerContext.stop();
 
     Assert.assertNull(appender.getEvents());
+  }
+
+  @Test
+  public void testMultiAppenders() throws Exception {
+    final File logDir = TEMP_FOLDER.newFolder();
+    LoggerContext loggerContext = new LoggerContext();
+    loggerContext.putProperty("logDirectory", logDir.getAbsolutePath());
+
+    PipelineConfigurator configurator = new PipelineConfigurator();
+    configurator.setContext(loggerContext);
+    URL configURL = getClass().getClassLoader().getResource("pipeline-multi-appenders.xml");
+    Assert.assertNotNull(configURL);
+    configurator.doConfigure(configURL);
+
+    String topic = "testMultiAppenders";
+    TestCheckpointManager checkpointManager = new TestCheckpointManager();
+    KafkaPipelineConfig config = new KafkaPipelineConfig(topic, Collections.singleton(0), 1024, 100, 1048576, 200);
+    KAFKA_TESTER.createTopic(topic, 1);
+
+    loggerContext.start();
+    KafkaLogProcessorPipeline pipeline = new KafkaLogProcessorPipeline(
+      new PipelineContext("testMultiAppenders", loggerContext), checkpointManager,
+      KAFKA_TESTER.getBrokerService(), config);
+
+    pipeline.startAndWait();
+
+    // Publish some log messages to Kafka using a non-specific logger
+    long now = System.currentTimeMillis();
+    publishLog(topic, ImmutableList.of(
+      createLoggingEvent("logger.trace", Level.TRACE, "TRACE", now - 1000),
+      createLoggingEvent("logger.debug", Level.DEBUG, "DEBUG", now - 900),
+      createLoggingEvent("logger.info", Level.INFO, "INFO", now - 800),
+      createLoggingEvent("logger.warn", Level.WARN, "WARN", now - 700),
+      createLoggingEvent("logger.error", Level.ERROR, "ERROR", now - 600)
+    ));
+
+    // All logs should get logged to the default.log file
+    Tasks.waitFor(true, new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        File logFile = new File(logDir, "default.log");
+        List<String> lines = Files.readAllLines(logFile.toPath(), StandardCharsets.UTF_8);
+        return Arrays.asList("TRACE", "DEBUG", "INFO", "WARN", "ERROR").equals(lines);
+      }
+    }, 5, TimeUnit.SECONDS, 100, TimeUnit.MILLISECONDS);
+
+    // Publish some more log messages via the non-additive "test.info" logger.
+    now = System.currentTimeMillis();
+    publishLog(topic, ImmutableList.of(
+      createLoggingEvent("test.info.trace", Level.TRACE, "TRACE", now - 1000),
+      createLoggingEvent("test.info.debug", Level.DEBUG, "DEBUG", now - 900),
+      createLoggingEvent("test.info", Level.INFO, "INFO", now - 800),
+      createLoggingEvent("test.info.warn", Level.WARN, "WARN", now - 700),
+      createLoggingEvent("test.info.error", Level.ERROR, "ERROR", now - 600)
+    ));
+
+    // Only logs with INFO or above level should get written to the info.log file
+    Tasks.waitFor(true, new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        File logFile = new File(logDir, "info.log");
+        List<String> lines = Files.readAllLines(logFile.toPath(), StandardCharsets.UTF_8);
+        return Arrays.asList("INFO", "WARN", "ERROR").equals(lines);
+      }
+    }, 5, TimeUnit.SECONDS, 100, TimeUnit.MILLISECONDS);
+
+    // The default.log file shouldn't be changed, because the test.info logger is non additive
+    File defaultLogFile = new File(logDir, "default.log");
+    List<String> lines = Files.readAllLines(defaultLogFile.toPath(), StandardCharsets.UTF_8);
+    Assert.assertEquals(Arrays.asList("TRACE", "DEBUG", "INFO", "WARN", "ERROR"), lines);
+
+    // Publish a log messages via the additive "test.error" logger.
+    now = System.currentTimeMillis();
+    publishLog(topic, ImmutableList.of(
+      createLoggingEvent("test.error.1.2", Level.ERROR, "ERROR", now - 1000)
+    ));
+
+    // Expect the log get appended to both the error.log file as well as the default.log file
+    Tasks.waitFor(true, new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        File logFile = new File(logDir, "error.log");
+        List<String> lines = Files.readAllLines(logFile.toPath(), StandardCharsets.UTF_8);
+        if (!Collections.singletonList("ERROR").equals(lines)) {
+          return false;
+        }
+
+        logFile = new File(logDir, "default.log");
+        lines = Files.readAllLines(logFile.toPath(), StandardCharsets.UTF_8);
+        return Arrays.asList("TRACE", "DEBUG", "INFO", "WARN", "ERROR", "ERROR").equals(lines);
+      }
+    }, 5, TimeUnit.SECONDS, 100, TimeUnit.MILLISECONDS);
+
+    pipeline.stopAndWait();
+    loggerContext.stop();
   }
 
   private <T extends Appender<ILoggingEvent>> T getAppender(Logger logger, String name, Class<T> cls) {
