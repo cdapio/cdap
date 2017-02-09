400bad7199552c848f4218dfccf2eabe6e360c87
CuriousVini <vinisha@cask.co>
2017-02-08 17:57:18 -0800

    Addressing comments

--- cdap-distributions/src/hdinsight/pkg/install.sh
+++ cdap-distributions/src/hdinsight/pkg/install.sh
@@ -22,9 +22,9 @@
 # The git branch to clone
 CDAP_BRANCH='release/4.0'
 # Optional tag to checkout - All released versions of this script should set this
-CDAP_TAG='v4.0.2-SNAPSHOT'
+CDAP_TAG='v4.0.2'
 # The CDAP package version passed to Chef
-CDAP_VERSION='4.0.2-SNAPSHOT-1'
+CDAP_VERSION='4.0.2-1'
 # The version of Chef to install
 CHEF_VERSION='12.10.24'
 # cdap-site.xml configuration parameters
