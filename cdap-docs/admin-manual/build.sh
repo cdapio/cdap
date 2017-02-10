4db69a0e61533c00277bb8ac04572167bfad276d
John Jackson <john@cask.co>
2017-02-09 20:31:42 -0800

    Fix description and MD5 hash.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="37526640fb21c8aa569033288c05de74"
+DEFAULT_XML_MD5_HASH="0997638d7983e5ec17540254cfce0e55"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
