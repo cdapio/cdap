b46a64b727db6f2276b7be279e99818dcc577782
Chris Gianelloni <chris@cask.co>
2017-02-16 19:14:14 -0500

    Update ark and krb5 dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,12 @@ description      'Installs/Configures Cask Data Application Platform (CDAP)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.28.4'
 
-%w(ambari apt java nodejs ntp yum yum-epel).each do |cb|
+%w(ambari ark apt java nodejs ntp yum yum-epel).each do |cb|
   depends cb
 end
 
-depends 'ark', '< 2.2.0' # Fails saying "owner" isn't a valid attribute
 depends 'hadoop', '>= 2.0.0'
-depends 'krb5', '>= 2.2.0'
+depends 'krb5', '>= 2.2.1'
 
 %w(amazon centos debian redhat scientific ubuntu).each do |os|
   supports os
