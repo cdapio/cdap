3ac2174c4197627fa7088eb34b5e1989c3b0bec0
Derek Wood <derek@cask.co>
2017-02-17 16:10:41 -0800

    Version bump to 2.28.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@cask.co'
 license          'Apache 2.0'
 description      'Installs/Configures Cask Data Application Platform (CDAP)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.28.4'
+version          '2.28.5'
 
 %w(ambari ark apt java nodejs ntp yum yum-epel).each do |cb|
   depends cb
