80d474dfbd38ea43f6a00eadcb0d79778b9d376a
John Jackson <john@cask.co>
2017-02-26 16:21:53 -0800

    Fix order of XML files. Updated MD5 hash. Added a sort option to doc tool.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="26875f94b11aa8d3ef04de8ed8d8f9d8"
+DEFAULT_XML_MD5_HASH="97ddb1f9b71fa064d57b094d8b306865"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
