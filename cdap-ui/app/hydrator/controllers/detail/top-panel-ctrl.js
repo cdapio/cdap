89c145f357c4759cd3f93284a301221c62fa9ade
Ajai <ajainarayanan@gmail.com>
2017-03-01 16:18:18 -0800

    [HYDRATOR-1364] Fixes pipeline studio to remove __ui__ property while exporting pipeline
      - Removes adding __ui__ while exporting pipeline config
      - Fixes scope comparison to be case insensitive while importing a pipeline

--- cdap-ui/app/hydrator/controllers/detail/top-panel-ctrl.js
+++ cdap-ui/app/hydrator/controllers/detail/top-panel-ctrl.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 Cask Data, Inc.
+ * Copyright © 2016-2017 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -255,6 +255,7 @@ class HydratorDetailTopPanelController {
       plugin: stage.plugin
     }));
     let exportConfig = this.HydratorPlusPlusDetailNonRunsStore.getCloneConfig();
+    delete exportConfig.__ui__;
     this.myPipelineExportModalService.show(config, exportConfig);
   }
   do(action) {
