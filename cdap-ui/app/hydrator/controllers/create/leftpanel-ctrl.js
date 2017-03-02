f5fb8b460adb831e4a7b891fc389ae3b2892a248
Ajai <ajainarayanan@gmail.com>
2017-03-01 16:18:18 -0800

    [HYDRATOR-1364] Fixes pipeline studio to remove __ui__ property while exporting pipeline
      - Removes adding __ui__ while exporting pipeline config
      - Fixes scope comparison to be case insensitive while importing a pipeline

--- cdap-ui/app/hydrator/controllers/create/leftpanel-ctrl.js
+++ cdap-ui/app/hydrator/controllers/create/leftpanel-ctrl.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Cask Data, Inc.
+ * Copyright © 2015-2017 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -173,7 +173,7 @@ class HydratorPlusPlusLeftPanelCtrl {
       let isScopeExists = [];
       let isNameExists = this.artifacts.filter( artifact => artifact.name === importArtifact.name );
       isVersionExists = isNameExists.filter( artifact => artifact.version === importArtifact.version );
-      isScopeExists = isNameExists.filter( artifact => artifact.scope === importArtifact.scope );
+      isScopeExists = isNameExists.filter( artifact => artifact.scope.toUpperCase() === importArtifact.scope.toUpperCase() );
       return {
         name: isNameExists.length > 0,
         version: isVersionExists.length > 0,
