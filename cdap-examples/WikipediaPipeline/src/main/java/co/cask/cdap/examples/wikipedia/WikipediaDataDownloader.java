37f8d1d74e3b33f7047939283ca5559be182b2f2
deepakwadhwani <deepak@cask.co>
2017-03-05 22:10:09 -0800

    Add namespace arg support to Wikipedia Example for ITN usage Removing Usedataset because app can be configured to run without datasets in its own namespace

--- cdap-examples/WikipediaPipeline/src/main/java/co/cask/cdap/examples/wikipedia/WikipediaDataDownloader.java
+++ cdap-examples/WikipediaPipeline/src/main/java/co/cask/cdap/examples/wikipedia/WikipediaDataDownloader.java
@@ -58,8 +58,10 @@ public class WikipediaDataDownloader extends AbstractMapReduce {
     Job job = context.getHadoopJob();
     job.setMapperClass(WikipediaDataDownloaderMapper.class);
     job.setNumReduceTasks(0);
-    context.addInput(Input.ofDataset(WikipediaPipelineApp.PAGE_TITLES_DATASET));
-    context.addOutput(Output.ofDataset(WikipediaPipelineApp.RAW_WIKIPEDIA_DATASET));
+    String dataNamespace = context.getRuntimeArguments().get(WikipediaPipelineApp.NAMESPACE_ARG);
+    dataNamespace = dataNamespace == null ? getContext().getNamespace() : dataNamespace;
+    context.addInput(Input.ofDataset(WikipediaPipelineApp.PAGE_TITLES_DATASET).fromNamespace(dataNamespace));
+    context.addOutput(Output.ofDataset(WikipediaPipelineApp.RAW_WIKIPEDIA_DATASET).fromNamespace(dataNamespace));
   }
 
   @Override
