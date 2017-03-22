b8cc08722c09a98188a7474af071b2e6db03aa04
Ajai <ajainarayanan@gmail.com>
2017-03-22 08:18:10 -0700

    WIP 1: Initial evaluation

---
+++ cdap-ui/backstop_data/html_report/config.js
@@ -0,0 +1,342 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_0_global-navbar_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_0_global-navbar_0_laptop.png",
+        "selector": ".global-navbar",
+        "fileName": "prod_test_CDAP_Homepage_0_global-navbar_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 1
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 13,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170322-000124/failed_diff_prod_test_CDAP_Homepage_0_global-navbar_0_laptop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_1_global-navbarbrand-section_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_1_global-navbarbrand-section_0_laptop.png",
+        "selector": ".global-navbar .brand-section",
+        "fileName": "prod_test_CDAP_Homepage_1_global-navbarbrand-section_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 2,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_2_global-navbarnavbar-list-sectionli_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_2_global-navbarnavbar-list-sectionli_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li",
+        "fileName": "prod_test_CDAP_Homepage_2_global-navbarnavbar-list-sectionli_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 3,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_3_global-navbarnavbar-list-sectionli__n1_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_3_global-navbarnavbar-list-sectionli__n1_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n1",
+        "fileName": "prod_test_CDAP_Homepage_3_global-navbarnavbar-list-sectionli__n1_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 5,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_4_global-navbarnavbar-list-sectionli__n2_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_4_global-navbarnavbar-list-sectionli__n2_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n2",
+        "fileName": "prod_test_CDAP_Homepage_4_global-navbarnavbar-list-sectionli__n2_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 2,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_5_global-navbarnavbar-list-sectionli__n3_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_5_global-navbarnavbar-list-sectionli__n3_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n3",
+        "fileName": "prod_test_CDAP_Homepage_5_global-navbarnavbar-list-sectionli__n3_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 60,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_6_global-navbarnavbar-list-sectionli__n4_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_6_global-navbarnavbar-list-sectionli__n4_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n4",
+        "fileName": "prod_test_CDAP_Homepage_6_global-navbarnavbar-list-sectionli__n4_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 56,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_7_global-navbarnavbar-list-sectionli__n5_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_7_global-navbarnavbar-list-sectionli__n5_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n5",
+        "fileName": "prod_test_CDAP_Homepage_7_global-navbarnavbar-list-sectionli__n5_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 73,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_8_global-navbarnavbar-list-sectionli__n6_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_8_global-navbarnavbar-list-sectionli__n6_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li.__n6",
+        "fileName": "prod_test_CDAP_Homepage_8_global-navbarnavbar-list-sectionli__n6_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 55,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_9_global-navbarnavbar-list-sectionliactive_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_9_global-navbarnavbar-list-sectionliactive_0_laptop.png",
+        "selector": ".global-navbar .navbar-list-section li .active",
+        "fileName": "prod_test_CDAP_Homepage_9_global-navbarnavbar-list-sectionliactive_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 2,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_10_global-navbarnavbar-right-section_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_10_global-navbarnavbar-right-section_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section",
+        "fileName": "prod_test_CDAP_Homepage_10_global-navbarnavbar-right-section_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 10,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_11_global-navbarnavbar-right-sectionwith-shadow_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_11_global-navbarnavbar-right-sectionwith-shadow_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section .with-shadow",
+        "fileName": "prod_test_CDAP_Homepage_11_global-navbarnavbar-right-sectionwith-shadow_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 4,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_12_global-navbarnavbar-right-sectionwith-shadow__n1_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_12_global-navbarnavbar-right-sectionwith-shadow__n1_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section .with-shadow.__n1",
+        "fileName": "prod_test_CDAP_Homepage_12_global-navbarnavbar-right-sectionwith-shadow__n1_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 7,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_13_global-navbarnavbar-right-sectionwith-shadow__n2_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_13_global-navbarnavbar-right-sectionwith-shadow__n2_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section .with-shadow.__n2",
+        "fileName": "prod_test_CDAP_Homepage_13_global-navbarnavbar-right-sectionwith-shadow__n2_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 2,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_14_global-navbarnavbar-right-sectionheader-namespace-dropdown_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_14_global-navbarnavbar-right-sectionheader-namespace-dropdown_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section #header-namespace-dropdown",
+        "fileName": "prod_test_CDAP_Homepage_14_global-navbarnavbar-right-sectionheader-namespace-dropdown_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 3,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/prod_test_CDAP_Homepage_15_global-navbarnavbar-right-sectionwith-shadowcdap-menu_0_laptop.png",
+        "test": "../bitmaps_test/20170322-000124/prod_test_CDAP_Homepage_15_global-navbarnavbar-right-sectionwith-shadowcdap-menu_0_laptop.png",
+        "selector": ".global-navbar .navbar-right-section .with-shadow.cdap-menu",
+        "fileName": "prod_test_CDAP_Homepage_15_global-navbarnavbar-right-sectionwith-shadowcdap-menu_0_laptop.png",
+        "label": "CDAP Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 3,
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
