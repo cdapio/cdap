2f7119e4f84d43f1e5c8db74912d258e386f42db
sagarkapare <sagar@cask.co>
2017-03-24 11:57:35 -0700

    Updated version in distribution.

--- cdap-distributions/src/hdinsight/pkg/install.sh
+++ cdap-distributions/src/hdinsight/pkg/install.sh
@@ -22,9 +22,9 @@
 # The git branch to clone
 CDAP_BRANCH='release/3.5'
 # Optional tag to checkout - All released versions of this script should set this
-CDAP_TAG='v3.5.4'
+CDAP_TAG='v3.5.5'
 # The CDAP package version passed to Chef
-CDAP_VERSION='3.5.4-1'
+CDAP_VERSION='3.5.5-1'
 # The version of Chef to install
 CHEF_VERSION='12.10.24'
 # cdap-site.xml configuration parameters
