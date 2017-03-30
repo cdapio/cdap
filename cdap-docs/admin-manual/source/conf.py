9a7795f5150a07ed1d5571c9499b698f93499ce6
John Jackson <john@cask.co>
2017-03-30 16:30:01 -0700

    Changes to logging: - Updates descriptions in the cdap-default.xml - Adds certain properties to the exclusions list - Adds code to use the exclusions list to the code generating tables from the defaults XML file - Updates the MD5 hash - Adds a note to the Logging documentation about the use of "application" as a partition key - Changed the order of the appendices to put the cdap-defaults page first

--- cdap-docs/admin-manual/source/conf.py
+++ cdap-docs/admin-manual/source/conf.py
@@ -21,15 +21,18 @@ if release:
 
 # Build property tables files imported by logging.rst
 
-def build_property_tables(defaults, rst_table_header, name, properties):
+def build_property_tables(defaults, exclusions, rst_table_header, name, properties):
     target = os.path.join(os.getcwd(), '../target/_includes', name)
     table = rst_table_header + '\n'
     for prop in properties:
-        if defaults.has_key(prop):
-            table += defaults[prop].rst()
+        if prop in exclusions:
+            print "Ignoring prop '%s' as it is in the exclusions list" % prop
         else:
-            print "Unable to find prop '%s' in defaults" % prop
-            raise Exception('build_property_tables', "Unable to find prop '%s' in defaults" % prop)
+            if defaults.has_key(prop):
+                table += defaults[prop].rst()
+            else:
+                print "Unable to find prop '%s' in defaults" % prop
+                raise Exception('build_property_tables', "Unable to find prop '%s' in defaults" % prop)
     f = open(target, 'w')
     f.write(table)
     f.close()
@@ -39,6 +42,7 @@ defaults_dict = {}
 sys.path.insert(0, os.path.abspath('../../tools/cdap-default'))
 dcd = __import__("doc-cdap-default")
 defaults, tree = dcd.load_xml()
+exclusions = dcd.load_exclusions()
 if defaults:
     for item in defaults:
         defaults_dict[item.name] = item
@@ -50,16 +54,15 @@ writing_logs_to_kafka = [
     "log.publish.num.partitions",
     "log.publish.partition.key",
     ]
-build_property_tables(defaults_dict, dcd.RST_TABLE_HEADER, "logging-writing-logs-to-kafka.rst", writing_logs_to_kafka)
+build_property_tables(defaults_dict, exclusions, dcd.RST_TABLE_HEADER, "logging-writing-logs-to-kafka.rst", writing_logs_to_kafka)
 
 logging_log_saver_service = [
     "log.saver.max.instances",
     "log.saver.num.instances",
     "log.saver.container.memory.mb",
     "log.saver.container.num.cores",
-    "log.saver.status.bind.address",
     ]
-build_property_tables(defaults_dict, dcd.RST_TABLE_HEADER, "logging-log-saver-service.rst", logging_log_saver_service)
+build_property_tables(defaults_dict, exclusions, dcd.RST_TABLE_HEADER, "logging-log-saver-service.rst", logging_log_saver_service)
 
 logging_pipeline_configuration_1 = [
     "log.pipeline.cdap.dir.permissions",
@@ -69,19 +72,14 @@ logging_pipeline_configuration_1 = [
     "log.pipeline.cdap.file.max.size.bytes",
     "log.pipeline.cdap.file.permissions",
     "log.pipeline.cdap.file.retention.duration.days",
-    "log.pipeline.cdap.file.sync.interval.bytes",
     ]
-build_property_tables(defaults_dict, dcd.RST_TABLE_HEADER, "logging-pipeline-configuration-1.rst", logging_pipeline_configuration_1)
+build_property_tables(defaults_dict, exclusions, dcd.RST_TABLE_HEADER, "logging-pipeline-configuration-1.rst", logging_pipeline_configuration_1)
 
 logging_pipeline_configuration_2 = [
-    "log.process.pipeline.auto.buffer.ratio",
-    "log.process.pipeline.buffer.size",
     "log.process.pipeline.checkpoint.interval.ms",
     "log.process.pipeline.config.dir",
     "log.process.pipeline.event.delay.ms",
     "log.process.pipeline.kafka.fetch.size",
     "log.process.pipeline.lib.dir",
-    "log.process.pipeline.logger.cache.expiration.ms",
-    "log.process.pipeline.logger.cache.size",
     ]
-build_property_tables(defaults_dict, dcd.RST_TABLE_HEADER, "logging-pipeline-configuration-2.rst", logging_pipeline_configuration_2)
+build_property_tables(defaults_dict, exclusions, dcd.RST_TABLE_HEADER, "logging-pipeline-configuration-2.rst", logging_pipeline_configuration_2)
