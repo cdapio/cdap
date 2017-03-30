55f029330572fe596e0e01eda8fa7ddbb3798cc5
John Jackson <john@cask.co>
2017-03-30 16:42:37 -0700

    Fix hash.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="f90bec580da8c55f76e1c6ff4819dfe8"
+DEFAULT_XML_MD5_HASH="c851bd08827a95976cdff733a8d05d0b"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
