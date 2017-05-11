fc53d9d572b976632eb945fc78589f266a32a92c
Edwin Elia <edwinselia@gmail.com>
2017-05-10 17:47:23 -0700

    [CDAP-9284] query pipeline metrics using post body

--- cdap-ui/app/hydrator/services/detail/actions/pipeline-detail-metrics-actions.js
+++ cdap-ui/app/hydrator/services/detail/actions/pipeline-detail-metrics-actions.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 Cask Data, Inc.
+ * Copyright © 2016-2017 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -69,9 +69,20 @@ angular.module(PKG.name + '.feature.hydrator')
              **/
             dataSrc.request({
               method: 'POST',
-              _cdapPath: '/metrics/query?' + metricParams + '&metric=' + metricQuery.join('&metric=')
+              _cdapPath: '/metrics/query',
+              body: {
+                qid: {
+                  tags: params,
+                  metrics: metricQuery,
+                  aggregate: true,
+                  timeRange: {
+                    startTime: 0,
+                    endTime: 'now'
+                  }
+                }
+              }
             }).then(function(metrics) {
-              dispatcher.dispatch('onMetricsFetch', metrics);
+              dispatcher.dispatch('onMetricsFetch', metrics.qid);
             });
           }
         }.bind(this));
