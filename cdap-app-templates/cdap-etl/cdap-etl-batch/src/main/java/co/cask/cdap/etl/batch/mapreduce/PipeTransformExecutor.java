31530e76bf5777986713de6806c43ce83991dd71
deepakwadhwani <deepak@cask.co>
2017-05-12 14:29:04 -0700

    Add Pipeline fatal logs for User

--- cdap-app-templates/cdap-etl/cdap-etl-batch/src/main/java/co/cask/cdap/etl/batch/mapreduce/PipeTransformExecutor.java
+++ cdap-app-templates/cdap-etl/cdap-etl-batch/src/main/java/co/cask/cdap/etl/batch/mapreduce/PipeTransformExecutor.java
@@ -19,6 +19,11 @@ package co.cask.cdap.etl.batch.mapreduce;
 import co.cask.cdap.api.dataset.lib.KeyValue;
 import co.cask.cdap.etl.api.Destroyable;
 import co.cask.cdap.etl.batch.PipeTransformDetail;
+import co.cask.cdap.etl.common.Constants;
+import co.cask.cdap.etl.common.LocationAwareMDCWrapperLogger;
+import com.google.common.base.Throwables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Set;
@@ -31,6 +36,10 @@ public class PipeTransformExecutor<IN> implements Destroyable {
   private final Set<String> startingPoints;
   private final Map<String, PipeTransformDetail> transformDetailMap;
 
+  private static final Logger PIPELINE_LOG =
+    new LocationAwareMDCWrapperLogger(LoggerFactory.getLogger(PipeTransformExecutor.class), Constants.EVENT_TYPE_TAG,
+                                      Constants.PIPELINE_LIFECYCLE_TAG_VALUE);
+
   public PipeTransformExecutor(Map<String, PipeTransformDetail> transformDetailMap, Set<String> startingPoints) {
     this.transformDetailMap = transformDetailMap;
     this.startingPoints = startingPoints;
@@ -39,7 +48,15 @@ public class PipeTransformExecutor<IN> implements Destroyable {
   public void runOneIteration(IN input) throws Exception {
     for (String stageName : startingPoints) {
       PipeTransformDetail transformDetail = transformDetailMap.get(stageName);
+      try {
         transformDetail.process(new KeyValue<String, Object>(stageName, input));
+      } catch (Exception e) {
+        // Catch the exception and Generate a Error Log for the Pipeline User
+        PIPELINE_LOG.error("Failed to execute pipeline stage '{}' with the error: {}. Please review your pipeline " +
+                            "configuration and check the system logs for more details.", stageName,
+                          Throwables.getRootCause(e).getMessage(), Throwables.getRootCause(e));
+        throw e;
+      }
     }
   }
 
