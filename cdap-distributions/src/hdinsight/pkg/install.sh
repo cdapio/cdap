9835e9e13ebbcebabe327992c2593997a5b3ae8d
Derek Wood <derek@cask.co>
2017-05-18 15:24:18 -0700

    use hdi4.1 tag

--- cdap-distributions/src/hdinsight/pkg/install.sh
+++ cdap-distributions/src/hdinsight/pkg/install.sh
@@ -22,7 +22,7 @@
 # The git branch to clone
 CDAP_BRANCH='release/4.1'
 # Optional tag to checkout - All released versions of this script should set this
-CDAP_TAG='v4.1.1'
+CDAP_TAG='hdi4.1'
 # The CDAP package version passed to Chef
 CDAP_VERSION='4.1.1-2'
 # The version of Chef to install
