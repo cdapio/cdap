0428150127a396682716597fa7fcbb5a76c25aab
John Jackson <john@cask.co>
2017-05-23 16:59:46 -0700

    Fix inclusion of DataCleansing.java and MD5 hashes.

--- cdap-docs/examples-manual/build.sh
+++ cdap-docs/examples-manual/build.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Copyright © 2014-2017 Cask Data, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
 # the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-  
+
 # Build script for guide docs
 #
 # Copies the original README from the mounted GitHub repo in the local filesystem
@@ -32,7 +32,7 @@ function guide_rewrite_sed() {
   local includes_dir=${1}
   local guide=${2}
   local project_version=${PROJECT_SHORT_VERSION}
-  
+
   local source1="https://raw.githubusercontent.com/cdap-guides"
   if [ "x${GIT_BRANCH_CDAP_GUIDES}" != "x" ]; then
     local source2="${GIT_BRANCH_CDAP_GUIDES}"
@@ -46,7 +46,7 @@ function guide_rewrite_sed() {
   local readme="README.rst"
   local readme_source="README_SOURCE.rst"
   local redirect="\.\./\.\./${TARGET}/_includes" # Target, 2 redirects, escaped
-  
+
   if curl --output /dev/null --silent --head --fail "${url}/${readme}"; then
     mkdir ${includes_dir}/${guide}
     curl --silent ${url}/${readme} --output ${includes_dir}/${guide}/${readme_source}
@@ -58,28 +58,28 @@ function guide_rewrite_sed() {
         curl --silent ${url}/${image} --output ${includes_dir}/${guide}/${image_file}
       done
     fi
-    # For cdap-etl-adapter-guide, re-write links, image, and code 
+    # For cdap-etl-adapter-guide, re-write links, image, and code
     if [ "${guide}" == "cdap-etl-guide" ]; then
     sed -e "s|image:: docs/images/|image:: ${redirect}/${guide}/|g" \
         -e "s|.. code:: |.. code-block:: |g" \
         -e "s|.. _\(.*\): \(.*\)|.. _\1: https://github.com/cdap-guides/${guide}/tree/${source2}/\2|g" \
         ${includes_dir}/${guide}/${readme_source} > ${includes_dir}/${guide}/${readme}
     else
-      # Just rewrite image and code 
+      # Just rewrite image and code
       sed -e "s|image:: docs/images/|image:: ${redirect}/${guide}/|g" -e "s|.. code:: |.. code-block:: |g" \
-      ${includes_dir}/${guide}/${readme_source} > ${includes_dir}/${guide}/${readme}    
+      ${includes_dir}/${guide}/${readme_source} > ${includes_dir}/${guide}/${readme}
     fi
   else
     local m="URL does not exist: ${url}"
     echo_red_bold "${m}"
     set_message "${m}"
-  fi  
+  fi
 }
 
-function download_includes() { 
+function download_includes() {
   echo_red_bold "Downloading source files includes from GitHub..."
   set_version
-  
+
   local includes=${1}
   local includes_wise="${includes}/tutorial-wise"
   local project_version=${PROJECT_SHORT_VERSION}
@@ -113,9 +113,9 @@ function download_includes() {
   download_file $includes_wise $project_img wise_flow.png                 894828f13019dfbda5de43f514a8a49f
 
   echo_red_bold "Downloading files and any images and re-writing all the image links..."
-  guide_rewrite_sed $1 cdap-bi-guide 
-  guide_rewrite_sed $1 cdap-cube-guide 
-  guide_rewrite_sed $1 cdap-etl-guide 
+  guide_rewrite_sed $1 cdap-bi-guide
+  guide_rewrite_sed $1 cdap-cube-guide
+  guide_rewrite_sed $1 cdap-etl-guide
   guide_rewrite_sed $1 cdap-flow-guide
   guide_rewrite_sed $1 cdap-flume-guide
   guide_rewrite_sed $1 cdap-kafka-ingest-guide
@@ -124,9 +124,9 @@ function download_includes() {
   guide_rewrite_sed $1 cdap-timeseries-guide
   guide_rewrite_sed $1 cdap-twitter-ingest-guide
   guide_rewrite_sed $1 cdap-workflow-guide
-  
+
   echo_red_bold "Checking included example files for changes"
-  
+
   # Group alphabetically each example separately, files from each example together
 
   test_an_include 0d1fdc6d75995c3c417886786ccc8997 ../../cdap-examples/ClicksAndViews/src/main/java/co/cask/cdap/examples/clicksandviews/ClicksAndViews.java
@@ -138,18 +138,18 @@ function download_includes() {
   test_an_include 77d244f968d508d9ea2d91e463065b68 ../../cdap-examples/CountRandom/src/main/java/co/cask/cdap/examples/countrandom/NumberSplitter.java
   test_an_include 9f963a17090976d2c15a4d092bd9e8de ../../cdap-examples/CountRandom/src/main/java/co/cask/cdap/examples/countrandom/NumberCounter.java
 
-  test_an_include 07b401ea6e690443d720e67ce624e412 ../../cdap-examples/DataCleansing/src/main/java/co/cask/cdap/examples/datacleansing/DataCleansing.java
+  test_an_include fa746b8f3423736310316f2d48b0fe7d ../../cdap-examples/DataCleansing/src/main/java/co/cask/cdap/examples/datacleansing/DataCleansing.java
   test_an_include cc6989fe673cd88545d73cdaffad1bb1 ../../cdap-examples/DataCleansing/src/main/java/co/cask/cdap/examples/datacleansing/DataCleansingMapReduce.java
 
   test_an_include fe679b53a1dc757a15fd3fcafc2046fd ../../cdap-examples/FileSetExample/src/main/java/co/cask/cdap/examples/fileset/FileSetExample.java
   test_an_include 8fdb325ac2ad92bca959bd2f12fc91d7 ../../cdap-examples/FileSetExample/src/main/java/co/cask/cdap/examples/fileset/FileSetService.java
   test_an_include 4107ad528a3e9ca569a0cdee43b189c5 ../../cdap-examples/FileSetExample/src/main/java/co/cask/cdap/examples/fileset/WordCount.java
-  
+
   test_an_include cb9f3b1968418ed32176dc070946d104 ../../cdap-examples/HelloWorld/src/main/java/co/cask/cdap/examples/helloworld/HelloWorld.java
 
   test_an_include 4626aaec1bbc5bdbfd1de28cc721ec76 ../../cdap-examples/LogAnalysis/src/main/java/co/cask/cdap/examples/loganalysis/LogAnalysisApp.java
 
-  test_an_include e254ccce98a446ed7b5452905df90681 ../../cdap-examples/Purchase/src/main/java/co/cask/cdap/examples/purchase/PurchaseApp.java
+  test_an_include f90e4e4ce08851f45bdbe2563efb51dd ../../cdap-examples/Purchase/src/main/java/co/cask/cdap/examples/purchase/PurchaseApp.java
   test_an_include 29fe1471372678115e643b0ad431b28d ../../cdap-examples/Purchase/src/main/java/co/cask/cdap/examples/purchase/PurchaseStore.java
   test_an_include bb7344dd2c55c5ef70653ff0b5fcd6df ../../cdap-examples/Purchase/src/main/java/co/cask/cdap/examples/purchase/PurchaseHistoryBuilder.java
   test_an_include 80216a08a2b3d480e4a081722408222f ../../cdap-examples/Purchase/src/main/java/co/cask/cdap/examples/purchase/PurchaseHistoryService.java
@@ -157,13 +157,13 @@ function download_includes() {
   test_an_include 04abc21d3a3423cecc3b9c9619aa960d ../../cdap-examples/SpamClassifier/src/main/java/co/cask/cdap/examples/sparkstreaming/SpamClassifier.java
 
   test_an_include 3f25e035b2de8bd2d127733df1c58ff1 ../../cdap-examples/SparkKMeans/src/main/java/co/cask/cdap/examples/sparkkmeans/SparkKMeansApp.java
-  
+
   test_an_include f9acd1e2ed73ba5e83b9a0cd6c75f988 ../../cdap-examples/SparkPageRank/src/main/java/co/cask/cdap/examples/sparkpagerank/SparkPageRankApp.java
 
   test_an_include 61aac2c868c7966847aec6520dcf7529 ../../cdap-examples/SportResults/src/main/java/co/cask/cdap/examples/sportresults/SportResults.java
   test_an_include fb8055d43cb481c1d5b404d0d1278f51 ../../cdap-examples/SportResults/src/main/java/co/cask/cdap/examples/sportresults/UploadService.java
   test_an_include 6e0ed4027acafd163e8b891a1045717f ../../cdap-examples/SportResults/src/main/java/co/cask/cdap/examples/sportresults/ScoreCounter.java
-  
+
   test_an_include 6e407b943f7d6c92877023628dceef96 ../../cdap-examples/StreamConversion/src/main/java/co/cask/cdap/examples/streamconversion/StreamConversionApp.java
   test_an_include 2ec634340cca4f453cfc05bb4c064130 ../../cdap-examples/StreamConversion/src/main/java/co/cask/cdap/examples/streamconversion/StreamConversionMapReduce.java
 
@@ -172,9 +172,9 @@ function download_includes() {
   test_an_include 75aee2ce7b34eb125d41a295d5f3122d ../../cdap-examples/WebAnalytics/src/main/java/co/cask/cdap/examples/webanalytics/UniqueVisitor.java
   test_an_include cec2fd083dabf4da2b178559653e0992 ../../cdap-examples/WebAnalytics/src/main/java/co/cask/cdap/examples/webanalytics/UniqueVisitCount.java
   test_an_include 8ca118b98daab1cd34005dce37f24d4e ../../cdap-examples/WebAnalytics/src/main/java/co/cask/cdap/examples/webanalytics/WebAnalyticsFlow.java
-  
+
   test_an_include 8a608fa3ffdaa8ac5c79456591ca7603 ../../cdap-examples/WikipediaPipeline/src/main/java/co/cask/cdap/examples/wikipedia/WikipediaPipelineApp.java
-  
+
   test_an_include cfe55afe338b6c4aa40debc840594e0b ../../cdap-examples/WordCount/src/main/java/co/cask/cdap/examples/wordcount/WordCount.java
 
   echo_red_bold "Rewriting the Apps-Packs file"
