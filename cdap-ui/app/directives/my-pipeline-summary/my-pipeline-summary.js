705d31345a5f77c5f1d9cf5c4f8e9b92a02af2d6
Ajai <ajainarayanan@gmail.com>
2017-06-23 15:19:59 -0700

    Modifies my-pipeline-summary in pipeline detailed view to use PipelineSummary component from react

--- cdap-ui/app/directives/my-pipeline-summary/my-pipeline-summary.js
+++ cdap-ui/app/directives/my-pipeline-summary/my-pipeline-summary.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016 Cask Data, Inc.
+ * Copyright © 2016-2017 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -15,11 +15,15 @@
  */
 
 angular.module(PKG.name + '.commons')
+  .directive('pipelineSummary', function(reactDirective) {
+    return reactDirective(window.CaskCommon.PipelineSummary);
+  })
   .directive('myPipelineSummary', function() {
     return {
       restrict: 'A',
       scope: {
         store: '=',
+        pipelineConfig: '=',
         actionCreator: '=',
         pipelineType: '@'
       },
