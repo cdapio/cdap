261c78a32ec45f6ab01cb7d853c2b4072a073a64
Tony Bach <tbach@cask.co>
2017-07-20 11:54:17 -0700

    Adds styling when hovering over connections

--- cdap-ui/app/directives/dag-plus/my-dag-factory.js
+++ cdap-ui/app/directives/dag-plus/my-dag-factory.js
@@ -18,7 +18,8 @@ angular.module(PKG.name + '.commons')
   .factory('DAGPlusPlusFactory', function() {
     var defaultSettings = {
       Connector : [ 'Flowchart', {gap: 6, stub: [10, 15], alwaysRespectStubs: true, midpoint: 0, cornerRadius: 20} ],
-      ConnectionsDetachable: true
+      ConnectionsDetachable: true,
+      HoverPaintStyle: { strokeStyle: '#58b7f6', lineWidth: 4 },
     };
     var connectorStyle = {
       strokeStyle: '#666e82',
