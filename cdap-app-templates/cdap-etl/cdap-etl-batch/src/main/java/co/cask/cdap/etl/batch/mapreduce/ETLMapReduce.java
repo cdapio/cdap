ba5df8c18c46ebc8401d8b6114a982c2bb14232b
Archil Shah <archil.shah@gmail.com>
2017-08-18 15:38:23 -0700

    add fieldlevellineage

--- cdap-app-templates/cdap-etl/cdap-etl-batch/src/main/java/co/cask/cdap/etl/batch/mapreduce/ETLMapReduce.java
+++ cdap-app-templates/cdap-etl/cdap-etl-batch/src/main/java/co/cask/cdap/etl/batch/mapreduce/ETLMapReduce.java
@@ -30,6 +30,7 @@ import co.cask.cdap.api.mapreduce.MapReduceTaskContext;
 import co.cask.cdap.api.metrics.Metrics;
 import co.cask.cdap.api.workflow.WorkflowToken;
 import co.cask.cdap.etl.api.AlertPublisher;
+import co.cask.cdap.etl.api.FieldLevelLineage;
 import co.cask.cdap.etl.api.StageSubmitterContext;
 import co.cask.cdap.etl.api.Transform;
 import co.cask.cdap.etl.api.batch.BatchAggregator;
@@ -228,11 +229,14 @@ public class ETLMapReduce extends AbstractMapReduce {
     }
     hConf.set(INPUT_ALIAS_KEY, GSON.toJson(inputAliasToStage));
 
-    for (StageSpec transformInfo : phase.getStagesOfType(Transform.PLUGIN_TYPE)) {
+    Set<StageSpec> transformStages = phase.getStagesOfType(Transform.PLUGIN_TYPE);
+    Map<String, FieldLevelLineage> transformFieldLevelLineages = new HashMap<>(transformStages.size());
+    for (StageSpec transformInfo : transformStages) {
       Transform transform = pluginInstantiator.newPluginInstance(transformInfo.getName(), evaluator);
       StageSubmitterContext transformContext =
         new MapReduceBatchContext(context, pipelineRuntime, transformInfo, connectorDatasets);
       transform.prepareRun(transformContext);
+      transformFieldLevelLineages.put(transformInfo.getName(), transformContext.getFieldLevelLineage());
       finishers.add(transform, transformContext);
     }
 
