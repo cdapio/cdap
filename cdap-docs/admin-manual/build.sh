e44fb3b6cafc8411707cb7bd28cbf00a852c3605
Sreevatsan Raman <sree@Sreevatsans-MacBook-Pro.local>
2017-08-19 13:04:47 -0700

    Fix docs build due to md5 mismatch on cdap-default

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="3c2d4d8f5ca0de4840e704cc569c6479"
+DEFAULT_XML_MD5_HASH="5a116bc8086699240d04aee72c4694ce"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
