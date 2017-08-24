65f6dbdd04f4c82e31e4839e5db97ce2d1044b0f
Terence Yim <terence@cask.co>
2017-08-24 00:20:56 -0700

    Update Gson codec for TriggeringScheduleInfo and WorkflowToken.

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/schedule/TriggeringScheduleInfoAdapter.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/schedule/TriggeringScheduleInfoAdapter.java
@@ -18,64 +18,40 @@ package co.cask.cdap.internal.app.runtime.schedule;
 
 import co.cask.cdap.api.schedule.TriggerInfo;
 import co.cask.cdap.api.schedule.TriggeringScheduleInfo;
+import co.cask.cdap.api.workflow.WorkflowToken;
 import co.cask.cdap.common.app.RunIds;
 import co.cask.cdap.internal.app.ApplicationSpecificationAdapter;
 import co.cask.cdap.internal.app.runtime.schedule.trigger.TriggerInfoCodec;
-import com.google.common.reflect.TypeToken;
+import co.cask.cdap.internal.app.runtime.workflow.BasicWorkflowToken;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
 import org.apache.twill.api.RunId;
-
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Helper class to encoded/decode {@link co.cask.cdap.api.schedule.TriggeringScheduleInfo} to/from json.
  */
 public class TriggeringScheduleInfoAdapter {
 
-  private static final Type LIST_TRIGGER_INFO_TYPE = new TypeToken<List<TriggerInfo>>() { }.getType();
-  private static final Type Map_STRING_STRING_TYPE = new TypeToken<Map<String, String>>() { }.getType();
-
   public static GsonBuilder addTypeAdapters(GsonBuilder builder) {
     return ApplicationSpecificationAdapter.addTypeAdapters(builder)
-      .registerTypeAdapter(TriggeringScheduleInfo.class, new TriggeringScheduleInfoCodec())
       .registerTypeAdapter(TriggerInfo.class, new TriggerInfoCodec())
-      .registerTypeAdapter(RunId.class, new RunIds.RunIdCodec());
-  }
-
-  private static final class TriggeringScheduleInfoCodec implements JsonSerializer<TriggeringScheduleInfo>,
-                                                                    JsonDeserializer<TriggeringScheduleInfo> {
-
-    @Override
-    public JsonElement serialize(TriggeringScheduleInfo src, Type typeOfSrc, JsonSerializationContext context) {
-      JsonObject jsonObj = new JsonObject();
-      jsonObj.addProperty("name", src.getName());
-      jsonObj.addProperty("description", src.getDescription());
-      jsonObj.add("triggerInfos", context.serialize(src.getTriggerInfos(), LIST_TRIGGER_INFO_TYPE));
-      jsonObj.add("properties", context.serialize(src.getProperties(), Map_STRING_STRING_TYPE));
-
-      return jsonObj;
-    }
-
-    @Override
-    public TriggeringScheduleInfo deserialize(JsonElement json, Type typeOfT,
-                                              JsonDeserializationContext context) throws JsonParseException {
-      JsonObject jsonObj = json.getAsJsonObject();
-
-      String name = jsonObj.get("name").getAsString();
-      String description = jsonObj.get("description").getAsString();
-      List<TriggerInfo> triggerInfos = context.deserialize(jsonObj.get("triggerInfos"), LIST_TRIGGER_INFO_TYPE);
-      Map<String, String> properties = context.deserialize(jsonObj.get("properties"), Map_STRING_STRING_TYPE);
-
-      return new DefaultTriggeringScheduleInfo(name, description, triggerInfos, properties);
-    }
+      .registerTypeAdapter(RunId.class, new RunIds.RunIdCodec())
+      .registerTypeAdapterFactory(new TypeAdapterFactory() {
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+          if (TriggeringScheduleInfo.class.equals(type.getRawType())) {
+            //noinspection unchecked
+            return (TypeAdapter<T>) gson.getAdapter(DefaultTriggeringScheduleInfo.class);
+          }
+          if (WorkflowToken.class.equals(type.getRawType())) {
+            //noinspection unchecked
+            return (TypeAdapter<T>) gson.getAdapter(BasicWorkflowToken.class);
+          }
+          return null;
+        }
+      });
   }
 }
