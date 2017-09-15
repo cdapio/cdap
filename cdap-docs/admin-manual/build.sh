af5c5e142aaf3658ad7e54d0638ad366c7bded37
sagarkapare <sagar@cask.co>
2017-09-14 18:07:36 -0700

    Fixed md5 hash.

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="85cf45ea10a60f68487bde1910a67881"
+DEFAULT_XML_MD5_HASH="ad6e3755a83393d854f720d40772d607"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
