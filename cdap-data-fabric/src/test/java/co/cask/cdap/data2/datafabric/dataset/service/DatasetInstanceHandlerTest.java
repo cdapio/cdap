3eca2ba1f1cd21d916d9e268fff3b882cb76329f
sagarkapare <sagar@cask.co>
2017-09-18 07:11:37 -0700

    Fixed test case to allow put on data/datasets endpoint.

--- cdap-data-fabric/src/test/java/co/cask/cdap/data2/datafabric/dataset/service/DatasetInstanceHandlerTest.java
+++ cdap-data-fabric/src/test/java/co/cask/cdap/data2/datafabric/dataset/service/DatasetInstanceHandlerTest.java
@@ -540,7 +540,7 @@ public class DatasetInstanceHandlerTest extends DatasetServiceTestBase {
       validateGet(getUrl("/data/datasets"), 200);
       validateGet(getUrl("nosuchnamespace", "/data/datasets"), 404);
       validateGet(getUrl("inval+d", "/data/datasets"), 400);
-      validatePut(getUrl("/data/datasets"), "", 405);
+      validatePut(getUrl("/data/datasets"), "", 200);
       validatePost(getUrl("/data/datasets"), "", 405);
 
       validateGet(getUrl("/data/datasets/nusuch"), 404);
