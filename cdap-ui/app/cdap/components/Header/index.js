a8fe37472a2a34aabef950e9aade68d380e5893f
Ajai <ajainarayanan@gmail.com>
2017-10-19 14:16:08 -0700

    Adds Analytics item in Header navbar

--- cdap-ui/app/cdap/components/Header/index.js
+++ cdap-ui/app/cdap/components/Header/index.js
@@ -116,12 +116,26 @@ export default class Header extends Component {
     let dataprepBasePath = `${basePath}/dataprep`;
     let connectionsBasePath = `${basePath}/connections`;
     let rulesenginepath = `${basePath}/rulesengine`;
+    let analytics = `${basePath}/experiments`;
     if (
       location.pathname.startsWith(basePath) &&
       !location.pathname.startsWith(dataprepBasePath) &&
       !location.pathname.startsWith(connectionsBasePath) &&
-      !location.pathname.startsWith(rulesenginepath)
+      !location.pathname.startsWith(rulesenginepath) &&
+      !location.pathname.startsWith(analytics)
     ) {
+      return true;
+    }
+    return false;
+  };
+
+  isMMDSActive = (match, location = window.location) => {
+    if (match && match.isExact) {
+      return true;
+    }
+    let {selectedNamespace: namespace} = NamespaceStore.getState();
+    let experimentsBasePath = `/ns/${namespace}/experiments`;
+    if (location.pathname.startsWith(experimentsBasePath)) {
       return true;
     }
     return false;
@@ -131,6 +145,8 @@ export default class Header extends Component {
     let baseCDAPURL = `/ns/${this.state.currentNamespace}`;
     let rulesengineUrl = `${baseCDAPURL}/rulesengine`;
     let dataprepUrl = `${baseCDAPURL}/dataprep`;
+    let mmdsurl = `${baseCDAPURL}/experiments`;
+
     let pipelinesListUrl =  window.getHydratorUrl({
       stateName: 'hydrator.list',
       stateParams: {
@@ -140,7 +156,6 @@ export default class Header extends Component {
       }
     });
     let isPipelinesViewActive = location.pathname.indexOf('/pipelines/') !== -1;
-
 
     return (
       <div className="global-navbar">
@@ -191,6 +206,15 @@ export default class Header extends Component {
             >
               {T.translate('features.Navbar.pipelinesLabel')}
             </a>
+          </li>
+          <li>
+            <NavLinkWrapper
+              isNativeLink={this.props.nativeLink}
+              to={this.props.nativeLink ? `/cdap${mmdsurl}` : mmdsurl}
+              isActive={this.isMMDSActive}
+              >
+              {T.translate(`features.Navbar.MMDS`)}
+            </NavLinkWrapper>
           </li>
           <li>
               <NavLinkWrapper
