ca74410c0a02628ad61998a8b89f190a07f82f92
Ajai <ajainarayanan@gmail.com>
2017-10-24 16:32:08 -0700

    Adds a minimal piechart component. This is because vega-lite doesn't come with pie chart yet

---
+++ cdap-ui/app/cdap/components/PieChart/index.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright © 2017 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+import d3 from 'd3';
+import shortid from 'shortid';
+import isNil from 'lodash/isNil';
+
+export default class PieChart extends Component {
+  propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape({
+      color: PropTypes.string,
+      value: PropTypes.string
+    })),
+    width: PropTypes.number,
+    height: PropTypes.number
+  };
+  state = {
+    data: this.props.data,
+    id: shortid.generate()
+  };
+  componentDidMount() {
+    this.drawPie();
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({data: nextProps.data});
+  }
+  drawPie() {
+    if (isNil(this.state.data) || (Array.isArray(this.state.data) && !this.state.data.length)) {
+      return;
+    }
+    var svg = d3.select(`#${this.state.id} svg`),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        radius = Math.min(width, height) / 2,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.count; });
+
+    var path = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+
+    var arc = g.selectAll(".arc")
+        .data(pie(this.state.data))
+        .enter().append("g")
+          .attr("class", "arc");
+
+    arc.append('title')
+      .text((d) => `${d.data.value} (${d.data.count})`);
+    arc.append("path")
+        .attr("d", path)
+        .attr("fill", (d) => d.data.color);
+  }
+  render() {
+    return (
+      <div id={this.state.id}> <svg width={this.props.width || "50"} height={this.props.height || "50"}></svg></div>
+    );
+  }
+}
