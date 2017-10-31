373294f05dccb36e5d2245347254136994749be7
Terence Yim <terence@cask.co>
2017-10-30 21:31:23 -0700

    Fix doc MD5

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="096dbf2caf5650aaba8d5f1295a07258"
+DEFAULT_XML_MD5_HASH="e42bac30d3bec69fc4bf3b99e4f54271"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
