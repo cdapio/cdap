cfa4a5837eb922d88742d3fc5a594e7d1cd86b2b
Chengfeng <mao@cask.co>
2018-03-21 17:50:00 -0700

    Add classes representing data in HTTP request/response and data used in Spark SQL calculation

---
+++ cdap-app-templates/cdap-program-report/src/main/java/co/cask/cdap/report/proto/ReportGenerationInfo.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2018 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.report.proto;
+
+/**
+ * Represents the information of a report generation in an HTTP response.
+ */
+public class ReportGenerationInfo {
+  private final long created;
+  private final ReportStatus status;
+  private final String request;
+
+  public ReportGenerationInfo(long created, ReportStatus status, String request) {
+    this.created = created;
+    this.status = status;
+    this.request = request;
+  }
+
+  /**
+   * @return the time when this report is created
+   */
+  public long getCreated() {
+    return created;
+  }
+
+  /**
+   * @return the report generation status
+   */
+  public ReportStatus getStatus() {
+    return status;
+  }
+
+  /**
+   * @return the request for generating this report
+   */
+  public String getRequest() {
+    return request;
+  }
+}
