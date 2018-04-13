4085f1b261f03e3d94b13f5d2753f5c2f30c7d7f
Derek Wood <derek@cask.co>
2018-04-12 17:43:38 -0700

    [CDAP-13308] update EMR installer to use dedicated emr tag

--- cdap-distributions/src/emr/install.sh
+++ cdap-distributions/src/emr/install.sh
@@ -24,7 +24,7 @@ CDAP_BRANCH=${CDAP_BRANCH:-release/4.2}
 # Optional tag to checkout - All released versions of this script should set this
 # like this: CDAP_TAG=${CDAP_TAG:+tag} as this allows setting tag to empty/null
 # otherwise, it should be CDAP_TAG=''
-CDAP_TAG=${CDAP_TAG:+v4.2.1}
+CDAP_TAG=${CDAP_TAG:+emr4.2}
 # The CDAP package version passed to Chef
 CDAP_VERSION=${CDAP_VERSION:-4.2.1-1}
 # The version of Chef to install
