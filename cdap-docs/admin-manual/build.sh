6ced6877ff5d4637ac26366643d9d2a9a5ba0f17
sreevatsanraman <sreevatsan@gmail.com>
2018-06-04 10:56:52 -0700

    (CDAP-13409) Change SDK default version to spark 2

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="e49c16a4b85482ba14e19fc99cd03798"
+DEFAULT_XML_MD5_HASH="1fb3b315cebee520b82de26d458728e3"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
