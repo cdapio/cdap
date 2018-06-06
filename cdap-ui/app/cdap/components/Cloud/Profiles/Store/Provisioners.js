57521fe82f54a49df4f45dd93badb4c2abc7f88b
Ajai <ajainarayanan@gmail.com>
2018-06-06 13:02:58 -0700

    [CDAP-13455][CDAP-13515] Fixes choosing profile for schedule + fixes the "save" button behavior in pipeline configure step
     - Adds a provisioner store to be reused across all profile references
     - Fixes Profiles DetailedView to use Provisioners Store
     - Fixes Profiles ListView to use Provisioners Store
     - Fixes ProfilesListView in pipelines to use Provisioner Store:
     - Fixes PipelineScheduler to use Provisioner Store + to show the right provisoner label for a selected profile
     - Removes the unused ProfilesListView
     - Fixes AbstractWidget's StateWrapper to allow accepting a function for value to get upto date value on each re-render
     - Modifies ProfileCustomization popover to save proper values on open/close accordion panes
     - Fixes pipeline scheduler to close profiles dropdown on select
     - Removes saving profiles to schedule from profile customize popover
     - Removes the restriction of saving anything in pipeline configuration modeless
     - Fixes getProvisionerMap api to be more concrete to remove unnecessary code duplication during its usage
     - Removes check for empty run time arguments before saving it to preference store
     - Fixed onhover styling for profiles table view

---
+++ cdap-ui/app/cdap/components/Cloud/Profiles/Store/Provisioners.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright © 2018 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+
+import {createStore} from 'redux';
+import {defaultAction, composeEnhancers} from 'services/helpers';
+import {MyCloudApi} from 'api/cloud';
+import isEmpty from 'lodash/isEmpty';
+import {Observable} from 'rxjs/Observable';
+
+const ACTIONS = {
+  SAVE_PROVISIONERS: 'SAVE_PROVISIONERS'
+};
+const DEFAULT_STATE = {
+  nameToLabelMap: {},
+  list: []
+};
+
+export const fetchProvisioners = () => {
+  let returnObservable$ = MyCloudApi.getProvisioners();
+  returnObservable$
+    .subscribe(
+      provisioners => {
+        store.dispatch({
+          type: ACTIONS.SAVE_PROVISIONERS,
+          payload: {
+            provisioners
+          }
+        });
+      },
+      err => {
+        console.log('Fetching provisioners failed: ', err);
+      }
+    );
+  return returnObservable$;
+};
+
+const provisioners = (state = DEFAULT_STATE, action = defaultAction) => {
+  switch (action.type) {
+    case ACTIONS.SAVE_PROVISIONERS: {
+      let {provisioners} = action.payload;
+      let provisionersMap = {};
+      provisioners.forEach(provisioner => {
+        provisionersMap[provisioner.name] = provisioner.label;
+      });
+      return {
+        nameToLabelMap: provisionersMap,
+        list: provisioners
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export const getProvisionersMap = () => {
+  let {list} = store.getState();
+  const observableReturn = () => Observable.create(observer => {
+    observer.next(store.getState());
+  });
+  if (isEmpty(list)) {
+    return fetchProvisioners().flatMap(observableReturn);
+  } else {
+    return observableReturn();
+  }
+};
+
+const store = createStore(
+  provisioners,
+  DEFAULT_STATE,
+  composeEnhancers('ProvisionersMapStore')()
+);
+
+export default store;
