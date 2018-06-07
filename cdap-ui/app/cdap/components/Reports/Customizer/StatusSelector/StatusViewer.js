9e57438ab896c9162f1d8e3347117886a2b1a959
tonybach <tonybach@google.com>
2018-06-07 13:13:59 -0700

    [CDAP-13457][CDAP-13470][CDAP-13476] Bugfixes/improvements in Reports UI - Fixes Popover component closing on every click - Changes how we display runtime arguments in reports detail page - Still reverts back to previous statuses when user clicks outside, but displays the statuses as user selects them, and changes Apply link to button - Makes runtime args column fixed width and displays in one line, and adds title attributes to all table headers and cells - Fixes so that gear popover on a report shows up correctly - Don't show effect when hovering over grid row in reports detailed table

--- cdap-ui/app/cdap/components/Reports/Customizer/StatusSelector/StatusViewer.js
+++ cdap-ui/app/cdap/components/Reports/Customizer/StatusSelector/StatusViewer.js
@@ -17,13 +17,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IconSVG from 'components/IconSVG';
-import {connect} from 'react-redux';
+import {STATUS_OPTIONS} from 'components/Reports/store/ReportsStore';
 
-function StatusViewerView({selections}) {
+function StatusViewer(selections) {
   let text = 'Select one';
+  let numSelections = selections.length;
 
-  if (selections.length > 0) {
-    text = selections.join(', ');
+  if (numSelections > 0) {
+    if (numSelections === STATUS_OPTIONS.length) {
+      text = 'All statuses';
+    } else {
+      text = selections.join(', ');
+      if (numSelections > 1) {
+        text = `(${numSelections}) ` + text;
+      }
+    }
   }
 
   return (
@@ -39,18 +47,8 @@ function StatusViewerView({selections}) {
   );
 }
 
-StatusViewerView.propTypes = {
+StatusViewer.propTypes = {
   selections: PropTypes.array
 };
-
-const mapStateToProps = (state) => {
-  return {
-    selections: state.status.statusSelections
-  };
-};
-
-const StatusViewer = connect(
-  mapStateToProps
-)(StatusViewerView);
 
 export default StatusViewer;
