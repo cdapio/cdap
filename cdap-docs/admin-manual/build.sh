2ac7daeb4e74aa1438cfa0cbdd639576f73c8392
sreevatsan <sreevatsan@sreevatsan-macbookpro2.roam.corp.google.com>
2018-06-13 12:44:42 -0700

    Remove spark version in cdap-default and add it to appropriate tests

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="516960d3839c8c2764916963db738f24"
+DEFAULT_XML_MD5_HASH="f0556b82dab3226e10c455981ada9624"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
