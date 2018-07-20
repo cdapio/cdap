d1f5bc1ad79dd3785187b7d6bfd16ad9f5ed7c0b
Sree Raman <sreevatsan@sreevatsan-macbookpro2.roam.corp.google.com>
2018-07-19 23:38:00 -0700

    Deprecate flow CLI commands

--- cdap-cli/src/main/java/co/cask/cdap/cli/command/GetProgramRuntimeArgsCommand.java
+++ cdap-cli/src/main/java/co/cask/cdap/cli/command/GetProgramRuntimeArgsCommand.java
@@ -61,6 +61,13 @@ public class GetProgramRuntimeArgsCommand extends AbstractAuthCommand {
 
   @Override
   public String getDescription() {
-    return String.format("Gets the runtime arguments of %s", Fragment.of(Article.A, elementType.getName()));
+    switch(elementType) {
+      case FLOW:
+        return String.format("Gets the runtime arguments of %s. Flows are deprecated as of release 5.0," +
+                             " use SparkStreaming as a replacement technology.",
+                             Fragment.of(Article.A, elementType.getName()));
+      default:
+        return String.format("Gets the runtime arguments of %s", Fragment.of(Article.A, elementType.getName()));
+    }
   }
 }
