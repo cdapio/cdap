a89f1e214698a75cbad9e8a1e5cc9e604dad2cf3
albertshau <ashau@google.com>
2018-09-07 15:05:36 -0700

    update hash for cdap-default to fix docs build

--- cdap-docs/admin-manual/build.sh
+++ cdap-docs/admin-manual/build.sh
@@ -20,7 +20,7 @@ source ../vars
 source ../_common/common-build.sh
 
 DEFAULT_XML="../../cdap-common/src/main/resources/cdap-default.xml"
-DEFAULT_XML_MD5_HASH="522e4f4319651a664b0f2314b9fd2255"
+DEFAULT_XML_MD5_HASH="17ac16da2f865223a1d29c903052c53f"
 
 DEFAULT_TOOL="../tools/cdap-default/doc-cdap-default.py"
 DEFAULT_DEPRECATED_XML="../tools/cdap-default/cdap-default-deprecated.xml"
