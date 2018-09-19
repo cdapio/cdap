03508555fe3304e381db0a3f828d3068e1193f7f
Edwin Elia <edwinelia@google.com>
2018-09-19 14:48:49 -0700

    [CDAP-14327] add indicator for fields with no lineage

--- cdap-ui/app/cdap/components/FieldLevelLineage/index.js
+++ cdap-ui/app/cdap/components/FieldLevelLineage/index.js
@@ -24,6 +24,8 @@ import {objectQuery} from 'services/helpers';
 import EntityTopPanel from 'components/EntityTopPanel';
 import TopPanel from 'components/FieldLevelLineage/TopPanel';
 import T from 'i18n-react';
+import Helmet from 'react-helmet';
+import { Theme } from 'services/ThemeHelper';
 
 const PREFIX = 'commons';
 
@@ -52,12 +54,19 @@ export default class FieldLevelLineage extends Component {
   }
 
   render() {
+    const datasetId = this.props.match.params.datasetId;
     return (
       <Provider store={Store}>
         <div className="field-level-lineage">
+          <Helmet
+            title={T.translate('features.FieldLevelLineage.title', {
+              datasetId: datasetId,
+              productName: Theme.productName,
+            })}
+          />
           <EntityTopPanel
             breadCrumbAnchorLabel={T.translate(`${PREFIX}.back`)}
-            title={this.props.match.params.datasetId}
+            title={datasetId}
             entityType={T.translate(`${PREFIX}.entity.dataset.singular`)}
             entityIcon="icon-datasets"
             historyBack={true}
