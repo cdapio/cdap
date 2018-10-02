70f43d2b8b135dfcc85df4dd69978402b7117101
Sumit Chawla <sumit@orcada.co>
2018-10-02 19:14:07 +0100

    CDAP-13554 | Pipelines with many sinks may exceed the Variable substitution depth in the Hadoop conf

--- cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceContextConfig.java
+++ cdap-app-fabric/src/main/java/co/cask/cdap/internal/app/runtime/batch/MapReduceContextConfig.java
@@ -183,7 +183,7 @@ public final class MapReduceContextConfig {
    * Returns the plugins being used in the MapReduce program.
    */
   public Map<String, Plugin> getPlugins() {
-    String spec = hConf.get(HCONF_ATTR_PLUGINS);
+    String spec = hConf.getRaw(HCONF_ATTR_PLUGINS);
     if (spec == null) {
       return ImmutableMap.of();
     }
