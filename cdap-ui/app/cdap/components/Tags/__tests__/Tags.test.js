b8552dc0adf437af245b3b0091a506cf2744f5d9
Tony Bach <tonybach@google.com>
2018-10-29 14:25:46 -0700

    Uses fireevent instead of moustrap, and prettier changes

--- cdap-ui/app/cdap/components/Tags/__tests__/Tags.test.js
+++ cdap-ui/app/cdap/components/Tags/__tests__/Tags.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { cleanup, render, fireEvent, wait, waitForElement } from 'react-testing-library';
 import Tags from 'components/Tags';
-import Mousetrap from 'mousetrap';
 import 'jest-dom/extend-expect';
 
 afterEach(cleanup);
@@ -10,7 +9,7 @@ jest.mock('api/metadata');
 
 const testEntity = {
   type: 'application',
-  id: 'test'
+  id: 'test',
 };
 
 describe('Tags', () => {
@@ -21,21 +20,26 @@ describe('Tags', () => {
 
   it('adds new tag when user inputs a tag and presses Enter', async () => {
     const TEST_TAG = 'UI_TEST_TAG';
-    const { getByTestId, queryByTestId, getByText, container } = render(<Tags entity={testEntity} />);
+    const { getByTestId, queryByTestId, getByText, container } = render(
+      <Tags entity={testEntity} />
+    );
     await wait(() => !queryByTestId('loading-icon')); // wait until loading icon is gone, which means async call is done
 
     const plusButtonElem = getByTestId('tag-plus-button');
-    fireEvent.click(plusButtonElem, {container}); // click on plus button to show input box
+    fireEvent.click(plusButtonElem, { container }); // click on plus button to show input box
     await waitForElement(() => getByTestId('tag-input'));
 
     const inputElem = getByTestId('tag-input');
     expect(inputElem).toHaveTextContent('');
-    fireEvent.change(inputElem, {target: {value: TEST_TAG}});
-    Mousetrap.trigger('return'); // enter value, and then press Enter to add tag
+    fireEvent.change(inputElem, { target: { value: TEST_TAG } });
+    fireEvent.keyDown(inputElem, {
+      key: 'Enter',
+      keyCode: 13,
+      which: 13,
+    }); // enter value, and then press Enter to add tag
     await waitForElement(() => getByText(TEST_TAG));
 
     expect(inputElem).not.toBeInTheDocument(); // input elem is hidden after user presses Enter
     expect(getByText(TEST_TAG)).toBeVisible();
   });
 });
-
