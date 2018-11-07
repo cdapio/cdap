f1b04763c5ebaf4a1e101ebee6f0d91ccbc3a04a
Tony Bach <tonybach@google.com>
2018-11-07 11:53:33 -0800

    Debugging

--- cdap-ui/cypress/integration/pipeline.spec.js
+++ cdap-ui/cypress/integration/pipeline.spec.js
@@ -24,10 +24,10 @@ function getArtifactsPoll() {
     method: 'GET',
     url: `http://${Cypress.env('host')}:11015/v3/namespaces/default/artifacts?scope=SYSTEM`,
     failOnStatusCode: false,
-  }).then((response) => {
-    if (response.status >= 400) {
-      getArtifactsPoll();
-    }
+  // }).then((response) => {
+  //   if (response.status >= 400) {
+  //     getArtifactsPoll();
+  //   }
   });
 }
 
