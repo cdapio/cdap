cbbb4f57d79e36f6112b58fe6e0b1eb86ddedf24
anew <aneu@google.com>
2018-12-18 11:04:09 -0800

    fix more unit tests

--- cdap-notifications/src/test/java/co/cask/cdap/notifications/NotificationTest.java
+++ cdap-notifications/src/test/java/co/cask/cdap/notifications/NotificationTest.java
@@ -36,6 +36,8 @@ import co.cask.cdap.data.runtime.DataSetsModules;
 import co.cask.cdap.data2.datafabric.dataset.service.DatasetService;
 import co.cask.cdap.data2.datafabric.dataset.service.executor.DatasetOpExecutor;
 import co.cask.cdap.data2.dataset2.DatasetFramework;
+import co.cask.cdap.data2.metadata.writer.MetadataPublisher;
+import co.cask.cdap.data2.metadata.writer.NoOpMetadataPublisher;
 import co.cask.cdap.explore.guice.ExploreClientModule;
 import co.cask.cdap.messaging.MessagingService;
 import co.cask.cdap.messaging.guice.MessagingServerRuntimeModule;
@@ -148,12 +150,13 @@ public abstract class NotificationTest {
         protected void configure() {
           bind(UGIProvider.class).to(UnsupportedUGIProvider.class);
           bind(OwnerAdmin.class).to(NoOpOwnerAdmin.class);
+          bind(MetadataPublisher.class).to(NoOpMetadataPublisher.class);
         }
       }
     );
   }
 
-  public static void startServices(Injector injector) throws Exception {
+  public static void startServices(Injector injector) {
     messagingService = injector.getInstance(MessagingService.class);
     if (messagingService instanceof Service) {
       ((Service) messagingService).startAndWait();
