ea02799ba56fac0d8449c31c493dd4dda0d2b5d3
Anuj Kanojia <anuj.kanojia@guavus.com>
2019-01-07 14:01:57 +0530

    RAF-69:  Merge FS changes

--- cdap-ui/app/cdap/components/FeatureUI/FeatureSelection/GridContainer/index.js
+++ cdap-ui/app/cdap/components/FeatureUI/FeatureSelection/GridContainer/index.js
@@ -1,8 +1,9 @@
+/* eslint react/prop-types: 0 */
 import React, { Component } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-import './GridContainer.scss'
+import './GridContainer.scss';
 
 class GridContainer extends Component {
     constructor(props) {
@@ -11,31 +12,16 @@ class GridContainer extends Component {
         this.state = {
           columnDefs:this.props.gridColums,
           rowData:this.props.rowData
-            // columnDefs:
-            // [
-            //     {headerName: "Make", field: "make", width: 250, checkboxSelection: true},
-            //     {headerName: "Model", field: "model"},
-            //     {headerName: "Price", field: "price"}
-
-            // ],
-            // rowData: [
-            //     {make: "Toyota", model: "Celica", price: 35000},
-            //     {make: "Ford", model: "Mondeo", price: 32000},
-            //     {make: "Porsche", model: "Boxter", price: 72000}
-            // ]
-        }
+        };
     }
 
     refreshGridColumns = (data) => {
-
-
+      console.log(data);
     }
 
     onSelectionChanged = (data) => {
-      if(isnil)
-
       alert("I am an alert box!");
-
+      console.log(data);
     }
 
     render() {
