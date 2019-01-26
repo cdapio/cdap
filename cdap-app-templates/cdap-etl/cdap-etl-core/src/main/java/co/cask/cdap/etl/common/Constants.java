62405754a7a3652f1646e65c00d0a538a02f6c59
albertshau <ashau@google.com>
2019-01-25 16:34:02 -0800

    CDAP-13326 pipeline syntax cleanup
    remove unused code and some syntactical updates to remove warnings

--- cdap-app-templates/cdap-etl/cdap-etl-core/src/main/java/co/cask/cdap/etl/common/Constants.java
+++ cdap-app-templates/cdap-etl/cdap-etl-core/src/main/java/co/cask/cdap/etl/common/Constants.java
@@ -16,8 +16,6 @@
 
 package co.cask.cdap.etl.common;
 
-import co.cask.cdap.api.data.schema.Schema;
-
 /**
  * Constants used in ETL Applications.
  */
@@ -25,19 +23,11 @@ public final class Constants {
   public static final String ID_SEPARATOR = ":";
   public static final String PIPELINEID = "pipeline";
   public static final String PIPELINE_SPEC_KEY = "pipeline.spec";
-  public static final String PIPELINE_PLAN = "pipeline.plan";
   public static final String STAGE_LOGGING_ENABLED = "stage.logging.enabled";
   public static final String EVENT_TYPE_TAG = "MDC:eventType";
   public static final String PIPELINE_LIFECYCLE_TAG_VALUE = "lifecycle";
   public static final String SPARK_PROGRAM_PLUGIN_TYPE = "sparkprogram";
   public static final String CONNECTOR_DATASETS = "connector.datasets";
-  public static final Schema ERROR_SCHEMA = Schema.recordOf(
-    "error",
-    Schema.Field.of(ErrorDataset.ERRCODE, Schema.of(Schema.Type.INT)),
-    Schema.Field.of(ErrorDataset.ERRMSG, Schema.unionOf(Schema.of(Schema.Type.STRING),
-                                                                  Schema.of(Schema.Type.NULL))),
-    Schema.Field.of(ErrorDataset.INVALIDENTRY, Schema.of(Schema.Type.STRING))
-  );
   public static final String MDC_STAGE_KEY = "pipeline.stage";
   public static final String FIELD_OPERATION_KEY_IN_WORKFLOW_TOKEN = "field.operations";
   public static final String SPARK_PIPELINE_AUTOCACHE_ENABLE_FLAG = "spark.cdap.pipeline.autocache.enable";
@@ -56,16 +46,6 @@ public final class Constants {
     public static final String SOURCE_TYPE = "source";
     public static final String SINK_TYPE = "sink";
     public static final String DATA_DIR = "data";
-  }
-
-  /**
-   * Constants related to error dataset used in transform
-   */
-  public static final class ErrorDataset {
-    public static final String ERRCODE = "errCode";
-    public static final String ERRMSG = "errMsg";
-    public static final String TIMESTAMP = "errTimestamp";
-    public static final String INVALIDENTRY = "invalidRecord";
   }
 
   /**
