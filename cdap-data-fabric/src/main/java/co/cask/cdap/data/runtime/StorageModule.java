adityadivami/cdap-ui-forked

CDAP-14673 Create a NoSQL schema registry

cdap-data-fabric/src/main/java/co/cask/cdap/data/runtime/StorageModule.java
@@ -23,6 +23,7 @@ import co.cask.cdap.common.conf.SConfiguration;
 import co.cask.cdap.data2.nosql.NoSqlStructuredTableAdmin;
 import co.cask.cdap.data2.nosql.NoSqlTransactionRunner;
 import co.cask.cdap.spi.data.StructuredTableAdmin;
+import co.cask.cdap.spi.data.table.StructuredTableRegistry;
 import co.cask.cdap.spi.data.transaction.TransactionRunner;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -40,8 +41,10 @@ public class StorageModule extends PrivateModule {
   protected void configure() {
     bind(TransactionRunner.class).toProvider(TransactionRunnerProvider.class);
     bind(StructuredTableAdmin.class).toProvider(StructuredTableAdminProvider.class);
+    bind(StructuredTableRegistry.class).toProvider(StructuredTableRegistryProvider.class);
     expose(TransactionRunner.class);
     expose(StructuredTableAdmin.class);
+    expose(StructuredTableRegistry.class);
   }
 
   /**
@@ -112,6 +115,44 @@ public class StorageModule extends PrivateModule {
       storageImpl = storageImpl.toLowerCase();
       if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_NOSQL)) {
         return injector.getInstance(NoSqlStructuredTableAdmin.class);
+      }
+      if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_SQL)) {
+        // TODO: CDAP-14780, connect to the sql using the connection, user name and password from the sConf
+        return null;
+      }
+      throw new UnsupportedOperationException(
+        String.format("%s is not a supported storage implementation, the supported implementations are %s and %s",
+                      storageImpl, Constants.Dataset.DATA_STORAGE_NOSQL, Constants.Dataset.DATA_STORAGE_SQL));
+    }
+  }
+
+  /**
+   * Structure table registry provider to provide the {@link StructuredTableRegistry} class, the actual implementation
+   * will be based on the configuration file.
+   */
+  @Singleton
+  private static final class StructuredTableRegistryProvider implements Provider<StructuredTableRegistry> {
+    private final CConfiguration cConf;
+    private final SConfiguration sConf;
+    private final Injector injector;
+
+    @Inject
+    StructuredTableRegistryProvider(CConfiguration cConf, SConfiguration sConf, Injector injector) {
+      this.cConf = cConf;
+      this.sConf = sConf;
+      this.injector = injector;
+    }
+
+    @Override
+    public StructuredTableRegistry get() {
+      String storageImpl = cConf.get(Constants.Dataset.DATA_STORAGE_IMPLEMENTATION);
+      if (storageImpl == null) {
+        throw new IllegalStateException("No storage implementation is specified in the configuration file");
+      }
+
+      storageImpl = storageImpl.toLowerCase();
+      if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_NOSQL)) {
+        return injector.getInstance(StructuredTableRegistry.class);
       }
       if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_SQL)) {
         // TODO: CDAP-14780, connect to the sql using the connection, user name and password from the sConf

cdap-data-fabric/src/test/java/co/cask/cdap/data2/nosql/NoSqlStructuredTableRegistryTest.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.nosql;
+
+import co.cask.cdap.data2.dataset2.DatasetFrameworkTestUtil;
+import co.cask.cdap.spi.data.table.StructuredTableRegistry;
+import co.cask.cdap.spi.data.table.StructuredTableRegistryTest;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.tephra.TransactionManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+
+/**
+ *
+ */
+public class NoSqlStructuredTableRegistryTest extends StructuredTableRegistryTest {
+  @ClassRule
+  public static DatasetFrameworkTestUtil dsFrameworkUtil = new DatasetFrameworkTestUtil();
+
+  private static TransactionManager txManager;
+
+  @Override
+  protected StructuredTableRegistry getStructuredTableSpecificationRegistry() {
+    return dsFrameworkUtil.getInjector().getInstance(NoSqlStructuredTableRegistry.class);
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    Configuration txConf = HBaseConfiguration.create();
+    txManager = new TransactionManager(txConf);
+    txManager.startAndWait();
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    if (txManager != null) {
+      txManager.stopAndWait();
+    }
+  }
+}

cdap-storage-spi/src/test/java/co/cask/cdap/spi/data/table/StructuredTableRegistryTest.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.spi.data.table;
+
+import co.cask.cdap.spi.data.TableAlreadyExistsException;
+import co.cask.cdap.spi.data.table.field.FieldType;
+import co.cask.cdap.spi.data.table.field.Fields;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+public abstract class StructuredTableRegistryTest {
+
+  protected abstract StructuredTableRegistry getStructuredTableSpecificationRegistry();
+
+  @Test
+  public void testRegistry() throws Exception {
+    StructuredTableRegistry registry = getStructuredTableSpecificationRegistry();
+    registry.initialize();
+
+    StructuredTableId tableId1 = new StructuredTableId("table1");
+    FieldType key = Fields.intType("i");
+    FieldType str = Fields.stringType("s");
+    FieldType lng = Fields.longType("l");
+    StructuredTableSpecification spec1 = new StructuredTableSpecification.Builder()
+      .withId(tableId1)
+      .withFields(key, str, lng)
+      .withPrimaryKeys(key.getName())
+      .withIndexes(str.getName())
+      .build();
+
+    // Assert empty
+    Assert.assertTrue(registry.isEmpty());
+    Assert.assertNull(registry.getSpecification(tableId1));
+
+    // Register table1
+    registry.registerSpecification(spec1);
+    Assert.assertEquals(spec1, registry.getSpecification(tableId1));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Register table2
+    StructuredTableId tableId2 = new StructuredTableId("table2");
+    StructuredTableSpecification spec2 = new StructuredTableSpecification.Builder()
+      .withId(tableId2)
+      .withFields(key, str, lng, Fields.floatType("f"))
+      .withPrimaryKeys(key.getName())
+      .withIndexes(lng.getName())
+      .build();
+
+    registry.registerSpecification(spec2);
+    Assert.assertEquals(spec2, registry.getSpecification(tableId2));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Re-registering table1 should fail
+    try {
+      registry.registerSpecification(spec1);
+      Assert.fail("Expected re-registration to fail");
+    } catch (TableAlreadyExistsException e) {
+      // Expected
+    }
+
+    // Remove spec for table1, and try again to register
+    registry.removeSpecification(tableId1);
+    Assert.assertNull(registry.getSpecification(tableId1));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Now re-register table1
+    registry.registerSpecification(spec1);
+    Assert.assertEquals(spec1, registry.getSpecification(tableId1));
+
+    // Delete both the specs
+    registry.removeSpecification(tableId1);
+    registry.removeSpecification(tableId2);
+    Assert.assertTrue(registry.isEmpty());
+  }
+}

cdapio/cdap

cdap-data-fabric/src/main/java/co/cask/cdap/data/runtime/StorageModule.java
@@ -21,8 +21,10 @@ import co.cask.cdap.common.conf.CConfiguration;
 import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.conf.SConfiguration;
 import co.cask.cdap.data2.nosql.NoSqlStructuredTableAdmin;
+import co.cask.cdap.data2.nosql.NoSqlStructuredTableRegistry;
 import co.cask.cdap.data2.nosql.NoSqlTransactionRunner;
 import co.cask.cdap.spi.data.StructuredTableAdmin;
+import co.cask.cdap.spi.data.table.StructuredTableRegistry;
 import co.cask.cdap.spi.data.transaction.TransactionRunner;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
@@ -40,8 +42,10 @@ public class StorageModule extends PrivateModule {
   protected void configure() {
     bind(TransactionRunner.class).toProvider(TransactionRunnerProvider.class);
     bind(StructuredTableAdmin.class).toProvider(StructuredTableAdminProvider.class);
+    bind(StructuredTableRegistry.class).toProvider(StructuredTableRegistryProvider.class);
     expose(TransactionRunner.class);
     expose(StructuredTableAdmin.class);
+    expose(StructuredTableRegistry.class);
   }
 
   /**
@@ -112,6 +116,44 @@ public class StorageModule extends PrivateModule {
       storageImpl = storageImpl.toLowerCase();
       if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_NOSQL)) {
         return injector.getInstance(NoSqlStructuredTableAdmin.class);
+      }
+      if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_SQL)) {
+        // TODO: CDAP-14780, connect to the sql using the connection, user name and password from the sConf
+        return null;
+      }
+      throw new UnsupportedOperationException(
+        String.format("%s is not a supported storage implementation, the supported implementations are %s and %s",
+                      storageImpl, Constants.Dataset.DATA_STORAGE_NOSQL, Constants.Dataset.DATA_STORAGE_SQL));
+    }
+  }
+
+  /**
+   * Structure table registry provider to provide the {@link StructuredTableRegistry} class, the actual implementation
+   * will be based on the configuration file.
+   */
+  @Singleton
+  private static final class StructuredTableRegistryProvider implements Provider<StructuredTableRegistry> {
+    private final CConfiguration cConf;
+    private final SConfiguration sConf;
+    private final Injector injector;
+
+    @Inject
+    StructuredTableRegistryProvider(CConfiguration cConf, SConfiguration sConf, Injector injector) {
+      this.cConf = cConf;
+      this.sConf = sConf;
+      this.injector = injector;
+    }
+
+    @Override
+    public StructuredTableRegistry get() {
+      String storageImpl = cConf.get(Constants.Dataset.DATA_STORAGE_IMPLEMENTATION);
+      if (storageImpl == null) {
+        throw new IllegalStateException("No storage implementation is specified in the configuration file");
+      }
+
+      storageImpl = storageImpl.toLowerCase();
+      if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_NOSQL)) {
+        return injector.getInstance(NoSqlStructuredTableRegistry.class);
       }
       if (storageImpl.equals(Constants.Dataset.DATA_STORAGE_SQL)) {
         // TODO: CDAP-14780, connect to the sql using the connection, user name and password from the sConf

cdap-data-fabric/src/test/java/co/cask/cdap/data2/nosql/NoSqlStructuredTableRegistryTest.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.data2.nosql;
+
+import co.cask.cdap.data2.dataset2.DatasetFrameworkTestUtil;
+import co.cask.cdap.spi.data.table.StructuredTableRegistry;
+import co.cask.cdap.spi.data.table.StructuredTableRegistryTest;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.tephra.TransactionManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+
+/**
+ *
+ */
+public class NoSqlStructuredTableRegistryTest extends StructuredTableRegistryTest {
+  @ClassRule
+  public static DatasetFrameworkTestUtil dsFrameworkUtil = new DatasetFrameworkTestUtil();
+
+  private static TransactionManager txManager;
+
+  @Override
+  protected StructuredTableRegistry getStructuredTableRegistry() {
+    return dsFrameworkUtil.getInjector().getInstance(NoSqlStructuredTableRegistry.class);
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    Configuration txConf = HBaseConfiguration.create();
+    txManager = new TransactionManager(txConf);
+    txManager.startAndWait();
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    if (txManager != null) {
+      txManager.stopAndWait();
+    }
+  }
+}

cdap-storage-spi/src/test/java/co/cask/cdap/spi/data/table/StructuredTableRegistryTest.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package co.cask.cdap.spi.data.table;
+
+import co.cask.cdap.spi.data.TableAlreadyExistsException;
+import co.cask.cdap.spi.data.table.field.FieldType;
+import co.cask.cdap.spi.data.table.field.Fields;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for {@link StructuredTableRegistry}.
+ */
+public abstract class StructuredTableRegistryTest {
+  /**
+   * @return the right implementation of the registry based on the underlying storage.
+   */
+  protected abstract StructuredTableRegistry getStructuredTableRegistry();
+
+  @Test
+  public void testRegistry() throws Exception {
+    StructuredTableRegistry registry = getStructuredTableRegistry();
+    registry.initialize();
+
+    StructuredTableId tableId1 = new StructuredTableId("table1");
+    FieldType key = Fields.intType("i");
+    FieldType str = Fields.stringType("s");
+    FieldType lng = Fields.longType("l");
+    StructuredTableSpecification spec1 = new StructuredTableSpecification.Builder()
+      .withId(tableId1)
+      .withFields(key, str, lng)
+      .withPrimaryKeys(key.getName())
+      .withIndexes(str.getName())
+      .build();
+
+    // Assert empty
+    Assert.assertTrue(registry.isEmpty());
+    Assert.assertNull(registry.getSpecification(tableId1));
+
+    // Register table1
+    registry.registerSpecification(spec1);
+    Assert.assertEquals(spec1, registry.getSpecification(tableId1));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Register table2
+    StructuredTableId tableId2 = new StructuredTableId("table2");
+    StructuredTableSpecification spec2 = new StructuredTableSpecification.Builder()
+      .withId(tableId2)
+      .withFields(key, str, lng, Fields.floatType("f"))
+      .withPrimaryKeys(key.getName())
+      .withIndexes(lng.getName())
+      .build();
+
+    registry.registerSpecification(spec2);
+    Assert.assertEquals(spec2, registry.getSpecification(tableId2));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Re-registering table1 should fail
+    try {
+      registry.registerSpecification(spec1);
+      Assert.fail("Expected re-registration to fail");
+    } catch (TableAlreadyExistsException e) {
+      // Expected
+    }
+
+    // Remove spec for table1, and try again to register
+    registry.removeSpecification(tableId1);
+    Assert.assertNull(registry.getSpecification(tableId1));
+    Assert.assertFalse(registry.isEmpty());
+
+    // Now re-register table1
+    registry.registerSpecification(spec1);
+    Assert.assertEquals(spec1, registry.getSpecification(tableId1));
+
+    // Delete both the specs
+    registry.removeSpecification(tableId1);
+    registry.removeSpecification(tableId2);
+    Assert.assertTrue(registry.isEmpty());
+  }
+}