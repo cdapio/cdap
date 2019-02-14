6982f730dd63d58a487190c4bdd2fc48fb0c3e86
vinisha <vinishashah@google.com>
2019-02-14 12:28:26 -0800

    CDAP-14882 Adding concurrent writer to handle concurrent writes to log buffer

--- cdap-watchdog/src/test/java/co/cask/cdap/logging/logbuffer/ConcurrentLogBufferWriterTest.java
+++ cdap-watchdog/src/test/java/co/cask/cdap/logging/logbuffer/ConcurrentLogBufferWriterTest.java
@@ -19,7 +19,8 @@ package co.cask.cdap.logging.logbuffer;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
-import co.cask.cdap.api.common.Bytes;
+import co.cask.cdap.common.conf.CConfiguration;
+import co.cask.cdap.common.conf.Constants;
 import co.cask.cdap.common.logging.LoggingContext;
 import co.cask.cdap.logging.appender.LogMessage;
 import co.cask.cdap.logging.context.WorkerLoggingContext;
@@ -29,43 +30,40 @@ import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Iterator;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
- * Tests for {@link LogBufferWriter}.
+ * Tests for {@link ConcurrentLogBufferWriter}.
  */
-public class LogBufferWriterTest {
-  private final LoggingEventSerializer serializer = new LoggingEventSerializer();
+public class ConcurrentLogBufferWriterTest {
+  private static final Logger LOG = LoggerFactory.getLogger(ConcurrentLogBufferWriterTest.class);
+  private static final LoggingEventSerializer serializer = new LoggingEventSerializer();
 
   @ClassRule
   public static final TemporaryFolder TMP_FOLDER = new TemporaryFolder();
 
   @Test
-  public void testLogBufferWriter() throws Exception {
+  public void testWrites() throws IOException {
+    CConfiguration cConf = CConfiguration.create();
     String absolutePath = TMP_FOLDER.newFolder().getAbsolutePath();
+    cConf.set(Constants.LogBuffer.LOG_BUFFER_BASE_DIR, absolutePath);
+    cConf.setLong(Constants.LogBuffer.LOG_BUFFER_MAX_FILE_SIZE_BYTES, 100000);
 
-    LogBufferWriter writer = new LogBufferWriter(absolutePath, 100000);
+    ConcurrentLogBufferWriter writer = new ConcurrentLogBufferWriter(cConf);
     ImmutableList<byte[]> events = getLoggingEvents();
-    Iterator<LogBufferEvent> writtenEvents = writer.write(events.iterator()).iterator();
-    writer.close();
+    writer.process(new LogBufferRequest(0, events));
 
-    int i = 0;
-    int startPos = 0;
-    // verify if correct offsets were set without file rotation
-    while (writtenEvents.hasNext()) {
-      LogBufferEvent bufferEvent = writtenEvents.next();
-      Assert.assertEquals(bufferEvent.getLogEvent().getMessage(), "" + i++);
-      // There will not be any rotation.
-      Assert.assertEquals(bufferEvent.getOffset().getFilePos(), startPos);
-      startPos = startPos + Bytes.SIZEOF_INT + serializer.toBytes(bufferEvent.getLogEvent()).length;
-    }
-
-    // verify if the events were serialized and written correctly
+    // verify if the events were written to log buffer
     try (DataInputStream dis = new DataInputStream(new FileInputStream(absolutePath + "/0.buf"))) {
       for (byte[] eventBytes : events) {
         ILoggingEvent event = serializer.fromBytes(ByteBuffer.wrap(eventBytes));
@@ -75,33 +73,43 @@ public class LogBufferWriterTest {
   }
 
   @Test
-  public void testFileRotation() throws Exception {
-    // Make sure rotation happens after every event is written
-    LogBufferWriter writer = new LogBufferWriter(TMP_FOLDER.newFolder().getAbsolutePath(), 10);
+  public void testConcurrentWrites() throws Exception {
+    int threadCount = 20;
+
+    CConfiguration cConf = CConfiguration.create();
+    String absolutePath = TMP_FOLDER.newFolder().getAbsolutePath();
+    cConf.set(Constants.LogBuffer.LOG_BUFFER_BASE_DIR, absolutePath);
+    cConf.setLong(Constants.LogBuffer.LOG_BUFFER_MAX_FILE_SIZE_BYTES, 100000);
+
+    ConcurrentLogBufferWriter writer = new ConcurrentLogBufferWriter(cConf);
     ImmutableList<byte[]> events = getLoggingEvents();
-    Iterator<LogBufferEvent> writtenEvents = writer.write(events.iterator()).iterator();
-    writer.close();
 
-    int i = 0;
-    // verify if correct offsets and file id were set with file rotation
-    while (writtenEvents.hasNext()) {
-      LogBufferEvent bufferEvent = writtenEvents.next();
-      Assert.assertEquals(bufferEvent.getLogEvent().getMessage(), "" + i++);
-      // There will be one rotation per event.
-      Assert.assertEquals(bufferEvent.getOffset().getFileId(), i + ".buf");
-      Assert.assertEquals(bufferEvent.getOffset().getFilePos(), 0);
+    ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+    final CyclicBarrier barrier = new CyclicBarrier(threadCount + 1);
+    for (int i = 0; i < threadCount; i++) {
+      executor.submit(() -> {
+        try {
+          barrier.await();
+          writer.process(new LogBufferRequest(0, events));
+        } catch (Exception e) {
+          LOG.error("Exception raised when processing log events.", e);
+        }
+      });
     }
-  }
 
-  @Test (expected = IOException.class)
-  public void testWritesOnClosedWriter() throws IOException {
-    LogBufferWriter writer = new LogBufferWriter(TMP_FOLDER.newFolder().getAbsolutePath(), 100000);
-    writer.close();
-    // should throw IOException
-    writer.write(ImmutableList.of(
-      serializer.toBytes(createLoggingEvent("test.logger", Level.INFO, "0", 1,
-                                            new WorkerLoggingContext("default", "app1", "worker1", "run1",
-                                                                     "instance1")))).iterator());
+    barrier.await();
+    executor.shutdown();
+    Assert.assertTrue(executor.awaitTermination(1, TimeUnit.MINUTES));
+
+    // verify if the events were written to log buffer
+    try (DataInputStream dis = new DataInputStream(new FileInputStream(absolutePath + "/0.buf"))) {
+      for (int i = 0; i < threadCount; i++) {
+        for (byte[] eventBytes : events) {
+          ILoggingEvent event = serializer.fromBytes(ByteBuffer.wrap(eventBytes));
+          Assert.assertEquals(event.getMessage(), getEvent(dis, serializer.toBytes(event).length).getMessage());
+        }
+      }
+    }
   }
 
   private ImmutableList<byte[]> getLoggingEvents() {
