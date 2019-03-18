d1535cdd71fcb740d713ddb32f6c91da46f1cf6e
Anuj Kanojia <anuj.kanojia@guavus.com>
2019-03-18 17:07:26 +0530

    RAF-223: Added header for files

--- cdap-ui/app/cdap/components/FeatureUI/config.js
+++ cdap-ui/app/cdap/components/FeatureUI/config.js
@@ -1,20 +1,20 @@
-import { getEpochDateString } from "./GridFormatters";
-
 /*
- * Copyright © 2018 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+* Copyright © 2018 Cask Data, Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
+
+import { getEpochDateString } from "./GridFormatters";
 export const REMOTE_IP = "http://192.168.156.36:11015";
 const SERVICE_PORT = "11015";
 
