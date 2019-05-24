acfb664e63e281a64c2a16487b1e2ae65714e682
vinisha <vinishashah@google.com>
2019-05-24 11:06:39 -0700

    Bump script versions

--- cdap-distributions/src/hdinsight/pkg/install.sh
+++ cdap-distributions/src/hdinsight/pkg/install.sh
@@ -26,7 +26,7 @@ CDAP_BRANCH='release/6.0'
 # otherwise, it should be CDAP_TAG=''. This should be next version to be released from this branch.
 CDAP_TAG=${CDAP_TAG:+hdi6.0}
 # The CDAP package version passed to Chef. This should be next version to be released from this branch.
-CDAP_VERSION='6.0.0-1'
+CDAP_VERSION='6.0.1-1'
 # The version of Chef to install
 CHEF_VERSION='13.8.5'
 # cdap-site.xml configuration parameters
