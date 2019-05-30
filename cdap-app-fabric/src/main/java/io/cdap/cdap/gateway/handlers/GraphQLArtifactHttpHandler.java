c56b8c2f82ca1fd0ec92167451ec2bdde73b6ad8
Miguel Velez <miguelvelez@google.com>
2019-05-30 09:59:30 -0700

    executed graphql query in get reques

--- cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/GraphQLArtifactHttpHandler.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/GraphQLArtifactHttpHandler.java
@@ -17,13 +17,28 @@
 
 package io.cdap.cdap.gateway.handlers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.graphql.provider.GraphQLProvider;
+import io.cdap.cdap.graphql.store.artifact.ArtifactGraphQLProvider;
+import io.cdap.cdap.graphql.store.artifact.runtimewiring.ArtifactDescriptorTypeRuntimeWiring;
+import io.cdap.cdap.graphql.store.artifact.runtimewiring.ArtifactDetailTypeRuntimeWiring;
+import io.cdap.cdap.graphql.store.artifact.runtimewiring.QueryTypeRuntimeWiring;
+import io.cdap.cdap.internal.io.SchemaTypeAdapter;
 import io.cdap.http.AbstractHttpHandler;
 import io.cdap.http.HttpResponder;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -35,6 +50,24 @@ import javax.ws.rs.PathParam;
 @Path(Constants.Gateway.API_VERSION_3)
 public class GraphQLArtifactHttpHandler extends AbstractHttpHandler {
 
+  private static final Gson GSON = new GsonBuilder().registerTypeAdapter(Schema.class, new SchemaTypeAdapter())
+    .create();
+
+  private final GraphQL graphQL;
+
+  @Inject
+  GraphQLArtifactHttpHandler(QueryTypeRuntimeWiring queryTypeRuntimeWiring,
+                             ArtifactDetailTypeRuntimeWiring artifactDetailTypeRuntimeWiring,
+                             ArtifactDescriptorTypeRuntimeWiring artifactDescriptorTypeRuntimeWiring)
+    throws IOException {
+    String schemaDefinitionFile = "artifactSchema.graphqls";
+    GraphQLProvider graphQLProvider = new ArtifactGraphQLProvider(schemaDefinitionFile,
+                                                                  queryTypeRuntimeWiring,
+                                                                  artifactDetailTypeRuntimeWiring,
+                                                                  artifactDescriptorTypeRuntimeWiring);
+    this.graphQL = graphQLProvider.buildGraphQL();
+  }
+
   /**
    * TODO
    */
@@ -43,7 +76,19 @@ public class GraphQLArtifactHttpHandler extends AbstractHttpHandler {
   public void getArtifacts(HttpRequest request, HttpResponder responder,
                            @PathParam("namespace-id") String namespaceId) {
 
-    responder.sendJson(HttpResponseStatus.OK, "WORK");
+    String query = "{"
+      + "  artifacts {"
+      + "    name"
+      + "    version"
+      + "    scope"
+      + "  }"
+      + "}";
+
+    ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
+    CompletableFuture<ExecutionResult> promise = graphQL.executeAsync(executionInput);
+    ExecutionResult executionResult = promise.join();
+
+    responder.sendJson(HttpResponseStatus.OK, GSON.toJson(executionResult.toSpecification()));
   }
 
 }
