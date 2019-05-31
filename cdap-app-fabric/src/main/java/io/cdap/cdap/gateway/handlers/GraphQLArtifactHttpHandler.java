52d65f68c530fe76d99f60e48dd17ab9d29487cb
Miguel Velez <miguelvelez@google.com>
2019-05-31 10:37:41 -0700

    udpated how to parse queries from the request

--- cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/GraphQLArtifactHttpHandler.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/GraphQLArtifactHttpHandler.java
@@ -19,37 +19,27 @@ package io.cdap.cdap.gateway.handlers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import io.cdap.cdap.api.data.schema.Schema;
-import io.cdap.cdap.common.BadRequestException;
 import io.cdap.cdap.common.conf.Constants;
 import io.cdap.cdap.graphql.provider.GraphQLProvider;
 import io.cdap.cdap.graphql.store.artifact.ArtifactGraphQLProvider;
 import io.cdap.cdap.graphql.store.artifact.runtimewiring.ArtifactTypeRuntimeWiring;
 import io.cdap.cdap.graphql.store.artifact.runtimewiring.QueryTypeRuntimeWiring;
-import io.cdap.cdap.internal.guava.reflect.TypeToken;
 import io.cdap.cdap.internal.io.SchemaTypeAdapter;
 import io.cdap.http.AbstractHttpHandler;
 import io.cdap.http.HttpResponder;
-import io.netty.buffer.ByteBufInputStream;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 
 /**
  * TODO
@@ -76,27 +66,12 @@ public class GraphQLArtifactHttpHandler extends AbstractHttpHandler {
 
   @POST
   @Path("/graphql")
-  public void queryGraphQL(FullHttpRequest request, HttpResponder responder) throws BadRequestException, IOException {
-    // Parse graphql query
-    StringBuilder stringBuilder = new StringBuilder();
+  public void queryGraphQL(FullHttpRequest request, HttpResponder responder) {
+    String query = request.content().toString(StandardCharsets.UTF_8);
 
-    try (Reader reader = new InputStreamReader(new ByteBufInputStream(request.content()), StandardCharsets.UTF_8)) {
-      char[] charBuffer = new char[128];
-      int bytesRead;
-      while ((bytesRead = reader.read(charBuffer)) > 0) {
-        stringBuilder.append(charBuffer, 0, bytesRead);
-      }
-    } catch (JsonSyntaxException e) {
-      throw new BadRequestException("Unable to parse request: " + e.getMessage(), e);
-    }
-
-    String query = stringBuilder.toString();
-
-    // Execute query
     ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
     ExecutionResult executionResult = graphQL.execute(executionInput);
 
-    // Return results
     responder.sendJson(HttpResponseStatus.OK, GSON.toJson(executionResult.toSpecification()));
   }
 
