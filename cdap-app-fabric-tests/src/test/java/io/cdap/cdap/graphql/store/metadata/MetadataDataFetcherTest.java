78fa308573db4624628909a3ce252b2563f65947
Miguel Velez <miguelvelez@google.com>
2019-06-07 15:48:39 -0700

    changed the structure of metadata to the structure fetched with responseFormat=v6

---
+++ cdap-app-fabric-tests/src/test/java/io/cdap/cdap/graphql/store/metadata/MetadataDataFetcherTest.java
@@ -0,0 +1,61 @@
+/*
+ *
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.graphql.store.metadata;
+
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import io.cdap.cdap.graphql.CDAPGraphQLTest;
+import io.cdap.cdap.graphql.cdap.schema.GraphQLFields;
+import io.cdap.cdap.graphql.store.application.schema.ApplicationFields;
+import io.cdap.cdap.graphql.store.metadata.schema.MetadataFields;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class MetadataDataFetcherTest extends CDAPGraphQLTest {
+
+  @Test
+  public void testGetMetadataRecord() {
+    String query = "{ "
+      + "  application(name: \"JavascriptTransform\") {"
+      + "    metadata {"
+      + "      tags {"
+      + "        name"
+      + "        scope"
+      + "      }"
+      + "    }"
+      + "  }"
+      + "}";
+
+    ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
+    ExecutionResult executionResult = graphQL.execute(executionInput);
+
+    Assert.assertTrue(executionResult.getErrors().isEmpty());
+
+    Map<String, Map> data = executionResult.getData();
+    Map<String, List> application = data.get(ApplicationFields.APPLICATION);
+    Map<String, List> metadata = (Map<String, List>) application.get(ApplicationFields.METADATA);
+
+    Map<String, String> tag = (Map<String, String>) metadata.get(MetadataFields.TAGS).get(0);
+    Assert.assertNotNull(tag.get(GraphQLFields.NAME));
+    Assert.assertNotNull(tag.get(MetadataFields.SCOPE));
+  }
+
+}
---
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/graphql/store/metadata/schema/MetadataFields.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.graphql.store.metadata.schema;
+
+import io.cdap.cdap.graphql.schema.Fields;
+
+/**
+ * Helper class with a collection of fields relevant to metadata that are used in the server
+ */
+public class MetadataFields implements Fields {
+
+  public static final String SCOPE = "scope";
+  public static final String TAGS = "tags";
+
+  private MetadataFields() {
+    throw new UnsupportedOperationException("Helper class should not be instantiated");
+  }
+
+}
