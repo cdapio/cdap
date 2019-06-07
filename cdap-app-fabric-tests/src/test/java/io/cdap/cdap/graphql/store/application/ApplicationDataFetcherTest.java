828db64517d388a39f659917346e98e63011d08e
Miguel Velez <miguelvelez@google.com>
2019-06-07 08:47:15 -0700

    added fetcher to get metadata for an application detail

--- cdap-app-fabric-tests/src/test/java/io/cdap/cdap/graphql/store/application/ApplicationDataFetcherTest.java
+++ cdap-app-fabric-tests/src/test/java/io/cdap/cdap/graphql/store/application/ApplicationDataFetcherTest.java
@@ -85,6 +85,9 @@ public class ApplicationDataFetcherTest extends CDAPGraphQLTest {
       + "        }"
       + "      }"
       + "    }"
+      + "    metadata {"
+      + "      tags"
+      + "    }"
       + "  }"
       + "}";
 
