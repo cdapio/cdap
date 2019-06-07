9dd44482ca4488e40ab3d060818e964a75d71055
Miguel Velez <miguelvelez@google.com>
2019-06-07 08:48:52 -0700

    updated test to get multiple applications by also querying the metadata of each application

--- cdap-app-fabric-tests/src/test/java/io/cdap/cdap/graphql/store/application/ApplicationDataFetcherTest.java
+++ cdap-app-fabric-tests/src/test/java/io/cdap/cdap/graphql/store/application/ApplicationDataFetcherTest.java
@@ -50,6 +50,9 @@ public class ApplicationDataFetcherTest extends CDAPGraphQLTest {
       + "          }"
       + "        }"
       + "      }"
+      + "      metadata {"
+      + "        tags"
+      + "      }"
       + "    }"
       + "  }"
       + "}";
