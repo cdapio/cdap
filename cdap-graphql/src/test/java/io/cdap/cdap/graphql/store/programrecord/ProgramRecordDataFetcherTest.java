6d877193e24f26e0fb4daf7c62f8232546b1a81c
Miguel Velez <miguelvelez@google.com>
2019-06-11 14:15:53 -0700

    fixed test to fetch data from runs

--- cdap-graphql/src/test/java/io/cdap/cdap/graphql/store/programrecord/ProgramRecordDataFetcherTest.java
+++ cdap-graphql/src/test/java/io/cdap/cdap/graphql/store/programrecord/ProgramRecordDataFetcherTest.java
@@ -21,12 +21,16 @@ import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import io.cdap.cdap.AppWithSchedule;
 import io.cdap.cdap.AppWithServices;
+import io.cdap.cdap.AppWithServicesAndWorker;
 import io.cdap.cdap.AppWithWorkflow;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.common.utils.Tasks;
 import io.cdap.cdap.graphql.CDAPGraphQLTest;
 import io.cdap.cdap.graphql.cdap.schema.GraphQLFields;
 import io.cdap.cdap.graphql.store.application.schema.ApplicationFields;
 import io.cdap.cdap.graphql.store.programrecord.schema.ProgramRecordFields;
 import io.cdap.cdap.internal.app.runtime.batch.AppWithMapReduce;
+import io.cdap.cdap.proto.ProgramRunStatus;
 import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.NamespaceId;
 import io.cdap.cdap.proto.id.ProgramId;
@@ -36,6 +40,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ProgramRecordDataFetcherTest extends CDAPGraphQLTest {
 
@@ -73,9 +78,21 @@ public class ProgramRecordDataFetcherTest extends CDAPGraphQLTest {
   }
 
   @Test
-  public void testGetWorkflowWithType() {
+  public void testGetWorkflowWithType() throws Exception {
+    deploy(AppWithWorkflow.class, 200, Constants.Gateway.API_VERSION_3_TOKEN, NamespaceId.DEFAULT.getNamespace());
+
+    ApplicationId appId = new ApplicationId(NamespaceId.DEFAULT.getNamespace(), AppWithWorkflow.NAME);
+    ProgramId sampleWorkflow = appId.workflow(AppWithWorkflow.SampleWorkflow.NAME);
+    Assert.assertEquals("STOPPED", getProgramStatus(sampleWorkflow));
+
+    startProgram(sampleWorkflow);
+    waitState(sampleWorkflow, "RUNNING");
+
+    // stopProgram(sampleWorkflow);
+    waitState(sampleWorkflow, "STOPPED");
+
     String query = "{ "
-      + "  application(name: \"JavascriptTransform\") {"
+      + "  application(name: \"" + AppWithWorkflow.NAME + "\") {"
       + "    programs(type: \"Workflow\") {"
       + "      ... on Workflow {"
       + "        runs {"
@@ -101,107 +118,65 @@ public class ProgramRecordDataFetcherTest extends CDAPGraphQLTest {
     Map<String, String> programRecord = (Map<String, String>) programs.get(0);
     Assert.assertNotNull(programRecord.get(ProgramRecordFields.RUNS));
     Assert.assertNotNull(programRecord.get(ProgramRecordFields.SCHEDULES));
+
+    deleteAppAndData(NamespaceId.DEFAULT.app(AppWithWorkflow.NAME));
   }
 
   @Test
   public void testGetRuns() throws Exception {
-    deploy(AppWithWorkflow.class, 200, null, NamespaceId.DEFAULT.getNamespace());
+    deploy(AppWithWorkflow.class, 200, Constants.Gateway.API_VERSION_3_TOKEN, NamespaceId.DEFAULT.getNamespace());
 
     ApplicationId appId = new ApplicationId(NamespaceId.DEFAULT.getNamespace(), AppWithWorkflow.NAME);
-    ProgramId sampleWorkflow = appId.service(AppWithWorkflow.SampleWorkflow.NAME);
+    ProgramId sampleWorkflow = appId.workflow(AppWithWorkflow.SampleWorkflow.NAME);
     Assert.assertEquals("STOPPED", getProgramStatus(sampleWorkflow));
 
-    // startProgram(sampleWorkflow);
-    // waitState(sampleWorkflow, "RUNNING");
-    //
-    // stopProgram(sampleWorkflow);
-    // waitState(sampleWorkflow, "STOPPED");
+    startProgram(sampleWorkflow);
+    waitState(sampleWorkflow, "RUNNING");
 
+    // stopProgram(sampleWorkflow);
+    waitState(sampleWorkflow, "STOPPED");
+
+    String query = "{ "
+      + "  application(name: \"" + AppWithWorkflow.NAME + "\") {"
+      + "    programs(type: \"Workflow\") {"
+      + "      ... on Workflow {"
+      + "        runs {"
+      + "          pid"
+      + "          startTs"
+      + "          runTs"
+      + "          stopTs"
+      + "          suspendTs"
+      + "          resumeTs"
+      + "          status"
+      + "          profileId"
+      + "        }"
+      + "      }"
+      + "    }"
+      + "  }"
+      + "}";
+
+    ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
+    ExecutionResult executionResult = graphQL.execute(executionInput);
+
+    Assert.assertTrue(executionResult.getErrors().isEmpty());
+
+    Map<String, Map> data = executionResult.getData();
+    Map<String, List> application = data.get(ApplicationFields.APPLICATION);
+    List programs = application.get(ApplicationFields.PROGRAMS);
+    Map<String, List> programRecord = (Map<String, List>) programs.get(0);
+
+    Map<String, String> run = (Map<String, String>) programRecord.get(ProgramRecordFields.RUNS).get(0);
+    Assert.assertNotNull(run.get(ProgramRecordFields.PID));
+    Assert.assertNotNull(run.get(ProgramRecordFields.START_TS));
+    Assert.assertNotNull(run.get(ProgramRecordFields.RUN_TS));
+    Assert.assertNotNull(run.get(ProgramRecordFields.STOP_TS));
+    Assert.assertNotNull(run.get(ProgramRecordFields.STATUS));
+    Assert.assertNotNull(run.get(ProgramRecordFields.PROFILE_ID));
+
+    Assert.assertTrue(run.containsKey(ProgramRecordFields.SUSPEND_TS));
+    Assert.assertTrue(run.containsKey(ProgramRecordFields.RESUME_TS));
 
     deleteAppAndData(NamespaceId.DEFAULT.app(AppWithWorkflow.NAME));
-
-
-    /*
-    Sample code to run a workflow
-     */
-    // deploy(AppWithServicesAndWorker.class, 200, Constants.Gateway.API_VERSION_3_TOKEN, TEST_NAMESPACE1);
-    //
-    // ApplicationId appId = new ApplicationId(TEST_NAMESPACE1, AppWithServicesAndWorker.NAME);
-    // ProgramId noOpService = appId.service(AppWithServicesAndWorker.NO_OP_SERVICE);
-    // ProgramId pingService = appId.service(AppWithServicesAndWorker.PING_SERVICE);
-    // ProgramId noOpWorker = appId.worker(AppWithServicesAndWorker.NO_OP_WORKER);
-    //
-    // // all programs are stopped initially
-    // Assert.assertEquals(STOPPED, getProgramStatus(noOpService));
-    // Assert.assertEquals(STOPPED, getProgramStatus(pingService));
-    // Assert.assertEquals(STOPPED, getProgramStatus(noOpWorker));
-    //
-    // // start both services and check the status
-    // startProgram(noOpService);
-    // waitState(noOpService, RUNNING);
-    //
-    // startProgram(pingService);
-    // waitState(pingService, RUNNING);
-    //
-    // // start the worker and check that it is rejected
-    // startProgram(noOpWorker, 409);
-    // Tasks.waitFor(1, () -> getProgramRuns(noOpWorker, ProgramRunStatus.ALL).size(), 10, TimeUnit.SECONDS);
-    // Assert.assertEquals(ProgramRunStatus.REJECTED, getProgramRuns(noOpWorker, ProgramRunStatus.ALL).get(0).getStatus());
-    //
-    // // stop one service
-    // stopProgram(noOpService);
-    // waitState(noOpService, STOPPED);
-    //
-    // // starting the worker should now succeed
-    // startProgram(noOpWorker, 200);
-    // Tasks.waitFor(1, () -> getProgramRuns(noOpWorker, ProgramRunStatus.COMPLETED).size(), 10, TimeUnit.SECONDS);
-    //
-    // // stop other service
-    // stopProgram(pingService);
-    // waitState(pingService, STOPPED);
-
-
-
-
-    // String query = "{ "
-    //   + "  application(name: \"Test\") {"
-    //   + "    programs(type: \"Workflow\") {"
-    //   + "      ... on Workflow {"
-    //   + "        runs {"
-    //   + "          pid"
-    //   + "          startTs"
-    //   + "          runTs"
-    //   + "          stopTs"
-    //   + "          suspendTs"
-    //   + "          resumeTs"
-    //   + "          status"
-    //   + "          profileId"
-    //   + "        }"
-    //   + "      }"
-    //   + "    }"
-    //   + "  }"
-    //   + "}";
-    //
-    // ExecutionInput executionInput = ExecutionInput.newExecutionInput().query(query).build();
-    // ExecutionResult executionResult = graphQL.execute(executionInput);
-    //
-    // Assert.assertTrue(executionResult.getErrors().isEmpty());
-    //
-    // Map<String, Map> data = executionResult.getData();
-    // Map<String, List> application = data.get(ApplicationFields.APPLICATION);
-    // List programs = application.get(ApplicationFields.PROGRAMS);
-    // Map<String, List> programRecord = (Map<String, List>) programs.get(0);
-    //
-    // Map<String, String> run = (Map<String, String>) programRecord.get(ProgramRecordFields.RUNS).get(0);
-    // Assert.assertNotNull(run.get(ProgramRecordFields.PID));
-    // Assert.assertNotNull(run.get(ProgramRecordFields.START_TS));
-    // Assert.assertNotNull(run.get(ProgramRecordFields.RUN_TS));
-    // Assert.assertNotNull(run.get(ProgramRecordFields.STOP_TS));
-    // Assert.assertNotNull(run.get(ProgramRecordFields.STATUS));
-    // Assert.assertNotNull(run.get(ProgramRecordFields.PROFILE_ID));
-    //
-    // Assert.assertTrue(run.containsKey(ProgramRecordFields.SUSPEND_TS));
-    // Assert.assertTrue(run.containsKey(ProgramRecordFields.RESUME_TS));
   }
 
   @Test
