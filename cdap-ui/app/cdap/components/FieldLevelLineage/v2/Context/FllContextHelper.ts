a29a90389632a24321f5db122b6bef16a2e7845a
yukiej <yukiej@gmail.com>
2019-06-11 14:39:26 -0700

    Highlight active fields when a field is clicked and filter to show only active fields when "View" is clicked
    Create FllField component to use in FllTable
    
    Redraw tables when you click View
    
    Replace id with field object in link source and destination to avoid parsing
    
    Move LineageSummary state to context

--- cdap-ui/app/cdap/components/FieldLevelLineage/v2/Context/FllContextHelper.ts
+++ cdap-ui/app/cdap/components/FieldLevelLineage/v2/Context/FllContextHelper.ts
@@ -14,61 +14,122 @@
  * the License.
 */
 
-// Parses an incoming or outgoing entity object from backend response to get unique nodes (one per incoming or outgoing field), connections, and an object with fieldnames for each incoming or outgoing dataset
+// types for backend response
+interface IFllEntity {
+  entityId?: IEntityId;
+  relations?: IRelation[];
+}
 
-export function parseRelations(namespace, target, ents, isCause = true) {
-  const tables = {};
-  const relNodes = [];
+interface IEntityId {
+  namespace?: string;
+  dataset?: string;
+  entity?: string;
+}
+
+export interface IRelation {
+  source?: string;
+  destination?: string;
+}
+
+// These types are used by the frontend
+export interface IField {
+  id: string;
+  type: string;
+  name: string;
+  dataset: string;
+  namespace: string;
+}
+
+export interface ILink {
+  source: IField;
+  destination: IField;
+}
+
+export interface ITableFields {
+  [tablename: string]: IField[];
+}
+
+/** Parses an incoming or outgoing entity object from backend response
+ * to get array of edges and an object with fields keyed by dataset.
+ * namespace and target are the target namespace and dataset name
+ */
+
+export function parseRelations(
+  namespace: string,
+  target: string,
+  ents: IFllEntity[],
+  isCause: boolean = true
+) {
+  const tables: ITableFields = {};
   const relLinks = [];
   ents.map((ent) => {
-    // Assumes that all tableNames are unique (since all datasets in a namespace should have unique names)
-    // tableName is later used to display the table name in the header
+    // Assumes that all tableNames are unique within a namespace
 
     const tableId = `${isCause ? 'cause' : 'impact'}_ns-${ent.entityId.namespace}_ds-${
       ent.entityId.dataset
     }`;
     // tables keeps track of fields for each incoming or outgoing dataset.
     tables[tableId] = [];
-    // fieldIds keeps track of fields we've seen already, since a single field can have multiple connections
+    // fieldIds keeps track of fields, since a single field can have multiple connections
     const fieldIds = new Map();
     ent.relations.map((rel) => {
-      // backend response assumes connection goes from left to right, i.e. an incoming connection's source = incoming field and destination = target field, and outgoing connection's source = target field.
+      /** backend response assumes connection goes from left to right
+       * i.e. an incoming connection's destination = target field,
+       * and outgoing connection's source = target field.
+       */
+
       const fieldName = isCause ? rel.source : rel.destination;
       let id = fieldIds.get(fieldName);
-      const field = {
+      const field: IField = {
         id,
+        type: isCause ? 'cause' : 'impact',
         name: fieldName,
-        group: ent.entityId.dataset,
+        dataset: ent.entityId.dataset,
+        namespace: ent.entityId.namespace,
       };
       if (!fieldIds.has(fieldName)) {
         id = `${tableId}_fd-${fieldName}`;
         field.id = id;
         fieldIds.set(fieldName, id);
         tables[tableId].push(field);
-        relNodes.push(field);
       }
-      relLinks.push({
-        // if connection goes from cause to target
-        source: isCause
-          ? fieldIds.get(fieldName)
-          : `target_ns-${namespace}_ds-${target}_fd-${rel.source}`,
-        destination: isCause
-          ? `target_ns-${namespace}_ds-${target}_fd-${rel.destination}`
-          : fieldIds.get(fieldName),
-      });
+      const targetField: IField = {
+        id: `target_ns-${namespace}_ds-${target}_fd-${isCause ? rel.destination : rel.source}`,
+        type: 'target',
+        dataset: target,
+        namespace,
+        name: isCause ? rel.destination : rel.source,
+      };
+      const link: ILink = {
+        source: isCause ? field : targetField,
+        destination: isCause ? targetField : field,
+      };
+      relLinks.push(link);
     });
   });
-  return { tables, relNodes, relLinks };
+  return { tables, relLinks };
 }
 
-export function makeTargetNodes(entityId, fields) {
-  const targetNodes = fields.map((field) => {
-    const id = `target_ns-${entityId.namespace}_ds-${entityId.dataset}_fd-${field}`;
-    return {
+export function getFieldsAndLinks(d) {
+  const incoming = parseRelations(d.entityId.namespace, d.entityId.dataset, d.incoming);
+  const outgoing = parseRelations(d.entityId.namespace, d.entityId.dataset, d.outgoing, false);
+  const causeTables = incoming.tables;
+  const impactTables = outgoing.tables;
+  const links = incoming.relLinks.concat(outgoing.relLinks);
+  return { causeTables, impactTables, links };
+}
+
+export function makeTargetFields({ namespace, dataset }: IEntityId, fields: string[]) {
+  const targetFields = fields.map((fieldname) => {
+    const id = `target_ns-${namespace}_ds-${dataset}_fd-${fieldname}`;
+    const field: IField = {
       id,
-      name: field,
-      group: entityId.dataset,
+      type: 'target',
+      name: fieldname,
+      dataset,
+      namespace,
     };
+    return field;
   });
-  return targetNodes;
+  return targetFields;
 }
--- cdap-ui/app/cdap/components/FieldLevelLineage/v2/FllTable/index.tsx
+++ cdap-ui/app/cdap/components/FieldLevelLineage/v2/FllTable/index.tsx
@@ -14,19 +14,23 @@
  * the License.
 */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import SortableStickyGrid from 'components/SortableStickyGrid/index.js';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 import createStyles from '@material-ui/core/styles/createStyles';
 import T from 'i18n-react';
 import classnames from 'classnames';
-import { INode } from 'components/FieldLevelLineage/v2/Context/FllContext';
+import { IField } from 'components/FieldLevelLineage/v2/Context/FllContextHelper';
+import FllField from 'components/FieldLevelLineage/v2/FllTable/FllField';
+import { FllContext, IContextState } from 'components/FieldLevelLineage/v2/Context/FllContext';
 
+// TO DO: Consolidate different fontsizes in ThemeWrapper
 const styles = (theme) => {
   return createStyles({
     table: {
       width: '170px',
       border: `1px solid ${theme.palette.grey[300]}`,
+      fontSize: '0.92rem',
       marginBottom: '10px',
       '& .grid.grid-container': {
         maxHeight: 'none',
@@ -40,7 +44,15 @@ const styles = (theme) => {
       '& .grid-row': {
         paddingLeft: '10px',
         paddingRight: '10px',
-        borderBottom: `1px solid ${theme.palette.grey[500]}`,
+        borderTop: `1px solid ${theme.palette.grey[500]}`,
+      },
+      ' & .grid-row:hover': {
+        backgroundColor: theme.palette.grey[700],
+      },
+      ' & .grid-row.selected': {
+        backgroundColor: theme.palette.yellow[200],
+        color: theme.palette.orange[50],
+        fontWeight: 'bold',
       },
     },
     tableHeader: {
@@ -48,58 +60,63 @@ const styles = (theme) => {
       height: '40px',
       paddingLeft: '10px',
       fontWeight: 'bold',
+      fontSize: '1rem',
     },
     tableSubheader: {
       fontWeight: 'normal',
       color: theme.palette.grey[200],
-      paddingBottom: 5,
-      paddingTop: 3,
+      fontSize: '0.92rem',
+    },
+    viewLineage: {
+      visibility: 'hidden',
+      color: theme.palette.blue[300],
     },
   });
 };
 
 interface ITableProps extends WithStyles<typeof styles> {
   tableId: string;
-  fields: INode[];
-  isTarget?: boolean;
+  fields: IField[];
 }
 
-function renderGridHeader(fields, tableId, classes) {
+function renderGridHeader(fields: IField[], showingOneField: boolean, classes) {
   const count: number = fields.length;
-  const tableName = fields[0].group;
+  const tableName = fields[0].dataset;
   return (
     <div className={classes.tableHeader}>
-      {tableName}
+      <div>{tableName}</div>
       <div className={classes.tableSubheader}>
-        {T.translate('features.FieldLevelLineage.v2.TableSubheader', { count })}
+        {showingOneField
+          ? T.translate('features.FieldLevelLineage.v2.FllTable.relatedFieldsCount', {
+              context: count,
+            })
+          : T.translate('features.FieldLevelLineage.v2.FllTable.fieldsCount', { context: count })}
       </div>
     </div>
   );
 }
 
-function renderGridBody(fields, tableName, classes) {
+function renderGridBody(fields: IField[], tableName: string, classes) {
   return (
     <div className={classes.gridBody} id={tableName}>
       {fields.map((field) => {
-        return (
-          <div className={classnames('grid-row', 'grid-link')} key={field.id} id={field.id}>
-            {field.name}
-          </div>
-        );
+        return <FllField key={field.id} field={field} />;
       })}
     </div>
   );
 }
 
-function FllTable({ tableId, fields, classes, isTarget = false }: ITableProps) {
+function FllTable({ tableId, fields, classes }: ITableProps) {
   const GRID_HEADERS = [{ property: 'name', label: tableId }];
+  const { showingOneField } = useContext<IContextState>(FllContext);
+  const isTarget = fields[0].type === 'target';
   return (
     <SortableStickyGrid
       key={`cause ${tableId}`}
       entities={fields}
       gridHeaders={GRID_HEADERS}
       className={classnames(classes.table, { [classes.targetTable]: isTarget })}
-      renderGridHeader={renderGridHeader.bind(null, fields, tableId, classes)}
+      renderGridHeader={renderGridHeader.bind(null, fields, showingOneField, classes)}
       renderGridBody={renderGridBody.bind(this, fields, tableId, classes)}
     />
   );
