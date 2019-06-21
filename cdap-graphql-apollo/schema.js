0c06ecfaeef026ca7d7f4699cbb6fca71b22f23c
Miguel Velez <miguelvelez@google.com>
2019-06-21 10:32:01 -0700

    cleaned up schema

--- cdap-graphql-apollo/schema.js
+++ cdap-graphql-apollo/schema.js
@@ -1,26 +1,27 @@
 const typeDefs = `
+
 type Product {
+
   id: ID!
+
   name: String!
+
   shortDescription: String
 }
 
 type Variant {
+
   id: ID!
+
   name: String!
+
   shortDescription: String
 }
 
 type Query {
-  # ### GET products
-  #
-  # _Arguments_
-  # - **id**: Product's id (optional)
+
   products(id: Int): [Product]
-  # ### GET variants
-  #
-  # _Arguments_
-  # - **id**: Variant's id (optional)
+
   variants(id: Int): [Variant]
 }
 `
