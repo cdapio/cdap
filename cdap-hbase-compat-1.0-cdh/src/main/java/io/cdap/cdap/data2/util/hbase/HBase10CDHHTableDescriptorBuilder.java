548cc561f16b08bbc6103170401b1e0318ed2e08
Terence Yim <terencey@google.com>
2019-08-27 22:09:17 -0700

    Fix HBase 1.0 CDH

---
+++ cdap-hbase-compat-1.0-cdh/src/main/java/io/cdap/cdap/data2/util/hbase/HBase10CDHHTableDescriptorBuilder.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.data2.util.hbase;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ *
+ */
+public class HBase10CDHHTableDescriptorBuilder extends HTableDescriptorBuilder {
+
+  HBase10CDHHTableDescriptorBuilder(TableName tableName) {
+    super(tableName);
+  }
+
+  HBase10CDHHTableDescriptorBuilder(HTableDescriptor toCopy) {
+    super(toCopy);
+  }
+
+  /* Methods whose signature changed in HBase 1.0 due to HBASE-10841 */
+  @Override
+  public HTableDescriptorBuilder setValue(byte[] key, byte[] value) {
+    instance.setValue(key, value);
+    return this;
+  }
+
+  @Override
+  public HTableDescriptorBuilder setValue(String key, String value) {
+    instance.setValue(key, value);
+    return this;
+  }
+
+  @Override
+  public HTableDescriptorBuilder addFamily(HColumnDescriptor columnDescriptor) {
+    instance.addFamily(columnDescriptor);
+    return this;
+  }
+
+  @Override
+  public HTableDescriptorBuilder addCoprocessor(String className) throws IOException {
+    instance.addCoprocessor(className);
+    return this;
+  }
+
+  @Override
+  public HTableDescriptorBuilder addCoprocessor(String className, Path jarFilePath, int priority,
+                                                Map<String, String> keyValues) throws IOException {
+    instance.addCoprocessor(className, jarFilePath, priority, keyValues);
+    return this;
+  }
+}
--- cdap-hbase-compat-1.0-cdh/src/main/java/io/cdap/cdap/data2/util/hbase/HBase10CDHTableUtil.java
+++ cdap-hbase-compat-1.0-cdh/src/main/java/io/cdap/cdap/data2/util/hbase/HBase10CDHTableUtil.java
@@ -34,7 +34,11 @@ import org.apache.hadoop.hbase.NamespaceNotFoundException;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.compress.Compression;
 import org.apache.hadoop.hbase.security.access.AccessControlClient;
 import org.slf4j.Logger;
@@ -53,13 +57,13 @@ public class HBase10CDHTableUtil extends HBaseTableUtil {
   @Override
   public HTableDescriptorBuilder buildHTableDescriptor(TableId tableId) {
     Preconditions.checkArgument(tableId != null, "Table id should not be null");
-    return new HTableDescriptorBuilder(HTableNameConverter.toTableName(tablePrefix, tableId));
+    return new HBase10CDHHTableDescriptorBuilder(HTableNameConverter.toTableName(tablePrefix, tableId));
   }
 
   @Override
   public HTableDescriptorBuilder buildHTableDescriptor(HTableDescriptor tableDescriptorToCopy) {
     Preconditions.checkArgument(tableDescriptorToCopy != null, "Table descriptor should not be null");
-    return new HTableDescriptorBuilder(tableDescriptorToCopy);
+    return new HBase10CDHHTableDescriptorBuilder(tableDescriptorToCopy);
   }
 
   @Override
@@ -240,5 +244,50 @@ public class HBase10CDHTableUtil extends HBaseTableUtil {
   @Override
   public Class<? extends Coprocessor> getPayloadTableRegionObserverClassForVersion() {
     return PayloadTableRegionObserver.class;
+  }
+
+  @Override
+  public ScanBuilder buildScan() {
+    return new HBase10CDHScanBuilder();
+  }
+
+  @Override
+  public ScanBuilder buildScan(Scan scan) throws IOException {
+    return new HBase10CDHScanBuilder(scan);
+  }
+
+  @Override
+  public IncrementBuilder buildIncrement(byte[] row) {
+    return new HBase10CDHIncrementBuilder(row);
+  }
+
+  @Override
+  public PutBuilder buildPut(byte[] row) {
+    return new HBase10CDHPutBuilder(row);
+  }
+
+  @Override
+  public PutBuilder buildPut(Put put) {
+    return new HBase10CDHPutBuilder(put);
+  }
+
+  @Override
+  public GetBuilder buildGet(byte[] row) {
+    return new HBase10CDHGetBuilder(row);
+  }
+
+  @Override
+  public GetBuilder buildGet(Get get) {
+    return new HBase10CDHGetBuilder(get);
+  }
+
+  @Override
+  public DeleteBuilder buildDelete(byte[] row) {
+    return new HBase10CDHDeleteBuilder(row);
+  }
+
+  @Override
+  public DeleteBuilder buildDelete(Delete delete) {
+    return new HBase10CDHDeleteBuilder(delete);
   }
 }
