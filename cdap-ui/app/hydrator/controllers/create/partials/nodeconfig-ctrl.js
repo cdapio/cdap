da9b11f8ab05f413ac67099eeeb0a60e1dc66398
avantika-karwasara <avantika.karwasara@guavus.com>
2019-12-10 15:20:51 +0530

    RAFD-3286: can set empty value in default field (#247)

--- cdap-ui/app/hydrator/controllers/create/partials/nodeconfig-ctrl.js
+++ cdap-ui/app/hydrator/controllers/create/partials/nodeconfig-ctrl.js
@@ -352,7 +352,7 @@ class HydratorPlusPlusNodeConfigCtrl {
               angular.forEach(group.fields, (field) => {
                 field.errorTooltip = '\'' + field.label + '\' is a required field';
                 if (field.defaultValue) {
-                  this.state.node.plugin.properties[field.name] = this.state.node.plugin.properties[field.name] || field['widget-attributes'].default;
+                  this.state.node.plugin.properties[field.name] = (this.state.node.plugin.properties[field.name] === undefined) ? field['widget-attributes'].default : this.state.node.plugin.properties[field.name];
                 }
               });
             });
