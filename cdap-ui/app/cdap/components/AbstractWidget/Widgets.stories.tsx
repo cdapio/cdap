92bbde9e6345441122d18423443cbb4020a3ce71
yukiej <yukijung@google.com>
2020-01-16 18:05:50 -0800

    Add all simple-ish widgets

--- cdap-ui/app/cdap/components/AbstractWidget/Widgets.stories.tsx
+++ cdap-ui/app/cdap/components/AbstractWidget/Widgets.stories.tsx
@@ -15,30 +15,90 @@
  */
 
 import * as React from 'react';
-import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import AbstractWidget from './index';
-import WidgetWrapper from 'components/ConfigurationGroup/WidgetWrapper';
-import Select from 'components/AbstractWidget/FormInputs/Select';
 
-const options = [
+export default {
+  component: AbstractWidget,
+  title: 'Widgets',
+  decorators: [withInfo],
+};
+
+const selectOptions = [
   { value: 'choice A', label: 'choice A' },
   { value: 'choice B', label: 'choice B' },
 ];
 
-const widgetProps = { options };
+const radioOptions = [{ label: 'choice A', id: 'choice A' }, { label: 'choice B', id: 'choice B' }];
 
-storiesOf('Widget', module).add(
-  'Select',
-  withInfo({
-    text: 'Render a select dropdown widget',
-  })(() => (
-    <AbstractWidget
-      value={'choice A'}
-      type="select"
-      widgetProps={widgetProps}
-      onChange={action('onChange')}
-    />
-  ))
+const widgetProps = {
+  select: { options: selectOptions },
+  radio: { options: radioOptions },
+  toggle: {
+    on: { value: 'on', label: 'on' },
+    off: { value: 'off', label: 'off' },
+  },
+};
+export const connectionBrowser = () => <AbstractWidget type="connection-browser" />;
+
+export const csv = () => <AbstractWidget type="csv" />;
+
+export const datasetSelector = () => <AbstractWidget type="dataset-selector" />;
+
+export const multipleValues = () => <AbstractWidget type="ds-multiplevalues" />;
+
+export const dsv = () => <AbstractWidget type="dsv" />;
+
+export const functionDropdownWithAlias = () => (
+  <AbstractWidget type="function-dropdown-with-alias" />
 );
+
+export const dlp = () => <AbstractWidget type="dlp" />;
+
+export const getSchema = () => <AbstractWidget type="get-schema" />;
+
+export const inputFieldDropdown = () => <AbstractWidget type="input-field-selector" />;
+
+export const keyValueDropdown = () => <AbstractWidget type="keyvalue-dropdown" />;
+
+export const keyValueEncoded = () => <AbstractWidget type="keyvalue-encoded" />;
+
+export const keyValue = () => <AbstractWidget type="keyvalue" />;
+
+export const memoryDropdown = () => (
+  <AbstractWidget type="memory-dropdown" widgetProps={widgetProps.select} />
+);
+
+export const memoryTextbox = () => <AbstractWidget type="memory-textbox" />;
+
+export const multiSelect = () => <AbstractWidget type="multi-select" />;
+
+export const numberTextbox = () => <AbstractWidget type="number" />;
+
+export const password = () => <AbstractWidget type="password" />;
+
+export const radioGroup = () => (
+  <AbstractWidget type="radio-group" widgetProps={widgetProps.radio} value="choice A" />
+);
+
+export const secureKeyPassword = () => <AbstractWidget type="securekey-password" />;
+
+export const secureKeyText = () => <AbstractWidget type="securekey-text" />;
+
+export const secureKeyTextArea = () => <AbstractWidget type="securekey-textarea" />;
+
+export const selectDropdown = () => (
+  <AbstractWidget
+    value={'choice A'}
+    type="select"
+    widgetProps={widgetProps.select}
+    onChange={action('onChange')}
+  />
+);
+
+// export const text = () => <AbstractWidget type="text" />;
+
+export const textbox = () => <AbstractWidget type="textbox" />;
+
+export const toggle = () => <AbstractWidget type="toggle" widgetProps={widgetProps.toggle} />;
