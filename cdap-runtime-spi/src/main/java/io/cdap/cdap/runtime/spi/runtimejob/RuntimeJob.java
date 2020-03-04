f072da1fb13137619474424df9439402036ce1ec
vinisha <vinishashah@google.com>
2020-03-04 15:34:11 -0800

    [CDAP-16218] Adding RuntimeJobManager interface to launch Runtime Job

---
+++ cdap-runtime-spi/src/main/java/io/cdap/cdap/runtime/spi/runtimejob/RuntimeJob.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.runtime.spi.runtimejob;
+
+/**
+ * Represents a job that will be executed on a given runtime environment.
+ */
+public interface RuntimeJob {
+  /**
+   * This method will be responsible for submitting the runtime job to underlying runner.
+   *
+   * @param runtimeJobEnvironment runtime job environment
+   * @throws Exception thrown if any error while job execution
+   */
+  void run(RuntimeJobEnvironment runtimeJobEnvironment) throws Exception;
+}
---
+++ cdap-runtime-spi/src/main/java/io/cdap/cdap/runtime/spi/runtimejob/RuntimeJobInfo.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.runtime.spi.runtimejob;
+
+import org.apache.twill.api.LocalFile;
+
+import java.util.Collection;
+
+/**
+ * Runtime job information. The instance of this interface will be provided to
+ * {@link RuntimeJobManager#launch(RuntimeJobInfo)} with files to be localized and runtime job class implementation.
+ */
+public interface RuntimeJobInfo {
+  /**
+   * Returns a collection of files that are used to launch the job.
+   */
+  Collection<? extends LocalFile> getLocalizeFiles();
+
+  /**
+   * Returns default implementation of a {@link RuntimeJob}. This class is responsible for submitting runtime job to
+   * provided environment.
+   */
+  Class<? extends RuntimeJob> getRuntimeJobClass();
+}
