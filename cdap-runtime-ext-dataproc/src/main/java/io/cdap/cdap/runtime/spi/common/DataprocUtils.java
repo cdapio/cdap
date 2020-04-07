5ebc9657ec0661f67768c27e805e367938960101
Shivakumar Gokaram <shiva@cirus.io>
2020-04-06 21:53:32 -0400

    Dataproc enhancements to support metadata, network tags and initactions

--- cdap-runtime-ext-dataproc/src/main/java/io/cdap/cdap/runtime/spi/common/DataprocUtils.java
+++ cdap-runtime-ext-dataproc/src/main/java/io/cdap/cdap/runtime/spi/common/DataprocUtils.java
@@ -23,6 +23,10 @@ import com.google.cloud.storage.StorageBatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nullable;
+
 /**
  * This class contains common methods that are needed by DataprocProvisioner and DataprocRuntimeJobManager.
  */
@@ -68,6 +72,35 @@ public final class DataprocUtils {
       return bucket.substring(GS_PREFIX.length());
     }
     return bucket;
+  }
+
+  /**
+   * Utilty class to parse the keyvalue string from UI Widget and return back HashMap.
+   * String is of format  <key><keyValueDelimiter><value><delimiter><key><keyValueDelimiter><value>
+   * eg:  networktag1=out2internet;networktag2=priority
+   * The return from the method is a map with key value pairs of (networktag1 out2internet) and (networktag2 priority)
+   *
+   * @param configValue String to be parsed into key values format
+   * @param delimiter Delimiter used for keyvalue pairs
+   * @param keyValueDelimiter Delimiter between key and value.
+   * @return Map of Key value pairs parsed from input configValue using the delimiters.
+   */
+  public static Map<String, String> parseKeyValueConfig(@Nullable String configValue, String delimiter,
+                                                        String keyValueDelimiter) throws IllegalArgumentException {
+    Map<String, String> map = new HashMap<>();
+    if (configValue == null) {
+      return map;
+    }
+    for (String property : configValue.split(delimiter)) {
+      String[] parts = property.split(keyValueDelimiter, 2);
+      if (parts.length != 2) {
+        throw new IllegalArgumentException("Invalid KeyValue " + property);
+      }
+      String key = parts[0];
+      String value = parts[1];
+      map.put(key, value);
+    }
+    return map;
   }
 
   private DataprocUtils() {
