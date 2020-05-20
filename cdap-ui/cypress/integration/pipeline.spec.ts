ebe34a58615eb2b024c24b7a6f03b6bd0b6e08d5
yukiej <yukijung@google.com>
2020-05-20 15:00:34 -0700

    Fix  callback
    Add data-cy attributes for preview
    
    Add preview tests to run locally to check for behavior when post-run action exists

--- cdap-ui/cypress/integration/pipeline.spec.ts
+++ cdap-ui/cypress/integration/pipeline.spec.ts
@@ -18,7 +18,23 @@ import * as Helpers from '../helpers';
 
 const TEST_PIPELINE_NAME = '__UI_test_pipeline';
 const TEST_PATH = '__UI_test_path';
+const TEST_SENDER = '__UI_test_sender';
+const TEST_RECIPIENT = '__UI_test@test.com';
+const TEST_SUBJECT = '__UI_test_subject';
+const PREVIEW_FAILED_BANNER_MSG = 'Please add a source and sink to the pipeline';
+
 let headers = {};
+
+// We don't have preview enabled in distributed mode.
+// TODO(CDAP-16620) To enable preview in distributed mode. Once it is enabled we can remove.
+let skipPreviewTests = false;
+// @ts-ignore "cy.state" is not in the "cy" type
+const getMochaContext = () => cy.state('runnable').ctx;
+const skip = () => {
+  const ctx = getMochaContext();
+  return ctx.skip();
+};
+
 describe('Creating a pipeline', () => {
   // Uses API call to login instead of logging in manually through UI
   before(() => {
@@ -55,7 +71,7 @@ describe('Creating a pipeline', () => {
     });
   });
 
-  it('is configured correctly', () => {
+  it('can configure simple pipeline, including post-run actions', () => {
     // Go to Pipelines studio
     cy.visit('/cdap/ns/default/pipelines');
     cy.get('#resource-center-btn').click();
@@ -85,7 +101,7 @@ describe('Creating a pipeline', () => {
 
     cy.get('[data-testid=close-config-popover]').click();
 
-    // Click on Configure, toggle Instrumentation value, then Save
+    // Click on Configure, toggle Instrumentation value, set up email alert, then Save
     cy.contains('Configure').click();
     cy.get('.label-with-toggle')
       .contains('Instrumentation')
@@ -96,13 +112,95 @@ describe('Creating a pipeline', () => {
       .find('.toggle-switch')
       .click();
     cy.get('@instrumentationDiv').contains('Off');
-    cy.get('[data-testid=config-apply-close]').click();
 
-    // Name pipeline then deploy pipeline
+    cy.contains('Pipeline alert').click();
+    cy.contains('+').click();
+    cy.contains('Send Email').click();
+
+    // enter sender, recipients, subject, message
+
+    cy.get('[data-cy="sender"]').within(() => {
+      cy.get('input')
+        .scrollIntoView()
+        .focus()
+        .type(TEST_SENDER);
+    });
+    cy.get('[data-cy="recipients"]').within(() => {
+      cy.get('input').type(TEST_RECIPIENT);
+    });
+    cy.get('[data-cy="subject"]').within(() => {
+      cy.get('input').type(TEST_SUBJECT);
+    });
+    // validate and see error
+    cy.get('[data-cy="validate-btn"]').click();
+
+    cy.contains('error found').should('exist');
+
+    // Fix missing field to resolve error
+    cy.get('[data-cy="message"]').within(() => {
+      cy.get('button').click();
+    });
+    // validate
+    cy.get('[data-cy="validate-btn"]').click();
+    cy.wait(2000);
+    cy.contains('error found').should('not.exist');
+
+    // click next
+    cy.get('[data-cy="next-btn"]').click();
+
+    // click confirm
+    cy.get('[data-cy="confirm-btn"]').click();
+
+    // See email alert
+    cy.get('[data-cy="saved-alerts"]').within(() => {
+      cy.contains('Email').should('exist');
+    });
+
+    cy.get('[data-testid=config-apply-close]').click();
+  });
+  it('does not run Preview when user clicks No on postrun action modal', () => {
+    cy.window().then((window) => {
+      skipPreviewTests = window.CDAP_CONFIG.hydrator.previewEnabled !== true;
+    });
+    if (skipPreviewTests) {
+      skip();
+    }
+    cy.get('[data-cy="pipeline-preview-btn"]').click();
+    cy.get('[data-cy="run-preview-btn"]').click();
+    cy.get('[data-cy="run-preview-action-modal"]').should('be.visible');
+
+    // When user clicks No, Preview should not start
+    cy.get('[data-cy="no-preview-btn"]').click();
+    cy.get('[data-cy="run-preview-btn"]').should('be.visible');
+  });
+  it('runs Preview when user clicks Yes on postrun action modal', () => {
+    cy.window().then((window) => {
+      skipPreviewTests = window.CDAP_CONFIG.hydrator.previewEnabled !== true;
+    });
+    if (skipPreviewTests) {
+      skip();
+    }
+    // When user clicks Yes, Preview should fail to start and show banner about missing source and sink
+    cy.get('[data-cy="run-preview-btn"]').click();
+    cy.get('[data-cy="run-preview-action-modal"]').should('be.visible');
+    cy.get('[data-cy="yes-preview-btn"]').click();
+    // Close banner reminding user to add source and sink
+    cy.get('[data-cy="valium-banner-hydrator"]').contains(PREVIEW_FAILED_BANNER_MSG);
+    cy.get('.close').click();
+    cy.get('[data-cy="toggle-preview-btn"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-cy="schedule-pipeline-btn"]').should('be.visible');
+  });
+
+  it('shows correct configuration after deploying pipeline', () => {
+    // Name pipeline
     cy.get('.pipeline-name').click();
     cy.get('#pipeline-name-input')
       .type(TEST_PIPELINE_NAME)
       .type('{enter}');
+
+    // Deploy pipeline
     cy.get('[data-testid=deploy-pipeline]').click();
 
     // Do assertions
