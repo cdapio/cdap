3f3f4473928f940abc904bdc34d6b1ae98948512
yukiej <yukijung@google.com>
2020-05-20 15:00:34 -0700

    Fix  callback
    Add data-cy attributes for preview
    
    Add preview tests to run locally to check for behavior when post-run action exists

--- cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
+++ cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
@@ -524,7 +524,7 @@ class HydratorPlusPlusTopPanelCtrl {
   }
 
   onPreviewStart() {
-    this._checkAndShowConfirmationModalOnActionPlugin(this.runPreview());
+    this._checkAndShowConfirmationModalOnActionPlugin(this.runPreview.bind(this));
   }
 
   runPreview() {
