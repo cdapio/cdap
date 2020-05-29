86222b0e78d82b365cc22cd56c5b7ca66c7db2aa
Jay Pandya <jaypandya@google.com>
2020-05-28 17:02:59 -0700

    Add new API in application interface for upgrading application config

---
+++ cdap-api/src/main/java/io/cdap/cdap/api/app/ApplicationUpdateResult.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.api.app;
+
+import io.cdap.cdap.api.Config;
+
+/**
+ * Stores results of upgrading an application config.
+ *
+ * @param <T> {@link Config} config class that represents the configuration type of an Application.
+ */
+public class ApplicationUpdateResult<T extends Config> {
+
+  // Upgraded config.
+  private final T newConfig;
+
+  private ApplicationUpdateResult(T newConfig) {
+    this.newConfig = newConfig;
+  }
+
+  public T getNewConfig() {
+    return newConfig;
+  }
+}
