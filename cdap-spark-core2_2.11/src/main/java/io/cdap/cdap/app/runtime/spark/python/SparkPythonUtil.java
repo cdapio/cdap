b2121f72a3b8aefbce5864f558efe0252e170365
Terence Yim <terencey@google.com>
2020-06-11 18:19:23 -0700

    (CDAP-16870) Fix PySpark support for Spark 2.1.3+
    - Enable py4j authentication in Spark2 py4j client
    - Also enable py4j authentication for CDAP own py4j server in executor for authenticated communication

--- cdap-spark-core2_2.11/src/main/java/io/cdap/cdap/app/runtime/spark/python/SparkPythonUtil.java
+++ cdap-spark-core2_2.11/src/main/java/io/cdap/cdap/app/runtime/spark/python/SparkPythonUtil.java
@@ -16,13 +16,73 @@
 
 package io.cdap.cdap.app.runtime.spark.python;
 
+import io.cdap.cdap.app.runtime.spark.SparkRuntimeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import py4j.GatewayServer;
+import py4j.Py4JNetworkException;
+
+import java.io.IOException;
+import java.nio.channels.ByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.EnumSet;
 
 /**
  * Utility class to provide methods for PySpark integration.
  */
 @SuppressWarnings("unused")
-public final class SparkPythonUtil extends AbstractSparkPythonUtil {
+public final class SparkPythonUtil {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SparkPythonUtil.class);
+
+  /**
+   * Starts a Py4j gateway server.
+   *
+   * @param dir the local directory for writing information for the gateway server, such as port and auth token.
+   * @return the gateway server
+   * @throws IOException if failed to start the server or failed to write out the port.
+   */
+  public static GatewayServer startPy4jGateway(Path dir) throws IOException {
+    // 256 bits secret
+    byte[] secret = new byte[256 / 8];
+    new SecureRandom().nextBytes(secret);
+    String authToken = Base64.getEncoder().encodeToString(secret);
+
+    GatewayServer server = new GatewayServer.GatewayServerBuilder()
+      .javaPort(0)
+      .authToken(authToken)
+      .build();
+    try {
+      server.start();
+    } catch (Py4JNetworkException e) {
+      throw new IOException(e);
+    }
+
+    // Write the port number in string form to the port file
+    Files.write(dir.resolve(SparkRuntimeUtils.PYSPARK_PORT_FILE_NAME),
+                Integer.toString(server.getListeningPort()).getBytes(StandardCharsets.UTF_8));
+    // Write the auth token
+    Path secretFile = dir.resolve(SparkRuntimeUtils.PYSPARK_SECRET_FILE_NAME);
+    try (ByteChannel channel = Files.newByteChannel(dir.resolve(SparkRuntimeUtils.PYSPARK_SECRET_FILE_NAME),
+                                               EnumSet.of(StandardOpenOption.CREATE, StandardOpenOption.WRITE),
+                                               PosixFilePermissions.asFileAttribute(
+                                                 EnumSet.of(PosixFilePermission.OWNER_READ,
+                                                            PosixFilePermission.OWNER_WRITE)))) {
+      channel.write(StandardCharsets.UTF_8.encode(authToken));
+    }
+
+    LOG.debug("Py4j Gateway server started at port {} with auth token of {} bits",
+              server.getListeningPort(), secret.length);
+
+    return server;
+  }
 
   /**
    * Updates the python callback port in the {@link GatewayServer}.
