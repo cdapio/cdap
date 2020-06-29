9cf73bc35b721d409b4acfbfaf02acc07e539eed
yukiej <yukijung@google.com>
2020-06-29 15:45:26 -0400

    Update alert component to handle info case

--- cdap-ui/app/services/alert-on-valium/alert-on-valium.js
+++ cdap-ui/app/services/alert-on-valium/alert-on-valium.js
@@ -19,14 +19,14 @@ angular.module(PKG.name + '.services')
     var isAnAlertOpened = false,
         alertObj;
 
-    var SUCCESS_ALERT_DURATION = 4; // duration amount in seconds
+    var SUCCESS_ALERT_DURATION = 3; // duration amount in seconds
 
     function show(obj) {
       if (alertObj) {
         alertObj.hide();
       }
 
-      obj.duration = obj.type === 'success' ? SUCCESS_ALERT_DURATION : false;
+      obj.duration = obj.type === 'success' || obj.type === 'info' ? SUCCESS_ALERT_DURATION : false;
       alertObj = $alert(obj);
       if (obj.templateUrl) {
         alertObj.$scope.templateScope = obj.templateScope;
