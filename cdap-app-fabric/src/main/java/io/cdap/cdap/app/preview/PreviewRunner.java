cdapio/cdap-ui

CDAP-16987 Keep single store for all preview runs owned by PreviewManager.

cdap-app-fabric/src/main/java/io/cdap/cdap/app/preview/PreviewRunner.java
@@ -17,14 +17,15 @@
 package io.cdap.cdap.app.preview;
 
 import com.google.gson.JsonElement;
+import io.cdap.cdap.common.NotFoundException;
 import io.cdap.cdap.internal.app.store.RunRecordDetail;
 import io.cdap.cdap.metrics.query.MetricsQueryHelper;
+import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.ProgramRunId;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import javax.annotation.Nullable;
 
 /**
  * Interface responsible for managing the lifecycle of a single preview application
@@ -33,27 +34,26 @@ import javax.annotation.Nullable;
 public interface PreviewRunner {
 
   /**
-   * Returns the {@link PreviewRequest} for this runner.
-   */
-  PreviewRequest getPreviewRequest();
-
-  /**
    * Start the preview of an application.
-   * @throws Exception if there were any error during starting preview
+   * @param previewRequest request representing preview
+   * @throws Exception when there is error while starting preview
    */
-  void startPreview() throws Exception;
+  void startPreview(PreviewRequest previewRequest) throws Exception;
 
   /**
-   * Get the status of the preview represented by this {@link PreviewRunner}.
-   * @return the status associated with the preview
+   * Get the status of the preview.
+   * @param applicationId id of the preview application for which preview status is to be returned
+   * @return the status of the preview
+   * @throws NotFoundException if preview application is not found
    */
-  PreviewStatus getStatus();
+  PreviewStatus getStatus(ApplicationId applicationId) throws NotFoundException;
 
   /**
-   * Stop the preview run represented by this {@link PreviewRunner}.
-   * @throws Exception if there were any error during stopping preview
+   * Stop the preview run represented by this {@link ApplicationId}.
+   * @param applicationId id of the preview
+   * @throws Exception thrown when any error in stopping the preview run
    */
-  void stopPreview() throws Exception;
+  void stopPreview(ApplicationId applicationId) throws Exception;
 
   /**
    * Get list of tracers used in the preview run represented by this {@link PreviewRunner}.
@@ -63,23 +63,18 @@ public interface PreviewRunner {
 
   /**
    * Get the data associated with the preview run represented by this {@link PreviewRunner}.
+   * @param applicationId the id of the preview application
    * @param tracerName the name of the tracer used for preview
    * @return the {@link Map} of properties associated with the tracer for a given preview
    */
-  Map<String, List<JsonElement>> getData(String tracerName);
-
-  /**
-   * Get the run id of the program executed as a part of preview.
-   * @return the {@link ProgramRunId} associated with the preview
-   */
-  ProgramRunId getProgramRunId();
+  Map<String, List<JsonElement>> getData(ApplicationId applicationId, String tracerName);
 
   /**
    * Get the run record of the program executed as a part of preview.
-   * @return the {@link RunRecordDetail} associated with the preview or {@code null} if there is no run record
+   * @param applicationId the id of the preview application
+   * @return the {@link ProgramRunId} associated with the preview or {@code null} if there is no run record
    */
-  @Nullable
-  RunRecordDetail getRunRecord();
+  RunRecordDetail getRunRecord(ApplicationId applicationId) throws Exception;
 
   /**
    * Get the helper object to query for metrics for the preview run.

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/preview/PreviewManagerTest.java
@@ -24,7 +24,6 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import io.cdap.cdap.api.artifact.ArtifactSummary;
 import io.cdap.cdap.app.guice.AppFabricServiceRuntimeModule;
@@ -68,8 +67,8 @@ import io.cdap.cdap.metrics.query.MetricsQueryHelper;
 import io.cdap.cdap.proto.ProgramType;
 import io.cdap.cdap.proto.artifact.AppRequest;
 import io.cdap.cdap.proto.artifact.preview.PreviewConfig;
+import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.NamespaceId;
-import io.cdap.cdap.proto.id.ProgramRunId;
 import io.cdap.cdap.security.authorization.AuthorizationEnforcementModule;
 import io.cdap.cdap.security.authorization.AuthorizerInstantiator;
 import io.cdap.cdap.security.guice.SecureStoreServerModule;
@@ -170,19 +169,6 @@ public class PreviewManagerTest {
     txManager.stopAndWait();
   }
 
-  @Test(expected = IllegalStateException.class)
-  public void testLimit() throws Exception {
-    PreviewConfig previewConfig = new PreviewConfig("test", ProgramType.WORKFLOW, null, null);
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-
-    // We allow 2 concurrent previews, so running the third one would fail
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-  }
-
   @Test
   public void testReclaim() throws Exception {
     PreviewConfig previewConfig = new PreviewConfig("test", ProgramType.WORKFLOW, null, null);
@@ -211,11 +197,10 @@ public class PreviewManagerTest {
                             PrivilegesManager privilegesManager, PreferencesService preferencesService,
                             ProgramRuntimeProviderLoader programRuntimeProviderLoader,
                             PluginFinderProvider pluginFinderProvider,
-                            PreferencesFetcherProvider preferencesFetcherProvider,
-                            @Assisted PreviewRequest previewRequest) {
+                            PreferencesFetcherProvider preferencesFetcherProvider) {
       super(readerProvider, artifactStore, authorizerInstantiator, authorizationEnforcer,
             privilegesManager, preferencesService, programRuntimeProviderLoader, pluginFinderProvider,
-            preferencesFetcherProvider, previewRequest);
+            preferencesFetcherProvider);
     }
 
     @Override
@@ -229,31 +214,22 @@ public class PreviewManagerTest {
    */
   private static final class MockPreviewRunner implements PreviewRunner {
 
-    private final PreviewRequest previewRequest;
-
     @Inject
-    MockPreviewRunner(PreviewRequest previewRequest) {
-      this.previewRequest = previewRequest;
-    }
-
-
-    @Override
-    public PreviewRequest getPreviewRequest() {
-      return previewRequest;
+    MockPreviewRunner() {
     }
 
     @Override
-    public void startPreview() {
+    public void startPreview(PreviewRequest previewRequest) {
       // no-op
     }
 
     @Override
-    public PreviewStatus getStatus() {
+    public PreviewStatus getStatus(ApplicationId applicationId) {
       return PREVIEW_STATUS.get();
     }
 
     @Override
-    public void stopPreview() {
+    public void stopPreview(ApplicationId applicationId) {
       // no-op
     }
 
@@ -263,17 +239,12 @@ public class PreviewManagerTest {
     }
 
     @Override
-    public Map<String, List<JsonElement>> getData(String tracerName) {
+    public Map<String, List<JsonElement>> getData(ApplicationId applicationId, String tracerName) {
       return null;
     }
 
     @Override
-    public ProgramRunId getProgramRunId() {
-      return null;
-    }
-
-    @Override
-    public RunRecordDetail getRunRecord() {
+    public RunRecordDetail getRunRecord(ApplicationId applicationId) {
       return null;
     }
 

cdapio/cdap

cdap-app-fabric/src/main/java/io/cdap/cdap/app/preview/PreviewRunner.java
@@ -17,9 +17,10 @@
 package io.cdap.cdap.app.preview;
 
 import com.google.gson.JsonElement;
+import io.cdap.cdap.common.NotFoundException;
 import io.cdap.cdap.internal.app.store.RunRecordMeta;
 import io.cdap.cdap.metrics.query.MetricsQueryHelper;
-import io.cdap.cdap.proto.id.ProgramRunId;
+import io.cdap.cdap.proto.id.ApplicationId;
 
 import java.util.List;
 import java.util.Map;
@@ -33,27 +34,26 @@ import javax.annotation.Nullable;
 public interface PreviewRunner {
 
   /**
-   * Returns the {@link PreviewRequest} for this runner.
-   */
-  PreviewRequest getPreviewRequest();
-
-  /**
    * Start the preview of an application.
-   * @throws Exception if there were any error during starting preview
+   * @param previewRequest request representing preview
+   * @throws Exception when there is error while starting preview
    */
-  void startPreview() throws Exception;
+  void startPreview(PreviewRequest previewRequest) throws Exception;
 
   /**
-   * Get the status of the preview represented by this {@link PreviewRunner}.
-   * @return the status associated with the preview
+   * Get the status of the preview.
+   * @param applicationId id of the preview application for which preview status is to be returned
+   * @return the status of the preview
+   * @throws NotFoundException if preview application is not found
    */
-  PreviewStatus getStatus();
+  PreviewStatus getStatus(ApplicationId applicationId) throws NotFoundException;
 
   /**
-   * Stop the preview run represented by this {@link PreviewRunner}.
-   * @throws Exception if there were any error during stopping preview
+   * Stop the preview run represented by this {@link ApplicationId}.
+   * @param applicationId id of the preview
+   * @throws Exception thrown when any error in stopping the preview run
    */
-  void stopPreview() throws Exception;
+  void stopPreview(ApplicationId applicationId) throws Exception;
 
   /**
    * Get list of tracers used in the preview run represented by this {@link PreviewRunner}.
@@ -63,23 +63,20 @@ public interface PreviewRunner {
 
   /**
    * Get the data associated with the preview run represented by this {@link PreviewRunner}.
+   * @param applicationId the id of the preview application
    * @param tracerName the name of the tracer used for preview
    * @return the {@link Map} of properties associated with the tracer for a given preview
    */
-  Map<String, List<JsonElement>> getData(String tracerName);
-
-  /**
-   * Get the run id of the program executed as a part of preview.
-   * @return the {@link ProgramRunId} associated with the preview
-   */
-  ProgramRunId getProgramRunId();
+  Map<String, List<JsonElement>> getData(ApplicationId applicationId, String tracerName);
 
   /**
    * Get the run record of the program executed as a part of preview.
+   * @param applicationId the id of the preview application
    * @return the {@link RunRecordMeta} associated with the preview or {@code null} if there is no run record
+   * @throws Exception when there is error while getting run record
    */
   @Nullable
-  RunRecordMeta getRunRecord();
+  RunRecordMeta getRunRecord(ApplicationId applicationId) throws Exception;
 
   /**
    * Get the helper object to query for metrics for the preview run.

cdap-app-fabric/src/test/java/io/cdap/cdap/internal/app/preview/PreviewManagerTest.java
@@ -24,7 +24,6 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import io.cdap.cdap.api.artifact.ArtifactSummary;
 import io.cdap.cdap.app.guice.AppFabricServiceRuntimeModule;
@@ -66,8 +65,8 @@ import io.cdap.cdap.metrics.query.MetricsQueryHelper;
 import io.cdap.cdap.proto.ProgramType;
 import io.cdap.cdap.proto.artifact.AppRequest;
 import io.cdap.cdap.proto.artifact.preview.PreviewConfig;
+import io.cdap.cdap.proto.id.ApplicationId;
 import io.cdap.cdap.proto.id.NamespaceId;
-import io.cdap.cdap.proto.id.ProgramRunId;
 import io.cdap.cdap.security.authorization.AuthorizationEnforcementModule;
 import io.cdap.cdap.security.authorization.AuthorizerInstantiator;
 import io.cdap.cdap.security.guice.SecureStoreServerModule;
@@ -168,19 +167,6 @@ public class PreviewManagerTest {
     txManager.stopAndWait();
   }
 
-  @Test(expected = IllegalStateException.class)
-  public void testLimit() throws Exception {
-    PreviewConfig previewConfig = new PreviewConfig("test", ProgramType.WORKFLOW, null, null);
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-
-    // We allow 2 concurrent previews, so running the third one would fail
-    previewManager.start(NamespaceId.DEFAULT, new AppRequest<>(new ArtifactSummary("test", "1.0"), null,
-                                                               previewConfig));
-  }
-
   @Test
   public void testReclaim() throws Exception {
     PreviewConfig previewConfig = new PreviewConfig("test", ProgramType.WORKFLOW, null, null);
@@ -207,10 +193,9 @@ public class PreviewManagerTest {
     MockPreviewRunnerModule(ArtifactRepository artifactRepository, ArtifactStore artifactStore,
                             AuthorizerInstantiator authorizerInstantiator, AuthorizationEnforcer authorizationEnforcer,
                             PrivilegesManager privilegesManager, PreferencesService preferencesService,
-                            ProgramRuntimeProviderLoader programRuntimeProviderLoader,
-                            @Assisted PreviewRequest previewRequest) {
+                            ProgramRuntimeProviderLoader programRuntimeProviderLoader) {
       super(artifactRepository, artifactStore, authorizerInstantiator, authorizationEnforcer,
-            privilegesManager, preferencesService, programRuntimeProviderLoader, previewRequest);
+            privilegesManager, preferencesService, programRuntimeProviderLoader);
     }
 
     @Override
@@ -224,31 +209,22 @@ public class PreviewManagerTest {
    */
   private static final class MockPreviewRunner implements PreviewRunner {
 
-    private final PreviewRequest previewRequest;
-
     @Inject
-    MockPreviewRunner(PreviewRequest previewRequest) {
-      this.previewRequest = previewRequest;
-    }
-
-
-    @Override
-    public PreviewRequest getPreviewRequest() {
-      return previewRequest;
+    MockPreviewRunner() {
     }
 
     @Override
-    public void startPreview() {
+    public void startPreview(PreviewRequest previewRequest) {
       // no-op
     }
 
     @Override
-    public PreviewStatus getStatus() {
+    public PreviewStatus getStatus(ApplicationId applicationId) {
       return PREVIEW_STATUS.get();
     }
 
     @Override
-    public void stopPreview() {
+    public void stopPreview(ApplicationId applicationId) {
       // no-op
     }
 
@@ -258,17 +234,12 @@ public class PreviewManagerTest {
     }
 
     @Override
-    public Map<String, List<JsonElement>> getData(String tracerName) {
+    public Map<String, List<JsonElement>> getData(ApplicationId applicationId, String tracerName) {
       return null;
     }
 
     @Override
-    public ProgramRunId getProgramRunId() {
-      return null;
-    }
-
-    @Override
-    public RunRecordMeta getRunRecord() {
+    public RunRecordMeta getRunRecord(ApplicationId applicationId) {
       return null;
     }
 