13c50c43d37ea000e6312f6e9d56ec87eec02da2
yukiej <yukijung@google.com>
2020-07-30 17:06:53 -0400

    Fix stop preview callback to remove empty error banner and allow retry to stop preview
    Reset timer label when backend status returns error
    
    Let user retry stopping preview run if preview stop returns error from backend
    
    Stop polling for preview status and show stop success banner as long as backend does not reply with error when stopping  preview

--- cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
+++ cdap-ui/app/hydrator/controllers/create/toppanel-ctrl.js
@@ -678,15 +678,31 @@ class HydratorPlusPlusTopPanelCtrl {
     };
     this.previewLoading = true;
     this.loadingLabel = 'Stopping';
-    this.stopTimer();
-    this.updateTimerLabelAndTitle();
     this.myPipelineApi
         .stopPreview(params, {})
         .$promise
         .then(
-          (err) => {
+          () => {
+            this.stopTimer();
+            this.updateTimerLabelAndTitle();
             this.previewLoading = false;
             this.previewRunning = false;
+            this.dataSrc.stopPoll(this.pollId);
+            this.pollId = null;
+
+            let pipelinePreviewPlaceholder = 'The preview of the pipeline';
+            let pipelineName = this.HydratorPlusPlusConfigStore.getName();
+            if (pipelineName.length > 0) {
+              pipelinePreviewPlaceholder += ` "${pipelineName}"`;
+            }
+            this.myAlertOnValium.show({
+              type: 'success',
+              content: `${pipelinePreviewPlaceholder} was stopped.`
+            });
+          },
+          (err) => {
+            this.previewLoading = false;
+            this.previewRunning = true;
             this.myAlertOnValium.show({type: 'danger', content: err.data});
         });
   }
@@ -698,6 +714,7 @@ class HydratorPlusPlusTopPanelCtrl {
       _cdapNsPath: '/previews/' + previewId + '/status',
       interval: 1000
     }, (res) => {
+      this.pollId = res.__pollId__;
       if (this.previewStore) {
         this.previewStore.dispatch({
           type: this.PREVIEWSTORE_ACTIONS.SET_PREVIEW_STATUS,
@@ -722,6 +739,7 @@ class HydratorPlusPlusTopPanelCtrl {
         this.stopTimer();
         this.previewRunning = false;
         this.dataSrc.stopPoll(res.__pollId__);
+        this.pollId = null;
         let pipelinePreviewPlaceholder = 'The preview of the pipeline';
         let pipelineName = this.HydratorPlusPlusConfigStore.getName();
         if (pipelineName.length > 0) {
@@ -732,11 +750,6 @@ class HydratorPlusPlusTopPanelCtrl {
             type: 'success',
             content: `${pipelinePreviewPlaceholder} has completed successfully.`
           });
-        } else if (res.status === KILLED) {
-          this.myAlertOnValium.show({
-            type: 'success',
-            content: `${pipelinePreviewPlaceholder} was stopped.`
-          });
         } else if (res.status === DEPLOY_FAILED || res.status === RUN_FAILED) {
           this.myAlertOnValium.show({
             type: 'danger',
@@ -746,6 +759,7 @@ class HydratorPlusPlusTopPanelCtrl {
       }
     }, (err) => {
       this.stopTimer();
+      this.updateTimerLabelAndTitle();
       let errorMsg = this.myHelpers.extractErrorMessage(err);
       this.myAlertOnValium.show({
         type: 'danger',
@@ -753,6 +767,7 @@ class HydratorPlusPlusTopPanelCtrl {
       });
       this.previewRunning = false;
       this.dataSrc.stopPoll(poll.__pollId__);
+      this.pollId = null;
     });
   }
 
