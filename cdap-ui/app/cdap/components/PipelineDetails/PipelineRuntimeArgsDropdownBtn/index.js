002f7eb32dd0ddd8bb9f6e320b1dabfa5ac14bbe
Nick Briggs <njbriggs@google.com>
2020-11-12 15:12:39 -0800

    Convert the Pipeline Run arguments dropdown to use material-ui Popover

--- cdap-ui/app/cdap/components/PipelineDetails/PipelineRuntimeArgsDropdownBtn/index.js
+++ cdap-ui/app/cdap/components/PipelineDetails/PipelineRuntimeArgsDropdownBtn/index.js
@@ -19,12 +19,11 @@ import React, { Component } from 'react';
 import IconSVG from 'components/IconSVG';
 import PipelineModeless from 'components/PipelineDetails/PipelineModeless';
 import classnames from 'classnames';
-import Popover from 'components/Popover';
 import { Provider } from 'react-redux';
 import PipelineConfigurationsStore from 'components/PipelineConfigurations/Store';
 import RuntimeArgsModeless from 'components/PipelineDetails/PipelineRuntimeArgsDropdownBtn/RuntimeArgsModeless';
 import { fetchAndUpdateRuntimeArgs } from 'components/PipelineConfigurations/Store/ActionCreator';
-import { preventPropagation } from 'services/helpers';
+import Popover from '@material-ui/core/Popover';
 
 require('./PipelineRuntimeArgsDropdownBtn.scss');
 
@@ -43,13 +42,11 @@ export default class PipelineRuntimeArgsDropdownBtn extends Component {
     showRunOptions: this.props.showRunOptions,
   };
 
-  toggleRunConfigOption = (showRunOptions) => {
-    if (showRunOptions === this.state.showRunOptions) {
-      return;
-    }
+  toggleRunConfigOption = (anchorEl) => {
     this.setState(
       {
-        showRunOptions: showRunOptions || !this.state.showRunOptions,
+        showRunOptions: !this.state.showRunOptions,
+        anchorEl,
       },
       () => {
         // FIXME: This is to when the user opens/closes the runtime args modeless.
@@ -73,60 +70,44 @@ export default class PipelineRuntimeArgsDropdownBtn extends Component {
   };
 
   render() {
-    const Btn = (
-      <div
-        className={classnames('btn pipeline-action-btn pipeline-run-btn', {
-          'btn-popover-open': this.state.showRunOptions,
-        })}
-        onClick={(e) => {
-          /*
-            ugh. This is NOT a good approach.
-            The react-popper should have had a disabled prop that
-            which passed on should just disable the popover like a button.
-            Right now I am circumventing that by preventing the propagation/bubbling the event
-            here so it looks like it is disabled.
-
-            We should upgrade react-popper and see if later versions have this prop.
-          */
-          if (this.props.disabled) {
-            preventPropagation(e);
-            return false;
-          }
-        }}
-      >
-        <IconSVG name="icon-caret-down" />
-      </div>
-    );
     return (
       <fieldset disabled={this.props.disabled}>
-        <Provider store={PipelineConfigurationsStore}>
-          <Popover
-            target={() => Btn}
-            className="arrow-btn-container"
-            placement="bottom"
-            enableInteractionInPopover={true}
-            showPopover={this.state.showRunOptions}
-            onTogglePopover={this.toggleRunConfigOption}
-            injectOnToggle={true}
-            modifiers={{
-              flip: {
-                enabled: true,
-                behavior: ['bottom'],
-              },
-              preventOverflow: {
-                enabled: true,
-                boundariesElement: 'scrollParent',
-              },
+        <div className="arrow-btn-container">
+          <div
+            className={classnames('btn pipeline-action-btn pipeline-run-btn', {
+              'btn-popover-open': this.state.showRunOptions,
+            })}
+            onClick={(e) => {
+              if (!this.props.disabled) {
+                this.toggleRunConfigOption(e.currentTarget);
+              }
             }}
           >
+            <IconSVG name="icon-caret-down" />
+          </div>
+        </div>
+        <Popover
+          open={this.state.showRunOptions}
+          onClose={this.toggleRunConfigOption}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <Provider store={PipelineConfigurationsStore}>
             <PipelineModeless
               title="Runtime Arguments"
               onClose={this.toggleRunConfigOption.bind(this, false)}
             >
               <RuntimeArgsModeless onClose={this.toggleRunConfigOption} />
             </PipelineModeless>
-          </Popover>
-        </Provider>
+          </Provider>
+        </Popover>
       </fieldset>
     );
   }
