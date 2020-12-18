aaa465910fed4afeff95fab47e97270055860843
Ed Maciulis <edvinas.maciulis@liveramp.com>
2020-12-18 21:30:21 +0000

    [CDAP-17466] Implementation

--- cdap-master/src/main/java/io/cdap/cdap/master/environment/k8s/AuthenticationServiceMain.java
+++ cdap-master/src/main/java/io/cdap/cdap/master/environment/k8s/AuthenticationServiceMain.java
@@ -25,17 +25,17 @@ import com.google.inject.Module;
 import io.cdap.cdap.common.ServiceBindException;
 import io.cdap.cdap.common.conf.CConfiguration;
 import io.cdap.cdap.common.conf.Constants;
-import io.cdap.cdap.common.guice.DFSLocationModule;
 import io.cdap.cdap.common.guice.ZKClientModule;
 import io.cdap.cdap.common.logging.LoggingContext;
 import io.cdap.cdap.common.logging.ServiceLoggingContext;
-import io.cdap.cdap.gateway.router.NettyRouter;
-import io.cdap.cdap.gateway.router.RouterModules;
 import io.cdap.cdap.master.spi.environment.MasterEnvironment;
 import io.cdap.cdap.master.spi.environment.MasterEnvironmentContext;
+import io.cdap.cdap.messaging.MessagingService;
 import io.cdap.cdap.messaging.guice.MessagingClientModule;
 import io.cdap.cdap.proto.id.NamespaceId;
 import io.cdap.cdap.security.guice.SecurityModules;
+import io.cdap.cdap.security.impersonation.SecurityUtil;
+import io.cdap.cdap.security.server.ExternalAuthenticationServer;
 import org.apache.twill.internal.Services;
 import org.apache.twill.zookeeper.ZKClientService;
 import org.slf4j.Logger;
@@ -44,61 +44,55 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.Nullable;
 
 /**
- * The main class to run router service.
+ * The main class responsible for Authentication  .
  */
-public class RouterServiceMain extends AbstractServiceMain<EnvironmentOptions> {
+public class AuthenticationServiceMain extends AbstractServiceMain<EnvironmentOptions> {
 
-  private static final Logger LOG = LoggerFactory.getLogger(RouterServiceMain.class);
+  private static final Logger LOG = LoggerFactory.getLogger(AuthenticationServiceMain.class);
 
-  /**
-   * Main entry point
-   */
   public static void main(String[] args) throws Exception {
-    main(RouterServiceMain.class, args);
+    main(AuthenticationServiceMain.class, args);
   }
 
   @Override
   protected List<Module> getServiceModules(
-      MasterEnvironment masterEnv,
-      EnvironmentOptions options, CConfiguration cConf) {
+      MasterEnvironment masterEnv, EnvironmentOptions options, CConfiguration cConf) {
     return Arrays.asList(
-        new MessagingClientModule(),
-        new RouterModules().getDistributedModules(),
-        new DFSLocationModule(),
+        getDataFabricModule(),
         new ZKClientModule(),
-        securityModuleBasedOn(cConf)
-    );
-  }
-
-  private Module securityModuleBasedOn(CConfiguration cConf) {
-    if (cConf.getBoolean(Constants.Security.ENABLED)) {
-      return new SecurityModules().getDistributedModules();
-    } else {
-      return new SecurityModules().getStandaloneModules();
-    }
+        new SecurityModules().getDistributedModules(),
+        new MessagingClientModule());
   }
 
   @Override
   protected void addServices(
-      Injector injector, List<? super Service> services,
-      List<? super AutoCloseable> closeableResources,
-      MasterEnvironment masterEnv, MasterEnvironmentContext masterEnvContext,
-      EnvironmentOptions options) {
+      Injector injector, List<? super Service> services, List<? super AutoCloseable> closeableResources,
+      MasterEnvironment masterEnv, MasterEnvironmentContext masterEnvContext, EnvironmentOptions options) {
+
+    MessagingService messagingService = injector.getInstance(MessagingService.class);
+    if (messagingService instanceof Service) {
+      services.add((Service) messagingService);
+    }
+
     CConfiguration configuration = injector.getInstance(CConfiguration.class);
 
     if (configuration.getBoolean(Constants.Security.ENABLED)) {
+
       services.add(new AbstractService() {
 
         @Override
         protected void doStart() {
 
           ZKClientService zkClientService = injector.getInstance(ZKClientService.class);
+          ExternalAuthenticationServer authServer = injector.getInstance(ExternalAuthenticationServer.class);
 
           try {
-            LOG.info("Connecting to Zookeeper.");
+            LOG.info("Starting AuthenticationServer.");
+
+            // Enable Kerberos login
+            SecurityUtil.enableKerberosLogin(configuration);
 
             io.cdap.cdap.common.service.Services.startAndWait(
                 zkClientService,
@@ -111,14 +105,15 @@ public class RouterServiceMain extends AbstractServiceMain<EnvironmentOptions> {
                         "ZooKeeper quorum settings are correct in " +
                         "cdap-site.xml. Currently configured as: %s",
                     configuration.get(Constants.Zookeeper.QUORUM)));
+            authServer.startAndWait();
 
             notifyStarted();
           } catch (Exception e) {
             Throwable rootCause = Throwables.getRootCause(e);
             if (rootCause instanceof ServiceBindException) {
-              LOG.error("Failed to connect to Zookeeper: {}", rootCause.getMessage());
+              LOG.error("Failed to start Authentication Server: {}", rootCause.getMessage());
             } else {
-              LOG.error("Failed to connect to Zookeeper", e);
+              LOG.error("Failed to start Authentication Server", e);
             }
           }
         }
@@ -127,29 +122,31 @@ public class RouterServiceMain extends AbstractServiceMain<EnvironmentOptions> {
         protected void doStop() {
 
           ZKClientService zkClientService = injector.getInstance(ZKClientService.class);
+          ExternalAuthenticationServer authServer = injector.getInstance(ExternalAuthenticationServer.class);
 
-          LOG.info("Stopping Zookeeper client service.");
-          Futures.getUnchecked(Services.chainStop(zkClientService));
+          LOG.info("Stopping AuthenticationServer.");
+          Futures.getUnchecked(Services.chainStop(authServer, zkClientService));
 
           notifyStopped();
         }
       });
-    }
 
-    services.add(injector.getInstance(NettyRouter.class));
+      services.add(injector.getInstance(ZKClientService.class));
+      services.add(injector.getInstance(ExternalAuthenticationServer.class));
+    } else {
+      String warning = "AuthenticationServer not started since security is disabled." +
+          " To enable security, set \"security.enabled\" = \"true\" in cdap-site.xml" +
+          " and edit the appropriate configuration.";
+      LOG.warn(warning);
+    }
   }
 
-  @Nullable
   @Override
-  protected LoggingContext getLoggingContext(EnvironmentOptions options) {
+  protected LoggingContext getLoggingContext(
+      EnvironmentOptions options) {
     return new ServiceLoggingContext(
         NamespaceId.SYSTEM.getNamespace(),
         Constants.Logging.COMPONENT_NAME,
-        Constants.Service.GATEWAY);
-  }
-
-  @Override
-  protected void initializeDataSourceConnection(CConfiguration cConf) {
-    // no-op since we don't connect to dataset in router service
+        Constants.Service.AUTHENTICATION);
   }
 }
---
+++ cdap-master/src/test/java/io/cdap/cdap/master/environment/k8s/RouterServiceMainWithSecurityEnabledTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.master.environment.k8s;
+
+import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants.Security;
+import io.cdap.cdap.common.conf.SConfiguration;
+import io.cdap.cdap.common.http.DefaultHttpRequestConfig;
+import io.cdap.cdap.security.server.BasicAuthenticationHandler;
+import io.cdap.common.http.HttpRequest;
+import io.cdap.common.http.HttpRequests;
+import io.cdap.common.http.HttpResponse;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RouterServiceMainWithSecurityEnabledTest extends MasterServiceMainTestBase {
+
+  @BeforeClass
+  public static void init() throws Exception {
+    final CConfiguration cConf = CConfiguration.create();
+    cConf.setBoolean(Security.ENABLED, true);
+    cConf.set(Security.AUTH_HANDLER_CLASS, BasicAuthenticationHandler.class.getName());
+    cConf.setBoolean(Security.KERBEROS_ENABLED, false);
+    cConf.set(Security.BASIC_REALM_FILE, realmFile());
+    MasterServiceMainTestBase.cConf = cConf;
+
+    final SConfiguration sConf = SConfiguration.create();
+    sConf.set(Security.AuthenticationServer.SSL_KEYSTORE_PATH, "src/test/resources/KeyStore.jks");
+    sConf.set(Security.AuthenticationServer.SSL_KEYSTORE_PASSWORD, "123456");
+    MasterServiceMainTestBase.sConf = sConf;
+
+    MasterServiceMainTestBase.init();
+  }
+
+  private static String realmFile() throws IOException {
+    File tmpFile = File.createTempFile("basicrealm", ".tmp");
+    FileWriter writer = new FileWriter(tmpFile);
+    writer.write("router: router");
+    writer.close();
+
+    return tmpFile.getAbsolutePath();
+  }
+
+  @Test
+  public void testRouterServiceWithAuthenticationEnabled() throws Exception {
+    URL url = getRouterBaseURI().resolve("/").toURL();
+    HttpResponse response = HttpRequests.execute(HttpRequest.get(url).build(), new DefaultHttpRequestConfig(false));
+
+    Assert.assertEquals(HttpURLConnection.HTTP_UNAUTHORIZED, response.getResponseCode());
+  }
+}
