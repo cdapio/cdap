06509fd28b013683bcf8f0a8278533a6ce5b8bcf
Edwin Elia <edwinelia@google.com>
2021-01-07 19:01:02 -0700

    [CDAP-17550] replication label changes

--- cdap-ui/app/cdap/components/Replicator/Create/steps.tsx
+++ cdap-ui/app/cdap/components/Replicator/Create/steps.tsx
@@ -24,7 +24,7 @@ import Summary from 'components/Replicator/Create/Content/Summary';
 
 export const STEPS = [
   {
-    label: 'Name replication pipeline',
+    label: 'Specify basic information',
     component: NameDescription,
   },
   {
