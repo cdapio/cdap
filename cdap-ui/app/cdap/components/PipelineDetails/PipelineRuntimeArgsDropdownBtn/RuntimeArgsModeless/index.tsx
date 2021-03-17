1cb92a49de4eeb030019d25bb984d8b556054ae9
Edwin Elia <edwinelia@google.com>
2021-03-17 16:12:57 -0600

    [CDAP-17791] Fix error message in runtime arguments modaless to wrap

--- cdap-ui/app/cdap/components/PipelineDetails/PipelineRuntimeArgsDropdownBtn/RuntimeArgsModeless/index.tsx
+++ cdap-ui/app/cdap/components/PipelineDetails/PipelineRuntimeArgsDropdownBtn/RuntimeArgsModeless/index.tsx
@@ -30,7 +30,6 @@ import T from 'i18n-react';
 import If from 'components/If';
 import LoadingSVGCentered from 'components/LoadingSVGCentered';
 import withStyles, { StyleRules, WithStyles } from '@material-ui/styles/withStyles';
-import Button from '@material-ui/core/Button';
 
 const styles = (theme): StyleRules => {
   return {
@@ -39,7 +38,7 @@ const styles = (theme): StyleRules => {
       gridTemplateRows: 'auto',
       margin: '0',
       textAlign: 'left',
-      width: '100%',
+      width: '730px',
       padding: '10px 30px 30px',
     },
     loading: {
