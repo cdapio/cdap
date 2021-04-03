e964e3e2d2d2382c246f31263c570c622d7ba3f6
wyzhang <wyzhang@google.com>
2021-04-03 02:54:21 -0700

    Adding http handler for task worker

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/dispatcher/TaskWorkerService.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/dispatcher/TaskWorkerService.java
@@ -16,50 +16,85 @@
 
 package io.cdap.cdap.internal.app.dispatcher;
 
-import com.google.common.util.concurrent.AbstractExecutionThreadService;
+import com.google.common.util.concurrent.AbstractIdleService;
 import com.google.inject.Inject;
 import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.common.conf.SConfiguration;
+import io.cdap.cdap.common.discovery.ResolvingDiscoverable;
+import io.cdap.cdap.common.discovery.URIScheme;
+import io.cdap.cdap.common.http.CommonNettyHttpServiceBuilder;
+import io.cdap.cdap.common.security.HttpsEnabler;
+import io.cdap.http.NettyHttpService;
+import org.apache.twill.common.Cancellable;
+import org.apache.twill.discovery.DiscoveryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.net.InetSocketAddress;
 
 /**
  * A scheduled service that periodically poll for new preview request and execute it.
  */
-public class TaskWorkerService extends AbstractExecutionThreadService {
+public class TaskWorkerService extends AbstractIdleService {
 
   private static final Logger LOG = LoggerFactory.getLogger(TaskWorkerService.class);
 
-  private CConfiguration cConf;
-  private AtomicBoolean cancelRequested;
+  private final CConfiguration cConf;
+  private final SConfiguration sConf;
+  private final DiscoveryService discoveryService;
+  private NettyHttpService httpService;
+  private Cancellable cancelDiscovery;
+  private InetSocketAddress bindAddress;
 
   @Inject
-  TaskWorkerService(CConfiguration cConf) {
+  TaskWorkerService(CConfiguration cConf,
+                    SConfiguration sConf,
+                    DiscoveryService discoveryService) {
     this.cConf = cConf;
-    this.cancelRequested = new AtomicBoolean(false);
-  }
-
-  @Override
-  protected void triggerShutdown() {
-    cancelRequested.set(true);
+    this.sConf = sConf;
+    this.discoveryService = discoveryService;
   }
 
   @Override
   protected void startUp() throws Exception {
-    LOG.debug("Starting task worker service");
-  }
-
-  @Override
-  protected void run() throws Exception {
-    while(!cancelRequested.get()) {
-      LOG.debug("TaskWorkerService::run()");
-      Thread.sleep(10000);
-    }
+    LOG.debug("Starting TaskWorker service");
+    launchHttpServer();
+    LOG.debug("Starting TaskWorker service has completed");
   }
 
   @Override
   protected void shutDown() throws Exception {
-    LOG.debug("Task worker service shutdown completed");
+    LOG.debug("Shutting down TaskWorker service");
+    shutdownHttpServer();
+    LOG.debug("Shutting down TaskWorker service has completed");
+  }
+
+  private void launchHttpServer() throws Exception {
+    LOG.info("Starting TaskWorker http server");
+    NettyHttpService.Builder builder = new CommonNettyHttpServiceBuilder(cConf, Constants.Service.TASK_WORKER)
+      .setHost(cConf.get(Constants.TaskWorker.ADDRESS))
+      .setPort(cConf.getInt(Constants.TaskWorker.PORT))
+      .setExecThreadPoolSize(cConf.getInt(Constants.TaskWorker.EXEC_THREADS))
+      .setBossThreadPoolSize(cConf.getInt(Constants.TaskWorker.BOSS_THREADS))
+      .setWorkerThreadPoolSize(cConf.getInt(Constants.TaskWorker.WORKER_THREADS))
+      .setHttpHandlers(new TaskWorkerHttpHandlerInternal(this.cConf));
+
+    if (cConf.getBoolean(Constants.Security.SSL.INTERNAL_ENABLED)) {
+      new HttpsEnabler().configureKeyStore(cConf, sConf).enable(builder);
+    }
+    httpService = builder.build();
+
+    httpService.start();
+    bindAddress = httpService.getBindAddress();
+    cancelDiscovery = discoveryService.register(
+      ResolvingDiscoverable.of(URIScheme.createDiscoverable(Constants.Service.TASK_WORKER, httpService)));
+
+    LOG.info("Starting TaskWorker http server has completed on {}", bindAddress);
+  }
+
+  private void shutdownHttpServer() throws Exception {
+    cancelDiscovery.cancel();
+    httpService.stop();
   }
 }
