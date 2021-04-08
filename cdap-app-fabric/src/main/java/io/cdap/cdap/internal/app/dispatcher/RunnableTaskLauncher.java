f57a910f6b13de4a81c947920e50c6c756061a85
Masoud Saeida Ardekani <ardekani@google.com>
2021-04-08 16:14:40 -0700

    Add RunnableTaskLauncher to load task runnable class, and run the task

---
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/dispatcher/RunnableTaskLauncher.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.internal.app.dispatcher;
+
+import com.google.common.util.concurrent.Service;
+
+/**
+ * RunnableTaskLauncher launches a runnable task.
+ */
+public class RunnableTaskLauncher {
+
+  public String launchRunnableTask(RunnableTaskRequest request) throws Exception {
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    Class<?> cClass = classLoader.loadClass(request.className);
+    Object obj = cClass.getDeclaredConstructor().newInstance();
+    if (!(obj instanceof RunnableTask)) {
+      throw new ClassCastException(String.format("%s is not a RunnableTask", request.className));
+    }
+    RunnableTask runnableTask = (RunnableTask) obj;
+    if (runnableTask.start().get() != Service.State.RUNNING) {
+      throw new Exception(String.format("service %s failed to start", request.className));
+    }
+    return runnableTask.runTask(request.param);
+  }
+}
