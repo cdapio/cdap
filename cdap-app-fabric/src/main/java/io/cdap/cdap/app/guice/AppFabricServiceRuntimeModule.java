23cb3678af28cff02f7d8671abd3618c8e76dec7
Masoud Saeida Ardekani <ardekani@google.com>
2021-04-15 16:09:23 -0700

    Undo AppFabricServiceRuntimeModule changes

--- cdap-app-fabric/src/main/java/io/cdap/cdap/app/guice/AppFabricServiceRuntimeModule.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/app/guice/AppFabricServiceRuntimeModule.java
@@ -288,7 +288,6 @@ public final class AppFabricServiceRuntimeModule extends RuntimeModule {
       bind(PipelineFactory.class).to(SynchronousPipelineFactory.class);
 
       bind(PluginFinder.class).to(LocalPluginFinder.class);
-
       install(
         new FactoryModuleBuilder()
           .implement(new TypeLiteral<Manager<AppDeploymentInfo, ApplicationWithPrograms>>() {
@@ -446,7 +445,7 @@ public final class AppFabricServiceRuntimeModule extends RuntimeModule {
 
   /**
    * A Guice provider for the {@link UGIProvider} class based on the CDAP configuration.
-   * <p>
+   *
    * When Kerberos is enabled, it provides {@link DefaultUGIProvider} instance. Otherwise, an
    * {@link UnsupportedUGIProvider} will be used.
    */
