4a665ffb51451f61e92d5e52483fe3779b43bb60
Mo Eseifan <meseifan@google.com>
2021-06-01 15:20:04 -0700

    Added logic for fetching, locally caching and unpacking artifacts

--- cdap-kubernetes/src/main/java/io/cdap/cdap/k8s/runtime/ArtifactLocalizer.java
+++ cdap-kubernetes/src/main/java/io/cdap/cdap/k8s/runtime/ArtifactLocalizer.java
@@ -15,53 +15,118 @@
 package io.cdap.cdap.k8s.runtime;
 
 import com.google.common.io.ByteStreams;
-import org.apache.twill.discovery.Discoverable;
-import org.apache.twill.discovery.DiscoveryServiceClient;
+import io.cdap.cdap.master.spi.environment.MasterEnvironment;
+import io.cdap.cdap.master.spi.environment.MasterEnvironmentRunnableContext;
+import org.apache.twill.filesystem.LocalLocationFactory;
 import org.apache.twill.filesystem.Location;
+import org.apache.twill.filesystem.LocationFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ArtifactLocalizer {
-  private final URI uri;
-  private static final Logger LOG = LoggerFactory.getLogger(FileLocalizer.class);
 
-  public ArtifactLocalizer(DiscoveryServiceClient discoveryClient) {
+  private static final Logger LOG = LoggerFactory.getLogger(ArtifactLocalizer.class);
 
-    Discoverable discoverable = discoveryClient.discover("appfabric").iterator().next();
-    String scheme = "http";
-    InetSocketAddress address = discoverable.getSocketAddress();
-    String path = "v3Internal/location/";
-    uri = URI.create(String.format("%s://%s:%d/%s", scheme, address.getHostName(), address.getPort(), path));
+  private final MasterEnvironmentRunnableContext context;
+
+  private static final String PD_DIR = "data/";
+  private LocationFactory locationFactory;
+
+  public ArtifactLocalizer(MasterEnvironmentRunnableContext context,
+                           @SuppressWarnings("unused") MasterEnvironment masterEnv) {
+    this.context = context;
+    locationFactory = new LocalLocationFactory();
   }
 
-  private void getAndStoreArtifact(Location remoteLocation) throws IOException {
-    URL url = uri.resolve(remoteLocation.toURI()).toURL();
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//    urlConn.setConnectTimeout(httpRequestConfig.getConnectTimeout());
-//    urlConn.setReadTimeout(httpRequestConfig.getReadTimeout());
-    connection.setDoInput(true);
+  //  public ArtifactLocalizer(DiscoveryServiceClient discoveryClient) {
+  //
+  //    Discoverable discoverable = discoveryClient.discover("appfabric").iterator().next();
+  //    String scheme = "http";
+  //    InetSocketAddress address = discoverable.getSocketAddress();
+  //    String path = "v3Internal/location/";
+  //    uri = URI.create(String.format("%s://%s:%d/%s", scheme, address.getHostName(), address.getPort(), path));
+  //  }
+
+  public Location getArtifact(Location remoteLocation) throws IOException {
+    String url = "v3Internal/location/" + remoteLocation.toURI().toString();
+    LOG.error("Attempting to read from URL: " + url);
+    HttpURLConnection connection = context
+      .openHttpURLConnection(url);
+
+    LOG.error("Connection opened to: " + connection.getURL().toString());
+
+    // If the location already exists then its cached and we dont need to do anything
+    Location localLocation = getArtifactLocalLocation(remoteLocation);
+    if (localLocation.exists()) {
+      return localLocation;
+    }
 
     try {
       try (InputStream is = connection.getInputStream()) {
-        try (OutputStream os = location.getOutputStream()) {
+        try (OutputStream os = localLocation.getOutputStream()) {
           ByteStreams.copy(is, os);
         }
-        LOG.debug("Stored artifact into " + location.toURI().toString());
-      } catch (BadRequestException e) {
+        LOG.debug("Stored artifact into " + localLocation.toURI().toString());
+      } catch (Exception e) {
+        LOG.error("Got exception when trying to store artifact into: " + localLocation.toURI().toString(), e);
         // Just treat bad request as IOException since it won't be retriable
         throw new IOException(e);
       }
     } finally {
       connection.disconnect();
     }
+
+    return localLocation;
   }
 
+  public Location getAndUnpackArtifact(Location remoteLocation) throws Exception {
+    Location artifactLocation = getArtifact(remoteLocation);
+    File artifactFile = Paths.get(artifactLocation.toURI()).toFile();
+    Path targetPath = getUnpackLocalPath(remoteLocation);
+    try (ZipInputStream input = new ZipInputStream(new BufferedInputStream(new FileInputStream(artifactFile)))) {
+
+      Files.createDirectories(targetPath);
+
+      ZipEntry entry;
+      while ((entry = input.getNextEntry()) != null) {
+
+        Path output = targetPath.resolve(entry.getName());
+
+        if (entry.isDirectory()) {
+          Files.createDirectories(output);
+        } else {
+          Files.createDirectories(output.getParent());
+          Files.copy(input, output);
+        }
+      }
+    }
+    return locationFactory.create(targetPath.toUri());
+  }
+
+  private Location getLocalLocation(String basePath, Location remoteLocation) {
+    String path = Paths.get(PD_DIR, basePath, remoteLocation.toURI().toString()).toUri().toString();
+    return locationFactory.create(path);
+  }
+
+  private Location getArtifactLocalLocation(Location remoteLocation) {
+    return getLocalLocation("artifacts", remoteLocation);
+  }
+
+  private Path getUnpackLocalPath(Location remoteLocation) {
+    // TODO: Fix this really gross way to get a path
+    return Paths.get(getLocalLocation("unpacked", remoteLocation).toURI().toString().replace(".jar", ""));
+  }
 }
