f8c206e40b75671bc6f6102165bb1ef93454c868
Masoud Saeida Ardekani <ardekani@google.com>
2021-06-01 17:03:51 -0700

    Add file localizer service

---
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/worker/sidecar/FileLocalizerService.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright © 2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.internal.app.worker.sidecar;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.AbstractIdleService;
+import com.google.inject.Inject;
+import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.common.conf.SConfiguration;
+import io.cdap.cdap.common.discovery.ResolvingDiscoverable;
+import io.cdap.cdap.common.discovery.URIScheme;
+import io.cdap.cdap.common.http.CommonNettyHttpServiceBuilder;
+import io.cdap.cdap.common.internal.worker.RunnableTask;
+import io.cdap.cdap.common.security.HttpsEnabler;
+import io.cdap.cdap.internal.app.worker.TaskWorkerHttpHandlerInternal;
+import io.cdap.http.NettyHttpService;
+import org.apache.twill.common.Cancellable;
+import org.apache.twill.discovery.DiscoveryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Launches an HTTP server for receiving and handling {@link RunnableTask}
+ */
+public class FileLocalizerService extends AbstractIdleService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(FileLocalizerService.class);
+
+  private final CConfiguration cConf;
+  private final SConfiguration sConf;
+  private final DiscoveryService discoveryService;
+  private final NettyHttpService httpService;
+  private Cancellable cancelDiscovery;
+  private InetSocketAddress bindAddress;
+
+  @Inject
+  FileLocalizerService(CConfiguration cConf,
+                       SConfiguration sConf,
+                       DiscoveryService discoveryService) {
+    this.cConf = cConf;
+    this.sConf = sConf;
+    this.discoveryService = discoveryService;
+
+    NettyHttpService.Builder builder = new CommonNettyHttpServiceBuilder(cConf, Constants.Service.TASK_WORKER)
+      .setHost("127.0.0.1")
+      .setPort(cConf.getInt(Constants.FileLocalizer.PORT))
+      .setExecThreadPoolSize(cConf.getInt(Constants.FileLocalizer.EXEC_THREADS))
+      .setBossThreadPoolSize(cConf.getInt(Constants.FileLocalizer.BOSS_THREADS))
+      .setWorkerThreadPoolSize(cConf.getInt(Constants.FileLocalizer.WORKER_THREADS))
+      .setHttpHandlers(new TaskWorkerHttpHandlerInternal(this.cConf, this::stopService));
+
+    if (cConf.getBoolean(Constants.Security.SSL.INTERNAL_ENABLED)) {
+      new HttpsEnabler().configureKeyStore(cConf, sConf).enable(builder);
+    }
+    httpService = builder.build();
+  }
+
+  @Override
+  protected void startUp() throws Exception {
+    LOG.info("Starting TaskWorkerService");
+    httpService.start();
+    bindAddress = httpService.getBindAddress();
+    cancelDiscovery = discoveryService.register(
+      ResolvingDiscoverable.of(URIScheme.createDiscoverable(Constants.Service.TASK_WORKER, httpService)));
+    LOG.info("Starting TaskWorkerService has completed");
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    LOG.info("Shutting down TaskWorkerService");
+    cancelDiscovery.cancel();
+    httpService.stop(5, 5, TimeUnit.SECONDS);
+    LOG.debug("Shutting down TaskWorkerService has completed");
+  }
+
+  private void stopService(String className) {
+    /** TODO: Expand this logic such that
+     * based on number of requests per particular class,
+     * the service gets stopped.
+     */
+    stop();
+  }
+
+  @VisibleForTesting
+  public InetSocketAddress getBindAddress() {
+    return bindAddress;
+  }
+}
