7989b1912341d049d10097bf2ed86efc5cebcbd4
greeshmaswaminathan <greeshmas@google.com>
2021-10-25 18:42:07 +0000

    Event publishing - part 1
    Move spi interfaces to cdap-event-writer-spi module
    
    Inject event classes
    
    Add in App Fabric start and end of Event Manager
    Add new constants referer to events writer extesnsion directory and prefix of parameters
    Add DefaultEventWriterContext
    Add Provider implementation
    Add Provier usage in EventPublishManager to initialize
    Add initialize eventwriters with context in ProgramStatusEventPublisher
    
    Inject event classes
    
    Add binder to EventWriterProvider
    
    Local testing executing in sandbox and checking the functionallity with the system logs that prints the messages directly to the console
    
    Missing class
    
    update publisher functionality
    
    update constants
    
    Including unit testing and allowing stop events to publish
    
    Fixing unused services
    
    Papulating details with error
    
    resolving comments from changes-context-provider
    
    Refactoring EventWriter generic type and style
    
    Fixing formatting issues
    
    Rearrange code
    
    Removing new line
    
    Fixing indents
    
    Fixing error messages and adding javadocs
    
    Removing only pipeline events filter
    
    Disabling event publish by default

---
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/events/DefaultEventWriterContext.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.internal.events;
+
+import io.cdap.cdap.common.conf.CConfiguration;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.spi.events.EventWriterContext;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Provides an initialized default context for EventWriter implementing {@link EventWriterContext}
+ */
+public class DefaultEventWriterContext implements EventWriterContext {
+
+  private final Map<String, String> properties;
+
+  DefaultEventWriterContext(CConfiguration cConf, String eventsWriterId) {
+    String prefix = String.format("%s.%s.", Constants.Event.EVENTS_WRITER_PREFIX, eventsWriterId);
+    this.properties = Collections.unmodifiableMap(cConf.getPropsWithPrefix(prefix));
+  }
+
+  @Override
+  public Map<String, String> getProperties() {
+    return properties;
+  }
+}
