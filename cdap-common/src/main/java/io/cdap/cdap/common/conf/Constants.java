ea7c16174c8af4f329a36d7399fedfb330408260
Baijun Zhu <bjzhu@google.com>
2021-11-15 22:50:35 -0800

    [CDAP-18579] third part adding tasks and according tests

--- cdap-common/src/main/java/io/cdap/cdap/common/conf/Constants.java
+++ cdap-common/src/main/java/io/cdap/cdap/common/conf/Constants.java
@@ -1781,10 +1781,10 @@ public final class Constants {
   public static final class SupportBundle {
     public static final String MAX_FOLDER_SIZE = "support.bundle.max.folder.size";
     public static final String MAX_THREADS = "support.bundle.max.threads";
+    public static final String DEFAULT_WORKFLOW = "support.bundle.default.workflow";
     public static final String LOCAL_DATA_DIR = "support.bundle.local.data.dir";
     public static final String TASK_FACTORY = "support.bundle.task.factory";
     public static final String MAX_RETRY_TIMES = "support.bundle.max.retry.times";
     public static final String MAX_THREAD_TIMEOUT = "support.bundle.max.thread.timeout";
-    public static final String SYSTEM_LOG_START_TIME = "support.bundle.system.log.start.time";
   }
 }
