9db8ad808c8834ccb9c8e3141c8493baca5a8f35
Wangyuan Zhang <wyzhang@google.com>
2021-12-26 14:13:53 -0800

    wip

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/worker/sidecar/ArtifactLocalizerHttpHandlerInternal.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/worker/sidecar/ArtifactLocalizerHttpHandlerInternal.java
@@ -41,6 +41,7 @@ import java.io.IOException;
 import java.util.Optional;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
@@ -65,7 +66,7 @@ public class ArtifactLocalizerHttpHandlerInternal extends AbstractHttpHandler {
     this.previewRequestFetcher = previewRequestFetcher;
   }
 
-  @GET
+  @POST
   @Path("/requests/pull")
   public void pullPreview(HttpRequest request, HttpResponder responder) throws IOException, UnauthorizedException {
     Optional<PreviewRequest> previewRequest = previewRequestFetcher.fetch();
