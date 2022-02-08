000cc96fd7e536696769d844a3f6a9f7d087b18b
Terence Yim <terencey@google.com>
2022-02-08 02:44:24 -0800

    Enhanced Provisioner Extension loader
    - Allows loading extensions from the system classloader with guice injection
    - Derive provisioner json config from the classloader
    - Supports loading config json from within the jar

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/provision/ProvisionerExtensionLoader.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/provision/ProvisionerExtensionLoader.java
@@ -17,6 +17,7 @@
 package io.cdap.cdap.internal.provision;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import io.cdap.cdap.common.conf.CConfiguration;
 import io.cdap.cdap.common.conf.Constants;
 import io.cdap.cdap.common.lang.ClassPathResources;
@@ -26,7 +27,6 @@ import io.cdap.cdap.runtime.spi.provisioner.Provisioner;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -39,6 +39,8 @@ public class ProvisionerExtensionLoader extends AbstractExtensionLoader<String, 
   private static final Set<String> ALLOWED_RESOURCES = createAllowedResources();
   private static final Set<String> ALLOWED_PACKAGES = createPackageSets(ALLOWED_RESOURCES);
 
+  private final Injector injector;
+
   private static Set<String> createAllowedResources() {
     try {
       return ClassPathResources.getResourcesWithDependencies(Provisioner.class.getClassLoader(), Provisioner.class);
@@ -49,8 +51,9 @@ public class ProvisionerExtensionLoader extends AbstractExtensionLoader<String, 
   }
 
   @Inject
-  ProvisionerExtensionLoader(CConfiguration cConf) {
+  ProvisionerExtensionLoader(Injector injector, CConfiguration cConf) {
     super(cConf.get(Constants.Provisioner.EXTENSIONS_DIR));
+    this.injector = injector;
   }
 
   @Override
@@ -77,11 +80,12 @@ public class ProvisionerExtensionLoader extends AbstractExtensionLoader<String, 
 
   @Override
   public Map<String, Provisioner> loadProvisioners() {
-    Map<String, Provisioner> provisioners = new HashMap<>();
-    // always include the native provisioner
-    Provisioner nativeProvisioner = new NativeProvisioner();
-    provisioners.put(nativeProvisioner.getSpec().getName(), nativeProvisioner);
-    provisioners.putAll(getAll());
-    return provisioners;
+    return getAll();
+  }
+
+  @Override
+  protected Provisioner prepareSystemExtension(Provisioner provisioner) {
+    injector.injectMembers(provisioner);
+    return provisioner;
   }
 }
