ef4ac1776d747543bb1f53128caaefe20444bae2
Baijun Zhu <bjzhu@google.com>
2022-02-11 23:32:18 -0800

    create new supportbundle entity to enforce authentication

--- cdap-app-fabric/src/main/java/io/cdap/cdap/support/handlers/SupportBundleHttpHandler.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/support/handlers/SupportBundleHttpHandler.java
@@ -20,10 +20,9 @@ import com.google.inject.Inject;
 import io.cdap.cdap.common.conf.Constants;
 import io.cdap.cdap.gateway.handlers.util.AbstractAppFabricHttpHandler;
 import io.cdap.cdap.proto.ProgramType;
-import io.cdap.cdap.proto.id.NamespaceId;
-import io.cdap.cdap.proto.id.ProgramId;
-import io.cdap.cdap.proto.id.ProgramRunId;
-import io.cdap.cdap.proto.security.StandardPermission;
+import io.cdap.cdap.proto.element.EntityType;
+import io.cdap.cdap.proto.id.InstanceId;
+import io.cdap.cdap.proto.security.SupportBundlePermission;
 import io.cdap.cdap.security.spi.authorization.ContextAccessEnforcer;
 import io.cdap.cdap.support.services.SupportBundleService;
 import io.cdap.cdap.support.status.SupportBundleConfiguration;
@@ -46,7 +45,7 @@ public class SupportBundleHttpHandler extends AbstractAppFabricHttpHandler {
   private final SupportBundleService bundleService;
   private final ContextAccessEnforcer contextAccessEnforcer;
 
-    @Inject
+  @Inject
   SupportBundleHttpHandler(SupportBundleService supportBundleService, ContextAccessEnforcer contextAccessEnforcer) {
     this.bundleService = supportBundleService;
     this.contextAccessEnforcer = contextAccessEnforcer;
@@ -72,7 +71,9 @@ public class SupportBundleHttpHandler extends AbstractAppFabricHttpHandler {
                                     String programName, @Nullable @QueryParam("run") String run,
                                   @Nullable @QueryParam("maxRunsPerProgram") @DefaultValue("1")
                                     Integer maxRunsPerProgram) throws Exception {
-    ensureVisibilityOnProgram(namespace, application, programType, programName, run);
+    /** ensure the user has authentication to create supportBundle */
+    contextAccessEnforcer.enforceOnParent(EntityType.SUPPORT_BUNDLE, InstanceId.SELF,
+                                          SupportBundlePermission.GENERATE_SUPPORT_BUNDLE);
     // Establishes the support bundle configuration
     SupportBundleConfiguration bundleConfig =
       new SupportBundleConfiguration(namespace, application, run, ProgramType.valueOfCategoryName(programType),
@@ -80,27 +81,5 @@ public class SupportBundleHttpHandler extends AbstractAppFabricHttpHandler {
     // Generates support bundle and returns with uuid
     String uuid = bundleService.generateSupportBundle(bundleConfig);
     responder.sendString(HttpResponseStatus.OK, uuid);
-  }
-
-  /** ensure the user has visibility for the query request they made */
-  private void ensureVisibilityOnProgram(String namespace, String application, String programType, String programName,
-                                         String run) throws Exception {
-    if (namespace != null) {
-      if (application != null) {
-        if (run == null) {
-          ProgramId programId =
-            new ProgramId(namespace, application, ProgramType.valueOfCategoryName(programType), programName);
-          contextAccessEnforcer.enforce(programId, StandardPermission.GET);
-        } else {
-          ProgramRunId programRunId =
-            new ProgramRunId(namespace, application, ProgramType.valueOfCategoryName(programType), programName, run);
-          contextAccessEnforcer.enforce(programRunId, StandardPermission.GET);
-        }
-      } else {
-        NamespaceId namespaceId =
-          new NamespaceId(namespace);
-        contextAccessEnforcer.enforce(namespaceId, StandardPermission.GET);
-      }
-    }
   }
 }
---
+++ cdap-proto/src/main/java/io/cdap/cdap/proto/id/SupportBundleEntityId.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2015-2021 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.cdap.cdap.proto.id;
+
+import io.cdap.cdap.proto.element.EntityType;
+
+import java.util.Collections;
+
+/**
+ * Uniquely identifies a support bundle entity.
+ */
+public class SupportBundleEntityId extends EntityId {
+  // the name of the entity
+  protected final String supportBundleName;
+
+  public SupportBundleEntityId(String supportBundleName) {
+    super(EntityType.SUPPORT_BUNDLE);
+    if (supportBundleName == null) {
+      throw new NullPointerException("supportBundleName can not be null.");
+    }
+    this.supportBundleName = supportBundleName;
+  }
+
+  @Override
+  public String getEntityName() {
+    return supportBundleName;
+  }
+
+  @Override
+  public Iterable<String> toIdParts() {
+    return Collections.singletonList(supportBundleName);
+  }
+}
+
