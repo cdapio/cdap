f5402d4aa1b0ec5d6def9fc08a14c81314ea20d3
Dennis Li <23002167+dli357@users.noreply.github.com>
2022-03-03 19:33:09 -0800

    [CDAP-18869] Add GCPRemoteAuthenticator extension module

---
+++ cdap-authenticator-ext-gcp/src/main/java/io/cdap/cdap/authenticator/gcp/GCPRemoteAuthenticator.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.authenticator.gcp;
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.annotations.VisibleForTesting;
+import io.cdap.cdap.proto.security.Credential;
+import io.cdap.cdap.security.spi.authenticator.RemoteAuthenticator;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.time.Clock;
+import javax.annotation.Nullable;
+
+/**
+ * {@link RemoteAuthenticator} Authenticator which returns Google application default credentials.
+ * For additional information, see https://google.aip.dev/auth/4110.
+ */
+public class GCPRemoteAuthenticator implements RemoteAuthenticator {
+
+  public static final String GCP_REMOTE_AUTHENTICATOR_NAME = "gcp-remote-authenticator";
+
+  private final GoogleCredentials googleCredentials;
+  private final Clock clock;
+
+  @Nullable
+  private AccessToken accessToken;
+
+  @VisibleForTesting
+  GCPRemoteAuthenticator(GoogleCredentials googleCredentials, Clock clock, @Nullable AccessToken accessToken) {
+    this.googleCredentials = googleCredentials;
+    this.clock = clock;
+    this.accessToken = accessToken;
+  }
+
+  public GCPRemoteAuthenticator() throws IOException {
+    this(GoogleCredentials.getApplicationDefault(), Clock.systemUTC(), null);
+  }
+
+  @Override
+  public String getName() {
+    return GCP_REMOTE_AUTHENTICATOR_NAME;
+  }
+
+  @Nullable
+  @Override
+  public Credential getCredentials() throws IOException {
+    // If access token is expired, renew the access token.
+    if (accessToken == null || accessToken.getExpirationTime().before(Date.from(clock.instant()))) {
+      accessToken = googleCredentials.refreshAccessToken();
+    }
+    return new Credential(accessToken.getTokenValue(), Credential.CredentialType.EXTERNAL_BEARER);
+  }
+}
---
+++ cdap-authenticator-ext-gcp/src/test/java/io/cdap/cdap/authenticator/gcp/GCPRemoteAuthenticatorTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.authenticator.gcp;
+
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+import io.cdap.cdap.proto.security.Credential;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link GCPRemoteAuthenticator}.
+ */
+public class GCPRemoteAuthenticatorTest {
+
+  @Test
+  public void testRemoteAuthenticatorRefreshesNullAccessToken() throws Exception {
+    String accessTokenValue = "access-token";
+    // This is just an arbitrary fixed point in time.
+    Instant fixedInstant = Instant.ofEpochSecond(1646358109);
+    Clock fixedClock = Clock.fixed(fixedInstant, ZoneId.systemDefault());
+    GoogleCredentials mockGoogleCredentials = mock(GoogleCredentials.class);
+    AccessToken accessToken = new AccessToken(accessTokenValue, Date.from(fixedInstant.plus(Duration.ofHours(1))));
+    when(mockGoogleCredentials.refreshAccessToken()).thenReturn(accessToken);
+    GCPRemoteAuthenticator gcpRemoteAuthenticator = new GCPRemoteAuthenticator(mockGoogleCredentials, fixedClock,
+                                                                               null);
+
+    // Verify expected credential value and that refresh was called exactly once.
+    Credential credential = gcpRemoteAuthenticator.getCredentials();
+    Assert.assertEquals(accessTokenValue, credential.getValue());
+    verify(mockGoogleCredentials, times(1)).refreshAccessToken();
+  }
+
+  @Test
+  public void testRemoteAuthenticatorRefreshesExpiredAccessToken() throws Exception {
+    String expiredAccessTokenValue = "expired-access-token";
+    String accessTokenValue = "access-token";
+    // This is just an arbitrary fixed point in time.
+    Instant fixedInstant = Instant.ofEpochSecond(1646358109);
+    Clock fixedClock = Clock.fixed(fixedInstant, ZoneId.systemDefault());
+    GoogleCredentials mockGoogleCredentials = mock(GoogleCredentials.class);
+    AccessToken expiredAccessToken = new AccessToken(expiredAccessTokenValue,
+                                                     Date.from(fixedInstant.minus(Duration.ofHours(1))));
+    AccessToken accessToken = new AccessToken(accessTokenValue, Date.from(fixedInstant.plus(Duration.ofHours(1))));
+    when(mockGoogleCredentials.refreshAccessToken()).thenReturn(accessToken);
+    GCPRemoteAuthenticator gcpRemoteAuthenticator = new GCPRemoteAuthenticator(mockGoogleCredentials, fixedClock,
+                                                                               expiredAccessToken);
+
+    // Verify expected credential value and that refresh was called exactly once.
+    Credential credential = gcpRemoteAuthenticator.getCredentials();
+    Assert.assertEquals(accessTokenValue, credential.getValue());
+    verify(mockGoogleCredentials, times(1)).refreshAccessToken();
+  }
+
+  @Test
+  public void testRemoteAuthenticatorReturnsValidAccessToken() throws Exception {
+    String accessTokenValue = "access-token";
+    // This is just an arbitrary fixed point in time.
+    Instant fixedInstant = Instant.ofEpochSecond(1646358109);
+    Clock fixedClock = Clock.fixed(fixedInstant, ZoneId.systemDefault());
+    GoogleCredentials mockGoogleCredentials = mock(GoogleCredentials.class);
+    AccessToken accessToken = new AccessToken(accessTokenValue, Date.from(fixedInstant.plus(Duration.ofHours(1))));
+    when(mockGoogleCredentials.refreshAccessToken()).thenReturn(accessToken);
+    GCPRemoteAuthenticator gcpRemoteAuthenticator = new GCPRemoteAuthenticator(mockGoogleCredentials, fixedClock,
+                                                                               accessToken);
+
+    // Verify expected credential value and that refresh was not called.
+    Credential credential = gcpRemoteAuthenticator.getCredentials();
+    Assert.assertEquals(accessTokenValue, credential.getValue());
+    verify(mockGoogleCredentials, times(0)).refreshAccessToken();
+  }
+}
