7faa4c7cb03eb1f3ab7feeb5c7951901c9bba936
Sebastian Echegaray <sechegaray@google.com>
2022-03-15 12:05:50 -0500

    Revert "remove unnecessary dependency"
    This reverts commit 2f3e6e9851fe3e6cbf49a7074e893cfc6afdbdfd.

--- cdap-master/src/main/java/io/cdap/cdap/master/environment/k8s/SupportBundleServiceMain.java
+++ cdap-master/src/main/java/io/cdap/cdap/master/environment/k8s/SupportBundleServiceMain.java
@@ -21,6 +21,7 @@ import com.google.inject.Binding;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import io.cdap.cdap.app.guice.AuthorizationModule;
 import io.cdap.cdap.app.guice.SupportBundleServiceModule;
 import io.cdap.cdap.common.conf.CConfiguration;
 import io.cdap.cdap.common.conf.Constants;
@@ -37,6 +38,7 @@ import io.cdap.cdap.messaging.guice.MessagingClientModule;
 import io.cdap.cdap.proto.id.NamespaceId;
 import io.cdap.cdap.security.auth.TokenManager;
 import io.cdap.cdap.security.authorization.AuthorizationEnforcementModule;
+import io.cdap.cdap.security.guice.SecureStoreServerModule;
 import io.cdap.cdap.security.impersonation.SecurityUtil;
 import org.apache.twill.zookeeper.ZKClientService;
 
@@ -69,8 +71,10 @@ public class SupportBundleServiceMain extends AbstractServiceMain<EnvironmentOpt
       new SystemDatasetRuntimeModule().getStandaloneModules(),
       // The Dataset set modules are only needed to satisfy dependency injection
       new DataSetsModules().getStandaloneModules(),
-      new AuthorizationEnforcementModule().getDistributedModules(),
+      new AuthorizationModule(),
+      new AuthorizationEnforcementModule().getMasterModule(),
       new SupportBundleServiceModule(),
+      new SecureStoreServerModule(),
       new DFSLocationModule()
     );
   }
