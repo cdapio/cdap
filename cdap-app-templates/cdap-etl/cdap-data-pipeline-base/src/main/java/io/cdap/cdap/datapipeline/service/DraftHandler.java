d6c9b1f7939bf396d86ed338704be9a218961776
xiaoweic <xiaoweic@google.com>
2022-04-25 23:25:08 -0700

    xiaoweic-test

--- cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/DraftHandler.java
+++ cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/DraftHandler.java
@@ -31,7 +31,6 @@ import io.cdap.cdap.etl.proto.v2.DataStreamsConfig;
 import io.cdap.cdap.etl.proto.v2.ETLBatchConfig;
 import io.cdap.cdap.etl.proto.v2.ETLConfig;
 import io.cdap.cdap.internal.io.SchemaTypeAdapter;
-import io.cdap.cdap.proto.element.EntityType;
 import io.cdap.cdap.proto.id.NamespaceId;
 import io.cdap.cdap.proto.security.StandardPermission;
 import io.cdap.cdap.security.spi.authorization.ContextAccessEnforcer;
@@ -73,7 +72,7 @@ public class DraftHandler extends AbstractDataPipelineHandler {
   }
 
   /**
-   * Returns a list of drafts associated with a namespace and the current user
+   * Returns a list of drafts associated with a namespace.
    */
   @GET
   @Path(API_VERSION + "/contexts/{context}/drafts/")
@@ -91,8 +90,7 @@ public class DraftHandler extends AbstractDataPipelineHandler {
       }
 
       SortRequest sortRequest = new SortRequest(sortBy, sortOrder);
-      String userId = request.getUserId();
-      userId = userId == null ? "" : userId;
+      String userId = "";
       responder.sendJson(draftService.listDrafts(namespace, userId, includeConfig, sortRequest, filter));
     });
   }
@@ -106,8 +104,7 @@ public class DraftHandler extends AbstractDataPipelineHandler {
                        @PathParam("context") String namespaceName,
                        @PathParam("draft") String draftId) {
     respond(namespaceName, responder, (namespace) -> {
-      String userId = request.getUserId();
-      userId = userId == null ? "" : userId;
+      String userId = "";
       DraftId id = new DraftId(namespace, draftId, userId);
       responder.sendJson(draftService.getDraft(id));
     });
@@ -127,8 +124,7 @@ public class DraftHandler extends AbstractDataPipelineHandler {
 
       String requestStr = StandardCharsets.UTF_8.decode(request.getContent()).toString();
       DraftStoreRequest<ETLConfig> draftStoreRequest = deserializeDraftStoreRequest(requestStr);
-      String userId = request.getUserId();
-      userId = userId == null ? "" : userId;
+      String userId = "";
       DraftId id = new DraftId(namespace, draftId, userId);
       draftService.writeDraft(id, draftStoreRequest);
 
@@ -146,8 +142,7 @@ public class DraftHandler extends AbstractDataPipelineHandler {
                           @PathParam("draft") String draftId) {
     contextAccessEnforcer.enforce(new NamespaceId(namespaceName), StandardPermission.UPDATE);
     respond(namespaceName, responder, (namespace) -> {
-      String userId = request.getUserId();
-      userId = userId == null ? "" : userId;
+      String userId = "";
       DraftId id = new DraftId(namespace, draftId, userId);
 
       draftService.deleteDraft(id);
