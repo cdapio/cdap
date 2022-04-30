0d25291fd84a4ea9bb41fee52c63613c5578baf4
Sai Aditya Mukkamala <saimukkamala@google.com>
2022-04-30 09:48:55 +0530

    fixing aggregation bug

--- cdap-watchdog/src/main/java/io/cdap/cdap/metrics/store/DefaultMetricStore.java
+++ cdap-watchdog/src/main/java/io/cdap/cdap/metrics/store/DefaultMetricStore.java
@@ -197,13 +197,11 @@ public class DefaultMetricStore implements MetricStore {
       // i.e. for components only
       ImmutableList.of(Constants.Metrics.Tag.NAMESPACE, Constants.Metrics.Tag.COMPONENT)));
 
-    /*
+    
     aggs.put(BY_SCHEDULE, new DefaultAggregation(
-        ImmutableList.of(Constants.Metrics.Tag.NAMESPACE, Constants.Metrics.Tag.COMPONENT,
-            Constants.Metrics.Tag.APP, Constants.Metrics.Tag.SCHEDULE),
-        ImmutableList.of(Constants.Metrics.Tag.NAMESPACE, Constants.Metrics.Tag.APP,
-            Constants.Metrics.Tag.COMPONENT, Constants.Metrics.Tag.SCHEDULE)));
-    */
+      ImmutableList.of(Constants.Metrics.Tag.NAMESPACE, Constants.Metrics.Tag.COMPONENT,
+                       Constants.Metrics.Tag.APP, Constants.Metrics.Tag.SCHEDULE),
+      ImmutableList.of(Constants.Metrics.Tag.NAMESPACE, Constants.Metrics.Tag.COMPONENT)));
 
     
     AGGREGATIONS = Collections.unmodifiableMap(aggs);
