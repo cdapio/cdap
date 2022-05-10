244d8d56b9240f19dd13ef3307084d498c0a6dd9
Baijun Zhu <bjzhu@google.com>
2022-05-10 15:59:06 -0700

    add unit test for downloading support bundle

--- cdap-support-bundle/src/test/java/io/cdap/cdap/support/handlers/SupportBundleHttpHandlerTest.java
+++ cdap-support-bundle/src/test/java/io/cdap/cdap/support/handlers/SupportBundleHttpHandlerTest.java
@@ -16,11 +16,14 @@
 
 package io.cdap.cdap.support.handlers;
 
+import com.google.common.io.Files;
 import io.cdap.cdap.SupportBundleTestBase;
 import io.cdap.cdap.common.conf.Constants;
 import io.cdap.cdap.common.discovery.RandomEndpointStrategy;
 import io.cdap.cdap.common.discovery.URIScheme;
 import io.cdap.cdap.common.http.DefaultHttpRequestConfig;
+import io.cdap.cdap.common.lang.jar.BundleJarUtil;
+import io.cdap.cdap.common.utils.Tasks;
 import io.cdap.cdap.proto.id.NamespaceId;
 import io.cdap.common.http.HttpRequest;
 import io.cdap.common.http.HttpRequests;
@@ -32,32 +35,62 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Monitor handler tests.
  */
 public class SupportBundleHttpHandlerTest extends SupportBundleTestBase {
 
+  private static String bundleId = "";
   private static final NamespaceId NAMESPACE = TEST_NAMESPACE_META1.getNamespaceId();
 
   @Test
   public void testCreateSupportBundleWithValidNamespace() throws Exception {
 
-    String bundleId = requestBundle(Collections.singletonMap("namespace", NAMESPACE.getNamespace()));
-
     Assert.assertNotNull(bundleId);
     Assert.assertFalse(bundleId.isEmpty());
+  }
+
+  @Test
+  public void testExportSupportBundle() throws Exception {
+    HttpResponse response = requestExportBundle(bundleId);
+
+    // Validate the message digest of the content
+    String digest = response.getHeaders().get("digest").stream().findFirst().orElse(null);
+    Assert.assertNotNull(digest);
+    String[] splits = digest.split("=", 2);
+    MessageDigest md = MessageDigest.getInstance(splits[0]);
+    Assert.assertEquals(splits[1], Base64.getEncoder().encodeToString(md.digest(response.getResponseBody())));
+
+    File bundleFile = TEMP_FOLDER.newFile();
+    bundleFile.delete();
+    Files.write(response.getResponseBody(), bundleFile);
+
+    File bundleDir = TEMP_FOLDER.newFolder();
+    BundleJarUtil.unJar(bundleFile, bundleDir);
+
+    File bundleExport = new File(bundleDir, bundleId);
+    Assert.assertTrue(bundleExport.isDirectory());
+    Assert.assertTrue(new File(bundleExport, "status.json").isFile());
+    Assert.assertTrue(new File(bundleExport, "system-log").isDirectory());
   }
 
   @Before
   public void setup() throws Exception {
     Assert.assertEquals(HttpURLConnection.HTTP_OK, createNamespace(NAMESPACE).getResponseCode());
+    bundleId = requestBundle(Collections.singletonMap("namespace", NAMESPACE.getNamespace()));
   }
 
   @After
@@ -72,7 +105,8 @@ public class SupportBundleHttpHandlerTest extends SupportBundleTestBase {
    * @return the bundle UUID
    * @throws IOException if failed to request bundle generation
    */
-  private String requestBundle(Map<String, String> params) throws IOException {
+  private String requestBundle(Map<String, String> params)
+    throws IOException, ExecutionException, InterruptedException, TimeoutException {
     DiscoveryServiceClient discoveryServiceClient = getInjector().getInstance(DiscoveryServiceClient.class);
     Discoverable discoverable =
       new RandomEndpointStrategy(() -> discoveryServiceClient.discover(Constants.Service.SUPPORT_BUNDLE_SERVICE)).pick(
@@ -93,9 +127,36 @@ public class SupportBundleHttpHandlerTest extends SupportBundleTestBase {
     String path = String.format("%s/support/bundles%s", Constants.Gateway.API_VERSION_3, queryBuilder);
 
     HttpRequest request = HttpRequest.post(URIScheme.createURI(discoverable, path).toURL()).build();
-    HttpResponse response = HttpRequests.execute(request, new DefaultHttpRequestConfig(false));
+    AtomicReference<HttpResponse> response =
+      new AtomicReference<>(HttpRequests.execute(request, new DefaultHttpRequestConfig(false)));
 
-    Assert.assertEquals(HttpURLConnection.HTTP_CREATED, response.getResponseCode());
-    return response.getResponseBodyAsString();
+    //Wait for the previous bundle created
+    Tasks.waitFor(HttpURLConnection.HTTP_CREATED, () -> {
+      response.set(HttpRequests.execute(request, new DefaultHttpRequestConfig(false)));
+      return response.get().getResponseCode();
+    }, 60, TimeUnit.SECONDS, 1, TimeUnit.SECONDS);
+    Assert.assertEquals(HttpURLConnection.HTTP_CREATED, response.get().getResponseCode());
+    return response.get().getResponseBodyAsString();
+  }
+
+  /**
+   * Requests zip and download support bundle.
+   *
+   * @param bundleId String
+   * @return HTTPResponse
+   * @throws IOException if failed to request bundle download
+   */
+  private HttpResponse requestExportBundle(String bundleId) throws IOException {
+    DiscoveryServiceClient discoveryServiceClient = getInjector().getInstance(DiscoveryServiceClient.class);
+    Discoverable discoverable =
+      new RandomEndpointStrategy(() -> discoveryServiceClient.discover(Constants.Service.SUPPORT_BUNDLE_SERVICE)).pick(
+        5, TimeUnit.SECONDS);
+
+    Assert.assertNotNull("No service for support bundle", discoverable);
+
+    String path = String.format("%s/support/bundles/%s", Constants.Gateway.API_VERSION_3, bundleId);
+
+    HttpRequest request = HttpRequest.post(URIScheme.createURI(discoverable, path).toURL()).build();
+    return HttpRequests.execute(request, new DefaultHttpRequestConfig(false));
   }
 }
