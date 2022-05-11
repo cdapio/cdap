50fd1ec60777c0389a487213c2763f2d7e5a0d9b
Sai Aditya Mukkamala <saimukkamala@google.com>
2022-05-11 17:34:30 +0530

    merge conflict resolution

--- cdap-gateway/src/main/java/io/cdap/cdap/gateway/router/handlers/HttpRequestRouter.java
+++ cdap-gateway/src/main/java/io/cdap/cdap/gateway/router/handlers/HttpRequestRouter.java
@@ -98,7 +98,7 @@ public class HttpRequestRouter extends ChannelDuplexHandler {
     this.serviceLookup = serviceLookup;
     this.messageSenders = new HashMap<>();
     this.metricsCollectionService = metricsCollectionService;
-    emitMetricsEnabled = Feature.ROUTER_METRICS.isEnabled(new DefaultFeatureFlagsProvider(cConf));
+    this.emitMetricsEnabled = Feature.ROUTER_METRICS.isEnabled(new DefaultFeatureFlagsProvider(cConf));
   }
 
   @Override
