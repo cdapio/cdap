6dc2709efd6ee9e5e549d96b5dbbf5a4cb1951fe
xiaoweic <xiaoweic@google.com>
2022-05-11 16:22:23 -0700

    add requester id in log

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/ApplicationLifecycleService.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/ApplicationLifecycleService.java
@@ -112,6 +112,7 @@ import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -1115,4 +1116,14 @@ public class ApplicationLifecycleService extends AbstractIdleService {
     return builder.build();
   }
 
+  public String decodeUserId(AuthenticationContext authenticationContext) {
+    String decodedUserId = "emptyUserId";
+    try {
+      byte[] decodedBytes = Base64.getDecoder().decode(authenticationContext.getPrincipal().getName());
+      decodedUserId = new String(decodedBytes);
+    } catch (Exception e) {
+      LOG.debug("Failed to decode userId with exception {}", e);
+    }
+    return decodedUserId;
+  }
 }
