e54f2e752da0dcb0d3ef6975bceb824dca1e56b2
Fernando Velasquez <fevelasquez@google.com>
2022-06-21 17:17:38 -0400

    Added logic to output and handle decimal values using delimited and json formats.
    Added support for encoding/decoding byte fields as Base64

--- cdap-formats/src/main/java/io/cdap/cdap/format/StructuredRecordStringConverter.java
+++ cdap-formats/src/main/java/io/cdap/cdap/format/StructuredRecordStringConverter.java
@@ -25,17 +25,19 @@ import io.cdap.cdap.format.io.JsonDecoder;
 import io.cdap.cdap.format.io.JsonEncoder;
 import io.cdap.cdap.format.io.JsonStructuredRecordDatumReader;
 import io.cdap.cdap.format.io.JsonStructuredRecordDatumWriter;
+import io.cdap.cdap.format.utils.FormatUtils;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.stream.Collectors;
 
 /**
  * Utility class for converting {@link StructuredRecord} to and from json.
  */
-public final class StructuredRecordStringConverter {
+public class StructuredRecordStringConverter {
 
   private static final JsonStructuredRecordDatumWriter JSON_DATUM_WRITER = new JsonStructuredRecordDatumWriter();
   private static final JsonStructuredRecordDatumReader JSON_DATUM_READER = new JsonStructuredRecordDatumReader(true);
@@ -65,9 +67,7 @@ public final class StructuredRecordStringConverter {
    */
   public static String toDelimitedString(final StructuredRecord record, String delimiter) {
     return record.getSchema().getFields().stream()
-      .map(Schema.Field::getName)
-      .map(record::get)
-      .map(value -> value == null ? "" : value.toString())
+      .map(f -> fieldToString(record, f))
       .collect(Collectors.joining(delimiter));
   }
 
@@ -79,17 +79,113 @@ public final class StructuredRecordStringConverter {
     Iterator<Schema.Field> fields = schema.getFields().iterator();
 
     for (String part : Splitter.on(delimiter).split(delimitedString)) {
-      if (part.isEmpty()) {
-        builder.set(fields.next().getName(), null);
-      } else {
-        builder.convertAndSet(fields.next().getName(), part);
-      }
+      Schema.Field field = fields.next();
+      parseAndSetFieldValue(builder, field, part);
     }
 
     return builder.build();
   }
 
-  private StructuredRecordStringConverter() {
+  /**
+   * Set field value in the supplied Structured Record Builder.
+   * @param builder Structured Record Builder instance
+   * @param field field to set
+   * @param part String portion of the delimited input
+   */
+  protected static void parseAndSetFieldValue(StructuredRecord.Builder builder,
+                                              Schema.Field field,
+                                              String part) {
+    if (part.isEmpty()) {
+      builder.set(field.getName(), null);
+    } else {
+      handleFieldConversion(builder, field, part);
+    }
+  }
+
+  /**
+   * Get the string representation for a given record field. BigDecimals are printed as plain strings.
+   *
+   * @param record record to process
+   * @param field  field to extract
+   * @return String representing the value for this field.
+   */
+  protected static String fieldToString(StructuredRecord record, Schema.Field field) {
+    String fieldName = field.getName();
+    Object value = record.get(fieldName);
+
+    // Return null value as empty string.
+    if (value == null) {
+      return "";
+    }
+
+    // Get the field schema.
+    Schema fieldSchema = field.getSchema();
+    if (fieldSchema.isNullable()) {
+      fieldSchema = fieldSchema.getNonNullable();
+    }
+
+    // Write decimal values as decimal strings.
+    if (fieldSchema.getLogicalType() == Schema.LogicalType.DECIMAL) {
+      BigDecimal decimalValue = record.getDecimal(fieldName);
+
+      // Throw exception if the field is expected tu be decimal, but it could not be processed as such.
+      if (decimalValue == null) {
+        throw new IllegalArgumentException("Invalid schema for field " + fieldName + ". Decimal was expected.");
+      }
+      return decimalValue.toPlainString();
+    }
+
+    // Output bytes fields as Base64
+    if (fieldSchema.getType() == Schema.Type.BYTES) {
+      try {
+        return FormatUtils.base64Encode(value);
+      } catch (IOException ioe) {
+        throw new IllegalArgumentException("Invalid schema for field " + fieldName + ". " +
+                                             "ByteBuffer or Byte Array was expected.", ioe);
+      }
+    }
+
+    return value.toString();
+  }
+
+  /**
+   * Handle field mapping and conversion into Structured Record
+   *
+   * @param field field schema to parse
+   * @param part  portion of the string to parse
+   */
+  protected static void handleFieldConversion(StructuredRecord.Builder builder,
+                                              Schema.Field field,
+                                              String part) {
+    String fieldName = field.getName();
+
+    // Get the underlying field schema
+    Schema fieldSchema = field.getSchema();
+    if (fieldSchema.isNullable()) {
+      fieldSchema = fieldSchema.getNonNullable();
+    }
+
+    // read decimal values as decimal strings.
+    if (fieldSchema.getLogicalType() == Schema.LogicalType.DECIMAL) {
+      builder.setDecimal(fieldName, FormatUtils.parseDecimal(fieldSchema, part));
+      return;
+    }
+
+    // Decode base64 payload for byte fields.
+    if (fieldSchema.getType() == Schema.Type.BYTES) {
+      try {
+        builder.set(fieldName, FormatUtils.base64Decode(part));
+        return;
+      } catch (IOException ioe) {
+        throw new IllegalArgumentException("Unable to extract Base64 payload from field " + fieldName, ioe);
+      }
+    }
+
+    // Handle all other field types
+    builder.convertAndSet(fieldName, part);
+  }
+
+  protected StructuredRecordStringConverter() {
     //inaccessible constructor for static class
   }
 }
