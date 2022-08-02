9b9a59816331bfe248b073d5b5a59402b7e636c1
Dennis Li <23002167+dli357@users.noreply.github.com>
2022-08-01 22:51:33 -0700

    [CDAP-19448] Switch SystemAppTaskModule to uae DISTRIBUTED ProgramRuntimeProvider.Mode

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/worker/SystemAppModule.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/worker/SystemAppModule.java
@@ -22,11 +22,16 @@ import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.multibindings.MapBinder;
 import com.google.inject.multibindings.OptionalBinder;
 import io.cdap.cdap.api.artifact.ArtifactManager;
+import io.cdap.cdap.app.guice.ClusterMode;
 import io.cdap.cdap.app.guice.DefaultProgramRunnerFactory;
 import io.cdap.cdap.app.runtime.ProgramRunner;
 import io.cdap.cdap.app.runtime.ProgramRunnerFactory;
 import io.cdap.cdap.app.runtime.ProgramRuntimeProvider;
 import io.cdap.cdap.app.runtime.ProgramStateWriter;
+import io.cdap.cdap.common.conf.Constants;
+import io.cdap.cdap.common.namespace.NamespaceQueryAdmin;
+import io.cdap.cdap.common.namespace.RemoteNamespaceQueryClient;
+import io.cdap.cdap.common.twill.NoopTwillRunnerService;
 import io.cdap.cdap.internal.app.program.MessagingProgramStateWriter;
 import io.cdap.cdap.internal.app.runtime.artifact.ArtifactManagerFactory;
 import io.cdap.cdap.internal.app.runtime.artifact.ArtifactRepository;
@@ -41,6 +46,7 @@ import io.cdap.cdap.metadata.RemotePreferencesFetcherInternal;
 import io.cdap.cdap.proto.ProgramType;
 import io.cdap.cdap.security.impersonation.CurrentUGIProvider;
 import io.cdap.cdap.security.impersonation.UGIProvider;
+import org.apache.twill.api.TwillRunner;
 
 /**
  * Modules loaded for system app tasks
@@ -51,7 +57,15 @@ public class SystemAppModule extends AbstractModule {
   protected void configure() {
     MapBinder.newMapBinder(binder(), ProgramType.class, ProgramRunner.class);
     bind(ProgramStateWriter.class).to(MessagingProgramStateWriter.class);
-    bind(ProgramRuntimeProvider.Mode.class).toInstance(ProgramRuntimeProvider.Mode.LOCAL);
+    // Since we don't actually use SparkProgramRuntimeProvider to run a spark program here, we provide no-op
+    // implementations for all classes required by DistributedSparkProgramRunner.
+    // We use ProgramRuntimeProvider.Mode.DISTRIBUTED to reduce the number of required modules because LOCAL requires
+    // more complex bindings.
+    // TODO(CDAP-19459): Remove this entirely after refactoring ArtifactClassLoaderFactory to not use ProgramRunner.
+    bind(ProgramRuntimeProvider.Mode.class).toInstance(ProgramRuntimeProvider.Mode.DISTRIBUTED);
+    bind(ClusterMode.class).toInstance(ClusterMode.ON_PREMISE);
+    bind(TwillRunner.class).annotatedWith(Constants.AppFabric.ProgramRunner.class).to(NoopTwillRunnerService.class);
+    bind(NamespaceQueryAdmin.class).to(RemoteNamespaceQueryClient.class);
     bind(ProgramRunnerFactory.class).to(DefaultProgramRunnerFactory.class).in(Scopes.SINGLETON);
 
     bind(UGIProvider.class).to(CurrentUGIProvider.class).in(Scopes.SINGLETON);
