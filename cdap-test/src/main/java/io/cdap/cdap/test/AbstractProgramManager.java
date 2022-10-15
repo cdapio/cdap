49a6ab4f41213a800cfda5b3290cf21aceb8756e
Sumeng Wang <sumengwang@google.com>
2022-10-14 19:49:11 -0700

    fix FileUploadServiceTestRun

--- cdap-test/src/main/java/io/cdap/cdap/test/AbstractProgramManager.java
+++ cdap-test/src/main/java/io/cdap/cdap/test/AbstractProgramManager.java
@@ -114,6 +114,16 @@ public abstract class AbstractProgramManager<T extends ProgramManager> implement
   }
 
   @Override
+  public void stop(String version) {
+    ProgramId versionedProgramId = new ProgramId(programId.getNamespace(),
+                                                 programId.getApplication(),
+                                                 version,
+                                                 programId.getType(),
+                                                 programId.getProgram());
+    applicationManager.stopProgram(versionedProgramId);
+  }
+
+  @Override
   public boolean isRunning() {
     return applicationManager.isRunning(programId);
   }
