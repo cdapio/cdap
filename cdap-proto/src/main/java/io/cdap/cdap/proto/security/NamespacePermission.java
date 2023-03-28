a7ac053d46acdce2666ddd7706438080a78581d6
de-lan <delandl@google.com>
2023-03-28 14:25:45 -0700

    feat(CDAP-20496): Add READ_REPOSITORY, WRITE_REPOSITORY, UPDATE_REPOSITORY_METADATA permissions in cdap proto

---
+++ cdap-proto/src/main/java/io/cdap/cdap/proto/security/NamespacePermission.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2023 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.cdap.proto.security;
+
+/**
+ * Permissions specifically for CDAP Namespace.
+ */
+public enum NamespacePermission implements Permission {
+  /**
+   * Permission to read data in the SCM repository of a namespace.
+   */
+  READ_REPOSITORY,
+  /**
+   * Permission to write data in the SCM repository of a namespace.
+   */
+  WRITE_REPOSITORY,
+  /**
+   * Permission to update metadata of the SCM repository of a namespace.
+   */
+  UPDATE_REPOSITORY_METADATA,
+  ;
+
+  @Override
+  public PermissionType getPermissionType() {
+    return PermissionType.NAMESPACE;
+  }
+}
