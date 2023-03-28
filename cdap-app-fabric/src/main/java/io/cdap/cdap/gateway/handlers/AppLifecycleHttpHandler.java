1ed357fabae38fb294f7320e7c68c2c5d13c3e2a
sanjanasandeep <sansans@google.com>
2023-03-28 15:50:57 -0700

    Enforcing access permissions on listAppVersions

--- cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/AppLifecycleHttpHandler.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/gateway/handlers/AppLifecycleHttpHandler.java
@@ -389,6 +389,9 @@ public class AppLifecycleHttpHandler extends AbstractAppFabricHttpHandler {
       @PathParam("namespace-id") final String namespaceId,
       @PathParam("app-id") final String appName) throws Exception {
     validateApplicationId(namespaceId, appName);
+    // enforce LIST privileges on the app
+    accessEnforcer.enforce(new ApplicationId(namespaceId, appName), authenticationContext.getPrincipal(),
+                           StandardPermission.LIST);
     Collection<String> versions = applicationLifecycleService
         .getAppVersions(new ApplicationReference(namespaceId, appName));
     if (versions.isEmpty()) {
