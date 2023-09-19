81c56640c0df758ec3130750f5f7f3fadc604eee
Vitalii Tymchyshyn <vitaliit@google.com>
2023-09-19 15:24:01 -0700

    [CDAP-20807] Update Guava version to match google libraries needs and ensure Errors (e.g. method not found) are properly propagated.

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/provision/task/ProvisioningTask.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/provision/task/ProvisioningTask.java
@@ -152,7 +152,7 @@ public abstract class ProvisioningTask implements RepeatedTask {
       return 0;
     } catch (InterruptedException e) {
       throw e;
-    } catch (Exception e) {
+    } catch (Throwable e) {
       LOG.error("{} task failed in {} state for program run {} due to {}.",
                 currentTaskInfo.getProvisioningOp().getType(), state, programRunId,
                 Exceptions.condenseThrowableMessage(e), e);
@@ -220,7 +220,7 @@ public abstract class ProvisioningTask implements RepeatedTask {
    * @param taskInfo task info for the failure
    * @param e the non-retryable exception
    */
-  protected abstract void handleSubtaskFailure(ProvisioningTaskInfo taskInfo, Exception e);
+  protected abstract void handleSubtaskFailure(ProvisioningTaskInfo taskInfo, Throwable e);
 
   /**
    * Logic to run when task info could not be saved to the ProvisionerStore.
