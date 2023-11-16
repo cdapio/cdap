691c6a1a6c897527cd713bbc87093489e58451de
Vinisha Shah <vinishashah@google.com>
2023-11-16 14:31:34 -0800

    sdf

--- cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/ValidationHandler.java
+++ cdap-app-templates/cdap-etl/cdap-data-pipeline-base/src/main/java/io/cdap/cdap/datapipeline/service/ValidationHandler.java
@@ -132,7 +132,7 @@ public class ValidationHandler extends AbstractSystemHttpServiceHandler {
               namespace), remoteTaskException.getMessage());
     } catch (Exception e) {
       LOG.error("#### Exception occurred: ", e.getMessage());
-      responder.sendError(HttpURLConnection.HTTP_INTERNAL_ERROR, e.get);
+      responder.sendJson(GSON.toJson(e));
     }
   }
 
