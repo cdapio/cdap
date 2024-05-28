70d83eb16be086cd4fce05d6cecc8f1be6136e04
itsankit-google <itsankitjain@google.com>
2024-05-28 11:18:58 +0000

    some commit

--- cdap-api-common/src/main/java/io/cdap/cdap/api/exception/WrappedException.java
+++ cdap-api-common/src/main/java/io/cdap/cdap/api/exception/WrappedException.java
@@ -16,29 +16,43 @@
 
 package io.cdap.cdap.api.exception;
 
+import io.cdap.cdap.error.api.ErrorTagProvider;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Sets the stage name in exception.
  */
-public class WrappedException extends RuntimeException {
+public class WrappedException extends RuntimeException implements ErrorTagProvider {
 
   private final String stageName;
+  private final Set<ErrorTag> errorTags = new HashSet<>();
 
   public WrappedException(Throwable cause, String stageName) {
     super(cause);
     this.stageName = stageName;
+    this.errorTags.add(ErrorTag.PLUGIN);
   }
 
   public WrappedException(String message, String stageName) {
     super(message);
     this.stageName = stageName;
+    this.errorTags.add(ErrorTag.PLUGIN);
   }
 
   public WrappedException(Throwable cause, String message, String stageName) {
     super(message, cause);
     this.stageName = stageName;
+    this.errorTags.add(ErrorTag.PLUGIN);
   }
 
   public String getStageName() {
     return stageName;
+  }
+
+  @Override
+  public Set<ErrorTag> getErrorTags() {
+    return Collections.unmodifiableSet(errorTags);
   }
 }
