53a2332b6fcacd43b461e33254b20bf3319ec22a
Vaibhav Sethi <vaibhavsethi@google.com>
2024-07-11 02:16:17 +0530

    token refactor

--- cdap-ml/src/main/java/io/cdap/cdap/ml/VertexAIService.java
+++ cdap-ml/src/main/java/io/cdap/cdap/ml/VertexAIService.java
@@ -1,29 +1,31 @@
 package io.cdap.cdap.ml;
 
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.Credentials;
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
-import com.google.cloud.vertexai.api.PredictionServiceClient;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
-import java.io.FileInputStream;
+import com.google.common.base.Strings;
 import java.io.IOException;
 
 public class VertexAIService {
 
-  public String summarizePipeline(String project, String location, String modelName, String accessTokenString) throws IOException {
-    AccessToken accessToken = new AccessToken(accessTokenString, null);
-    GoogleCredentials credentials = GoogleCredentials.create(accessToken);
-    VertexAI vertexAI = new VertexAI.Builder()
+  public String summarizePipeline(String project,
+      String location,
+      String modelName,
+      String accessTokenString) throws IOException {
+    VertexAI.Builder vertexAIBuilder = new VertexAI.Builder()
         .setLocation(location)
-        .setProjectId(project)
-        .setCredentials(credentials)
-        .build();
+        .setProjectId(project);
 
+    if (!Strings.isNullOrEmpty(accessTokenString)) {
+      AccessToken accessToken = new AccessToken(accessTokenString, null);
+      GoogleCredentials credentials = GoogleCredentials.create(accessToken);
+      vertexAIBuilder.setCredentials(credentials);
+    }
+
+    VertexAI vertexAI = vertexAIBuilder.build();
     GenerativeModel model = new GenerativeModel(modelName, vertexAI);
     String inputText = "How many colors are in the rainbow?";
     GenerateContentResponse response = model.generateContent(inputText);
