c20eec64acb1d5ba2a472f8f5280c0bcf6602931
Aman Mishra <ramamanmishra@google.com>
2024-07-24 08:14:25 +0000

    Added cdap-ml-ext-vertexai module with changes

--- cdap-ml-spi/src/main/java/io.cdap.cdap.ml.spi/AIService.java
+++ cdap-ml-spi/src/main/java/io.cdap.cdap.ml.spi/AIService.java
@@ -24,13 +24,12 @@ import io.cdap.cdap.proto.ApplicationDetail;
  */
 public interface AIService {
 
-  /**
-   * Summarizes the given application details in mentioned format.
-   * @param applicationDetail The detailed information about the application.
-   * @param format The format in which the summary should be returned.
-   * @return A summarized representation of the application details, formatted according to the specified format.
-   */
-  public String summarizeApp(ApplicationDetail applicationDetail, String format);
+     /**
+      * Summarizes the given application details in mentioned format.
+      * @param applicationDetail The detailed information about the application.
+      * @param format The format in which the summary should be returned.
+      * @return A summarized representation of the application details, formatted according to the specified format.
+      */
+     public String summarizeApp(ApplicationDetail applicationDetail, String format);
 }
-
 
