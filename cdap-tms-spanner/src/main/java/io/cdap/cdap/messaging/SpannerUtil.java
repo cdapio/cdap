7fe6e695e99d3812e7a43b5fae4962584d4fc45d
sidhdirenge <sidhdirenge@google.com>
2024-10-11 16:21:43 +0530

    Fix license issue with newly added files.

--- cdap-tms-spanner/src/main/java/io/cdap/cdap/messaging/SpannerUtil.java
+++ cdap-tms-spanner/src/main/java/io/cdap/cdap/messaging/SpannerUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.cdap.cdap.messaging;
 
 import com.google.cloud.spanner.DatabaseAdminClient;
@@ -7,6 +23,9 @@ import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
 import io.cdap.cdap.common.conf.CConfiguration;
 
+/**
+ * Utility class for spanner messaging service.
+ */
 public class SpannerUtil {
 
   public static DatabaseClient getSpannerDbClient(CConfiguration cConf, Spanner spanner) {
--- cdap-tms-spanner/src/main/java/io/cdap/cdap/messaging/client/SpannerMessagingService.java
+++ cdap-tms-spanner/src/main/java/io/cdap/cdap/messaging/client/SpannerMessagingService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2016 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.cdap.cdap.messaging.client;
 
 import com.google.api.gax.longrunning.OperationFuture;
@@ -39,6 +55,9 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The Spanner implementation of {@link MessagingService}.
+ */
 public class SpannerMessagingService extends AbstractIdleService implements MessagingService {
 
   private static final Logger LOG = LoggerFactory.getLogger(SpannerMessagingService.class);
@@ -199,7 +218,8 @@ public class SpannerMessagingService extends AbstractIdleService implements Mess
       throws TopicNotFoundException, IOException {
     String sqlStatement =
         String.format(
-            "SELECT %s, %s, UNIX_MICROS(%s), %s FROM %s where (payload_sequence_id>-1 and publish_ts > TIMESTAMP_MICROS(%s)) or"
+            "SELECT %s, %s, UNIX_MICROS(%s), %s FROM %s where (payload_sequence_id>-1"
+                + " and publish_ts > TIMESTAMP_MICROS(%s)) or"
                 + " (payload_sequence_id>-1 and publish_ts = TIMESTAMP_MICROS(%s) and sequence_id > %s) order by"
                 + " publish_ts,sequence_id LIMIT %s",
             SpannerMessagingService.SEQUENCE_ID_FIELD,
