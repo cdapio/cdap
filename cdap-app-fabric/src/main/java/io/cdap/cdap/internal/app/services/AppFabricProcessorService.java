35da3eba586ada28433096abbe82347d418ff48c
Vaibhav Sethi <vaibhavsethi@google.com>
2024-10-14 21:22:54 +0530

    Skip starting of netty server and k8s service for appfabric processor

--- cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/AppFabricProcessorService.java
+++ cdap-app-fabric/src/main/java/io/cdap/cdap/internal/app/services/AppFabricProcessorService.java
@@ -27,15 +27,15 @@ import io.cdap.cdap.api.metrics.MetricsCollectionService;
 import io.cdap.cdap.app.runtime.ProgramRuntimeService;
 import io.cdap.cdap.common.conf.CConfiguration;
 import io.cdap.cdap.common.conf.Constants;
-import io.cdap.cdap.common.conf.SConfiguration;
-import io.cdap.cdap.common.discovery.ResolvingDiscoverable;
-import io.cdap.cdap.common.discovery.URIScheme;
+// import io.cdap.cdap.common.conf.SConfiguration;
+// import io.cdap.cdap.common.discovery.ResolvingDiscoverable;
+// import io.cdap.cdap.common.discovery.URIScheme;
 import io.cdap.cdap.common.feature.DefaultFeatureFlagsProvider;
 import io.cdap.cdap.common.http.CommonNettyHttpServiceFactory;
 import io.cdap.cdap.common.logging.LoggingContextAccessor;
 import io.cdap.cdap.common.logging.ServiceLoggingContext;
-import io.cdap.cdap.common.metrics.MetricsReporterHook;
-import io.cdap.cdap.common.security.HttpsEnabler;
+// import io.cdap.cdap.common.metrics.MetricsReporterHook;
+// import io.cdap.cdap.common.security.HttpsEnabler;
 import io.cdap.cdap.features.Feature;
 import io.cdap.cdap.internal.app.store.AppMetadataStore;
 import io.cdap.cdap.internal.bootstrap.BootstrapService;
@@ -53,16 +53,16 @@ import io.cdap.cdap.spi.data.transaction.TransactionRunners;
 import io.cdap.cdap.spi.data.transaction.TxCallable;
 import io.cdap.cdap.store.DefaultNamespaceStore;
 import io.cdap.http.HttpHandler;
-import io.cdap.http.NettyHttpService;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
+// import io.cdap.http.NettyHttpService;
+// import java.net.InetAddress;
+// import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+// import java.util.stream.Collectors;
 import javax.annotation.Nullable;
-import org.apache.twill.common.Cancellable;
+// import org.apache.twill.common.Cancellable;
 import org.apache.twill.discovery.DiscoveryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -75,7 +75,7 @@ public class AppFabricProcessorService extends AbstractIdleService {
   private static final Logger LOG = LoggerFactory.getLogger(AppFabricProcessorService.class);
 
   private final DiscoveryService discoveryService;
-  private final InetAddress hostname;
+  // private final InetAddress hostname;
   private final ProgramRuntimeService programRuntimeService;
   private final ApplicationLifecycleService applicationLifecycleService;
   private final Set<String> servicesNames;
@@ -96,22 +96,22 @@ public class AppFabricProcessorService extends AbstractIdleService {
   private final RepositoryCleanupService repositoryCleanupService;
   private final OperationNotificationSubscriberService operationNotificationSubscriberService;
   private final CConfiguration cConf;
-  private final SConfiguration sConf;
+  // private final SConfiguration sConf;
   private final boolean sslEnabled;
   private final TransactionRunner transactionRunner;
 
-  private Cancellable cancelHttpService;
+  // private Cancellable cancelHttpService;
   private Set<HttpHandler> handlers;
   private MetricsCollectionService metricsCollectionService;
-  private CommonNettyHttpServiceFactory commonNettyHttpServiceFactory;
+  // private CommonNettyHttpServiceFactory commonNettyHttpServiceFactory;
 
   /**
    * Construct the AppFabricServer with service factory and cConf coming from guice injection.
    */
   @Inject
-  public AppFabricProcessorService(CConfiguration cConf, SConfiguration sConf,
+  public AppFabricProcessorService(CConfiguration cConf, /* SConfiguration sConf,*/
       DiscoveryService discoveryService,
-      @Named(Constants.Service.MASTER_SERVICES_BIND_ADDRESS) InetAddress hostname,
+      // @Named(Constants.Service.MASTER_SERVICES_BIND_ADDRESS) InetAddress hostname,
       @Named(Constants.AppFabric.HANDLERS_BINDING) Set<HttpHandler> handlers,
       @Nullable MetricsCollectionService metricsCollectionService,
       ProgramRuntimeService programRuntimeService,
@@ -135,11 +135,11 @@ public class AppFabricProcessorService extends AbstractIdleService {
       SourceControlOperationRunner sourceControlOperationRunner,
       RepositoryCleanupService repositoryCleanupService,
       OperationNotificationSubscriberService operationNotificationSubscriberService) {
-    this.hostname = hostname;
+    // this.hostname = hostname;
     this.discoveryService = discoveryService;
     this.handlers = handlers;
     this.cConf = cConf;
-    this.sConf = sConf;
+    // this.sConf = sConf;
     this.metricsCollectionService = metricsCollectionService;
     this.programRuntimeService = programRuntimeService;
     this.servicesNames = servicesNames;
@@ -159,7 +159,7 @@ public class AppFabricProcessorService extends AbstractIdleService {
     this.transactionRunner = transactionRunner;
     this.runRecordCounterService = runRecordCounterService;
     this.runDataTimeToLiveService = runDataTimeToLiveService;
-    this.commonNettyHttpServiceFactory = commonNettyHttpServiceFactory;
+    // this.commonNettyHttpServiceFactory = commonNettyHttpServiceFactory;
     this.sourceControlOperationRunner = sourceControlOperationRunner;
     this.repositoryCleanupService = repositoryCleanupService;
     this.operationNotificationSubscriberService = operationNotificationSubscriberService;
@@ -199,31 +199,32 @@ public class AppFabricProcessorService extends AbstractIdleService {
     Futures.allAsList(futuresList).get();
 
     // Create handler hooks
-    List<MetricsReporterHook> handlerHooks = handlerHookNames.stream()
-        .map(name -> new MetricsReporterHook(cConf, metricsCollectionService, name))
-        .collect(Collectors.toList());
-
-    // Run http service on random port
-    NettyHttpService.Builder httpServiceBuilder = commonNettyHttpServiceFactory
-        .builder(Constants.Service.APP_FABRIC_HTTP)
-        .setHost(hostname.getCanonicalHostName())
-        .setHandlerHooks(handlerHooks)
-        .setHttpHandlers(handlers)
-        .setConnectionBacklog(cConf.getInt(Constants.AppFabric.BACKLOG_CONNECTIONS,
-            Constants.AppFabric.DEFAULT_BACKLOG))
-        .setExecThreadPoolSize(cConf.getInt(Constants.AppFabric.EXEC_THREADS,
-            Constants.AppFabric.DEFAULT_EXEC_THREADS))
-        .setBossThreadPoolSize(cConf.getInt(Constants.AppFabric.BOSS_THREADS,
-            Constants.AppFabric.DEFAULT_BOSS_THREADS))
-        .setWorkerThreadPoolSize(cConf.getInt(Constants.AppFabric.WORKER_THREADS,
-            Constants.AppFabric.DEFAULT_WORKER_THREADS))
-        .setPort(cConf.getInt(Constants.AppFabric.SERVER_PORT));
-    if (sslEnabled) {
-      new HttpsEnabler().configureKeyStore(cConf, sConf).enable(httpServiceBuilder);
-    }
+    // List<MetricsReporterHook> handlerHooks = handlerHookNames.stream()
+    //     .map(name -> new MetricsReporterHook(cConf, metricsCollectionService, name))
+    //     .collect(Collectors.toList());
 
     // TODO: Remove this and don't start HTTP Service.
-    cancelHttpService = startHttpService(httpServiceBuilder.build());
+    // Run http service on random port
+    // NettyHttpService.Builder httpServiceBuilder = commonNettyHttpServiceFactory
+    //     .builder(Constants.Service.APP_FABRIC_HTTP)
+    //     .setHost(hostname.getCanonicalHostName())
+    //     .setHandlerHooks(handlerHooks)
+    //     .setHttpHandlers(handlers)
+    //     .setConnectionBacklog(cConf.getInt(Constants.AppFabric.BACKLOG_CONNECTIONS,
+    //         Constants.AppFabric.DEFAULT_BACKLOG))
+    //     .setExecThreadPoolSize(cConf.getInt(Constants.AppFabric.EXEC_THREADS,
+    //         Constants.AppFabric.DEFAULT_EXEC_THREADS))
+    //     .setBossThreadPoolSize(cConf.getInt(Constants.AppFabric.BOSS_THREADS,
+    //         Constants.AppFabric.DEFAULT_BOSS_THREADS))
+    //     .setWorkerThreadPoolSize(cConf.getInt(Constants.AppFabric.WORKER_THREADS,
+    //         Constants.AppFabric.DEFAULT_WORKER_THREADS))
+    //     .setPort(cConf.getInt(Constants.AppFabric.SERVER_PORT));
+    // if (sslEnabled) {
+    //   new HttpsEnabler().configureKeyStore(cConf, sConf).enable(httpServiceBuilder);
+    // }
+
+    // TODO: Remove this and don't start HTTP Service.
+    // cancelHttpService = startHttpService(httpServiceBuilder.build());
     long applicationCount = TransactionRunners.run(transactionRunner,
         (TxCallable<Long>) context ->
             AppMetadataStore.create(context).getApplicationCount());
@@ -242,7 +243,7 @@ public class AppFabricProcessorService extends AbstractIdleService {
     coreSchedulerService.stopAndWait();
     bootstrapService.stopAndWait();
     systemAppManagementService.stopAndWait();
-    cancelHttpService.cancel();
+    // cancelHttpService.cancel();
     programRuntimeService.stopAndWait();
     applicationLifecycleService.stopAndWait();
     programNotificationSubscriberService.stopAndWait();
@@ -259,43 +260,44 @@ public class AppFabricProcessorService extends AbstractIdleService {
     operationNotificationSubscriberService.stopAndWait();
   }
 
-  private Cancellable startHttpService(NettyHttpService httpService) throws Exception {
-    httpService.start();
-
-    String announceAddress = cConf.get(Constants.Service.MASTER_SERVICES_ANNOUNCE_ADDRESS,
-        httpService.getBindAddress().getHostName());
-    int announcePort = cConf.getInt(Constants.AppFabric.SERVER_ANNOUNCE_PORT,
-        httpService.getBindAddress().getPort());
-
-    final InetSocketAddress socketAddress = new InetSocketAddress(announceAddress, announcePort);
-    LOG.info("AppFabric HTTP Service announced at {}", socketAddress);
-
-    // Tag the discoverable's payload to mark it as supporting ssl.
-    URIScheme uriScheme = sslEnabled ? URIScheme.HTTPS : URIScheme.HTTP;
-    // TODO accept a list of services, and start them here
-    // When it is running, register it with service discovery
-
-    final List<Cancellable> cancellables = new ArrayList<>();
-    for (final String serviceName : servicesNames) {
-      cancellables.add(discoveryService.register(
-          ResolvingDiscoverable.of(uriScheme.createDiscoverable(serviceName, socketAddress))));
-    }
-
-    return () -> {
-      LOG.debug("Stopping AppFabric HTTP service.");
-      for (Cancellable cancellable : cancellables) {
-        if (cancellable != null) {
-          cancellable.cancel();
-        }
-      }
-
-      try {
-        httpService.stop();
-      } catch (Exception e) {
-        LOG.warn("Exception raised when stopping AppFabric HTTP service", e);
-      }
-
-      LOG.info("AppFabric HTTP service stopped.");
-    };
-  }
+  // TODO: Remove this and don't start HTTP Service.
+  // private Cancellable startHttpService(NettyHttpService httpService) throws Exception {
+  //   httpService.start();
+  //
+  //   String announceAddress = cConf.get(Constants.Service.MASTER_SERVICES_ANNOUNCE_ADDRESS,
+  //       httpService.getBindAddress().getHostName());
+  //   int announcePort = cConf.getInt(Constants.AppFabric.SERVER_ANNOUNCE_PORT,
+  //       httpService.getBindAddress().getPort());
+  //
+  //   final InetSocketAddress socketAddress = new InetSocketAddress(announceAddress, announcePort);
+  //   LOG.info("AppFabric HTTP Service announced at {}", socketAddress);
+  //
+  //   // Tag the discoverable's payload to mark it as supporting ssl.
+  //   URIScheme uriScheme = sslEnabled ? URIScheme.HTTPS : URIScheme.HTTP;
+  //   // TODO accept a list of services, and start them here
+  //   // When it is running, register it with service discovery
+  //
+  //   final List<Cancellable> cancellables = new ArrayList<>();
+  //   for (final String serviceName : servicesNames) {
+  //     cancellables.add(discoveryService.register(
+  //         ResolvingDiscoverable.of(uriScheme.createDiscoverable(serviceName, socketAddress))));
+  //   }
+  //
+  //   return () -> {
+  //     LOG.debug("Stopping AppFabric HTTP service.");
+  //     for (Cancellable cancellable : cancellables) {
+  //       if (cancellable != null) {
+  //         cancellable.cancel();
+  //       }
+  //     }
+  //
+  //     try {
+  //       httpService.stop();
+  //     } catch (Exception e) {
+  //       LOG.warn("Exception raised when stopping AppFabric HTTP service", e);
+  //     }
+  //
+  //     LOG.info("AppFabric HTTP service stopped.");
+  //   };
+  // }
 }
