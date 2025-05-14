0662e2b05ce477b3fc0ecfc5a91d207713c32f4c
sahusanket <sanketsahu@google.com>
2025-05-14 15:32:22 +0530

    Removing GET ...makes it sync and wait

--- cdap-runtime-ext-dataproc/src/main/java/io/cdap/cdap/runtime/spi/runtimejob/ServerlessDataprocRuntimeJobManager.java
+++ cdap-runtime-ext-dataproc/src/main/java/io/cdap/cdap/runtime/spi/runtimejob/ServerlessDataprocRuntimeJobManager.java
@@ -195,8 +195,8 @@ public class ServerlessDataprocRuntimeJobManager extends DataprocRuntimeJobManag
         OperationFuture<Batch, BatchOperationMetadata> submitJobAsOperationAsyncRequest =
           getBatchControllerClient().createBatchAsync(locationName, batch, getBatchId(runInfo));
         LOG.warn("SANKET : afterjobsumbit");
-        LOG.warn("Successfully submitted BATCH job {} to Serverless",
-                 submitJobAsOperationAsyncRequest.get().getName());
+//        LOG.warn("Successfully submitted BATCH job {} to Serverless",
+//                 submitJobAsOperationAsyncRequest.get().getName());
       } catch (AlreadyExistsException ex) {
         //the job id already exists, ignore the job.
         LOG.warn("The dataproc job {} already exists. Ignoring resubmission of the job.",
@@ -409,7 +409,7 @@ public class ServerlessDataprocRuntimeJobManager extends DataprocRuntimeJobManag
 
     ExecutionConfig executionConfig = ExecutionConfig.newBuilder()
       .setNetworkUri(provisionerContext.getProperties().getOrDefault("network", "default"))
-      .setSubnetworkUri(provisionerContext.getProperties().getOrDefault("subnet", ""))
+      .setSubnetworkUri("pga-subnet")
       .build();
 
     //TODO : To make this an advanced option via UI
