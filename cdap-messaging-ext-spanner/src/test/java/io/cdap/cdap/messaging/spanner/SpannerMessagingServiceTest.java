4cf4098dd9982988006dadf8175773340bbeb557
sidhdirenge <sidhdirenge@google.com>
2025-06-11 06:27:14 +0000

    [CDAP-21101] Run Spanner Messaging Service tests using cloud spanner emulator.

--- cdap-messaging-ext-spanner/src/test/java/io/cdap/cdap/messaging/spanner/SpannerMessagingServiceTest.java
+++ cdap-messaging-ext-spanner/src/test/java/io/cdap/cdap/messaging/spanner/SpannerMessagingServiceTest.java
@@ -13,11 +13,21 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package io.cdap.cdap.messaging.spanner;
 
 import static io.cdap.cdap.messaging.spanner.SpannerMessagingService.getMessageId;
 import static java.lang.Thread.sleep;
 
+import com.google.cloud.spanner.DatabaseAdminClient;
+import com.google.cloud.spanner.DatabaseNotFoundException;
+import com.google.cloud.spanner.InstanceAdminClient;
+import com.google.cloud.spanner.InstanceConfigId;
+import com.google.cloud.spanner.InstanceId;
+import com.google.cloud.spanner.InstanceInfo;
+import com.google.cloud.spanner.InstanceNotFoundException;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
 import io.cdap.cdap.api.common.Bytes;
 import io.cdap.cdap.api.dataset.lib.CloseableIterator;
 import io.cdap.cdap.api.messaging.TopicNotFoundException;
@@ -35,32 +45,54 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- * Unit tests for Cloud spanner implementation of the
- * {@link io.cdap.cdap.messaging.spi.MessagingService}. This test needs the following Java
- * properties to run. If they are not provided, tests will be ignored.
+ * Unit tests for the Cloud Spanner implementation of the
+ * {@link io.cdap.cdap.messaging.spi.MessagingService}.
  *
- * <ul>
- *   <li>gcp.project - GCP project name</li>
- *   <li>gcp.spanner.instance - GCP spanner instance name</li>
- *   <li>gcp.spanner.database - GCP spanner database name</li>
- *   <li>(optional) gcp.credentials.path - Local file path to the service account
- *   json that has the "Cloud Spanner Database User" role</li>
- * </ul>
- * TODO [CDAP-21101] : These tests should be part of the continuous build & test runs.
+ * <h2>Setting Up the Spanner Emulator Locally</h2>
+ * To run these tests, you need to set up the Cloud Spanner Emulator on your local machine. Follow
+ * these steps:
+ *
+ * <pre>
+ *   gcloud config configurations create emulator
+ *   gcloud config set auth/disable_credentials true
+ *   gcloud config set api_endpoint_overrides/spanner http://localhost:9010/
+ *   gcloud config configurations activate emulator
+ *   gcloud emulators spanner start
+ * </pre>
+ *
+ * <h2>Configuring IDE while running the test</h2>
+ * To run test, make sure you set the env variable : `SPANNER_EMULATOR_HOST=localhost:9010`. In
+ * Intellij, you will need to edit test configuration and add this env variable.
+ *
+ * <h2>Cleaning Up the Environment</h2>
+ * After running the tests, clean up your environment by executing:
+ * <pre>
+ *   gcloud config unset auth/disable_credentials
+ *   gcloud config unset api_endpoint_overrides/spanner
+ * </pre>
+ * <p>
+ * Note: The Cloud Spanner Emulator is intended for local development and testing purposes only. It
+ * does not persist data across sessions and is not suitable for production use.
  */
 public class SpannerMessagingServiceTest {
 
-  private static SpannerMessagingService service;
+  private static final String EMULATOR_HOST = "localhost:9010";
+  private static final String PROJECT_ID = "test-project";
+  private static final String INSTANCE_ID = "test-instance";
+  private static final String DATABASE_ID = "test-database";
 
   static final List<TopicMetadata> SYSTEM_TOPICS = Arrays.asList(
       new SpannerTopicMetadata(new TopicId("system", "t1"), new HashMap<>()),
@@ -77,28 +109,63 @@ public class SpannerMessagingServiceTest {
     }
   });
 
+  private static final Comparator<TopicId> TOPIC_ID_COMPARATOR = Comparator.comparing(
+      TopicId::getNamespace).thenComparing(TopicId::getTopic);
+
+  private static SpannerMessagingService service;
+  private static DatabaseAdminClient dbAdminClient;
+  private static InstanceAdminClient adminClient;
+  private static Spanner spanner;
+
+  // Indicates whether the Spanner emulator is active, guiding cleanup decisions.
+  private static boolean isEmulatorRunning;
+
   @BeforeClass
-  public static void createSpannerMessagingService() throws Exception {
-    String project = System.getProperty("gcp.project");
-    String instance = System.getProperty("gcp.spanner.instance");
-    String database = System.getProperty("gcp.spanner.database");
-    String credentialsPath = System.getProperty("gcp.credentials.path");
+  public static void setUp() throws Exception {
+    // Spanner Emulator host not set. Skipping test.
+    Assume.assumeNotNull(System.getenv(SpannerUtil.SPANNER_EMULATOR_HOST));
 
-    // GCP project, instance, and database must be provided
-    Assume.assumeNotNull(project, instance, database);
+    // Initialize Spanner service.
+    SpannerOptions options = SpannerOptions.newBuilder()
+        .setEmulatorHost(EMULATOR_HOST)
+        .setProjectId(PROJECT_ID)
+        .build();
+    // If emulator is not running, but the env variable SPANNER_EMULATOR_HOST is still set,
+    // Exception is thrown here. Thus, isEmulatorRunning variable is not set to true.
+    spanner = options.getService();
+    dbAdminClient = spanner.getDatabaseAdminClient();
+    adminClient = spanner.getInstanceAdminClient();
 
-    Map<String, String> configs = new HashMap<>();
-    configs.put(SpannerUtil.PROJECT, project);
-    configs.put(SpannerUtil.INSTANCE, instance);
-    configs.put(SpannerUtil.DATABASE, database);
+    // Cleanup of Spanner resources is necessary only if the emulator is operational.
+    // Without this check, the test may hang indefinitely due to the asynchronous nature of dropDatabase().
+    isEmulatorRunning = true;
 
-    if (credentialsPath != null) {
-      configs.put(SpannerUtil.CREDENTIALS_PATH, credentialsPath);
+    // Create instance if it doesn't exist.
+    try {
+      adminClient.createInstance(InstanceInfo.newBuilder(InstanceId.of(PROJECT_ID, INSTANCE_ID))
+          .setInstanceConfigId(InstanceConfigId.of(PROJECT_ID, "regional-us-central1"))
+          .setDisplayName("Test Instance")
+          .build()).get();
+    } catch (ExecutionException e) {
+      // Instance already exists, proceed.
     }
 
+    // Create database if it doesn't exist.
+    try {
+      dbAdminClient.createDatabase(INSTANCE_ID, DATABASE_ID, Collections.emptyList()).get();
+    } catch (ExecutionException e) {
+      // DB already exists, proceed.
+    }
+
+    // Initialize messaging service.
+    Map<String, String> configs = new HashMap<>();
+    configs.put(SpannerUtil.PROJECT, PROJECT_ID);
+    configs.put(SpannerUtil.INSTANCE, INSTANCE_ID);
+    configs.put(SpannerUtil.DATABASE, DATABASE_ID);
     configs.put(SpannerUtil.PUBLISH_DELAY_MILLIS, "2");
     configs.put(SpannerUtil.PUBLISH_BATCH_SIZE, "2");
     configs.put(SpannerUtil.PUBLISH_BATCH_TIMEOUT_MILLIS, "5");
+    configs.put(SpannerUtil.SPANNER_EMULATOR_HOST, EMULATOR_HOST);
     MessagingServiceContext context = new MockMessagingServiceContext(configs);
 
     service = new SpannerMessagingService();
@@ -106,14 +173,54 @@ public class SpannerMessagingServiceTest {
   }
 
   @After
-  public void cleanUp() throws Exception {
-    try {
-      service.deleteTopic(SIMPLE_TOPIC.getTopicId());
-      service.deleteTopic(TOPIC_WITH_PROPERTIES.getTopicId());
-      service.deleteTopic(SYSTEM_TOPICS.get(0).getTopicId());
-      service.deleteTopic(SYSTEM_TOPICS.get(1).getTopicId());
-    } catch (TopicNotFoundException e) {
-      // no-op
+  public void deleteTopics() {
+    List<TopicId> topicsToClean = Arrays.asList(
+        SIMPLE_TOPIC.getTopicId(),
+        TOPIC_WITH_PROPERTIES.getTopicId(),
+        SYSTEM_TOPICS.get(0).getTopicId(),
+        SYSTEM_TOPICS.get(1).getTopicId()
+    );
+
+    for (TopicId topicId : topicsToClean) {
+      try {
+        service.deleteTopic(topicId);
+      } catch (TopicNotFoundException e) {
+        // Topic not found, proceed.
+      } catch (Exception e) {
+        throw new RuntimeException("Failed to clean up topic " + topicId.getTopic(), e);
+      }
+    }
+  }
+
+  @AfterClass
+  public static void cleanUp() {
+    if (isEmulatorRunning) {
+      // Drop the database.
+      if (dbAdminClient != null) {
+        try {
+          dbAdminClient.dropDatabase(INSTANCE_ID, DATABASE_ID);
+        } catch (DatabaseNotFoundException e) {
+          // Database not found, proceed.
+        } catch (Exception e) {
+          throw new RuntimeException("Failed to drop database during @AfterClass cleanup", e);
+        }
+      }
+
+      // Delete the instance.
+      if (adminClient != null) {
+        try {
+          adminClient.deleteInstance(INSTANCE_ID);
+        } catch (InstanceNotFoundException e) {
+          // Instance not found, proceed.
+        } catch (Exception e) {
+          throw new RuntimeException("Failed to delete instance during @AfterClass cleanup", e);
+        }
+      }
+
+      // Close the main Spanner client.
+      if (spanner != null) {
+        spanner.close();
+      }
     }
   }
 
@@ -149,14 +256,21 @@ public class SpannerMessagingServiceTest {
   public void testListTopics() throws Exception {
     service.createTopic(SIMPLE_TOPIC);
     service.createTopic(TOPIC_WITH_PROPERTIES);
-    List<TopicId> topics = service.listTopics(new NamespaceId("system"));
-    Assert.assertEquals(Arrays.asList(new TopicId("system",
+    List<TopicId> actualTopics = new ArrayList<>(service.listTopics(new NamespaceId("system")));
+
+    // Create the expected list
+    List<TopicId> expectedTopics = new ArrayList<>(Arrays.asList(new TopicId("system",
             SpannerMessagingService.getTableName(SYSTEM_TOPICS.get(0).getTopicId())),
         new TopicId("system",
             SpannerMessagingService.getTableName(SYSTEM_TOPICS.get(1).getTopicId())),
         new TopicId("system", SpannerMessagingService.getTableName(SIMPLE_TOPIC.getTopicId())),
         new TopicId("system",
-            SpannerMessagingService.getTableName(TOPIC_WITH_PROPERTIES.getTopicId()))), topics);
+            SpannerMessagingService.getTableName(TOPIC_WITH_PROPERTIES.getTopicId()))));
+
+    // Sort both lists before comparison
+    actualTopics.sort(TOPIC_ID_COMPARATOR);
+    expectedTopics.sort(TOPIC_ID_COMPARATOR);
+    Assert.assertEquals(expectedTopics, actualTopics);
   }
 
   @Test
@@ -199,8 +313,8 @@ public class SpannerMessagingServiceTest {
     List<String> messagesBatch = Arrays.asList("message_0", "message_1", "message_2");
     service.publish(new SpannerStoreRequest(SIMPLE_TOPIC.getTopicId(),
         Collections.singletonList(messagesBatch.get(0))));
-    long firstMsgTimestampMicros = System.currentTimeMillis() * 1000 + 4;
-    sleep(5);
+    long firstMsgTimestampMicros = (System.currentTimeMillis() + 6) * 1000;
+    sleep(10);
     service.publish(new SpannerStoreRequest(SIMPLE_TOPIC.getTopicId(),
         Collections.singletonList(messagesBatch.get(1))));
     service.publish(new SpannerStoreRequest(SIMPLE_TOPIC.getTopicId(),
