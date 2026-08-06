746118ab2a3c058b909994af1242cc71aeca3e11
shruthi713 <venkataramansh@google.com>
2026-08-06 07:12:35 +0000

    test(rbac): hardcode fallback timeout to 90s for testing

--- cdap-common/src/main/java/io/cdap/cdap/common/internal/remote/RemoteTaskExecutor.java
+++ cdap-common/src/main/java/io/cdap/cdap/common/internal/remote/RemoteTaskExecutor.java
@@ -105,10 +105,7 @@ public class RemoteTaskExecutor {
         Constants.Gateway.INTERNAL_API_VERSION_3);
     this.metricsCollectionService = metricsCollectionService;
     this.userEncryptionAeadCipher = aeadCipher;
-    String configPrefix = workerType == Type.TASK_WORKER
-        ? Constants.Service.TASK_WORKER : Constants.Service.SYSTEM_WORKER;
-    long maxTimeSecs = cConf.getLong(configPrefix + "." + Constants.Retry.MAX_TIME_SECS, 60L);
-    this.fallbackTimeoutMs = maxTimeSecs * 1000;
+    this.fallbackTimeoutMs = 90000; // Hardcoded to 90s for testing
     if (workerType == Type.TASK_WORKER) {
       this.workerUrl = TASK_WORKER_URL;
       this.retryStrategy = RetryStrategies.fromConfiguration(cConf,
